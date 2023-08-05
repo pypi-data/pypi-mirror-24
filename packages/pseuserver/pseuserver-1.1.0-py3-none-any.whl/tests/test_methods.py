f58c30b2936744653489f7d559f8f5fc85906491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:02 +0000

    Add package pseuserver/1.1.0/pseuserver-1.1.0-py3-none-any.whl

---
+++ packages/pseuserver/pseuserver-1.1.0-py3-none-any.whl/tests/test_methods.py
@@ -0,0 +1,48 @@
+import unittest
+from flask  import Flask, abort, request, make_response, current_app , Response
+from pseuserver import *
+from pseuserver.methods import *
+import pseuserver
+import requests
+import json
+from pprint import pprint as pp
+import os
+from pseudb import *
+# from pseudb.storages import JSONStorage, MemoryStorage
+
+
+class TestMethods(unittest.TestCase):
+    def setUp(self):
+        # pseuserver.DEFAULT_CONFIG = 'test.config.json'
+        # _db = 'test.db.json'
+        # this_dir = os.path.dirname(os.path.realpath(__file__))
+        
+        # self.db_file = os.path.join(this_dir, _db)
+        # self.db = PseuDB(self.db_file)
+
+
+
+    # def tearDown(self):
+    #     self.db.close()
+
+    #     if os.path.exists(self.db_file):
+    #         os.remove(self.db_file)
+
+    
+
+    def test_output_json(self):
+        self.db.purge_tables()
+        kwargs = {}
+        kwargs[CONFIG_DB] = self.db_file
+        kwargs[RESOURCE_DOCUMENT] = 'posts'
+        kwargs[RESOURCE_DATA] = b'{\"text\": \"post 1\", \"author\": \"harry\" }'
+
+        result = create(**kwargs)
+
+        self.assertEqual( result ,
+            {'id':1, 'text': 'post 1', 'author': 'harry'})
+
+    
+
+if __name__ == "__main__":
+    unittest.main()            
