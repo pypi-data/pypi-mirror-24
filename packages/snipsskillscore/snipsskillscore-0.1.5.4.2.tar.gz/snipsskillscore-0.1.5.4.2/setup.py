394f12b65403ab3361870358f8ef4f2858316921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package snipsskillscore/0.1.5.4.2/snipsskillscore-0.1.5.4.2.tar.gz

---
+++ packages/snipsskillscore/snipsskillscore-0.1.5.4.2.tar.gz/snipsskillscore-0.1.5.4.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='snipsskillscore',
+    version='0.1.5.4.2',
+    description='The Snips skills core utilities for creating end-to-end assistants',
+    author='Michael Fester',
+    author_email='michael.fester@gmail.com',
+    url='https://github.com/snipsco/snips-skills-core',
+    download_url='',
+    license='MIT',
+    install_requires=[
+        'hidapi',
+        'webrtcvad',
+        'paho-mqtt',
+        'pyyaml',
+        'pyaudio',
+        'pygame',
+        'pyusb',
+        'respeaker',
+        'gTTS'
+    ],
+    dependency_links=[
+        "hg+http://bitbucket.org/pygame/pygame",
+        "git+https://github.com/snipsco/respeaker_python_library.git#egg=respeaker"
+    ],
+    test_suite="tests",
+    keywords=['snips'],
+    packages=[
+        'snipsskillscore'
+    ]
+)
