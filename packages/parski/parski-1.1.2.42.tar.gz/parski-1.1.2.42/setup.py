9c76ef8ce208dc964ccca557fa86e5ec82107f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:26 +0000

    Add package parski/1.1.2.42/parski-1.1.2.42.tar.gz

---
+++ packages/parski/parski-1.1.2.42.tar.gz/parski-1.1.2.42/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+setup(
+  name = 'parski',
+  packages = ['parski'],
+  version = '1.1.2.42',
+  description = 'Helper library for parsing vpc data feed',
+  author = 'Russell Endicott',
+  author_email = 'rendicott@gmail.com',
+  url = 'https://github.build.company.com/CloudPod/parski', # use the URL to the github repo
+  download_url = 'https://github.build.company.com/CloudPod/parski/releases/download/1.1.2.42/parski-1.1.2.42.tar.gz', # I'll explain this in a second
+  keywords = ['vpc', 'json', 'parski'],
+  classifiers = [],
+)
