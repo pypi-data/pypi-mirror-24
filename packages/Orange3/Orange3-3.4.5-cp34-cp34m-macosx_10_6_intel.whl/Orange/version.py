2c93cd8acb1890666f3072df02ba192b8e35b820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:51 +0000

    Add package Orange3/3.4.5/Orange3-3.4.5-cp34-cp34m-macosx_10_6_intel.whl

---
+++ packages/Orange3/Orange3-3.4.5-cp34-cp34m-macosx_10_6_intel.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.4.5'
+version = '3.4.5'
+full_version = '3.4.5'
+git_revision = '68d6782040ee20e6ba756c0b5585354d3defd8a7'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
