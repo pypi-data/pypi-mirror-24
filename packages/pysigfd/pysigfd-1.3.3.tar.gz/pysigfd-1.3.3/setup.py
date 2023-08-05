f7996e3718c2b74dcf33b1c62904edaba0afcca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:45 +0000

    Add package pysigfd/1.3.3/pysigfd-1.3.3.tar.gz

---
+++ packages/pysigfd/pysigfd-1.3.3.tar.gz/pysigfd-1.3.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+import sys
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only python version 3 is supported")
+
+setuptools.setup(
+    name='pysigfd',
+    version='1.3.3',
+    description='pysigfd is a module to help you deal with signals within python using the linux signal file \
+    description construct',
+    long_description='pysigfd is a module to help you deal with signals within python using the linux signal file description construct',
+    url='https://github.com/veltzer/pysigfd',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='PSF',
+    classifiers=[
+    ],
+    keywords='signalfd python3 linux',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'cffi',
+    ],
+)
