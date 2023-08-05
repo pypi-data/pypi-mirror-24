b9fa969b763d4031b3d02b6d8ab03b1d05d0f217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package aruudy/0.1.1/aruudy-0.1.1.tar.gz

---
+++ packages/aruudy/aruudy-0.1.1.tar.gz/aruudy-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+setup(
+    name = "aruudy",
+    version = "0.1.1",
+    author = "Abdelkrime Aries",
+    author_email = "kariminfo0@gmail.com",
+    description = ("Arabic poetry package"),
+    license = "Apache-2.0",
+    keywords = "arabic nlp languages poetry",
+    url = "https://github.com/kariminf/aruudy",
+    packages=['aruudy'],
+    long_description=readme()
+)
