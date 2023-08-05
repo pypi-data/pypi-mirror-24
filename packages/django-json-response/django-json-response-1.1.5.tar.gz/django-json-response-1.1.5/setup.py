522669c14d84f3b2696b2f38f633aa2bf00816ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:10 +0000

    Add package django-json-response/1.1.5/django-json-response-1.1.5.tar.gz

---
+++ packages/django-json-response/django-json-response-1.1.5.tar.gz/django-json-response-1.1.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+
+from setuptools import setup
+
+
+version = '1.1.5'
+
+
+setup(
+    name='django-json-response',
+    version=version,
+    keywords='django-json-response django-jsonp-response',
+    description="DIY Django's JsonResponse and JsonpResponse",
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/django-xxx/django-json-response',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    py_modules=['json_response'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
