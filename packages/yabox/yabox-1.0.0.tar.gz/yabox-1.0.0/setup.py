9a26d0a5079f8dd8be7fcfb1e8e6a894d6c5dfb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package yabox/1.0.0/yabox-1.0.0.tar.gz

---
+++ packages/yabox/yabox-1.0.0.tar.gz/yabox-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='yabox',
+    version='1.0.0',
+    description='Yet another black-box optimization library for Python',
+    author='Pablo Rodriguez-Mier',
+    author_email="pablo.rodriguez.mier@gmail.com",
+    packages=['yabox'],
+    install_requires=['numpy>=1.7'],
+    extras_require={
+        'notebooks': ['jupyter>=1.0.0'],
+        'visualization': ['matplotlib>=1.3']},
+    tests_require=['pytest', 'pytest-pep8'],
+    url='https://github.com/pablormier/yabox',
+    download_url='https://github.com/pablormier/yabox/archive/1.0.0.tar.gz',
+    keywords=['optimization', 'black-box', 'data science', 'evolutionary', 'algorithms'],
+    license='Apache License 2.0',
+    classifiers=['Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'Intended Audience :: Education',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Operating System :: Microsoft :: Windows',
+                 'Programming Language :: Python :: 3.4'],
+)
