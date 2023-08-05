23d512afd063ff5f8c9926ae3937a14d5d55e05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package trunofficial/0.1.7/trunofficial-0.1.7-py2.py3-none-any.whl

---
+++ packages/trunofficial/trunofficial-0.1.7-py2.py3-none-any.whl/trunofficial-0.1.7.dist-info/DESCRIPTION.rst
@@ -0,0 +1,67 @@
+trunofficial
+============
+
+|pypi.python.org| |readthedocs.org| |build Status|
+
+Unofficial API to the Truecaller phone number search.
+
+Installation
+------------
+
+``pip install trunofficial``
+
+or if you like to live on the bleeding edge:
+
+::
+
+    python setup.py install
+
+Usage Examples
+--------------
+
+::
+
+    import trunofficial
+
+    owner = trunofficial.search('2024561111')
+    print(owner.name)
+
+    mobile = owner.phone
+    print(mobile.number)
+    print(mobile.countrycode)
+    print(mobile.carrier)
+
+    house = owner.address
+    print(house.city)
+    print(house.timezone)
+
+For more examples, check the `examples directory <examples>`__ or read
+the full
+`Documentation <http://trunofficial.readthedocs.io/en/latest/>`__.
+
+Running Tests
+-------------
+
+::
+
+    python -m pytest test
+
+Disclaimer
+----------
+
+This method of accessing Truecaller's database may stop working any
+moment. Use it at your own risk.
+
+License
+-------
+
+``The MIT License``
+
+.. |pypi.python.org| image:: https://img.shields.io/pypi/v/trunofficial.svg
+   :target: https://pypi.org/project/trunofficial/
+.. |readthedocs.org| image:: https://readthedocs.org/projects/trunofficial/badge/?version=latest
+   :target: http://trunofficial.readthedocs.io/en/latest/
+.. |build Status| image:: https://travis-ci.org/ritiek/trunofficial.svg?branch=master
+   :target: https://travis-ci.org/ritiek/trunofficial/
+
+
---
+++ packages/trunofficial/trunofficial-0.1.7-py2.py3-none-any.whl/trunofficial/trunofficial.py
@@ -0,0 +1,95 @@
+try:
+    from urllib import urlopen
+    from urllib import urlencode
+except ImportError:
+    from urllib.request import urlopen
+    from urllib.parse import urlencode
+import json
+
+
+class TruecallerError(Exception):
+    def __init__(self, message=None):
+        super(TruecallerError, self).__init__(message)
+
+
+def search(*numbers):
+    if len(numbers) > 1:
+        results = []
+        for number in numbers:
+            result = _MainSearch(number)
+            results.append(result)
+        return results
+    else:
+        return _MainSearch(numbers[0])
+
+
+class _MainSearch:
+    def __init__(self, number):
+        URL = 'https://search5.truecaller.com/v2/search?'
+
+        raw_params = {
+            'q': number,
+            'type': '4',
+            'locAddr': '',
+            'placement': 'SEARCHRESULTS,HISTORY,DETAILS',
+            'clientId': '1',
+            'myNumber': 'lS5757de85c2804a87d452c139OpYeO6gR6qlj0QFJJQMpo1',
+            'registerId': '568140610',
+            'encoding': 'json',
+        }
+
+        params = urlencode(raw_params)
+        url_params = URL + params
+        response = urlopen(url_params).read()
+        response = response.decode('utf-8')
+        parsed = json.loads(response)
+
+        try:
+            basic = parsed['data'][0]
+            phone_parsed = parsed['data'][0]['phones'][0]
+            address_parsed = parsed['data'][0]['addresses'][0]
+
+            self.id = basic['id']
+            self.name = basic['name']
+            self.score = basic['score']
+            self.access = basic['access']
+            self.enhanced = basic['enhanced']
+            self.internet_address = basic['internetAddresses']
+            self.badges = basic['badges']
+            self.tags = basic['tags']
+            self.sources = basic['sources']
+
+            self.phone = _Phone(phone_parsed)
+            self.address = _Address(address_parsed)
+
+            self.provider = parsed['provider']
+            self.trace = parsed['trace']
+            self.sourcestats = parsed['stats']['sourceStats']
+
+        except KeyError:
+            raise TruecallerError("Cannot find the number in Truecaller database, " + str(number) + '.')
+
+
+class _Phone:
+    def __init__(self, phone):
+        self.number = phone['e164Format']
+        self.numbertype= phone['numberType']
+        self.national = phone['nationalFormat']
+        self.dialcode = phone['dialingCode']
+        self.countrycode = phone['countryCode']
+        self.carrier = phone['carrier']
+        try:
+            self.spamscore = phone['spamScore']
+            self.spamtype = phone['spamType']
+        except KeyError:
+            self.spamscore = None
+            self.spamtype = None
+        self.phonetype = phone['type']
+
+class _Address:
+    def __init__(self, address):
+        self.area = address['area']
+        self.city = address['city']
+        self.countrycode = address['countryCode']
+        self.timezone = address['timeZone']
+        self.type = address['type']
