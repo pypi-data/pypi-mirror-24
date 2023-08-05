8f052293a03089c30feeccf63bdb1093ffce54c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:08 +0000

    Add package opflow/0.1.0/opflow-0.1.0.tar.gz

---
+++ packages/opflow/opflow-0.1.0.tar.gz/opflow-0.1.0/opflow/__init__.py
@@ -0,0 +1,11 @@
+
+__title__ = 'opflow'
+__version__ = '0.1.0'
+__author__ = 'Devebot'
+__email__ = 'contact@devebot.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Devebot <contact@devebot.com>'
+
+def sayHello():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
