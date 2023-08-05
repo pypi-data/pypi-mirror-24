a160ba5cee76674908a1c109795cf416447d5f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package Axelrod/3.4.0/Axelrod-3.4.0-py2.py3-none-any.whl

---
+++ packages/Axelrod/Axelrod-3.4.0-py2.py3-none-any.whl/axelrod/fingerprint.py
@@ -0,0 +1,378 @@
+from collections import namedtuple
+from tempfile import NamedTemporaryFile
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+import axelrod as axl
+from axelrod import on_windows, Player
+from axelrod.strategy_transformers import JossAnnTransformer, DualTransformer
+from axelrod.interaction_utils import (
+    compute_final_score_per_turn, read_interactions_from_file)
+
+from typing import List, Any, Union
+
+Point = namedtuple('Point', 'x y')
+
+
+def create_points(step: float, progress_bar: bool = True) -> List[Point]:
+    """Creates a set of Points over the unit square.
+
+    A Point has coordinates (x, y). This function constructs points that are
+    separated by a step equal to `step`. The points are over the unit
+    square which implies that the number created will be (1/`step` + 1)^2.
+
+    Parameters
+    ----------
+    step : float
+        The separation between each Point. Smaller steps will produce more
+        Points with coordinates that will be closer together.
+    progress_bar : bool
+        Whether or not to create a progress bar which will be updated
+
+    Returns
+    ----------
+    points : list
+        of Point objects with coordinates (x, y)
+    """
+    num = int((1 / step) // 1) + 1
+
+    if progress_bar:
+        p_bar = tqdm.tqdm(total=num ** 2, desc="Generating points")
+
+    points = []
+    for x in np.linspace(0, 1, num):
+        for y in np.linspace(0, 1, num):
+            points.append(Point(x, y))
+
+            if progress_bar:
+                p_bar.update()
+
+    if progress_bar:
+        p_bar.close()
+
+    return points
+
+
+def create_jossann(point: Point, probe: Any) -> Player:
+    """Creates a JossAnn probe player that matches the Point.
+
+    If the coordinates of point sums to more than 1 the parameters are
+    flipped and subtracted from 1 to give meaningful probabilities. We also
+    use the Dual of the probe. This is outlined further in [Ashlock2010]_.
+
+    Parameters
+    ----------
+    point : Point
+    probe : class or instance
+        A class that must be descended from axelrod.Player or an instance of
+        axelrod.Player.
+
+    Returns
+    ----------
+    joss_ann: Joss-AnnTitForTat object
+        `JossAnnTransformer` with parameters that correspond to `point`.
+    """
+    x, y = point
+
+    if isinstance(probe, axl.Player):
+        init_kwargs = probe.init_kwargs
+        probe = probe.__class__
+    else:
+        init_kwargs = {}
+
+    if x + y >= 1:
+        joss_ann = DualTransformer()(
+            JossAnnTransformer((1 - x, 1 - y))(probe))(**init_kwargs)
+    else:
+        joss_ann = JossAnnTransformer((x, y))(probe)(**init_kwargs)
+    return joss_ann
+
+
+def create_probes(probe: Union[type, Player], points: list,
+                  progress_bar: bool = True) -> List[Player]:
+    """Creates a set of probe strategies over the unit square.
+
+    Constructs probe strategies that correspond to points with coordinates
+    (x, y). The probes are created using the `JossAnnTransformer`.
+
+    Parameters
+    ----------
+    probe : class or instance
+        A class that must be descended from axelrod.Player or an instance of
+        axelrod.Player.
+    points : list
+        of Point objects with coordinates (x, y)
+    progress_bar : bool
+        Whether or not to create a progress bar which will be updated
+
+    Returns
+    ----------
+    probes : list
+        A list of `JossAnnTransformer` players with parameters that
+        correspond to point.
+    """
+    if progress_bar:
+        points = tqdm.tqdm(points, desc="Generating probes")
+    probes = [create_jossann(point, probe) for point in points]
+    return probes
+
+
+def create_edges(points: List[Point], progress_bar: bool = True) -> list:
+    """Creates a set of edges for a spatial tournament.
+
+    Constructs edges that correspond to `points`. All edges begin at 0, and
+    connect to the index +1 of the probe.
+
+    Parameters
+    ----------
+    points : list
+        of Point objects with coordinates (x, y)
+    progress_bar : bool
+        Whether or not to create a progress bar which will be updated
+
+
+    Returns
+    ----------
+    edges : list of tuples
+        A list containing tuples of length 2. All tuples will have 0 as the
+        first element. The second element is the index of the
+        corresponding probe (+1 to allow for including the Strategy).
+    """
+    if progress_bar:
+        points = tqdm.tqdm(points, desc="Generating network edges")
+    edges = [(0, index + 1) for index, point in enumerate(points)]
+    return edges
+
+
+def generate_data(interactions: dict, points: list, edges: list) -> dict:
+    """Generates useful data from a spatial tournament.
+
+    Matches interactions from `results` to their corresponding Point in
+    `probe_points`.
+
+    Parameters
+    ----------
+    interactions : dictionary
+        A dictionary mapping edges to the corresponding interactions of
+        those players.
+    points : list
+        of Point objects with coordinates (x, y).
+    edges : list of tuples
+        A list containing tuples of length 2. All tuples will have either 0
+        or 1 as the first element. The second element is the index of the
+        corresponding probe (+1 to allow for including the Strategy).
+
+    Returns
+    ----------
+    point_scores : dictionary
+        A dictionary where the keys are Points of the form (x, y) and
+        the values are the mean score for the corresponding interactions.
+    """
+    edge_scores = [np.mean([
+        compute_final_score_per_turn(scores)[0]
+        for scores in interactions[edge]])
+        for edge in edges
+    ]
+    point_scores = dict(zip(points, edge_scores))
+    return point_scores
+
+
+def reshape_data(data: dict, points: list, size: int) -> np.ndarray:
+    """Shape the data so that it can be plotted easily.
+
+    Parameters
+    ----------
+    data : dictionary
+        A dictionary where the keys are Points of the form (x, y) and
+        the values are the mean score for the corresponding interactions.
+
+    points : list
+        of Point objects with coordinates (x, y).
+
+    size : int
+        The number of Points in every row/column.
+
+    Returns
+    ----------
+    plotting_data : list
+        2-D numpy array of the scores, correctly shaped to ensure that the
+        score corresponding to Point (0, 0) is in the left hand corner ie.
+        the standard origin.
+    """
+    ordered_data = [data[point] for point in points]
+    shaped_data = np.reshape(ordered_data, (size, size), order='F')
+    plotting_data = np.flipud(shaped_data)
+    return plotting_data
+
+
+class AshlockFingerprint(object):
+    def __init__(self, strategy: Union[type, Player],
+                 probe: Union[type, Player]=axl.TitForTat) -> None:
+        """
+        Parameters
+        ----------
+        strategy : class or instance
+            A class that must be descended from axelrod.Player or an instance of
+            axelrod.Player.
+        probe : class or instance
+            A class that must be descended from axelrod.Player or an instance of
+            axelrod.Player.
+            Default: Tit For Tat
+        """
+        self.strategy = strategy
+        self.probe = probe
+
+    def construct_tournament_elements(self, step: float,
+                                      progress_bar: bool = True) -> tuple:
+        """Build the elements required for a spatial tournament
+
+        Parameters
+        ----------
+        step : float
+            The separation between each Point. Smaller steps will
+            produce more Points that will be closer together.
+        progress_bar : bool
+            Whether or not to create a progress bar which will be updated
+
+
+        Returns
+        ----------
+        edges : list of tuples
+            A list containing tuples of length 2. All tuples will have either 0
+            or 1 as the first element. The second element is the index of the
+            corresponding probe (+1 to allow for including the Strategy).
+
+        tournament_players : list
+            A list containing instances of axelrod.Player. The first item is the
+            original player, the rest are the probes.
+
+        """
+        self.points = create_points(step, progress_bar=progress_bar)
+        edges = create_edges(self.points, progress_bar=progress_bar)
+        probe_players = create_probes(self.probe, self.points,
+                                      progress_bar=progress_bar)
+
+        if isinstance(self.strategy, axl.Player):
+            tournament_players = [self.strategy] + probe_players
+        else:
+            tournament_players = [self.strategy()] + probe_players
+
+        return edges, tournament_players
+
+    def fingerprint(
+        self, turns: int = 50, repetitions: int = 10, step: float = 0.01,
+        processes: int=None, filename: str = None, in_memory: bool = False,
+        progress_bar: bool = True
+    ) -> dict:
+        """Build and play the spatial tournament.
+
+        Creates the probes and their edges then builds a spatial tournament.
+        When the coordinates of the probe sum to more than 1, the dual of the
+        probe is taken instead and then the Joss-Ann Transformer is applied. If
+        the coordinates sum to less than 1 (or equal), then only the Joss-Ann is
+        applied, a dual is not required.
+
+        Parameters
+        ----------
+        turns : integer, optional
+            The number of turns per match
+        repetitions : integer, optional
+            The number of times the round robin should be repeated
+        step : float, optional
+            The separation between each Point. Smaller steps will
+            produce more Points that will be closer together.
+        processes : integer, optional
+            The number of processes to be used for parallel processing
+        filename: string, optional
+            The name of the file for self.spatial_tournament's interactions.
+            if None and in_memory=False, will auto-generate a filename.
+        in_memory: bool
+            Whether self.spatial_tournament keeps interactions_dict in memory or
+            in a file.
+        progress_bar : bool
+            Whether or not to create a progress bar which will be updated
+
+        Returns
+        ----------
+        self.data : dictionary
+            A dictionary where the keys are coordinates of the form (x, y) and
+            the values are the mean score for the corresponding interactions.
+        """
+
+        if on_windows and (filename is None):  # pragma: no cover
+            in_memory = True
+        elif filename is None:
+            outputfile = NamedTemporaryFile(mode='w')
+            filename = outputfile.name
+
+        edges, tourn_players = self.construct_tournament_elements(
+            step, progress_bar=progress_bar)
+
+        self.step = step
+        self.spatial_tournament = axl.Tournament(tourn_players, turns=turns,
+                                                 repetitions=repetitions,
+                                                 edges=edges)
+        self.spatial_tournament.play(build_results=False,
+                                     filename=filename,
+                                     processes=processes,
+                                     in_memory=in_memory,
+                                     progress_bar=progress_bar)
+        if in_memory:
+            self.interactions = self.spatial_tournament.interactions_dict
+        else:
+            self.interactions = read_interactions_from_file(
+                filename, progress_bar=progress_bar)
+
+        self.data = generate_data(self.interactions, self.points, edges)
+        return self.data
+
+    def plot(self, cmap: str = 'seismic', interpolation: str = 'none',
+             title: str = None, colorbar: bool = True,
+             labels: bool = True) -> plt.Figure:
+        """Plot the results of the spatial tournament.
+
+        Parameters
+        ----------
+        cmap : str, optional
+            A matplotlib colour map, full list can be found at
+            http://matplotlib.org/examples/color/colormaps_reference.html
+        interpolation : str, optional
+            A matplotlib interpolation, full list can be found at
+            http://matplotlib.org/examples/images_contours_and_fields/interpolation_methods.html
+        title : str, optional
+            A title for the plot
+        colorbar : bool, optional
+            Choose whether the colorbar should be included or not
+        labels : bool, optional
+            Choose whether the axis labels and ticks should be included
+
+        Returns
+        ----------
+        figure : matplotlib figure
+            A heat plot of the results of the spatial tournament
+        """
+        size = int((1 / self.step) // 1) + 1
+        plotting_data = reshape_data(self.data, self.points, size)
+        fig, ax = plt.subplots()
+        cax = ax.imshow(
+            plotting_data, cmap=cmap, interpolation=interpolation)
+
+        if colorbar:
+            max_score = max(self.data.values())
+            min_score = min(self.data.values())
+            ticks = [min_score, (max_score + min_score) / 2, max_score]
+            fig.colorbar(cax, ticks=ticks)
+
+        plt.xlabel('$x$')
+        plt.ylabel('$y$', rotation=0)
+        ax.tick_params(axis='both', which='both', length=0)
+        plt.xticks([0, len(plotting_data) - 1], ['0', '1'])
+        plt.yticks([0, len(plotting_data) - 1], ['1', '0'])
+
+        if not labels:
+            plt.axis('off')
+
+        if title is not None:
+            plt.title(title)
+        return fig
---
+++ packages/Axelrod/Axelrod-3.4.0-py2.py3-none-any.whl/axelrod/tournament.py
@@ -0,0 +1,369 @@
+from collections import defaultdict
+import csv
+import logging
+from multiprocessing import Process, Queue, cpu_count
+from tempfile import NamedTemporaryFile
+import warnings
+
+import tqdm
+
+from axelrod import on_windows, DEFAULT_TURNS
+from axelrod.player import Player
+from axelrod.action import actions_to_str
+from .game import Game
+from .match import Match
+from .match_generator import MatchGenerator
+from .result_set import ResultSetFromFile, ResultSet
+
+from typing import List, Tuple
+
+
+class Tournament(object):
+
+    def __init__(self, players: List[Player],
+                 name: str = 'axelrod', game: Game = None, turns: int = None,
+                 prob_end: float = None, repetitions: int = 10,
+                 noise: float = 0, edges: List[Tuple] = None,
+                 match_attributes: dict = None) -> None:
+        """
+        Parameters
+        ----------
+        players : list
+            A list of axelrod.Player objects
+        name : string
+            A name for the tournament
+        game : axelrod.Game
+            The game object used to score the tournament
+        turns : integer
+            The number of turns per match
+        prob_end : float
+            The probability of a given turn ending a match
+        repetitions : integer
+            The number of times the round robin should be repeated
+        noise : float
+            The probability that a player's intended action should be flipped
+        prob_end : float
+            The probability of a given turn ending a match
+        edges : list
+            A list of edges between players
+        match_attributes : dict
+            Mapping attribute names to values which should be passed to players.
+            The default is to use the correct values for turns, game and noise
+            but these can be overridden if desired.
+        """
+        if game is None:
+            self.game = Game()
+        else:
+            self.game = game
+        self.name = name
+        self.noise = noise
+        self.num_interactions = 0
+        self.players = players
+        self.repetitions = repetitions
+        self.edges = edges
+
+        if turns is None and prob_end is None:
+            turns = DEFAULT_TURNS
+
+        self.turns = turns
+        self.prob_end = prob_end
+        self.match_generator = MatchGenerator(players=players, turns=turns,
+                                              game=self.game,
+                                              repetitions=self.repetitions,
+                                              prob_end=prob_end,
+                                              noise=self.noise,
+                                              edges=edges,
+                                              match_attributes=match_attributes)
+        self._logger = logging.getLogger(__name__)
+
+    def setup_output(self, filename=None, in_memory=False):
+        """Open a CSV writer for tournament output."""
+        if in_memory:
+            self.interactions_dict = {}
+            self.writer = None
+        else:
+            if filename:
+                self.outputfile = open(filename, 'w')
+            else:
+                # Setup a temporary file
+                self.outputfile = NamedTemporaryFile(mode='w')
+                filename = self.outputfile.name
+            self.writer = csv.writer(self.outputfile, lineterminator='\n')
+            # Save filename for loading ResultSet later
+            self.filename = filename
+
+    def play(self, build_results: bool = True, filename: str = None,
+             processes: int = None, progress_bar: bool = True,
+             keep_interactions: bool = False, in_memory: bool = False
+             ) -> ResultSetFromFile:
+        """
+        Plays the tournament and passes the results to the ResultSet class
+
+        Parameters
+        ----------
+        build_results : bool
+            whether or not to build a results st
+        filename : string
+            name of output file
+        processes : integer
+            The number of processes to be used for parallel processing
+        progress_bar : bool
+            Whether or not to create a progress bar which will be updated
+        keep_interactions : bool
+            Whether or not to load the interactions in to memory
+        in_memory : bool
+            By default interactions are written to a file.
+            If this is True they will be kept in memory.
+            This is not advised for large tournaments.
+
+        Returns
+        -------
+        axelrod.ResultSetFromFile
+        """
+        if progress_bar:
+            self.progress_bar = tqdm.tqdm(total=len(self.match_generator),
+                                          desc="Playing matches")
+
+        if on_windows and (filename is None):  # pragma: no cover
+            in_memory = True
+
+        self.setup_output(filename, in_memory)
+
+        if not build_results and not filename:
+            warnings.warn(
+                "Tournament results will not be accessible since "
+                "build_results=False and no filename was supplied.")
+
+        if (processes is None) or (on_windows):
+            self._run_serial(progress_bar=progress_bar)
+        else:
+            self._run_parallel(processes=processes, progress_bar=progress_bar)
+
+        if progress_bar:
+            self.progress_bar.close()
+
+        # Make sure that python has finished writing to disk
+        if not in_memory:
+            self.outputfile.flush()
+
+        if build_results:
+            return self._build_result_set(progress_bar=progress_bar,
+                                          keep_interactions=keep_interactions,
+                                          in_memory=in_memory)
+        elif not in_memory:
+            self.outputfile.close()
+
+    def _build_result_set(self, progress_bar: bool = True,
+                          keep_interactions: bool = False,
+                          in_memory: bool = False):
+        """
+        Build the result set (used by the play method)
+
+        Returns
+        -------
+        axelrod.BigResultSet
+        """
+        if not in_memory:
+            result_set = ResultSetFromFile(
+                filename=self.filename,
+                progress_bar=progress_bar,
+                num_interactions=self.num_interactions,
+                repetitions=self.repetitions,
+                players=[str(p) for p in self.players],
+                keep_interactions=keep_interactions,
+                game=self.game)
+            self.outputfile.close()
+        else:
+            result_set = ResultSet(
+                players=[str(p) for p in self.players],
+                interactions=self.interactions_dict,
+                repetitions=self.repetitions,
+                progress_bar=progress_bar,
+                game=self.game)
+        return result_set
+
+    def _run_serial(self, progress_bar: bool = False) -> bool:
+        """
+        Run all matches in serial
+
+        Parameters
+        ----------
+
+        progress_bar : bool
+            Whether or not to update the tournament progress bar
+        """
+        chunks = self.match_generator.build_match_chunks()
+
+        for chunk in chunks:
+            results = self._play_matches(chunk)
+            self._write_interactions(results)
+
+            if progress_bar:
+                self.progress_bar.update(1)
+
+        return True
+
+    def _write_interactions(self, results):
+        """Write the interactions to file or to a dictionary"""
+        if self.writer is not None:
+          self._write_interactions_to_file(results)
+        elif self.interactions_dict is not None:
+          self._write_interactions_to_dict(results)
+
+    def _write_interactions_to_file(self, results):
+        """Write the interactions to csv."""
+        for index_pair, interactions in results.items():
+            for interaction in interactions:
+                row = list(index_pair)
+                row.append(str(self.players[index_pair[0]]))
+                row.append(str(self.players[index_pair[1]]))
+                history1 = actions_to_str([i[0] for i in interaction])
+                history2 = actions_to_str([i[1] for i in interaction])
+                row.append(history1)
+                row.append(history2)
+                self.writer.writerow(row)
+                self.num_interactions += 1
+
+    def _write_interactions_to_dict(self, results):
+        """Write the interactions to memory"""
+        for index_pair, interactions in results.items():
+            for interaction in interactions:
+                try:
+                    self.interactions_dict[index_pair].append(interaction)
+                except KeyError:
+                    self.interactions_dict[index_pair] = [interaction]
+                self.num_interactions += 1
+
+    def _run_parallel(self, processes: int=2, progress_bar: bool = False
+                      ) -> bool:
+        """
+        Run all matches in parallel
+
+        Parameters
+        ----------
+
+        progress_bar : bool
+            Whether or not to update the tournament progress bar
+        """
+        # At first sight, it might seem simpler to use the multiprocessing Pool
+        # Class rather than Processes and Queues. However, Pool can only accept
+        # target functions which can be pickled and instance methods cannot.
+        work_queue = Queue()
+        done_queue = Queue()
+        workers = self._n_workers(processes=processes)
+
+        chunks = self.match_generator.build_match_chunks()
+        for chunk in chunks:
+            work_queue.put(chunk)
+
+        self._start_workers(workers, work_queue, done_queue)
+        self._process_done_queue(workers, done_queue, progress_bar=progress_bar)
+
+        return True
+
+    def _n_workers(self, processes: int = 2) -> int:
+        """
+        Determines the number of parallel processes to use.
+
+        Returns
+        -------
+        integer
+        """
+        if (2 <= processes <= cpu_count()):
+            n_workers = processes
+        else:
+            n_workers = cpu_count()
+        return n_workers
+
+    def _start_workers(self, workers: int, work_queue: Queue,
+                       done_queue: Queue) -> bool:
+        """
+        Initiates the sub-processes to carry out parallel processing.
+
+        Parameters
+        ----------
+        workers : integer
+            The number of sub-processes to create
+        work_queue : multiprocessing.Queue
+            A queue containing an entry for each round robin to be processed
+        done_queue : multiprocessing.Queue
+            A queue containing the output dictionaries from each round robin
+        """
+        for worker in range(workers):
+            process = Process(
+                target=self._worker, args=(work_queue, done_queue))
+            work_queue.put('STOP')
+            process.start()
+        return True
+
+    def _process_done_queue(self, workers: int, done_queue: Queue,
+                            progress_bar: bool = False):
+        """
+        Retrieves the matches from the parallel sub-processes
+
+        Parameters
+        ----------
+        workers : integer
+            The number of sub-processes in existence
+        done_queue : multiprocessing.Queue
+            A queue containing the output dictionaries from each round robin
+        progress_bar : bool
+            Whether or not to update the tournament progress bar
+        """
+        stops = 0
+        while stops < workers:
+            results = done_queue.get()
+
+            if results == 'STOP':
+                stops += 1
+            else:
+                self._write_interactions(results)
+
+                if progress_bar:
+                    self.progress_bar.update(1)
+        return True
+
+    def _worker(self, work_queue: Queue, done_queue: Queue):
+        """
+        The work for each parallel sub-process to execute.
+
+        Parameters
+        ----------
+        work_queue : multiprocessing.Queue
+            A queue containing an entry for each round robin to be processed
+        done_queue : multiprocessing.Queue
+            A queue containing the output dictionaries from each round robin
+        """
+        for chunk in iter(work_queue.get, 'STOP'):
+            interactions = self._play_matches(chunk)
+            done_queue.put(interactions)
+        done_queue.put('STOP')
+        return True
+
+    def _play_matches(self, chunk):
+        """
+        Play matches in a given chunk.
+
+        Parameters
+        ----------
+        chunk : tuple (index pair, match_parameters, repetitions)
+            match_parameters are also a tuple: (turns, game, noise)
+
+        Returns
+        -------
+        interactions : dictionary
+            Mapping player index pairs to results of matches:
+
+                (0, 1) -> [(C, D), (D, C),...]
+        """
+        interactions = defaultdict(list)
+        index_pair, match_params, repetitions = chunk
+        p1_index, p2_index = index_pair
+        player1 = self.players[p1_index].clone()
+        player2 = self.players[p2_index].clone()
+        match_params["players"] = (player1, player2)
+        match = Match(**match_params)
+        for _ in range(repetitions):
+            match.play()
+            interactions[index_pair].append(match.result)
+        return interactions
