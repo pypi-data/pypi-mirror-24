c111ca516be89c04d2ba60bed83bbd8dcaab9918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:56 +0000

    Add package sockspy/0.2.6/sockspy-0.2.6.tar.gz

---
+++ packages/sockspy/sockspy-0.2.6.tar.gz/sockspy-0.2.6/tests/test_sockspy.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `sockspy` package."""
+import multiprocessing
+import pytest
+# from click.testing import CliRunner
+from sockspy import sockspy_main
+# from sockspy import cli
+import requests
+import time
+
+
+SERVER_HOST = "localhost"
+SERVER_PORT = 3333
+
+
+def _try_socks5_proxy(url):
+    proxy = {
+        'http':'socks5://'+SERVER_HOST+':'+str(SERVER_PORT),
+        'https':'socks5://'+SERVER_HOST+':'+str(SERVER_PORT)
+    }
+    return requests.get(url, proxies=proxy)
+
+
+def _try_socks5(url):
+
+    def run_sockspy():
+        sockspy_main.run()
+
+    process = multiprocessing.Process(target=run_sockspy)
+    process.start()
+    time.sleep(2)
+    try:
+        return _try_socks5_proxy(url)
+    finally:
+        process.terminate()
+        process.join()
+
+
+def test_valid_address():
+    response = _try_socks5("https://www.bing.com")
+    assert response.status_code == requests.codes.ok
+
+
+def test_invalid_address():
+    with pytest.raises(Exception) as ex:
+        response = _try_socks5("https://www.baidu.co")
+        assert response.status_code != requests.codes.ok
+    assert ex is not None
+
+
+"""
+def test_command_line_interface():
+    runner = CliRunner()
+    result = runner.invoke(cli.sockspy_main)
+    assert result.exit_code == 0
+    assert 'sockspy.cli.main' in result.output
+    help_result = runner.invoke(cli.sockspy_main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
+"""
