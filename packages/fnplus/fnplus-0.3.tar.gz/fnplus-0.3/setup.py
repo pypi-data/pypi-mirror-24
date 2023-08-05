0c430279f56a32660048e0bb90c68cc7659ad575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package fnplus/0.3/fnplus-0.3.tar.gz

---
+++ packages/fnplus/fnplus-0.3.tar.gz/fnplus-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fnplus',
+    version='0.3',
+    description='Yet another functional programming library',
+    url='http://github.com/mdowds/fnplus',
+    author='Matt Dowds',
+    license='MIT',
+    packages=['fnplus'],
+    test_suite='fnplus.tests',
+    zip_safe=False)
