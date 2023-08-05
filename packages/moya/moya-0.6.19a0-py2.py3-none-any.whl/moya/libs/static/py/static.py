3a48e6c503770cacd63cab779367ae6541efba92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package moya/0.6.19a0/moya-0.6.19a0-py2.py3-none-any.whl

---
+++ packages/moya/moya-0.6.19a0-py2.py3-none-any.whl/moya/libs/static/py/static.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+import moya.expose
+from moya.filesystems import FSInfo
+
+from fs.wildcard import imatch_any
+
+
+@moya.expose.macro("read_directory")
+def read_directory(app, fs, path, permissions=True):
+    if not fs.isdir(path):
+        return None
+    hide_wildcards = app.settings.get_list("hide")
+    namespaces = ['details', 'access'] if permissions else ['details']
+    dir_fs = fs.opendir(path)
+    directory = [
+        FSInfo(resource)
+        for resource in dir_fs.scandir('/', namespaces=namespaces)
+        if not (hide_wildcards and imatch_any(hide_wildcards, resource.name))
+    ]
+    return {'fs': dir_fs, "directory": directory}
