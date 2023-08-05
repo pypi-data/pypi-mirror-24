ff1d31c6cb4653fb9dfbb2c708d8aa884f92eac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package tensor2tensor/1.1.5/tensor2tensor-1.1.5.tar.gz

---
+++ packages/tensor2tensor/tensor2tensor-1.1.5.tar.gz/tensor2tensor-1.1.5/setup.py
@@ -0,0 +1,40 @@
+"""Install tensor2tensor."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='tensor2tensor',
+    version='1.1.5',
+    description='Tensor2Tensor',
+    author='Google Inc.',
+    author_email='no-reply@google.com',
+    url='http://github.com/tensorflow/tensor2tensor',
+    license='Apache 2.0',
+    packages=find_packages(),
+    package_data={'tensor2tensor.data_generators': ['test_data/*']},
+    scripts=[
+        'tensor2tensor/bin/t2t-trainer',
+        'tensor2tensor/bin/t2t-datagen',
+        'tensor2tensor/bin/t2t-make-tf-configs',
+    ],
+    install_requires=[
+        'numpy',
+        'requests',
+        'sympy',
+        'six',
+    ],
+    extras_require={
+        'tensorflow': ['tensorflow>=1.2.0rc1'],
+        'tensorflow_gpu': ['tensorflow-gpu>=1.2.0rc1'],
+    },
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='tensorflow machine learning',)
