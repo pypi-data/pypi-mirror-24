af8208e9113c75cee7bfb7ddba6f1b4e648c425b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:58 +0000

    Add package dtool-cli/0.1.0/dtool-cli-0.1.0.tar.gz

---
+++ packages/dtool-cli/dtool-cli-0.1.0.tar.gz/dtool-cli-0.1.0/README.rst
@@ -0,0 +1,69 @@
+README
+======
+
+Data management command line tool: dtool.
+
+Overview
+--------
+
+The ``dtool-cli`` python package provides a skeleton command line interface to
+dtool that other "dtool" command line packages can be plugged into.
+
+It makes use of `click <https://github.com/pallets/click>`_ and `click-plugins
+<https://github.com/click-contrib/click-plugins>`_.
+
+
+Creating a plugin
+-----------------
+
+The ``dtool-cli`` plug-in system exposes two name spaces for registering commands:
+
+- ``dtool.dataset``
+- ``dtool.collection``
+
+To create a ``dtool-cli`` plug-in, create a Python package with a ``dataset``
+and/or a ``collection`` module(s). Below is the content of a hypothetical
+``dtool_create/dataset.py`` file, aka the ``dataset`` module in a
+``dtool-create`` plugin::
+
+    """dataset command line module."""
+
+    import click
+
+
+    @click.command()
+    def create():
+        print("Creating dataset...")
+
+
+Then create entry points along the lines of the below in the ``setup.py``::
+
+    from setuptools import setup
+
+    setup(
+        ...
+        entry_points={
+            "dtool.dataset": [
+                "create=dtool_create.dataset:create",
+                "markup=dtool_create.dataset:markup",
+                "manifest=dtool_create.dataset:manifest",
+            ],
+            "dtool.collection": [
+                "create=dtool_create.collection:create",
+                "markup=dtool_create.collection:markup",
+            ],
+        },
+        ...
+    )
+
+
+
+Installation
+------------
+
+To install the dtool-cli package.
+
+.. code-block:: bash
+
+    cd dtool-cli
+    python setup.py install
---
+++ packages/dtool-cli/dtool-cli-0.1.0.tar.gz/dtool-cli-0.1.0/dtool_cli/cli.py
@@ -0,0 +1,65 @@
+"""Skeleton defining the dtool plugin entry points."""
+
+import os
+from pkg_resources import iter_entry_points
+
+import click
+from click_plugins import with_plugins
+
+import dtoolcore
+
+from . import __version__
+
+
+def _path_is_dataset(ctx, param, value):
+    abspath = os.path.abspath(value)
+    if not os.path.isdir(abspath):
+        raise click.BadParameter(
+            "Not a directory: {}".format(abspath))
+    try:
+        dtoolcore.DataSet.from_path(abspath)
+    except (dtoolcore.DtoolTypeError, dtoolcore.NotDtoolObject) as e:
+        raise click.BadParameter(
+            "Not a dataset: {}".format(e))
+    return abspath
+
+
+dataset_path_argument = click.argument(
+    'dataset_path',
+    callback=_path_is_dataset)
+
+
+def pretty_version_text():
+    """Return pretty version text listing all plugins."""
+    modules = [ep.module_name for ep in iter_entry_points("dtool.dataset")]
+    modules = modules + [ep.module_name for ep in iter_entry_points("dtool.collection")]
+    packages = set([m.split(".")[0] for m in modules])
+    version_lines = ["dtool, version {}".format(__version__)]
+    version_lines.append("\nCore:")
+    version_lines.append("dtoolcore, version {}".format(dtoolcore.__version__))
+    version_lines.append("\nPlugins:")
+    for p in packages:
+        dyn_load_p = __import__(p)
+        version_lines.append("{}, version {}".format(p,  dyn_load_p.__version__))
+    return "\n".join(version_lines)
+
+@click.group()
+@click.version_option(message=pretty_version_text())
+def dtool():
+    """Manage data as datasets and collections."""
+
+
+@with_plugins(iter_entry_points("dtool.dataset"))
+@dtool.group()
+def dataset():
+    """Commands to work on a dataset."""
+
+
+@with_plugins(iter_entry_points("dtool.collection"))
+@dtool.group()
+def collection():
+    """Commands to work on a collection."""
+
+
+if __name__ == "__main__":
+    dtool()
