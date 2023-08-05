2b0748952ca2bff4d751ba5112f9766a48392126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package snipsskillscore/0.1.5.3.5/snipsskillscore-0.1.5.3.5.tar.gz

---
+++ packages/snipsskillscore/snipsskillscore-0.1.5.3.5.tar.gz/snipsskillscore-0.1.5.3.5/snipsskillscore/audio_player.py
@@ -0,0 +1,47 @@
+# -*-: coding utf-8 -*-
+""" A simple audio player based on pygame. """
+
+import pygame
+
+from .logging import log_warning
+
+class AudioPlayer:
+    """ A simple audio player based on pygame. """
+
+    @classmethod
+    def play(cls, file_path, on_done=None):
+        """ Play an audio file.
+
+        :param file_path: the path to the file to play.
+        :param on_done: callback when audio playback completes.
+        """
+        pygame.mixer.init()
+        try:
+            pygame.mixer.music.load(file_path)
+        except pygame.error as e:
+            debug_log_warning(str(e))
+            return
+
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
+        if on_done:
+            on_done()
+
+    @classmethod
+    def stop(cls):
+        """ Stop the audio. """
+        pygame.mixer.init()
+        pygame.mixer.music.stop()
+
+    @classmethod
+    def pause(cls):
+        """ Pause the audio. """
+        pygame.mixer.init()
+        pygame.mixer.music.pause()
+
+    @classmethod
+    def resume(cls):
+        """ Resume the audio. """
+        pygame.mixer.init()
+        pygame.mixer.music.unpause()
