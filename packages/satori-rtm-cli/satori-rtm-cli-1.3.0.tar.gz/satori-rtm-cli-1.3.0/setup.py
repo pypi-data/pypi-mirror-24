6779d805474e2237044b6176121d7bf7bc0accac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package satori-rtm-cli/1.3.0/satori-rtm-cli-1.3.0.tar.gz

---
+++ packages/satori-rtm-cli/satori-rtm-cli-1.3.0.tar.gz/satori-rtm-cli-1.3.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='satori-rtm-cli',
+      version='1.3.0',
+      description='Satori RTM CLI',
+      author='Satori',
+      author_email='sdk@satori.com',
+      url='http://www.satori.com/',
+      scripts=['satori-rtm-cli'],
+      install_requires=['satori-rtm-sdk >=1.2.1', 'docopt'])
