3c81a1b11ad0d1c5186bdecf8efe1f5ce059f330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:06 +0000

    Add package wafer/0.5.5/wafer-0.5.5-py2-none-any.whl

---
+++ packages/wafer/wafer-0.5.5-py2-none-any.whl/wafer/schedule/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+
+
+from wafer.schedule.views import (
+    CurrentView, ScheduleView, ScheduleItemViewSet, ScheduleXmlView, VenueView)
+
+router = routers.DefaultRouter()
+router.register(r'scheduleitems', ScheduleItemViewSet)
+
+urlpatterns = [
+    url(r'^$', ScheduleView.as_view(), name='wafer_full_schedule'),
+    url(r'^venue/(?P<pk>\d+)/$', VenueView.as_view(), name='wafer_venue'),
+    url(r'^current/$', CurrentView.as_view(), name='wafer_current'),
+    url(r'^pentabarf\.xml$', ScheduleXmlView.as_view(),
+        name='wafer_pentabarf_xml'),
+    url(r'^api/', include(router.urls)),
+]
