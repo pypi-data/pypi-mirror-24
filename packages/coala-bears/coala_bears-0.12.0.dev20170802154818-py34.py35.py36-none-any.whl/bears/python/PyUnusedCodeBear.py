17e2de7c126586887a31d224d340ee29493ffd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:48 +0000

    Add package coala-bears/0.12.0.dev20170802154818/coala_bears-0.12.0.dev20170802154818-py34.py35.py36-none-any.whl

---
+++ packages/coala-bears/coala_bears-0.12.0.dev20170802154818-py34.py35.py36-none-any.whl/bears/python/PyUnusedCodeBear.py
@@ -0,0 +1,44 @@
+import autoflake
+
+from coalib.bears.LocalBear import LocalBear
+from dependency_management.requirements.PipRequirement import PipRequirement
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
+
+
+class PyUnusedCodeBear(LocalBear):
+    LANGUAGES = {'Python', 'Python 2', 'Python 3'}
+    REQUIREMENTS = {PipRequirement('autoflake', '0.6.6')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Unused Code'}
+
+    def run(self, filename, file,
+            remove_all_unused_imports: bool=True,
+            remove_unused_variables: bool=True):
+        """
+        Detects unused code. By default this functionality is limited to:
+
+        - Unneeded pass statements.
+        - All unused imports - might have side effects
+        - Unused variables - might have side effects
+
+        :param remove_all_unused_imports:
+            ``False`` removes only unused builtin imports
+        :param remove_unused_variables:
+            ``False`` keeps unused variables
+        """
+
+        corrected = autoflake.fix_code(
+                       ''.join(file),
+                       additional_imports=None,
+                       remove_all_unused_imports=remove_all_unused_imports,
+                       remove_unused_variables=remove_unused_variables
+                       ).splitlines(True)
+
+        for diff in Diff.from_string_arrays(file, corrected).split_diff():
+            yield Result(self,
+                         'This file contains unused source code.',
+                         affected_code=(diff.range(filename),),
+                         diffs={filename: diff})
