9d848f9c48d89c1784a3c4ee6cf14ff2b4bd5d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:32 +0000

    Add package teamSpeakTelegram/0.5.1/teamSpeakTelegram-0.5.1.tar.gz

---
+++ packages/teamSpeakTelegram/teamSpeakTelegram-0.5.1.tar.gz/teamSpeakTelegram-0.5.1/teamSpeakTelegram/__init__.py
@@ -0,0 +1,36 @@
+import os
+from functools import wraps
+
+import gettext
+
+import sys
+from telegram.ext import Filters
+
+
+# Config the translations
+try:
+    path = os.path.join(os.path.dirname(sys.modules['teamSpeakTelegram'].__file__), "locale")
+    lang_es = gettext.translation("teamspeak", localedir=path, languages=["es"])
+except OSError:
+    lang_es = gettext
+
+_lang = gettext.gettext
+
+
+def _(msg): return _lang(msg)
+
+
+def user_language(func):
+    @wraps(func)
+    def wrapper(bot, update, *args, **kwargs):
+        global _lang
+
+        if update.effective_user.language_code.startswith('es'):
+            # If language is es_ES, translates
+            _lang = lang_es.gettext
+        else:
+            _lang = gettext.gettext
+
+        result = func(bot, update, *args, **kwargs)
+        return result
+    return wrapper
