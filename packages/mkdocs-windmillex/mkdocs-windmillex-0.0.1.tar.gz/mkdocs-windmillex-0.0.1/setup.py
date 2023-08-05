6512771a664be2dad050280af2a6571bcccd30de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package mkdocs-windmillex/0.0.1/mkdocs-windmillex-0.0.1.tar.gz

---
+++ packages/mkdocs-windmillex/mkdocs-windmillex-0.0.1.tar.gz/mkdocs-windmillex-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+setup(
+    name="mkdocs-windmillex",
+    version=VERSION,
+    url='https://github.com/guenbakku/mkdocs-windmillex',
+    license='MIT',
+    description='MkDocs theme focused on navigation and usability',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'windmillex = mkdocs_windmillex',
+        ]
+    },
+    zip_safe=False
+)
