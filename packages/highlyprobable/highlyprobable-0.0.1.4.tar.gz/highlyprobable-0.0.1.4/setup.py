5b0a44867a0375ec109fb7b25c8049e67e089a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package highlyprobable/0.0.1.4/highlyprobable-0.0.1.4.tar.gz

---
+++ packages/highlyprobable/highlyprobable-0.0.1.4.tar.gz/highlyprobable-0.0.1.4/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+requirements = [l.split('=')[0] for l in open('requirements.txt', 'r').read().split('\n') if l]
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+'''
+python setup.py register -r pypi # run this only once to setup your environment
+python setup.py sdist upload -r pypi
+'''
+
+setup(
+    name='highlyprobable',
+    version='0.0.1.4',
+    description=(
+        'Client for HighlyProbable Cloud AI Framework.'
+    ),
+    url='https://github.com/Tranquant/HighlyProbable/',
+    author='Mehrdad Pazooki',
+    author_email='mehrdad@tranquant.com',
+    license='Apache 2.0',
+    install_requires=requirements,
+    packages=find_packages('highlyprobable'),
+    package_dir={'':'highlyprobable'},
+    keywords = [
+        'Data Science', 'AI', 'Machine Learning', 'Natural Language Processing',
+        'Recommendation', 'Apache Spark', 'Classification'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    platforms='any',
+    zip_safe=True
+)
