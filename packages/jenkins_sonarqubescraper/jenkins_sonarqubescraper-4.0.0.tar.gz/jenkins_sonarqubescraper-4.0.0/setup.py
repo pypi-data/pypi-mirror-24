7eb97373d3500dbab51af2f4f955f254ec99cdeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package jenkins_sonarqubescraper/4.0.0/jenkins_sonarqubescraper-4.0.0.tar.gz

---
+++ packages/jenkins_sonarqubescraper/jenkins_sonarqubescraper-4.0.0.tar.gz/jenkins_sonarqubescraper-4.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jenkins_sonarqubescraper',
+      version='4.0.0',
+      description='Sonar Qube Scraper for Jenkins',
+      url='https://github.com/KMK-ONLINE/PivotalTrackerScraper',
+      author='Henry Xu',
+      author_email='hxu@bbmtek.com',
+      license='MIT',
+      packages=['SonarQubeScraper'],
+      scripts=['bin/sonar-analysis'],
+      zip_safe=False)
