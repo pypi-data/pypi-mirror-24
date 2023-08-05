a48ddc402c9c00b5de6e1cd13fd7afcc39c4cc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:37 +0000

    Add package arable/0.2.dev3/arable-0.2.dev3.tar.gz

---
+++ packages/arable/arable-0.2.dev3.tar.gz/arable-0.2.dev3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from arable import __author__
+from arable import __version__
+
+
+setup(
+    name='arable',
+    packages=['arable'],  # this must be the same as the name above,
+    version=__version__,
+    description='A client library for connecting with Arable data service',
+    author=__author__,
+    author_email='developer@arable.com',
+    url='https://github.com/Arable/apiclient.git',  # URL github repo
+    # download_url='https://github.com/Arable/apiclient.git/tarball/0.1',
+    keywords=['weather', 'datascience', 'api'],  # arbitrary keywords
+    install_requires=['requests>=2.12.0'],
+    classifiers=[],
+)
