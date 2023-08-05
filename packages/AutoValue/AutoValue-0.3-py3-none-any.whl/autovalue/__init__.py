0b7ade5f0a2e5c99b0755380715fb50697a585b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package AutoValue/0.3/AutoValue-0.3-py3-none-any.whl

---
+++ packages/AutoValue/AutoValue-0.3-py3-none-any.whl/autovalue/__init__.py
@@ -0,0 +1,45 @@
+from functools import wraps
+from inspect import getmembers, signature
+
+__version__ = '0.3'
+
+
+def autovalue(cls):
+    attributes = []
+    for member in getmembers(cls):
+        if member[0] == '__init__':
+            for name, parameter in signature(member[1]).parameters.items():
+                if name != 'self':
+                    attributes.append(name)
+
+    def init(self, *args, **kwargs):
+        cls.__init__(self, *args, **kwargs)
+        self.__initialized = True
+
+    def setter(self, key, value):
+        if self.__initialized and key in attributes:
+            raise ValueError('cannot assign member {} to class {}'.format(key, cls.__name__))
+        cls.__setattr__(self, key, value)
+
+    def eq(self, other):
+        for attr in attributes:
+            if getattr(self, attr) != getattr(other, attr):
+                return False
+        return True
+
+    def tostring(self):
+        attr_format = ['{}={}'.format(name, str(getattr(self, name))) for name in attributes]
+        return '{}({})'.format(cls.__name__, ', '.join(attr_format))
+
+    wraps(cls.__init__)(init)
+
+    name = 'AutoValue_{}'.format(cls.__name__)
+    auto_value = type(name, (cls,),
+                      {'__init__': init,
+                       '__setattr__': setter,
+                       '__eq__': eq,
+                       '__str__': tostring,
+                       '__repr__': tostring,
+                       '__initialized': False})
+
+    return auto_value
