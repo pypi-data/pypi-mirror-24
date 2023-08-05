4c66999fbb49687fde9c5b864219c69c17de0a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:20 +0000

    Add package metsim/1.0/metsim-1.0.tar.gz

---
+++ packages/metsim/metsim-1.0.tar.gz/metsim-1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(name='metsim',
+      version='1.0',
+      description='Meteorology Simulator',
+      url='https://github.com/UW-Hydro/MetSim',
+      download_url='https://github.com/UW-Hydro/MetSim/archive/v1.0.tar.gz',
+      author='Andrew Bennett',
+      author_email='bennett.andr@gmail.com',
+      packages=['metsim', 'metsim.methods'],
+      scripts=['scripts/ms'],
+      install_requires=['xarray', 'numba'],
+      keywords=['meteorology', 'disaggregation', 'hydrology',
+                'climate', 'mtclim'],
+      tests_require=['pytest'],)
