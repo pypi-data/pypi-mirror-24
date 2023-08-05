e1823b326b1c1aa467bf414b371608913c6ed0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:26 +0000

    Add package datapackage-pipelines/1.1.3/datapackage-pipelines-1.1.3.tar.gz

---
+++ packages/datapackage-pipelines/datapackage-pipelines-1.1.3.tar.gz/datapackage-pipelines-1.1.3/tests/stdlib/test_stdlib.py
@@ -0,0 +1,22 @@
+import os
+from datapackage_pipelines.utilities.lib_test_helpers import ProcessorFixtureTestsBase
+
+ROOT_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
+ENV = os.environ.copy()
+ENV['PYTHONPATH'] = ROOT_PATH
+
+ENV['EXISTENT_ENV'] = 'file://tests/data/sample.csv'
+
+class StdlibfixtureTests(ProcessorFixtureTestsBase):
+
+    def _get_procesor_env(self):
+        return ENV
+
+    def _get_processor_file(self, processor):
+        processor = processor.replace('.', '/')
+        return os.path.join(ROOT_PATH, 'datapackage_pipelines', 'lib', processor.strip() + '.py')
+
+
+for filename, _func in StdlibfixtureTests(os.path.join(os.path.dirname(__file__), 'fixtures')).get_tests():
+    globals()['test_stdlib_%s' % filename] = _func
+
