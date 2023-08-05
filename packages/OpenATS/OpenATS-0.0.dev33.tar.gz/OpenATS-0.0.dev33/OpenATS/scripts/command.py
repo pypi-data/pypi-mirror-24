35b2e376d24062a8656bcf1a43c330ba3628c7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package OpenATS/0.0.dev33/OpenATS-0.0.dev33.tar.gz

---
+++ packages/OpenATS/OpenATS-0.0.dev33.tar.gz/OpenATS-0.0.dev33/OpenATS/scripts/command.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+
+def main():
+    print("main!!")
+    print(sys.argv)
+    
+    
+def init():
+    print("OpenATS.scripts.command.init")
+    print("Create Directory Tree")
+    print(sys.argv)
+
+def makeDataset():
+	print("OpenATS.scripts.command.makeDataset")
+	print("Create Dataset")
+
+def run():
+	print("OpenATS.scripts.command.run")
+	pass
+
+def runAll():
+	print("OpenATS.scripts.command.runAll")
+	pass
+
