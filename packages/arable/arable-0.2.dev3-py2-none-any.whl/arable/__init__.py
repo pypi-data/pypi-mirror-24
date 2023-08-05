14f290cbb16862c6fbd03c4ac41a69331f18b81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:37 +0000

    Add package arable/0.2.dev3/arable-0.2.dev3-py2-none-any.whl

---
+++ packages/arable/arable-0.2.dev3-py2-none-any.whl/arable/__init__.py
@@ -0,0 +1,8 @@
+from .client import ArableClient
+
+__author__ = 'Arable Labs, Inc.'
+__copyright__ = '2017 Arable Labs, Inc.'
+
+__version_info__ = ('0', '2', 'dev3')
+__version__ = '.'.join(__version_info__)
+__short_version__ = '.'.join(__version_info__)
