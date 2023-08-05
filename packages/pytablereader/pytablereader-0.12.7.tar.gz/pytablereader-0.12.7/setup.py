f3499788f2ee1e668e10a1e27408e7e9e6c67f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package pytablereader/0.12.7/pytablereader-0.12.7.tar.gz

---
+++ packages/pytablereader/pytablereader-0.12.7.tar.gz/pytablereader-0.12.7/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+import os.path
+import sys
+
+import setuptools
+
+
+REQUIREMENT_DIR = "requirements"
+
+needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+with open(os.path.join("docs", "pages", "introduction", "summary.txt")) as f:
+    summary = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+if any([
+        sys.version_info.major < 3,
+        sys.version_info.major == 3 and sys.version_info.minor < 4,
+]):
+    install_requires.append("enum34")
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_require = [line.strip() for line in f if line.strip()]
+
+setuptools.setup(
+    name="pytablereader",
+    version="0.12.7",
+    url="https://github.com/thombashi/pytablereader",
+
+    author="Tsuyoshi Hombashi",
+    author_email="tsuyoshi.hombashi@gmail.com",
+    description=summary,
+    include_package_data=True,
+    install_requires=install_requires,
+    keywords=[
+        "table", "reader", "pandas",
+        "CSV", "Excel", "HTML", "JSON", "LTSV", "Markdown", "MediaWiki", "TSV",
+        "SQLite",
+    ],
+    long_description=long_description,
+    license="MIT License",
+    packages=setuptools.find_packages(exclude=["test*"]),
+    setup_requires=pytest_runner,
+    tests_require=tests_require,
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
