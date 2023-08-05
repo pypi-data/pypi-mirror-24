911748597f32432509f9de84f7915c9b33e6d436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:48 +0000

    Add package nester_lian/1.1.0/nester_lian-1.1.0.zip

---
+++ packages/nester_lian/nester_lian-1.1.0.zip/nester_lian-1.1.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 17:15:42 2017
+
+@author: Garvey
+"""
+
+from distutils.core import setup
+
+setup(
+      name='nester_lian',
+      version='1.1.0',
+      py_modules=['nester_lian'],
+      author='garveylian',
+      author_email='lianjiawei18@163.com',
+      url='http://garveylian.com',
+      description='A simple printer of nested lists',
+      )
