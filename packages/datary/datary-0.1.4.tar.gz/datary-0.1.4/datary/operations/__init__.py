4821d749a3f94ebb51d8870dec580192848a8c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:05 +0000

    Add package datary/0.1.4/datary-0.1.4.tar.gz

---
+++ packages/datary/datary-0.1.4.tar.gz/datary-0.1.4/datary/operations/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Datary Api python sdk Operations module.
+"""
+from .add import DataryAddOperation
+from .modify import DataryModifyOperation
+from .remove import DataryRemoveOperation
+from .clean import DataryCleanOperation
+
+
+class DataryOperations(DataryAddOperation, DataryModifyOperation,
+                       DataryRemoveOperation, DataryCleanOperation):
+    pass
