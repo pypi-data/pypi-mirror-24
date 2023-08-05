907282df40300f2959122f9f02b8566790cafe1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package trunity-3-client/0.1/trunity_3_client-0.1-py3.5.egg

---
+++ packages/trunity-3-client/trunity_3_client-0.1-py3.5.egg/trunity_3_client/__init__.py
@@ -0,0 +1,11 @@
+# from trunity_3_client.clients import *
+from .clients import (
+    TopicsClient,
+    ContentsClient,
+    ContentType,
+    ContentTypeError,
+    ResourceType,
+    ResourceTypeError,
+    Client,
+)
+
---
+++ packages/trunity-3-client/trunity_3_client-0.1-py3.5.egg/trunity_3_client/clients/__init__.py
@@ -0,0 +1,9 @@
+from trunity_3_client.clients.endpoints.topics import TopicsClient
+from trunity_3_client.clients.endpoints.contents import (
+    ContentsClient,
+    ContentType,
+    ContentTypeError,
+    ResourceType,
+    ResourceTypeError,
+)
+from trunity_3_client.clients.client import Client
