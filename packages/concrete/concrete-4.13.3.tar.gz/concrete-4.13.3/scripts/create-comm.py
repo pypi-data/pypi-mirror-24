5d1629edce9f2d5d51077ad746c87cf4a849888a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package concrete/4.13.3/concrete-4.13.3.tar.gz

---
+++ packages/concrete/concrete-4.13.3.tar.gz/concrete-4.13.3/scripts/create-comm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+'Convert text file to Concrete Communication file.'
+from __future__ import unicode_literals
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import codecs
+
+import concrete.version
+from concrete.util.file_io import write_communication_to_file
+from concrete.util.simple_comm import (
+    create_comm, AL_NONE, add_annotation_level_argparse_argument
+)
+from concrete.util import set_stdout_encoding
+
+
+def main():
+    set_stdout_encoding()
+
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description='Convert text file to communication',
+    )
+    parser.set_defaults(annotation_level=AL_NONE)
+    parser.add_argument('text_path', type=str,
+                        help='Input text file path (- for stdin)')
+    parser.add_argument('concrete_path', type=str,
+                        help='Output concrete file path (- for stdout)')
+    add_annotation_level_argparse_argument(parser)
+    concrete.version.add_argparse_argument(parser)
+    ns = parser.parse_args()
+
+    # Won't work on Windows
+    text_path = '/dev/fd/0' if ns.text_path == '-' else ns.text_path
+    concrete_path = (
+        '/dev/fd/1' if ns.concrete_path == '-' else ns.concrete_path
+    )
+    annotation_level = ns.annotation_level
+
+    with codecs.open(text_path, encoding='utf-8') as f:
+        comm = create_comm(text_path, f.read(),
+                           annotation_level=annotation_level)
+        write_communication_to_file(comm, concrete_path)
+
+
+if __name__ == "__main__":
+    main()
