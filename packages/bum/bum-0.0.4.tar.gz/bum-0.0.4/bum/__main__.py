9fb7936c99c10e7ac540a17e5d3d76a6a6482a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:46 +0000

    Add package bum/0.0.4/bum-0.0.4.tar.gz

---
+++ packages/bum/bum-0.0.4.tar.gz/bum-0.0.4/bum/__main__.py
@@ -0,0 +1,75 @@
+"""
+'||
+ || ...  ... ...  .. .. ..
+ ||'  ||  ||  ||   || || ||
+ ||    |  ||  ||   || || ||
+ '|...'   '|..'|. .|| || ||.
+
+Created by Dylan Araps
+"""
+import argparse
+import pathlib
+import signal
+
+from . import display
+from . import song
+
+
+__version__ = "0.0.4"
+
+
+def get_args():
+    """Get the script arguments."""
+    description = "bum - Download and display album art \
+                   for mpd tracks."
+    arg = argparse.ArgumentParser(description=description)
+
+    arg.add_argument("--size", metavar="\"px\"",
+                     help="what size to display the album art in.",
+                     default=250)
+
+    arg.add_argument("--cache_dir", metavar="\"/path/to/dir\"",
+                     help="Where to store the downloaded cover art.",
+                     default=pathlib.Path.home() / ".cache/bum")
+
+    arg.add_argument("--version", action="store_true",
+                     help="Print \"bum\" version.")
+
+    return arg.parse_args()
+
+
+def process_args(args):
+    """Process the arguments."""
+    if args.version:
+        print(f"bum {__version__}")
+        exit(0)
+
+
+def main():
+    """Main script function."""
+    args = get_args()
+    process_args(args)
+
+    def signal_usr1(sig, frame):
+        """Handle 'pkill -USR1 bum'."""
+        # This function is here so it inherits main()'s scope.
+        # Signal doesn't allow us to send arguments to the handler.
+        print("signal: Recieved SUGUSR1, swapping album art.")
+
+        song.get_art(args.cache_dir, args.size)
+
+        # Args are required when using signal, but we don't need them.
+        del sig, frame
+
+    signal.signal(signal.SIGUSR1, signal_usr1)
+    disp = display.init(args.size)
+
+    song.get_art(args.cache_dir, args.size)
+
+    while True:
+        display.launch(disp, args.cache_dir / "current.jpg")
+        signal.pause()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/bum/bum-0.0.4.tar.gz/bum-0.0.4/bum/song.py
@@ -0,0 +1,51 @@
+"""
+Get song info.
+"""
+import shutil
+import subprocess
+
+from . import brainz
+from . import util
+
+
+def get():
+    """Get the current playing song."""
+    song = subprocess.getoutput("mpc current -f "
+                                "'%albumartist%💩"
+                                "%title%💩"
+                                "%album%💩"
+                                "%artist%'")
+    song = song.split("💩")
+
+    if song[0] == "Various Artists":
+        song[0] = song[-1]
+
+    return song
+
+
+def get_art(cache_dir, size):
+    """Get the album art."""
+    song_data = get()
+
+    if len(song_data) < 2:
+        print("album: Nothing currently playing.")
+        return
+
+    file_name = f"{song_data[0]}_{song_data[2]}_{size}.jpg".replace("/", "")
+    file_name = cache_dir / file_name
+
+    if file_name.is_file():
+        shutil.copy(file_name, cache_dir / "current.jpg")
+        print("album: Found cached art.")
+
+    else:
+        print("album: Downloading album art...")
+
+        brainz.init()
+        album_art = brainz.get_cover(song_data, size)
+
+        if album_art:
+            util.bytes_to_file(album_art, cache_dir / file_name)
+            util.bytes_to_file(album_art, cache_dir / "current.jpg")
+
+            print(f"album: Swapped art to {song_data[0]}, {song_data[2]}.")
