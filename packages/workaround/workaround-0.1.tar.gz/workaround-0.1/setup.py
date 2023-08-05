6d2fe92095805353a07af96576402d66eef9df16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package workaround/0.1/workaround-0.1.tar.gz

---
+++ packages/workaround/workaround-0.1.tar.gz/workaround-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='workaround',
+    version='0.1',
+    packages=['workaround'],
+    url='',
+    license='',
+    author='Tom Forbes',
+    author_email='tom@tomforb.es',
+    description=''
+)
