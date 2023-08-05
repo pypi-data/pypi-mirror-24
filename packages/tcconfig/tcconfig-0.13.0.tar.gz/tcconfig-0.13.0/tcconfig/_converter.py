4a3d7e49d9be0549924ea2092fdd949fbba22004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package tcconfig/0.13.0/tcconfig-0.13.0.tar.gz

---
+++ packages/tcconfig/tcconfig-0.13.0.tar.gz/tcconfig-0.13.0/tcconfig/_converter.py
@@ -0,0 +1,101 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+from collections import namedtuple
+import re
+
+import typepy
+
+from ._error import (
+    InvalidParameterError,
+    UnitNotFoundError,
+)
+from ._logger import logger
+
+ByteUnit = namedtuple("ByteUnit", "regexp factor")
+
+
+class Humanreadable(object):
+    __UNIT_LIST = [
+        ByteUnit(regexp=re.compile("^b$", re.IGNORECASE), factor=0),
+        ByteUnit(regexp=re.compile("^bps$", re.IGNORECASE), factor=0),
+        ByteUnit(regexp=re.compile("^k$", re.IGNORECASE), factor=1),
+        ByteUnit(regexp=re.compile("^kbps$", re.IGNORECASE), factor=1),
+        ByteUnit(regexp=re.compile("^m$", re.IGNORECASE), factor=2),
+        ByteUnit(regexp=re.compile("^mbps$", re.IGNORECASE), factor=2),
+        ByteUnit(regexp=re.compile("^g$", re.IGNORECASE), factor=3),
+        ByteUnit(regexp=re.compile("^gbps$", re.IGNORECASE), factor=3),
+        ByteUnit(regexp=re.compile("^t$", re.IGNORECASE), factor=4),
+        ByteUnit(regexp=re.compile("^gbps$", re.IGNORECASE), factor=3),
+        ByteUnit(regexp=re.compile("^p$", re.IGNORECASE), factor=5),
+    ]
+    __RE_NUMBER = re.compile("^[\-\+]?[0-9\.]+")
+
+    def __init__(self, readable_size, kilo_size=1024):
+        """
+        String converter that humanreadable byte size to a number.
+
+        :param str readable_size: human readable size (bytes). e.g. 256 M
+        :param int kilo_size: size of ``kilo``. 1024 or 1000
+        """
+
+        self.__readable_size = readable_size
+        self.kilo_size = kilo_size
+
+        self.__validate_kilo_size()
+
+    def to_bit(self):
+        """
+        :raises ValueError:
+        """
+
+        logger.debug("readable_size: {}".format(self.__readable_size))
+
+        if not typepy.is_not_null_string(self.__readable_size):
+            raise TypeError(
+                "readable_size must be a string: actual={}".format(
+                    self.__readable_size))
+
+        self.__readable_size = self.__readable_size.strip()
+
+        try:
+            size = self.__RE_NUMBER.search(self.__readable_size).group()
+        except AttributeError:
+            raise InvalidParameterError(
+                "invalid value: {}".format(self.__readable_size))
+        size = float(size)
+        if size < 0:
+            raise InvalidParameterError(
+                "size must be greater or equals to zero")
+
+        unit = self.__RE_NUMBER.sub("", self.__readable_size).strip().lower()
+
+        return size * self.__get_coefficient(unit)
+
+    def to_kilo_bit(self):
+        """
+        :param str readable_size: human readable size (bytes). e.g. 256 M
+        :raises ValueError:
+        """
+
+        return self.to_bit() / self.kilo_size
+
+    def __validate_kilo_size(self):
+        if self.kilo_size not in [1000, 1024]:
+            raise ValueError("invalid kilo size: {}".format(self.kilo_size))
+
+    def __get_coefficient(self, unit_str):
+        self.__validate_kilo_size()
+
+        for unit in self.__UNIT_LIST:
+            if unit.regexp.search(unit_str):
+                return self.kilo_size ** unit.factor
+
+        raise UnitNotFoundError("unit not found: value={}".format(unit_str))
---
+++ packages/tcconfig/tcconfig-0.13.0.tar.gz/tcconfig-0.13.0/test/test_traffic_control.py
@@ -0,0 +1,336 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+import itertools
+
+from allpairspy import AllPairs
+import pytest
+
+from tcconfig._const import (
+    ShapingAlgorithm,
+    Tc,
+    TrafficDirection,
+)
+from tcconfig._error import (
+    InvalidParameterError,
+    UnitNotFoundError,
+)
+from tcconfig.traffic_control import TrafficControl
+
+from .common import is_invalid_param
+
+
+MIN_VALID_PACKET_LOSS = 0.0000000232  # [%]
+
+
+@pytest.fixture
+def device_option(request):
+    return request.config.getoption("--device")
+
+
+@pytest.mark.parametrize(["value"], [
+    ["".join(opt_list)]
+    for opt_list in AllPairs([
+        ["0.1", "+1.25", "30"],
+        [
+            "k", " k ", "K", " K ", "kbps", "Kbps",
+            "m", " m ", "M", " M ", "mbps", "Mbps",
+            "g", " g ", "G", " G ", "gbps", "Gbps",
+        ]
+    ])
+])
+def test_TrafficControl_validate_bandwidth_rate_normal(value):
+    tc_obj = TrafficControl(
+        "dummy", bandwidth_rate=value, direction=TrafficDirection.OUTGOING,
+        shaping_algorithm=ShapingAlgorithm.HTB)
+    tc_obj.validate_bandwidth_rate()
+
+
+@pytest.mark.parametrize(["value", "expected"], [
+    ["".join(opt_list), UnitNotFoundError]
+    for opt_list in AllPairs([
+        ["0.1", "1"],
+        ["", "kb", "KB", "mb", "MB", "gb", "GB"]
+    ])
+] + [
+    ["".join(value), InvalidParameterError]
+    for value in ("B", "K", "M", "G")
+] + [
+    ["0bps", InvalidParameterError],
+    ["34359738361bps", InvalidParameterError],
+])
+def test_TrafficControl_validate_bandwidth_rate_exception_1(value, expected):
+    with pytest.raises(expected):
+        tc_obj = TrafficControl(
+            "dummy", bandwidth_rate=value, direction=TrafficDirection.OUTGOING,
+            shaping_algorithm=ShapingAlgorithm.HTB)
+        tc_obj.validate_bandwidth_rate()
+
+
+@pytest.mark.parametrize(["value", "expected"], [
+    ["".join(opt_list), InvalidParameterError]
+    for opt_list in itertools.product(
+        ["-1", "0", "0.0"],
+        ["k", "K", "m", "M", "g", "G"]
+    )
+])
+def test_TrafficControl_validate_bandwidth_rate_exception_2(value, expected):
+    with pytest.raises(expected):
+        tc_obj = TrafficControl(
+            "dummy", bandwidth_rate=value,
+            shaping_algorithm=ShapingAlgorithm.HTB)
+        tc_obj.validate_bandwidth_rate()
+
+
+class Test_TrafficControl_validate(object):
+
+    @pytest.mark.parametrize(
+        [
+            "rate", "direction", "delay", "delay_distro", "loss", "duplicate",
+            "corrupt",
+            "src_network", "exclude_src_network",
+            "dst_network", "exclude_dst_network",
+            "src_port", "exclude_src_port",
+            "dst_port", "exclude_dst_port",
+            "shaping_algorithm",
+        ],
+        [
+            opt_list
+            for opt_list in AllPairs([
+                [None, "", "100K", "0.5M", "0.1G"],  # rate
+                [TrafficDirection.OUTGOING],
+                [
+                    None, TrafficControl.MIN_LATENCY_MS,
+                    TrafficControl.MAX_LATENCY_MS,
+                ],  # delay
+                [
+                    None, TrafficControl.MIN_LATENCY_MS,
+                    TrafficControl.MAX_LATENCY_MS,
+                ],  # delay_distro
+                [
+                    None,
+                    TrafficControl.MIN_PACKET_LOSS_RATE, MIN_VALID_PACKET_LOSS,
+                    TrafficControl.MAX_PACKET_LOSS_RATE,
+                ],  # loss
+                [
+                    None, TrafficControl.MIN_PACKET_DUPLICATE_RATE,
+                    TrafficControl.MAX_PACKET_DUPLICATE_RATE,
+                ],  # duplicate
+                [
+                    None, TrafficControl.MIN_CORRUPTION_RATE,
+                    TrafficControl.MAX_CORRUPTION_RATE,
+                ],  # corrupt
+                [None, "192.168.0.1", "192.168.0.0/24"],  # src_network
+                [None, "192.168.0.1", "192.168.0.0/25"],  # exclude_src_network
+                [
+                    None,
+                    "",
+                    "192.168.0.1", "192.168.0.254",
+                    "192.168.0.1/32", "192.168.0.0/24"
+                ],  # dst_network
+                [None, "192.168.0.1", "192.168.0.0/25"],  # exclude_dst_network
+                [None, 65535],  # src_port
+                [None, 22],  # exclude_src_port
+                [None, 65535],  # dst_port
+                [None, 22],  # exclude_dst_port
+                [
+                    ShapingAlgorithm.HTB, ShapingAlgorithm.TBF,
+                ],  # shaping_algorithm
+            ], n=3)
+        ])
+    def test_normal(
+            self, device_option, rate, direction, delay, delay_distro,
+            loss, duplicate, corrupt,
+            src_network, exclude_src_network,
+            dst_network, exclude_dst_network,
+            src_port, exclude_src_port,
+            dst_port, exclude_dst_port,
+            shaping_algorithm):
+        if device_option is None:
+            pytest.skip("device option is null")
+
+        tc_obj = TrafficControl(
+            device=device_option,
+            direction=direction,
+            bandwidth_rate=rate,
+            latency_ms=delay,
+            latency_distro_ms=delay_distro,
+            packet_loss_rate=loss,
+            packet_duplicate_rate=duplicate,
+            corruption_rate=corrupt,
+            src_network=src_network,
+            exclude_src_network=exclude_src_network,
+            dst_network=dst_network,
+            exclude_dst_network=exclude_dst_network,
+            src_port=src_port,
+            exclude_src_port=exclude_src_port,
+            dst_port=dst_port,
+            exclude_dst_port=exclude_dst_port,
+            is_enable_iptables=True,
+            shaping_algorithm=shaping_algorithm,
+        )
+
+        if is_invalid_param(
+                rate, delay, loss, duplicate, corrupt, reordering=None):
+            with pytest.raises(ValueError):
+                tc_obj.validate()
+        else:
+            tc_obj.validate()
+
+    @pytest.mark.parametrize(
+        ["direction", "delay", "reordering"],
+        [
+            opt_list
+            for opt_list in AllPairs([
+                [TrafficDirection.OUTGOING],
+                [
+                    TrafficControl.MIN_LATENCY_MS + 0.1,
+                    TrafficControl.MAX_LATENCY_MS,
+                ],  # delay
+                [
+                    TrafficControl.MIN_REORDERING_RATE,
+                    TrafficControl.MAX_REORDERING_RATE,
+                ],  # reordering
+            ], n=2)
+        ])
+    def test_normal_reordering(
+            self, device_option, direction, delay, reordering):
+        if device_option is None:
+            pytest.skip("device option is null")
+
+        tc_obj = TrafficControl(
+            device=device_option,
+            direction=direction,
+            latency_ms=delay,
+            reordering_rate=reordering,
+            shaping_algorithm=ShapingAlgorithm.HTB,
+        )
+
+        tc_obj.validate()
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [{"latency_ms": TrafficControl.MIN_LATENCY_MS - 1}, ValueError],
+        [{"latency_ms": TrafficControl.MAX_LATENCY_MS + 1}, ValueError],
+
+        [
+            {
+                "latency_ms": 100,
+                "latency_distro_ms": TrafficControl.MIN_LATENCY_MS - 1,
+            },
+            ValueError
+        ],
+        [
+            {
+                "latency_ms": 100,
+                "latency_distro_ms": TrafficControl.MAX_LATENCY_MS + 1,
+            },
+            ValueError
+        ],
+
+        [
+            {"packet_loss_rate": TrafficControl.MIN_PACKET_LOSS_RATE - 0.1},
+            ValueError
+        ],
+        [
+            {"packet_loss_rate": TrafficControl.MAX_PACKET_LOSS_RATE + 0.1},
+            ValueError
+        ],
+
+        [
+            {
+                "latency_ms": 100,
+                "packet_duplicate_rate": TrafficControl.MIN_PACKET_DUPLICATE_RATE - 0.1,
+            },
+            ValueError
+        ],
+        [
+            {
+                "latency_ms": 100,
+                "packet_duplicate_rate": TrafficControl.MAX_PACKET_DUPLICATE_RATE + 0.1,
+            },
+            ValueError
+        ],
+
+        [
+            {"corruption_rate": TrafficControl.MIN_CORRUPTION_RATE - 0.1},
+            ValueError
+        ],
+        [
+            {"corruption_rate": TrafficControl.MAX_CORRUPTION_RATE + 0.1},
+            ValueError
+        ],
+
+        [
+            {"reordering_rate": TrafficControl.MIN_REORDERING_RATE - 0.1},
+            ValueError
+        ],
+        [
+            {"reordering_rate": TrafficControl.MAX_REORDERING_RATE + 0.1},
+            ValueError
+        ],
+
+        [{Tc.Param.DST_NETWORK: "192.168.0."}, ValueError],
+        [{Tc.Param.DST_NETWORK: "192.168.0.256"}, ValueError],
+        [{Tc.Param.DST_NETWORK: "192.168.0.0/0"}, ValueError],
+        [{Tc.Param.DST_NETWORK: "192.168.0.0/33"}, ValueError],
+        [{Tc.Param.DST_NETWORK: "192.168.0.2/24"}, ValueError],
+        [{Tc.Param.DST_NETWORK: "192.168.0.0000/24"}, ValueError],
+
+        [{"src_port": -1}, ValueError],
+        [{"src_port": 65536}, ValueError],
+
+        [{"dst_port": -1}, ValueError],
+        [{"dst_port": 65536}, ValueError],
+    ])
+    def test_exception(self, device_option, value, expected):
+        if device_option is None:
+            pytest.skip("device option is null")
+
+        tc_obj = TrafficControl(
+            device=device_option,
+            bandwidth_rate=value.get("bandwidth_rate"),
+            latency_ms=value.get("latency_ms"),
+            latency_distro_ms=value.get("latency_distro_ms"),
+            packet_loss_rate=value.get("packet_loss_rate"),
+            packet_duplicate_rate=value.get("packet_duplicate_rate"),
+            corruption_rate=value.get("corruption_rate"),
+            dst_network=value.get(Tc.Param.DST_NETWORK),
+            src_port=value.get("src_port"),
+            dst_port=value.get("dst_port"),
+            shaping_algorithm=ShapingAlgorithm.HTB,
+        )
+
+        with pytest.raises(expected):
+            tc_obj.validate()
+
+
+class Test_TrafficControl_ipv4(object):
+
+    @pytest.mark.parametrize(
+        [
+            "network", "is_ipv6",
+            "expected_ip_ver", "expected_protocol", "expected_protocol_match"
+        ],
+        [
+            ["192.168.0.1", False, 4, "ip", "ip"],
+            ["192.168.0.0/24", False, 4, "ip", "ip"],
+            ["::1", True, 6, "ipv6", "ip6"],
+            ["2001:db00::0/24", True, 6, "ipv6", "ip6"],
+        ])
+    def test_normal(
+            self, device_option, network, is_ipv6,
+            expected_ip_ver, expected_protocol, expected_protocol_match):
+        if device_option is None:
+            pytest.skip("device option is null")
+
+        tc_obj = TrafficControl(
+            device=device_option, dst_network=network, is_ipv6=is_ipv6,
+            shaping_algorithm=ShapingAlgorithm.HTB,
+        )
+
+        assert tc_obj.ip_version == expected_ip_ver
+        assert tc_obj.protocol == expected_protocol
+        assert tc_obj.protocol_match == expected_protocol_match
