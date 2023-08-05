aff2803a068dd34f6b23420f2a0240515c0d8bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:24 +0000

    Add package build-bot/1.2/build_bot-1.2.tar.gz

---
+++ packages/build-bot/build_bot-1.2.tar.gz/build_bot-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='build_bot',
+    packages=['build_bot'],
+    version='1.2',
+    description='A fast and easy way to manage CI builds',
+    author='davemachado',
+    author_email='contact@davemachado.com',
+    url='https://github.com/davemachado/build-bot',
+    download_url=(
+        'https://github.com/davemachado/build-bot/archive/1.0.tar.gz'),
+    keywords=['build', 'bot', 'github'],
+    classifiers=[],
+    scripts=['build_bot/build_bot'],
+)
