bf1687f11a705a8d848c8a0b5c7e6c8c14ef349a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package durationpy/0.5/durationpy-0.5.tar.gz

---
+++ packages/durationpy/durationpy-0.5.tar.gz/durationpy-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name         = 'durationpy',
+    description  = 'Module for converting between datetime.timedelta and Go\'s Duration strings.',
+    url          = 'https://github.com/icholy/durationpy',
+    author       = 'Ilia Choly',
+    author_email = 'ilia.choly@gmail.com',
+    download_url = 'https://github.com/icholy/durationpy/tarball/0.5',
+    version      = '0.5',
+    packages     = ['durationpy'],
+    license      = 'MIT'
+)
