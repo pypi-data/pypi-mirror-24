01c5fbf1d8e1d8357dc0a4e92d9f610efc07a145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:36 +0000

    Add package fteikpy/1.2.0/fteikpy-1.2.0.tar.gz

---
+++ packages/fteikpy/fteikpy-1.2.0.tar.gz/fteikpy-1.2.0/fteikpy/layered_model.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author: Keurfon Luu <keurfon.luu@mines-paristech.fr>
+License: MIT
+"""
+
+import numpy as np
+from .eikonal import Eikonal
+
+__all__ = [ "lay2vel", "lay2tt" ]
+
+
+def lay2vel(lay, dz, grid_shape, smooth = False):
+    """
+    Convert a layered model to a continuous velocity model.
+    
+    Parameters
+    ----------
+    lay : ndarray
+        Layer velocities (first column) and interface depth (second column).
+    dz : float
+        Grid size in Z coordinate in meters.
+    grid_shape : tuple (nz, nx[, ny])
+        Gris shape.
+    smooth : bool, default False
+        If True, smooth the velocity model.
+    
+    Returns
+    -------
+    vel : ndarray
+        Velocity model grid in m/s.
+    """
+    # Check inputs
+    if not isinstance(lay, np.ndarray) and not lay.ndim in [ 1, 2 ]:
+        raise ValueError("grid must be a 1-D or 2-D ndarray")
+    if (lay.ndim == 1 and lay[0] < 0.) or (lay.ndim == 2 and lay[:,0].min() < 0.):
+        raise ValueError("velocities must be positive")
+    if not isinstance(dz, (float, int)) or dz < 0.:
+        raise ValueError("dz must be positive")
+    zmax = dz*grid_shape[0]
+    if (lay.ndim == 1 and lay[1] > zmax) or (lay.ndim == 2 and lay[:,1].max() > zmax):
+        raise ValueError("last layer depth must be %.2f" % zmax)
+    if not np.all([isinstance(n, int) for n in grid_shape]) or len(grid_shape) not in [ 1, 2, 3 ]:
+        raise ValueError("grid_shape must be a tuple of integers of size 1, 2 or 3")
+    if not isinstance(smooth, bool):
+        raise ValueError("smooth must either be True or False")
+    
+    # Create a continuous velocity model
+    if len(lay.shape) == 1:
+        vel = np.full(grid_shape, lay[0])
+    else:
+        nlayer = lay.shape[0]
+        vel1d = np.zeros(grid_shape[0])
+        ztop = 0
+        zbot = int(np.floor(lay[0,1]/dz)) - 1
+        if smooth:
+            vel1d[ztop:zbot+1] = np.linspace(lay[0,0], lay[1,0], zbot-ztop+1)
+            for i in range(1, nlayer-1):
+                ztop = zbot + 1
+                zbot = int(np.floor(lay[i,1]/dz)) - 1
+                vel1d[ztop:zbot+1] = np.linspace(lay[i,0], lay[i+1,0], zbot-ztop+1)
+        else:
+            vel1d[ztop:zbot+1].fill(lay[0,0])
+            for i in range(1, nlayer-1):
+                ztop = zbot + 1
+                zbot = int(np.floor(lay[i,1]/dz)) - 1
+                vel1d[ztop:zbot+1].fill(lay[i,0])
+        vel1d[zbot+1:].fill(lay[-1,0])
+        
+        vel = vel1d
+        if len(grid_shape) > 1:
+            vel = np.tile(vel[:,None], grid_shape[1])
+        if len(grid_shape) > 2:
+            vel = np.tile(vel[:,:,None], grid_shape[2])
+    return vel
+
+
+def lay2tt(eikonal, sources, receivers, n_threads = 1):
+    """
+    Given a layered velocity model, compute the first arrivel traveltime for
+    each source and each receiver. Only useful if working in 3-D as a 2-D
+    eikonal solver is used for traveltime computation.
+    
+    Parameters
+    ----------
+    eikonal : Eikonal object
+        2-D eikonal solver with a layered velocity model.
+    sources : ndarray
+        Sources coordinates (Z, X[, Y]).
+    receivers : ndarray
+        Receivers coordinates (Z, X[, Y]).
+    n_threads : int, default 1
+        Number of threads to pass to OpenMP.
+        
+    Returns
+    -------
+    tcalc : ndarray of shape (nrcv, nsrc)
+        Traveltimes for each source and each receiver.
+    """
+    # Check inputs
+    if not isinstance(eikonal, Eikonal):
+        raise ValueError("eikonal must be an Eikonal object")
+    if not hasattr(eikonal, "_velocity_model"):
+        raise ValueError("eikonal must have a defined velocity model")
+    if not isinstance(sources, np.ndarray) or sources.shape[1] != 3:
+        raise ValueError("sources must be ndarray with 3 columns")
+    if not isinstance(receivers, np.ndarray) or receivers.shape[1] != 3:
+        raise ValueError("receivers must be ndarray with 3 columns")
+    
+    # Parameters
+    nrcv = receivers.shape[0]
+    nsrc = sources.shape[0]
+    
+    # Switch sources and receivers to minimize calls to eikonals
+    n1 = min(nrcv, nsrc)
+    n2 = max(nrcv, nsrc)
+    tcalc = np.zeros((n2, n1))
+    if n1 == nsrc:
+        rcv, src = np.array(receivers), np.array(sources)
+    else:
+        rcv, src = np.array(sources), np.array(receivers)
+        
+    # Compute traveiltime grid using eikonal solver
+    src2d = np.vstack((src[:,0], np.zeros(n1))).transpose()
+    tt = eikonal.solve(src2d, n_threads = n_threads)
+    
+    # Compute traveltimes using eikonal solver
+    dhorz = np.zeros(n2)        
+    for i in range(n1):
+        for j in range(n2):
+            dhorz[j] = np.linalg.norm(src[i,1:] - rcv[j,1:])
+        tcalc[:,i] = [ tt[i].get(zrcv, xrcv, check = False) for zrcv, xrcv in zip(rcv[:,0], dhorz) ]
+            
+    # Transpose to reshape to [ nrcv, nsrc ]
+    if n1 == nrcv:
+        return tcalc.transpose()
+    else:
+        return tcalc
