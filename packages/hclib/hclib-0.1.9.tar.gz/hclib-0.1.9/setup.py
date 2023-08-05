41c33c2bd0c20390384af4eb385a8cabd74a696c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package hclib/0.1.9/hclib-0.1.9.tar.gz

---
+++ packages/hclib/hclib-0.1.9.tar.gz/hclib-0.1.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "hclib",
+    py_modules = ["hclib"],
+    version = "0.1.9",
+    description = "A library to connect to https://hack.chat/",
+    long_description = open("README.rst").read(),
+    url = "https://github.com/neelkamath/hack.chat-library",
+    author = "Neel Kamath",
+    author_email = "neelkamath@protonmail.com",
+    license = "MIT",
+    keywords = "hack.chat library",
+    install_requires = ["websocket-client"]
+)
