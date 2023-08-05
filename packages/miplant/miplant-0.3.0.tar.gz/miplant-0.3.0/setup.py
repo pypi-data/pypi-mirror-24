52486065f856bcf72b32bc4008d047c51c44c560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:16 +0000

    Add package miplant/0.3.0/miplant-0.3.0.tar.gz

---
+++ packages/miplant/miplant-0.3.0.tar.gz/miplant-0.3.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='miplant',
+    version='0.3.0',
+    description='A library for reading cheap plant monitoring sensors manufactured by Xiaomi.',
+    author='Kimmo Huoman',
+    author_email='kipenroskaposti@gmail.com',
+    url='https://github.com/kipe/miplant',
+    license='MIT',
+    packages=[
+        'miplant',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        'bluepy>=1.0.5'
+    ])
