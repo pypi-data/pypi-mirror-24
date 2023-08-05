adaacf6b94b33de18fe9542186b1f54ed7e00e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package launchpad-py/0.7.0/launchpad_py-0.7.0.tar.gz

---
+++ packages/launchpad-py/launchpad_py-0.7.0.tar.gz/launchpad_py-0.7.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+import sys
+
+
+if not sys.version_info[0] == 2:
+	sys.exit("Error: Launchpad.py requires Python 2")
+
+
+setup(
+	name = "launchpad_py",
+	version = "0.7.0",
+	description = "A Novation Launchpad control suite for Python",
+	long_description = open('README').read(),
+	author = "FMMT666(ASkr)",
+	author_email = "fmmt666@users.noreply.github.com",
+	license = "CC BY 4.0",
+	keywords = "novation launchpad midi",
+	url = "https://github.com/FMMT666/launchpad.py",
+	packages = ["launchpad_py"],
+)
