da970d6d38eb796411d17062fb0e3b913a2a5b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package pyden/0.3.5/pyden-0.3.5.tar.gz

---
+++ packages/pyden/pyden-0.3.5.tar.gz/pyden-0.3.5/setup.py
@@ -0,0 +1,64 @@
+"""
+File: setup.py
+Author: Aaron Bach
+Email: bachya1208@gmail.com
+Github: https://github.com/bachya
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+
+from setuptools import setup
+
+try:
+    # Python 3
+    from os import dirname  # pylint: disable=ungrouped-imports
+except ImportError:
+    # Python 2
+    from os.path import dirname
+
+HERE = os.path.abspath(dirname(__file__))
+
+with codecs.open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESC = '\n' + f.read()
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+REQUIRED = ['cachecontrol', 'geocoder', 'icalendar', 'maya', 'requests']
+
+PACKAGES = ['pyden']
+
+ABOUT = dict()
+with open(os.path.join(HERE, 'pyden', '__version__.py'), 'r') as f:
+    exec(f.read(), ABOUT)  # pylint: disable=exec-used
+
+setup(
+    name='pyden',
+    version=ABOUT['__version__'],
+    description='A library to get for the City and County of Denver, CO',
+    long_description=LONG_DESC,
+    author='Aaron Bach',
+    author_email='bachya1208@gmail.com',
+    url='https://github.com/bachya/pyden',
+    packages=PACKAGES,
+    install_requires=REQUIRED,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 4 - Beta', 'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English', 'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'), )
