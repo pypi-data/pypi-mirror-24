550ddb2c2fec659ebf7ae3af302cb5b3abb6783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:14 +0000

    Add package django_ali_base_service/1.4.1/django_ali_base_service-1.4.1.tar.gz

---
+++ packages/django_ali_base_service/django_ali_base_service-1.4.1.tar.gz/django_ali_base_service-1.4.1/README.rst
@@ -0,0 +1,5 @@
+=====
+Ali Base Service
+=====
+
+This project created for personal use only! It was not intended for public usage and it lacks detail documentation. You can use it however if you want. 
---
+++ packages/django_ali_base_service/django_ali_base_service-1.4.1.tar.gz/django_ali_base_service-1.4.1/ali_base_service/util/base_services.py
@@ -0,0 +1,67 @@
+import abc
+
+from rest_framework.authentication import get_authorization_header
+from rest_framework.views import APIView
+
+
+from ali_base_service.util.handlers import *
+from main import models
+
+
+class BaseListAPI(APIView):
+    __metaclass__  = abc.ABCMeta
+
+    def get(self, request, **kwargs):
+        self.request = request
+        self.args=kwargs
+        search_object, start, count = read_get_request_data(request.GET)
+        order_by = request.GET.get('order_by')
+        phone_number = request.META.get('HTTP_PHONE_NUMBER')
+
+        return get_successful_response(self.get_query_set().list(
+            serializer=self.get_serializer(),
+            search_object=search_object,
+            start=start,
+            count=count,
+            order_by=order_by,
+            phone_number=phone_number
+        ))
+
+    @abc.abstractmethod
+    def get_query_set(self):
+        pass
+
+    @abc.abstractmethod
+    def get_serializer(self):
+        pass
+
+    def get_argument(self, key):
+        return self.args[key]
+
+
+class BaseGetAPI(APIView):
+    __metaclass__ = abc.ABCMeta
+
+    def get(self, request, id):
+        phone_number = request.META.get('HTTP_PHONE_NUMBER')
+
+        return get_successful_response(
+            self.get_query_set().instance(
+                instance_id=id,
+                without_user_serializer=self.get_serializer(),
+                phone_number=phone_number
+            ))
+
+    @abc.abstractmethod
+    def get_query_set(self):
+        pass
+
+    @abc.abstractmethod
+    def get_serializer(self):
+        pass
+
+
+def get_client(request):
+    return None  # todo rename to get current user and return django user instead
+
+
