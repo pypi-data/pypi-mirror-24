5aeb93936e68293fb3686d6422dc0e9e9a4e08ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package lambda-toolkit/0.3.4/lambda_toolkit-0.3.4-py2.py3-none-any.whl

---
+++ packages/lambda-toolkit/lambda_toolkit-0.3.4-py2.py3-none-any.whl/lambda_toolkit/modules/cli.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+from lambda_toolkit.modules.conf import Conf
+from lambda_toolkit.modules.utils import Utils
+import click
+from lambda_toolkit import __version__
+import sys
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "tail":
+        # External modudle
+        import tail_toolkit.modules.cli
+else:
+    sys.argv[0] == "--help"
+
+def execute_cli(args):
+    Utils.click_validate_required_options(click.get_current_context(), conf)
+    module = click.get_current_context().info_name
+    myclass = __import__("lambda_toolkit.modules." + module)
+    clazz = getattr(getattr(myclass.modules, module), module.title())
+    getattr(clazz(conf, args), args['action'].replace("-", "_") + "_" + module)().save_config()
+
+conf = Conf()
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(**kwargs):
+    pass
+
+
+
+@cli.command()
+@click.argument('action', required=True, type=click.Choice(Utils.click_get_command_choice("queue", conf)))
+@click.option('--sqsname', '-q', callback=Utils.click_append_fifo_in_queue, help="Define the queue.")
+@click.option('--region', '-R', type=click.Choice(conf.aws_regions), help="Define the optional region.")
+@Utils.docstring_parameter(conf)
+def queue(**kwargs):
+    execute_cli(kwargs)
+
+@cli.command()
+@click.argument('action', required=True, type=click.Choice(Utils.click_get_command_choice("proxy", conf)))
+@click.option('--proxyname', '-p', help="Define the proxy name.")
+@click.option('--sqsname', '-q', callback=Utils.click_append_fifo_in_queue, help="Define the queue name.",
+              type=click.Choice(Utils.click_list_queues_without_fifo(conf)))
+@click.option('--rolename', '-r', default=conf.sett['C_DEFAULT_ROLE'], callback=Utils.click_verify_role_exists,
+              help="Define the role or try to get the default.")
+@click.option('--runtime','-e', default="python2.7", help="Define runtime. (Default: Python2.7)",
+              type=click.Choice(Utils.click_list_runtime()))
+@click.option('--region', '-R', type=click.Choice(conf.aws_regions), help="Define the optional region.")
+@Utils.docstring_parameter(conf)
+def proxy(**kwargs):
+    execute_cli(kwargs)
+
+@cli.command()
+@click.argument('action', required=True, type=click.Choice(Utils.click_get_command_choice("project", conf)))
+@click.option( '--projectname', '-p', help="Define the project.")
+@click.option('--rolename', '-r', default=conf.sett['C_DEFAULT_ROLE'], callback=Utils.click_verify_role_exists,
+              help="Define the role or try to get the default.")
+@click.option('--runtime','-e', default="python2.7", help="Define runtime. (Default: Python2.7)",
+              type=click.Choice(Utils.click_list_runtime()))
+@click.option('--region', '-R', type=click.Choice(conf.aws_regions), help="Define the optional region.")
+@click.option( '--value', '-V', help="Define the a value to a variable")
+@click.option( '--variable', '-v', help="Define the a variable name")
+@Utils.docstring_parameter(conf)
+def project(**kwargs):
+    execute_cli(kwargs)
+
+@cli.command()
+@click.argument('action', required=True, type=click.Choice(Utils.click_get_command_choice("role", conf)))
+@click.option('--rolename', '-r',
+              help="Define the role or try to get the default.")
+@Utils.docstring_parameter(conf)
+def role(**kwargs):
+    execute_cli(kwargs)
+
+@cli.command()
+@click.argument('action', required=True, type=click.Choice(Utils.click_get_command_choice("receiver", conf)))
+@click.option('--sqsname', '-q', callback=Utils.click_append_fifo_in_queue, help="Define the queue name.",
+              type=click.Choice(Utils.click_list_queues_without_fifo(conf)))
+@click.option( '--projectname', '-p', help="Define the project.", type=click.Choice(conf.projects.keys()))
+@Utils.docstring_parameter(conf)
+def receiver(**kwargs):
+    execute_cli(kwargs)
+
+@cli.command()
+@click.argument('action', required=True, type=click.Choice(Utils.click_get_command_choice("invoke", conf)))
+@Utils.docstring_parameter(conf)
+@click.option( '--projectname', '-p', help="Define the project.", type=click.Choice(conf.projects.keys()))
+@click.option( '--event-file', '-f', help="Define a file.",
+               type=click.Choice(Utils.click_list_event_files(conf)))
+@click.option( '--proxyname', '-pp', help="Define the proxy.", type=click.Choice(conf.proxies.keys()))
+def invoke(**kwargs):
+    execute_cli(kwargs)
+
+@cli.command()
+def tail(**kwargs):
+    """Forward to tail-toolkit"""
+    pass
+
+@cli.command()
+@Utils.docstring_parameter(conf)
+def list(**kwargs):
+    modules = ['project', 'queue', 'proxy']
+
+    for m in modules:
+        conf.log.info("")
+        myclass = __import__("lambda_toolkit.modules." + m)
+        args = {}
+        args['action'] = "list"
+        clazz = getattr(getattr(myclass.modules, m), m.title())
+        getattr(clazz(conf, args), args['action'].replace("-", "_") + "_" + m)().save_config()
+
+
+print("Initializing lambda-toolkit CLI (v" + __version__ + ") - Region: " + conf.region + " - Auth: " + conf.auth_mode)
+cli()
---
+++ packages/lambda-toolkit/lambda_toolkit-0.3.4-py2.py3-none-any.whl/lambda_toolkit/modules/invoke.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import base64
+from lambda_toolkit.modules.lambdacontext import LambdaContext
+
+class Invoke:
+    def __init__(self, conf, kwargs):
+        self.lbs = conf.get_boto3("lambda", "client")
+        self.log = conf.log
+        self.conf = conf
+        self.kwargs = kwargs
+
+    def local_invoke(self):
+        if 'python' not in self.conf.projects[self.kwargs['projectname']]['runtime']:
+            self.log.error("Cannot invoke locally a JS runtime project in Python lambda-toolkit environment.")
+            self.log.critical("Please find: lambda-toolkit-js project.")
+        else:
+            self.log.info("Importing project " + self.kwargs['projectname'])
+            pp = os.path.join(os.path.expanduser(self.conf.sett['C_BASE_DIR']),
+                              self.conf.sett['C_LAMBDAS_DIR'],
+                              self.conf.region,
+                              self.kwargs['projectname'])
+
+            self.log.debug("Using project dir: " + pp)
+            sys.path.append(pp)
+
+            if 'variables' in self.conf.projects[self.kwargs['projectname']]:
+                vars = self.conf.projects[self.kwargs['projectname']]['variables']
+
+                for v in vars:
+                    self.log.info("Injecting lambda variable '" + v + "' with value '" + vars[v] + "'.")
+                    os.environ[v] = vars[v]
+
+            ctx = LambdaContext(json.loads(open(os.path.join(os.path.expanduser(self.conf.sett['C_BASE_DIR']),
+                                                             self.conf.sett['C_INVOKE_DIR_CTX'],
+                                                             self.conf.sett['C_INVOKE_CTX_FILE'])).read()))
+
+            func = getattr(__import__("index"), "lambda_handler")
+            func(self._get_event(), ctx)
+
+        return self.conf
+
+    def remote_invoke(self):
+        if "projectname" in self.kwargs and self.kwargs['projectname'] is not None:
+            invoke_lambda = self.kwargs['projectname']
+            if self.conf.projects[invoke_lambda]['deployed'] == False:
+                self.log.critical("Project '" + invoke_lambda + "' is not deployed.")
+        else:
+            invoke_lambda = self.kwargs['proxyname']
+
+        self.log.info("Invoking the project " + invoke_lambda)
+        ret = self.lbs.invoke(
+            FunctionName=invoke_lambda,
+            LogType='Tail',
+            InvocationType='RequestResponse',
+            Payload=open(os.path.join(os.path.expanduser(self.conf.sett['C_BASE_DIR']),
+                                      self.conf.sett['C_INVOKE_DIR_EVT'],
+                                      self.kwargs['event_file'])).read()
+        )
+
+        print(base64.b64decode(ret['LogResult']).decode())
+
+        return self.conf
+
+    def _get_event(self):
+        with open(os.path.join(os.path.expanduser(self.conf.sett['C_BASE_DIR']),
+                               self.conf.sett['C_INVOKE_DIR_EVT'],
+                               self.kwargs['event_file']),
+                  'r') as f:
+            return json.loads(f.read())
