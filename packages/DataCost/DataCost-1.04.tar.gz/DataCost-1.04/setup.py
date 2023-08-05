f68912133dced37d3354e684f88e919917dc57bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package DataCost/1.04/DataCost-1.04.tar.gz

---
+++ packages/DataCost/DataCost-1.04.tar.gz/DataCost-1.04/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'DataCost',
+  py_modules = ['datacost'],
+  version = '1.04',
+  description = 'Calculate cost based metrics about data based on the number\
+    of positive and negative data points.',
+  author = 'Michael J. Siers',
+  author_email = 'mikesiers@live.com',
+  url = 'https://github.com/mikesiers/DataCost/',
+  download_url = 'https://github.com/mikesiers/DataCost/archive/1.04.tar.gz',
+  keywords = ['cost-sensitive', 'machine-learning', 'data-science'],
+  classifiers = []
+)
