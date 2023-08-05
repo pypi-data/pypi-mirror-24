930cc5c2fafffb46db2dfb60bd6be440534258cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:52 +0000

    Add package ctec_consumer/0.3.9/ctec_consumer-0.3.9.zip

---
+++ packages/ctec_consumer/ctec_consumer-0.3.9.zip/ctec_consumer-0.3.9/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+version = '0.3.9'
+
+setuptools.setup(
+        name='ctec_consumer',
+        version=version,
+        packages=setuptools.find_packages(),
+        author='ZhangZhaoyuan',
+        author_email='zhangzhy@chinatelecom.cn',
+        url='http://www.189.cn',
+        description='189 rabbitMQ consumer',
+        install_requires=['kombu==3.0.35', 'gevent==1.2.1', 'pika==0.10.0']
+)
