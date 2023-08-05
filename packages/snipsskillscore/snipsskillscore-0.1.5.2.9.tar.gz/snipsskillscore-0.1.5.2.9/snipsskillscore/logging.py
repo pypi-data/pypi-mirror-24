9985b6c3bf68da024e4f713d673bc9b89392efcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package snipsskillscore/0.1.5.2.9/snipsskillscore-0.1.5.2.9.tar.gz

---
+++ packages/snipsskillscore/snipsskillscore-0.1.5.2.9.tar.gz/snipsskillscore-0.1.5.2.9/snipsskillscore/logging.py
@@ -0,0 +1,77 @@
+# -*-: coding utf-8 -*-
+""" Logging utilities. """
+
+import inspect
+
+LOGGING_ENABLED = True
+
+
+class TermincalColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def log(message):
+    """ Print a log message.
+
+    :param message: the message to print.
+    """
+    __log(inspect.stack(), message)
+
+
+def log_error(message):
+    """ Print an error message.
+
+    :param message: the message to print.
+    """
+    __log(inspect.stack(), message, TermincalColors.FAIL)
+
+
+def log_success(message):
+    """ Print a success message.
+
+    :param message: the message to print.
+    """
+    __log(inspect.stack(), message, TermincalColors.OKGREEN)
+
+
+def log_warning(message):
+    """ Print a warning message.
+
+    :param message: the message to print.
+    """
+    __log(inspect.stack(), message, TermincalColors.WARNING)
+
+
+def log_blue(message):
+    """ Print a blue message.
+
+    :param message: the message to print.
+    """
+    __log(inspect.stack(), message, TermincalColors.OKBLUE)
+
+
+def __log(stack, message, color=None):
+    """ Print a message, optionally in a given color.
+
+    :param message: the message to print.
+    :param color: the color of the message.
+    """
+    if not LOGGING_ENABLED or message is None:
+        return
+
+    frame = stack[1][0]
+    caller = frame.f_locals.get('self', None)
+    tag = caller.__class__.__name__
+    message = "[{}] {}".format(tag, message)
+
+    if not color:
+        print(message)
+    else:
+        print("{}{}{}".format(color, message, TermincalColors.ENDC))
---
+++ packages/snipsskillscore/snipsskillscore-0.1.5.2.9.tar.gz/snipsskillscore-0.1.5.2.9/snipsskillscore/sound_service.py
@@ -0,0 +1,35 @@
+# -*-: coding utf-8 -*-
+""" Sound service for playing various state sounds. """
+
+from .audio_player import AudioPlayer
+
+
+SOUND_DIR = "data/sounds"
+
+class SoundService:
+    """ Sound service for playing various state sounds. """
+
+    class State:
+        """ States handled by the sound service. """
+        none, welcome, goodbye, hotword_detected, asr_text_captured, error = range(6)
+
+    @staticmethod
+    def play(state):
+        """ Play sound for a given state.
+
+        :param state: a State value.
+        """
+        filename = None
+        if state == SoundService.State.welcome:
+            filename = "pad_glow_welcome1.wav"
+        elif state == SoundService.State.goodbye:
+            filename = "pad_glow_power_off.wav"
+        elif state == SoundService.State.hotword_detected:
+            filename = "pad_soft_on.wav"
+        elif state == SoundService.State.asr_text_captured:
+            filename = "pad_soft_off.wav"
+        elif state == SoundService.State.error:
+            filename = "music_marimba_error_chord_2x.wav"
+
+        if filename is not None:
+            AudioPlayer.play("{}/{}".format(SOUND_DIR, filename))
