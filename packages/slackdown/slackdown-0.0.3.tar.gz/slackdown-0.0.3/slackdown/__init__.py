46d7a61509fa4e0b6c5188b4ae40b638a8d86193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:13 +0000

    Add package slackdown/0.0.3/slackdown-0.0.3.tar.gz

---
+++ packages/slackdown/slackdown-0.0.3.tar.gz/slackdown-0.0.3/slackdown/__init__.py
@@ -0,0 +1,313 @@
+import re
+try:
+    # Python 2 module
+    from HTMLParser import HTMLParser
+except ImportError:
+    # Python 3 module
+    from html.parser import HTMLParser
+
+
+"""
+Slack characters that serve as delimination options.
+"""
+LIST_DELIMITERS = {
+    '\-': 'dash',
+    u'\u2022': 'dot',
+    '\d*\.': 'numbered',
+}
+
+"""
+Slack characters that serve as formatting options.
+"""
+FORMATTERS = {
+    '*': 'b',
+    '_': 'i',
+    '~': 's',
+    '`': 'code',
+}
+
+"""
+List types outputed by slackdown.render().
+"""
+LIST_TYPES = {
+    'dot': 'ul',
+    'dash': 'ul',
+    'numbered': 'ol',
+}
+
+"""
+Predefined elements that serve as "parents".
+"""
+PARENT_ELEMENTS = [
+    'p',
+    'pre',
+    'ul',
+    'ol',
+    'blockquote'
+]
+
+
+def parse(message):
+    """
+    Accepts a Slack message object and returns HTML.
+    """
+    html = render(message['text'])
+
+    return html
+
+
+def render(txt):
+    """
+    Accepts Slack formatted text and returns HTML.
+    """
+
+    # Removing links to other channels
+    txt = re.sub(r'<#[^\|]*\|(.*)>', r'#\g<1>', txt)
+
+    # Removing links to other users
+    txt = re.sub(r'<(@.*)>', r'\g<1>', txt)
+
+    # handle named hyperlinks
+    txt = re.sub(r'<([^\|]*)\|([^\|]*)>', r'<a href="\g<1>" target="blank">\g<2></a>', txt)
+
+    # handle unnamed hyperlinks
+    txt = re.sub(r'<([^a|/a].*)>', r'<a href="\g<1>" target="blank">\g<1></a>', txt)
+
+    # handle ordered and unordered lists
+    for delimeter in LIST_DELIMITERS:
+        slack_tag = delimeter
+        class_name = LIST_DELIMITERS[delimeter]
+
+        # Wrap any lines that start with the slack_tag in <li></li>
+        list_regex = u'(?:^|\n){}\s?(.*)'.format(slack_tag)
+        list_repl = r'<li class="list-item-{}">\g<1></li>'.format(class_name)
+        txt = re.sub(list_regex, list_repl, txt)
+
+    # hanlde blockquotes
+    txt = re.sub(u'(^|\n)(?:&gt;){3}\s?(.*)$', r'\g<1><blockquote>\g<2></blockquote>', txt, flags=re.DOTALL)
+    txt = re.sub(u'(?:^|\n)&gt;\s?(.*)\n?', r'<blockquote>\g<1></blockquote>', txt)
+
+    # handle code blocks
+    txt = re.sub(r'```\n?(.*)```', r'<pre>\g<1></pre>', txt, flags=re.DOTALL)
+    txt = re.sub(r'\n(</pre>)', r'\g<1>', txt)
+
+    # handle bolding, italics, and strikethrough
+    for wrapper in FORMATTERS:
+        slack_tag = wrapper
+        html_tag = FORMATTERS[wrapper]
+
+        # Grab all text in formatted characters on the same line unless escaped
+        regex = r'(?<!\\)\{t}([^\{t}|\n]*)\{t}'.format(t=slack_tag)
+        repl = r'<{t}>\g<1></{t}>'.format(t=html_tag)
+        txt = re.sub(regex, repl, txt)
+
+    # convert line breaks
+    txt = txt.replace('\n', '<br />')
+
+    # clean up bad HTML
+    parser = CustomSlackdownHTMLParser(txt)
+    txt = parser.clean()
+
+    # convert multiple spaces
+    txt = txt.replace(r'  ', ' &nbsp')
+
+    return txt
+
+
+class CustomSlackdownHTMLParser(HTMLParser):
+    """
+    Custom HTML parser for cleaning up the slackdown HTML output.
+    """
+    def __init__(self, txt):
+        """
+        Initialize custom parser properties.
+        """
+        self.dirty_html = txt
+        self.cleaned_html = ''
+        self.current_parent_element = {}
+        self.current_parent_element['tag'] = ''
+        self.current_parent_element['attrs'] = {}
+        self.parsing_li = False
+
+        HTMLParser.__init__(self)
+
+    def _open_list(self, list_type):
+        """
+        Add an open list tag corresponding to the specification in the
+        parser's LIST_TYPES.
+        """
+        if list_type in LIST_TYPES.keys():
+            tag = LIST_TYPES[list_type]
+        else:
+            raise Exception('CustomSlackdownHTMLParser:_open_list: Not a valid list type.')
+
+        html = '<{t} class="list-container-{c}">'.format(
+            t=tag,
+            c=list_type
+        )
+        self.cleaned_html += html
+        self.current_parent_element['tag'] = LIST_TYPES[list_type]
+        self.current_parent_element['attrs'] = {'class': list_type}
+
+    def _close_list(self):
+        """
+        Add an close list tag corresponding to the currently open
+        list found in current_parent_element.
+        """
+        list_type = self.current_parent_element['attrs']['class']
+        tag = LIST_TYPES[list_type]
+
+        html = '</{t}>'.format(
+            t=tag
+        )
+        self.cleaned_html += html
+        self.current_parent_element['tag'] = ''
+        self.current_parent_element['attrs'] = {}
+
+    def handle_starttag(self, tag, attrs):
+        """
+        Called by HTMLParser.feed when a start tag is found.
+        """
+        # Parse the tag attributes
+        attrs_dict = dict(t for t in attrs)
+
+        # If the tag is a predefined parent element
+        if tag in PARENT_ELEMENTS:
+            # If parser is parsing another parent element
+            if self.current_parent_element['tag'] != '':
+                # close the parent element
+                self.cleaned_html += '</{}>'.format(self.current_parent_element['tag'])
+
+            self.current_parent_element['tag'] = tag
+            self.current_parent_element['attrs'] = {}
+
+            self.cleaned_html += '<{}>'.format(tag)
+
+        # If the tag is a list item
+        elif tag == 'li':
+            self.parsing_li = True
+
+            # Parse the class name & subsequent type
+            class_name = attrs_dict['class']
+            list_type = class_name[10:]
+
+            # Check if parsing a list
+            if self.current_parent_element['tag'] == 'ul' or self.current_parent_element['tag'] == 'ol':
+                cur_list_type = self.current_parent_element['attrs']['class']
+                # Parsing a different list
+                if cur_list_type != list_type:
+                    # Close that list
+                    self._close_list()
+
+                    # Open new list
+                    self._open_list(list_type)
+            # Not parsing a list
+            else:
+                # if parsing some other parent
+                if self.current_parent_element['tag'] != '':
+                    self.cleaned_html += '</{}>'.format(self.current_parent_element['tag'])
+                # Open new list
+                self._open_list(list_type)
+
+            self.cleaned_html += '<{}>'.format(tag)
+
+        # If the tag is a line break
+        elif tag == 'br':
+            # If parsing a paragraph, close it
+            if self.current_parent_element['tag'] == 'p':
+                self.cleaned_html += '</p>'
+                self.current_parent_element['tag'] = ''
+                self.current_parent_element['attrs'] = {}
+            # If parsing a list, close it
+            elif self.current_parent_element['tag'] == 'ul' or self.current_parent_element['tag'] == 'ol':
+                self._close_list()
+            # If parsing any other parent element, keep it
+            elif self.current_parent_element['tag'] in PARENT_ELEMENTS:
+                self.cleaned_html += '<br />'
+            # If not in any parent element, create an empty paragraph
+            else:
+                self.cleaned_html += '<p></p>'
+
+        # If the tag is something else, like a <b> or <i> tag
+        else:
+            # If not parsing any parent element
+            if self.current_parent_element['tag'] == '':
+                self.cleaned_html += '<p>'
+                self.current_parent_element['tag'] = 'p'
+            self.cleaned_html += '<{}'.format(tag)
+
+            for attr in attrs_dict.keys():
+                self.cleaned_html += ' {k}="{v}"'.format(
+                    k=attr,
+                    v=attrs_dict[attr]
+                )
+
+            self.cleaned_html += '>'
+
+    def handle_endtag(self, tag):
+        """
+        Called by HTMLParser.feed when an end tag is found.
+        """
+        if tag in PARENT_ELEMENTS:
+            self.current_parent_element['tag'] = ''
+            self.current_parent_element['attrs'] = ''
+
+        if tag == 'li':
+            self.parsing_li = True
+        if tag != 'br':
+            self.cleaned_html += '</{}>'.format(tag)
+
+    def handle_data(self, data):
+        """
+        Called by HTMLParser.feed when text is found.
+        """
+        if self.current_parent_element['tag'] == '':
+            self.cleaned_html += '<p>'
+            self.current_parent_element['tag'] = 'p'
+
+        self.cleaned_html += data
+
+    def _remove_pre_formatting(self):
+        """
+        Removes formatting tags added to pre elements.
+        """
+        preformatted_wrappers = [
+            'pre',
+            'code'
+        ]
+
+        for wrapper in preformatted_wrappers:
+            for formatter in FORMATTERS:
+                tag = FORMATTERS[formatter]
+                character = formatter
+
+                regex = r'(<{w}>.*)<{t}>(.*)</{t}>(.*</{w}>)'.format(
+                    t=tag,
+                    w=wrapper
+                )
+                repl = r'\g<1>{c}\g<2>{c}\g<3>'.format(c=character)
+                self.cleaned_html = re.sub(regex, repl, self.cleaned_html)
+
+    def feed(self):
+        """
+        Uses the dirty_html property as the argument for HTMLParser.feed
+        """
+        HTMLParser.feed(self, self.dirty_html)
+
+    def clean(self):
+        """
+        Goes through the txt input and cleans up any problematic HTML.
+        """
+        # Calls handle_starttag, handle_endtag, and handle_data
+        self.feed()
+
+        # Clean up any parent tags left open
+        if self.current_parent_element['tag'] != '':
+            self.cleaned_html += '</{}>'.format(self.current_parent_element['tag'])
+
+        # Remove empty <p> added after lists
+        self.cleaned_html = re.sub(r'(</[u|o]l>)<p></p>', r'\g<1>', self.cleaned_html)
+
+        self._remove_pre_formatting()
+
+        return self.cleaned_html
---
+++ packages/slackdown/slackdown-0.0.3.tar.gz/slackdown-0.0.3/slackdown/tests.py
@@ -0,0 +1,138 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tests Slackdown API wrapper.
+"""
+import unittest
+import slackdown
+
+
+class SlackdownTest(unittest.TestCase):
+    def test_bold(self):
+        txt = slackdown.render('Some *bold* text.')
+        self.assertEqual(txt, '<p>Some <b>bold</b> text.</p>')
+
+    def test_italics(self):
+        txt = slackdown.render('Some _italics_ text.')
+        self.assertEqual(txt, '<p>Some <i>italics</i> text.</p>')
+
+    def test_strikethrough(self):
+        txt = slackdown.render('Some ~strikethrough~ text.')
+        self.assertEqual(txt, '<p>Some <s>strikethrough</s> text.</p>')
+
+    def test_line_breaks(self):
+        txt = slackdown.render('multiple\n\ntext\nlines')
+        self.assertEqual(txt, '<p>multiple</p><p></p><p>text</p><p>lines</p>')
+
+    def test_spaces(self):
+        txt = slackdown.render('large     spaces    between     text')
+        self.assertEqual(txt, '<p>large &nbsp &nbsp spaces &nbsp &nbspbetween &nbsp &nbsp text</p>')
+
+    def test_single_line_code(self):
+        txt = slackdown.render('Some `*code*` text.')
+        self.assertEqual(txt, '<p>Some <code>*code*</code> text.</p>')
+
+    def test_multi_line_code(self):
+        txt = slackdown.render('```multiple\n*lines* of\ncode```')
+        self.assertEqual(txt, '<pre>multiple<br />*lines* of<br />code</pre>')
+
+    def test_bullet_list(self):
+        txt = slackdown.render(u'\u2022 item 1\n\u2022 item 2')
+        self.assertEqual(txt, '<ul class="list-container-dot"><li>item 1</li><li>item 2</li></ul>')
+
+    def test_hyphen_list(self):
+        txt = slackdown.render(u'- item 1\n- item 2')
+        self.assertEqual(txt, '<ul class="list-container-dash"><li>item 1</li><li>item 2</li></ul>')
+
+    def test_numbered_list(self):
+        txt = slackdown.render(u'1. item 1\n24. item 2')
+        self.assertEqual(txt, '<ol class="list-container-numbered"><li>item 1</li><li>item 2</li></ol>')
+
+    def test_single_line_blockquote(self):
+        txt = slackdown.render('&gt; Someone else said this.')
+        self.assertEqual(txt, '<blockquote>Someone else said this.</blockquote>')
+
+    def test_multi_line_blockquote(self):
+        txt = slackdown.render('&gt;&gt;&gt; Someone else\nsaid this.')
+        self.assertEqual(txt, '<blockquote>Someone else<br />said this.</blockquote>')
+
+    def test_multiple_blockquotes(self):
+        txt = slackdown.render('&gt; A couple of\n&gt;&gt;&gt; Blockquotes until\nthe end of the message.')
+        self.assertEqual(txt, '\
+<blockquote>A couple of</blockquote><blockquote>Blockquotes until<br />the end of the message.</blockquote>')
+
+    def test_multiple_same_lists(self):
+        txt = slackdown.render('- item 1\n- item 2\n\n- item 3\n- item 4')
+        self.assertEqual(txt, '\
+<ul class="list-container-dash">\
+<li>item 1</li>\
+<li>item 2</li>\
+</ul>\
+<ul class="list-container-dash">\
+<li>item 3</li>\
+<li>item 4</li>\
+</ul>\
+')
+
+    def test_multiple_diff_lists(self):
+        txt = slackdown.render('These are *two* lists.\n- item 1\n- item 2\n1. item 3\n2. item 4\nRight?')
+        self.maxDiff = None
+        self.assertEqual(txt, '\
+<p>These are <b>two</b> lists.</p>\
+<ul class="list-container-dash">\
+<li>item 1</li>\
+<li>item 2</li>\
+</ul>\
+<ol class="list-container-numbered">\
+<li>item 3</li>\
+<li>item 4</li>\
+</ol>\
+<p>Right?</p>\
+')
+
+    def test_multiple_parents(self):
+        txt = slackdown.render('A blockquote looks like this:\n&gt; Someone else said this.')
+        self.assertEqual(txt, '<p>A blockquote looks like this:</p><blockquote>Someone else said this.</blockquote>')
+
+    def test_all_bold(self):
+        txt = slackdown.render('*All bold text*')
+        self.assertEqual(txt, '<p><b>All bold text</b></p>')
+
+    def test_bold_in_list(self):
+        txt = slackdown.render(u'- *item 1*\n- item 2')
+        self.assertEqual(txt, '<ul class="list-container-dash"><li><b>item 1</b></li><li>item 2</li></ul>')
+
+    def test_bad_bold_wrapping(self):
+        txt = slackdown.render('*Multiple lines\nof bold text.*')
+        self.assertEqual(txt, '<p>*Multiple lines</p><p>of bold text.*</p>')
+
+    def test_escaping(self):
+        txt = slackdown.render('The \*unbolded\* text should not be *bold*.')
+        self.assertEqual(txt, '<p>The \*unbolded\* text should not be <b>bold</b>.</p>')
+
+    def test_hyperlinks(self):
+        txt = slackdown.render('This is a tweet <https://example.com>.')
+        self.assertEqual(txt, '\
+<p>This is a tweet <a href="https://example.com" target="blank">https://example.com</a>.</p>\
+')
+
+
+class CustomSlackdownParserTest(unittest.TestCase):
+    def test_bad_open_list(self):
+        bad_txt = '<li class="list-container-bad_type"></li>'
+        error_msg = 'CustomSlackdownHTMLParser:_open_list: Not a valid list type.'
+        parser = slackdown.CustomSlackdownHTMLParser(bad_txt)
+
+        with self.assertRaises(Exception) as context:
+            parser.clean()
+
+        self.assertTrue(error_msg in str(context.exception))
+
+
+class SlackdownParseTest(unittest.TestCase):
+    def test_parse_render(self):
+        msg = {
+            'text': 'Some *bold* text.'
+        }
+        txt = slackdown.parse(msg)
+        self.assertEqual(txt, '<p>Some <b>bold</b> text.</p>')
