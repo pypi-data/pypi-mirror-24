88c2f82d692d9584e431f9887ae10e63939fe294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:54 +0000

    Add package singer-python/2.1.3/singer-python-2.1.3.tar.gz

---
+++ packages/singer-python/singer-python-2.1.3.tar.gz/singer-python-2.1.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import subprocess
+
+setup(name="singer-python",
+      version='2.1.3',
+      description="Singer.io utility library",
+      author="Stitch",
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      url="http://singer.io",
+      install_requires=[
+          'pendulum==1.2.0',
+          'simplejson==3.11.1',
+      ],
+      packages=find_packages(),
+      package_data = {
+          'singer': [
+              'logging.conf'
+              ]
+          },
+)
