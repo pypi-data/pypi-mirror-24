16c2fd8f4562328909483ea7a1901c64fce3cbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package amocrm/0.7.7/amocrm-0.7.7-py3-none-any.whl

---
+++ packages/amocrm/amocrm-0.7.7-py3-none-any.whl/amocrm/entities/lead.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+
+from ..exceptions import MissingArgument
+from ..dictwrap import DictWrap
+from ..fields import Field
+from ..util import cast_to_cls_list
+
+from .entity import Entity
+
+
+class Lead(Entity):
+
+    def __init__(self, name=None, status_id=None, **kwargs):
+        super().__init__(**kwargs)
+        if not name:
+            raise MissingArgument('Missing argument: "name"')
+        if not status_id:
+            raise MissingArgument('Missing argument: "status_id"')
+
+        self.name = name
+        self.status_id = status_id
+
+    def todict(self, verb='add', id_=None):
+        self._dict = {
+            "name": self.name,
+            "status_id": self.status_id,
+        }
+
+        d = {
+            "request":  {
+                "leads":  {
+                    "add":  [
+                        self._dict
+                    ]
+                }
+            }
+        }
+
+        return deepcopy(d)
