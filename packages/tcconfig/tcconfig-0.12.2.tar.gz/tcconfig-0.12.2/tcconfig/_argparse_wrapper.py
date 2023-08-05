7b4e49a4f49fb0a0f6658e61a70b70e4c56e8d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:12 +0000

    Add package tcconfig/0.12.2/tcconfig-0.12.2.tar.gz

---
+++ packages/tcconfig/tcconfig-0.12.2.tar.gz/tcconfig-0.12.2/tcconfig/_argparse_wrapper.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+import argparse
+
+import logbook
+
+from ._const import TcCommandOutput
+
+
+class ArgparseWrapper(object):
+    """
+    wrapper class of argparse
+    """
+
+    def __init__(self, version, description=""):
+        epilog = "Issue tracker: https://github.com/thombashi/tcconfig/issues"
+
+        self.parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=description, epilog=epilog)
+        self.parser.add_argument(
+            "--version", action="version", version="%(prog)s " + version)
+
+        self._add_tc_command_arg_group()
+        self._add_log_level_argument_group()
+
+    def _add_log_level_argument_group(self):
+        dest = "log_level"
+
+        group = self.parser.add_mutually_exclusive_group()
+        group.add_argument(
+            "--debug", dest=dest, action="store_const",
+            const=logbook.DEBUG, default=logbook.INFO,
+            help="for debug print.")
+        group.add_argument(
+            "--quiet", dest=dest, action="store_const",
+            const=logbook.NOTSET, default=logbook.INFO,
+            help="suppress execution log messages.")
+
+        return group
+
+    def _add_tc_command_arg_group(self):
+        group = self.parser.add_mutually_exclusive_group()
+        group.add_argument(
+            "--tc-command", dest="tc_command_output", action="store_const",
+            const=TcCommandOutput.STDOUT, default=TcCommandOutput.NOT_SET,
+            help="""
+            display tc commands to be executed and exit.
+            these commands are not executed.
+            """)
+
+        group.add_argument(
+            "--tc-script", dest="tc_command_output", action="store_const",
+            const=TcCommandOutput.SCRIPT, default=TcCommandOutput.NOT_SET,
+            help="""
+            generate a script file that described tc commands which equivalent
+            with execution tcconfig command. the script can be execute without
+            tcconfig package installation.
+            """)
---
+++ packages/tcconfig/tcconfig-0.12.2.tar.gz/tcconfig-0.12.2/test/test_tcset_one.py
@@ -0,0 +1,221 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+
+import itertools
+import platform
+
+import pingparsing
+import pytest
+from subprocrunner import SubprocessRunner
+import typepy
+
+from tcconfig._const import TcCommand
+
+from .common import execute_tcdel
+
+
+WAIT_TIME = 3  # [sec]
+
+
+@pytest.fixture
+def device_option(request):
+    return request.config.getoption("--device")
+
+
+@pytest.fixture
+def dst_host_option(request):
+    return request.config.getoption("--dst-host")
+
+
+@pytest.fixture
+def transmitter():
+    transmitter = pingparsing.PingTransmitter()
+    transmitter.ping_option = "-f -q"
+    transmitter.waittime = WAIT_TIME
+
+    return transmitter
+
+
+@pytest.fixture
+def pingparser():
+    return pingparsing.PingParsing()
+
+
+class Test_tcset_one_network(object):
+    """
+    Tests in this class are not executable on CI services.
+    Execute the following command at the local environment to running tests:
+
+        python setup.py test --addopts \
+            "--device=<test device> --dst-host=<hostname/IP-addr>"
+
+    These tests expected to execute in the following environment:
+       - Linux w/ iputils-ping package
+       - English locale (for parsing ping output)
+    """
+
+    @pytest.mark.parametrize(["shaping_algo", "delay"], [
+        [params[0], params[1]]
+        for params in itertools.product(["htb", "tbf"], [100])
+    ])
+    def test_const_latency(
+            self, device_option, dst_host_option, transmitter, pingparser,
+            shaping_algo, delay):
+        if device_option is None:
+            pytest.skip("device option is null")
+
+        if typepy.is_null_string(dst_host_option):
+            pytest.skip("destination host is null")
+
+        execute_tcdel(device_option)
+        transmitter.destination_host = dst_host_option
+
+        # w/o latency tc ---
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        without_tc_rtt_avg = pingparser.rtt_avg
+
+        # w/ latency tc ---
+        command_list = [
+            TcCommand.TCSET,
+            "--device {:s}".format(device_option),
+            "--shaping-algo {:s}".format(shaping_algo),
+            "--delay {:d}".format(delay),
+        ]
+        assert SubprocessRunner(" ".join(command_list)).run() == 0
+
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        with_tc_rtt_avg = pingparser.rtt_avg
+
+        # assertion ---
+        rtt_diff = with_tc_rtt_avg - without_tc_rtt_avg
+        assert rtt_diff > (delay / 2.0)
+
+        # finalize ---
+        execute_tcdel(device_option)
+
+    @pytest.mark.skipif("platform.system() == 'Windows'")
+    @pytest.mark.parametrize(["delay", "delay_distro"], [
+        [100, 50],
+    ])
+    def test_const_latency_distro(
+            self, device_option, dst_host_option, transmitter, pingparser,
+            delay, delay_distro):
+        if typepy.is_null_string(dst_host_option):
+            # alternative to pytest.mark.skipif
+            return
+
+        execute_tcdel(device_option)
+        transmitter.destination_host = dst_host_option
+
+        # w/o latency tc ---
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        without_tc_rtt_avg = pingparser.rtt_avg
+        without_tc_rtt_mdev = pingparser.rtt_mdev
+
+        # w/ latency tc ---
+        command_list = [
+            TcCommand.TCSET,
+            "--device " + device_option,
+            "--delay {:d}".format(delay),
+            "--delay-distro {:d}".format(delay_distro),
+        ]
+        assert SubprocessRunner(" ".join(command_list)).run() == 0
+
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        with_tc_rtt_avg = pingparser.rtt_avg
+        with_tc_rtt_mdev = pingparser.rtt_mdev
+
+        # assertion ---
+        rtt_diff = with_tc_rtt_avg - without_tc_rtt_avg
+        assert rtt_diff > (delay / 2.0)
+
+        rtt_diff = with_tc_rtt_mdev - without_tc_rtt_mdev
+        assert rtt_diff > (delay_distro / 2.0)
+
+        # finalize ---
+        execute_tcdel(device_option)
+
+    @pytest.mark.parametrize(["option", "value"], [
+        ["--loss", 10],
+        ["--corrupt", 10],
+    ])
+    def test_const_packet_loss(
+            self, device_option, dst_host_option, transmitter, pingparser,
+            option, value):
+        if typepy.is_null_string(dst_host_option):
+            # alternative to pytest.mark.skipif
+            return
+
+        execute_tcdel(device_option)
+        transmitter.destination_host = dst_host_option
+
+        # w/o packet loss tc ---
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        without_tc_loss_rate = pingparser.packet_loss_rate
+
+        # w/ packet loss tc ---
+        command_list = [
+            TcCommand.TCSET,
+            "--device " + device_option,
+            "{:s} {:f}".format(option, value),
+        ]
+        assert SubprocessRunner(" ".join(command_list)).run() == 0
+
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        with_tc_loss_rate = pingparser.packet_loss_rate
+
+        # assertion ---
+        loss_diff = with_tc_loss_rate - without_tc_loss_rate
+        assert loss_diff > (value / 2)
+
+        # finalize ---
+        execute_tcdel(device_option)
+
+    @pytest.mark.parametrize(["option", "value"], [
+        ["--duplicate", 50],
+    ])
+    def test_const_packet_duplicate(
+            self, device_option, dst_host_option, transmitter, pingparser,
+            option, value):
+        if typepy.is_null_string(dst_host_option):
+            # alternative to pytest.mark.skipif
+            return
+
+        execute_tcdel(device_option)
+        transmitter.destination_host = dst_host_option
+
+        # w/o packet duplicate tc ---
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        without_tc_duplicate_rate = pingparser.packet_duplicate_rate
+
+        # w/ packet duplicate tc ---
+        command_list = [
+            TcCommand.TCSET,
+            "--device {:s}".format(device_option),
+            "{:s} {:f}".format(option, value),
+        ]
+        assert SubprocessRunner(" ".join(command_list)).run() == 0
+
+        result = transmitter.ping()
+        pingparser.parse(result.stdout)
+        with_tc_duplicate_rate = pingparser.packet_duplicate_rate
+
+        # assertion ---
+        duplicate_rate_diff = with_tc_duplicate_rate - without_tc_duplicate_rate
+        assert duplicate_rate_diff > (value / 2)
+
+        # finalize ---
+        execute_tcdel(device_option)
