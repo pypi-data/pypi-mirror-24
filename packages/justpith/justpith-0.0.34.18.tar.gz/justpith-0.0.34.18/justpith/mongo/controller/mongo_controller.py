af7ceb4f65c7fb02e86e9aa1eb0a10121c5bcdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:50 +0000

    Add package justpith/0.0.34.18/justpith-0.0.34.18.tar.gz

---
+++ packages/justpith/justpith-0.0.34.18.tar.gz/justpith-0.0.34.18/justpith/mongo/controller/mongo_controller.py
@@ -0,0 +1,29 @@
+from ..mongo import Mongo
+
+class MongoController(Mongo):
+    def __init__(self,host, port, db):
+        super(MongoController,self).__init__(host, port, db)
+        self.collection = "Controller"
+
+    def insert_job(self,job):
+        selected_collection = self.connection[self.collection]
+        selected_collection.insert_one(job)
+
+    def insert_model_into_job(self, job_id, model_id, type):
+        selected_collection = self.connection[self.collection]
+        if type == "text":
+            result = selected_collection.update_one({"_id":job_id},{"$set":{"id_text": str(model_id)}})
+        elif type == "matrix":
+            result = selected_collection.update_one({"_id":job_id}, {"$set": {"id_matrix": str(model_id)}})
+
+    def get_job(self, job_id):
+        selected_collection = self.connection[self.collection]
+        result = selected_collection.find_one({"_id": job_id} )
+        return result
+
+    def deactiva_job(self, job_id):
+        pass
+
+    def active_job(self, job_id):
+        selected_collection = self.connection[self.collection]
+        result = selected_collection.update_one({"_id": job_id}, {"$set": {"active": 1}})
