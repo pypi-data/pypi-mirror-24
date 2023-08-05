a2ac6726c4b251ea09ce363193f904a6319e75f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package pp-saturn/0.1/pp_saturn-0.1.tar.gz

---
+++ packages/pp-saturn/pp_saturn-0.1.tar.gz/pp_saturn-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pp_saturn',
+      version='0.1',
+      description="Saturn (especially Saturn V) was the main vehicle for both manned and unmanned Apollo's missions. PP's Saturn is a collection of python helpers.",
+      url='https://git.parcelperform.com/dev-team/pp_saturn',
+      author='Khang Nguyen',
+      author_email='khang@parcelperform.com',
+      packages=['pp_saturn'],
+      zip_safe=False)
