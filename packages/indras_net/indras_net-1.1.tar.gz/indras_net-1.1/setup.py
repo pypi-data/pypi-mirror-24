a9f6b13553dcfb690a28b4c4fd2043c8a12bd749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:41 +0000

    Add package indras_net/1.1/indras_net-1.1.tar.gz

---
+++ packages/indras_net/indras_net-1.1.tar.gz/indras_net-1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='indras_net',
+    version='1.1',
+    description='A framework for agent-based modeling in Python.',
+    url='http://github.com/gcallah/indra',
+    author='Gene Callahan and Nathan Conroy',
+    author_email='gcallah@mac.com',
+    license='GNU',
+    zip_safe=False,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                 "tests"]),
+    install_requires=[
+          "networkx",
+          "numpy",
+    ],
+    test_suite="",
+    entry_points={
+        "console_scripts": ["bigbox = bigbox.__main__:main",
+                            "schelling = schelling.__main__:main",
+                            "models = models.__main__:main"
+                           ]
+    },
+)
