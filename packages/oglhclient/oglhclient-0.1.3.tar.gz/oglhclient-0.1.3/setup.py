59a3a97768441852191c782289a40ea96a8bbc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:34 +0000

    Add package oglhclient/0.1.3/oglhclient-0.1.3.tar.gz

---
+++ packages/oglhclient/oglhclient-0.1.3.tar.gz/oglhclient-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'oglhclient',
+  packages = ['oglhclient'],
+  version = '0.1.3',
+  description = 'A client for Lighthouse API',
+  author = 'Lighthouse Team',
+  author_email = 'engineering@opengear.com',
+  url = 'https://github.com/thiagolcmelo/oglhclient',
+  download_url = 'https://github.com/thiagolcmelo/oglhclient/archive/0.1.tar.gz',
+  keywords = ['api', 'opengear', 'lighthouse'],
+  classifiers = [],
+)
