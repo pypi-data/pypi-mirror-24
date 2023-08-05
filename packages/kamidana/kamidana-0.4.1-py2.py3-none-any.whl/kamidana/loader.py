62a082c39e42f946f9e293aaff48ababf3a615b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:35 +0000

    Add package kamidana/0.4.1/kamidana-0.4.1-py2.py3-none-any.whl

---
+++ packages/kamidana/kamidana-0.4.1-py2.py3-none-any.whl/kamidana/loader.py
@@ -0,0 +1,34 @@
+import sys
+from dictknife import deepmerge
+from dictknife import loading
+from dictknife.langhelpers import reify
+from magicalimport import import_module
+from . import collect_marked_items
+from .interfaces import ITemplateLoader
+
+
+class TemplateLoader(ITemplateLoader):
+    def __init__(self, data_path_list, additional_path_list, extensions, format=None):
+        self.data_path_list = data_path_list
+        self.additional_path_list = additional_path_list
+        self.extensions = extensions
+        self.format = format
+
+    def load(self, filename):
+        with open(filename) as rf:
+            return rf.read()
+
+    @reify
+    def data(self):
+        data = deepmerge(* [loading.loadfile(d) for d in self.data_path_list], override=True)
+        if self.format is not None:
+            data = deepmerge(data, loading.load(sys.stdin, format=self.format), override=True)
+        return data
+
+    @reify
+    def additionals(self):
+        d = {}
+        for path in self.additional_path_list:
+            m = import_module(path)
+            d = deepmerge(d, collect_marked_items(m))
+        return d
