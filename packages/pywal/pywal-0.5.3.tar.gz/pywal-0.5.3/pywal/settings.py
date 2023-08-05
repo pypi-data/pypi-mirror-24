183554d0e6f41f16ac5dd1d221af2d5e0c38a791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:19 +0000

    Add package pywal/0.5.3/pywal-0.5.3.tar.gz

---
+++ packages/pywal/pywal-0.5.3.tar.gz/pywal-0.5.3/pywal/settings.py
@@ -0,0 +1,21 @@
+"""
+                                      '||
+... ...  .... ... ... ... ...  ....    ||
+ ||'  ||  '|.  |   ||  ||  |  '' .||   ||
+ ||    |   '|.|     ||| |||   .|' ||   ||
+ ||...'     '|       |   |    '|..'|' .||.
+ ||      .. |
+''''      ''
+Created by Dylan Araps.
+"""
+
+import pathlib
+
+
+__version__ = "0.5.3"
+
+
+HOME = pathlib.Path.home()
+CACHE_DIR = HOME / ".cache/wal/"
+MODULE_DIR = pathlib.Path(__file__).parent
+COLOR_COUNT = 16
