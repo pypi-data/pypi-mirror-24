1d9976852a643dfb16b096d245faf4e81a8a743c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:42 +0000

    Add package serviceform/0.9.8/serviceform-0.9.8.tar.gz

---
+++ packages/serviceform/serviceform-0.9.8.tar.gz/serviceform-0.9.8/CHANGES.rst
@@ -0,0 +1,16 @@
+0.9.8 (2017-08-01)
+------------------
+
+- Fix MANIFEST.in to include translation files
+
+
+0.9.7 (2017-08-01)
+------------------
+
+- Fix requirements in setup.py
+
+
+0.9.6 (2017-08-01)
+------------------
+
+ - Bugfixes
---
+++ packages/serviceform/serviceform-0.9.8.tar.gz/serviceform-0.9.8/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="serviceform",
+    version='0.9.8',
+    description="Django web application to collect volunteering willingness "
+                "data and report to responsible people",
+    long_description=readme,
+    author="Tuomas Airaksinen",
+    author_email="tuomas.airaksinen@gmail.com",
+    url="https://github.com/tuomas2/serviceform",
+    keywords='django, church, volunteering, recruiting, reporting',
+    packages=['serviceform.'+i for i in find_packages('serviceform')],
+    include_package_data=True,
+    install_requires=[
+        "Django",
+        "django-libsass",
+        "psycopg2",
+        "django-nested-admin",
+        "django-crispy-forms",
+        "django-colorful",
+        "colour",
+        "django-redis-cache",
+        "django-compressor",
+        "django-guardian",
+        "django-select2-forms",
+        "raven",
+    ],
+    extras_require={
+        'cachalot': ['django-cachalot'],
+        'grappelli': ['django-grappelli'],
+        'celery': ['django-celery', 'celery'],
+    },
+    license="GPL",
+    zip_safe=False,
+    classifiers=[
+        'Natural Language :: English',
+        'Natural Language :: Finnish',
+        'Framework :: Django',
+        'Intended Audience :: Religion',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
