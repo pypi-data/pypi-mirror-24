792aa7be6647fe1e8670ffc60d5b64b318a84622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:44 +0000

    Add package snipsfakeweather/0.1.4.5.4/snipsfakeweather-0.1.4.5.4.tar.gz

---
+++ packages/snipsfakeweather/snipsfakeweather-0.1.4.5.4.tar.gz/snipsfakeweather-0.1.4.5.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='snipsfakeweather',
+    version='0.1.4.5.4',
+    description='Fake weather forescasts skill for Snips',
+    author='Michael Fester',
+    author_email='michael.fester@gmail.com',
+    url='https://github.com/snipsco/snips-skill-fakeweather',
+    download_url='',
+    license='MIT',
+    install_requires=[],
+    test_suite="tests",
+    keywords=['snips'],
+    include_package_data=True,
+    packages=[
+        'snipsfakeweather'
+    ]
+)
