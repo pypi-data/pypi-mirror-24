8803235390d265f6a6d8a6782bb9405bb5875418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package snipsskills/0.1.4.8.4/snipsskills-0.1.4.8.4-py2.py3-none-any.whl

---
+++ packages/snipsskills/snipsskills-0.1.4.8.4-py2.py3-none-any.whl/snipsskills/commands/run.py
@@ -0,0 +1,75 @@
+# -*-: coding utf-8 -*-
+"""The run command."""
+# pylint: disable=too-few-public-methods,import-error
+
+import os
+import subprocess
+from sys import path
+
+from snipsskillscore.server import Server
+
+from .base import Base
+
+path.append(".snips/intents")
+path.append(".snips/intents/intents")
+
+# pylint: disable=wrong-import-position,wrong-import-order
+from intent_registry import IntentRegistry
+# pylint: disable=wildcard-import,wrong-import-position,wrong-import-order
+from intents import *
+
+BINDINGS_FILE = "bindings.py"
+INTENT_REGISTRY_FILE = ".snips/intents/intent_registry.py"
+
+try:
+    from subprocess import DEVNULL # py3k
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+
+class Run(Base):
+    """The run command."""
+
+    # pylint: disable=undefined-variable,exec-used,eval-used
+    def run(self):
+        """ Command runner. """
+        self.snipsfile = Base.load_snipsfile()
+
+        self.skills = {}
+        for skilldef in self.snipsfile.skills:
+            module_name = skilldef.package_name + "." + skilldef.package_name
+            exec("from {} import {}".format(module_name, skilldef.class_name))
+            cls = eval(skilldef.class_name)
+            self.skills[skilldef.package_name] = cls(**skilldef.params)
+
+        print("Starting the Snips SDK.")
+        p1 = subprocess.Popen("docker stop snips".split())
+        output, error = p1.communicate()
+        p1 = subprocess.Popen("snips &> /dev/null".split())
+        output, error = p1.communicate()
+        # p1 = subprocess.Popen("snips".split(), stdout=DEVNULL)
+        # output, error = p1.communicate()
+        # os.system("snips &> /dev/null")
+
+        print("[SKILLS] Starting the Snips Skills server.")
+        registry = IntentRegistry()
+        server = Server(self.snipsfile, registry, self.handle_intent)
+        server.start()
+
+    def handle_intent(self, intent):
+        """ Handle an intent.
+
+        :param intent: the incoming intent to handle.
+        """
+        for skilldef in self.snipsfile.skills:
+            intent_def = skilldef.find(intent)
+            if intent_def != None:
+                skill = self.skills[skilldef.package_name]
+                if intent_def.action.startswith("{%"):
+                    action = intent_def.action \
+                        .replace("{%", "") \
+                        .replace("%}", "") \
+                        .strip()
+                    exec(action)
+                else:
+                    getattr(skill, intent_def.action)()
