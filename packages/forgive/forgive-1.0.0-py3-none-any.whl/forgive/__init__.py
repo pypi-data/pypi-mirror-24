72f0027c552f030e6bcc8928219ea7d7f01244d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package forgive/1.0.0/forgive-1.0.0-py3-none-any.whl

---
+++ packages/forgive/forgive-1.0.0-py3-none-any.whl/forgive/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from forgive.db import ForgiveDB
+
+__all__ = [
+    '__version__'
+    'ForgiveDB',
+]
+
+__version__ = '1.0.0'
