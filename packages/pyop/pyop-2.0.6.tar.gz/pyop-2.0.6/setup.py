9a529a8b2aade099f7be23d5c6c435cd6d81dd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:50 +0000

    Add package pyop/2.0.6/pyop-2.0.6.tar.gz

---
+++ packages/pyop/pyop-2.0.6.tar.gz/pyop-2.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyop',
+    version='2.0.6',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/its-dirg/pyop',
+    license='Apache 2.0',
+    author='Rebecka Gulliksson',
+    author_email='rebecka.gulliksson@umu.se',
+    description='OpenID Connect Provider (OP) library in Python.',
+    install_requires=[
+        'oic==0.9.0.0',
+        'pymongo'
+    ]
+)
