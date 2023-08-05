83ffd98eb7f3f6a58afc593f4ee01c74c745e046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package TimeSeriesGif/0.2/TimeSeriesGif-0.2.tar.gz

---
+++ packages/TimeSeriesGif/TimeSeriesGif-0.2.tar.gz/TimeSeriesGif-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'TimeSeriesGif',
+  packages = ['TimeSeriesGif'],
+  version = '0.2',
+  description = 'Create gifs from pandas time series.',
+  author = 'Andrew Han',
+  author_email = 'handrew11@gmail.com',
+  url = 'https://github.com/handrew/TimeSeriesGif', 
+  download_url = 'https://github.com/handrew/TimeSeriesGif/archive/0.1.tar.gz', 
+  keywords = ['time series', 'pandas', 'visualization'], 
+  install_requires=['numpy', 'pandas', 'matplotlib', 'imageio'],
+  classifiers = []
+)
