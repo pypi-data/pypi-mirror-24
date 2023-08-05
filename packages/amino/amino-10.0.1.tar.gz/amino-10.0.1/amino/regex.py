fa77ff485b51e1938a46ba6f06913d630dce3283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:57 +0000

    Add package amino/10.0.1/amino-10.0.1.tar.gz

---
+++ packages/amino/amino-10.0.1.tar.gz/amino-10.0.1/amino/regex.py
@@ -0,0 +1,77 @@
+import re
+from typing import Any
+
+from amino import L, _, Maybe, Map, List, Either
+
+
+class Regex:
+
+    def __init__(self, spec) -> None:
+        self.spec = spec
+        self.rex = re.compile(spec)
+
+    def __getattr__(self, name):
+        if hasattr(self.rex, name):
+            return getattr(self.rex, name)
+        else:
+            raise AttributeError('Regex has no attribute \'{}\''.format(name))
+
+    def match(self, data, *a, **kw) -> Maybe['Match']:
+        return (
+            Maybe(self.rex.match(data, *a, **kw))
+            .to_either('`{}` does not match `{}`'.format(data, self.spec)) /
+            L(Match)(self, _, data)
+        )
+
+    def search(self, data, *a, **kw) -> Maybe['Match']:
+        return (
+            Maybe(self.rex.search(data, *a, **kw))
+            .to_either('`{}` does not contain `{}`'.format(data, self.spec)) /
+            L(Match)(self, _, data)
+        )
+
+    def __str__(self) -> str:
+        return 'Regex({})'.format(self.spec)
+
+
+class Match:
+
+    def __init__(self, regex: Regex, internal: Any, data: str) -> None:
+        self.regex = regex
+        self.internal = internal
+        self.data = data
+
+    @property
+    def group_map(self) -> Map[str, str]:
+        return Map(self.internal.groupdict())
+
+    m = group_map
+
+    def group(self, id: str) -> Either[str, str]:
+        return (
+            self.group_map
+            .get(id)
+            .to_either('no group `{}` in {}'.format(id, self))
+        )
+
+    g = group
+
+    @property
+    def groups(self) -> List[str]:
+        return List.wrap(self.internal.groups())
+
+    l = groups
+
+    def all_groups(self, *ids: str) -> Either[str, List[str]]:
+        return (self.group_map.get_all(*ids)
+                .to_either('not all groups `{}` in {}'.format(ids, self)))
+
+    @property
+    def match(self) -> str:
+        return self.internal.group(0)
+
+    def __str__(self) -> str:
+        return 'Match({}, {}, {})'.format(self.regex, self.data,
+                                          self.group_map)
+
+__all__ = ('Regex',)
---
+++ packages/amino/amino-10.0.1.tar.gz/amino-10.0.1/amino/tc/traverse.py
@@ -0,0 +1,41 @@
+import abc
+from typing import TypeVar, Generic, Callable
+
+from amino.tc.base import TypeClass
+from amino.func import I
+from amino import _
+
+A = TypeVar('A')
+B = TypeVar('B')
+Z = TypeVar('Z')
+
+
+class TraverseF(Generic[A]):
+    pass
+
+
+class TraverseG(Generic[A]):
+    pass
+
+F = TraverseF
+G = TraverseG
+
+
+class Traverse(TypeClass):
+    # FIXME lens functions return index lenses, which is not a property of
+    # Traverse
+
+    @abc.abstractmethod
+    def traverse(self, fa: F[G[A]], f: Callable[[A], B], tpe: type) -> G[F[B]]:
+        ...
+
+    def flat_traverse(self, fa: F[G[A]], f: Callable[[A], F[B]], tpe: type) -> G[F[B]]:
+        return self.traverse(fa, f, tpe).map(_.join)  # type: ignore
+
+    def sequence(self, fa: F[G[A]], tpe: type) -> G[F[A]]:
+        return self.traverse(fa, I, tpe)
+
+    def flat_sequence(self, fa: F[G[A]], tpe: type) -> G[F[B]]:
+        return self.sequence(fa, tpe).map(_.join)  # type: ignore
+
+__all__ = ('Traverse',)
