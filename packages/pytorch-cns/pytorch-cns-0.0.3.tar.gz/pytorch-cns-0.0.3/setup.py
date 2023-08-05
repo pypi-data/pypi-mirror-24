c965806e6f0c8302cfeaa8a663d49a30ad88ca66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package pytorch-cns/0.0.3/pytorch-cns-0.0.3.tar.gz

---
+++ packages/pytorch-cns/pytorch-cns-0.0.3.tar.gz/pytorch-cns-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='pytorch-cns',
+    version='0.0.3',
+    description='Compressed network search with PyTorch',
+    author='Adam Wentz',
+    author_email='adam@adamwentz.com',
+    url='https://github.com/awentzonline/pytorch-cns',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'torch',
+        'torchvision',
+        'scipy',
+        'six',
+    ]
+)
