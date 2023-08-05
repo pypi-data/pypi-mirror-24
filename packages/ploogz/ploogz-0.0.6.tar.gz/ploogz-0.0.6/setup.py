f7f4b49b32be90a741b31147d7793ae9ed6862f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:20 +0000

    Add package ploogz/0.0.6/ploogz-0.0.6.tar.gz

---
+++ packages/ploogz/ploogz-0.0.6.tar.gz/ploogz-0.0.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: setup.py
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+This file is used to create the package uploaded to PyPI.
+"""
+
+import ploogz
+
+from distutils.core import setup
+setup(
+  name='ploogz',
+  packages=['ploogz'],  # This must be the same as the name above.
+  version=ploogz.__version__,
+  install_requires=[
+    'automat'
+  ],
+  python_requires=">=3.6.1",
+  description='A pretty simple plugin framework.',
+  license='MIT',
+  author='Pat Daburu',
+  author_email='pat@daburu.net',
+  url='http://ploogz.readthedocs.io/en/latest/index.html',  # Use the URL to the github repo.
+  download_url='https://github.com/pblair/ploogz/archive/{version}.tar.gz'.format(version=ploogz.__version__),
+  keywords=['plugin'],  # arbitrary keywords
+  classifiers=[],
+)
