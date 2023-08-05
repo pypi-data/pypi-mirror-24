1086e98c37ef5bcc3c0fc7f73ef573de8f390835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package pretix/1.6.1/pretix-1.6.1.tar.gz

---
+++ packages/pretix/pretix-1.6.1.tar.gz/pretix-1.6.1/pretix/plugins/banktransfer/mt940import.py
@@ -0,0 +1,27 @@
+import io
+
+import mt940
+
+from pretix.base.decimal import round_decimal
+
+
+def parse(file):
+    data = file.read()
+    try:
+        import chardet
+
+        charset = chardet.detect(data)['encoding']
+    except ImportError:
+        charset = file.charset
+    data = data.decode(charset or 'utf-8')
+    mt = mt940.parse(io.StringIO(data.strip()))
+    result = []
+    for t in mt:
+        result.append({
+            'reference': "\n".join([
+                t.data.get(f) for f in ('transaction_details', 'customer_reference', 'bank_reference',
+                                        'extra_details', 'non_swift_text') if t.data.get(f, '')]),
+            'amount': str(round_decimal(t.data['amount'].amount)),
+            'date': t.data['date'].isoformat()
+        })
+    return result
