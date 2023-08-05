9fc7bdccc65a57dd345b0caef822b1fd6e960fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:58 +0000

    Add package aquests/0.7.6.10/aquests-0.7.6.10.tar.gz

---
+++ packages/aquests/aquests-0.7.6.10.tar.gz/aquests-0.7.6.10/aquests/response_builder.py
@@ -0,0 +1,9 @@
+from .lib.attrdict import AttrDict
+
+class HTTPResponse:
+	def __init__ (self, handler):
+		self.response = handler.response
+		self.request = self.response.request
+		
+	def __getattr__ (self, name):
+		return getattr (self.response, name)
