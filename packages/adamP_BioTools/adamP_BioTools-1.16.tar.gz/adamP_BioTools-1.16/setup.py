9e4feba3fa36ceea3c0af083a8fda53d5268e0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:53 +0000

    Add package adamP_BioTools/1.16/adamP_BioTools-1.16.tar.gz

---
+++ packages/adamP_BioTools/adamP_BioTools-1.16.tar.gz/adamP_BioTools-1.16/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='adamP_BioTools',
+	version='1.16',
+	description='Personal Tools for working with biological data',
+	url='https://github.com/adamnicolaspelletier/adamP_BioTools',
+	author='Adam Pelletier',
+	author_email='adam.nicolas.pelletier@gmail.com',
+	license='None',
+	packages=['adamP_BioTools'],
+	download_url = 'https://github.com/adamnicolaspelletier/adamP_BioTools/archive/1.16.tar.gz',
+	zip_safe=False)
