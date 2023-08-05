041da8beebbfb453b731078d36865fa598232fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:54 +0000

    Add package insta_browser/0.7.2/insta_browser-0.7.2-py2.py3-none-any.whl

---
+++ packages/insta_browser/insta_browser-0.7.2-py2.py3-none-any.whl/insta_browser-0.7.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,26 @@
+Instabrowser
+============
+
+|Build Status| |PyPI| |Code Climate|
+
+👍 ♥️ 🏆 Library for instagram.com automation. Like instagram feed (whole
+or by count), like username profile (whole or by count), like posts from
+location.
+
+You need to have PhantomJS or Chrome web driver for work.
+
+Download and place binary from archive to bin folder: -
+`PhantomJS <http://phantomjs.org/download.html>`__ -
+`Chrome <https://sites.google.com/a/chromium.org/chromedriver/downloads>`__
+
+Example of using package:
+`insta\_bot <https://github.com/aLkRicha/insta_bot>`__
+
+.. |Build Status| image:: https://travis-ci.org/aLkRicha/insta_browser.svg?branch=master
+   :target: https://travis-ci.org/aLkRicha/insta_browser
+.. |PyPI| image:: https://img.shields.io/pypi/v/insta_browser.svg
+   :target: https://pypi.python.org/pypi/insta_browser
+.. |Code Climate| image:: https://img.shields.io/codeclimate/github/aLkRicha/insta_browser.svg
+   :target: https://codeclimate.com/github/aLkRicha/insta_browser
+
+
---
+++ packages/insta_browser/insta_browser-0.7.2-py2.py3-none-any.whl/insta_browser/base/processor.py
@@ -0,0 +1,58 @@
+import selenium.common.exceptions as excp
+
+
+class BaseProcessor:
+    post_skipped_excluded = 0
+    posts_count_to_like = 0
+    feed_scrolled_down = 0
+    post_already_liked = 0
+    post_excluded = 0
+    post_skipped = 0
+    like_limit = 500
+    progress = None
+    post_liked = 0
+    heart = None
+    count = 0
+
+    def get_summary(self):
+        return {'liked': self.post_liked,
+                'skipped': self.post_skipped,
+                'excluded': self.post_skipped_excluded,
+                'already_liked': self.post_already_liked,
+                'scrolled': self.feed_scrolled_down}
+
+    @staticmethod
+    def _get_feed_post_link(post):
+        """
+        Get link to post from post web-element from feed
+
+        :param post:
+        :return:
+        """
+        try:
+            post_link = post.find_element_by_css_selector('div:nth-child(3) div:nth-child(4) a')
+        except excp.NoSuchElementException:
+            post_link = post.find_element_by_css_selector('div:nth-child(3) div:nth-child(3) a')
+        return post_link.get_attribute('href')
+
+    @staticmethod
+    def _get_feed_post_media(post):  # TODO: refactor searching image
+        """
+        Get link to post from post web-element from feed
+
+        :param post:
+        :return:
+        """
+        try:
+            image = post.find_element_by_css_selector('div:nth-child(2) img')
+            return image.get_attribute('src')
+        except excp.NoSuchElementException:
+            pass
+
+        try:
+            video = post.find_element_by_tag_name('video')
+            return video.get_attribute('src')
+        except excp.NoSuchElementException:
+            pass
+
+        return False
