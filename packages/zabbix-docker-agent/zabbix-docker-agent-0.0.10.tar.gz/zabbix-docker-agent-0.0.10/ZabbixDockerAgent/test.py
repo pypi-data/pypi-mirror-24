438b58534d3dc2c9d8c4442c80147ff9b69d7a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:29 +0000

    Add package zabbix-docker-agent/0.0.10/zabbix-docker-agent-0.0.10.tar.gz

---
+++ packages/zabbix-docker-agent/zabbix-docker-agent-0.0.10.tar.gz/zabbix-docker-agent-0.0.10/ZabbixDockerAgent/test.py
@@ -0,0 +1,19 @@
+import json
+from ZabbixDockerAgent import dockerData
+
+
+class TestRun(object):
+    def __init__(self):
+        self.collect_metrics()
+
+    def collect_metrics(self):
+        dckr = dockerData()
+
+        dckr.discover_containers()
+
+        info = {}
+        for containerId in dckr.containers:
+            data = dckr.metrics(containerId)
+            info[containerId] = data
+
+        print(json.dumps(info))
