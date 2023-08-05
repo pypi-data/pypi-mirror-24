f0f35e48fe7e45f051e77574249343e0cdf6762a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package OmniDatabase/0.28/OmniDatabase-0.28.tar.gz

---
+++ packages/OmniDatabase/OmniDatabase-0.28.tar.gz/OmniDatabase-0.28/README.md
@@ -0,0 +1,6 @@
+# OmniDatabase.
+
+Python library that provides methods to gather metadata information about any supported DBMS (uses [spartacus](https://github.com/wind39/spartacus))
+
+This is a Work in Progress.
+
---
+++ packages/OmniDatabase/OmniDatabase-0.28.tar.gz/OmniDatabase-0.28/setup.py
@@ -0,0 +1,52 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2014-2017 The OmniDB Team
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import os
+import sys
+from setuptools import setup
+
+rootdir = os.path.abspath(os.path.dirname(__file__))
+long_description = open(os.path.join(rootdir, 'README.md')).read()
+
+setup(name='OmniDatabase',
+      version='0.28',
+      description='Generic database metadata and access provider',
+      long_description=long_description,
+      url='http://github.com/OmniDB/OmniDatabase',
+      author='William Ivanski',
+      author_email='william.ivanski@gmail.com',
+      license='MIT',
+      packages=['OmniDatabase'],
+      install_requires=['Spartacus'],
+      extras_require={
+        'postgresql': ['Spartacus[postgresql]'],
+        'mysql':      ['Spartacus[mysql]'],
+        'mariadb':    ['Spartacus[mariadb]'],
+        'firebird':   ['Spartacus[firebird]'],
+        'oracle':     ['Spartacus[oracle]'],
+        'mssql':      ['Spartacus[mssql]'],
+        'ibmdb2':     ['Spartacus[ibmdb2]'],
+        'complete':   ['Spartacus[complete]']
+      },
+      zip_safe=False)
