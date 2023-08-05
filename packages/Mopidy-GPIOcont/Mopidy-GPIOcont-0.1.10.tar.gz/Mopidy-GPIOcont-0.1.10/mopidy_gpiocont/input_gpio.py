20690ee158774ff83c340df6c10c11d2c8b636ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:17 +0000

    Add package Mopidy-GPIOcont/0.1.10/Mopidy-GPIOcont-0.1.10.tar.gz

---
+++ packages/Mopidy-GPIOcont/Mopidy-GPIOcont-0.1.10.tar.gz/Mopidy-GPIOcont-0.1.10/mopidy_gpiocont/input_gpio.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+
+import logging
+logger = logging.getLogger(__name__)
+
+class input_GPIO():
+    def __init__(self, frontend, pins):
+        self.frontend = frontend
+
+        try:
+
+            # set pin mode to BCM
+            GPIO.setmode(GPIO.BCM)
+
+            # Play button
+            GPIO.setup(pins['play_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(pins['play_pin'], GPIO.FALLING, callback=self.play, bouncetime=30)
+
+        except RuntimeError:
+            logger.error("TTSGPIO: Not enough permission ")
+
+    def play(self, channel):
+        self.frontend.input_event('play')
