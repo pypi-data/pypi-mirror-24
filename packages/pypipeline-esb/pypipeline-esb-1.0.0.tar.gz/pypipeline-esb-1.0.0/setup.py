4827fc1f353501a1fbe4eb8f273aa82374ac47f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:29 +0000

    Add package pypipeline-esb/1.0.0/pypipeline-esb-1.0.0.tar.gz

---
+++ packages/pypipeline-esb/pypipeline-esb-1.0.0.tar.gz/pypipeline-esb-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name = "pypipeline-esb",
+    packages = ["pypipeline"],
+    version = "1.0.0",
+    description = "ESB for Python",
+    author = "Vaibhav Sinha",
+    author_email = "vaibhavsinh@gmail.com",
+    url = "https://github.com/vaibhav-sinha/pypipeline",
+    keywords = ["esb", "eip", "pipeline"],
+    python_requires='>=3',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+PyPipeline is meant to be a lightweight ESB, configurable via an intutive DSL. PyPipeline implements many of the Enterprise Integration Patterns.
+""",
+)
