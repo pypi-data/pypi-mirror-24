d686015f82139739f9cf87baafb2af724fea87e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:43 +0000

    Add package drf-fcm/0.4.3/drf-fcm-0.4.3.tar.gz

---
+++ packages/drf-fcm/drf-fcm-0.4.3.tar.gz/drf-fcm-0.4.3/drf_fcm/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Device
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'device_id', 'reg_id', 'is_active', 'user')
+    fields = ('name', 'device_id', 'reg_id', 'is_active', 'user')
+    ordering = ('name',)
+
+
+admin.site.register(Device, DeviceAdmin)
