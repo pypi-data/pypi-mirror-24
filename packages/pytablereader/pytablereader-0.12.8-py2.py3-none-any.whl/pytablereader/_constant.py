b0008f404ed29f8f740b7febed9bd43e3d1cba4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package pytablereader/0.12.8/pytablereader-0.12.8-py2.py3-none-any.whl

---
+++ packages/pytablereader/pytablereader-0.12.8-py2.py3-none-any.whl/pytablereader/_constant.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import enum
+
+
+class SourceType(object):
+    TEXT = "text"
+    FILE = "file"
+    URL = "url"
+
+
+class TableNameTemplate(object):
+    __FORMAT = "%({:s})s"
+    DEFAULT = __FORMAT.format("default")
+    FILENAME = __FORMAT.format("filename")
+    FORMAT_NAME = __FORMAT.format("format_name")
+    FORMAT_ID = __FORMAT.format("format_id")
+    GLOBAL_ID = __FORMAT.format("global_id")
+    KEY = __FORMAT.format("key")
+    TITLE = __FORMAT.format("title")
+    SHEET = __FORMAT.format("sheet")
+
+
+@enum.unique
+class PatternMatch(enum.Enum):
+    OR = 0
+    AND = 1
