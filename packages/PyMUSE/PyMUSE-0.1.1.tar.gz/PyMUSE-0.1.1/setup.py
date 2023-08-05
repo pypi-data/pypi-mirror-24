fe942b76699e75da225fe81ddfb2feb72499b6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:23 +0000

    Add package PyMUSE/0.1.1/PyMUSE-0.1.1.tar.gz

---
+++ packages/PyMUSE/PyMUSE-0.1.1.tar.gz/PyMUSE-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+#from Cython.Build import cythonize
+
+setup(
+    name="PyMUSE",
+    version="0.1.1",
+    description="Python software for handling VLT/MUSE data.",
+    author="I. Pessa",
+    license="MIT",
+    author_email="ismael.pessa@gmail.com",
+    url="https://github.com/ismaelpessa/Muse_Cube",
+    packages=['PyMUSE'])
