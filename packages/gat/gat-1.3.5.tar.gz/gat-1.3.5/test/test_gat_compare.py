5f604d050d1126dea16542117b2e99240e61079d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package gat/1.3.5/gat-1.3.5.tar.gz

---
+++ packages/gat/gat-1.3.5.tar.gz/gat-1.3.5/test/test_gat_compare.py
@@ -0,0 +1,14 @@
+"""test gat implementation."""
+
+import unittest
+import os
+import numpy
+
+
+class GatTest(unittest.TestCase):
+
+    def shortDescription(self):
+        return None
+
+if __name__ == '__main__':
+    unittest.main()
