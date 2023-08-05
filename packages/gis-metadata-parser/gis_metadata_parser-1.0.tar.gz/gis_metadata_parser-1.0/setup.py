ba0495c538381d6ce5a1b360a5cc03ee2e267a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package gis-metadata-parser/1.0/gis_metadata_parser-1.0.tar.gz

---
+++ packages/gis-metadata-parser/gis_metadata_parser-1.0.tar.gz/gis_metadata_parser-1.0/setup.py
@@ -0,0 +1,35 @@
+import subprocess
+import sys
+
+from setuptools import Command, setup
+
+
+class RunTests(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, '-m', 'unittest', 'gis_metadata.tests.tests'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='gis_metadata_parser',
+    description='Parser for GIS metadata standards including FGDC and ISO-19115',
+    keywords='arcgis,fgdc,iso,ISO-19115,ISO-19139,gis,metadata,parser,xml,gis_metadata,gis_metadata_parser',
+    version='1.0',
+    packages=[
+        'gis_metadata', 'gis_metadata.tests'
+    ],
+    install_requires=[
+        'parserutils', 'six'
+    ],
+    url='https://github.com/consbio/gis-metadata-parser',
+    license='BSD',
+    cmdclass={'test': RunTests}
+)
