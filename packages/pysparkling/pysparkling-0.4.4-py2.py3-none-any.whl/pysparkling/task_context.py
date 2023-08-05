b9110ae75919a02ad8f7d56deb9b779f59499bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package pysparkling/0.4.4/pysparkling-0.4.4-py2.py3-none-any.whl

---
+++ packages/pysparkling/pysparkling-0.4.4-py2.py3-none-any.whl/pysparkling/task_context.py
@@ -0,0 +1,31 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class TaskContext(object):
+    def __init__(self, cache_manager, stage_id=0, partition_id=0,
+                 max_retries=3, retry_wait=0):
+        self.cache_manager = cache_manager
+        self.stage_id = stage_id
+        self.partition_id = partition_id
+        self.max_retries = max_retries
+        self.retry_wait = retry_wait
+
+        self.attempt_number = 0
+        self.is_completed = False
+        self.is_running_locally = True
+        self.task_completion_listeners = []
+
+    def _create_child(self):
+        return TaskContext(self.cache_manager, stage_id=self.stage_id + 1,
+                           partition_id=self.partition_id)
+
+    def attemptNumber(self):
+        return self.attempt_number
+
+    def partitionId(self):
+        return self.partition_id
+
+    def stageId(self):
+        return self.stage_id
