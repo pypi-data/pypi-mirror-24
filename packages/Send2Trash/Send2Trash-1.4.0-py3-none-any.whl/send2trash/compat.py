f9652d70107758262b9f91092f30e46d6d185b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:35 +0000

    Add package Send2Trash/1.4.0/Send2Trash-1.4.0-py3-none-any.whl

---
+++ packages/Send2Trash/Send2Trash-1.4.0-py3-none-any.whl/send2trash/compat.py
@@ -0,0 +1,18 @@
+# Copyright 2017 Virgil Dupras
+
+# This software is licensed under the "BSD" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
+# http://www.hardcoded.net/licenses/bsd_license
+
+import sys
+import os
+
+PY3 = sys.version_info[0] >= 3
+if PY3:
+    text_type = str
+    binary_type = bytes
+    environb = os.environb
+else:
+    text_type = unicode
+    binary_type = str
+    environb = os.environ
