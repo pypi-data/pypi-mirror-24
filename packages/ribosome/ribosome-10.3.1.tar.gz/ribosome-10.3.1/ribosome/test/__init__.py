68a9d0ee6d475d0c23987eba13615a86edc24134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:51 +0000

    Add package ribosome/10.3.1/ribosome-10.3.1.tar.gz

---
+++ packages/ribosome/ribosome-10.3.1.tar.gz/ribosome-10.3.1/ribosome/test/__init__.py
@@ -0,0 +1,4 @@
+from ribosome.test.integration import (IntegrationSpec, VimIntegrationSpec,
+                                     PluginIntegrationSpec)
+
+__all__ = ('VimIntegrationSpec', 'IntegrationSpec', 'PluginIntegrationSpec')
