28f488784accd1900169a2da37878dcf8b383847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:15 +0000

    Add package adage/0.8.0/adage-0.8.0.tar.gz

---
+++ packages/adage/adage-0.8.0.tar.gz/adage-0.8.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'adage',
+    author = 'Lukas Heinrich',
+    author_email = 'lukas.heinrich@gmail.com',
+    version = '0.8.0',
+    description = 'running dynamic DAG workflows',
+    packages = find_packages(),
+    install_requires = [
+        'networkx',
+        'pydot2',
+        'pygraphviz',
+        'pydotplus'
+    ]
+)
