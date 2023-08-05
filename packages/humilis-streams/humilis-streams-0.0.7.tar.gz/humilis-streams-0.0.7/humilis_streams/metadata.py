5ea041ba579f20771fae1c22f2d4e271503d9717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:55 +0000

    Add package humilis-streams/0.0.7/humilis-streams-0.0.7.tar.gz

---
+++ packages/humilis-streams/humilis-streams-0.0.7.tar.gz/humilis-streams-0.0.7/humilis_streams/metadata.py
@@ -0,0 +1,12 @@
+"""Project metadata."""
+
+package = "humilis_streams"
+project = "humilis-streams"
+version = '0.0.7'
+description = "Deploys Kinesis streams"
+authors = ["German Gomez-Herrero"]
+authors_string = ', '.join(authors)
+emails = ["german@findhotel.net"]
+license = "MIT"
+copyright = "2017, German Gomez-Herrero and FindHotel BV"
+url = 'http://github.com/humilis/humilis-streams'
