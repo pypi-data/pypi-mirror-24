4ab7d5d9bd08c3a9e95a66facead9e292b066220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:50 +0000

    Add package babbage/0.2.3/babbage-0.2.3.tar.gz

---
+++ packages/babbage/babbage-0.2.3.tar.gz/babbage-0.2.3/babbage/__init__.py
@@ -0,0 +1,8 @@
+""" Babbage, an OLAP-like, light-weight database analytical engine. """
+
+__version__ = "0.2.3"
+
+from babbage.manager import CubeManager, JSONCubeManager  # noqa
+from babbage.api import configure_api  # noqa
+from babbage.validation import validate_model  # noqa
+from babbage.exc import BabbageException, QueryException, BindingException  # noqa
