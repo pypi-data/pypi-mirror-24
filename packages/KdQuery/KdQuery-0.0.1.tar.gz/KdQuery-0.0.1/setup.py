5093b16ddc3e565f8c4ce6d8b417cbdfa7b615dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package KdQuery/0.0.1/KdQuery-0.0.1.tar.gz

---
+++ packages/KdQuery/KdQuery-0.0.1.tar.gz/KdQuery-0.0.1/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='KdQuery',
+
+    version='0.0.1',
+
+    description='Nearest point query for any kd-tree implementation',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/DeVilhena-Paulo/kdtree',
+
+    # Author details
+    author='Paulo Emilio de Vilhena',
+    author_email='pevilhena2@gmail.com',
+
+    # Choose your license
+    license='APv2',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='kdtree',
+
+    py_modules=["KdQuery"],
+)
