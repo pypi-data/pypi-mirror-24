4d7c32fb723133bfd939831de29d68d259658da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:05 +0000

    Add package nimblehelper/0.6.19/nimblehelper-0.6.19.tar.gz

---
+++ packages/nimblehelper/nimblehelper-0.6.19.tar.gz/nimblehelper-0.6.19/nimblehelper/testsuite.py
@@ -0,0 +1,13 @@
+from requests import post, get
+from .settings import KONG_ADMIN_URL
+
+
+def login(url, username, password):
+    data = {"username": username, "password": password}
+    return post(url=url, data=data)
+
+
+def get_x_consumer_id(username):
+    url = KONG_ADMIN_URL + "consumers/" + username + "/"
+    print(url)
+    return get(url=url)
