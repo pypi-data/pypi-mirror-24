44b44608761b322649c079a4f0b1128cf624b386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:28 +0000

    Add package transmute-core/1.4.5/transmute_core-1.4.5-py2.py3-none-any.whl

---
+++ packages/transmute-core/transmute_core-1.4.5-py2.py3-none-any.whl/transmute_core/tests/http_parameters/test_swagger.py
@@ -0,0 +1,28 @@
+from transmute_core.http_parameters import (
+    get_swagger_parameters, ParamSet, Param, Parameters
+)
+from transmute_core.function.signature import Argument, NoDefault
+
+
+def test_post_schema_swagger(parameter_post_schema, context):
+    assert get_swagger_parameters(parameter_post_schema, context)[0].to_primitive() == {
+        "in": "body",
+        "name": "body",
+        "required": True,
+        "schema": {
+            "type": "integer"
+        }
+    }
+
+
+def test_header_only_schema(context):
+    params = Parameters(
+        path=ParamSet({"path": Param(argument_name="path",
+                                     arginfo=Argument("path", NoDefault, int))}),
+    )
+    assert get_swagger_parameters(params, context)[0].to_primitive() == {
+        "in": "path",
+        "name": "path",
+        "required": True,
+        "type": "integer"
+    }
