c2645c0b2e7b4b02f551beaa3b95acaf166a24a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:36 +0000

    Add package cli-helpers/0.2.3/cli_helpers-0.2.3.tar.gz

---
+++ packages/cli-helpers/cli_helpers-0.2.3.tar.gz/cli_helpers-0.2.3/tests/tabular_output/test_delimited_output_adapter.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""Test the delimited output adapter."""
+
+from __future__ import unicode_literals
+from textwrap import dedent
+
+import pytest
+
+from cli_helpers.tabular_output import delimited_output_adapter
+
+
+def test_csv_wrapper():
+    """Test the delimited output adapter."""
+    # Test comma-delimited output.
+    data = [['abc', '1'], ['d', '456']]
+    headers = ['letters', 'number']
+    output = delimited_output_adapter.adapter(iter(data), headers)
+    assert output == dedent('''\
+        letters,number\r\n\
+        abc,1\r\n\
+        d,456\r\n''')
+
+    # Test tab-delimited output.
+    data = [['abc', '1'], ['d', '456']]
+    headers = ['letters', 'number']
+    output = delimited_output_adapter.adapter(
+        iter(data), headers, table_format='tsv')
+    assert output == dedent('''\
+        letters\tnumber\r\n\
+        abc\t1\r\n\
+        d\t456\r\n''')
+
+    with pytest.raises(ValueError):
+        output = delimited_output_adapter.adapter(
+            iter(data), headers, table_format='foobar')
+
+
+def test_unicode_with_csv():
+    """Test that the csv wrapper can handle non-ascii characters."""
+    data = [['观音', '1'], ['Ποσειδῶν', '456']]
+    headers = ['letters', 'number']
+    output = delimited_output_adapter.adapter(data, headers)
+    assert output == dedent('''\
+        letters,number\r\n\
+        观音,1\r\n\
+        Ποσειδῶν,456\r\n''')
+
