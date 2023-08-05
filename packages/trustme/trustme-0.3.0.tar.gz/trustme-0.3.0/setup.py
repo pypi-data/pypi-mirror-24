8a44e157b10e3b4e0a88d70cc2e91a839e252794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:39 +0000

    Add package trustme/0.3.0/trustme-0.3.0.tar.gz

---
+++ packages/trustme/trustme-0.3.0.tar.gz/trustme-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+# defines __version__
+exec(open("trustme/_version.py").read())
+
+setup(
+    name="trustme",
+    version=__version__,
+    description=
+      "#1 quality TLS certs while you wait, for the discerning tester",
+    long_description=open("README.rst").read(),
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="MIT -or- Apache License 2.0",
+    packages=find_packages(),
+    url="https://github.com/python-trio/trustme",
+    install_requires=["cryptography"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: System :: Networking",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Testing",
+    ])
