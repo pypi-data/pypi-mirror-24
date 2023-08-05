840fea9855a1b97a47faaaabbf9cb7b89c405f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:30 +0000

    Add package Mopidy-GPIOcont/0.1.19/Mopidy-GPIOcont-0.1.19.tar.gz

---
+++ packages/Mopidy-GPIOcont/Mopidy-GPIOcont-0.1.19.tar.gz/Mopidy-GPIOcont-0.1.19/mopidy_gpiocont/__init__.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+
+import logging
+import os
+
+from mopidy import config, ext
+
+
+__version__ = '0.1.19'
+
+logger = logging.getLogger(__name__)
+
+
+
+class Extension(ext.Extension):
+
+    dist_name = 'Mopidy-GPIOcont'
+    ext_name = 'gpiocont'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        logger.info("GPIOcont: Default configuration loaded.")
+        return config.read(conf_file)
+
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['play_pin'] = config.Integer()
+        logger.info("GPIOcont: User specified configuration loaded.")
+        return schema
+
+    def setup(self, registry):
+        logger.info("GPIOcont: Trying to ")
+        from .frontend import GPIOcont
+        registry.add('frontend', GPIOcont)
+        logger.info("GPIOcont: setup done.")
+
+
