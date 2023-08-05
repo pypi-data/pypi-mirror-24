1010f95a15d20d643471edbaa903abbbac45d1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:27 +0000

    Add package nester-n/1.2.0/nester-n-1.2.0.tar.gz

---
+++ packages/nester-n/nester-n-1.2.0.tar.gz/nester-n-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name="nester-n",
+        version="1.2.0",
+        py_modules=["nester_walle"],
+        author="walle",
+        author_email="walle.ok@qq.com",
+        url="http://www.baidu.com",
+        description="A simple printer of nested lists",
+    )
