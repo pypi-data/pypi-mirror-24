6ca46fff2cbc96279a2a5e9c1db242453246a947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package shellbot/17.8.5/shellbot-17.8.5-py2.py3-none-any.whl

---
+++ packages/shellbot/shellbot-17.8.5-py2.py3-none-any.whl/shellbot/commands/update.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .base import Command
+
+
+class Update(Command):
+    """
+    Update input data
+    """
+
+    keyword = u'update'
+    information_message = u'Update input content'
+
+    no_arg = u'Thanks to provide the key and the data'
+    no_input = u'There is nothing to update, input is empty'
+    ok_msg = u'Update successfuly done'
+
+    def execute(self, bot, arguments=None, **kwargs):
+        """
+        Update input data
+
+        :param bot: The bot for this execution
+        :type bot: Shellbot
+
+        :param arguments: The arguments for this command
+        :type arguments: str or ``None``
+
+        """
+        if arguments in (None, ''):
+            bot.say(self.no_arg, content=self.no_arg)
+            return
+
+        input = bot.recall('input')
+        if input in (None, {}):
+            bot.say(self.no_input)
+            return
+
+        tokens = arguments.split(' ')
+        if len(tokens) < 2:
+            bot.say(self.no_arg)
+            return
+
+        bot.update('input', tokens[0], tokens[1])
+        bot.say(self.ok_msg)
