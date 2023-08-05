e44a378c38ff7fd1236061e8b42ab98a04b3b16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:57 +0000

    Add package germanium/2.0.6/germanium-2.0.6-py2-none-any.whl

---
+++ packages/germanium/germanium-2.0.6-py2-none-any.whl/germanium/locators/StaticElementLocator.py
@@ -0,0 +1,21 @@
+import collections
+
+from .DeferredLocator import DeferredLocator
+
+
+class StaticElementLocator(DeferredLocator):
+    def __init__(self, germanium, element):
+        """ Just holds a static reference to the elements. """
+        super(StaticElementLocator, self).__init__(germanium)
+
+        if not isinstance(element, collections.Iterable):
+            self._element = [element]
+        else:
+            self._element = element
+
+    def _find_element(self):
+        """ Returns the locally stored element. """
+        return self._element[0]
+
+    def _find_element_list(self):
+        return self._element
