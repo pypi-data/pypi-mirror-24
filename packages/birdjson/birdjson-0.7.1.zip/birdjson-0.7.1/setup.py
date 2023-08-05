7eb2e944a467c7c09354462583e0664d00b8b813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:50 +0000

    Add package birdjson/0.7.1/birdjson-0.7.1.zip

---
+++ packages/birdjson/birdjson-0.7.1.zip/birdjson-0.7.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="birdjson",
+    version="0.7.1",
+    author="Anthony Casagrande",
+    author_email="birdapi@gmail.com",
+    description=("Generic json to object parser for python"),
+    license="MIT",
+    keywords="json birdapi",
+    url="https://pypi.python.org/pypi/birdjson",
+    packages=['birdjson'],
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
