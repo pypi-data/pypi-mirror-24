e38bca7af261a28265dda2bfa21b2432bad127d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package PyRealtime/0.1.1/PyRealtime-0.1.1-py3-none-any.whl

---
+++ packages/PyRealtime/PyRealtime-0.1.1-py3-none-any.whl/pyrealtime/input_layers.py
@@ -0,0 +1,19 @@
+import time
+
+from pyrealtime.layer import ProducerMixin, ThreadLayer
+
+
+class InputLayer(ProducerMixin, ThreadLayer):
+    def __init__(self, frame_generator=None, rate=30, *args, **kwargs):
+        super().__init__( *args, **kwargs)
+        self._generate = frame_generator if frame_generator is not None else self.generate
+        self.rate = rate
+
+    def generate(self, counter):
+        return counter
+
+    def get_input(self):
+        time.sleep(1.0/self.rate)
+        data = self._generate(self.counter)
+        self.tick()
+        return data
