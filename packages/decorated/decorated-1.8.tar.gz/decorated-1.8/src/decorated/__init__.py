01805e3db9bb07fdc6a71c4e1b95e554d3db3ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package decorated/1.8/decorated-1.8.tar.gz

---
+++ packages/decorated/decorated-1.8.tar.gz/decorated-1.8/src/decorated/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+from decorated.base import NOTSET
+from decorated.base.context import Context, ctx
+from decorated.base.function import ContextFunction, Function, WrapperFunction, partial
+from decorated.decorators import events
+from decorated.decorators.conditional import Conditional
+from decorated.decorators.events import Event
+from decorated.decorators.files import TempDir, TempFile, WritingFile
+from decorated.decorators.once import Once, OnceSession
+from decorated.decorators.profile import Profile
+from decorated.decorators.remove_extra_args import RemoveExtraArgs
+from decorated.decorators.retries import Retries
+from decorated.decorators.timeit import TimeIt
+from decorated.decorators.timeout import Timeout
+
+NOTSET = NOTSET
+
+Contxt = Context
+ctx = ctx
+
+Function = Function
+WrapperFunction = WrapperFunction
+ContextFunction = ContextFunction
+partial = partial
+
+conditional = Conditional
+event = events.event
+Event = Event
+once = Once
+OnceSession = OnceSession
+profile = Profile
+remove_extra_args = RemoveExtraArgs
+retries = Retries
+tempfile = TempFile
+tempdir = TempDir
+timeit = TimeIt
+timeout = Timeout
+writing_file = WritingFile
---
+++ packages/decorated/decorated-1.8.tar.gz/decorated-1.8/src/decorated/decorators/retries.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from decorated.base.function import Function
+import logging
+import time
+
+log = logging.getLogger(__name__)
+
+class Retries(Function):
+    def _init(self, times, delay=0, error_types=(Exception,)): # pylint: disable=arguments-differ
+        if times < 0:
+            raise Exception('Times cannot be negative.')
+        self._times = times
+        self._delay = delay
+        self._error_types = error_types
+        
+    def _call(self, *args, **kw):
+        times = 0
+        while True:
+            try:
+                return super(Retries, self)._call(*args, **kw)
+            except Exception as e:
+                times += 1
+                if times <= self._times and isinstance(e, self._error_types):
+                    log.warn('Execution failed. Will retry in %f seconds.', self._delay, exc_info=True)
+                    time.sleep(self._delay)
+                else:
+                    log.warn('Execution failed permanently.')
+                    raise
+        
