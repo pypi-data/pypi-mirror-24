7f76445d752c906c9c2745db68582642c938cd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:19 +0000

    Add package s3contents/0.1.4/s3contents-0.1.4.tar.gz

---
+++ packages/s3contents/s3contents-0.1.4.tar.gz/s3contents-0.1.4/s3contents/tests/test_s3manager.py
@@ -0,0 +1,39 @@
+from s3contents.ipycompat import TestContentsManager
+
+from s3contents import S3ContentsManager
+
+
+class S3ContentsManagerTestCase(TestContentsManager):
+
+    def setUp(self):
+        """
+        This setup is a hardcoded to the use a little minio server that run on travis CI or play.minio.io:9000
+        """
+        self.contents_manager = S3ContentsManager(
+            access_key_id="access-key",
+            secret_access_key="secret-key",
+            endpoint_url="http://localhost:9000",
+            bucket_name="notebooks",
+            # endpoint_url="https://play.minio.io:9000",
+            # bucket_name="s3contents-test2",
+            signature_version="s3v4")
+
+    def tearDown(self):
+        bucket = self.contents_manager.s3fs.bucket
+
+        objects_to_delete = []
+        for obj in bucket.objects.filter(Prefix=""):
+            objects_to_delete.append({"Key": obj.key})
+
+        bucket.delete_objects(Delete={"Objects": objects_to_delete})
+
+    # Overwrites from TestContentsManager
+
+    def make_dir(self, api_path):
+        self.contents_manager.new(
+            model={"type": "directory"},
+            path=api_path,)
+
+
+# This needs to be removed or else we'll run the main IPython tests as well.
+del TestContentsManager
