3cc0292df33aea31c31b7db521ea5c3e9a75ec6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:22 +0000

    Add package brap-compiler-proxy/0.0.1/brap-compiler-proxy-0.0.1.tar.gz

---
+++ packages/brap-compiler-proxy/brap-compiler-proxy-0.0.1.tar.gz/brap-compiler-proxy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='brap-compiler-proxy',
+    packages=['brap-compiler-proxy'],
+    version='0.0.1',
+    description='brap compiler: proxy',
+    author='Brian Graham',
+    author_email='drakain@gmail.com',
+    url='https://github.com/brap/brap-compiler-proxy',
+    download_url='https://github.com/incognito/brap/tarball/0.0.1',
+    keywords=['dependency', 'injection', 'container'],
+    classifiers=[],
+)
