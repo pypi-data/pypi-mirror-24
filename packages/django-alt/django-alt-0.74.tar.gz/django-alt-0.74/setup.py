78a73dc2ebfbfb932bb119cabe49336fcf26389c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:02 +0000

    Add package django-alt/0.74/django-alt-0.74.tar.gz

---
+++ packages/django-alt/django-alt-0.74.tar.gz/django-alt-0.74/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='django-alt',
+      version='0.74',
+      description='Alternative approach to data validation and REST endpoints in Django and DRF',
+      url='https://github.com/poskadesign/django-alt',
+      author='Vilius Poška',
+      author_email='vilius@poska.lt',
+      license='MIT',
+      install_requires=['django', 'djangorestframework'],
+      packages=find_packages() + ['django_alt_tests', 'django_alt_tests.conf', 'django_alt_tests.conf.migrations'])
