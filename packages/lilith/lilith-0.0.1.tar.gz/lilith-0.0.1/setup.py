3f3a449ce584b54ae08aa5ce4fba2d9132f8f853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:28 +0000

    Add package lilith/0.0.1/lilith-0.0.1.tar.gz

---
+++ packages/lilith/lilith-0.0.1.tar.gz/lilith-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+__author__ = 'derobukal'
+__time__ = '2017/8/2 23:14'
+
+setup(
+    name='lilith',
+    version='0.0.1',
+    url='https://github.com/RitterHou/Lilith',
+    author='derobukal',
+    author_email='derobukal@gmail.com',
+    description='show qrcode in terminal',
+    license='GPL3',
+    packages=['lilith'],
+    entry_points={
+        'console_scripts': [
+            'lilith=lilith.main:show'
+        ],
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'qrcode>=5.3',
+    ],
+)
