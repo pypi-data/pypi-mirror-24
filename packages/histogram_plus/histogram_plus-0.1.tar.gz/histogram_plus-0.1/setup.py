be1161db2c019bb164fe393797755853771b47bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:56 +0000

    Add package histogram_plus/0.1/histogram_plus-0.1.tar.gz

---
+++ packages/histogram_plus/histogram_plus-0.1.tar.gz/histogram_plus-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='histogram_plus',
+      version='0.1',
+      description='Overloaded histogram maker based on mpl',
+      url='http://github.com/brovercleveland/histogram_plus',
+      author='Brian Pollack',
+      author_email='brianleepollack@gmail.com',
+      license='MIT',
+      packages=['histogram_plus'],
+      install_requires=[ 'numpy', 'pandas', 'matplotlib'],
+      zip_safe=False)
