a51ecf418c48609ae146fc475d650a4f80a8b493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:12 +0000

    Add package lob/3.1.1/lob-3.1.1.tar.gz

---
+++ packages/lob/lob-3.1.1.tar.gz/lob-3.1.1/lob/__init__.py
@@ -0,0 +1,8 @@
+api_key = None
+api_base = 'https://api.lob.com/v1'
+
+#Resources
+from lob.resource import (Address, Area, BankAccount, Check, Country,
+        Letter, Postcard, Route, State, USVerification, USZipLookup, IntlVerification)
+
+from lob.version import VERSION
