c4974c79f17b60876aea5cc7d38c6880b08099dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:14 +0000

    Add package django-dynamic-content/0.3.2/django-dynamic-content-0.3.2.tar.gz

---
+++ packages/django-dynamic-content/django-dynamic-content-0.3.2.tar.gz/django-dynamic-content-0.3.2/dynamic_content/tests/test_settings.py
@@ -0,0 +1,90 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+
+
+DEBUG = True
+SITE_ID = 1
+
+APP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'dynamic_content.tests.urls'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(APP_ROOT, '../app_static')
+MEDIA_ROOT = os.path.join(APP_ROOT, '../app_media')
+STATICFILES_DIRS = (
+    os.path.join(APP_ROOT, 'static'),
+)
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'APP_DIRS': True,
+    'DIRS': [os.path.join(APP_ROOT, 'tests/test_app/templates')],
+    'OPTIONS': {
+        'context_processors': (
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.i18n',
+            'django.template.context_processors.request',
+            'django.template.context_processors.media',
+            'django.template.context_processors.static',
+        )
+    }
+}]
+
+EXTERNAL_APPS = [
+    'ckeditor',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+    'hvad',
+]
+
+INTERNAL_APPS = [
+    'dynamic_content',
+    'dynamic_content.tests.test_app',
+]
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+
+SECRET_KEY = 'foobar'
+
+# ckeditor settings
+CKEDITOR_UPLOAD_PATH = 'ckeditor_uploads/'
+CKEDITOR_IMAGE_BACKEND = 'pillow'
+CKEDITOR_JQUERY_URL = \
+    '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': [{
+            'name': 'basic',
+            'items': ['Bold', 'Italic', 'Underline', 'RemoveFormat', '-',
+                      'PasteText', 'Undo', 'Redo', 'Format', 'Source', ],
+        }],
+    },
+}
