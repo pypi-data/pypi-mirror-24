5f9d3bfce198ba59eaf4c348fc16623c501b9fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package mar/2.1.1/mar-2.1.1.tar.gz

---
+++ packages/mar/mar-2.1.1.tar.gz/mar-2.1.1/tests/test_mozilla.py
@@ -0,0 +1,15 @@
+import os
+
+from mardor.mozilla import dep1
+from mardor.mozilla import dep2
+from mardor.mozilla import nightly1
+from mardor.mozilla import nightly2
+from mardor.mozilla import release1
+from mardor.mozilla import release2
+from mardor.reader import MarReader
+
+
+def test_testmar_sig_bz2():
+    TEST_MAR = os.path.join(os.path.dirname(__file__), 'test-bz2.mar')
+    with MarReader(open(TEST_MAR, 'rb')) as m:
+        assert m.verify(release1)
