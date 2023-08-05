a3fedfead23e7809bc76cf9b30e75be64f71db4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:53 +0000

    Add package medioex/0.1.8/medioex-0.1.8.tar.gz

---
+++ packages/medioex/medioex-0.1.8.tar.gz/medioex-0.1.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+
+module = Extension('medioex',
+                   sources=['medioex.c'],
+                   libraries=['bcm2835'],
+                   extra_compile_args=['-std=gnu11', '-lbcm2835'],
+                   extra_link_args=['-std=gnu11']
+                   )
+
+setup(name='medioex',
+      version='0.1.8',
+      description='Python MedIOEx module',
+      url = 'https://github.com/beyaznet/python-medioex-module',
+      author = 'Beyaz R&D Team',
+      author_email = 'arge@beyaz.net',
+      license = 'GNU GPLv3',
+      keywords = 'raspberry pi medioex taliabee',
+      python_requires='>=3',
+      ext_modules=[module],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: C'
+      ]
+)
