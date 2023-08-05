94b35f1dd76a321b95fe8f92f84cf61a9c0e5e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package pines/1.41/pines-1.41-py2.py3-none-any.whl

---
+++ packages/pines/pines-1.41-py2.py3-none-any.whl/pines/zipdir.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import os
+import zipfile
+
+def _zipdir(path, ziph, skip_dots=True):
+	# ziph is zipfile handle
+	skips = set()
+	for root, dirs, files in os.walk(path):
+		folder = os.path.basename(root)
+		for s in skips:
+			if s in root:
+				continue
+		if (len(folder) and folder[0] != '.') or not skip_dots:
+			print('zipping folder:', folder, "in", root)
+			for file in files:
+				if file[0]!='.' or not skip_dots:
+					ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, '..')))
+		else:
+			print('not zipping folder:', folder, "in", root)
+			skips.add(os.path.join(root,folder))
+
+def zipdir(source_dir, zip_file_name, skip_dots=True):
+	with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+		_zipdir(source_dir, zipf, skip_dots=skip_dots)
+
+def zipmod(module, zip_file_name, skip_dots=True):
+	"""
+	Create a zipfile from a module
+
+	Parameters
+	----------
+	module
+	zip_file_name
+	skip_dots
+
+	Returns
+	-------
+
+	"""
+	with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+		_zipdir(module.__path__[0], zipf, skip_dots=skip_dots)
+
+
+def zipmod_temp(module, skip_dots=True):
+	import tempfile
+	tempdir = tempfile.TemporaryDirectory()
+	zip_file_name = os.path.join(tempdir.name, module.__name__+".zip")
+	zipmod(module, zip_file_name, skip_dots=skip_dots)
+	return zip_file_name, tempdir
