1d6d61cb6494164c1354962de2b2a72679e7aba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package granadilla/0.6.2/granadilla-0.6.2.tar.gz

---
+++ packages/granadilla/granadilla-0.6.2.tar.gz/granadilla-0.6.2/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+from granadilla.version import VERSION
+
+
+def read(filename):
+    with open(filename) as fp:
+        return fp.read()
+
+
+setup(
+    name="granadilla",
+    version=VERSION,
+    author="Polyconseil Sysadmin Team",
+    author_email="sysadmin+granadilla@polyconseil.fr",
+    description="An opinionated LDAP frontend.",
+    license="GPL",
+    keywords=['granadilla', 'ldap', 'directory', 'admin'],
+    url="https://github.com/Polyconseil/granadilla",
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=[
+        # Django core
+        'Django>=1.11,<2.0',
+
+        # Databases
+        'django-ldapdb>=0.6.0',
+
+        # Configuration
+        'django-appconf',
+        'getconf>=1.2.0',
+        'django-zxcvbn-password>=2.0.0',
+        'django-password-strength>=1.2.1',
+
+
+        # Passwords
+        'zxcvbn-python',
+
+        # Command line
+        'colorama',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Environment :: Console",
+        "Framework :: Django :: 1.11",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+    ],
+    include_package_data=True,
+)
