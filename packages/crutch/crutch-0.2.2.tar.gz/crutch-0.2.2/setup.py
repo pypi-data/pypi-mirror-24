bde5578909f9a82a7dcdb38db91ef33a5c791498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:44 +0000

    Add package crutch/0.2.2/crutch-0.2.2.tar.gz

---
+++ packages/crutch/crutch-0.2.2.tar.gz/crutch-0.2.2/setup.py
@@ -0,0 +1,39 @@
+"""Setup for crutch."""
+
+import io
+
+from setuptools import setup
+
+
+def requirements():
+  with io.open('requirements.txt') as out:
+    return out.read()
+
+
+def version():
+  with io.open('VERSION') as out:
+    return out.read()
+
+
+def readme():
+  with io.open('README.rst') as out:
+    return out.read()
+
+
+setup(
+    name='crutch',
+    version=version(),
+    description='Gets a project running quickly',
+    long_description=readme(),
+    license='MIT',
+    author='Artyom Goncharov',
+    author_email='m4yers@gmail.com',
+    url='https://github.com/m4yers/crutch',
+    keywords='projects, bootstrap',
+    install_requires=requirements(),
+    python_requires='>=2.6',
+    package_data={
+        'templates': ['*.*'],
+    },
+    entry_points={'console_scripts': ['crutch = crutch:main']},
+)
