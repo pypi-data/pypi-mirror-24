6a1bcd888545314d688204dbf7b1e63b40cf81fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package assetbuilder/0.4.4/assetbuilder-0.4.4.tar.gz

---
+++ packages/assetbuilder/assetbuilder-0.4.4.tar.gz/assetbuilder-0.4.4/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='assetbuilder',
+      version='0.4.4',
+      description='',
+      py_modules=['assetbuilder'],
+      packages=[],
+      install_requires=['pathlib', 'portalocker'])
