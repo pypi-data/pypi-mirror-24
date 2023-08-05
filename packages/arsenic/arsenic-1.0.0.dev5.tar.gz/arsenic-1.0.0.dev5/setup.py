30de1a256f64de3bffbf2a1ca8184bd0b446d264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package arsenic/1.0.0.dev5/arsenic-1.0.0.dev5.tar.gz

---
+++ packages/arsenic/arsenic-1.0.0.dev5.tar.gz/arsenic-1.0.0.dev5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    version='1.0.0.dev5',
+    name='arsenic',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'attrs>=17.1.0',
+        'structlog',
+        'aiohttp>=2',
+    ],
+    license='APLv2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
