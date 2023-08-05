80a742a8a1c109c7fbf7e543ed8acd864c3cc101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package wtss/0.4.0/wtss-0.4.0.tar.gz

---
+++ packages/wtss/wtss-0.4.0.tar.gz/wtss-0.4.0/setup.py
@@ -0,0 +1,32 @@
+#
+#   Copyright (C) 2014 National Institute For Space Research (INPE) - Brazil.
+#
+#  This file is part of Python Client API for Web Time Series Service.
+#
+#  Web Time Series Service for Python is free software: you can
+#  redistribute it and/or modify it under the terms of the
+#  GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License,
+#  or (at your option) any later version.
+#
+#  Web Time Series Service for Python is distributed in the hope that
+#  it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Web Time Series Service for Python. See LICENSE. If not, write to
+#  e-sensing team at <esensing-team@dpi.inpe.br>.
+#
+
+from setuptools import setup
+
+setup(name='wtss',
+      version='0.4.0',
+      description='Python Client API for Web Time Series Service',
+      url='https://github.com/e-sensing/wtss.py',
+      author='Gilberto Ribeiro de Queiroz',
+      author_email='gribeiro@dpi.inpe.br',
+      license='LGPL3',
+      packages=['wtss'],
+      zip_safe=False)
