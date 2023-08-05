30c7c2673c466507e08b04ac79125b464a7eb0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:33 +0000

    Add package sockspy/0.2.4/sockspy-0.2.4.tar.gz

---
+++ packages/sockspy/sockspy-0.2.4.tar.gz/sockspy-0.2.4/sockspy/core/pool.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import selectors
+import logging
+import click
+
+
+class EndpointPool(object):
+
+    def __init__(self, selector_type=selectors.DefaultSelector):
+        self.listener = None
+        self.poller = selector_type()
+        self.logger = logging.getLogger(__name__)
+        self.endpoint_set = set()
+
+    def set_listener(self, listener):
+        self.listener = listener
+        self.poller.register(listener, selectors.EVENT_READ)
+
+    def register(self, endpoint, events):
+        self.poller.register(endpoint, events)
+        self.endpoint_set.add(endpoint)
+
+    def modify(self, endpoint, events):
+        self.poller.modify(endpoint, events)
+
+    def unregister(self, endpoint):
+        self.poller.unregister(endpoint)
+        self.endpoint_set.remove(endpoint)
+
+    def poll(self, engine, timeout):
+        click.echo("sockspy started!")
+        while True:
+            for (key, event) in self.poller.select(timeout):
+                endpoint = key.fileobj
+                if endpoint == self.listener:
+                    sock, address = self.listener.accept()
+                    engine.accept(sock)
+                else:
+                    endpoint.event = event
+                    self.logger.debug("[poll]   fd: %s, event: %s", endpoint.fileno(),
+                                      "event_read" if event & selectors.EVENT_READ else "event_write")
+                    engine.process_event(endpoint)
+            engine.process_loop()
+
+    def set_events(self, endpoint):
+        self.modify(endpoint, selectors.EVENT_WRITE | selectors.EVENT_READ if len(endpoint.peer.stream) > 0 else selectors.EVENT_READ)
+
+    def remove_listener(self):
+        self.poller.unregister(self.listener)
+        self.listener.close()
