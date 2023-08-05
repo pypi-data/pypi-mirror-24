6125c4e283e7a71d33373e70b64353264f7ecfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package mml_gmm/0.11/mml_gmm-0.11.tar.gz

---
+++ packages/mml_gmm/mml_gmm-0.11.tar.gz/mml_gmm-0.11/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mml_gmm',
+      version='0.11',
+      description='Unsupervised cluster selection',
+      url='https://github.com/abriosi/mml_gmm/',
+      author='abriosi',
+      license='MIT',
+      packages=['mml_gmm'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'sklearn',
+      ],
+      zip_safe=False)
