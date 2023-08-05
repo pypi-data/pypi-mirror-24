cb52bb9ea97e2ff145c4a24ef98b34425de32e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package django-croppie/1.0/django-croppie-1.0.tar.gz

---
+++ packages/django-croppie/django-croppie-1.0.tar.gz/django-croppie-1.0/croppie/static/croppie/form-process.js
@@ -0,0 +1,29 @@
+var element = document.getElementById('cropper');
+var cropper = new Croppie(element, croppieOptions);
+
+function readFile(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            cropper.bind({
+                url: e.target.result
+            });
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+var photoInput = document.getElementById('id_' + croppieFieldName + '_0');
+photoInput.addEventListener('change', function() {
+    readFile(this);
+});
+
+element.addEventListener('update', function(cr) {
+    var data = cropper.get();
+    var baseSelector = 'id_' + croppieFieldName + '_';
+    var pointInput = null;
+    for (var i = 1; i <= 4; i++) {
+        pointInput = document.getElementById(baseSelector + i)
+        pointInput.value = data.points[i-1];
+    }
+});
