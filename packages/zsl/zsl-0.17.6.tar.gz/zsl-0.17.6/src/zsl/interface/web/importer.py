faea1e58d2c9c953ff9c3052a50f7f1e345ed022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:56 +0000

    Add package zsl/0.17.6/zsl-0.17.6.tar.gz

---
+++ packages/zsl/zsl-0.17.6.tar.gz/zsl-0.17.6/src/zsl/interface/web/importer.py
@@ -0,0 +1,10 @@
+"""
+:mod:`zsl.interface.webservice.importer`
+----------------------------------------
+"""
+from __future__ import unicode_literals
+import sys
+import os
+
+# TODO: Consider removing automatic path initialization!
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
