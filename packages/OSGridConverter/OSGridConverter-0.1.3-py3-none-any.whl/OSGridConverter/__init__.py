7003f2e734ce9cfeb53af35cc0766a162cce66a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:10 +0000

    Add package OSGridConverter/0.1.3/OSGridConverter-0.1.3-py3-none-any.whl

---
+++ packages/OSGridConverter/OSGridConverter-0.1.3-py3-none-any.whl/OSGridConverter/__init__.py
@@ -0,0 +1,24 @@
+from .grid import OSGridReference
+from .latlong import LatLong
+from .mapping import Tags
+
+def latlong2grid(latitude,longitude,tag='WGS84'):
+    ll=LatLong(latitude,longitude,tag=tag)
+    return OSGridReference(ll)
+    
+def grid2latlong(grid,tag='WGS84'):
+    gr=OSGridReference(grid)
+    return gr.toLatLong(tag=tag)
+
+def parse(arg):
+    try:
+        return OSGridReference(arg)
+    except:
+        pass
+    try:
+        return LatLong(arg)
+    except:
+        pass
+    return arg
+        
+
