9678bb70e6f3c904c84a272fa6a930c0b2b83c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:52 +0000

    Add package kolors/0.0.2/kolors-0.0.2.tar.gz

---
+++ packages/kolors/kolors-0.0.2.tar.gz/kolors-0.0.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='kolors',
+    version='0.0.2',
+    description='Simple and lightweight shell color output function.',
+    long_description=(read('README.md')),
+    author='Maxime Haineault',
+    author_email='haineault@gmail.com',
+    license='MIT',
+    url='https://github.com/h3/python-kolors',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'kolors': [
+        'README.rst',
+        'CHANGELOG',
+        ]},
+    zip_safe=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
