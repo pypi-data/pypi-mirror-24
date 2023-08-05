cfa6e732c8abbedb4df2d0794a56dd5982eb2b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package colorharmonies/1.0.3/colorharmonies-1.0.3.tar.gz

---
+++ packages/colorharmonies/colorharmonies-1.0.3.tar.gz/colorharmonies-1.0.3/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='colorharmonies',
+      version='1.0.3',
+      description='Library to generate all your color harmonies with simplicity!',
+      url='https://github.com/baptistemanteau/colorharmonies',
+      author='Baptiste Manteau',
+      author_email='manteau.baptiste@gmail.com',
+      license='MIT',
+      packages=['colorharmonies'],
+      python_requires='>=3',
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
