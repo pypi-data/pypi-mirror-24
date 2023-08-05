a622dd5d9f79ea868fe6d5b104205ab2a5cedd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package distill.py/0.0.2/distill.py-0.0.2.tar.gz

---
+++ packages/distill.py/distill.py-0.0.2.tar.gz/distill.py-0.0.2/distill/cli.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for distill"""
+    click.echo("Welcome to distill.py!")
+    click.echo("Happy coding and writing!")
+
+
+if __name__ == "__main__":
+    main()
