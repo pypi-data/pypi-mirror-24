9d57a20bc553301e7972559403000a21fabe28d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:29 +0000

    Add package deployfish/0.15.0/deployfish-0.15.0.tar.gz

---
+++ packages/deployfish/deployfish-0.15.0.tar.gz/deployfish-0.15.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from deployfish import __version__
+from setuptools import setup, find_packages  # @UnresolvedImport
+
+intro = open('docs/source/intro.rst').read()
+
+setup(name="deployfish",
+      version=__version__,
+      description="ECS related deployment tools",
+      author="IMSS ADS",
+      author_email="imss-ads-staff@caltech.edu",
+      url="https://github.com/caltechads/deployfish",
+      long_description=intro,
+      keywords=['aws', 'ecs', 'docker', 'devops'],
+      classifiers = [
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3"
+      ],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          "boto3 >= 1.4.4",
+          "click >= 6.7",
+          "PyYAML == 3.12",
+          "tzlocal == 1.4"
+      ],
+      entry_points={'console_scripts': [
+          'deploy = deployfish.dplycli:main',
+          'dpy = deployfish.dplycli:main'
+      ]}
+      )
