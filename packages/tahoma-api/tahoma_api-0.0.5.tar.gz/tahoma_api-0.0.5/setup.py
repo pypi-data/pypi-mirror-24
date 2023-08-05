45736b88a61398b39b8df84e4353e528a9251675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package tahoma-api/0.0.5/tahoma_api-0.0.5.tar.gz

---
+++ packages/tahoma-api/tahoma_api-0.0.5.tar.gz/tahoma_api-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import os
+from os.path import exists
+from setuptools import setup, find_packages
+from tahoma_api import __version__
+
+setup(
+    name='tahoma_api',
+    version=__version__,
+    url='http://github.com/philklei/tahoma-api/',
+    license='Apache Software License',
+    author='Philip Kleimeyer',
+    install_requires=['requests'],
+    author_email='philip.kleimeyer@gmail.com',
+    description='Tahoma Api - Python connect to Tahoma REST API',
+    long_description=open('README.rst').read() if exists("README.rst") else "",
+    py_modules=['tahoma_api'],
+    include_package_data=True,
+    keywords='tahoma somfy io covers senors api',
+    platforms='any'
+)
