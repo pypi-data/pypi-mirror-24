2cb04a6dab517e06a7ee869943a98bb110384ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:23 +0000

    Add package tweet-parser/1.0.3/tweet_parser-1.0.3.tar.gz

---
+++ packages/tweet-parser/tweet_parser-1.0.3.tar.gz/tweet_parser-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='tweet_parser',
+      packages=find_packages(),
+      scripts=["tools/parse_tweets.py"],
+      version='1.0.3',
+      license='MIT',
+      author='Fiona Pigott',
+      author_email='fpigott@twitter.com',
+      description="Tools for Tweet parsing",
+      url='https://github.com/fionapigott/tweet_parser',
+      install_requires=[],
+      python_requires='>=3',
+      package_data={
+          'tweet_parser': ['test/tweet_payload_examples/activity_streams_examples.json',
+                           'test/tweet_payload_examples/original_format_examples.json',
+                           'test/tweet_payload_examples/broken_and_unsupported_payloads/*.json'],
+          },
+      )
