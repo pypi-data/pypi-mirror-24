59e18235dda18235fb4d720befc9cb41c6ec1f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package chemhelper/0.1/chemhelper-0.1.tar.gz

---
+++ packages/chemhelper/chemhelper-0.1.tar.gz/chemhelper-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='chemhelper',
+    version='0.1',
+    packages=['chemsolver'],
+    url='https://github.com/DRosenman/chemsolver',
+    license='MIT License',
+    author='rosen',
+    author_email='rosenmd1@tcnj.edu',
+    description='Package for solving chemistry problems and also showing step by step solutions.'
+)
