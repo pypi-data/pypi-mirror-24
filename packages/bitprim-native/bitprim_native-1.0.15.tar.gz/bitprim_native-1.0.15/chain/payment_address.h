efd27e5adeaff8b4ab8bb34f6ea6dddbe066f239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package bitprim-native/1.0.15/bitprim_native-1.0.15.tar.gz

---
+++ packages/bitprim-native/bitprim_native-1.0.15.tar.gz/bitprim_native-1.0.15/chain/payment_address.h
@@ -0,0 +1,16 @@
+#ifndef BITPRIM_PY_CHAIN_PAYMENT_ADDRESS_H_
+#define BITPRIM_PY_CHAIN_PAYMENT_ADDRESS_H_
+
+#include <Python.h>
+#include <bitprim/nodecint.h>
+#include "../utils.h"
+
+
+PyObject* bitprim_native_chain_payment_address_destruct(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_payment_address_encoded(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_payment_address_version(PyObject* self, PyObject* args);
+PyObject* bitprim_native_chain_payment_address_construct_from_string(PyObject* self, PyObject* args);
+
+
+
+#endif
