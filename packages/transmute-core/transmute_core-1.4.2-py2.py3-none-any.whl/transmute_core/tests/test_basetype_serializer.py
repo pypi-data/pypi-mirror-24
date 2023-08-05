8fe8a9467a9b3c5431b3996121c5e13c13f931b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package transmute-core/1.4.2/transmute_core-1.4.2-py2.py3-none-any.whl

---
+++ packages/transmute-core/transmute_core-1.4.2-py2.py3-none-any.whl/transmute_core/tests/test_basetype_serializer.py
@@ -0,0 +1,70 @@
+import pytest
+from transmute_core.compat import string_type
+from transmute_core.exceptions import SerializationException
+from datetime import datetime
+
+NOW = datetime.now()
+
+
+@pytest.mark.parametrize("inp, expected_output", [
+    ("10", 10), ("-1", -1)
+])
+def test_int_load_happy(serializer, inp, expected_output):
+    """ test all happy cases for the integer serializer """
+    assert serializer.load(int, inp) == expected_output
+
+
+@pytest.mark.parametrize("unhappy_input", [
+    "foo", "bar"
+])
+def test_int_load_unhappy(serializer, unhappy_input):
+    """ test all unhappy cases for the integer serializer """
+    with pytest.raises(SerializationException):
+        serializer.load(int, unhappy_input)
+
+
+@pytest.mark.parametrize("inp, expected_output", [
+    ("10", 10), ("1.0", 1.0)
+])
+def test_float_load_happy(serializer, inp, expected_output):
+    """ test all happy cases for the integer serializer """
+    assert serializer.load(float, inp) == expected_output
+
+
+@pytest.mark.parametrize("unhappy_input", [
+    "foo", "bar"
+])
+def test_float_load_unhappy(serializer, unhappy_input):
+    """ test all unhappy cases for the integer serializer """
+    with pytest.raises(SerializationException):
+        serializer.load(float, unhappy_input)
+
+
+@pytest.mark.parametrize("inp, expected_output", [
+    ("true", True), ("false", False),
+    (True, True), (False, False),
+])
+def test_bool_load_happy(serializer, inp, expected_output):
+    assert serializer.load(bool, inp) is expected_output
+
+
+@pytest.mark.parametrize("inp, expected_output", [
+    ("foo", "foo")
+])
+def test_string_load_happy(serializer, inp, expected_output):
+    assert serializer.load(string_type, inp) == expected_output
+
+
+@pytest.mark.parametrize("inp, expected_output", [
+    (NOW.isoformat(), NOW)
+])
+def test_datetime_load_happy(serializer, inp, expected_output):
+    assert serializer.load(datetime, inp) == expected_output
+
+
+@pytest.mark.parametrize("inp", [
+    ("")
+])
+def test_datetime_load_unhappy(serializer, inp):
+    with pytest.raises(SerializationException):
+        serializer.load(datetime, inp)
---
+++ packages/transmute-core/transmute_core-1.4.2-py2.py3-none-any.whl/transmute_core/tests/test_benchmark.py
@@ -0,0 +1,120 @@
+import cProfile
+import json
+import pytest
+import sys
+from transmute_core import (
+    TransmuteFunction, describe, annotate,
+    ParamExtractor, NoArgument, process_result
+)
+from schematics.models import Model
+from schematics.types import StringType, BooleanType, IntType
+from schematics.types.compound import ModelType
+
+
+class User(Model):
+    name = StringType()
+    age = IntType()
+
+
+class ComplexModel(Model):
+    user = ModelType(User)
+    description = StringType()
+    is_allowed = BooleanType()
+
+
+@describe(paths="/foo", body_parameters="body")
+@annotate({"body": ComplexModel, "return": ComplexModel})
+def complex_body_method(body):
+    return body
+
+
+@describe(paths="/foo", body_parameters="body")
+@annotate({"body": int, "return": int})
+def simple_body_method(body):
+    return body
+
+
+@describe(paths="/foo", body_parameters="body")
+@annotate({"body": str, "return": str})
+def body_string(body):
+    return body
+
+
+def execute(context, func, obj_as_json):
+    extractor = ParamExtractorMock(obj_as_json)
+    args, kwargs = extractor.extract_params(
+        context, func, "application/json"
+    )
+    exc, result = None, None
+    try:
+        result = func(*args, **kwargs)
+    except Exception as e:
+        exc = e
+        exc.__traceback__ = sys.exc_info[:2]
+    process_result(func, context, result, exc, "application/json")
+
+
+def test_large_str_benchmark(benchmark, context):
+    """
+    a benchmark of a fake full execution flow of a transmute function.
+    """
+    s = "a" * 100000
+
+    func = TransmuteFunction(body_string)
+    obj_json = json.dumps(s)
+
+    benchmark(lambda: execute(context, func, obj_json))
+
+
+def test_complex_benchmark(benchmark, context):
+    """
+    a benchmark of a fake full execution flow of a transmute function.
+    """
+    obj = ComplexModel({
+        "user": {
+            "name": "Richard Stallman",
+            "age": 104
+        },
+        "description": "this is a test",
+        "is_allowed": True
+    })
+
+    complex_func = TransmuteFunction(complex_body_method)
+    complex_json = json.dumps(obj.to_primitive())
+
+    benchmark(lambda: execute(context, complex_func, complex_json))
+
+
+def test_simple_benchmark(benchmark, context):
+
+    simple_func = TransmuteFunction(simple_body_method)
+    simple_json = json.dumps(1)
+
+    benchmark(lambda: execute(context, simple_func, simple_json))
+
+    # def profile():
+    #     for i in range(10000):
+    #         execute()
+    # cProfile.runctx('profile()', globals(), locals())
+
+
+class ParamExtractorMock(ParamExtractor):
+
+    def __init__(self, body):
+        self._body = body
+
+    @property
+    def body(self):
+        return self._body
+
+    def _get_framework_args(self):
+        return {}
+
+    def _query_argument(self, key, is_list):
+        return NoArgument
+
+    def _header_argument(self, key):
+        return NoArgument
+
+    def _path_argument(self, key):
+        return NoArgument
