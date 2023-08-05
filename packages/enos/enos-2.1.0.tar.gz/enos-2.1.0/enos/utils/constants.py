5134dddae8892e89a1878d5217fca1aac7bc26ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package enos/2.1.0/enos-2.1.0.tar.gz

---
+++ packages/enos/enos-2.1.0.tar.gz/enos-2.1.0/enos/utils/constants.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -
+
+# Note: Never import dependencies to the rest of enos here: this will
+# put a mess during the packaging. `setup.py` imports this file to get
+# enos version. So importing the rest of enos here will evaluate the
+# rest of enos and obviously enos needs dependencies that won't be
+# installed yet.
+
+import os
+
+# PATH constants
+ENOS_PATH = os.path.abspath(
+  os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+SYMLINK_NAME = os.path.abspath(os.path.join(os.getcwd(), 'current'))
+TEMPLATE_DIR = os.path.join(ENOS_PATH, 'templates')
+ANSIBLE_DIR = os.path.join(ENOS_PATH, 'ansible')
+
+# NIC constants
+NETWORK_IFACE = 0
+EXTERNAL_IFACE = 1
+
+# ENOS Setup
+VERSION = '2.1.0'
