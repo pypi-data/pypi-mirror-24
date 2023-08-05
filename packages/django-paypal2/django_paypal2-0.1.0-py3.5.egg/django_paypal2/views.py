6681841e40a11b3b08eb956e2e952eead0f7c691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:53 +0000

    Add package django-paypal2/0.1.0/django_paypal2-0.1.0-py3.5.egg

---
+++ packages/django-paypal2/django_paypal2-0.1.0-py3.5.egg/django_paypal2/views.py
@@ -0,0 +1,19 @@
+import random
+
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+from paypal.models import PaypalTransaction
+
+@csrf_exempt
+def sample_pay_view(request):
+    if request.method == 'GET':
+        return render(request, 'sample_pay.html')
+    elif request.method =='POST':
+        tx = PaypalTransaction(
+            description="sample pay",
+        )
+        tx.add_item("sample", 'USD', float(request.POST['amount']))
+        tx.reference_id = str(random.randint(10000,99999))
+        tx.save()
+        return redirect('paypal_redirect', pk=tx.pk)
