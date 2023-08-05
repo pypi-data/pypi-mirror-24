3fa61918dcdb20754ba6a2be166261deab141992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package smartpath/0.11/smartpath-0.11.tar.gz

---
+++ packages/smartpath/smartpath-0.11.tar.gz/smartpath-0.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='smartpath',
+	version='0.11',
+	description='smartpath implements the SmartFileSystem class (and connector classes) to provide unified object-oriented access to filesystems of various protocols.  It draws a number of parallels to the Windows FileSystemObject',
+	url='http://github.com/miking7/smartpath',
+	author='Michael Engelbrecht',
+	author_email='michaelndani@gmail.com',
+	license='MIT',
+	packages=['smartpath'],
+	package_data={'smartpath': ['php/*']},
+	zip_safe=False)
