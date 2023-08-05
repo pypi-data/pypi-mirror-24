7a88c9e5cca56db3c7af264e05b961d45c0a12e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package simplepip/0.2/simplepip-0.2.tar.gz

---
+++ packages/simplepip/simplepip-0.2.tar.gz/simplepip-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'simplepip',
+  packages = ['simplepip'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A random test lib',
+  author = 'Ishpreet Singh',
+  author_email = 'ishpreetsingh2808@gmail.com',
+  url = 'https://github.com/ishpreet-singh/simplepip', # use the URL to the github repo
+  download_url = 'https://github.com/ishpreet-singh/simplepip/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
---
+++ packages/simplepip/simplepip-0.2.tar.gz/simplepip-0.2/simplepip/hello.py
@@ -0,0 +1,3 @@
+
+def abba():
+	print('Hello World!');
