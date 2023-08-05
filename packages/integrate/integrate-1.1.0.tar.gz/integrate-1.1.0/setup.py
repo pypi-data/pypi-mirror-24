d170006b7280f2a61166282b0e19e16069eb4845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package integrate/1.1.0/integrate-1.1.0.tar.gz

---
+++ packages/integrate/integrate-1.1.0.tar.gz/integrate-1.1.0/setup.py
@@ -0,0 +1,33 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+from setuptools import setup, find_packages
+
+setup(
+	name='integrate',
+	version='1.1.0',
+	description='Test framework for integration tests with dependencies',
+	url='https://github.com/anfema/integrate',
+	author='Johannes Schriewer',
+	author_email='hallo@dunkelstern.de',
+	license='BSD',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+    	'Topic :: Software Development :: Testing',
+    	'License :: OSI Approved :: BSD License',
+    	'Programming Language :: Python :: 2.7',
+    	'Programming Language :: Python :: 3.3',
+    	'Programming Language :: Python :: 3.4',
+    	'Programming Language :: Python :: 3.5'
+	],
+	keywords='integration test tests',
+	packages=find_packages(exclude=['docs', 'tests*']),
+	install_requires=[
+	],
+	dependency_links=[
+	]
+)
