a7259871a60109a20b852246f6bf52b68b14b7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:42 +0000

    Add package brainart/1.0.8/brainart-1.0.8.tar.gz

---
+++ packages/brainart/brainart-1.0.8.tar.gz/brainart-1.0.8/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    # Application name:
+    name="brainart",
+
+    # Version number (initial):
+    version="1.0.8",
+
+    # Application author details:
+    author="vsoch",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    # Data files
+    package_data = {'brainart.templates':['*.html'],
+                    'brainart.data':['*.pkl']},
+
+    include_package_data=True,
+    zip_safe=False,
+
+    # Details
+    url="http://www.github.com/vsoch/brainart",
+
+    license="LICENSE",
+    description="make images out of brain imaging data",
+    keywords='brain art neuroimaging',
+
+    install_requires = ['numpy','pandas','pyneurovault','nilearn','matplotlib','Pillow'],
+
+    entry_points = {
+        'console_scripts': [
+            'brainart=brainart.scripts:main',
+        ],
+    },
+
+)
