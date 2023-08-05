8d7689275b531febf9c7bd1db352652e7bd5e039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package bsym/1.0b1/bsym-1.0b1.tar.gz

---
+++ packages/bsym/bsym-1.0b1.tar.gz/bsym-1.0b1/bsym/__init__.py
@@ -0,0 +1,10 @@
+from .symmetry_operation import SymmetryOperation
+from .symmetry_group import SymmetryGroup
+from .space_group import SpaceGroup
+from .point_group import PointGroup
+from .configuration import Configuration
+from .configuration_space import ConfigurationSpace
+from .coordinate_config_space import CoordinateConfigSpace
+
+__version__ = '1.0.b1'
+
