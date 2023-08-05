d2f437a0db4b72491964b21c1c8dfe040ef2ed7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:23 +0000

    Add package qn2-sig/0.1.1/qn2-sig-0.1.1.tar.gz

---
+++ packages/qn2-sig/qn2-sig-0.1.1.tar.gz/qn2-sig-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='qn2-sig',
+      version='0.1.1',
+      description='signal processing tools',
+      author='Costas Smaragdakis',
+      url='http://www.tem.uoc.gr/~kesmarag',
+      author_email='kesmarag@gmail.com',
+      packages=['qn2.sig'],
+      install_requires=['numpy',
+                        'PyWavelets'],
+      package_dir={'qn2.sig': './'}, )
