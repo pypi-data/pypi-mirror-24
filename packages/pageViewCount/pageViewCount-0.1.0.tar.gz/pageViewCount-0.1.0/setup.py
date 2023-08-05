50bf9b09cfde51c612c186fbce94a892026198bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:14 +0000

    Add package pageViewCount/0.1.0/pageViewCount-0.1.0.tar.gz

---
+++ packages/pageViewCount/pageViewCount-0.1.0.tar.gz/pageViewCount-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pageViewCount',
+    version='0.1.0',
+    url='https://github.com/Fiz1994/pageViewCount',
+    author='FizLin',
+    author_email='fizlbq@gamil.com',
+    description='Statistics page number of views ',
+    install_requires=[
+        "Django >= 1.7",
+    ],
+    packages=['pageViewCount'],
+
+)
