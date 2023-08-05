9709408f61d49869cb6c8f218f9415e636bc89f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package epistasis/0.2.0/epistasis-0.2.0.tar.gz

---
+++ packages/epistasis/epistasis-0.2.0.tar.gz/epistasis-0.2.0/epistasis/models/__init__.py
@@ -0,0 +1,5 @@
+from .linear import EpistasisLinearRegression
+from .power import EpistasisPowerTransform
+from .nonlinear import EpistasisNonlinearRegression
+from .classifiers import EpistasisLogisticRegression
+from .mixed import EpistasisMixedRegression
