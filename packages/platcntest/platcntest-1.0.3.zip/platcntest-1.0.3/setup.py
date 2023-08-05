2324dd4b27a643ea6fed3f0083abea8262f91f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:59 +0000

    Add package platcntest/1.0.3/platcntest-1.0.3.zip

---
+++ packages/platcntest/platcntest-1.0.3.zip/platcntest-1.0.3/setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+setup(
+	name 		= 'platcntest',
+	version		= '1.0.3',
+	py_modules	= ['platcntest'],
+	author		= 'bill',
+	author_email= 'cosysiny@gmail.com',
+	url			= 'http://liange.applinzi.com',
+	description	= 'A simple printer of nested lists',
+	)
