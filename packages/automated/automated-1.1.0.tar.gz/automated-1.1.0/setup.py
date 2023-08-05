099b821811c5e6382077aa012ae735599825716d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:54 +0000

    Add package automated/1.1.0/automated-1.1.0.tar.gz

---
+++ packages/automated/automated-1.1.0.tar.gz/automated-1.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# Gabriel Fernandes <gabrielfernndss@gmail.com>
+
+from setuptools import setup
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+   name='automated',
+   version='1.1.0',
+   description='A automated module',
+   long_description=long_description,
+   license='MIT',
+   classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+   url='https://github.com/gabrielfern/automated-leda-tasks',
+   author='Gabriel Fernandes',
+   author_email='gabrielfernndss@gmail.com',
+   packages=['automated'],
+   install_requires=['requests'],
+)
