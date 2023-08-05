cbfca6313bad76c69bc0fbccb43b5e1e3bce9252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package hudai/2.2.2/hudai-2.2.2.tar.gz

---
+++ packages/hudai/hudai-2.2.2.tar.gz/hudai-2.2.2/hudai/resources/user_company.py
@@ -0,0 +1,19 @@
+from ..resource import Resource
+
+
+class UserCompanyResource(Resource):
+    def __init__(self, client):
+        Resource.__init__(self, client, base_path='/users-companies')
+        self.resource_name = 'UserCompany'
+
+    def list(self, user_id=None, company_id=None):
+        return self._list(user_id=user_id, company_id=company_id)
+
+    def create(self, user_id=None, company_id=None):
+        return self._create(user_id=user_id, company_id=company_id)
+
+    def fetch(self, entity_id):
+        return self._fetch(entity_id)
+
+    def delete(self, entity_id):
+        return self._delete(entity_id)
