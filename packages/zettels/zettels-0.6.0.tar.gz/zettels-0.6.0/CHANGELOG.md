abe9a86d97cabe0d7ade7a9d002c2f2aac1f9da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:30 +0000

    Add package zettels/0.6.0/zettels-0.6.0.tar.gz

---
+++ packages/zettels/zettels-0.6.0.tar.gz/zettels-0.6.0/CHANGELOG.md
@@ -0,0 +1,33 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [0.6.0]
+### Added
+### Changed
+- Adjusted ignore settings in default settings file to ignore hidden 
+  directories, too.
+- Properly handling pandoc's specs for YAML metadata, now. Metadata block now 
+  may also end with '---'.
+- Reimplemented function to find updated files. Should be much faster now, 
+  when dealing with a large number of Zettels. Depending on UNIX tool `find`
+  now, tested against GNU find.
+### Deprecated
+### Removed
+### Fixed
+- With every run of zettels, the index was built from scratch. Now it only 
+  updates it if files in the Zettelkasten directory have changed.
+### Security
+
+## [0.5.0] - 2017-06-09
+### Added
+- This change log
+### Changed
+- dropped the `query` (or `q`) subcommand. So what used to be `zettels q` is 
+  now just `zettels`.
+- renamed the `-t` (or `--targets`) flag to  `-l` (or `--links`)
+- First column of the default `pretty` output format is now 40 spaces wide
+### Fixed
+- Issue with external links inside the markdown files (#9)
---
+++ packages/zettels/zettels-0.6.0.tar.gz/zettels-0.6.0/setup.py
@@ -0,0 +1,105 @@
+"""A setuptools based setup module.
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+version = '0.6.0'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='zettels',
+    version=version,
+    description='A command line tool implementing Luhmann\'s system of a "Zettelkasten".',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/sthesing/Zettels',
+
+    # Author details
+    author='Stefan Thesing',
+    author_email='software@webdings.de',
+
+    # Choose your license
+    license='GPLv3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Other/Nonlisted Topic',
+        
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='notetaking zettelkasten',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages = ['zettels'],
+    
+    include_package_data=True,
+    
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['PyYAML', 'pathspec>=0.5.0'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest', 'pypandoc'],
+        'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'zettels': ['resources/*'],
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'zettels = zettels.zettels:main',
+        ],
+    },
+)
