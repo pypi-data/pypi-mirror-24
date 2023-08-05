68d70bbd1da35511c754be9d1aac8bb4c8767a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:48 +0000

    Add package django-sp/0.3b31/django-sp-0.3b31.tar.gz

---
+++ packages/django-sp/django-sp-0.3b31.tar.gz/django-sp-0.3b31/socialprofile/admin.py
@@ -0,0 +1,17 @@
+"""Django Admin Site configuration for socialprofiles"""
+
+# pylint: disable=R0901,R0904
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from socialprofile.models import SocialProfile
+from image_cropping.admin import ImageCroppingMixin
+
+@admin.register(SocialProfile)
+class CustomUserAdmin(ImageCroppingMixin, UserAdmin):
+    """Sets up the custom user admin display"""
+    list_display = ('username', 'email', 'gender', 'first_name', 'last_name', 'is_staff', 'is_active', 'manually_edited','visible')
+
+# @admin.register(Group)
+# class GroupAdmin(BaseGroupAdmin):
+    # pass
