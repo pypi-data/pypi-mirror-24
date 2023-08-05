26cf5980ab1d9074338f0093492289f928bd6c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package skakdollar/0.1/skakdollar-0.1.tar.gz

---
+++ packages/skakdollar/skakdollar-0.1.tar.gz/skakdollar-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='skakdollar',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/killall5/skakdollar',
+      author='Alexey Tamarevskiy',
+      author_email='killall5@me.com',
+      license='MIT',
+      packages=['skakdollar'])
