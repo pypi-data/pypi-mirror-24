7c43b911a5758cbcc74b170b56bb97a9dc7dc42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:00 +0000

    Add package ledger-rest-api-dev/0.1.9/ledger_rest_api_dev-0.1.9-py3-none-any.whl

---
+++ packages/ledger-rest-api-dev/ledger_rest_api_dev-0.1.9-py3-none-any.whl/ledger_rest_api_dev-0.1.9.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+REST wrapper for Sovrin client
+
+
---
+++ packages/ledger-rest-api-dev/ledger_rest_api_dev-0.1.9-py3-none-any.whl/sovrin_client_rest/test/constants.py
@@ -0,0 +1,6 @@
+user_wallet_seed = ('ULtgFQJe6bjiFbs7ke3NJD', 'restserverdefaultwallet000000000')
+another_user_wallet_seed = ('6g1u9vpX1qPn8YBjWvtu4c', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+trust_anchor_seed = ('KvgbE1vQHdNNzQsPTGEiKh', 'trust anchors are people too....')
+unadded_dest = ('SomeUnAddedDest', 'unaddeddest000000000000000000000')
+
+default_seed = "00000000000000000000000000000000"
