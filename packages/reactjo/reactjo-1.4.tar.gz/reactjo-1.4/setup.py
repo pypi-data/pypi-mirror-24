2b0feef6014ae09fe13c61570aef86c8d39bc758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:50 +0000

    Add package reactjo/1.4/reactjo-1.4.tar.gz

---
+++ packages/reactjo/reactjo-1.4.tar.gz/reactjo-1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('reactjo/reactjo.py').read(),
+    re.M
+    ).group(1)
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "reactjo",
+    packages = ["reactjo"],
+    entry_points = {
+        "console_scripts": ['reactjo = reactjo.reactjo:main']
+        },
+    version = 1.4,
+    description = "Extensible scaffolding engine.",
+    long_description = long_descr,
+    author = "Aaron Price",
+    author_email = "coding.aaronp@gmail.com",
+    url = "https://github.com/aaron-price/reactjo.git"
+    )
