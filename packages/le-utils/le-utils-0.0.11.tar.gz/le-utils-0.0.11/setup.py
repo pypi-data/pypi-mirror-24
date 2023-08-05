6612b13c2355a8419fa124496c0bb8b448d5b892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:03 +0000

    Add package le-utils/0.0.11/le-utils-0.0.11.tar.gz

---
+++ packages/le-utils/le-utils-0.0.11.tar.gz/le-utils-0.0.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name="le-utils",
+    packages = find_packages(),
+    version="0.0.11",
+    description="LE Utils and constants shared across Kolibri, Ricecooker and the Content Curation Server.",
+    long_description=long_description,
+    license="MIT",
+    url="https://github.com/learningequality/le-utils",
+    download_url="https://github.com/learningequality/le-utils/tarball/0.0.9c14",
+    keywords="le-utils le_utils le utils kolibri ricecooker content curation",
+    package_data={"le_utils": ["resources/*.json"], },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+    ],
+    author="Jordan Yoshihara",
+    author_email="jordan@learningequality.org", )
