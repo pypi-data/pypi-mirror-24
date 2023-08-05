45660fc13fa18502ddc014f782fc631f97b691de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:04 +0000

    Add package fdict/0.6.5/fdict-0.6.5.tar.gz

---
+++ packages/fdict/fdict-0.6.5.tar.gz/fdict-0.6.5/fdict/_version.py
@@ -0,0 +1,9 @@
+# Definition of the version number
+
+__all__ = ["__version__"]
+
+# major, minor, patch, -extra
+version_info = 0, 6, 5
+
+# Nice string for the version
+__version__ = '.'.join(map(str, version_info))
