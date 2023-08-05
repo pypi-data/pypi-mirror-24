b8951dcff046167d5917cea8f958ebfa83ae569b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:28 +0000

    Add package Mopidy-GPIOcont/0.1.31/Mopidy-GPIOcont-0.1.31.tar.gz

---
+++ packages/Mopidy-GPIOcont/Mopidy-GPIOcont-0.1.31.tar.gz/Mopidy-GPIOcont-0.1.31/mopidy_gpiocont/frontend.py
@@ -0,0 +1,21 @@
+import pykka
+import logging
+
+from mopidy import core
+
+logger = logging.getLogger(__name__)
+logger.warning("GPIOcont: Frontend dingen")
+
+class GPIOcont(pykka.ThreadingActor, core.CoreListener):
+
+    def __init__(self, config, core):
+        super(GPIOcont, self).__init__()
+        self.core = core
+        #from .input_gpio import input_GPIO
+        #self.input = input_GPIO(self, config['gpiocont'])
+
+
+
+    def input(self, input_event):
+        logger.error("GPIOcont: Input event arrived at frontend.")
+
