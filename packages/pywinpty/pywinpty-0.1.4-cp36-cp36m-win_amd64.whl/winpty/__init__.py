0729267e6f5d2e4bbe8db33cabfc7f4120294437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package pywinpty/0.1.4/pywinpty-0.1.4-cp36-cp36m-win_amd64.whl

---
+++ packages/pywinpty/pywinpty-0.1.4-cp36-cp36m-win_amd64.whl/winpty/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Pywinpty
+========
+This package provides a Cython wrapper around winpty C++ library.
+"""
+
+# yapf: disable
+
+# Local imports
+from .winpty_wrapper import PTY
+
+
+# yapf: enable
+
+PTY
+VERSION_INFO = (0, 1, 4)
+__version__ = '.'.join(map(str, VERSION_INFO))
