64cbdc7a4e96042ab568dc5c5f14e2fbd2dbe164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:47 +0000

    Add package pylatus-brokers/2017.8.7/pylatus-brokers-2017.8.7.tar.gz

---
+++ packages/pylatus-brokers/pylatus-brokers-2017.8.7.tar.gz/pylatus-brokers-2017.8.7/pylatus_brokers/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .chunks import PylatusExperimentFinished, PylatusExperimentParams
+from . import dcu_broker, ppu_broker
---
+++ packages/pylatus-brokers/pylatus-brokers-2017.8.7.tar.gz/pylatus-brokers-2017.8.7/pylatus_brokers/constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
+EXCHANGE_DCU = 'Pilatus_data'
+QUEUE_DCU = 'Pilatus_data'
+EXCHANGE_SCAN = 'Pilatus_scans'
+WATCHDOG_SLEEP = 0.001  # 10 msec
+WAIT_TIME = 1  # sec
+WAIT_REPEAT = 10
+HTIME_TOLERANCE = 3  # sec
