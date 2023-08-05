cacf17fcaa56330595925a79f850a651ac8bdd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:55 +0000

    Add package collabtools/1.0.60/collabtools-1.0.60.tar.gz

---
+++ packages/collabtools/collabtools-1.0.60.tar.gz/collabtools-1.0.60/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'collabtools',
+  packages = ['collab'],
+  version = '1.0.60',
+  description = 'A program for interacting with the API of Collab.',
+  long_description = 'A program for interacting with the API of Collab.',
+  author = 'Florian Dietz',
+  author_email = 'floriandietz44@gmail.com',
+  url='http://example.com',
+  license = 'MIT',
+  package_data={
+      '': ['*.txt', # this covers both the LICENSE.txt file in this folder, and the TRUTH.txt file in the /collab/ folder
+          'fibonacci/*'], # this covers the data in the Docker example folder
+   },
+   entry_points = {
+        'console_scripts': [
+            'collabtools=collab.collabtools:main',
+        ],
+    },
+    install_requires=[
+        'docker',
+    ],
+)
