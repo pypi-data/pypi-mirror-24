0e44403f9800ff7fb5a9c3cd0b08956ccf6d475c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package flake8-graphql/0.1.2/flake8-graphql-0.1.2.tar.gz

---
+++ packages/flake8-graphql/flake8-graphql-0.1.2.tar.gz/flake8-graphql-0.1.2/flake8_graphql.py
@@ -0,0 +1,31 @@
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import ast
+
+from graphql import Source, parse
+from graphql.error import GraphQLError
+
+
+class GraphQLChecker(object):
+    """
+    flake8 plugin that lints your graphql strings
+    """
+    name = 'flake8-graphql'
+    version = '0.1.2'
+
+    def __init__(self, tree, *args, **kwargs):
+        self.tree = tree
+
+    def run(self):
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.Call) and getattr(node.func, 'id', None) == 'gql':
+                first_arg = node.args[0]
+
+                if isinstance(first_arg, ast.Str):
+                    query = first_arg.s
+                    try:
+                        Source(query)
+                        parse(query)
+                    except GraphQLError as e:
+                        yield (node.lineno, node.col_offset, 'GQL100: ' + e.message, type(self))
