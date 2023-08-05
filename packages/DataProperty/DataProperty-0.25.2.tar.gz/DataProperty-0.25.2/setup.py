d515944fde5ddd069f89abad03060a74381c1392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:30 +0000

    Add package DataProperty/0.25.2/DataProperty-0.25.2.tar.gz

---
+++ packages/DataProperty/DataProperty-0.25.2.tar.gz/DataProperty-0.25.2/setup.py
@@ -0,0 +1,80 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import io
+import os.path
+import sys
+
+import setuptools
+
+
+MISC_DIR = "misc"
+REQUIREMENT_DIR = "requirements"
+
+with io.open("README.rst", encoding="utf8") as f:
+    long_description = f.read()
+
+with io.open(os.path.join(MISC_DIR, "summary.txt"), encoding="utf8") as f:
+    summary = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
+    docs_requires = [line.strip() for line in f if line.strip()]
+
+needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+author = "Tsuyoshi Hombashi"
+email = "tsuyoshi.hombashi@gmail.com"
+project_name = "DataProperty"
+
+setuptools.setup(
+    name=project_name,
+    version="0.25.2",
+    url="https://github.com/thombashi/{}".format(project_name),
+
+    author=author,
+    author_email=email,
+    description=summary,
+    include_package_data=True,
+    install_requires=install_requires,
+    keywords=["data", "property"],
+    license="MIT License",
+    long_description=long_description,
+    maintainer=author,
+    maintainer_email=email,
+    packages=setuptools.find_packages(exclude=["test*"]),
+    setup_requires=pytest_runner,
+    tests_require=tests_requires,
+    extras_require={
+        "test": tests_requires,
+        "docs": docs_requires,
+    },
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
