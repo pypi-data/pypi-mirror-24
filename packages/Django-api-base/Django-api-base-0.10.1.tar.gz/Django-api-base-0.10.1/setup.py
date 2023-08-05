05a8989c975d5fb4878917ce9ebc7c7c59d474c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package Django-api-base/0.10.1/Django-api-base-0.10.1.tar.gz

---
+++ packages/Django-api-base/Django-api-base-0.10.1.tar.gz/Django-api-base-0.10.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='Django-api-base',
+    version='0.10.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Free',
+    description='A simple Django app to make the RESTFULL APIs',
+    long_description=README,
+    url='http://www.maneeshvshaji.in/',
+    author='Maneesh',
+    author_email='maneeshvettukattil@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=['django', 'rollbar'],
+)
