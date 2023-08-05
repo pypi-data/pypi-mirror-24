77747df6dc91727510f63922c6da666e613dd7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:50 +0000

    Add package highlyprobable/0.0.1.10/highlyprobable-0.0.1.10.tar.gz

---
+++ packages/highlyprobable/highlyprobable-0.0.1.10.tar.gz/highlyprobable-0.0.1.10/highlyprobable/config.py
@@ -0,0 +1,23 @@
+import logging
+from os.path import expanduser
+
+META_API_ROOT_URL = 'https://highlyprobable.com:9011/api/'
+AI_API_ROOT_URL = 'https://highlyprobable.com:9013/ai/'
+LOG_PATH = expanduser('~/hpcli.log')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename=LOG_PATH,
+    filemode='w'
+)
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
