f751ff26d075466411e0b7e457eb7ad248e993ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:53 +0000

    Add package claripy/6.7.7.27/claripy-6.7.7.27.tar.gz

---
+++ packages/claripy/claripy-6.7.7.27.tar.gz/claripy-6.7.7.27/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+setup(
+    name='claripy',
+    version='6.7.7.27',
+    packages=packages,
+    install_requires=[
+        'ana',
+        'angr-only-z3-custom==9002',
+    ],
+    description='An abstraction layer for constraint solvers',
+    url='https://github.com/angr/claripy',
+)
