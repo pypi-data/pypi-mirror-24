eed39da0b1914c6c4d926ccd0075508888f09c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package graphene-gae/2.0.dev2017073101/graphene_gae-2.0.dev2017073101-py2.py3-none-any.whl

---
+++ packages/graphene-gae/graphene_gae-2.0.dev2017073101-py2.py3-none-any.whl/graphene_gae/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .ndb.types import (
+    NdbObjectType
+)
+
+from .ndb.fields import (
+    NdbConnectionField,
+)
+
+__author__ = 'Eran Kampf'
+__version__ = '2.0.dev2017073101'
+
+__all__ = [
+    NdbObjectType,
+    NdbConnectionField,
+]
