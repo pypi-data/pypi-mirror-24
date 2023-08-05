086023fd8e7f8320e00489f9f65449386eb123a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:52 +0000

    Add package GloboNetworkAPI/0.7.5/GloboNetworkAPI-0.7.5.tar.gz

---
+++ packages/GloboNetworkAPI/GloboNetworkAPI-0.7.5.tar.gz/GloboNetworkAPI-0.7.5/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os.path
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = __import__('networkapiclient').VERSION
+
+setup(
+    name='GloboNetworkAPI',
+    version=version,
+    description='GloboNetworkAPI Client Python',
+    long_description='Python implementation of a client library for GloboNetworkAPI',
+    keywords='network GloboNetwork GloboNetworkAPI',
+    author='Marcus Vinicius Gonçalves Cesário',
+    author_email='marcus.vinicius@corp.globo.com',
+    url='https://github.com/globocom/GloboNetworkAPI-client-python',
+    license='LICENSE.txt',
+    install_requires=[
+        'requests==2.4.3',
+    ],
+    packages=find_packages(),
+)
