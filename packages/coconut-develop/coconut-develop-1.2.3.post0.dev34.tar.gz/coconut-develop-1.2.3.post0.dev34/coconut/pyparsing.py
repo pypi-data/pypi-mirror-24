b1e3604dee03fd6768795398936a3e5d3056acf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:47 +0000

    Add package coconut-develop/1.2.3.post0.dev34/coconut-develop-1.2.3.post0.dev34.tar.gz

---
+++ packages/coconut-develop/coconut-develop-1.2.3.post0.dev34.tar.gz/coconut-develop-1.2.3.post0.dev34/coconut/pyparsing.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-----------------------------------------------------------------------------------------------------------------------
+# INFO:
+#-----------------------------------------------------------------------------------------------------------------------
+
+"""
+Author: Evan Hubinger
+License: Apache 2.0
+Description: Wrapper around PyParsing that selects the best available implementation.
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------
+# IMPORTS:
+#-----------------------------------------------------------------------------------------------------------------------
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from coconut.root import *  # NOQA
+
+try:
+    from cPyparsing import *  # NOQA
+    if DEVELOP:
+        from cPyparsing import _trim_arity  # NOQA
+
+    from cPyparsing import __version__
+    PYPARSING = "Cython cPyparsing v" + __version__
+
+except ImportError:
+    from pyparsing import *  # NOQA
+    if DEVELOP:
+        from pyparsing import _trim_arity  # NOQA
+
+    from pyparsing import __version__
+    PYPARSING = "Python pyparsing v" + __version__
+
+from coconut.constants import (
+    use_packrat,
+    packrat_cache_size,
+    default_whitespace_chars,
+    varchars,
+)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# SETUP:
+#-----------------------------------------------------------------------------------------------------------------------
+
+if use_packrat:
+    ParserElement.enablePackrat(packrat_cache_size)
+
+ParserElement.setDefaultWhitespaceChars(default_whitespace_chars)
+
+Keyword.setDefaultKeywordChars(varchars)
