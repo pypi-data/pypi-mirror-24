dc6cfc16852473fdad8393263ad22bf56edca6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:43 +0000

    Add package bitprim-native/1.0.11/bitprim_native-1.0.11.tar.gz

---
+++ packages/bitprim-native/bitprim_native-1.0.11.tar.gz/bitprim_native-1.0.11/utils.h
@@ -0,0 +1,18 @@
+#ifndef BITPRIM_PY_UTILS_H_
+#define BITPRIM_PY_UTILS_H_
+
+
+#include <Python.h>
+#include <bitprim/nodecint.h>
+
+void* get_ptr(PyObject* obj);
+
+executor_t cast_executor(PyObject* obj);
+
+int char2int(char input);
+
+void hex2bin(const char* src, uint8_t* target);
+
+PyObject* to_py_obj(void*);
+
+#endif
