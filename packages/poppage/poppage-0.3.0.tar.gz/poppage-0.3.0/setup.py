beea83af8f42c7da5b3830658b2f59eba8a6c3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:26 +0000

    Add package poppage/0.3.0/poppage-0.3.0.tar.gz

---
+++ packages/poppage/poppage-0.3.0.tar.gz/poppage-0.3.0/setup.py
@@ -0,0 +1,31 @@
+from os.path import isfile
+from setuptools import setup, find_packages
+
+setup(
+    name = "poppage",
+    version = "0.3.0",
+    author = "Jeff Rimko",
+    author_email = "jeffrimko@gmail.com",
+    description = "Utility for generating files and directories.",
+    license = "MIT",
+    keywords = "project-templates file-templates file-generation",
+    url = "https://github.com/jeffrimko/PopPage",
+    py_modules=["poppage"],
+    scripts=["poppage.py","poppage.bat"],
+    long_description=open("README.rst").read() if isfile("README.rst") else "",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires=[
+        "auxly",
+        "binaryornot",
+        "docopt",
+        "jinja2",
+        "jinja2schema",
+        "pyyaml",
+        "qprompt",
+   ],
+)
