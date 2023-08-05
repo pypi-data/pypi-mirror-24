fad33b5a118f7a3dba79b579ff1448c3bcdb5af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:22 +0000

    Add package i2crelay/0.1.0/i2crelay-0.1.0.tar.gz

---
+++ packages/i2crelay/i2crelay-0.1.0.tar.gz/i2crelay-0.1.0/i2crelay/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Creation:    06.06.2017
+# Last Update: 21.07.2017
+#
+# <https://codewerft.net>
+#
+# Copyright (c) 2017 Codewerft UG (haftungsbeschränkt)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from i2crelay import I2CRelayBoard
