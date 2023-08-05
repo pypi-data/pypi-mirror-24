e6e602d90d7645833a36a05cafeaf1f02479cac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:27 +0000

    Add package Mopidy-GPIOcont/0.1.17/Mopidy-GPIOcont-0.1.17.tar.gz

---
+++ packages/Mopidy-GPIOcont/Mopidy-GPIOcont-0.1.17.tar.gz/Mopidy-GPIOcont-0.1.17/mopidy_gpiocont/frontend.py
@@ -0,0 +1,25 @@
+import pykka
+import logging
+
+from mopidy import core
+
+logger = logging.getLogger(__name__)
+
+class GPIOcont(pykka.ThreadingActor, core.CoreListener):
+    def __init__(self, config, core):
+        super(GPIOcont, self).__init__()
+        self.core = core
+        logger.info("GPIOcont: Frontend initialized")
+        #from .input_gpio import input_GPIO
+        #self.input = input_GPIO(self, config['gpiocont'])
+
+
+
+    def input(self, input_event):
+        logger.error(" input event")
+        if input_event == 'play':
+            if self.core.playback.state.get() == \
+                    core.PlaybackState.PLAYING:
+                self.core.playback.pause()
+            else:
+                self.core.playback.play()
