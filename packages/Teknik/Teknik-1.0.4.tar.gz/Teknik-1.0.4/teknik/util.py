11de48e8d52d8ab8f8a5a74092c5087ab73c9e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:49 +0000

    Add package Teknik/1.0.4/Teknik-1.0.4.tar.gz

---
+++ packages/Teknik/Teknik-1.0.4.tar.gz/Teknik-1.0.4/teknik/util.py
@@ -0,0 +1,19 @@
+from urllib.parse import urljoin
+
+defaultUrl = 'https://api.teknik.io/v1/'
+defaultUsername = ''
+defaultToken = ''
+
+def GetAPIUrl(url, service):
+  finalUrl = ''
+  
+  if url is None or url == '':
+    url = defaultUrl
+
+  server = service.lower()
+  if service == 'upload':
+    finalUrl = urljoin(url, 'Upload')
+  elif service == 'paste':
+    finalUrl = urljoin(url, 'Paste')
+  
+  return finalUrl
