3c46f6af2ca181debee24c78f0e21526f8dd0fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:03 +0000

    Add package webnsock/0.0.2/webnsock-0.0.2.tar.gz

---
+++ packages/webnsock/webnsock-0.0.2.tar.gz/webnsock-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='webnsock',
+    packages=['webnsock'],
+    version='0.0.2',
+    description='A web.py and websocket framework for interactive webservices',
+    author='Marc Hanheide',
+    author_email='marc@hanheide.net',
+    url='https://github.com/marc-hanheide/webnsock',  # use the URL to the github repo
+    download_url='https://github.com/marc-hanheide/webnsock/archive/0.0.2.tar.gz',  # I'll explain this in a second
+    keywords=['web.py', 'websockets', 'webserver'],  # arbitrary keywords
+    classifiers=[],
+    include_package_data=True,
+)
