ee91e464310994bdf1fa5846ca451c4385d7d516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:30 +0000

    Add package Mopidy-GPIOcont/0.1.34/Mopidy-GPIOcont-0.1.34.tar.gz

---
+++ packages/Mopidy-GPIOcont/Mopidy-GPIOcont-0.1.34.tar.gz/Mopidy-GPIOcont-0.1.34/mopidy_gpiocont/input_gpio.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+
+import logging
+logger = logging.getLogger(__name__)
+logger.debug("GPIOcont: input_gpio.py called.")
+
+class input_GPIO():
+    def __init__(self, frontend, pins):
+        self.frontend = frontend
+
+        try:
+            # set pin mode to BCM
+            GPIO.setmode(GPIO.BCM)
+            logger.debug("GPIOcont: Pinmode set.")
+            # Play button
+            GPIO.setup(pins['play_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(pins['play_pin'], GPIO.FALLING, callback=self.play, bouncetime=30)
+
+            logger.debug("GPIOcont: pin events added.")
+        except RuntimeError:
+            logger.error("GPIOcont: Not enough permission to open GPIO")
+
+    def play(self, channel):
+        self.frontend.input_event('play')
+        logger.debug("GPIOcont: Play pin pressed.")
+        if GPIO.input(channel)==0:
+            logger.debug("GPIOcont: Button pressed input_gpio.py.")
