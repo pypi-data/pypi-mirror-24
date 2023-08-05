c527b08e616200e99971f974fc1a3af4bcaaf72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package pypletics/0.0.4.8/pypletics-0.0.4.8-py3-none-any.whl

---
+++ packages/pypletics/pypletics-0.0.4.8-py3-none-any.whl/pypletics/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+'''
+    Le définition de ce package est à initialiser selon les réponses auxquelles 
+    ce dernier répondra !
+'''
+ 
+__version__ = '0.0.4.8'
+ 
+from pypletics.introduction import introduction
+from pypletics.structured import get_structured_df
+from pypletics.network import get_my_network
