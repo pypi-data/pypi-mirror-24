6be03391946745c2c98f2fd53cb7c796203533f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:02 +0000

    Add package snipsskillscore/0.1.5.5.23/snipsskillscore-0.1.5.5.23-py2.7.egg

---
+++ packages/snipsskillscore/snipsskillscore-0.1.5.5.23-py2.7.egg/snipsskillscore/usb_utils.py
@@ -0,0 +1,65 @@
+# -*-: coding utf-8 -*-
+""" USB utilities. """
+
+from __future__ import absolute_import
+import os
+import re
+import subprocess
+import logging
+
+import usb.core
+import usb.util
+
+
+class USB:
+
+    class Device:
+        unknown, respeaker, conexant = range(3)
+
+    # this method should work in all platorms
+    @staticmethod
+    def get_boards():
+        all_devices = usb.core.find(find_all=True)
+
+        if not all_devices:
+            return USB.Device.unknown
+
+        for board in all_devices:
+            try:
+                devices = board.product.lower()
+                if devices.find("respeaker") >= 0:
+                    return USB.Device.respeaker
+                elif devices.find("conexant") >= 0:
+                    return USB.Device.conexant
+                else:
+                    return USB.Device.unknown
+            except Exception as e:
+                logging.warning("Exception getting product string: %s", e)
+                continue
+
+    @staticmethod
+    def get_usb_led_device():
+        devices = USB.lsusb()
+        if not devices:
+            return None
+        devices = devices.lower()
+        if devices.find("respeaker") >= 0:
+            return USB.Device.respeaker
+        elif devices.find("conexant") >= 0:
+            # TODO: check if this is the string to look for
+            return USB.Device.conexant
+        else:
+            return USB.Device.unknown
+
+    @staticmethod
+    def lsusb():
+        FNULL = open(os.devnull, 'w')
+        try:
+            # Raspberry
+            return subprocess.check_output(["lsusb"])
+        except:
+            try:
+                # OSX
+                return subprocess.check_output(["system_profiler", "SPUSBDataType"])
+            except:
+                return None
