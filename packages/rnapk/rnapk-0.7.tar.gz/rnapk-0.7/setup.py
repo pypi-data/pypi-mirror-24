082584c845694709cfc3a312311b3db5e916bef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:29 +0000

    Add package rnapk/0.7/rnapk-0.7.tar.gz

---
+++ packages/rnapk/rnapk-0.7.tar.gz/rnapk-0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='rnapk',
+    version='0.7',
+    description='A simple utility to send React Native APKs via email.',
+    py_modules=['main'],
+    install_requires=[
+        'Click',
+        'PyYaml'
+    ],
+    entry_points='''
+        [console_scripts]
+        rnapk=main:cli
+    ''',
+    url='https://github.com/srishanbhattarai/rnapk',
+    author='Srishan Bhattarai',
+    author_email='srishanbhattarai@gmail.com',
+    license='MIT',
+    include_package_data=True
+)
