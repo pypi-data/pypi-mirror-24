ef1167a3ac47ee139827b5b4f35bce15791c64aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:46 +0000

    Add package vy/3.1.2/vy-3.1.2.tar.gz

---
+++ packages/vy/vy-3.1.2.tar.gz/vy-3.1.2/vyapp/tools.py
@@ -0,0 +1,42 @@
+"""
+This module implements a set of functions that are commonly used by plugins.
+"""
+
+from vyapp.app import root
+import sys
+
+
+def get_area_tab_index(area):
+    """
+    This function returns the tab index that area is
+    attached to.
+    """
+
+    return area.master.master.master
+
+def set_line(area, line):
+    """
+    This function receives an AreaVi widget instance and a line number
+    then sets the focus to the AreaVi widget and the cursor at line.
+    """
+
+    sys.stderr.write(area.filename + '\n')
+    root.note.select(get_area_tab_index(area))
+    area.focus()
+    area.setcur(line)
+
+def match_sub_pattern(pattern, lst):
+    # pattern = buffer(pattern)
+    for indi in lst:
+        for indj in range(0, len(pattern)):
+                if indi.startswith(pattern[indj:]):
+                    yield indi, indj
+                    
+
+
+
+
+
+
+
+
