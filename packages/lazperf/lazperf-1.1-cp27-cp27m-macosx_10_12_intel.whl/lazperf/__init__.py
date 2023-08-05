78542968a72969f5de45816e1a28e7cb319f5667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package lazperf/1.1/lazperf-1.1-cp27-cp27m-macosx_10_12_intel.whl

---
+++ packages/lazperf/lazperf-1.1-cp27-cp27m-macosx_10_12_intel.whl/lazperf/__init__.py
@@ -0,0 +1,7 @@
+__version__='1.1.0'
+from .pylazperfapi import PyDecompressor as Decompressor
+from .pylazperfapi import PyCompressor as Compressor
+from .pylazperfapi import PyVLRDecompressor as VLRDecompressor
+from .pylazperfapi import buildNumpyDescription
+from .pylazperfapi import buildGreyhoundDescription
+
