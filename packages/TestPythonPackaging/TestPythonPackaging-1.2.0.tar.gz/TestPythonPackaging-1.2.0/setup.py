ad0e64921783ca5917402a9527f8d5f56649798f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package TestPythonPackaging/1.2.0/TestPythonPackaging-1.2.0.tar.gz

---
+++ packages/TestPythonPackaging/TestPythonPackaging-1.2.0.tar.gz/TestPythonPackaging-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = 'TestPythonPackaging',
+	version = '1.2.0',
+	description = 'SmallDescription',
+	long_description = open("README.rst", "r").read(),
+	author = 'Othmane Lahrimi',
+	author_email = 'lahrimi.oth1996@live.fr',
+	packages = ['SecondPy'],
+)
