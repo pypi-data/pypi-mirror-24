3b8b3ac89ade97f444391836414ef5e07864af01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:57 +0000

    Add package curso/1.7/curso-1.7.tar.gz

---
+++ packages/curso/curso-1.7.tar.gz/curso-1.7/curso/urls.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from curso import views
+urlpatterns = [
+	url(r'^$', views.search, name='search'),
+	url(r'^incWeight/$', views.incWeight, name='incWeight'),
+]
---
+++ packages/curso/curso-1.7.tar.gz/curso-1.7/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name = 'curso',
+    packages = ['curso'],
+    version = '1.7',
+    description = 'A search Engine that use KCM & KEM api invented by UDIC at NCHU.',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/Stufinite/curso',
+    download_url = 'https://github.com/Stufinite/curso/archive/v1.7.tar.gz',
+    keywords = ['Search Engine', 'campass'],
+    classifiers = [],
+    license='MIT',
+    install_requires=[
+        'djangoApiDec',
+        'jieba==0.38',
+        'pymongo==3.4.0',
+        'PyPrind==2.9.9',
+        'requests==2.12.3',
+        'simplejson==3.10.0',
+    ],
+    zip_safe=True
+)
