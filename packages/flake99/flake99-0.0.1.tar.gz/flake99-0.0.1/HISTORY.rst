3a35ad48b817adabc2a4dbb15e72ee44fe1a9504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:16 +0000

    Add package flake99/0.0.1/flake99-0.0.1.tar.gz

---
+++ packages/flake99/flake99-0.0.1.tar.gz/flake99-0.0.1/HISTORY.rst
@@ -0,0 +1,14 @@
+.. :changelog:
+
+History
+-------
+
+Pending Release
+---------------
+
+.. Insert new release notes below this line
+
+1.0.0 (2017-06-16)
+------------------
+
+* First release on PyPI, featuring ``ec2_metadata`` object.
---
+++ packages/flake99/flake99-0.0.1.tar.gz/flake99-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='flake99',
+    version='0.0.1',
+    description='Next generation linter',
+    author='Adam Johnson',
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/flake99',
+    py_modules=[],
+    include_package_data=True,
+    install_requires=[],
+    license='ISC License',
+    zip_safe=False,
+)
