09b4d9682fa312dc457a69c38d7ea1ac3d9381f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package pyfakeuse/0.0.5/pyfakeuse-0.0.5.tar.gz

---
+++ packages/pyfakeuse/pyfakeuse-0.0.5.tar.gz/pyfakeuse-0.0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='pyfakeuse',
+    version='0.0.5',
+    description='pyfakeuse helps you fool development environments and code inspectors',
+    long_description='pyfakeuse helps you fool development environments and code inspectors',
+    url='https://veltzer.github.io/pyfakeuse',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='python fake use variables idea pycharm fool',
+    packages=setuptools.find_packages(),
+)
