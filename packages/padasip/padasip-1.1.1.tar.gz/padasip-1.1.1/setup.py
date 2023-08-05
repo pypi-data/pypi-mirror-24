c65bd6923a76be2f6b032f82198e1341b735d0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:33 +0000

    Add package padasip/1.1.1/padasip-1.1.1.tar.gz

---
+++ packages/padasip/padasip-1.1.1.tar.gz/padasip-1.1.1/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except:
+        pass
+
+setup(
+    name = 'padasip',
+    packages = find_packages(exclude=("tests",)),
+    version = '1.1.1',
+    description = 'Python Adaptive Signal Processing',
+    long_description = readme(),
+    author = 'Matous Cejnek',
+    maintainer = "Matous Cejnek",
+    author_email = 'matousc@gmail.com',
+    license = 'MIT',
+    url = 'http://matousc89.github.io/padasip/',
+    download_url = 'https://github.com/matousc89/padasip/',
+    keywords = ['adaptive', 'signal-processing'],
+    install_requires=[
+        'numpy',
+    ],    
+    bugtrack_url = "https://github.com/matousc89/padasip/issues", 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Adaptive Technologies',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Programming Language :: Python',
+    ],
+)
