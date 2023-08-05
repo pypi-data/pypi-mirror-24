fd8ac62b228621449f9407554325ce18e4cdc1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package python-750g-recipes/0.1.3/python-750g-recipes-0.1.3.tar.gz

---
+++ packages/python-750g-recipes/python-750g-recipes-0.1.3.tar.gz/python-750g-recipes-0.1.3/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+	name='python-750g-recipes',
+	version='0.1.3',
+	description='Easy-to-use Python API for the 750g.com cokking website.',
+	packages=['septcentcinquanteg_recipes'],
+	url='https://github.com/remaudcorentin-dev/python-750g-recipes',
+	author='Corentin Remaud',
+	author_email='remaudcorentin.dev@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=['bs4'],
+	)
