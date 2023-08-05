bb537ebb45b3bd2fba26a7ab59993a6084c82593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package pyupio/0.8.2/pyupio-0.8.2-py2.py3-none-any.whl

---
+++ packages/pyupio/pyupio-0.8.2-py2.py3-none-any.whl/pyup/errors.py
@@ -0,0 +1,15 @@
+
+class BranchExistsError(Exception):  # pragma: no cover
+    pass
+
+
+class NoPermissionError(Exception):  # pragma: no cover
+    pass
+
+
+class RepoDoesNotExistError(Exception):  # pragma: no cover
+    pass
+
+
+class UnsupportedScheduleError(Exception):  # pragma: no cover
+    pass
