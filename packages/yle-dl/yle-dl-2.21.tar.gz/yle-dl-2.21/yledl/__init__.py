e1060005ab0d6a4d78cc4c630c60db60db751765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:22 +0000

    Add package yle-dl/2.21/yle-dl-2.21.tar.gz

---
+++ packages/yle-dl/yle-dl-2.21.tar.gz/yle-dl-2.21/yledl/__init__.py
@@ -0,0 +1,14 @@
+from yledl import download, StreamFilters, IOContext, BackendFactory, \
+    StreamAction, version, RD_SUCCESS, RD_FAILED, RD_INCOMPLETE
+
+__all__ = [
+    'download',
+    'StreamFilters',
+    'IOContext',
+    'BackendFactory',
+    'StreamAction',
+    'version',
+    'RD_SUCCESS',
+    'RD_FAILED',
+    'RD_INCOMPLETE'
+]
