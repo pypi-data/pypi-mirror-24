275240407c60e5ab725be3325f118034420c8202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:37 +0000

    Add package lyrebird/0.1.0/lyrebird-0.1.0.tar.gz

---
+++ packages/lyrebird/lyrebird-0.1.0.tar.gz/lyrebird-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='lyrebird',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='meituanqa',
+    author_email='',
+    description='A sample python server',
+    install_requires=[
+        'flask',
+        'mitmproxy',
+        'requests',
+        'fire',
+        'colorama',
+        'xlrd'
+    ]
+)
