214a30257679f46161d4d80d930a25bca7396205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package unclebash/0.0.3/unclebash-0.0.3.tar.gz

---
+++ packages/unclebash/unclebash-0.0.3.tar.gz/unclebash-0.0.3/unclebash/__init__.py
@@ -0,0 +1,14 @@
+from .install import install, add_line, del_line
+
+help_str = """
+=============biubiubiubiu================
+This is the *unclebash* package you want!
+Version: 0.0.3
+=========================================
+
+unclebash.install(): install uncle to ~/.bashrc_uncle
+unclebash.add_line(): add a bottom line to ~/.bashrc file
+unclebash.del_line(): delete the bottom line from ~/.bashrc file
+
+"""
+print(help_str)
