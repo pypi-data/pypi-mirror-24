dbdfc6300bc2bf4b81b8b0b6779f480512a99744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package wafer/0.5.5/wafer-0.5.5-py3-none-any.whl

---
+++ packages/wafer/wafer-0.5.5-py3-none-any.whl/wafer/sponsors/templatetags/sponsors.py
@@ -0,0 +1,26 @@
+from django import template
+
+from wafer.sponsors.models import Sponsor, SponsorshipPackage
+
+register = template.Library()
+
+
+@register.inclusion_tag('wafer.sponsors/sponsors_block.html')
+def sponsors():
+    return {
+        'sponsors': Sponsor.objects.all().order_by('packages'),
+        'packages': SponsorshipPackage.objects.all().prefetch_related('files'),
+    }
+
+
+# We use assignment_tag for compatibility with Django 1.8
+# Once we drop 1.8 support, we should change this to
+# simple_tag
+@register.assignment_tag()
+def sponsor_image_url(sponsor, name):
+    """Returns the corresponding url from the sponsors images"""
+    if sponsor.files.filter(name=name).exists():
+        # We avoid worrying about multiple matches by always
+        # returning the first one.
+        return sponsor.files.filter(name=name).first().item.url
+    return ''
