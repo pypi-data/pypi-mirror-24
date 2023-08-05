df07527f91a3746fb8bb37cda853fbdc84af9d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:19 +0000

    Add package dacq2py/0.1/dacq2py-0.1.tar.gz

---
+++ packages/dacq2py/dacq2py-0.1.tar.gz/dacq2py-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dacq2py',
+	  version='0.1',
+	  description='Analysis of electrophysiological data recorded with the Axona recording system',
+	  url='https://github.com/rhayman/dacq2py',
+	  author='Robin Hayman',
+	  author_email='r.hayman@ucl.ac.uk',
+	  license='MIT',
+	  packages=['dacq2py'],
+	  zip_safe=False)
