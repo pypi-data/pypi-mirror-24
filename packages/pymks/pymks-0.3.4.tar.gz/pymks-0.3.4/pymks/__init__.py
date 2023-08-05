0d9620ff0c6b794075b1e82adfcfbea312a08396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:59 +0000

    Add package pymks/0.3.4/pymks-0.3.4.tar.gz

---
+++ packages/pymks/pymks-0.3.4.tar.gz/pymks-0.3.4/pymks/__init__.py
@@ -0,0 +1,51 @@
+try:
+    import pyfftw
+    ## ensure that pyfftw is always imported before numpy to avoid
+    ## https://github.com/materialsinnovation/pymks/issues/304
+except:
+    pass
+
+import os
+from .mks_localization_model import MKSLocalizationModel
+from .bases.primitive import PrimitiveBasis
+from .bases.legendre import LegendreBasis
+from .mks_structure_analysis import MKSStructureAnalysis
+from .mks_homogenization_model import MKSHomogenizationModel
+MKSRegressionModel = MKSLocalizationModel
+DiscreteIndicatorBasis = PrimitiveBasis
+ContinuousIndicatorBasis = PrimitiveBasis
+
+
+def test():
+    r"""
+    Run all the doctests available.
+    """
+    import pytest
+    path = os.path.split(__file__)[0]
+    pytest.main(args=[path, '--doctest-modules', '-r s'])
+
+
+def get_version():
+    """Get the version of the code from egg_info.
+
+    Returns:
+      the package version number
+    """
+    from pkg_resources import get_distribution, DistributionNotFound
+
+    try:
+        version = get_distribution(__name__.split('.')[0]).version # pylint: disable=no-member
+    except DistributionNotFound: # pragma: no cover
+        version = "unknown, try running `python setup.py egg_info`"
+
+    return version
+
+__version__ = get_version()
+
+__all__ = ['__version__',
+           'test',
+           'MKSLocalizationModel',
+           'PrimitiveBasis',
+           'LegendreBasis',
+           'MKSHomogenizationModel',
+           'MKSStructureAnalysis']
