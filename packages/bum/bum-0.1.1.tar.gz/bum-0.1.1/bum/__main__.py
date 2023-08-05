6d6df6b26a08cfd97b3e248a5cd01e8c24e3aee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:18 +0000

    Add package bum/0.1.1/bum-0.1.1.tar.gz

---
+++ packages/bum/bum-0.1.1.tar.gz/bum-0.1.1/bum/__main__.py
@@ -0,0 +1,66 @@
+"""
+'||
+ || ...  ... ...  .. .. ..
+ ||'  ||  ||  ||   || || ||
+ ||    |  ||  ||   || || ||
+ '|...'   '|..'|. .|| || ||.
+
+Created by Dylan Araps
+"""
+import argparse
+import pathlib
+
+from . import display
+from . import song
+
+
+__version__ = "0.1.1"
+
+
+def get_args():
+    """Get the script arguments."""
+    description = "bum - Download and display album art \
+                   for mpd tracks."
+    arg = argparse.ArgumentParser(description=description)
+
+    arg.add_argument("--size", metavar="\"px\"",
+                     help="what size to display the album art in.",
+                     default=250)
+
+    arg.add_argument("--cache_dir", metavar="\"/path/to/dir\"",
+                     help="Where to store the downloaded cover art.",
+                     default=pathlib.Path.home() / ".cache/bum")
+
+    arg.add_argument("--version", action="store_true",
+                     help="Print \"bum\" version.")
+
+    return arg.parse_args()
+
+
+def process_args(args):
+    """Process the arguments."""
+    if args.version:
+        print(f"bum {__version__}")
+        exit(0)
+
+
+def main():
+    """Main script function."""
+    args = get_args()
+    process_args(args)
+
+    disp = display.init(args.size)
+    client = song.init()
+
+    while True:
+        song.get_art(args.cache_dir, args.size, client)
+        display.launch(disp, args.cache_dir / "current.jpg")
+        client.send_idle()
+
+        if client.fetch_idle(["player"]):
+            print("album: Received player event from mpd. Swapping cover art.")
+            continue
+
+
+if __name__ == "__main__":
+    main()
