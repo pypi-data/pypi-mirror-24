6400e3d684af4a5fed4a8f2a34695c7fff407162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:19 +0000

    Add package s4cmb/0.5.1/s4cmb-0.5.1.tar.gz

---
+++ packages/s4cmb/s4cmb-0.5.1.tar.gz/s4cmb-0.5.1/coverage_and_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Script to measure the coverage of the test suite (via doctest).
+## Launch it using ./coverage
+## and open the html files under the folder htmlcov/
+for i in s4cmb/*.py
+do
+    coverage run $i
+    coverage report $i
+    coverage html $i
+done
