b4e8207949ab4936fec242aa67778f7f1b2d6a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:06 +0000

    Add package tomorrow3/1.1.0/tomorrow3-1.1.0.tar.gz

---
+++ packages/tomorrow3/tomorrow3-1.1.0.tar.gz/tomorrow3-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tomorrow3",
+    version="1.1.0",
+    author="Daniel Lupu",
+    author_email="lupu.daniel.f@gmail.com",
+    packages=find_packages(),
+    license="MIT License (See LICENSE)",
+    url="https://github.com/dflupu/tomorrow3",
+    description="simplified tomorrow for python3"
+)
