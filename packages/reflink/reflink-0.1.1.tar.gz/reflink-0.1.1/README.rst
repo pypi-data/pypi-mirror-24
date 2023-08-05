97e487d9d8632e1a82c45de04b14abfcf4243adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:26 +0000

    Add package reflink/0.1.1/reflink-0.1.1.tar.gz

---
+++ packages/reflink/reflink-0.1.1.tar.gz/reflink-0.1.1/README.rst
@@ -0,0 +1,56 @@
+==============
+Python reflink
+==============
+
+
+.. image:: https://img.shields.io/pypi/v/reflink.svg
+        :target: https://pypi.python.org/pypi/reflink
+
+.. image:: https://gitlab.com/rubdos/pyreflink/badges/master/build.svg
+        :target: https://gitlab.com/rubdos/pyreflink/pipelines
+
+.. image:: https://readthedocs.org/projects/reflink/badge/?version=latest
+        :target: https://reflink.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://readthedocs.org/projects/reflink/badge/?version=latest
+        :target: https://rubdos.gitlab.io/pyreflink/docs
+        :alt: Documentation Status
+
+
+Python wrapper around the ``reflink`` system calls.
+
+
+* Free software: MIT license
+* Documentation: https://reflink.readthedocs.io.
+* Documentationfor master branch: https://rubdos.gitlab.io/pyreflink/docs
+
+
+Features
+--------
+
+* Btrfs, XFS, OCFS2 ``reflink`` support. Btrfs is tested the most.
+* A convenience method that checks support for reflinks within a specific directory.
+
+Help wanted
+-----------
+
+Someone to implement the `macOS backend; please see the macOS/APFS issue <https://gitlab.com/rubdos/pyreflink/issues/1>`__.
+
+The same for a `Windows/ReFS implementation <https://gitlab.com/rubdos/pyreflink/issues/1>`__
+
+
+Support
+_______
+
+Support on `the GitLab repository <https://gitlab.com/rubdos/pyreflink/issues>`__,
+feel free to file an issue.
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
---
+++ packages/reflink/reflink-0.1.1.tar.gz/reflink-0.1.1/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import sys
+
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'cffi',
+]
+
+setup_requirements = [
+    'cffi',
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+if sys.platform == 'linux':
+    cffi_modules = [
+        "reflink/linux.py:ffibuilder"
+    ]
+elif sys.platform == 'win32':
+    cffi_modules = [
+        #"reflink/windows.py:ffibuilder"
+    ]
+
+if __name__ == '__main__':
+    setup(
+        name='reflink',
+        version='0.1.1',
+        description="Python reflink contai",
+        long_description=readme + '\n\n' + history,
+        author="Ruben De Smet",
+        author_email='pypi@rubdos.be',
+        url='https://gitlab.com/rubdos/pyreflink',
+        packages=find_packages(include=['reflink']),
+        include_package_data=True,
+        install_requires=requirements,
+        license="MIT license",
+        zip_safe=False,
+        keywords='reflink',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            "Programming Language :: Python :: 2",
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ],
+        test_suite='tests',
+        tests_require=test_requirements,
+        setup_requires=setup_requirements,
+        cffi_modules=cffi_modules,
+    )
