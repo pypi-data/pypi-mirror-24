8b97c8b7db1e5fca8ad0b58c1c3418fdc6a95c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:51 +0000

    Add package wafer/0.5.6/wafer-0.5.6-py3-none-any.whl

---
+++ packages/wafer/wafer-0.5.6-py3-none-any.whl/wafer/registration/forms.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Hidden, Submit
+
+
+class RegistrationFormHelper(FormHelper):
+    form_action = reverse('registration_register')
+
+    def __init__(self, request, *args, **kwargs):
+        super(RegistrationFormHelper, self).__init__(*args, **kwargs)
+        self.add_input(Submit('submit', _('Sign up')))
+
+
+class LoginFormHelper(FormHelper):
+    form_action = settings.LOGIN_URL
+
+    def __init__(self, request, *args, **kwargs):
+        super(LoginFormHelper, self).__init__(*args, **kwargs)
+        if 'next' in request.GET:
+            self.add_input(Hidden('next', request.GET['next']))
+        self.add_input(Submit('submit', _('Log in')))
