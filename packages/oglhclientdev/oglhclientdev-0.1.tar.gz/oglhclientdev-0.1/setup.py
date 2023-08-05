96c7a691698d7e16625548ed74fd9fd1ba517fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:06 +0000

    Add package oglhclientdev/0.1/oglhclientdev-0.1.tar.gz

---
+++ packages/oglhclientdev/oglhclientdev-0.1.tar.gz/oglhclientdev-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'oglhclientdev',
+  packages = ['oglhclientdev'],
+  version = '0.1',
+  description = 'A client for Lighthouse API',
+  author = 'Thiago Melo',
+  author_email = 'thiago.lc.melo@gmail.com',
+  url = 'https://github.com/thiagolcmelo/oglhclient',
+  download_url = 'https://github.com/thiagolcmelo/oglhclient/archive/0.1.tar.gz',
+  keywords = ['testing', 'opengear', 'lighthouse'],
+  classifiers = [],
+)
