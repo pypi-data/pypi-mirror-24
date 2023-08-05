cd527c9b4c9f1f0385902256c4eb916c82f47aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:44 +0000

    Add package kallikrein/0.16.1/kallikrein-0.16.1.tar.gz

---
+++ packages/kallikrein/kallikrein-0.16.1.tar.gz/kallikrein-0.16.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version_parts = (0, 16, 1)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='kallikrein',
+    description='spec framework',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/kallikrein',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'amino>=10.0.0',
+        'golgi>=1.7.0',
+        'hues',
+    ],
+    entry_points={
+        'console_scripts': [
+            'klk = kallikrein.run.cli:klk',
+        ],
+    },
+)
