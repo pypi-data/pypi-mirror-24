e59ddfcd136fa46cbb187c563be657e6c115b7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:08 +0000

    Add package django-frienships/1.0.0/django-frienships-1.0.0.tar.gz

---
+++ packages/django-frienships/django-frienships-1.0.0.tar.gz/django-frienships-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name="django-frienships",
+    version="1.0.0",
+    author="Zoltan Kohalmy",
+    author_email="zoltankohalmy@gmail.com",
+    url="https://github.com/Aperyon/django-friendships",
+    description="Simple solution for Django User Friendships"
+)
