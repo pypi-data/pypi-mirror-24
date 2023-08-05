157feff3be4869e06fa658da034291cb9e302113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:54 +0000

    Add package angrop/6.7.6.9/angrop-6.7.6.9.tar.gz

---
+++ packages/angrop/angrop-6.7.6.9.tar.gz/angrop-6.7.6.9/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='angrop',
+    version='6.7.6.9',
+    description='The rop chain builder based off of angr',
+    packages=['angrop'],
+    install_requires=[
+        'progressbar',
+        'angr',
+        'pyvex',
+        'claripy',
+        'simuvex',
+    ],
+)
