3405eba6b0d6e708ff4b2821014507d791d99dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package django-translate/1.0.16/django-translate-1.0.16.tar.gz

---
+++ packages/django-translate/django-translate-1.0.16.tar.gz/django-translate-1.0.16/django_translate/middleware.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.deprecation import MiddlewareMixin
+from django_translate.services import translator as django_translator
+
+
+class LocaleMiddleware(MiddlewareMixin):
+    """
+    This is a very simple middleware that parses a request
+    and decides what locale to activate in current django_translator
+    """
+    def process_request(self, request):
+        if not hasattr(request, 'LANGUAGE_CODE'):
+            raise RuntimeError('django.middleware.locale.LocaleMiddleware is required '
+                               'in order to use django_translate.middleware.LocaleMiddleware ')
+        django_translator.locale = request.LANGUAGE_CODE
