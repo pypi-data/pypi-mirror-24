ad7685d11f64e918cb75ed7e7eafb6c7fc0151a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package rpmspectool/1.99.6/rpmspectool-1.99.6.tar.gz

---
+++ packages/rpmspectool/rpmspectool-1.99.6.tar.gz/rpmspectool-1.99.6/scripts/rpmspectool
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+
+from rpmspectool import cli
+
+if __name__ == '__main__':
+    sys.exit(cli.main())
---
+++ packages/rpmspectool/rpmspectool-1.99.6.tar.gz/rpmspectool-1.99.6/setup.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import sys
+
+from setuptools import find_packages, setup
+
+if sys.version_info.major < 3:
+    print("Python versions older than 3 are not supported.", file=sys.stderr)
+    sys.exit(1)
+
+setup(
+    name="rpmspectool",
+    version="1.99.6",
+    author="Nils Philippsen",
+    author_email="nils@tiptoe.de",
+    url="https://github.com/nphilipp/rpmspectool",
+    download_url="https://pypi.python.org/pypi/rpmspectool",
+    install_requires=["pycurl"],
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=["scripts/rpmspectool"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Software Development :: Build Tools",
+        ],
+    )
