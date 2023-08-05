1cc85d48b1c79cdfb5f7e96b012a6cc833df03e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:13 +0000

    Add package simupy/0.1.0.dev2/simupy-0.1.0.dev2.zip

---
+++ packages/simupy/simupy-0.1.0.dev2.zip/simupy-0.1.0.dev2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='simupy',
+    version='0.1.0.dev2',
+    description='A framework for modeling and simulating dynamical systems.',
+    # long_description=long_description,
+    packages=['simupy',],
+    author='Benjamin Margolis',
+    author_email='ben@sixpearls.com',
+    url='https://github.com/sixpearls/simupy',
+    install_requires=['numpy','scipy'],
+    license="BSD 2-clause \"Simplified\" License",
+)
