39acfa7a18772ab9d471dc48d0838ceb8d29c527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package grapheme/0.2.1/grapheme-0.2.1-py3-none-any.whl

---
+++ packages/grapheme/grapheme-0.2.1-py3-none-any.whl/grapheme/__init__.py
@@ -0,0 +1,7 @@
+"""
+Main module for the grapheme package.
+"""
+
+from .api import graphemes, length, grapheme_lengths, slice, contains
+
+__all__ = ['graphemes', 'length', 'grapheme_lengths', 'slice', 'contains']
