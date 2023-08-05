1b31dba2aa727b8f116038b5a7d461a37545f7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:59 +0000

    Add package snipsskills/0.1.4.824/snipsskills-0.1.4.824-py2.py3-none-any.whl

---
+++ packages/snipsskills/snipsskills-0.1.4.824-py2.py3-none-any.whl/snipsskills/commands/install.py
@@ -0,0 +1,90 @@
+# -*-: coding utf-8 -*-
+"""The install command."""
+
+import os
+import shutil
+
+from .base import Base, ASSISTANT_DIR, ASSISTANT_ZIP_FILENAME, \
+    ASSISTANT_ZIP_PATH, INTENTS_DIR, SNIPSFILE
+
+from ..utils.snipsfile_parser import Snipsfile, SnipsfileParseException, \
+    SnipsfileNotFoundError
+
+from ..utils.assistant_downloader import AssistantDownloader, \
+    AssistantDownloaderException
+from ..utils.intent_class_generator import IntentClassGenerator
+from ..utils.pip_installer import PipInstaller
+from ..utils.snips_installer import SnipsInstaller, SnipsUnsupportedPlatform
+from ..utils.os_helpers import cmd_exists, is_raspi_os
+from ..utils.microphone_setup import MicrophoneSetup
+from ..utils.logging import log_message, log_success, log_error
+
+
+# pylint: disable=too-few-public-methods
+class Install(Base):
+    """The install command."""
+
+    def run(self):
+        """ Command runner. """
+        try:
+            snipsfile = Snipsfile(SNIPSFILE)
+        except SnipsfileNotFoundError:
+            log_error("Snipsfile not found. Please create one.")
+            return
+        except SnipsfileParseException as err:
+            print(err)
+            return
+
+        if not cmd_exists("snips"):
+            try:
+                log_message("Installing the Snips toolchain.")
+                SnipsInstaller.install()
+            except SnipsUnsupportedPlatform:
+                log_error("Currently, the Snips SDK only runs on a Raspberry Pi. " +
+                          "Skipping installation of the Snips SDK. " +
+                          "If you wish to install the Snips SDK, " +
+                          "run this command from a Raspberry Pi.")
+
+        if snipsfile.assistant_url is None:
+            log_error("No assistants found in Snipsfile.")
+            return
+
+        log_message("Fetching assistant.")
+        try:
+            AssistantDownloader.download(snipsfile.assistant_url,
+                                         ASSISTANT_DIR,
+                                         ASSISTANT_ZIP_FILENAME)
+        except AssistantDownloaderException:
+            log_error("Error downloading assistant. " +
+                      "Make sure the provided URL in the Snipsfile is correct, " +
+                      "and that there is a working network connection.")
+            return
+
+        if cmd_exists("snips"):
+            log_message("Loading Snips assistant.")
+            SnipsInstaller.load_assistant(ASSISTANT_ZIP_PATH)
+
+        log_message("Generating definitions.")
+        try:
+            shutil.rmtree(INTENTS_DIR)
+        except Exception:
+            pass
+
+        if is_raspi_os():
+            log_message("Setting up microphone.")
+            MicrophoneSetup.setup(snipsfile.microphone_config)
+        else:
+            log_message("System is not Raspberry Pi. Skipping microphone setup.")
+
+        generator = IntentClassGenerator()
+        generator.generate(ASSISTANT_ZIP_PATH, INTENTS_DIR)
+
+        if snipsfile.skilldefs is not None and len(snipsfile.skilldefs) > 0:
+            log_message("Installing skills.")
+            for skill in snipsfile.skilldefs:
+                log_message("Installing {}.".format(skill.package_name))
+                PipInstaller.install(skill.package_name)
+
+        log_message("Cleaning up.")
+        os.remove(ASSISTANT_ZIP_PATH)
+        log_success("All done! Run 'snipsskills run' to launch the skills server.")
---
+++ packages/snipsskills/snipsskills-0.1.4.824-py2.py3-none-any.whl/snipsskills/commands/run.py
@@ -0,0 +1,93 @@
+# -*-: coding utf-8 -*-
+"""The run command."""
+# pylint: disable=too-few-public-methods,import-error
+
+import asyncio
+import os
+import subprocess
+from sys import path
+
+from ..utils.snipsfile_parser import Snipsfile, SnipsfileParseException, \
+    SnipsfileNotFoundError
+from ..utils.logging import log_message, log_success, log_warning, log_error
+
+from snipsskillscore.server import Server
+from snipsskillscore.thread_handler import ThreadHandler
+
+from .base import Base, SNIPSFILE
+
+path.append(".snips/intents")
+path.append(".snips/intents/intents")
+
+# pylint: disable=wrong-import-position,wrong-import-order
+from intent_registry import IntentRegistry
+# pylint: disable=wildcard-import,wrong-import-position,wrong-import-order
+from intents import *
+
+BINDINGS_FILE = "bindings.py"
+INTENT_REGISTRY_FILE = ".snips/intents/intent_registry.py"
+
+import threading
+
+
+class Run(Base):
+    """The run command."""
+
+    # pylint: disable=undefined-variable,exec-used,eval-used
+    def run(self):
+        """ Command runner. """
+        try:
+            self.snipsfile = Snipsfile(SNIPSFILE)
+        except SnipsfileNotFoundError:
+            log_error("Snipsfile not found. Please create one.")
+            return
+        except SnipsfileParseException as err:
+            log_error(err)
+            return
+
+        self.skills = {}
+        for skilldef in self.snipsfile.skilldefs:
+            module_name = skilldef.package_name + "." + skilldef.package_name
+            exec("from {} import {}".format(module_name, skilldef.class_name))
+            cls = eval(skilldef.class_name)
+            try:
+                skill_instance = cls(**skilldef.params)
+                self.skills[skilldef.package_name] = skill_instance
+            except Exception as e:
+                log_warning("Error loading skill {}: {}".format(
+                    skilldef.package_name, str(e)))
+
+        self.thread_handler = ThreadHandler()
+
+        registry = IntentRegistry()
+        server = Server(self.snipsfile.mqtt_hostname,
+                        self.snipsfile.mqtt_port,
+                        self.snipsfile.logging_enabled,
+                        registry, self.handle_intent)
+        server.start()
+
+    def handle_intent(self, intent):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        wait_tasks = asyncio.wait(
+            [loop.create_task(self.handle_intent_async(intent))])
+        loop.run_until_complete(wait_tasks)
+
+    @asyncio.coroutine
+    def handle_intent_async(self, intent):
+        """ Handle an intent.
+
+        :param intent: the incoming intent to handle.
+        """
+        for skilldef in self.snipsfile.skilldefs:
+            intent_def = skilldef.find(intent)
+            if intent_def != None:
+                skill = self.skills[skilldef.package_name]
+                if intent_def.action.startswith("{%"):
+                    action = intent_def.action \
+                        .replace("{%", "") \
+                        .replace("%}", "") \
+                        .strip()
+                    exec(action)
+                else:
+                    getattr(skill, intent_def.action)()
