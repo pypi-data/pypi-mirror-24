f7fca91402c54201fd635f70935d41e5f3986059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:29 +0000

    Add package magpie-python/0.1/magpie-python-0.1.tar.gz

---
+++ packages/magpie-python/magpie-python-0.1.tar.gz/magpie-python-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='magpie-python',
+      version='0.1',
+      description='Magpie Python Library',
+      url='http://github.com/magpieim/magpie-python',
+      author='Jon Danao',
+      author_email='jon@magpie.im',
+      license='MIT',
+      packages=['magpie'],
+      zip_safe=False)
