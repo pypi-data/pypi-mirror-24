8f2234364044bf8cb49bc2155d10aa455491dbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:20 +0000

    Add package time_travel/1.0.0/time_travel-1.0.0-py2-none-any.whl

---
+++ packages/time_travel/time_travel-1.0.0-py2-none-any.whl/time_travel/time_travel.py
@@ -0,0 +1,55 @@
+"""Mocking interface for python time libraries."""
+
+from patchers.datetime_patcher import DatetimePatcher
+from patchers.time_patcher import TimePatcher
+from patchers.select_patcher import SelectPatcher
+
+from time_machine_clock import TimeMachineClock
+from events_pool import EventsPool
+
+import sys
+print sys.path
+
+
+class TimeTravel(object):
+    """Context-manager patching time libraries.
+    
+    - For setting timestamps and advancing the time, use the clock object
+      corresponiding to the time_machine_clock interface
+      
+    - For setting events for event based libraies (i.e. select) use the
+      events_pool object corresponding to the events_pool interface.
+    """
+    
+    class EventsType(object):
+        """Empty class to register events types on."""
+    
+    def __init__(self, start_time=0):
+        """Create the patch.
+        
+        @start_time is time in seconds since the epoch.
+        """
+        self.events_pool = EventsPool()
+        self.clock = TimeMachineClock(start_time, [self.events_pool])
+        
+        patches = [DatetimePatcher, TimePatcher, SelectPatcher]
+        self.patches = [patcher(self.clock, self.events_pool) for patcher in
+                        patches]
+        
+        self.events_types = TimeTravel.EventsType()
+        
+        for patcher in self.patches:
+            if patcher.get_events_namespace() is not None:
+                setattr(self.events_types,
+                        patcher.get_events_namespace(),
+                        patcher.get_events_types())
+   
+    def __enter__(self):
+        for patcher in self.patches:
+            patcher.start()
+        
+        return self
+        
+    def __exit__(self, *args):
+        for patcher in self.patches:
+            patcher.stop()
