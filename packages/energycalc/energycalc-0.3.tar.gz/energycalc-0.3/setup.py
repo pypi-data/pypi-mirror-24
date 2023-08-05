9f27458668bebbd324369ea2fdf4e37853097001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:54 +0000

    Add package energycalc/0.3/energycalc-0.3.tar.gz

---
+++ packages/energycalc/energycalc-0.3.tar.gz/energycalc-0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'energycalc',
+  packages = ['energycalc'],
+  version = '0.3',
+  description = 'Simple Energy Calculator',
+  long_description="",
+  author = 'Sepand Haghighi',
+  author_email = 'sepand@qpage.ir',
+  url = 'https://github.com/sepandhaghighi/Energy-Calculator',
+  download_url = 'https://github.com/sepandhaghighi/Energy-Calculator/tarball/v0.3',
+  keywords = ['calculator', 'energy', 'python3','python','energycalc'],
+  classifiers = [],
+  license='MIT',
+)
