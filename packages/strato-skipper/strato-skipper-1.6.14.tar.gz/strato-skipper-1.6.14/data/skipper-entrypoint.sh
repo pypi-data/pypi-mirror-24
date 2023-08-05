d323d6cf7d3d1eb25f0fcb2cc0f847ca7ea9f755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package strato-skipper/1.6.14/strato-skipper-1.6.14.tar.gz

---
+++ packages/strato-skipper/strato-skipper-1.6.14.tar.gz/strato-skipper-1.6.14/data/skipper-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+getent passwd ${SKIPPER_USERNAME} > /dev/null
+if [ x"$?" != x"0" ]; then
+	useradd -u ${SKIPPER_UID} --non-unique -M "${SKIPPER_USERNAME}"
+fi
+
+groupadd -g ${SKIPPER_DOCKER_GID} --non-unique docker
+usermod -G root,docker ${SKIPPER_USERNAME}
+
+su -m ${SKIPPER_USERNAME} -c "$@"
+
