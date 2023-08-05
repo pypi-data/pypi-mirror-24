7dfa20a925fae733177efb7c62bb4ab1a5158f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:52 +0000

    Add package amazon-fees/0.0.2/amazon_fees-0.0.2.tar.gz

---
+++ packages/amazon-fees/amazon_fees-0.0.2.tar.gz/amazon_fees-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amazon_fees',
+    version='0.0.2',
+    packages=find_packages(),
+    author='Dmitry Kalinin',
+    url='https://github.com/null-none/python-amazon-fees'
+)
