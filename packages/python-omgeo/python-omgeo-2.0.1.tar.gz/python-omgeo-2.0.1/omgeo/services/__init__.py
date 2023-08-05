011c4034c982c7cb7069cebe1f44f3f54e3a6972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package python-omgeo/2.0.1/python-omgeo-2.0.1.tar.gz

---
+++ packages/python-omgeo/python-omgeo-2.0.1.tar.gz/python-omgeo-2.0.1/omgeo/services/__init__.py
@@ -0,0 +1,7 @@
+from .bing import Bing
+from .esri import EsriEUSoap, EsriEU, EsriNASoap, EsriNA, EsriWGS, EsriWGSSSL
+from .us_census import USCensus
+from .mapquest import MapQuest, MapQuestSSL
+from .nominatim import Nominatim
+from .mapzen import Mapzen
+from .google import Google
