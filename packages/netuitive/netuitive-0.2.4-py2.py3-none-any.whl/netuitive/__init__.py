e4a3f141fe64a28c325e40f0b934de405c51dee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:57 +0000

    Add package netuitive/0.2.4/netuitive-0.2.4-py2.py3-none-any.whl

---
+++ packages/netuitive/netuitive-0.2.4-py2.py3-none-any.whl/netuitive/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Netuitive, Inc'
+__version__ = '0.2.4'
+
+from .client import Client  # nopep8 # flake8: noqa
+from .element import Element  # nopep8 # flake8: noqa
+from .event import Event  # nopep8 # flake8: noqa
+from .util import to_ms_timestamp  # nopep8 # flake8: noqa
