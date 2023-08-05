60b9848b5723cf65be93fefb5939639dabb7bcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:00 +0000

    Add package auto-py-torrent/1.0.8/auto_py_torrent-1.0.8-py3-none-any.whl

---
+++ packages/auto-py-torrent/auto_py_torrent-1.0.8-py3-none-any.whl/auto_py_torrent/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'auto_py_torrent'
+__description__ = 'Automated tool for file downloads, using torrent or magnet.'
+__url__ = 'https://github.com/ocslegna/auto_py_torrent'
+__version__ = '1.0.8'
+__build__ = 0x001000
+__author__ = 'Gabriel Scotillo'
+__author_email__ = 'gabrielscotillo@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Gabriel Scotillo'
