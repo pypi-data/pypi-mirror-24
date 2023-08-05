ae3854f07cd765e7a93bceccffbb1bd9a141adfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:59 +0000

    Add package django-page-slapper/1.5a5/django-page-slapper-1.5a5.tar.gz

---
+++ packages/django-page-slapper/django-page-slapper-1.5a5.tar.gz/django-page-slapper-1.5a5/page/__init__.py
@@ -0,0 +1,20 @@
+default_app_config = 'page.apps.MyAppConfig'
+
+__version_info__ = {
+    'major': 1,
+    'minor': 5,
+    'micro': False,
+    'releaselevel': 'alpha',
+    'serial': 5
+}
+
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
+    return ''.join(vers)
+
+__version__ = get_version()
