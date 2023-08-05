9952ff28665d7de2ce98270554b5f058424d88e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package snipssmartercoffee/0.1.2.6/snipssmartercoffee-0.1.2.6.tar.gz

---
+++ packages/snipssmartercoffee/snipssmartercoffee-0.1.2.6.tar.gz/snipssmartercoffee-0.1.2.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='snipssmartercoffee',
+    version='0.1.2.6',
+    description='Smarter Coffee skill for Snips',
+    author='Michael Fester',
+    author_email='michael.fester@gmail.com',
+    url='https://github.com/snipsco/snips-skill-smarter-coffee',
+    download_url='',
+    license='MIT',
+    install_requires=[],
+    test_suite="tests",
+    keywords=['snips'],
+    include_package_data=True,
+    packages=[
+        'snipssmartercoffee'
+    ]
+)
