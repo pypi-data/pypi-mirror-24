4d025f653b6af350b79a18c3f8494451b2bd3d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:08 +0000

    Add package FoxDot/0.4.2/FoxDot-0.4.2.zip

---
+++ packages/FoxDot/FoxDot-0.4.2.zip/FoxDot-0.4.2/FoxDot/__main__.py
@@ -0,0 +1,16 @@
+"""
+    FoxDot __main__.py
+    ------------------
+
+    Use FoxDot's interface by running this as a Python script, e.g.
+    python __main__.py or python -m FoxDot if you have FoxDot correctly
+    installed and Python on your path. 
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from lib import FoxDotCode
+from lib.Workspace import workspace
+
+FoxDot = workspace(FoxDotCode).run()
