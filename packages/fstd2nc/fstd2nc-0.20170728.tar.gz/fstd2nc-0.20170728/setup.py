75ff0f616f6fca72510753ea2e6e5e4c956a92c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:51 +0000

    Add package fstd2nc/0.20170728/fstd2nc-0.20170728.tar.gz

---
+++ packages/fstd2nc/fstd2nc-0.20170728.tar.gz/fstd2nc-0.20170728/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from fstd2nc import __version__
+
+setup (
+  name="fstd2nc",
+  version=__version__,
+  description = 'Converts RPN standard files (from Environment Canada) to netCDF files.',
+  url = 'https://github.com/neishm/fstd2nc',
+  author="Mike Neish",
+  license = 'LGPL-3',
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+  ],
+  packages = find_packages(),
+  py_modules = ['fstd2nc'],
+  install_requires = ['numpy','netcdf4','fstd2nc-deps'],
+  package_data = {
+    'fstd2nc_locale': ['*/LC_MESSAGES/fstd2nc.mo'],
+  },
+  entry_points={
+    'console_scripts': [
+      'fstd2nc = fstd2nc:_fstd2nc_cmdline_trapped',
+    ],
+  },
+
+)
+
