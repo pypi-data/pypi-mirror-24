40aaf38e52e717e799646c23c0e24ef0915afcce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:12 +0000

    Add package davidnester/1.0.0/davidnester-1.0.0.tar.gz

---
+++ packages/davidnester/davidnester-1.0.0.tar.gz/davidnester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'davidnester',
+    version = '1.0.0',
+    py_modules = ['davidnester'],
+    author = 'mhchoi8423',
+    author_email = 'mhchoi8423@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
