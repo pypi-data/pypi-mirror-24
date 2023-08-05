9eefa87f3c238396f492292c8bac98f0115e700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:35 +0000

    Add package OpenDSSDirect.py/0.2.7/OpenDSSDirect.py-0.2.7.tar.gz

---
+++ packages/OpenDSSDirect.py/OpenDSSDirect.py-0.2.7.tar.gz/OpenDSSDirect.py-0.2.7/README.md
@@ -0,0 +1,29 @@
+# OpenDSSDirect.py [![Build Status](https://travis-ci.org/NREL/OpenDSSDirect.py.svg?branch=master)](https://travis-ci.org/NREL/OpenDSSDirect.py) [![Build Status](https://ci.appveyor.com/api/projects/status/github/NREL/OpenDSSDirect.py?branch=master&svg=true)](https://ci.appveyor.com/project/kdheepak/opendssdirect-py) [![PyPI](https://img.shields.io/pypi/v/OpenDSSDirect.py.svg)](https://pypi.python.org/pypi/OpenDSSDirect.py/)
+
+OpenDSSDirect.py is a Python package implements a "direct" library interface to [OpenDSS](http://smartgrid.epri.com/SimulationTool.aspx).
+OpenDSS is an open-source distribution system simulator. See [OpenDSSDirect.jl](https://github.com/tshort/OpenDSSDirect.jl) for a similar package in Julia.
+
+**This package is available for Windows, Mac and Linux.**
+
+### Documentation
+
+The documentation for this package can be found [here](http://nrel.github.io/OpenDSSDirect.py).
+
+### Installation
+
+**Recommended**: Install Python using Miniconda or Anaconda
+
+Open a command line interface and type the following.
+
+```bash
+pip install OpenDSSDirect.py
+```
+
+See [installation](https://nrel.github.io/OpenDSSDirect.py/notebooks/Installation.html) instructions for more information.
+
+### Thanks
+
+Thanks to @tshort, Davis, @temcdrm, @GordStephen and @Muxelmann for their input and comments.
+
+See @Muxelmann's repo [here](https://github.com/Muxelmann/OpenDSSDirect.make) on how to build OpenDSS for Linux.
+
---
+++ packages/OpenDSSDirect.py/OpenDSSDirect.py-0.2.7.tar.gz/OpenDSSDirect.py-0.2.7/opendssdirect/utils.py
@@ -0,0 +1,178 @@
+import inspect
+import pandas as pd
+import warnings
+
+
+class Iterator(object):
+    def __init__(self, module, function):
+        assert inspect.ismodule(module), '{module} must be of type module'.format(module=module)
+        self.module = module
+        self.function = function
+
+    def __iter__(self):
+        self.module.First()
+
+        while True:
+
+            yield getattr(self.module, self.function)
+
+            if not self.module.Next() > 0:
+                break
+
+
+def run_command(text, dss=None):
+    '''Use Text interface of OpenDSS'''
+    if dss is None:
+        from . import dss
+
+    return dss.dss_lib.DSSPut_Command(text.encode('ascii')).decode('ascii')
+
+
+def to_dataframe(module):
+    data = dict()
+
+    for e in Iterator(module, 'Name'):
+        data[e()] = dict()
+
+    if len(data) != 0:
+
+        for i in Iterator(module, 'Name'):
+            element_name = i()
+            data[element_name] = {n: getattr(module, n)() for n, f in getmembers(module)}
+    else:
+        class_name = module.__name__
+        warnings.warn("Empty element type ({class_name})".format(class_name=class_name))
+
+    return pd.DataFrame(data).T
+
+
+def class_to_dataframe(class_name, dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    dss.Circuit.SetActiveClass('{class_name}'.format(class_name=class_name))
+    if class_name.lower() != dss.ActiveClass.ActiveClassName().lower():
+        raise NotImplementedError("`{class_name}` is not supported by the `class_to_dataframe` interface, please contact the developer for more information.".format(
+            class_name=class_name,
+        ))
+    import pandas as pd
+    data = dict()
+
+    for element in dss.ActiveClass.AllNames():
+        name = '{class_name}.{element}'.format(class_name=class_name, element=element)
+        dss.Circuit.SetActiveElement(name)
+
+        data[name] = dict()
+        for i, n in enumerate(dss.CktElement.AllPropertyNames()):
+            data[name][n] = dss.Properties.Value(str(i + 1))
+
+    return pd.DataFrame(data).T
+
+
+def getmembers(module):
+
+    for n, f in inspect.getmembers(module):
+
+        if n != 'AllNames' and n != 'Next' and n != 'First' and not (n.startswith('__') and n.endswith('__')):
+            yield n, f
+
+
+def capacitors_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Capacitors)
+
+
+def fuses_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Fuses)
+
+
+def generators_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Generators)
+
+
+def isource_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Isource)
+
+
+def lines_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Lines)
+
+
+def loadshape_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.LoadShape)
+
+
+def loads_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Loads)
+
+
+def meters_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Meters)
+
+
+def monitors_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Monitors)
+
+
+def pvsystems_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.PVsystems)
+
+
+def regcontrols_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.RegControls)
+
+
+def reclosers_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Reclosers)
+
+
+def relays_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Relays)
+
+
+def sensors_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Sensors)
+
+
+def transformers_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Transformers)
+
+
+def vsources_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.Vsources)
+
+
+def xycurves_to_dataframe(dss=None):
+    if dss is None:
+        import opendssdirect as dss
+    return to_dataframe(dss.XYCurves)
