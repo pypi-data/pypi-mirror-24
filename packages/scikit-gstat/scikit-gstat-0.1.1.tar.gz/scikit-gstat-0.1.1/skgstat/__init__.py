73b90743cc21df4f3446b61f06188f34ba9936f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:52 +0000

    Add package scikit-gstat/0.1.1/scikit-gstat-0.1.1.tar.gz

---
+++ packages/scikit-gstat/scikit-gstat-0.1.1.tar.gz/scikit-gstat-0.1.1/skgstat/__init__.py
@@ -0,0 +1,4 @@
+from .binning import binify_even_width, group_to_bin
+from .Variogram import Variogram
+from .optimize import VariogramFitter
+from .estimator import matheron, cressie, dowd, genton, minmax
