c725f2d3fdf2b102476ee110ed331e029b4358f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:23 +0000

    Add package plone.event/1.3.4/plone.event-1.3.4.tar.gz

---
+++ packages/plone.event/plone.event-1.3.4.tar.gz/plone.event-1.3.4/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '1.3.4'
+
+setup(
+    name='plone.event',
+    version=version,
+    description="Event and calendaring related tools not bound to Plone",
+    long_description=(
+        open("README.rst").read() + "\n" + open(("CHANGES.rst")).read()
+    ),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords='Plone calendar calendaring event recurring',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/plone/plone.event',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'python-dateutil',
+        'pytz',
+        'zope.component',
+        'zope.interface',
+    ],
+    extras_require={'test': [
+        'DateTime',
+        'mock',
+        'zope.configuration',
+    ], },
+)
