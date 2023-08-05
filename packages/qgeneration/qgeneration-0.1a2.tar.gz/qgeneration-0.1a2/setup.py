64221175117db6f3d08168e78ec1cec7a0bd91a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:19 +0000

    Add package qgeneration/0.1a2/qgeneration-0.1a2.tar.gz

---
+++ packages/qgeneration/qgeneration-0.1a2.tar.gz/qgeneration-0.1a2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='qgeneration',
+    version='0.1a2',
+    description='Data generation project',
+    long_description=long_description,
+    url='https://github.com/KirovVerst/qgeneration',
+    author='KirovVerst',
+    author_email='kirov.verst@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['data', 'generator', 'fixtures', 'test'],
+    packages=find_packages(exclude=["docs", "tests"]),
+)
