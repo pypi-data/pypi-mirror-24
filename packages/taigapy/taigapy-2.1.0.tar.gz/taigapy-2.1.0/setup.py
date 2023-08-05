bd7e4118947cbf950ad708f582ba6b417002e4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:25 +0000

    Add package taigapy/2.1.0/taigapy-2.1.0.tar.gz

---
+++ packages/taigapy/taigapy-2.1.0.tar.gz/taigapy-2.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import sys
+
+install_requires=['requests', 'pandas', 'boto3']
+if sys.version_info < (3, 5):
+    install_requires.append('enum34')
+
+setup(
+    name='taigapy',
+    version='2.1.0',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author="Philip Montgomery",
+    author_email="pmontgom@broadinstitute.org",
+    long_description=open('README.md').read(),
+    install_requires=install_requires
+    )
