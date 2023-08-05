8471d406672de8d0b2bb067d3806b1fe1ef84798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package fremove/1.1.0/fremove-1.1.0.tar.gz

---
+++ packages/fremove/fremove-1.1.0.tar.gz/fremove-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+				
+				name = 'fremove',
+				version = '1.1.0',
+				py_modules = ['fremove'],
+				author = 'suryapoojary',
+				author_email = 'pythonpoojary@gmail.com',
+				url = '',
+				description = 'A simple file and folder remover',
+
+
+)
