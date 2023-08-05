0c6c86c73440de8310ed4d44e7cd9cf610374c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package skyer/1.0.0/skyer-1.0.0.tar.gz

---
+++ packages/skyer/skyer-1.0.0.tar.gz/skyer-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name			= 'skyer',
+    version         = '1.0.0',
+    py_modules  	= ['skyer5217'],
+    author          = 'skyer',
+    author_email  	= '503612012@qq.com',
+    url   			= 'http://www.vhzsqm.com',
+    description     = 'My name is skyer,this is a upload test.',
+    )
