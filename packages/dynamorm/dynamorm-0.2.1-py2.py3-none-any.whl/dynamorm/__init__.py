a24ff35395db135a0f3119af5b5d3f6f312ce5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:42 +0000

    Add package dynamorm/0.2.1/dynamorm-0.2.1-py2.py3-none-any.whl

---
+++ packages/dynamorm/dynamorm-0.2.1-py2.py3-none-any.whl/dynamorm/__init__.py
@@ -0,0 +1,9 @@
+"""The base module namespace simply imports ``DynaModel`` from ``dynamorm.model`` so that you can:
+
+.. code-block:: python
+
+    from dynamorm import DynaModel
+
+"""
+from .model import DynaModel  # noqa
+from .table import Q  # noqa
