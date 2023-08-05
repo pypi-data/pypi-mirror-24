8b633cfb7a209076d43e150ef1d3e1ef8db52324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package funniestxy/0.2/funniestxy-0.2.tar.gz

---
+++ packages/funniestxy/funniestxy-0.2.tar.gz/funniestxy-0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='funniestxy',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/umeshmaheswari7/funniest',
+      author='Umesh Maheswari',
+      author_email='umeshmaheswari7@gmail.com',
+      license='Personal',
+      packages=['funniestxy'])
