b23ae2a2c630558c2de527e140ea160347764592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package techlabreactor/0.0.18/techlabreactor-0.0.18.tar.gz

---
+++ packages/techlabreactor/techlabreactor-0.0.18.tar.gz/techlabreactor-0.0.18/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+VERSION = "0.0.18"
+
+setup(
+  name='techlabreactor',
+  packages=['techlabreactor'],
+  version=VERSION,
+  description='Utility for performing replay analysis of SC2 replays',
+  author='Hugo Wainwright',
+  author_email='wainwrighthugo@gmail.com',
+  url='https://github.com/frugs/techlab-reactor',
+  keywords=['sc2', 'replay', 'sc2reader'],
+  classifiers=[],
+)
