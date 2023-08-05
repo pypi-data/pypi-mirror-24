d62d8936c02986f211238a4f7d2be8ef1993ce41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:15 +0000

    Add package bum/0.1.0/bum-0.1.0.tar.gz

---
+++ packages/bum/bum-0.1.0.tar.gz/bum-0.1.0/bum/brainz.py
@@ -0,0 +1,30 @@
+"""
+Musicbrainz related functions.
+"""
+import musicbrainzngs as mus
+
+
+def init():
+    """Initialize musicbrainz."""
+    mus.set_useragent("python-bum: A cover art daemon.",
+                      "0.0.1",
+                      "https://github.com/dylanaraps/bum")
+
+
+def get_cover(song, size=250):
+    """Download the cover art."""
+    try:
+        data = mus.search_releases(artist=song["artist"],
+                                   release=song["album"],
+                                   limit=1)
+        release_id = data["release-list"][0]["release-group"]["id"]
+        print(f"album: Using release-id: {data['release-list'][0]['id']}")
+
+        return mus.get_release_group_image_front(release_id, size=size)
+
+    except mus.NetworkError:
+        get_cover(song, size)
+
+    except mus.ResponseError:
+        print("error: Couldn't find album art for",
+              f"{song['artist']} - {song['album']}")
