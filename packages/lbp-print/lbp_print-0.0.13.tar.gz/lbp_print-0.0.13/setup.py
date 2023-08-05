9c1363e828f0e783861f3e4fca85092442682f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package lbp-print/0.0.13/lbp_print-0.0.13.tar.gz

---
+++ packages/lbp-print/lbp_print-0.0.13.tar.gz/lbp_print-0.0.13/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+about = {}
+with open(os.path.join('lbp_print', '__about__.py')) as fp:
+    exec(fp.read(), about)
+
+setup(name='lbp_print',
+      version=about['__version__'],
+      packages=find_packages(),
+      dependency_links=[
+          "git+https://github.com/lombardpress/lbppy.git@f272e44429fcbd3f19384471c9cfb3903100fb30#egg=lbppy",
+      ],
+      tests_require=[
+          'pytest==3.1.3',
+      ],
+      install_requires=[
+          'docopt==0.6.2',
+          'isodate==0.5.4',
+          'lbppy>=0.0.0',
+          'lxml==3.7.3',
+          'pyparsing==2.2.0',
+          'rdflib==4.2.2',
+          'SPARQLWrapper==1.8.0',
+          'untangle==1.1.0',
+      ],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Text Processing :: Markup :: XML',
+          'Topic :: Text Processing :: Markup :: LaTeX',
+      ],
+      entry_points={
+          'console_scripts': ['lbp_print=lbp_print.cli:main']
+      },
+      include_package_data=True,
+
+      description='Script for compiling LombardPress encoded XML documents to TeX and PDF.',
+      url='https://github.com/stenskjaer/lbp_print',
+      author='Michael Stenskjær Christensen',
+      author_email='michael.stenskjaer@gmail.com',
+      license='MIT',
+)
