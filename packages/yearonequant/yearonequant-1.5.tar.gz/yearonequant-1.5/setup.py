8bf855a734778e82625f25a3ac5ba430ab1a1fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package yearonequant/1.5/yearonequant-1.5.tar.gz

---
+++ packages/yearonequant/yearonequant-1.5.tar.gz/yearonequant-1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yearonequant',
+      version='1.5',
+      description='Think as quant, trade as quant',
+      url='http://github.com/hyqLeonardo/yearonequant',
+      author='Leonardo',
+      author_email='hyq335335@163.com',
+      license='MIT',
+      packages=['yearonequant'],
+      zip_safe=False)
