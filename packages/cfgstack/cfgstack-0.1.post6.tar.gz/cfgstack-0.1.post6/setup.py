760b3f9f09f7b95b9465030af590169b5f96b07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:36 +0000

    Add package cfgstack/0.1.post6/cfgstack-0.1.post6.tar.gz

---
+++ packages/cfgstack/cfgstack-0.1.post6.tar.gz/cfgstack-0.1.post6/setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+import versioneer
+from codecs import open
+
+setup (
+    name = "cfgstack",
+    version = versioneer.get_version (),
+    description = "Load stacks of YAML data",
+    long_description = open ("README.rst", 'r', encoding = 'utf-8').read (),
+    cmdclass = versioneer.get_cmdclass (),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        + "GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: Utilities",
+    ],
+    keywords = "logging, exceptions, callpoints, call tracing",
+    author = "J C Lawrence",
+    author_email = "claw@kanga.nu",
+    url = "https://github.com/clearclaw/cfgstack",
+    license = "LGPL v3.0",
+    test_suite = "tests",
+    packages = find_packages (exclude = ["tests",]),
+    package_data = {},
+    data_files = [],
+    zip_safe = False,
+    install_requires = [line.strip ()
+        for line in open ("requirements.txt", 'r',
+                          encoding = 'utf-8').readlines ()],
+    entry_points = {
+        "console_scripts": [
+        ],
+    },
+)
