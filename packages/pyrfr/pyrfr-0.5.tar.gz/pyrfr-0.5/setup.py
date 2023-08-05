403690195c1282184d94b6df7e4ac48033cce747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:31 +0000

    Add package pyrfr/0.5/pyrfr-0.5.tar.gz

---
+++ packages/pyrfr/pyrfr-0.5.tar.gz/pyrfr-0.5/setup.py
@@ -0,0 +1,59 @@
+from distutils.command.build import build
+from setuptools.command.install import install
+from distutils.core import setup, Extension
+
+import fnmatch
+import os
+
+
+
+
+include_files = []
+for root, dirnames, filenames in os.walk('include'):
+    for filename in filenames:
+        include_files.append(os.path.join(root, filename))
+
+
+
+class CustomInstall(install):
+	def run(self):
+		self.run_command('build_ext')
+		build.run(self)
+		self.do_egg_install()
+
+include_dirs = ['/home/sfalkner/repositories/github/random_forest_run/include', './include']
+extra_compile_args = ['-O2', '-std=c++11']
+#extra_compile_args = ['-g', '-std=c++11', '-O0']
+
+
+
+extensions = [	Extension(
+					name = '_regression',
+					sources=['pyrfr/regression.i'],
+					include_dirs = include_dirs,
+					swig_opts=['-c++', '-modern', '-features', 'nondynamic'] + ['-I{}'.format(s) for s in include_dirs],
+					extra_compile_args = extra_compile_args
+				),
+				Extension(
+					name = '_util',
+					sources=['pyrfr/util.i'],
+					include_dirs = include_dirs,
+					swig_opts=['-c++', '-modern', '-features', 'nondynamic'] + ['-I{}'.format(s) for s in include_dirs],
+					extra_compile_args = extra_compile_args
+				)
+			]
+
+setup(
+	name='pyrfr',
+	version='0.5',
+	author='Stefan Falkner',
+	author_email='sfalkner@cs.uni-freiburg.de',
+	license='Use as you wish. No guarantees whatsoever.',
+	classifiers=['Development Status :: 3 - Alpha'],
+	packages=['pyrfr'],
+	ext_modules=extensions,
+	python_requires='>=3',
+	package_data = {'pyrfr': ['docstrings.i']},
+	data_files = [('include', include_files)],
+	cmdclass={'install': CustomInstall}
+)
