9c77bc75dcddd3927e9475811391cdbd4231f1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:43 +0000

    Add package pipurge/0.1.3/pipurge-0.1.3.tar.gz

---
+++ packages/pipurge/pipurge-0.1.3.tar.gz/pipurge-0.1.3/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup
+
+with open("readme.rst", "r") as file:
+    __doc__ = file.read()
+
+setup(
+    name="pipurge",
+    version="0.1.3",
+    url="",
+    license="MIT",
+    author="Patrick Dill",
+    author_email="jamespatrickdill@gmail.com",
+    description="Uninstall every package. all of them.// now",
+    scripts=["pipurge.py", "readme.rst"],
+    long_description=__doc__,
+    include_package_data=True,
+    platforms="any",
+    install_requires=["delegator.py", "Click", "colorama"],
+    download_url="http://github.com/reshanie/pipurge/archive/master.tar.gz",
+    classifiers=[
+        # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # "Development Status :: 1 - Planning",
+        # "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        # "Development Status :: 6 - Mature",
+        # "Development Status :: 7 - Inactive",
+        "Programming Language :: Python",
+        # "Programming Language :: Python :: 2",
+        # "Programming Language :: Python :: 2.3",
+        # "Programming Language :: Python :: 2.4",
+        # "Programming Language :: Python :: 2.5",
+        # "Programming Language :: Python :: 2.6",
+        # "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Systems Administration",
+    ],
+    entry_points="""
+        [console_scripts]
+            pipurge=pipurge:purge
+    """
+)
