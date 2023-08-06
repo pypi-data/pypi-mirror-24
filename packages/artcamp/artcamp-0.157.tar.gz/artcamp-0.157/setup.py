1bbf4356b48dadb96c2027c8757fbc42f03c4651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:12 +0000

    Add package artcamp/0.157/artcamp-0.157.tar.gz

---
+++ packages/artcamp/artcamp-0.157.tar.gz/artcamp-0.157/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='artcamp',
+      version='0.157',
+      description='Query similarity between articles, orgs, and/or campaigns',
+      url='http://github.com/jwilber/artcamp',
+      author='Jared Wilber',
+      author_email='jwilber@classy.org',
+      include_package_data=True,
+      license='MIT',
+      packages=['artcamp'],
+      install_requires=[
+          'gensim',
+          'scikit-learn',
+      ],
+      zip_safe=False)
