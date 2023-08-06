de9dafeffd55a925bf890ed68843b540968ca0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:09 +0000

    Add package zschema/0.8.0/zschema-0.8.0.tar.gz

---
+++ packages/zschema/zschema-0.8.0.tar.gz/zschema-0.8.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import zschema
+
+setup(
+    name = "zschema",
+    description = "A schema language for JSON documents that allows validation and compilation into various database engines",
+    version = zschema.__version__,
+    license = zschema.__license__,
+    author = zschema.__author__,
+    author_email = zschema.__email__,
+    keywords = "python json schema bigquery elastic search",
+
+    install_requires = [
+        "python-dateutil"
+    ],
+
+    packages = [
+        "zschema"
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'zschema = zschema.__main__:main',
+        ]
+    },
+
+    tests_require = [ 'nose' ],
+    test_suite = 'nose.collector'
+)
