1831df501b5db0bb93df55a7ac3cf9694739590d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:19 +0000

    Add package outfancy/0.6.0/outfancy-0.6.0.tar.gz

---
+++ packages/outfancy/outfancy-0.6.0.tar.gz/outfancy-0.6.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+setup(
+    name = 'outfancy',
+    packages = ['outfancy'],
+    version = '0.6.0',
+    description = 'Python3 library to print tables in Terminal.',
+    author = 'Carlos A. Planchon',
+    author_email = 'bubbledoloresuruguay2@gmail.com',
+    url = 'https://github.com/carlosplanchon/outfancy',
+    download_url = 'https://github.com/carlosplanchon/outfancy/archive/0.6.0.tar.gz',
+    keywords = ['rendering', 'graphical', 'terminal'],
+    classifiers = [],
+)
