3d91f6f3c74ee9144a699549e5db3219b64022b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:01 +0000

    Add package pylint-string-spaces-checkers/0.2.3/pylint-string-spaces-checkers-0.2.3.tar.gz

---
+++ packages/pylint-string-spaces-checkers/pylint-string-spaces-checkers-0.2.3.tar.gz/pylint-string-spaces-checkers-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='pylint-string-spaces-checkers',
+    description='Additional string spaces checkers for Pylint',
+    long_description=open('README.md').read(),
+    author='ZhangBohan',
+    author_email='me@bohanzhang.com',
+    version='0.2.3',
+    download_url='https://github.com/ZhangBohan/pylint-string-spaces-checkers.git',
+    install_requires=[
+        'pylint',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ]
+)
