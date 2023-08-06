41a7994cbc648425fe4ab4b7a07cf99aca100af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:35 +0000

    Add package cuscatlan/0.0.4/cuscatlan-0.0.4.tar.gz

---
+++ packages/cuscatlan/cuscatlan-0.0.4.tar.gz/cuscatlan-0.0.4/cuscatlan/elemental.py
@@ -0,0 +1,6 @@
+def hellopipilworld():
+    return u'Niltze Cemanahuac! (Hello "World"! in Nahuatl.)'
+
+
+def custom_pearson(x, y):
+    return scipy.stats.pearsonr(x, y)[0]
---
+++ packages/cuscatlan/cuscatlan-0.0.4.tar.gz/cuscatlan-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='cuscatlan',
+      version='0.0.4',
+      description="Edwin Juarez's personal library.",
+      url='https://github.com/edjuaro/cuscatlan',
+      author='Edwin F. Juarez',
+      author_email='ejuarez@ucsd.edu',
+      license='MIT',
+      packages=['cuscatlan'],
+      zip_safe=False,
+      install_requires=[
+          'numpy',
+          'scipy',
+          'pandas',
+          'matplotlib',
+          ],
+      )
