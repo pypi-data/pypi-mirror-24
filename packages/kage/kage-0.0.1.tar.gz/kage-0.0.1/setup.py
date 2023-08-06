c29923b81ec12a7f298c6b5e25a8f227e1d9b5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:50 +0000

    Add package kage/0.0.1/kage-0.0.1.tar.gz

---
+++ packages/kage/kage-0.0.1.tar.gz/kage-0.0.1/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import ast
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with open('kage/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+
+requirements = [
+]
+
+setup(
+    name='kage',
+    version=version,
+    description="kage",
+    long_description=readme,
+    author="fate0",
+    author_email='fate0@fatezero.org',
+    url='https://github.com/fate0/kage',
+    packages=find_packages(),
+    package_dir={},
+    entry_points={
+        'console_scripts': [
+            'kage=kage.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='kage',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers'
+    ],
+)
