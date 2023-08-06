a5b304e88f0966232f77d313f59f2825a110e31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:18 +0000

    Add package molo.pwa/1.0.0/molo.pwa-1.0.0-py2-none-any.whl

---
+++ packages/molo.pwa/molo.pwa-1.0.0-py2-none-any.whl/molo/pwa/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, url
+
+from fcm_django.api.rest_framework import FCMDeviceViewSet
+from rest_framework.routers import DefaultRouter
+
+from .views import RegistrationTokenView, manifest, service_worker
+
+
+urlpatterns = patterns('', )
+
+router = DefaultRouter()
+router.register(r'devices', FCMDeviceViewSet)
+
+urlpatterns += patterns(
+    '',
+    url(
+        r'^notification_devices/$', RegistrationTokenView.as_view(),
+        name='registration-token'),
+    url('^serviceworker.js$', service_worker),
+    url('^manifest.json$', manifest),
+)
