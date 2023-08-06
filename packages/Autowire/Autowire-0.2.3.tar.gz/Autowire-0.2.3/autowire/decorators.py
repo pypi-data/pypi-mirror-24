fe7e2cec5c82aa8f56fba69e3a63545343e0cacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:35 +0000

    Add package Autowire/0.2.3/Autowire-0.2.3.tar.gz

---
+++ packages/Autowire/Autowire-0.2.3.tar.gz/Autowire-0.2.3/autowire/decorators.py
@@ -0,0 +1,24 @@
+"""
+autowire.decorators
+===================
+
+Decorators for autowire.
+
+"""
+from . import helpers
+from .base import BaseResource
+
+
+def autowired(*dependencies: BaseResource):
+    """
+    Convert a plain function to resource implementation with dependencies.
+    It changes interface of decorated function to implementation interface.
+    """
+    def decorator(fn):
+        return helpers.autowired(fn, *dependencies)
+    return decorator
+
+
+# It already has decorator interface
+shared = helpers.shared
+globally_shared = helpers.globally_shared
