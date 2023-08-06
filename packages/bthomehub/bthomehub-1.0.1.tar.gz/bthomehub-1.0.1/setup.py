25437cefbcf261be89c088f0da1fce695e1c2af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:25 +0000

    Add package bthomehub/1.0.1/bthomehub-1.0.1.tar.gz

---
+++ packages/bthomehub/bthomehub-1.0.1.tar.gz/bthomehub-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='bthomehub',
+      packages=["bthomehub"],
+      version='1.0.1',
+      description='A Python client that can interact with BT Home Hub routers.',
+      url='https://github.com/onegambler/bthomehub_client',
+      download_url='https://github.com/onegambler/bthomehub_client/archive/1.0.tar.gz',
+      author='Roberto Magale',
+      author_email='onegambler@outlook.com',
+      license='UNLICENSE',
+      install_requires=[
+          'requests'
+      ],
+      keywords=['bt home hub', 'devices list'],
+      zip_safe=False)
