05e0fdd595d83404beae80fb9fffcda91252bed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:56 +0000

    Add package LinuxDrop/0.5/LinuxDrop-0.5.tar.gz

---
+++ packages/LinuxDrop/LinuxDrop-0.5.tar.gz/LinuxDrop-0.5/sender/send.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+from os import getcwd
+
+from flask import Flask, send_from_directory, request
+
+app = Flask(__name__, static_url_path='')
+
+f_name = ''
+
+
+@app.route('/')
+def get_file():
+    return send_from_directory(getcwd(), f_name)
+
+
+@app.route('/end')
+def end_transmission():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    return "Server shutting down..."
+
+
+@app.route('/name')
+def get_file_name():
+    return f_name
+
+
+def start_server(file_name):
+    global f_name
+    f_name = file_name
+    app.run("0.0.0.0", "1921")
---
+++ packages/LinuxDrop/LinuxDrop-0.5.tar.gz/LinuxDrop-0.5/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='LinuxDrop',
+    version='0.5',
+    packages=['sender', 'receiver', ],
+    py_modules=['lidrop'],
+    install_requires=[
+        'netifaces',
+        'flask',
+        'Click',
+        'urllib2',
+    ],
+    entry_points='''
+        [console_scripts]
+        lidrop=lidrop:run
+    ''',
+    url='https://github.com/Livin21/LinuxDrop',
+    license='MIT',
+    author='livin',
+    author_email='livinmathew99@gmail.com',
+    description='Airdrop alternative for Linux <3'
+)
