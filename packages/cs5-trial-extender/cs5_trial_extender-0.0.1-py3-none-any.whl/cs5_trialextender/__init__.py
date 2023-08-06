b4a9f705cc9f2aea106f0bca630a3107d04d2e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:41 +0000

    Add package cs5-trial-extender/0.0.1/cs5_trial_extender-0.0.1-py3-none-any.whl

---
+++ packages/cs5-trial-extender/cs5_trial_extender-0.0.1-py3-none-any.whl/cs5_trialextender/__init__.py
@@ -0,0 +1,7 @@
+'''
+- Based on the blog POST: https://iamjamazing.wordpress.com/2016/07/01/how-to-extend-photoshop-cs5-trial/
+- Replace XML values to bypass trial of a specific software.
+- This module was developed for educational purpose. Do not use it to cheat!
+'''
+
+__version__ = '0.0.1'
