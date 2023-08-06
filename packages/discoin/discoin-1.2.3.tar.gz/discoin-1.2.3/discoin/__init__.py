b34ae418202b38e1ed2f3624ee64c59d7d6b3fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:13 +0000

    Add package discoin/1.2.3/discoin-1.2.3.tar.gz

---
+++ packages/discoin/discoin-1.2.3.tar.gz/discoin-1.2.3/discoin/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Discoin API Wrapper
+~~~~~~~~~~~~~~~~~~~
+An API wrapper for the Discoin API.
+
+"""
+
+__title__ = 'discoin'
+__author__ = 'Joseph Banks'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017-Present Joseph Banks'
+__version__ = '1.2.3'
+
+from .errors import DiscoinError
+from .classes import Transaction
+from .client import Discoin
