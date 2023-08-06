381e0923c1c06c43f13a9a6361817cb6bd3bfa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:06 +0000

    Add package python-smartqq-client/0.4.2/python-smartqq-client-0.4.2.tar.gz

---
+++ packages/python-smartqq-client/python-smartqq-client-0.4.2.tar.gz/python-smartqq-client-0.4.2/pyqqclient/LoginFinalizeHandler.py
@@ -0,0 +1,10 @@
+from requests import Response
+from .LoginStepHandler import LoginStepHandler
+from .OnlineChecker import OnlineChecker
+
+
+class LoginFinalizeHandler(LoginStepHandler):
+    def next_step(self, accumulated, last_response: Response) -> ({}, Response):
+        del accumulated["login_barcode"]
+        del accumulated["login_success_url"]
+        return accumulated, OnlineChecker.check_online(accumulated, self.session)
