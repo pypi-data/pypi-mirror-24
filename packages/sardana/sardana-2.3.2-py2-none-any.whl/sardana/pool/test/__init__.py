e32727f6f1648633ab11240f8bbc04fbdeafc445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:24 +0000

    Add package sardana/2.3.2/sardana-2.3.2-py2-none-any.whl

---
+++ packages/sardana/sardana-2.3.2-py2-none-any.whl/sardana/pool/test/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+##############################################################################
+##
+# This file is part of Sardana
+##
+# http://www.sardana-controls.org/
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Sardana is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Sardana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+"""This is the pool test module"""
+
+from .fake import *
+from .helper import *
+from .dummyconfs import *
+from .base import *
+from .test_acquisition import *
+from .test_ctacquisition import *
+from .test_measurementgroup import *
+from .test_poolcontroller import *
+from .test_poolcontrollermanager import *
+from .test_poolcountertimer import *
+from .test_poolsynchronization import *
+from .test_synchronization import *
