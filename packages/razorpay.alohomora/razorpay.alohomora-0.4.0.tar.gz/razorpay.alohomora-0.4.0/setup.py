e4f88651e7a62ed7c903270e148f7e5eab4d784a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:17 +0000

    Add package razorpay.alohomora/0.4.0/razorpay.alohomora-0.4.0.tar.gz

---
+++ packages/razorpay.alohomora/razorpay.alohomora-0.4.0.tar.gz/razorpay.alohomora-0.4.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='razorpay.alohomora',
+      version='0.4.0',
+      description='Secret distribution tool, written as a wrapper on credstash',
+      url='http://github.com/razorpay/alohomora',
+      author='Team Razorpay',
+      author_email='developers@razorpay.com',
+      keywords=["credstash", "ansible", "secrets", "jinja"],
+      license='MIT',
+      long_description=readme(),
+      packages=['razorpay', 'razorpay.alohomora'],
+      install_requires=[
+          'credstash',
+          'click',
+          'jinja2',
+          'botocore'
+      ],
+      entry_points={
+          'console_scripts': [
+              'alohomora = razorpay.alohomora.cli:cli'
+          ]
+      },
+      package_dir={'razorpay.alohomora':  'razorpay/alohomora'},
+      include_package_data=True,
+      zip_safe=False
+      )
