17906797902ae88db1d5a25bd5dd6c0444f3c0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package pyLinguist/0.1.1b0/pyLinguist-0.1.1b0.tar.gz

---
+++ packages/pyLinguist/pyLinguist-0.1.1b0.tar.gz/pyLinguist-0.1.1b0/pyLinguist/__init__.py
@@ -0,0 +1,34 @@
+"""
+    This module implements Yandex Translate and Dictionary APIs for Python 3.3+
+
+    Powered by Yandex.Translate — http://translate.yandex.com/
+    Powered by Yandex.Dictionary — https://tech.yandex.com/dictionary/
+"""
+
+from .config import logger
+
+from .baseTypes import YaTranslateException, _YaAPIHandler
+
+
+def Translator(*args, **kwargs):
+    from .Translate import Translator
+    return Translator(*args, **kwargs)
+
+
+def Dictionary(*args, **kwargs):
+    from .Vocabulary import Dictionary
+    return Dictionary(*args, **kwargs)
+
+
+def Predictor(*args, **kwargs):
+    from .Prediction import Predictor
+    return Predictor(*args, **kwargs)
+
+
+def Speller(*args, **kwargs):
+    from .Vocabulary import Speller
+    return Speller(*args, **kwargs)
+
+
+__all__ = ["Dictionary", "Translator", "YaTranslateException",
+           "Predictor", "Speller"]
