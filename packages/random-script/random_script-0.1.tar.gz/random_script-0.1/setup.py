9caa104ed9bff2a4e7cb4a99d83082288d618377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:56 +0000

    Add package random-script/0.1/random_script-0.1.tar.gz

---
+++ packages/random-script/random_script-0.1.tar.gz/random_script-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='random_script',
+    version='0.1',
+    scripts=['random_script'],
+	author = 'Ahmed Maher',
+	author_email = 'ahmed_maher1992@hotmail.com'
+)
