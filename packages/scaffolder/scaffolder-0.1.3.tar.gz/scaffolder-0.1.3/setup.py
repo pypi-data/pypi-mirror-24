f7cb08ceba7a2f71c9db6f4cd8d3fc7464cea79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:39 +0000

    Add package scaffolder/0.1.3/scaffolder-0.1.3.tar.gz

---
+++ packages/scaffolder/scaffolder-0.1.3.tar.gz/scaffolder-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "scaffolder",
+    version = "0.1.3",
+    author = "Goran Angelkovski",
+    author_email = "goran@scaffolder.io",
+    description = "High level project and application modeling tool based on the rules of Scaffolder DSL which utilizes Scaffolder aPaaS to generate all source code, configuration and testing files.",
+    keywords = ['code generator', 'scaffolding', 'automatic coding'],
+    license = "",
+    url = "https://scaffoder.io",
+    scripts = ['scaffolder'],
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Code Generators'
+    ],
+)
