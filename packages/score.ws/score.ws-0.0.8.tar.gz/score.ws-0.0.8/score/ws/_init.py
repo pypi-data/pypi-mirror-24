25dfb3428c9ed8ad62ffaa466ed5d313d3c9c76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:44 +0000

    Add package score.ws/0.0.8/score.ws-0.0.8.tar.gz

---
+++ packages/score.ws/score.ws-0.0.8.tar.gz/score.ws-0.0.8/score/ws/_init.py
@@ -0,0 +1,66 @@
+# Copyright © 2015-2017 STRG.AT GmbH, Vienna, Austria
+#
+# This file is part of the The SCORE Framework.
+#
+# The SCORE Framework and all its parts are free software: you can redistribute
+# them and/or modify them under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation which is in the
+# file named COPYING.LESSER.txt.
+#
+# The SCORE Framework and all its parts are distributed without any WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. For more details see the GNU Lesser General Public
+# License.
+#
+# If you have not received a copy of the GNU Lesser General Public License see
+# http://www.gnu.org/licenses/.
+#
+# The License-Agreement realised between you as Licensee and STRG.AT GmbH as
+# Licenser including the issue of its valid conclusion and its pre- and
+# post-contractual effects is governed by the laws of Austria. Any disputes
+# concerning this License-Agreement including the issue of its valid conclusion
+# and its pre- and post-contractual effects are exclusively decided by the
+# competent court, in whose district STRG.AT GmbH has its registered seat, at
+# the discretion of STRG.AT GmbH also the competent court, in whose district the
+# Licensee has his registered seat, an establishment or assets.
+
+from score.init import ConfiguredModule, parse_time_interval, parse_bool
+
+
+defaults = {
+    'serve.ip': '0.0.0.0',
+    'serve.port': 8081,
+    'stop_timeout': None,
+    'reuse_port': False,
+}
+
+
+def init(confdict, ctx):
+    """
+    Initializes this module acoording to :ref:`our module initialization
+    guidelines <module_initialization>` with the following configuration keys:
+    """
+    conf = dict(defaults.items())
+    conf.update(confdict)
+    host = conf['serve.ip']
+    port = int(conf['serve.port'])
+    stop_timeout = conf['stop_timeout']
+    if stop_timeout is not None:
+        stop_timeout = parse_time_interval(stop_timeout)
+    reuse_port = parse_bool(conf['reuse_port'])
+    return ConfiguredWsModule(ctx, host, port, stop_timeout, reuse_port)
+
+
+class ConfiguredWsModule(ConfiguredModule):
+    """
+    This module's :class:`configuration class <score.init.ConfiguredModule>`.
+    """
+
+    def __init__(self, ctx, host, port, stop_timeout, reuse_port):
+        import score.ws
+        super().__init__(score.ws)
+        self.ctx = ctx
+        self.host = host
+        self.port = port
+        self.stop_timeout = stop_timeout
+        self.reuse_port = reuse_port
