e335529a334e7e8a6cb726b6a3cc35357f0effb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:09 +0000

    Add package gtmanipulator/0.2.4/gtmanipulator-0.2.4.tar.gz

---
+++ packages/gtmanipulator/gtmanipulator-0.2.4.tar.gz/gtmanipulator-0.2.4/gtmanipulator/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""This module contains classes and methods intended for use on Illumina iScan
+beadarray data post GenomeStudio processing. Additionally, a GUI 
+implementation of the gtmanipulator module is also available. 
+The link is provided below.
+
+This module may be used freely in part or whole as long as credit is given.
+Please see the 'Credit' section for details.
+
+Credit:
+    Author - Travis Couture
+    Email - travis.m.couture@gmail.com
+
+Links:
+
+"""
+
+import gtmanipulator
