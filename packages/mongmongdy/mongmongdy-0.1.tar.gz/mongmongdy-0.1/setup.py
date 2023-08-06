8959f0843913becb26698dc9bd2d49b2eddb7666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:19 +0000

    Add package mongmongdy/0.1/mongmongdy-0.1.tar.gz

---
+++ packages/mongmongdy/mongmongdy-0.1.tar.gz/mongmongdy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(name="mongmongdy",
+      version="0.1",
+      description="optimizer",
+      author="DoyeobYeo",
+      author_email='greatday327@gmail.com',
+      platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+      license="BSD",
+      url="https://github.com/DoyeobYeo/MyProject",
+      packages=find_packages(),
+      )
