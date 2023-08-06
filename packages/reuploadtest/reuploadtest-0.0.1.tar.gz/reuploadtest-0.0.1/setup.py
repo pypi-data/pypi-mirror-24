d5e4d04f63b42a03c38da2c53f1a9929dfd75fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:04 +0000

    Add package reuploadtest/0.0.1/reuploadtest-0.0.1.tar.gz

---
+++ packages/reuploadtest/reuploadtest-0.0.1.tar.gz/reuploadtest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='reuploadtest',
+    version='0.0.1',
+    description='',
+    author='',
+    author_email='',
+    packages=find_packages(),
+)
