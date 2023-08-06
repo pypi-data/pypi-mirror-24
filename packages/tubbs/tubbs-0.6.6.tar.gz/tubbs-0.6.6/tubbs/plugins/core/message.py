0d9cfdd81c31421eb8e2f74a0e5bd95ab30fada4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:23 +0000

    Add package tubbs/0.6.6/tubbs-0.6.6.tar.gz

---
+++ packages/tubbs/tubbs-0.6.6.tar.gz/tubbs-0.6.6/tubbs/plugins/core/message.py
@@ -0,0 +1,15 @@
+from ribosome.machine import message, json_message
+
+StageI = message('StageI')
+AObj = message('AObj', 'ident')
+IObj = message('IObj', 'ident')
+AObjRule = message('AObj', 'rule')
+IObjRule = message('IObj', 'rule')
+Select = message('Select', 'parser', 'tpe', 'ident')
+Format = message('Format', 'parser', 'range')
+FormatRange = json_message('FormatRange')
+FormatAt = json_message('FormatAt', 'line')
+FormatExpr = json_message('FormatExpr', 'line', 'count')
+
+__all__ = ('StageI', 'AObj', 'IObj', 'AObjRule', 'IObjRule', 'Select', 'Format', 'FormatRange', 'FormatAt',
+           'FormatExpr')
