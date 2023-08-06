9664ce357da6c7e1694ea56c737d445699dc289f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:11 +0000

    Add package oglhclient/0.1.8.7.3/oglhclient-0.1.8.7.3.tar.gz

---
+++ packages/oglhclient/oglhclient-0.1.8.7.3.tar.gz/oglhclient-0.1.8.7.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'oglhclient',
+  #packages = ['oglhclient'],
+  version = '0.1.8.7.3',
+  description = 'A client for Lighthouse API',
+  author = 'Lighthouse Team',
+  author_email = 'engineering@opengear.com',
+  url = 'https://github.com/thiagolcmelo/oglhclient',
+  download_url = 'https://github.com/thiagolcmelo/oglhclient/archive/0.1.8.tar.gz',
+  keywords = ['api', 'opengear', 'lighthouse'],
+  classifiers = [],
+  install_requires = ['pyyaml'],
+  packages=find_packages('oglhclient'),
+  package_dir={'':'oglhclient'},
+  package_data={'': ['*.raml'],
+                'oglhclient':['*.raml'],}
+  #package_dir={'':'oglhclient'},
+  #include_package_data=True,
+)
