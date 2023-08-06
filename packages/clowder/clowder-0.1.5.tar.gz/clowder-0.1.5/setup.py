b68f6cd3eb7d662ebb08983a5172cda22b5c88bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:26 +0000

    Add package clowder/0.1.5/clowder-0.1.5.tar.gz

---
+++ packages/clowder/clowder-0.1.5.tar.gz/clowder-0.1.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+version = '0.1.5'
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='clowder',
+    version=version,
+    description = 'Client for the Clowder monitoring server',
+    long_description = long_description,
+    author = 'Keith Hackbarth',
+    author_email = 'keith@clowder.io',
+    license = 'LICENCE.txt',
+    url = 'https://github.com/keithhackbarth/clowder_client',
+    py_modules = ['clowder'],
+    download_url = 'https://github.com/keithhackbarth/clowder_client/archive/master.zip',
+    platforms='Cross-platform',
+    include_package_data=True,
+    zip_safe=False,
+    keywords='clowder',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
