34f61a84d0a3a4bab970f7e33c794560eba0136d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:20 +0000

    Add package htmldo/0.1/htmldo-0.1.tar.gz

---
+++ packages/htmldo/htmldo-0.1.tar.gz/htmldo-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='htmldo', 
+    scripts=['htmldo'],                  # The name of your scipt, and also the command you'll be using for calling it
+    install_requires = [
+        'beautifulsoup4'
+    ],
+    version='0.1',
+
+    description = '',
+    author = 'franklynchen',
+    author_email = 'test@test.test',
+    url = '',
+    download_url = '',
+    keywords = [''],
+    classifiers = [],
+)
