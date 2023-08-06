ca580e285f23f4e65d01604cba05178cd0c145c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package pyhiq/0.0.1/pyhiq-0.0.1.tar.gz

---
+++ packages/pyhiq/pyhiq-0.0.1.tar.gz/pyhiq-0.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyhiq',
+    version='0.0.1',
+    description="Hi queue",
+    long_description='',
+    author="fate0",
+    author_email='fate0@fatezero.org',
+    url='https://github.com/fate0/pyhiq',
+    packages=find_packages(),
+    package_dir={},
+    include_package_data=True,
+    install_requires=[],
+    license="BSD license",
+    zip_safe=False,
+    keywords='hiq',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
