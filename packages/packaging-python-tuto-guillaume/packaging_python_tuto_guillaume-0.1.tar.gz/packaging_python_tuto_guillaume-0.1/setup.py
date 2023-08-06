97ec2e0b3376de68e2d2a436f8d3677af7530e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:56 +0000

    Add package packaging-python-tuto-guillaume/0.1/packaging_python_tuto_guillaume-0.1.tar.gz

---
+++ packages/packaging-python-tuto-guillaume/packaging_python_tuto_guillaume-0.1.tar.gz/packaging_python_tuto_guillaume-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='packaging_python_tuto_guillaume',
+      version='0.1',
+      description='A random lib to test packaging',
+      url='https://bitbucket.org/guillaumeguillet/packaging_python_tuto_guillaume',
+      author='Guillaume GUILLET',
+      author_email='guillaume.guillet@ripplemotion.fr',
+      packages=['packaging_python_tuto_guillaume'],
+      zip_safe=False)
