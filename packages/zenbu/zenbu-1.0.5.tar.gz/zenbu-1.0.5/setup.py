a3762847a0ad3d5266b2a4f5215143218b74eeda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:55 +0000

    Add package zenbu/1.0.5/zenbu-1.0.5.tar.gz

---
+++ packages/zenbu/zenbu-1.0.5.tar.gz/zenbu-1.0.5/setup.py
@@ -0,0 +1,49 @@
+import codecs
+
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='zenbu',
+    version='1.0.5',
+    description='Jinja2 + YAML based config templater.',
+    long_description=long_description,
+    url='https://github.com/metakirby5/zenbu',
+    author='Ethan Chan',
+    author_email='metakirby5@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='zenbu config templater jinja2 yaml',
+    py_modules=['zenbu'],
+    install_requires=[
+        'argcomplete',
+        'colorlog',
+        'Jinja2',
+        'PyYAML',
+        'termcolor',
+        'watchdog',
+    ],
+    entry_points={
+        'console_scripts': [
+            'zenbu=zenbu:main',
+        ],
+    },
+)
+
