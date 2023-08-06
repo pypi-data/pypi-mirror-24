3f0e5e8beebe8f7acb87de8a2d91333282809372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:42 +0000

    Add package qiniuManager/1.3.2/qiniuManager-1.3.2.tar.gz

---
+++ packages/qiniuManager/qiniuManager-1.3.2.tar.gz/qiniuManager-1.3.2/setup.py
@@ -0,0 +1,39 @@
+# coding=utf8
+import sys
+from setuptools import setup
+reload(sys)
+sys.setdefaultencoding('utf8')
+__author__ = 'hellflame'
+__version__ = '1.3.2'
+
+setup(
+    name='qiniuManager',
+    version=__version__,
+    keywords=('qiniu', 'qiniu manager'),
+    description="终端管理七牛云空间",
+    license="MIT",
+    author=__author__,
+    author_email='hellflamedly@gmail.com',
+    url='https://github.com/hellflame/qiniu_manager',
+    packages=[
+        'qiniuManager'
+    ],
+    platforms="UNIX like",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        'Programming Language :: Python :: 2.7'
+    ],
+    entry_points={
+        'console_scripts': [
+            'qiniu=qiniuManager.run:main'
+        ]
+    }
+)
+
