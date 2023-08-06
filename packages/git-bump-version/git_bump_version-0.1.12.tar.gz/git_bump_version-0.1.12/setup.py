497ff0dc2901fce977712674e32502653d0376cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:01 +0000

    Add package git-bump-version/0.1.12/git_bump_version-0.1.12.tar.gz

---
+++ packages/git-bump-version/git_bump_version-0.1.12.tar.gz/git_bump_version-0.1.12/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'git_bump_version',
+  packages = ['git_bump_version'], # this must be the same as the name above
+  version = '0.1.12',
+  description = 'Automatically bumps version based on last tag and current branch',
+  author = 'Nathan Grubb',
+  author_email = 'mrnathangrubb@gmail.com',
+  url = 'https://github.com/silent-snowman/git_bump_version',
+  download_url = 'https://github.com/silent-snowman/git_bump_version/archive/0.1.12.tar.gz',
+  keywords = ['git', 'tag', 'version'],
+  classifiers = [],
+  entry_points={
+    'console_scripts' : [
+      'git_bump_version = git_bump_version.__main__:main'
+    ]},
+  install_requires=[
+    'GitPython',
+  ],
+)
