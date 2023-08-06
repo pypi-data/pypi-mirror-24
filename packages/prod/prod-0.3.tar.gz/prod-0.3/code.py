24f51fb8606e02cf1eba358ad7f8b53efb1188a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:01 +0000

    Add package prod/0.3/prod-0.3.tar.gz

---
+++ packages/prod/prod-0.3.tar.gz/prod-0.3/code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 09:44:22 2017
+
+@author: Whackamadoodle3000
+"""
+class u:
+    def ct(a_list_of_numbers):
+        product=1
+        for elem in a_list_of_numbers:
+            product=product*elem
+        return product
