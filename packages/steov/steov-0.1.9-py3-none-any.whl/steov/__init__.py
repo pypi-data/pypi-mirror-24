7cdb0a4530129d77ced977fd3b74aaa8a2b2c8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:53 +0000

    Add package steov/0.1.9/steov-0.1.9-py3-none-any.whl

---
+++ packages/steov/steov-0.1.9-py3-none-any.whl/steov/__init__.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+class Anon:
+    def __init__ (self, *dict_args, **dict_kwargs):
+        self.__dict__.update(dict(*dict_args, **dict_kwargs))
+
+    def __repr__ (self):
+        t = type(self)
+        return t.__module__ + "." + t.__name__ + "(" + repr(self.__dict__) + ")"
+
+# http://stackoverflow.com/questions/6086976/how-to-get-a-complete-exception-stack-trace-in-python
+def format_exc (additional=0, as_list=False):
+    import traceback
+    exception_list = traceback.format_stack()[:-(2+additional)]
+    ertype, ervalue, tb = sys.exc_info()
+    exception_list.extend(traceback.format_tb(tb))
+    exception_list.extend(traceback.format_exception_only(ertype, ervalue))
+    exception_list.insert(0, "Traceback (most recent call last):\n")
+    exception_str = "".join(exception_list)
+    if as_list:
+        return exception_str.split(os.linesep)[:-1]
+    else:
+        return exception_str
+
+def noop (*args, **kwargs):
+    pass
+
+def identity (self):
+    return self
+iden = identity
+
+def fread (file):
+    with open(file, "rb") as fp:
+        return fp.read()
+def fwrite (file, data):
+    with open(file, "wb") as fp:
+        return fp.write(data)
+def fappend (file, data):
+    with open(file, "ab") as fp:
+        return fp.write(data)
