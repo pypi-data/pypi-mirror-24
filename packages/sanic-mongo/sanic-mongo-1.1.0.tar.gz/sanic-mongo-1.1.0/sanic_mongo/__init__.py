2de661a981a07cb703348e296911e188a38157b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:29 +0000

    Add package sanic-mongo/1.1.0/sanic-mongo-1.1.0.tar.gz

---
+++ packages/sanic-mongo/sanic-mongo-1.1.0.tar.gz/sanic-mongo-1.1.0/sanic_mongo/__init__.py
@@ -0,0 +1,29 @@
+# @Author: Huang Sizhe
+# @Date:   08-Apr-2017
+# @Email:  hsz1273327@gmail.com
+# @Last modified by:   huangsizhe
+# @Last modified time: 08-Apr-2017
+# @License: Apache License 2.0
+__all__=["Mongo","MongoLogger","GridFS"]
+
+from sanic_mongo.mongo import Core as MongoCore
+from sanic_mongo.logger import Core as MongoLoggerCore
+from sanic_mongo.gridfs import Core as GridFSCore
+class Mongo(MongoCore):
+    def __init__(self, uri=None):
+        super().__init__(uri)
+
+class GridFS(GridFSCore):
+    def __init__(self,uri=None,bucket_name="fs",chunk_size_bytes= 261120,write_concern=None,read_preference=None):
+        super().__init__(uri,bucket_name,chunk_size_bytes,write_concern,read_preference)
+
+class MongoLogger(MongoLoggerCore):
+    def __init__(self, uri=None,session_in_cockie='session',
+            info_options={'size': 104857600, 'capped': False},
+            warming_options={'size': 104857600, 'capped': False},
+            error_options={'size': 104857600, 'capped': False}):
+        super().__init__(uri=uri,
+                session_in_cockie=session_in_cockie,
+                info_options=info_options,
+                warming_options=warming_options,
+                error_options=error_options)
---
+++ packages/sanic-mongo/sanic-mongo-1.1.0.tar.gz/sanic-mongo-1.1.0/sanic_mongo/mongo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""
+@Author: Huang Sizhe <huangsizhe>
+@Date:   08-Apr-2017
+@Email:  hsz1273327@gmail.com
+# @Last modified by:   huangsizhe
+# @Last modified time: 08-Apr-2017
+@License: Apache License 2.0
+@Description:
+"""
+
+__all__ = ["Core"]
+
+from sanic.log import log
+
+from sanic_mongo.standalone import MongoConnection
+from sanic_mongo.base import Base
+
+
+class Core(Base):
+    DBS = {}
+
+    def __init__(self, uri=None):
+        super().__init__(uri)
+        self.db = None
+
+    def init_app(self, app):
+        """绑定app
+        """
+        if not self.uri:
+            if app.config.MONGO_URI:
+                self.uri = app.config.MONGO_URI
+            else:
+                raise AssertionError("need a db url")
+
+        @app.listener("before_server_start")
+        async def init_mongo_connection(app, loop):
+            mongo = MongoConnection(self.uri, ioloop=loop)
+            Core.DBS[self.uri] = mongo.db
+            self.db = mongo.db
+
+        @app.listener("before_server_stop")
+        async def sub_close(app, loop):
+            log.info("mongo connection {numbr}".format(numbr=len(Core.DBS)))
+            self.db.client.close
+            log.info("mongo connection closed")
+
+        if "extensions" not in app.__dir__():
+            app.extensions = {}
+        app.extensions['SanicMongo'] = self
+        return self
