6bb7ba565894ad504096369a806749b279a9457b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:33 +0000

    Add package FolderRename/1.1/FolderRename-1.1.tar.gz

---
+++ packages/FolderRename/FolderRename-1.1.tar.gz/FolderRename-1.1/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='FolderRename',
+    version='1.1',
+    description='A sample Python project to rename folders.',
+    long_description=long_description,
+    url='https://github.com/pypa/sampleproject',
+    author='Akhilraj Rajkumar',
+    author_email='akhilr46@gmail.com',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='sample folder rename for development',
+    py_modules=['folder_rename'],
+    install_requires=[
+        'Click',
+    ],
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    entry_points='''
+        [console_scripts]
+        FolderRename=folder_rename:cli
+    '''
+)
