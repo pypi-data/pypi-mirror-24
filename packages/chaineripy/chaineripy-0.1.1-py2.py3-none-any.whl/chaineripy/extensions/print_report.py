d5b4b2612af09cb7cebb3ec061e28596ca03a7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:00 +0000

    Add package chaineripy/0.1.1/chaineripy-0.1.1-py2.py3-none-any.whl

---
+++ packages/chaineripy/chaineripy-0.1.1-py2.py3-none-any.whl/chaineripy/extensions/print_report.py
@@ -0,0 +1,84 @@
+from copy import copy
+
+from IPython.core.display import display
+from ipywidgets import HTML
+from pandas import DataFrame
+
+import chainer
+from chainer.training import extension
+from chainer.training.extensions import log_report as log_report_module
+
+
+class PrintReport(extension.Extension):
+
+    """Trainer extension to print the accumulated results.
+    This extension uses the log accumulated by a :class:`LogReport` extension
+    to print specified entries of the log in a human-readable format.
+    Args:
+        entries (list of str): List of keys of observations to print.
+        log_report (str or LogReport): Log report to accumulate the
+            observations. This is either the name of a LogReport extensions
+            registered to the trainer, or a LogReport instance to use
+            internally.
+        out: Stream to print the bar. Standard output is used by default.
+    """
+
+    def __init__(self, entries, log_report='LogReport'):
+        self._entries = entries
+        self._log_report = log_report
+
+        self._default_row = dict((e, None) for e in entries)
+        self._widget = HTML()
+        self.count = 0
+        self.update([])
+
+    def initialize(self, trainer):
+        display(self._widget)
+
+    def __call__(self, trainer):
+        if chainer.__version__[0] == '1':
+            if not hasattr(self, '_initialize_called_attr'):
+                self.initialize(trainer)
+                self._initialize_called_attr = None
+
+        log_report = self._log_report
+        if isinstance(log_report, str):
+            log_report = trainer.get_extension(log_report)
+        elif isinstance(log_report, log_report_module.LogReport):
+            log_report(trainer)  # update the log report
+        else:
+            raise TypeError('log report has a wrong type %s' %
+                            type(log_report))
+
+        self.update(log_report.log)
+
+    @property
+    def widget(self):
+        return self._widget
+
+    def update(self, log):
+        df = DataFrame(columns=self._entries)
+
+        for stat in log:
+            stat = copy(stat)
+            stat['epoch'] = int(stat['epoch'])
+            stat['iteration'] = int(stat['iteration'])
+
+            d = {}
+            for k in self._entries:
+                if k in stat:
+                    d[k] = stat[k]
+                else:
+                    d[k] = None
+            df = df.append(d, ignore_index=True)
+
+        if 'epoch' in df:
+            df['epoch'] = df['epoch'].astype(int)
+        if 'iteration' in df:
+            df['iteration'] = df['iteration'].astype(int)
+        self._widget.value = df.to_html(index=False, na_rep='')
+
+    def serialize(self, serializer):
+        log_report = self._log_report
+        if isinstance(log_report, log_report_module.LogReport):
+            log_report.serialize(serializer['_log_report'])
---
+++ packages/chaineripy/chaineripy-0.1.1-py2.py3-none-any.whl/chaineripy/extensions/progress_bar.py
@@ -0,0 +1,144 @@
+from __future__ import division
+import datetime
+import time
+
+from IPython.core.display import display
+from ipywidgets import FloatProgress, HBox, HTML, VBox
+
+import chainer
+from chainer.training import extension
+from chainer.training import trigger
+
+
+class ProgressBar(extension.Extension):
+
+    """Trainer extension to print a progress bar and recent training status.
+    This extension prints a progress bar at every call. It watches the current
+    iteration and epoch to print the bar.
+    Args:
+        training_length (tuple): Length of whole training. It consists of an
+            integer and either ``'epoch'`` or ``'iteration'``. If this value is
+            omitted and the stop trigger of the trainer is
+            :class:`IntervalTrigger`, this extension uses its attributes to
+            determine the length of the training.
+        update_interval (int): Number of iterations to skip printing the
+            progress bar.
+        bar_length (int): Length of the progress bar in characters.
+        out: Stream to print the bar. Standard output is used by default.
+    """
+
+    def __init__(self, training_length=None, update_interval=100,
+                 bar_length=50):
+        self._training_length = training_length
+        if training_length is not None:
+            self._init_status_template()
+        self._update_interval = update_interval
+        self._recent_timing = []
+
+        self._total_bar = FloatProgress(description='total',
+                                        min=0, max=1, value=0,
+                                        bar_style='info')
+        self._total_html = HTML()
+        self._epoch_bar = FloatProgress(description='this epoch',
+                                        min=0, max=1, value=0,
+                                        bar_style='info')
+        self._epoch_html = HTML()
+        self._status_html = HTML()
+
+        self._widget = VBox([HBox([self._total_bar, self._total_html]),
+                             HBox([self._epoch_bar, self._epoch_html]),
+                             self._status_html])
+
+    def initialize(self, trainer):
+        if self._training_length is None:
+            t = trainer.stop_trigger
+            if not isinstance(t, trigger.IntervalTrigger):
+                raise TypeError(
+                    'cannot retrieve the training length from %s' % type(t))
+            self._training_length = t.period, t.unit
+            self._init_status_template()
+
+        updater = trainer.updater
+        self.update(updater.iteration, updater.epoch_detail)
+        display(self._widget)
+
+    def __call__(self, trainer):
+        if chainer.__version__[0] == '1':
+            if not hasattr(self, '_initialize_called_attr'):
+                self.initialize(trainer)
+                self._initialize_called_attr = None
+
+        length, unit = self._training_length
+
+        updater = trainer.updater
+        iteration, epoch_detail = updater.iteration, updater.epoch_detail
+
+        if unit == 'iteration':
+            is_finished = iteration == length
+        else:
+            is_finished = epoch_detail == length
+
+        if iteration % self._update_interval == 0 or is_finished:
+            self.update(iteration, epoch_detail)
+
+    def finalize(self):
+        if self._total_bar.value != 1:
+            self._total_bar.bar_style = 'warning'
+            self._epoch_bar.bar_style = 'warning'
+
+    @property
+    def widget(self):
+        return self._widget
+
+    def update(self, iteration, epoch_detail):
+        length, unit = self._training_length
+
+        recent_timing = self._recent_timing
+        now = time.time()
+
+        recent_timing.append((iteration, epoch_detail, now))
+
+        if unit == 'iteration':
+            rate = iteration / length
+        else:
+            rate = epoch_detail / length
+        self._total_bar.value = rate
+        self._total_html.value = "{:6.2%}".format(rate)
+
+        epoch_rate = epoch_detail - int(epoch_detail)
+        self._epoch_bar.value = epoch_rate
+        self._epoch_html.value = "{:6.2%}".format(epoch_rate)
+
+        status = self._status_template.format(iteration=iteration,
+                                              epoch=int(epoch_detail))
+
+        if rate == 1:
+            self._total_bar.bar_style = 'success'
+            self._epoch_bar.bar_style = 'success'
+
+        old_t, old_e, old_sec = recent_timing[0]
+        span = now - old_sec
+        if span != 0:
+            speed_t = (iteration - old_t) / span
+            speed_e = (epoch_detail - old_e) / span
+        else:
+            speed_t = float('inf')
+            speed_e = float('inf')
+
+        if unit == 'iteration':
+            estimated_time = (length - iteration) / speed_t
+        else:
+            estimated_time = (length - epoch_detail) / speed_e
+        estimate = ('{:10.5g} iters/sec. Estimated time to finish: {}.'
+                    .format(speed_t,
+                            datetime.timedelta(seconds=estimated_time)))
+
+        self._status_html.value = status + estimate
+
+        if len(recent_timing) > 100:
+            del recent_timing[0]
+
+    def _init_status_template(self):
+        self._status_template = (
+            '{iteration:10} iter, {epoch} epoch / %s %ss<br />' %
+            self._training_length)
