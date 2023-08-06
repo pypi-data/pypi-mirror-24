971c8fbf3ef49eea27bfee15195f817eff37bd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:41 +0000

    Add package sc14npy/0.9.0rc2/sc14npy-0.9.0rc2.zip

---
+++ packages/sc14npy/sc14npy-0.9.0rc2.zip/sc14npy-0.9.0rc2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='sc14npy',
+      version='0.9.0rc2',
+      description='A Python interface to SC14N',
+      author='David Ireland',
+      url='http://www.di-mgt.com.au/sc14n/',
+      platforms=['Windows'],
+      py_modules=['sc14n'],
+      )
