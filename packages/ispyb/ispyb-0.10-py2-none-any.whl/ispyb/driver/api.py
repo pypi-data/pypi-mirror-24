6aa6b6d3108abfef83928e2e1ee95be0be7299ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:35 +0000

    Add package ispyb/0.10/ispyb-0.10-py2-none-any.whl

---
+++ packages/ispyb/ispyb-0.10-py2-none-any.whl/ispyb/driver/api.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division
+
+import ispyb.api.main
+
+class ISPyBAPIDriver(ispyb.api.main.API):
+  '''Not implemented.'''
---
+++ packages/ispyb/ispyb-0.10-py2-none-any.whl/ispyb/driver/dummy.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division
+
+import ispyb.api.main
+
+class ISPyBDummyDriver(ispyb.api.main.API):
+  pass
