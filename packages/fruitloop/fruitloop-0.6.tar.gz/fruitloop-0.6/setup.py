fac395559b604ac763c41be38fea4d538819d0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:52 +0000

    Add package fruitloop/0.6/fruitloop-0.6.tar.gz

---
+++ packages/fruitloop/fruitloop-0.6.tar.gz/fruitloop-0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages, Extension
+import numpy
+
+
+setup(name='fruitloop',
+      version='0.6',
+      url='https://www.github.com/neuroneuro15/fruitloop',
+      description='3D Graphics Engine for CAVE VR setups',
+      author='Nicholas A. Del Grosso',
+      author_email='delgrosso.nick@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['docs']),
+      include_package_data=True,
+      package_data={'': ['../assets/*.'+el for el in ['png', 'obj', 'mtl']] +
+                        ['../shaders/*'+el for el in ['vert', 'frag']]
+                    },
+      install_requires=['pyglet', 'numpy', 'wavefront_reader'],
+      ext_modules=[Extension('_transformations', sources=['third_party/transformations.c'], include_dirs=[numpy.get_include()])],
+      test_suite='tests',
+      keywords='graphics 3D pyglet psychopy python virtual reality VR',
+      classifiers=[
+          "Topic :: Multimedia :: Graphics :: 3D Rendering",
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+      ],
+      )
+
