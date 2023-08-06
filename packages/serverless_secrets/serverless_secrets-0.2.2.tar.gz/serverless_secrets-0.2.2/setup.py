d578ba54e2fa88637ef03cc701c769bf8ddb11c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:34 +0000

    Add package serverless_secrets/0.2.2/serverless_secrets-0.2.2.tar.gz

---
+++ packages/serverless_secrets/serverless_secrets-0.2.2.tar.gz/serverless_secrets-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+version = '0.2.2'
+
+setup(
+  name = 'serverless_secrets',
+  packages = ['serverless_secrets'],
+  version = version,
+  description = 'A library for accessing secrets for use in serverless applications',
+  author = 'Trek10',
+  author_email = 'kwinner@trek10.com',
+  url = 'https://github.com/trek10inc/serverless-secrets-python',
+  download_url = 'https://github.com/trek10inc/serverless-secrets-python/archive/' + version + '.tar.gz',
+  keywords = ['serverless', 'secrets'],
+  classifiers = [],
+  include_package_data=True,
+  install_requires=[
+    'boto3',
+    'json'
+  ],
+)
