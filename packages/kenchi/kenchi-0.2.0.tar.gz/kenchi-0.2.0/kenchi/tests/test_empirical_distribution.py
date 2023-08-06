a5cb5137ea78ef661f0cd5cda3eea5993e7b2523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:46 +0000

    Add package kenchi/0.2.0/kenchi-0.2.0.tar.gz

---
+++ packages/kenchi/kenchi-0.2.0.tar.gz/kenchi-0.2.0/kenchi/tests/test_empirical_distribution.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+import numpy as np
+from sklearn.model_selection import ParameterGrid
+
+from kenchi import EmpiricalDetector
+
+
+class EmpiricalDetectorTest(TestCase):
+    def test_score(self):
+        train_size = 1000
+        test_size  = 100
+        n_outliers = 10
+        n_features = 10
+
+        rnd        = np.random.RandomState(0)
+
+        mean       = np.zeros(n_features)
+        cov        = np.eye(n_features)
+
+        X_train    = rnd.multivariate_normal(mean, cov, train_size)
+
+        X_test     = np.concatenate((
+            rnd.multivariate_normal(mean, cov, test_size - n_outliers),
+            rnd.uniform(-10.0, 10.0, size=(n_outliers, n_features))
+        ))
+
+        y_test     = np.concatenate((
+            np.zeros(test_size - n_outliers, dtype=np.int32),
+            np.ones(n_outliers, dtype=np.int32),
+        ))
+
+        param_grid = {'fpr': [0.1], 'threshold': [None, 10.0]}
+
+        for params in ParameterGrid(param_grid):
+            det    = EmpiricalDetector().set_params(**params)
+
+            self.assertIsInstance(det.fit(X_train), EmpiricalDetector)
+            self.assertGreater(det.score(X_test, y_test), 0.5)
