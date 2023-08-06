fa492fcf21fecfef38ca447de056d7766451a156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:55 +0000

    Add package pyLPD/0.0.1.5/pyLPD-0.0.1.5.tar.gz

---
+++ packages/pyLPD/pyLPD-0.0.1.5.tar.gz/pyLPD-0.0.1.5/pyLPD/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+PyLPD v0.01
+
+We compile in this package many functions at our
+laboratory (LPD - sites.ifi.unicamp.br/LPD).
+
+These module has three major submodules:
+
+    * MLtools : interaction with matlab, matlab-like functions
+    * VISAtools: instrument control
+    * Simtools: simulation scripts
+
+
+Documentation
+-------------
+
+
+Usage
+-------------
+    >>> from pyLPD import MLtools   # definição do gerador de funções
+
+"""
+
+## import instrument modules
+#from .MLtools import loadmat 
+#import .MLtools
