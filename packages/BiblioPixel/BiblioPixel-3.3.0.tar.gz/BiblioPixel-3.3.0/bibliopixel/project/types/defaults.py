0e9a46dc837475cb18b2d4ec6dff9451c8264162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:20 +0000

    Add package BiblioPixel/3.3.0/BiblioPixel-3.3.0.tar.gz

---
+++ packages/BiblioPixel/BiblioPixel-3.3.0.tar.gz/BiblioPixel-3.3.0/bibliopixel/project/types/defaults.py
@@ -0,0 +1,12 @@
+from . import channel_order, color, duration, gamma, ledtype, rotation
+
+FIELD_TYPES = {
+    'c_order': channel_order,
+    'color': color,
+    'duration': duration,
+    'gamma': gamma,
+    'ledtype': ledtype,
+    'ledtype': ledtype,
+    'rotation': rotation,
+    'time': duration,
+}
