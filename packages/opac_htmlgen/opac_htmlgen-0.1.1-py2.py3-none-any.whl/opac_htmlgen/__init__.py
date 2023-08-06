92ab7e9659239a7b35875b904dc5407fb031c215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:47 +0000

    Add package opac_htmlgen/0.1.1/opac_htmlgen-0.1.1-py2.py3-none-any.whl

---
+++ packages/opac_htmlgen/opac_htmlgen-0.1.1-py2.py3-none-any.whl/opac_htmlgen/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for opac_htmlgen."""
+
+__author__ = """Juan Funez"""
+__email__ = 'juan.funez@scielo.org'
+__version__ = '0.1.1'
---
+++ packages/opac_htmlgen/opac_htmlgen-0.1.1-py2.py3-none-any.whl/opac_htmlgen/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for opac_htmlgen."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for opac_htmlgen."""
+    click.echo("Replace this message by putting your code into "
+               "opac_htmlgen.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
