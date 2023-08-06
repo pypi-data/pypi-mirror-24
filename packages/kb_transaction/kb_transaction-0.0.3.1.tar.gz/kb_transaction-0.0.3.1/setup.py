a2f6a09b32517d7a315c20338c9ecdbbb767af02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:40 +0000

    Add package kb_transaction/0.0.3.1/kb_transaction-0.0.3.1.tar.gz

---
+++ packages/kb_transaction/kb_transaction-0.0.3.1.tar.gz/kb_transaction-0.0.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='kb_transaction',
+      version='0.0.3.1',
+      url='https://github.com/beomi/kb-transaction',
+      license='MIT',
+      author='Junbum Lee',
+      author_email='jun@beomi.net',
+      description='Crawling KB bank transaction with IE Selenium',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      zip_safe=False,
+      setup_requires=['python-dateutil', 'beautifulsoup4', 'selenium'],
+      install_requires=['python-dateutil', 'beautifulsoup4', 'selenium'],
+      download_url='https://github.com/beomi/kb_transaction/archive/0.0.3.1.tar.gz',
+      )
