926f200192244211dce1642b440921010e495a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:34 +0000

    Add package WorkHours/0.1/WorkHours-0.1.zip

---
+++ packages/WorkHours/WorkHours-0.1.zip/WorkHours-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'WorkHours',
+  packages = ['WorkHours'], 
+  version = '0.1',
+  description = 'Time difference calculation in business hours',
+  author = 'KK',
+  author_email = 'kkshivnarain@gmail.com',
+  url = 'https://github.com/kkshivnarain/WorkHours', 
+  download_url = 'https://github.com/kkshivnarain/WorkHours/archive/0.2.tar.gz', 
+  keywords = ['Business Hours', 'Time Difference'], 
+  classifiers = [],
+)
