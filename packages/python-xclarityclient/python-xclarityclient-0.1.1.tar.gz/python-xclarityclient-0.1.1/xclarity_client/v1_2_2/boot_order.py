1d69c93b8fa6a2635edd31b4c6a71d24b16d8cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:26 +0000

    Add package python-xclarityclient/0.1.1/python-xclarityclient-0.1.1.tar.gz

---
+++ packages/python-xclarityclient/python-xclarityclient-0.1.1.tar.gz/python-xclarityclient-0.1.1/xclarity_client/v1_2_2/boot_order.py
@@ -0,0 +1,65 @@
+from ..constants import SYSTEM_X3250_M5, SYSTEM_X3550_M5, SYSTEM_X3650_M5
+
+
+"""
+    Boot Order Priority
+"""
+
+boot_order_priority = [
+    {'name': 'singleuse', 'value': None},
+    {'name': 'permanent', 'value': None},
+    {'name': 'wakeonlan', 'value': None}
+]
+
+setting_boot_type = {
+    'SingleUse': 'BootOrder.SingleUse',
+    'Permanent': 'BootOrder.BootOrder'
+}
+
+
+"""
+    Standard Boot Devices' names
+    These name should be consistent with the ones defined in
+    ironic/common/boot_devices.py
+"""
+
+PXE = 'pxe'
+"Boot from PXE boot"
+
+DISK = 'disk'
+"Boot from default Hard-drive"
+
+CDROM = 'cdrom'
+"Boot from CD/DVD"
+
+BIOS = 'bios'
+"Boot into BIOS setup"
+
+SAFE = 'safe'
+"Boot from default Hard-drive, request Safe Mode"
+
+WANBOOT = 'wanboot'
+"Boot from Wide Area Network"
+
+
+"""
+    Boot Devices per Machine types
+"""
+
+BOOT_DEVICES = {
+    SYSTEM_X3250_M5: {
+        PXE: 'network',
+        DISK: 'hd',
+        CDROM: 'cd'
+    },
+    SYSTEM_X3550_M5: {
+        PXE: 'PXE Network',
+        DISK: 'Hard Disk 0',
+        CDROM: 'CD/DVD Rom'
+    },
+    SYSTEM_X3650_M5: {
+        PXE: 'PXE Network',
+        DISK: 'Hard Disk 0',
+        CDROM: 'CD/DVD Rom'
+    }
+}
