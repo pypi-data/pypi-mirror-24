4772cd1307465a5003263e4f880af7e30a1d87b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:28 +0000

    Add package nipyapi/0.3.2/nipyapi-0.3.2.tar.gz

---
+++ packages/nipyapi/nipyapi-0.3.2.tar.gz/nipyapi-0.3.2/nipyapi/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Nipyapi."""
+
+from __future__ import absolute_import
+
+from .canvas import Canvas
+from .system import System
+
+__all__ = ['swagger_client']
+__author__ = """Daniel Chaffelson"""
+__email__ = 'chaffelson@gmail.com'
+__version__ = '0.3.2'
