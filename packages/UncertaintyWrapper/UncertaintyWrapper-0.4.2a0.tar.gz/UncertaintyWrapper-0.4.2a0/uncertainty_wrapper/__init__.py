eac94cfd86aae8373c35abad8cf7bf7e19c75a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:18 +0000

    Add package UncertaintyWrapper/0.4.2a0/UncertaintyWrapper-0.4.2a0.tar.gz

---
+++ packages/UncertaintyWrapper/UncertaintyWrapper-0.4.2a0.tar.gz/UncertaintyWrapper-0.4.2a0/uncertainty_wrapper/__init__.py
@@ -0,0 +1,10 @@
+"""UncertaintyWrapper"""
+
+from uncertainty_wrapper.core import unc_wrapper, unc_wrapper_args, jflatten
+
+__VERSION__ = '0.4.2a'
+__RELEASE__ = u"Cambrian Period"
+__URL__ = u'https://github.com/SunPower/UncertaintyWrapper'
+__AUTHOR__ = u"Mark Mikofski"
+__EMAIL__ = u'mark.mikofski@sunpowercorp.com'
+__all__ = ['unc_wrapper', 'unc_wrapper_args', 'jflatten']
