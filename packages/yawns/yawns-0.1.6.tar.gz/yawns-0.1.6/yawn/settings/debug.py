f0bb788c67a5a771fadda464fa8785df6f98f56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:57 +0000

    Add package yawns/0.1.6/yawns-0.1.6.tar.gz

---
+++ packages/yawns/yawns-0.1.6.tar.gz/yawns-0.1.6/yawn/settings/debug.py
@@ -0,0 +1,13 @@
+from yawn.settings.base import *  # NOQA
+
+SECRET_KEY = 'example secret key, change me'
+DEBUG = True
+
+# disable SSL settings
+CSRF_COOKIE_SECURE = False
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+
+# ALLOWED_HOSTS = [
+#     '.yawn.live',
+# ]
