6a016e15bb112c876a389ad4b1be526d63811f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:10 +0000

    Add package mosi/0.0.1/mosi-0.0.1-py2.py3-none-any.whl

---
+++ packages/mosi/mosi-0.0.1-py2.py3-none-any.whl/mosi/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+
+# TODO - remove message once contents exist
+def message():
+    print("Thank you for installing the mosi package (Modelling & Optimization Solver Interface).")
+    print("The mosi package is currently in development, "
+          "please watch https://github.com/alexbahnisch/mosi.py for updates.")
