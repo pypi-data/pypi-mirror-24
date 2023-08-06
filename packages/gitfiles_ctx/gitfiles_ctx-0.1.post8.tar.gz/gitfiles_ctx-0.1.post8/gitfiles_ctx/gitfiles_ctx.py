d9091b6399e2642c24f0d7e4999fe535781e172b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:11 +0000

    Add package gitfiles_ctx/0.1.post8/gitfiles_ctx-0.1.post8.tar.gz

---
+++ packages/gitfiles_ctx/gitfiles_ctx-0.1.post8.tar.gz/gitfiles_ctx-0.1.post8/gitfiles_ctx/gitfiles_ctx.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import git, logging, logtool, shutil, tempfile
+from path import Path
+
+LOG = logging.getLogger (__name__)
+# https://stackoverflow.com/questions/28291909/gitpython-and-ssh-keys
+# https://stackoverflow.com/questions/28803626/get-all-revisions-for-a-specific-file-in-gitpython
+
+class GitFiles_Ctx (object):
+
+  @logtool.log_call
+  def __init__ (self, url, ssh_keypath, prefix = "gitfiles_ctx__",
+                branch = "master"):
+    self.fpath = tempfile.mkdtemp (prefix = prefix)
+    os.environ['GIT_SSH_COMMAND'] = ssh_cmd
+    git.Repo.clone_from (url, self.fpath,
+                         env = {'GIT_SSH_COMMAND': ssh_cmd},
+                         branch = branch)
+
+  @logtool.log_call
+  def __enter__ (self):
+    return self
+
+  @logtool.log_call (log_exc = True)
+  def __exit__ (self, exc_type, exc_val, exc_tb):
+    shutil.rmtree (self.fpath)
+
+  @logtool.log_call
+  def read (self, fname):
+    return file (Path (self.fpath) / fname).read ()
