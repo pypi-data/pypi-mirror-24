93535ddc0f0de6c2e478e57a43d7b8a8262fd557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:44 +0000

    Add package jirachi/0.1.2.12/jirachi-0.1.2.12.tar.gz

---
+++ packages/jirachi/jirachi-0.1.2.12.tar.gz/jirachi-0.1.2.12/jirachi/io/__init__.py
@@ -0,0 +1,5 @@
+from .scheduler import SchedulerMonitor
+from .requests import RequestMonitor
+
+
+__all__ = ['SchedulerMonitor', 'RequestMonitor']
---
+++ packages/jirachi/jirachi-0.1.2.12.tar.gz/jirachi-0.1.2.12/jirachi/io/abstract.py
@@ -0,0 +1,54 @@
+from pulsar import get_application, get_actor, send
+from pulsar.apps import Application
+from itertools import count
+from random import randint
+
+
+__all__ = ['JirachiMonitor', 'JirachiMonitorNotFound']
+
+
+class JirachiMonitorNotFound(Exception):
+    pass
+
+
+class JirachiMonitor(Application):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def monitor_start(self, monitor, exec=None):
+        monitor.counter = count(0)
+
+    @classmethod
+    async def get_arbiter(cls):
+        arbiter = get_actor().get_actor('arbiter')
+        return arbiter
+
+    @classmethod
+    async def get_monitor(cls):
+        async def get_monitor_via_arbiter():
+            arbiter = get_actor().get_actor('arbiter')
+            monitor_name = next(
+                (m for m in arbiter.monitors if cls.name in m), None)
+            monitor = await get_application(monitor_name)
+            return monitor
+        name = cls.cfg.name or cls.name
+        monitor = get_actor().get_actor(name)
+        return monitor or await get_monitor_via_arbiter()
+
+    @classmethod
+    async def get_worker(cls, monitor):
+        workers = monitor.managed_actors
+        if workers:
+            if not monitor.counter:
+                index = randint(0, len(workers) - 1)
+            else:
+                index = next(monitor.counter) % len(workers)
+            wid = list(workers.keys())[index]
+            worker = get_actor().get_actor(wid)
+        return worker
+
+    @classmethod
+    async def kill(cls):
+        monitor = await cls.get_monitor()
+        return await send('arbiter', 'kill_actor', monitor.name)
