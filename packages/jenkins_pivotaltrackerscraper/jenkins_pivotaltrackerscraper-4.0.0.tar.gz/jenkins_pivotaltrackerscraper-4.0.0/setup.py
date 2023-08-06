a48c3f8b140fb5722bfb015caef0a5beaf9145b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:06 +0000

    Add package jenkins_pivotaltrackerscraper/4.0.0/jenkins_pivotaltrackerscraper-4.0.0.tar.gz

---
+++ packages/jenkins_pivotaltrackerscraper/jenkins_pivotaltrackerscraper-4.0.0.tar.gz/jenkins_pivotaltrackerscraper-4.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='jenkins_pivotaltrackerscraper',
+      version='4.0.0',
+      description='Pivotal Tracker Scraper for Jenkins',
+      url='https://github.com/KMK-ONLINE/PivotalTrackerScraper',
+      author='Henry Xu',
+      author_email='hxu@bbmtek.com',
+      license='MIT',
+      packages=['PivotalTrackerScraper'],
+      install_requires=[
+          'gitpython',
+          'requests',
+      ],
+      scripts=['bin/track-and-parse-stories'],
+      zip_safe=False)
