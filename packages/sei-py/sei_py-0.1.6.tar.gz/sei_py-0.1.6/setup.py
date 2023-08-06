32cb3bd4ee618fe220a08e5b89c2a66dd90949ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:46 +0000

    Add package sei-py/0.1.6/sei_py-0.1.6.tar.gz

---
+++ packages/sei-py/sei_py-0.1.6.tar.gz/sei_py-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name = 'sei_py',
+    packages = ['sei_py', 'sei_py.base', 'sei_py.rest'],
+    version = '0.1.6',
+    description = 'A Caveon SEI helper library',
+    author = 'Evan Anderson',
+    author_email = 'evan.anderson@caveon.com',
+    url = 'https://github.com/xh4zr/sei-py',
+    keywords = ['caveon', 'sei', 'exam', 'test security'],
+    install_requires = ['requests==2.18.4']
+)
