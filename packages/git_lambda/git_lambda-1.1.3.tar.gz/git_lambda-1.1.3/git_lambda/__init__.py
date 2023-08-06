eec3c0b00f40ea2b314f978cbceadd7663c91de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package git_lambda/1.1.3/git_lambda-1.1.3.tar.gz

---
+++ packages/git_lambda/git_lambda-1.1.3.tar.gz/git_lambda-1.1.3/git_lambda/__init__.py
@@ -0,0 +1,43 @@
+import os
+import atexit
+import tempfile
+import tarfile
+import shutil
+
+
+def setup(target_directory=None,
+          update_env=True):
+
+    if not target_directory:
+        target_directory = tempfile.mkdtemp()
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    tar = tarfile.open(os.path.join(dir_path, 'git-2.14.0.tar'))
+    tar.extractall(target_directory)
+    git_path = os.path.join(target_directory, 'git')
+
+    bin_path = os.path.join(git_path, 'bin')
+
+    template_dir = os.path.join(
+        git_path,
+        'share',
+        'git-core',
+        'templates'
+    )
+
+    exec_path = os.path.join(
+        git_path,
+        'libexec',
+        'git-core'
+    )
+
+    if update_env:
+        os.environ['PATH'] = bin_path + ':' + os.environ['PATH']
+        os.environ['GIT_TEMPLATE_DIR'] = template_dir
+        os.environ['GIT_EXEC_PATH'] = exec_path
+
+    atexit.register(teardown, target_directory)
+
+
+def teardown(git_dir):
+    shutil.rmtree(git_dir)
