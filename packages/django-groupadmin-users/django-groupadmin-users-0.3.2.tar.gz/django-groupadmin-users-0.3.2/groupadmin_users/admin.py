bf6c72a7b4677f41b05f7cbce5359b952d749e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package django-groupadmin-users/0.3.2/django-groupadmin-users-0.3.2.tar.gz

---
+++ packages/django-groupadmin-users/django-groupadmin-users-0.3.2.tar.gz/django-groupadmin-users-0.3.2/groupadmin_users/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+from groupadmin_users.forms import GroupAdminForm
+
+
+# Unregister the original Group admin.
+admin.site.unregister(Group)
+
+
+# Create a new Group admin.
+class GroupAdmin(admin.ModelAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+    readonly_fields = ('users',)
+
+# Register the new Group ModelAdmin.
+admin.site.register(Group, GroupAdmin)
