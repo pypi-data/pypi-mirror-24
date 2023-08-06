f47faa9e0560bd0f28ab4a8fce10a43bac140c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:30 +0000

    Add package labugr/0.1.1/labugr-0.1.1-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/labugr/labugr-0.1.1-cp35-cp35m-manylinux1_x86_64.whl/labugr/signal/__init__.py
@@ -0,0 +1,9 @@
+from .respuestaF import *
+from .spectral import *
+from .waveforms import *
+from .import windows as ventana
+
+from labugr.testing.utils import PytestTester
+test = PytestTester(__name__)
+del PytestTester
+
