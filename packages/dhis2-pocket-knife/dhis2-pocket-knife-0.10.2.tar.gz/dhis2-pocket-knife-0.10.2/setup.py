6b85fa0aa62f32731ff003eba1cc86c47587c7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:54 +0000

    Add package dhis2-pocket-knife/0.10.2/dhis2-pocket-knife-0.10.2.tar.gz

---
+++ packages/dhis2-pocket-knife/dhis2-pocket-knife-0.10.2.tar.gz/dhis2-pocket-knife-0.10.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+__version__ = ''
+with open(os.path.join('src', 'version.py')) as f:
+    exec (f.read())
+
+setup(
+    name='dhis2-pocket-knife',
+    version=__version__,
+    description='Command-line tools for interacting with DHIS2 REST API',
+    author='David Huser',
+    author_email='dhuser@baosystems.com',
+    url='https://github.com/davidhuser/dhis2-pocket-knife',
+    keywords='dhis2',
+    license='MIT',
+    install_requires=[
+        'requests>=2.4.2',
+        'six>=1.10.0',
+        'unicodecsv>=0.14.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dhis2-pk-indicator-definitions = src.scripts.indicator_definitions:main',
+            'dhis2-pk-share-objects = src.scripts.share_objects:main',
+            'dhis2-pk-metadata-dl = src.scripts.metadata_download:main',
+            'dhis2-pk-userinfo = src.scripts.userinfo:main',
+            'dhis2-pk-post-css = src.scripts.post_css:main',
+            'dhis2-pk-hnqis-orgunit-productivity = src.scripts.hnqis_ou_productivityvalues:main',
+            'dhis2-pk-hnqis-program-orgunit-assigner = src.scripts.hnqis_program_ou_assigner:main'
+        ]
+    },
+    packages=find_packages(exclude=['tests']),
+    test_suite='pytest',
+    tests_require=['pytest'],
+    setup_requires=['pytest-runner']
+)
