01f2d069874690a55434c9cec0a91d781d4f5d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package losswise/0.84/losswise-0.84.tar.gz

---
+++ packages/losswise/losswise-0.84.tar.gz/losswise-0.84/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='losswise',
+    version='0.84',
+    description='Official Losswise library for Python',
+    long_description=open('README.rst').read(),
+    url='https://losswise.com',
+    author='Losswise, Inc.',
+    author_email='nicodjimenez@gmail.com',
+    license='Apache',
+    install_requires=['requests >= 2.9.1'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'
+    ],
+
+    keywords='losswise analytics dashboard deep machine learning tensorflow pytorch optimization ML deep learning',
+    packages=['losswise'],
+)
+
