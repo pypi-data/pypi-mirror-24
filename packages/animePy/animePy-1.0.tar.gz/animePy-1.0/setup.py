1f9044144431c9aa86bd7968150b8039a0bcae03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:26 +0000

    Add package animePy/1.0/animePy-1.0.tar.gz

---
+++ packages/animePy/animePy-1.0.tar.gz/animePy-1.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='animePy',version='1.0', description='Anime crawler', url='http://github.com/cvsper/animeCrawler', author='sevens', author_email='se7nz7@gmail.com',license='MIT', packages=['animePy'],zipsafe=False)
