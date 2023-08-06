dee5e80e8bc19f8add72375aea6b0935dce6989f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:03 +0000

    Add package dnutils/0.1.5/dnutils-0.1.5.tar.gz

---
+++ packages/dnutils/dnutils-0.1.5.tar.gz/dnutils-0.1.5/setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'dnutils', 'VERSION'), 'r') as f:
+    __version__ = f.read().strip()
+
+setup(
+    name = 'dnutils',
+    packages = ['dnutils'],
+    version = __version__,
+    description = 'A collection of convenience tools for everyday Python programming',
+    author = 'Daniel Nyga',
+    author_email = 'daniel.nyga@t-online.de',
+    url = 'https://spritinio.de/dnutils',
+    download_url = 'https://github.com/danielnyga/dnutils/archive/%s.tar.gz' % __version__,
+    keywords = ['testing', 'logging', 'threading', 'multithreading', 'debugging', 'tools'],
+    classifiers = [],
+    install_requires=[
+        'colored==1.3.5',
+        'tabulate',
+        'portalocker',
+    ],
+    package_data={
+        '': [
+            'VERSION',
+        ]
+    },
+)
