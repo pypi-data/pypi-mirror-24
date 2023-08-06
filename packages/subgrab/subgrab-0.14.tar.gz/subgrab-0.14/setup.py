a06afed9a038db4a89d01e5ade2ae73534dc932f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:20 +0000

    Add package subgrab/0.14/subgrab-0.14.tar.gz

---
+++ packages/subgrab/subgrab-0.14.tar.gz/subgrab-0.14/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open as openit
+
+def readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+setup(
+    name='subgrab',
+    version='0.14',
+    description='A python script for automating subtitles downloading.',
+    long_description=readme(),
+    url='https://github.com/RafayGhafoor/Subscene-Subtitle-Grabber',
+    author='Rafay Ghafoor',
+    author_email='rafayghafoor@protonmail.com',
+    packages=['subgrab', 'subgrab.source', 'subgrab.modules'],
+    entry_points = {'console_scripts': ['subgrab = subgrab.__main__:main']},
+    zip_safe = False,
+    license='GPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='subtitle automation subscene opensubtitles media',
+    install_requires=['requests', 'bs4', 'lxml'],
+)
