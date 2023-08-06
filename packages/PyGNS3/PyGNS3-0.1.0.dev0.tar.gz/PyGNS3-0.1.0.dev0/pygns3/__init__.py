a8c842292e39fff846aa1bd09d9af4aa0bc9800c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:01 +0000

    Add package PyGNS3/0.1.0.dev0/PyGNS3-0.1.0.dev0.tar.gz

---
+++ packages/PyGNS3/PyGNS3-0.1.0.dev0.tar.gz/PyGNS3-0.1.0.dev0/pygns3/__init__.py
@@ -0,0 +1,5 @@
+from .controller import API, GNS3Compute, GNS3Controller, GNS3Project, GNS3VM, gns3_server_conf
+from requests.auth import HTTPBasicAuth
+
+__all__ = ['GNS3Compute', 'GNS3Controller', 'GNS3Project', 'GNS3VM']
+
