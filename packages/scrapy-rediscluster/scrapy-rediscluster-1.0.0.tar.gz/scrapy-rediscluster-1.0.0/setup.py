f790b94a372327816922abf358854decefa1641a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:51 +0000

    Add package scrapy-rediscluster/1.0.0/scrapy-rediscluster-1.0.0.tar.gz

---
+++ packages/scrapy-rediscluster/scrapy-rediscluster-1.0.0.tar.gz/scrapy-rediscluster-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'scrapy-rediscluster',
+    version = '1.0.0',
+    url='', 
+    author='duyongan', 
+    author_email='13261051171@163.com',
+    packages=['scrapy_rediscluster'],
+    )
