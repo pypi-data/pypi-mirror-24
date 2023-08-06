c370b8eb8cf8df0b8bba5614a4111570acfcda2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:34 +0000

    Add package CUriTools/0.1/CUriTools-0.1.tar.gz

---
+++ packages/CUriTools/CUriTools-0.1.tar.gz/CUriTools-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='CUriTools',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/gutorc92/uritools',
+    author='Gustavo Coelho',
+    author_email='gutorc@hotmail.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=long_description,
+    install_requires=['selenium','click','clint'],
+    entry_points={
+        'console_scripts': [
+            'curitools=curitools.uritools:uri',
+        ],
+    },
+    package_data={
+        'curitools': ['phantomjs','chromedriver'],
+    },
+    
+)
