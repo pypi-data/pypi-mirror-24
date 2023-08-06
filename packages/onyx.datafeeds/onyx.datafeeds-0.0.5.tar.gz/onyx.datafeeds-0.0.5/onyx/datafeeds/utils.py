1989afc5ab3ca62030a4cfcaebe5972790bc3705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:36 +0000

    Add package onyx.datafeeds/0.0.5/onyx.datafeeds-0.0.5.tar.gz

---
+++ packages/onyx.datafeeds/onyx.datafeeds-0.0.5.tar.gz/onyx.datafeeds-0.0.5/onyx/datafeeds/utils.py
@@ -0,0 +1,48 @@
+###############################################################################
+#
+#   Copyright: (c) 2017 Carlo Sbraccia
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+###############################################################################
+
+import json
+import hashlib
+
+REQ_FIELDS = ("sec", "field", "start", "end",
+              "adjusted", "overrides", "type", "real-time")
+
+
+###############################################################################
+class CacheLocked(object):
+    __slots__ = ()
+
+
+# -----------------------------------------------------------------------------
+def encode_message(msg):
+    return bytes(json.dumps(msg, separators=(",", ":")) + "\n", "utf-8")
+
+
+# -----------------------------------------------------------------------------
+def decode_message(msg):
+    return json.loads(msg.decode("utf-8"))
+
+
+# -----------------------------------------------------------------------------
+def request_to_uid(request, real_time):
+    # --- serialize request making sure we always get the same key for the
+    #     same request (NB: the request is a dictionary which means
+    #     ordering is not guaranteed).
+    req_vls = ["({0!s}".format(request.get(k, None)) for k in REQ_FIELDS]
+    req_vls.append("{0!s}".format(real_time))
+    return hashlib.sha1("".join(req_vls).encode("utf-8")).hexdigest()
