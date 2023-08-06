be571e13b2f1e788aaf1af1e92086b9ba5e28d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:42 +0000

    Add package salesforce-bulk/2.0.0.dev6/salesforce_bulk-2.0.0.dev6-py2.py3-none-any.whl

---
+++ packages/salesforce-bulk/salesforce_bulk-2.0.0.dev6-py2.py3-none-any.whl/salesforce_bulk/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from .salesforce_bulk import SalesforceBulk, BulkApiError, UploadResult
+from .csv_adapter import CsvDictsAdapter
+
+__version__ = '2.0.0dev6'
