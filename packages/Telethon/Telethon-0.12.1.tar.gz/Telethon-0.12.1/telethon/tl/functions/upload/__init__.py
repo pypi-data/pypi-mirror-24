8db2877b4d4be34b722e0ec49f3dde9b706d00fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package Telethon/0.12.1/Telethon-0.12.1.tar.gz

---
+++ packages/Telethon/Telethon-0.12.1.tar.gz/Telethon-0.12.1/telethon/tl/functions/upload/__init__.py
@@ -0,0 +1,7 @@
+from .save_file_part import SaveFilePartRequest
+from .get_file import GetFileRequest
+from .save_big_file_part import SaveBigFilePartRequest
+from .get_web_file import GetWebFileRequest
+from .get_cdn_file import GetCdnFileRequest
+from .reupload_cdn_file import ReuploadCdnFileRequest
+from .get_cdn_file_hashes import GetCdnFileHashesRequest
---
+++ packages/Telethon/Telethon-0.12.1.tar.gz/Telethon-0.12.1/telethon/tl/functions/users/__init__.py
@@ -0,0 +1,2 @@
+from .get_users import GetUsersRequest
+from .get_full_user import GetFullUserRequest
