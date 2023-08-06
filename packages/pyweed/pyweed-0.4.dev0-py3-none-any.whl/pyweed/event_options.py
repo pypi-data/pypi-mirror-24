7a712c19e118efa56175398abed2e41d9d9885c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:23 +0000

    Add package pyweed/0.4.dev0/pyweed-0.4.dev0-py3-none-any.whl

---
+++ packages/pyweed/pyweed-0.4.dev0-py3-none-any.whl/pyweed/event_options.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Event Options
+
+:copyright:
+    Mazama Science, IRIS
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+
+from pyweed.options import Options, DateOption, FloatOption, Option
+
+
+class EventOptions(Options):
+
+    time_choice = Option(hidden=True)
+    TIME_RANGE = 'timeBetween'
+    TIME_STATIONS = 'timeFromStations'
+
+    starttime = DateOption(default=-30)
+    endtime = DateOption(default=0)
+
+    minmagnitude = FloatOption(default=5.0)
+    maxmagnitude = FloatOption(default=10.0)
+    magnitudetype = Option()
+
+    mindepth = FloatOption(default=0)
+    maxdepth = FloatOption(default=6800)
+
+    location_choice = Option(hidden=True)
+    LOCATION_GLOBAL = 'locationGlobal'
+    LOCATION_BOX = 'locationRange'
+    LOCATION_POINT = 'locationDistanceFromPoint'
+    LOCATION_STATIONS = 'locationFromStations'
+
+    minlatitude = FloatOption(default=-90)
+    maxlatitude = FloatOption(default=90)
+    minlongitude = FloatOption(default=-180)
+    maxlongitude = FloatOption(default=180)
+
+    latitude = FloatOption()
+    longitude = FloatOption()
+    minradius = FloatOption()
+    maxradius = FloatOption(default=30)
+
+    def get_time_options(self, station_options=None):
+        if self.time_choice == self.TIME_RANGE:
+            return self.get_options(['starttime', 'endtime'])
+        elif self.time_choice == self.TIME_STATIONS and station_options:
+            return station_options.get_time_options()
+        else:
+            return {}
+
+    def get_location_options(self, station_options=None):
+        if self.location_choice == self.LOCATION_BOX:
+            return self.get_options(['minlatitude', 'maxlatitude', 'minlongitude', 'maxlongitude'])
+        elif self.location_choice == self.LOCATION_POINT:
+            return self.get_options(['latitude', 'longitude', 'minradius', 'maxradius'])
+        elif self.location_choice == self.LOCATION_STATIONS and station_options:
+            return station_options.get_location_options()
+        else:
+            return {}
+
+    def get_obspy_options(self, station_options=None):
+        base_keys = ['minmagnitude', 'maxmagnitude', 'magnitudetype', 'mindepth', 'maxdepth']
+        options = self.get_options(keys=base_keys)
+        # Need special handling for "All Types" value of magnitudetype
+        if options.get('magnitudetype', '').lower().startswith('all'):
+            del options['magnitudetype']
+        options.update(self.get_time_options(station_options))
+        options.update(self.get_location_options(station_options))
+        return options
---
+++ packages/pyweed/pyweed-0.4.dev0-py3-none-any.whl/pyweed/events_handler.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Container for events.
+
+:copyright:
+    Mazama Science, IRIS
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+
+from __future__ import (absolute_import, division, print_function)
+
+from pyweed.signals import SignalingThread, SignalingObject
+import logging
+from obspy.core.event.catalog import Catalog
+from pyweed.pyweed_utils import get_service_url
+
+LOGGER = logging.getLogger(__name__)
+
+
+class EventsLoader(SignalingThread):
+    """
+    Thread to handle event requests
+    """
+
+    def __init__(self, client, parameters):
+        """
+        Initialization.
+        """
+        self.client = client
+        self.parameters = parameters
+        super(EventsLoader, self).__init__()
+
+    def run(self):
+        """
+        Make a webservice request for events using the passed in options.
+        """
+        # Sanity check
+        try:
+            if 'starttime' not in self.parameters or 'endtime' not in self.parameters:
+                raise ValueError('starttime or endtime is missing')
+            LOGGER.info('Loading events: %s', get_service_url(self.client, 'event', self.parameters))
+            event_catalog = self.client.get_events(**self.parameters)
+            self.done.emit(event_catalog)
+        except Exception as e:
+            # If no results found, the client will raise an exception, we need to trap this
+            # TODO: this should be much cleaner with a fix to https://github.com/obspy/obspy/issues/1656
+            if str(e).startswith("No data"):
+                LOGGER.warning("No events found! Your query may be too narrow.")
+                self.done.emit(Catalog())
+            else:
+                self.done.emit(e)
+                raise
+
+
+class EventsHandler(SignalingObject):
+    """
+    Container for events.
+    """
+
+    def __init__(self, pyweed):
+        """
+        Initialization.
+        """
+        super(EventsHandler, self).__init__()
+        self.pyweed = pyweed
+        self.catalog_loader = None
+
+    def load_catalog(self, parameters=None):
+        if not parameters:
+            parameters = self.pyweed.get_event_obspy_options()
+        self.catalog_loader = EventsLoader(self.pyweed.event_client, parameters)
+        self.catalog_loader.done.connect(self.on_catalog_loaded)
+        self.catalog_loader.start()
+
+    def on_catalog_loaded(self, event_catalog):
+        self.done.emit(event_catalog)
+
+
+# ------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(exclude_empty=True)
