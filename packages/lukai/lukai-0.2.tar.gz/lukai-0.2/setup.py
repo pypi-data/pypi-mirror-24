fda1ffc01c7fbb0b6288e1a9950d1ff8bcf81baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:14 +0000

    Add package lukai/0.2/lukai-0.2.tar.gz

---
+++ packages/lukai/lukai-0.2.tar.gz/lukai-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+packages = find_packages('.')
+
+packages += ['lukai.proto.{}'.format(pkg) for pkg in next(os.walk('lukai/proto'))[1]]
+
+setup(
+    name='lukai',
+    packages=packages,
+    version='0.2',
+    description='Luk.ai management library',
+    install_requires=['grpcio', 'six'],
+    author='Tristan Rice',
+    author_email='rice@fn.lc',
+    url='https://github.com/luk-ai/lukai/tree/master/py',
+    keywords=['ml','machine learning','lukai'],
+    classifiers=[],
+)
