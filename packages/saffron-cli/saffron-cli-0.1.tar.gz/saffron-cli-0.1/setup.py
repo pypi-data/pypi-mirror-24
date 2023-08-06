5b0420fbd3ad7547e4747382f908ca02a1317849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:18 +0000

    Add package saffron-cli/0.1/saffron-cli-0.1.tar.gz

---
+++ packages/saffron-cli/saffron-cli-0.1.tar.gz/saffron-cli-0.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+setup(name='saffron-cli',
+      packages=find_packages(),
+      version = '0.1',
+      description = 'blockchain artifact development tools',
+      author = 'Lamden',
+      author_email = 'james@lamden.io',
+      url = 'https://github.com/Lamden/saffron',
+      download_url = 'https://github.com/Lamden/saffron/archive/0.1.tar.gz',
+      keywords = ['testing', 'logging', 'cryptocurrency', 'smartcontracts', 'ethereum', 'dashboards'],
+      classifiers = [],
+      install_requires=[
+          'werkzeug',
+          'gevent>=1.1.0',
+          'gunicorn',
+          'web3',
+          'jinja2',
+          'py-solc',
+          'pytest',
+          'py-solc',
+          'click'
+      ],
+      entry_points={
+          "console_scripts": [
+              "saffron=saffron.cli:cli",
+          ]
+      },
+      include_package_data=True,
+
+      package_data={
+          'saffron.': [
+              'default.conf',
+          ],
+          # 'hadron.node': [
+          #     'templates/*.html',
+          # ],
+      },
+      zip_safe=False
+      )
