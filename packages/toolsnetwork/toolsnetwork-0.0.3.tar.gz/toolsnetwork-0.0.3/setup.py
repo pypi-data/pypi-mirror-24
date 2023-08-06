04159e271ce49d5e750fadf2627e4c45f7607f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:50 +0000

    Add package toolsnetwork/0.0.3/toolsnetwork-0.0.3.tar.gz

---
+++ packages/toolsnetwork/toolsnetwork-0.0.3.tar.gz/toolsnetwork-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='toolsnetwork',
+      version='0.0.3',
+      description='A very simple networking library for seperatingwritten by Rohit G Bal (@rohitgbal).',
+      url='https://sites.google.com/site/rohitgbal/',
+      maintainer='Rohit G BAl',
+      maintainer_email='rohitgbal@gmail.com',
+      author='Rohit G Bal',
+      author_email='rohitgbal@gmail.com',
+      license='MIT Lisence',
+      py_modules=['network'],
+      zip_safe=False)
