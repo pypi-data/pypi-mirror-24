2825545b890d8d018507131b317a5e59a09625ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:59 +0000

    Add package solid-always-wins/0.1/solid_always_wins-0.1.tar.gz

---
+++ packages/solid-always-wins/solid_always_wins-0.1.tar.gz/solid_always_wins-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='solid_always_wins',
+      version='0.1',
+      description='The most solid joke in the world',
+      url='https://github.com/ageorgou/solid-octo-succotash',
+      author='A. and T.',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['solid'],
+      zip_safe=False)
---
+++ packages/solid-always-wins/solid_always_wins-0.1.tar.gz/solid_always_wins-0.1/solid/__init__.py
@@ -0,0 +1,1 @@
+from . import octo_functions
