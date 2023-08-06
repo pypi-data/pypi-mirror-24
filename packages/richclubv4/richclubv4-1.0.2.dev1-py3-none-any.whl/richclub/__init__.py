746d1045f45cdb52b33b900873b18e4087aa2b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:20 +0000

    Add package richclubv4/1.0.2.dev1/richclubv4-1.0.2.dev1-py3-none-any.whl

---
+++ packages/richclubv4/richclubv4-1.0.2.dev1-py3-none-any.whl/richclub/__init__.py
@@ -0,0 +1,16 @@
+__author__ = 'Haoruo Liu'
+__title__ = 'richclub'
+
+
+import richclub.rich_club_coefficient_degree
+from richclub.rich_club_coefficient_degree import *
+
+
+import richclub.rich_club_coefficient_rank
+from richclub.rich_club_coefficient_rank import *
+
+import richclub.secord_order_largest_degree
+from richclub.secord_order_largest_degree import *
+
+import richclub.secord_order_average_degree
+from richclub.secord_order_average_degree import *
