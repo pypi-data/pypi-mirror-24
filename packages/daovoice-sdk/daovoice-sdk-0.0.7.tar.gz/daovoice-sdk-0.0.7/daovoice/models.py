cd6ad020f055b9e0c5dea10df1e5b0bc28835a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package daovoice-sdk/0.0.7/daovoice-sdk-0.0.7.tar.gz

---
+++ packages/daovoice-sdk/daovoice-sdk-0.0.7.tar.gz/daovoice-sdk-0.0.7/daovoice/models.py
@@ -0,0 +1,71 @@
+# -*-encoding:utf-8-*-
+
+
+from utils.data_mapper import DataMapperBase
+
+
+class Model(DataMapperBase):
+    def __init__(self, src_data):
+        super(Model, self).__init__(src_data=src_data)
+
+        for k, v in iter(self):
+            setattr(self, k, v)
+
+        self.changed_attributes = []
+
+    @classmethod
+    def from_resp_json(cls, resp_json):
+        if isinstance(resp_json, dict):
+            return cls(resp_json)
+        elif isinstance(resp_json, list):
+            return [cls(l) for l in resp_json]
+
+    def __setattr__(self, key, value):
+        if hasattr(self, key) and getattr(self, key) != value:
+            self.changed_attributes.append(key)
+
+        super(Model, self).__setattr__(key, value)
+
+
+class Conversation(Model):
+    resource_path = "conversations"
+    mapper = {
+        "conversation_parts": ["conversation_parts", "daovoice.models.ConversationPart"],
+        "conversation_message": ["conversation_message", "daovoice.models.ConversationMessage"],
+        "user": ["user", "daovoice.models.User"]
+    }
+
+
+class ConversationPart(Model):
+    mapper = {"author": ["author", "daovoice.models.Author"]}
+
+
+class ConversationMessage(Model):
+    mapper = {"author": ["author", "daovoice.models.Author"]}
+
+
+class Author(Model):
+    mapper = {
+        "companies": ["companies", "daovoice.models.Company"]
+    }
+
+
+class Admin(Author):
+    resource_path = "admins"
+
+
+class Subscription(Model):
+    resource_path = "subscriptions"
+
+
+class User(Author):
+    resource_path = "users"
+
+
+class Company(Model):
+    pass
+
+
+class Event(Model):
+    resource_path = "events"
+
