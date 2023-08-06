10e1a3fca875ec4867d74c8c209256262c81f520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:11 +0000

    Add package singleton-decorator/1.0.0/singleton-decorator-1.0.0.tar.gz

---
+++ packages/singleton-decorator/singleton-decorator-1.0.0.tar.gz/singleton-decorator-1.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import singleton_decorator
+
+setup(
+    name = "singleton-decorator",
+    version = singleton_decorator.__version__,
+    fullname = "Singleton Decorator",
+    description = "A testable singleton decorator",
+    author = "Taras Gaidukov",
+    author_email = "kemaweyan@gmail.com",
+    keywords = "singleton decorator unittest",
+    long_description = open('README.rst').read(),
+    url = "https://github.com/Kemaweyan/singleton_decorator",
+    license = "GPLv3",
+    packages=find_packages(exclude=["*.tests"])
+)
