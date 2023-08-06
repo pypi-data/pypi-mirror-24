bbdb727109704b2fb32aa7273b0128fa25cffd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:19 +0000

    Add package PyMICE/1.2.1/PyMICE-1.2.1-cp34-none-win32.whl

---
+++ packages/PyMICE/PyMICE-1.2.1-cp34-none-win32.whl/pymice/_ObjectBase.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python
+# encoding: utf-8
+###############################################################################
+#                                                                             #
+#    PyMICE library                                                           #
+#                                                                             #
+#    Copyright (C) 2012-2017 Jakub M. Dzik a.k.a. Kowalski, S. Łęski          #
+#    (Laboratory of Neuroinformatics; Nencki Institute of Experimental        #
+#    Biology of Polish Academy of Sciences)                                   #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
+
+import numpy as np
+from operator import attrgetter
+from collections import Sequence
+
+class ObjectBase(object):
+  """
+  A base class for efficient object filtering.
+
+  >>> ob = ObjectBase()
+  >>> ob.get()
+  []
+
+  >>> len(ob)
+  0
+
+  >>> ob.put([ClassA(1, 4)])
+  >>> ob.get()
+  [ClassA(a=1, b=4)]
+
+  >>> len(ob)
+  1
+
+  >>> ob.put([ClassA(2, 2)])
+  >>> ob.get()
+  [ClassA(a=1, b=4), ClassA(a=2, b=2)]
+
+  >>> ob.put([ClassA(2, 2), ClassB(0, 0)])
+  >>> ob.get()
+  [ClassA(a=1, b=4), ClassA(a=2, b=2), ClassA(a=2, b=2), ClassB(c=0, d=0)]
+
+  >>> len(ob)
+  4
+
+  >>> tmp = ob.get()
+  >>> tmp.append(1)
+  >>> ob.get()
+  [ClassA(a=1, b=4), ClassA(a=2, b=2), ClassA(a=2, b=2), ClassB(c=0, d=0)]
+
+  >>> ob = ObjectBase()
+  >>> ob.put([ClassA(1, 4), ClassA(2, 2), ClassA(2, 2)])
+  >>> ob.get()
+  [ClassA(a=1, b=4), ClassA(a=2, b=2), ClassA(a=2, b=2)]
+
+  >>> tmp = ob.get()
+  >>> tmp[2].b = 42
+  >>> ob.get()
+  [ClassA(a=1, b=4), ClassA(a=2, b=2), ClassA(a=2, b=42)]
+
+  >>> tmp = []
+  >>> ob = ObjectBase()
+  >>> ob.put(tmp)
+  >>> ob.put([ClassA(2, 2), ClassB(0, 0)])
+  >>> tmp
+  []
+
+  >>> ob = ObjectBase()
+  >>> ob.put([ClassA(1, 4)])
+  >>> ob.get({'a': lambda x: x == 1})
+  [ClassA(a=1, b=4)]
+
+  >>> ob.get({'a': lambda x: x != 1})
+  []
+
+  >>> ob.put([ClassA(2, 2), ClassA(1, 2)])
+  >>> ob.get({'a': lambda x: x != 1})
+  [ClassA(a=2, b=2)]
+
+  >>> ob.get({'a': lambda x: x == 1,
+  ...         'b': lambda x: x == 2})
+  [ClassA(a=1, b=2)]
+
+  >>> ob.get({'a': lambda x: x == 2,
+  ...         'b': lambda x: x == 4})
+  []
+
+  >>> ob.get({})
+  [ClassA(a=1, b=4), ClassA(a=2, b=2), ClassA(a=1, b=2)]
+
+  >>> ob = ObjectBase()
+  >>> ob.get({'a': lambda x: x == 1,
+  ...         'b': lambda x: x == 2})
+  []
+
+  >>> ob = ObjectBase()
+  >>> ob.put([ClassA(ClassB(1, 2), 1), ClassA(ClassB(2, 3), 2),
+  ...         ClassA(ClassB(4, 3), 3)])
+  >>> ob.get({'b': lambda x: (x == 1) + (x == 3)})
+  [ClassA(a=ClassB(c=1, d=2), b=1), ClassA(a=ClassB(c=4, d=3), b=3)]
+
+  >>> ob.get({'a.d': lambda x: x == 3})
+  [ClassA(a=ClassB(c=2, d=3), b=2), ClassA(a=ClassB(c=4, d=3), b=3)]
+
+  >>> ob = ObjectBase({'a': lambda x: x.d - x.c})
+  >>> ob.put([ClassA(ClassB(1, 2), 1), ClassA(ClassB(2, 3), 2),
+  ...         ClassA(ClassB(4, 3), 3)])
+  >>> ob.get({'a': lambda x: x == 1})
+  [ClassA(a=ClassB(c=1, d=2), b=1), ClassA(a=ClassB(c=2, d=3), b=2)]
+
+  >>> ob.get({'a': (1,)})
+  [ClassA(a=ClassB(c=1, d=2), b=1), ClassA(a=ClassB(c=2, d=3), b=2)]
+
+  >>> ob.get({'a': ()})
+  []
+
+  >>> ob.get({'b': (1, 3)})
+  [ClassA(a=ClassB(c=1, d=2), b=1), ClassA(a=ClassB(c=4, d=3), b=3)]
+
+  >>> ob = ObjectBase({'a': lambda x: x.d - x.c})
+  >>> ob.get()
+  []
+
+  >>> ob = ObjectBase()
+  >>> ob.put((x * x for x in range(4)))
+  >>> ob.get()
+  [0, 1, 4, 9]
+  """
+  class MaskManager(object):
+    def __init__(self, values):
+      self.__values = np.array(values)
+      self.__cachedMasks = {}
+
+    def getMask(self, selector):
+      """
+      >>> mm = ObjectBase.MaskManager([])
+      >>> list(mm.getMask(lambda x: x != 0))
+      []
+
+      >>> list(mm.getMask(lambda x: x == 0))
+      []
+
+      >>> mm = ObjectBase.MaskManager([1, 2])
+      >>> list(mm.getMask(lambda x: x != 0))
+      [True, True]
+
+      >>> list(mm.getMask(lambda x: x == 0))
+      [False, False]
+
+      >>> list(mm.getMask(lambda x: x > 1))
+      [False, True]
+
+      >>> list(mm.getMask([]))
+      [False, False]
+
+      >>> list(mm.getMask([1]))
+      [True, False]
+
+      >>> list(mm.getMask([1, 2]))
+      [True, True]
+
+      >>> list(mm.getMask([3,]))
+      [False, False]
+
+      >>> mm = ObjectBase.MaskManager([u'a', u'a', u'b'])
+      >>> list(mm.getMask(['b']))
+      [False, False, True]
+      """
+      if hasattr(selector, '__call__'):
+        return selector(self.__values)
+
+      return self.__combineMasks(selector)
+
+    def __combineMasks(self, acceptedValues):
+      if not acceptedValues:
+        return np.zeros_like(self.__values, dtype=bool)
+
+      # XXX: Python3 fix
+      return self.__sumMasks(list(map(self.__getMasksMatchingValue, acceptedValues)))
+
+    def __sumMasks(self, masks):
+      if len(masks) == 1:
+        return masks[0]
+
+      return sum(masks[1:], masks[0])
+
+    def __getMasksMatchingValue(self, value):
+      try:
+        return self.__cachedMasks[value]
+
+      except KeyError:
+        return self.__makeAndCacheMask(value)
+
+    def __makeAndCacheMask(self, value):
+      mask = self.__values == value
+      self.__cachedMasks[value] = mask
+      return mask
+
+
+  def __init__(self, converters={}):
+    """
+    """
+    self.__objects = np.array([], dtype=object)
+    self.__cachedMaskManagers = {}
+    self.__converters = dict(converters)
+
+  def __len__(self):
+    return len(self.__objects)
+
+  def put(self, objects):
+    self.__objects = np.append(self.__objects,
+                               objects if isinstance(objects, Sequence) else list(objects))
+    self.__cachedMaskManagers.clear()
+
+  def get(self, filters=None):
+    return list(self.__getFilteredObjects(filters))
+
+  def __getFilteredObjects(self, filters):
+    if filters:
+      return self.__objects[self.__getProductOfMasks(filters)]
+
+    return self.__objects
+      
+  def __getProductOfMasks(self, selectors):
+    mask = True
+    for attributeName, selector in selectors.items():
+      mask = mask * self.__getMask(attributeName, selector)
+
+    return mask
+
+  def __getMask(self, attributeName, selector):
+    return self.__getMaskManager(attributeName).getMask(selector)
+
+  def __getMaskManager(self, attributeName):
+    try:
+      return self.__cachedMaskManagers[attributeName]
+
+    except KeyError:
+      maskManager = self.MaskManager(self.__getConvertedAttributeValues(attributeName))
+      self.__cachedMaskManagers[attributeName] = maskManager
+      return maskManager
+
+  def __getConvertedAttributeValues(self, attributeName):
+    attributeValues = self.getAttributes(attributeName)
+    if attributeName in self.__converters:
+      # XXX: Python3 fix - makes NumPy array working
+      return list(map(self.__converters[attributeName], attributeValues))
+
+    return attributeValues
+
+  def getAttributes(self, *attributeNames):
+    """
+    >>> ob = ObjectBase()
+    >>> ob.put([ClassA(ClassB(1, 2), 1), ClassA(ClassB(2, 3), 2),
+    ...        ClassA(ClassB(4, 3), 3)])
+    >>> ob.getAttributes('a')
+    [ClassB(c=1, d=2), ClassB(c=2, d=3), ClassB(c=4, d=3)]
+
+    >>> ob.getAttributes('b')
+    [1, 2, 3]
+
+    >>> ob.getAttributes('a.c')
+    [1, 2, 4]
+
+    >>> ob.getAttributes('a.c', 'b')
+    [(1, 1), (2, 2), (4, 3)]
+
+    >>> ob = ObjectBase()
+    >>> ob.getAttributes('a.c')
+    []
+
+    >>> ob = ObjectBase({'a': lambda x: x.c})
+    >>> ob.put([ClassA(ClassB(1, 2), 1)])
+    >>> ob.getAttributes('a')
+    [ClassB(c=1, d=2)]
+    """
+    # XXX: Python3 fix
+    return list(map(attrgetter(*attributeNames), self.__objects))
+
+
+if __name__ == '__main__':
+  import doctest
+  class ClassA(object):
+    def __init__(self, a, b):
+      self.a = a
+      self.b = b
+
+    def __repr__(self):
+      return "ClassA(a=%s, b=%s)" % (repr(self.a), repr(self.b))
+
+  class ClassB(object):
+    def __init__(self, c, d):
+      self.c = c
+      self.d = d
+
+    def __repr__(self):
+      return "ClassB(c=%s, d=%s)" % (repr(self.c), repr(self.d))
+
+  doctest.testmod(extraglobs={'ClassA': ClassA,
+                              'ClassB': ClassB})
---
+++ packages/PyMICE/PyMICE-1.2.1-cp34-none-win32.whl/pymice/_Tools.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+# encoding: utf-8
+###############################################################################
+#                                                                             #
+#    PyMICE library                                                           #
+#                                                                             #
+#    Copyright (C) 2012-2017 Jakub M. Dzik a.k.a. Kowalski, S. Łęski          #
+#    (Laboratory of Neuroinformatics; Nencki Institute of Experimental        #
+#    Biology of Polish Academy of Sciences)                                   #
+#                                                                             #
+#    This software is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This software is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this software.  If not, see http://www.gnu.org/licenses/.     #
+#                                                                             #
+###############################################################################
+
+import os
+import sys
+import time
+import warnings
+from datetime import datetime
+from math import modf
+from operator import attrgetter
+
+import pytz
+import zipfile
+
+from ._FixTimezones import LatticeOrderer
+
+# XXX: imported objects are imported from elsewhere
+if sys.version_info.major >= 3:
+  from ._Python3.Tools import isString, mapAsList
+
+  import io
+
+  class ArchiveZipFile(zipfile.ZipFile):
+    def open(self, *args, **kwargs):
+      return io.TextIOWrapper(super(ArchiveZipFile, self).open(*args, **kwargs))
+      # if sys.version_info < (3, 2):
+      #   # XXX: Python3 monkey-path
+      #   fh.readable = lambda: True
+      #   fh.writable = lambda: False
+      #   fh.seekable = lambda: False
+      #   fh.read1 = items_file.read
+      #   #io.BytesIO(fh.read())
+
+else:
+  from ._Python2.Tools import isString, mapAsList
+
+  class ArchiveZipFile(zipfile.ZipFile):
+    pass
+
+
+def timeString(x, tz=None):
+  return datetime.fromtimestamp(x, tz).strftime('%Y-%m-%d %H:%M:%S.%f%z')
+
+
+class PmWarnings(object):
+  class PmWarning(Warning):
+    """
+    A virtual class of PyMICE warnings always to be reported
+    - a hook for U{4180-wontfix-issue <http://bugs.python.org/issue4180>}
+    """
+    pass
+  
+  class PmDeprecationWarning(DeprecationWarning, PmWarning):
+    pass
+  
+  class PmUserWarning(UserWarning, PmWarning):
+    pass
+
+  __initialized__ = False
+
+  def __init__(self):
+    if not PmWarnings.__initialized__:
+      warnings.filterwarnings('always',
+                              category=PmWarnings.PmWarning)
+      PmWarnings.__initialized__ = True
+
+    self.enable()
+
+  def disable(self):
+    self.enabled = False
+
+  def enable(self):
+    self.enabled = True
+
+  def deprecated(self, message, stacklevel=1):
+    warnings.warn(message,
+                  self.PmDeprecationWarning if self.enabled else DeprecationWarning,
+                  stacklevel=stacklevel + 2)
+
+  def warn(self, message, stacklevel=1):
+    warnings.warn(message,
+                  self.PmUserWarning if self.enabled else UserWarning,
+                  stacklevel=stacklevel + 2)
+
+
+warn = PmWarnings()
+
+def deprecatedAlias(aliasedFunction, kind='method'):
+  def alias(*args, **kwargs):
+    warn.deprecated('{} deprecated - use {} instead'.format(kind,
+                                                            aliasedFunction.__name__))
+    return aliasedFunction(*args, **kwargs)
+
+  return alias
+
+
+
+def hTime(t):
+  """
+  Convert timestamp t to a human-readible UTC string.
+  """
+  dec, integer = modf(t)
+  return time.strftime("%Y-%m-%d %H:%M:%S" + ('{:01.3f}'.format(dec))[1:],
+                       time.gmtime(integer))
+
+EPOCH = datetime(1970,1,1)
+#UTC_OFFSET = time.mktime(EPOCH.timetuple())
+
+EPOCH_UTC = datetime(1970, 1, 1, tzinfo=pytz.UTC)
+
+def toTimestampUTC(x):
+  return (x - EPOCH_UTC).total_seconds()
+
+def toTimestamp(x):
+  return (x - EPOCH).total_seconds()
+
+
+
+def toDt(tmp):
+  if tmp is None or isinstance(tmp, datetime):
+    return tmp
+
+  if isinstance(tmp, (tuple, list)):
+    return datetime(*tmp)
+
+  if isinstance(tmp, dict):
+    return datetime(**tmp)
+
+  tmp = datetime.fromtimestamp(float(tmp)) # FIXME: seems to be broken
+
+
+
+def convertTime(tStr, tzinfo=None):
+  """
+  Converts time to timestamp - no timezones assumed.
+
+  >>> convertTime('1970-01-01 12:34:56.7')
+  datetime.datetime(1970, 1, 1, 12, 34, 56, 700000)
+  """
+  tSplit = tStr.replace('-', ' ').replace(':', ' ').split()
+  args = mapAsList(int, tSplit[:5])
+  secs = int(float(tSplit[5]) * 1000000) if len(tSplit) == 6 else 0
+  args.extend((secs // 1000000, secs % 1000000, tzinfo))
+  return datetime(*args)
+
+def timeToList(tStr):
+  date, time = tStr.split()
+  tokens = date.split('-') + time.split(':')
+
+  if len(tokens) == 5:
+    #return map(int, tokens) + [0, 0]
+    return LatticeOrderer.Node(map(int, tokens + [0, 0]))
+
+  decimal, seconds = modf(float(tokens[5]))
+  #return map(int, tokens[:5] + [seconds, round(decimal * 1000000)])
+  return LatticeOrderer.Node(map(int, tokens[:5] + [seconds, round(decimal * 1000000)]))
+
+
+class timeListList(list):
+  def __eq__(self, x):
+    return self[0] == x[0]
+
+  def __le__(self, x):
+    return self[0] <= x[0]
+
+  def __ge__(self, x):
+    return self[0] >= x[0]
+
+  def __ne__(self, x):
+    return self[0] != x[0]
+
+  def __lt__(self, x):
+    return self[0] < x[0]
+
+  def __gt__(self, x):
+    return self[0] > x[0]
+
+
+class DirectoryZipFile(object):
+  """
+  A class emulating zipfile.ZipFile behaviour with filesystem directories.
+  """
+  def __init__(self, path):
+    self.__path = path
+
+  def open(self, name, mode='r'):
+    fn = os.path.join(self.__path, name)
+    try:
+      return open(fn, mode)
+
+    except IOError:
+      raise KeyError(name)
+
+
+def groupBy(objects, getKey=lambda x: x, requiredKeys=()):
+  """
+  >>> import operator
+  >>> output = groupBy([(1, 2), (3, 4), (3, 2), (1, 1), (2, 1, 8)],
+  ...                  getKey=operator.itemgetter(1))
+  >>> for k in sorted(output):
+  ...   print("{} {}".format(k, output[k]))
+  1 [(1, 1), (2, 1, 8)]
+  2 [(1, 2), (3, 2)]
+  4 [(3, 4)]
+
+  >>> output = groupBy([(1, 2), (2, 1), (0, 2), (3, 0), (1, 1), (1, 1)],
+  ...                  getKey=lambda x: x[0] + x[1])
+  >>> for k in sorted(output):
+  ...   print("{} {}".format(k, output[k]))
+  2 [(0, 2), (1, 1), (1, 1)]
+  3 [(1, 2), (2, 1), (3, 0)]
+
+  >>> groupBy([], getKey=lambda x: x[0] + x[1])
+  {}
+
+  >>> output = groupBy([Pair(1, 2), Pair(1, 1), Pair(2, 1)], 'a')
+  >>> for k in sorted(output):
+  ...   print("{} {}".format(k, output[k]))
+  1 [Pair(a=1, b=2), Pair(a=1, b=1)]
+  2 [Pair(a=2, b=1)]
+
+  >>> output = groupBy([Pair(1, 2), Pair(1, 1), Pair(2, 1)], ('a', 'b'))
+  >>> for k in sorted(output):
+  ...   print("{} {}".format(k, output[k]))
+  (1, 1) [Pair(a=1, b=1)]
+  (1, 2) [Pair(a=1, b=2)]
+  (2, 1) [Pair(a=2, b=1)]
+
+  >>> output = groupBy([1, 2])
+  >>> for k in sorted(output):
+  ...   print("{} {}".format(k, output[k]))
+  1 [1]
+  2 [2]
+
+  >>> groupBy([], requiredKeys=['a'])
+  {'a': []}
+  """
+  return __groupByKey(objects,
+                      getKey if hasattr(getKey, '__call__') else __attrGetter(getKey),
+                      requiredKeys)
+
+def __attrGetter(attrs):
+  return attrgetter(attrs) if isString(attrs) else attrgetter(*attrs)
+
+def __groupByKey(objects, getKey, requiredKeys):
+  result = {key: [] for key in requiredKeys}
+  for o in objects:
+    key = getKey(o)
+    try:
+      result[key].append(o)
+
+    except KeyError:
+      result[key] = [o]
+
+  return result
+
+def mergeIntervalsValues(objects, getData, overlap=False, mergeWindow=None):
+  if len(objects) == 0:
+    return []
+
+  if not hasattr(getData, '__call__'):
+    getData = attrgetter(*getData)
+
+  result = []
+  data = sorted(map(getData, objects))
+  row = data[0]
+  lastStart, lastEnd = row[:2]
+  assert lastStart <= lastEnd
+
+  lastValue = row[2:]
+
+  for row in data[1:]:
+    start, end = row[:2]
+    value = row[2:]
+    assert overlap or lastEnd <= start
+    assert start <= end
+    if lastValue == value and (start - lastEnd <= mergeWindow or mergeWindow is None):
+      lastEnd = max(end, lastEnd)
+
+    else:
+      result.append((lastStart, lastEnd, lastValue))
+      lastStart, lastEnd, lastValue = start, end, value
+
+  result.append((lastStart, lastEnd, lastValue))
+  return result
