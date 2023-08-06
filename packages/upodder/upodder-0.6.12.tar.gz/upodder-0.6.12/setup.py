e37c1ad503001f57de4a487a5526bd40bf09a7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:13 +0000

    Add package upodder/0.6.12/upodder-0.6.12.tar.gz

---
+++ packages/upodder/upodder-0.6.12.tar.gz/upodder-0.6.12/setup.py
@@ -0,0 +1,39 @@
+import re
+import ast
+from setuptools import setup
+
+description = 'Command Line Podcast Downloader'
+
+setup(
+        name='upodder',
+        author='"Stan Vitkovskiy, Manuel Riel"',
+        author_email='manu@snapdragon.cc',
+        version='0.6.12',
+        license='LICENSE.txt',
+        url='https://github.com/m3nu/upodder',
+        description=description,
+        packages=['upodder', 'upodder.test'],
+        entry_points={'console_scripts': ['upodder = upodder.upodder:main']},
+        long_description=open('README.rst').read(),
+        install_requires=[
+            'clint',
+            'requests',
+            'feedparser',
+            'sqlobject >=3.0.0a2dev-20151224',
+            'tqdm',
+            'listparser'
+            ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+            ],
+        )
