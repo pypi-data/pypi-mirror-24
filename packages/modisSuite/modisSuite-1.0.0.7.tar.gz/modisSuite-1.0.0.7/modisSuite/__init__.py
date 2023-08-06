bfcfe5335acc19cc2e23ac263be6fed981b7f2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:16 +0000

    Add package modisSuite/1.0.0.7/modisSuite-1.0.0.7.tar.gz

---
+++ packages/modisSuite/modisSuite-1.0.0.7.tar.gz/modisSuite-1.0.0.7/modisSuite/__init__.py
@@ -0,0 +1,10 @@
+from .Downloader import telecharger as downloader
+from . import logMod
+from . import imageObject
+from . import Downloader
+from .mosaic import mosaic
+from .clip import clip
+from .subset import subset
+from .Compress import compress
+from .imageObject import imageModis
+from .imageObject import Afile
