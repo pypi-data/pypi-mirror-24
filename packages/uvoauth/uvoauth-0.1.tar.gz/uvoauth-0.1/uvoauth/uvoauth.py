4b7bd08dcb4d5c13291278f9487366b7cf5000c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:36 +0000

    Add package uvoauth/0.1/uvoauth-0.1.tar.gz

---
+++ packages/uvoauth/uvoauth-0.1.tar.gz/uvoauth-0.1/uvoauth/uvoauth.py
@@ -0,0 +1,10 @@
+class Oauth:
+    """
+    Oauth client for :mod:`uvhttp`.
+    
+    * ``session`` should be a :class:`uvhttp.http.Session`.
+    """
+    def __init__(self, session):
+        pass
+
+        
