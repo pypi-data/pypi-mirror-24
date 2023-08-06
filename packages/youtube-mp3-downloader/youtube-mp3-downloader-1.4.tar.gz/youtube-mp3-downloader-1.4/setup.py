62c94930825f383c3416b4dbc7de0505c1c63cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:36 +0000

    Add package youtube-mp3-downloader/1.4/youtube-mp3-downloader-1.4.tar.gz

---
+++ packages/youtube-mp3-downloader/youtube-mp3-downloader-1.4.tar.gz/youtube-mp3-downloader-1.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from setuptools import setup
+
+setup(
+    name='youtube-mp3-downloader',
+    version='1.4',
+    description='Download mp3 from youtube',
+    long_description='This python package lets you donwload mp3 from youtube videos to your local machine.',
+    author='Shubham Aggarwal',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    platform="any",
+    keywords="Youtube mp3 downloader offline local computer",
+    author_email='aggarwals5411@gmail.com',
+    url='https://github.com/shubhamaggarwal/youtube-mp3-downloader',
+    packages=['mp3'],
+    scripts=['bin/mp3'],
+    install_requires=[
+        "requests >= 2.18.1",
+        "beautifulsoup4 >= 4.6.0",
+        "PrettyTable"
+    ]
+)
