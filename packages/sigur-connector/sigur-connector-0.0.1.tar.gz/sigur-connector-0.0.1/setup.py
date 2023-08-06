f03d03f717a334095b6991c185734f3f41693b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:16 +0000

    Add package sigur-connector/0.0.1/sigur-connector-0.0.1.tar.gz

---
+++ packages/sigur-connector/sigur-connector-0.0.1.tar.gz/sigur-connector-0.0.1/setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='sigur-connector',
+    version='0.0.1',
+    author='GlebZaytsev',
+    author_email='winter1silent@gmail.com',
+    url='https://bitbucket.org/wintersilent/sigur_connector',
+    description='Sigur TCP wrapper',
+    download_url='https://bitbucket.org/wintersilent/sigur_connector/get/master.zip',
+    license='MIT',
+
+    packages=['sigur'],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
---
+++ packages/sigur-connector/sigur-connector-0.0.1.tar.gz/sigur-connector-0.0.1/sigur/__init__.py
@@ -0,0 +1,1 @@
+from .sigur import Sigur
