0535d6cdbdb44892daead4970814117b6af0ac44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:56 +0000

    Add package lawliet/2.4.9/lawliet-2.4.9.tar.gz

---
+++ packages/lawliet/lawliet-2.4.9.tar.gz/lawliet-2.4.9/lawliet/tools/cache.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import time
+CACHE = dict()
+
+
+class DoCache(object):
+    """缓存功能"""
+
+    @classmethod
+    def get(cls, key):
+        try:
+            a = CACHE[key]
+            if a[1] == 0:
+                return a[0]
+            elif a[1] > int(time.time()):
+                return a[0]
+            else:
+                return None
+        except (KeyError, IndexError):
+            return None
+
+    @classmethod
+    def set(cls, key, value, times=0):
+        try:
+            if times != 0:
+                times = int(time.time()) + int(times)
+            CACHE[key] = [value, times]
+        except:
+            class CacheSetError(Exception):
+                pass
+            raise CacheSetError()
+
+    @classmethod
+    def delete(cls, key):
+        CACHE.pop(key, None)
+
+    @classmethod
+    def expire(cls, key, times):
+        try:
+            a = CACHE[key]
+            a[1] = int(time.time()) + int(times)
+            CACHE[key] = a
+        except:
+            class CacheExpireError(Exception):
+                pass
+            raise CacheExpireError()
+
+    @classmethod
+    def id(cls):
+        return id(CACHE)
+
+    @classmethod
+    def get_all(cls):
+        return CACHE
---
+++ packages/lawliet/lawliet-2.4.9.tar.gz/lawliet-2.4.9/lawliet/tools/requests.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+
+import json as python_json
+import httplib
+import gzip
+import StringIO
+import urllib
+
+
+from lawliet.exc import (
+    RequestsUrlError,
+)
+
+
+def _unicode2str(uni):
+    if isinstance(uni, unicode):
+        uni = uni.encode('utf-8')
+    elif not isinstance(uni, str):
+        uni = str(uni)
+    return urllib.quote_plus(uni)
+
+
+def urlencode(kwargs):
+    return '&'.join([
+        _unicode2str(k) + '=' + _unicode2str(v)
+        for k, v in kwargs.items()
+    ])
+
+
+def _split_url(url):
+    """
+
+    :param url:
+    :return:
+     host
+     port
+     path
+     secure
+    """
+    if url:
+        if isinstance(url, unicode):
+            url = url.encode('utf-8')
+        protocol_l = url.split('//')
+        protocol = protocol_l.pop(0)
+
+        if protocol == 'http:':
+            secure = False
+        elif protocol == 'https:':
+            secure = True
+        else:
+            raise RequestsUrlError('Invalid URL %s' % url)
+
+        enpoint_l = ''.join(protocol_l).split('/')
+        host_port = enpoint_l.pop(0)
+        path = '/' + '/'.join(enpoint_l)
+
+        if ':' in host_port:
+            host, port = host_port.split(':')
+        else:
+            host = host_port
+            port = None
+        return host, port, path, secure
+
+    else:
+        raise RequestsUrlError()
+
+
+class RequestsResponse(object):
+
+    def __init__(self, response):
+        self.response = response
+
+    def json(self):
+        if self.headers.get('content-encoding') == 'gzip':
+            return python_json.loads(gzip.GzipFile(fileobj=StringIO.StringIO(self.response.read())).read())
+        else:
+            return python_json.loads(self.response.read())
+
+    @property
+    def content(self):
+        if self.headers.get('content-encoding') == 'gzip':
+            return gzip.GzipFile(fileobj=StringIO.StringIO(self.response.read())).read()
+        else:
+            return self.response.read()
+
+    @property
+    def status_code(self):
+        return self.response.status
+
+    @property
+    def headers(self):
+        return {k: v for k, v in self.response.getheaders()}
+
+    @property
+    def text(self):
+        return self.content
+
+
+class Requests(object):
+
+    @staticmethod
+    def get(url, params=None, headers=None, timeout=None):
+        host, port, path, secure = _split_url(url)
+        if secure:
+            http = httplib.HTTPSConnection(host=host, port=port, timeout=timeout)
+        else:
+            http = httplib.HTTPConnection(host=host, port=port, timeout=timeout)
+        if params:
+            if '?' in path:
+                path_l = path.split('?')
+                path = path_l.pop(0)
+                params.update({
+                    param.split('=')[0]: param.split('=')[1]
+                    for param in '?'.join(path_l).split('&')
+                    if len(param.split('=')) == 2
+                })
+
+            path += '?%s' % urlencode(params)
+
+        if headers:
+            headers['accept'] = '*/*'
+            headers.update({
+                'accept': '*/*',
+                'connection': 'keep-alive',
+            })
+        else:
+            headers = {
+                'accept': '*/*',
+                'connection': 'keep-alive',
+            }
+
+        http.request(method='GET', url=path, body=None, headers=headers)
+        return RequestsResponse(http.getresponse())
+
+    @staticmethod
+    def post(url, params=None, json=None, data=None, headers=None, timeout=None):
+        host, port, path, secure = _split_url(url)
+        if secure:
+            http = httplib.HTTPSConnection(host=host, port=port, timeout=timeout)
+        else:
+            http = httplib.HTTPConnection(host=host, port=port, timeout=timeout)
+
+        if json:
+            body = python_json.dumps(json)
+            headers = {'content-type': 'application/json'}
+        elif data:
+            body = data
+        else:
+            body = None
+
+        if params:
+            path += '?%s' % urlencode(params)
+
+        http.request(method='POST', url=path, body=body, headers=headers or {})
+        return RequestsResponse(http.getresponse())
+
+    @staticmethod
+    def delete(url, params=None, json=None, data=None, headers=None, timeout=None):
+        host, port, path, secure = _split_url(url)
+        if secure:
+            http = httplib.HTTPSConnection(host=host, port=port, timeout=timeout)
+        else:
+            http = httplib.HTTPConnection(host=host, port=port, timeout=timeout)
+        if json:
+            body = python_json.dumps(json)
+            headers = {'content-type': 'application/json'}
+        elif data:
+            body = data
+        else:
+            body = None
+
+        if params:
+            path += '?%s' % urlencode(params)
+
+        http.request(method='DELETE', url=path, body=body, headers=headers or {})
+        return RequestsResponse(http.getresponse())
+
+    @staticmethod
+    def put(url, params=None, json=None, data=None, headers=None, timeout=None):
+        host, port, path, secure = _split_url(url)
+        if secure:
+            http = httplib.HTTPSConnection(host=host, port=port, timeout=timeout)
+        else:
+            http = httplib.HTTPConnection(host=host, port=port, timeout=timeout)
+        if json:
+            body = python_json.dumps(json)
+            headers = {'content-type': 'application/json'}
+        elif data:
+            body = data
+        else:
+            body = None
+
+        if params:
+            path += '?%s' % urlencode(params)
+
+        http.request(method='PUT', url=path, body=body, headers=headers or {})
+        return RequestsResponse(http.getresponse())
