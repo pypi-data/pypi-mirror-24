05be41df80aa04173d04d37ae3e190edae603bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package curso/1.9/curso-1.9.tar.gz

---
+++ packages/curso/curso-1.9.tar.gz/curso-1.9/curso/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from curso import views
+urlpatterns = [
+	url(r'^get/search/$', views.search, name='search'),
+	url(r'^post/incWeight/$', views.incWeight, name='incWeight'),
+	url(r'^get/demo/$', views.demo, name='demo'),
+]
---
+++ packages/curso/curso-1.9.tar.gz/curso-1.9/curso/views.py
@@ -0,0 +1,31 @@
+from djangoApiDec.djangoApiDec import queryString_required
+from django.http import JsonResponse
+from .apps import SearchOb
+
+# Create your views here.
+@queryString_required(['keyword', 'school'])
+def search(request):
+	keyword = request.GET['keyword']
+	school = request.GET['school']
+	sob = SearchOb(keyword, school)
+				
+	return JsonResponse(sob.getResult(), safe=False)
+
+@queryString_required(['keyword', 'fullTitle'])
+def incWeight(request):
+	keyword = request.GET['keyword']
+	fullTitle = request.GET['fullTitle']
+	sob = SearchOb(keyword)
+	sob.incWeight(fullTitle)
+	return JsonResponse({"receive Weight success":1}, safe=False)
+
+@queryString_required(['keyword', 'school'])
+def demo(request):
+	from timetable.models import Course
+	keyword = request.GET['keyword']
+	school = request.GET['school']
+	sob = SearchOb(keyword, school)
+	codeList = sob.getResult()
+
+	result = [(Course.objects.filter(code=i)[0].title, i) for i in codeList]
+	return JsonResponse(result, safe=False)
