54f9119407c67f59f3b78c0ecf6bbda55e40472b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:25 +0000

    Add package rupture/1.4.5/rupture-1.4.5.tar.gz

---
+++ packages/rupture/rupture-1.4.5.tar.gz/rupture-1.4.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '1.4.5'
+
+packages = find_packages()
+
+setup(
+    name='rupture',
+    version=version,
+    description='A simple wrapper from requests and beautifulsoup.',
+    author='Shinz Natkid',
+    author_email='shinznatkid@gmail.com',
+    url='http://readthedocs.org/',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'requests>=1.0.0',
+        'beautifulsoup4>=4.0.0',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
