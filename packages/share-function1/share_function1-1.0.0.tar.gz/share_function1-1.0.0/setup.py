ad4ef56c6cfa546d3587e06626b396c664414efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:06 +0000

    Add package share-function1/1.0.0/share_function1-1.0.0.tar.gz

---
+++ packages/share-function1/share_function1-1.0.0.tar.gz/share_function1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'share_function1',
+	version		= '1.0.0',
+	py_modules 	= ['share_function1'],
+	author		= 'zhaohui',
+	author_email	= '1364496555@qq.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+	)
