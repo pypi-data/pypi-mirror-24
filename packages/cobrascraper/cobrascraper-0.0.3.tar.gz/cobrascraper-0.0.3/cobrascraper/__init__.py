8dbd28c38b061f8a47e677d6da46aacdea508841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:47 +0000

    Add package cobrascraper/0.0.3/cobrascraper-0.0.3.tar.gz

---
+++ packages/cobrascraper/cobrascraper-0.0.3.tar.gz/cobrascraper-0.0.3/cobrascraper/__init__.py
@@ -0,0 +1,4 @@
+from cobrascraper.extractor.extract import extract  # noqa: F401
+from cobrascraper.downloader.download import download  # noqa: F401
+from cobrascraper.downloader.download import get_source  # noqa: F401
+from cobrascraper.downloader.download import save_source  # noqa: F401
---
+++ packages/cobrascraper/cobrascraper-0.0.3.tar.gz/cobrascraper-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cobrascraper',
+    packages=find_packages(),
+    version='0.0.3',
+    description='Dymanic & scalable web scraping system',
+    author='Eddy Hintze',
+    author_email="eddy@hintze.co",
+    url="https://github.com/xtream1101/cobra",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    install_requires=['lxml',
+                      'cssselect',
+                      ]
+)
