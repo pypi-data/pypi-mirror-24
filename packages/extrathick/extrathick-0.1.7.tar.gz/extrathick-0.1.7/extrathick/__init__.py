4fa07a8c2c6b3f407ddb79ca8babf8cbfd037b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:12 +0000

    Add package extrathick/0.1.7/extrathick-0.1.7.tar.gz

---
+++ packages/extrathick/extrathick-0.1.7.tar.gz/extrathick-0.1.7/extrathick/__init__.py
@@ -0,0 +1,32 @@
+from steov import Anon
+
+def anonify (obj):
+    if isinstance(obj, dict):
+        return Anon({k: anonify(v) for k, v in obj.items()})
+    if isinstance(obj, (list, set, tuple)):
+        return type(obj)(map(anonify, obj))
+    return obj
+
+def unanonify (obj):
+    if isinstance(obj, Anon):
+        obj = vars(obj)
+    if isinstance(obj, dict):
+        return {k: unanonify(v) for k, v in obj.items()}
+    if isinstance(obj, (list, set, tuple)):
+        return type(obj)(map(unanonify, obj))
+    return obj
+
+
+def anonstat (st):
+    return Anon({attr: getattr(st, "st_"+attr) for attr in [
+        "mode",
+        "ino",
+        "dev",
+        "nlink",
+        "uid",
+        "gid",
+        "size",
+        "atime",
+        "mtime",
+        "ctime",
+    ]})
