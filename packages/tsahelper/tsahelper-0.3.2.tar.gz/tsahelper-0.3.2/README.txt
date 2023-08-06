68e73ba1b7f5a1f77eabd20fa3c411b858c9b8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:44 +0000

    Add package tsahelper/0.3.2/tsahelper-0.3.2.tar.gz

---
+++ packages/tsahelper/tsahelper-0.3.2.tar.gz/tsahelper-0.3.2/README.txt
@@ -0,0 +1,15 @@
+===========
+TSA Helper
+===========
+
+TSA Helper provides a number of functions that are useful for preprocessing 
+HD-AIT images provided by TSA for the Kaggle Passenger Screening Contest.  
+Typical usage looks like:
+
+    #!/usr/bin/env python
+
+    import tsahelper as tsa
+
+Detailed example usage is also available at:
+
+https://www.kaggle.com/jbfarrar/exploratory-data-analysis-and-example-generation
---
+++ packages/tsahelper/tsahelper-0.3.2.tar.gz/tsahelper-0.3.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='tsahelper',
+    version='0.3.2',
+    author='Brian Farrar',
+    author_email='brian.farrar@mavenwave.com',
+    packages=['tsahelper'],
+    url='http://pypi.python.org/pypi/tsahelper/',
+    license='LICENSE.txt',
+    install_requires=['numpy','seaborn','matplotlib','cv2','pandas','scipy'],
+    description='Useful preprocessing TSA HD-AIT image files.',
+    long_description=open('README.txt').read()
+)
