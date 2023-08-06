abcad3de2b17900ee4fcf1d5fb205ef0b539776b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:33 +0000

    Add package gluon/1.0.3/gluon-1.0.3.tar.gz

---
+++ packages/gluon/gluon-1.0.3.tar.gz/gluon-1.0.3/gluon/api/hooks/policy_enforcement.py
@@ -0,0 +1,67 @@
+#   Copyright 2016 Nokia
+#   All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import webob
+
+from oslo_config import cfg
+from oslo_policy import policy as oslo_policy
+# from oslo_utils import excutils
+from pecan import hooks
+
+from gluon import constants as gluon_constants
+from gluon import policy
+
+# from gluon._i18n import _
+
+
+class PolicyHook(hooks.PecanHook):
+    priority = 100
+
+    def before(self, state):
+
+        if cfg.CONF.api.auth_strategy == 'noauth':
+            return
+
+        if state.request.method not in ('GET', 'POST', 'PUT', 'DELETE'):
+            return
+
+        method = gluon_constants.ACTION_MAP[state.request.method]
+
+        path_info = state.request.path_info
+
+        if not path_info:
+            return
+
+        resource = state.request.context.get('resource')
+
+        if not resource:
+            return
+
+        action = "%s_%s" % (method, resource)
+
+        gluon_context = state.request.context.get('gluon_context')
+
+        policy.init()
+
+        try:
+            policy.enforce(
+                gluon_context, action, None)
+        except oslo_policy.PolicyNotAuthorized as e:
+            raise webob.exc.HTTPForbidden(str(e))
+
+    def after(self, state):
+        # This method could be used for implementing access control
+        # at the attribute level.
+        return
---
+++ packages/gluon/gluon-1.0.3.tar.gz/gluon-1.0.3/gluon/api/proton_controller.py
@@ -0,0 +1,58 @@
+#    Copyright 2015, Ericsson AB
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pecan
+from pecan import rest
+from wsme import types as wtypes
+import wsmeext.pecan as wsme_pecan
+
+from gluon.api.baseObject import APIBase
+from gluon.api import link
+from gluon.particleGenerator import generator as particle_generator
+from oslo_config import cfg
+
+
+class ProtonRoot(APIBase):
+    """The representation of the version 1 of the API."""
+
+    id = wtypes.text
+    """The ID of the version, also acts as the release number"""
+
+    links = [link.Link]
+
+    @staticmethod
+    def convert():
+        root = ProtonRoot()
+        root.id = "proton"
+        root.links = [link.Link.make_link('self', pecan.request.host_url,
+                                          'proton', '', bookmark=True), ]
+        return root
+
+
+class ProtonController(rest.RestController):
+    """Version 1 API controller root."""
+
+    def __init__(self):
+        services = str(cfg.CONF.api.service_list).split(',')
+        service_list = list()
+        for api_name in services:
+            service_list.append(api_name.strip())
+        particle_generator.build_api(self, service_list)
+
+    @wsme_pecan.wsexpose(ProtonRoot)
+    def get(self):
+        # NOTE: The reason why convert() is being called for every
+        #       request is because we need to get the host url from
+        #       the request object to make the links.
+        return ProtonRoot.convert()
