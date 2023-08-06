caaae053bd66034f97c31006d356ac39ffb173cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:26 +0000

    Add package pnutpy/0.2.0/pnutpy-0.2.0.tar.gz

---
+++ packages/pnutpy/pnutpy-0.2.0.tar.gz/pnutpy-0.2.0/HISTORY.rst
@@ -0,0 +1,15 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.1.0 (2016-12-02)
+++++++++++++++++++
+
+* initial release
+
+
+0.2.0 (2017-08-19)
+++++++++++++++++++
+
+* add most channel and message endpoints
---
+++ packages/pnutpy/pnutpy-0.2.0.tar.gz/pnutpy-0.2.0/pnutpy/consts.py
@@ -0,0 +1,112 @@
+PAGINATION_PARAMS = [
+    'since_id',
+    'before_id',
+    'count',
+]
+
+
+POST_PARAMS = [
+    'include_muted',
+    'include_deleted',
+    'include_directed_posts',
+    'include_bookmarked_by',
+    'include_reposted_by',
+    'include_raw',
+    'include_post_raw',
+    'include_user_raw',
+    'include_html',
+]
+
+
+USER_PARAMS = [
+    'include_raw',
+    'include_user_raw',
+    'include_html',
+]
+
+
+# USER_SEARCH_PARAMS = [
+    # 'q',
+    # 'count',
+# ]
+
+
+# POST_SEARCH_PARAMS = [
+    # 'index',
+    # 'order',
+    # 'query',
+    # 'text',
+    # 'hashtags',
+    # 'links',
+    # 'link_domains',
+    # 'mentions',
+    # 'leading_mentions',
+    # 'annotation_types',
+    # 'attachment_types',
+    # 'crosspost_url',
+    # 'crosspost_domain',
+    # 'place_id',
+    # 'is_reply',
+    # 'is_directed',
+    # 'has_location',
+    # 'has_checkin',
+    # 'is_crosspost',
+    # 'has_attachment',
+    # 'has_oembed_photo',
+    # 'has_oembed_video',
+    # 'has_oembed_html5video',
+    # 'has_oembed_rich',
+    # 'language',
+    # 'client_id',
+    # 'creator_id',
+    # 'reply_to',
+    # 'thread_id',
+# ]
+
+
+CHANNEL_PARAMS = [
+    'channel_types',
+    'include_marker',
+    'include_read',
+    'include_recent_message',
+    'include_raw',
+    'include_user_raw',
+    'include_message_raw',
+]
+
+
+MESSAGE_PARAMS = [
+    'include_muted',
+    'include_deleted',
+    'include_raw',
+    'include_message_raw',
+    'include_user_raw',
+    'include_html',
+]
+
+
+FILE_PARAMS = [
+    'file_types',
+    'include_incomplete',
+    'include_private',
+    'include_raw',
+    'include_file_raw',
+    'include_user_raw',
+]
+
+
+# PLACE_SEARCH_PARAMS = [
+    # 'latitude',
+    # 'longitude',
+    # 'q',
+    # 'radius',
+    # 'count',
+    # 'remove_closed',
+    # 'altitude',
+    # 'horizontal_accuracy',
+    # 'vertical_accuracy',
+# ]
+
+# APP_STREAM_PARAMS = [
+    # 'key',
+# ]
