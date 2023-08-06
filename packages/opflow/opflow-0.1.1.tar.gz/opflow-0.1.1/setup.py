fa638573469d6579635caf26d10a7b1507b1c6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:53 +0000

    Add package opflow/0.1.1/opflow-0.1.1.tar.gz

---
+++ packages/opflow/opflow-0.1.1.tar.gz/opflow-0.1.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+  name = 'opflow',
+  packages = ['opflow'],
+  version = '0.1.1',
+  description = 'Devebot Opflow for Python',
+  author = 'Devebot Team',
+  author_email = 'contact@devebot.com',
+  url = 'https://github.com/opflow/opflow-python',
+  download_url = 'https://github.com/opflow/opflow-python/archive/0.1.1.tar.gz',
+  keywords = ['devebot', 'opflow', 'amqplib'],
+  classifiers = [],
+)
