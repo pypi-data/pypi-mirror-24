73603ba577f3af71c97e591377e2c62a7f3a28fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:58 +0000

    Add package Ciw/1.1.3/Ciw-1.1.3.tar.gz

---
+++ packages/Ciw/Ciw-1.1.3.tar.gz/Ciw-1.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGES.rst') as changes_file:
+    changes = changes_file.read()
+
+with open('AUTHORS.rst') as authors_file:
+    authors = authors_file.read()
+
+# Read in the version number
+exec(open('ciw/version.py', 'r').read())
+
+setup(
+    name='Ciw',
+    version=__version__,
+    url='https://github.com/CiwPython/Ciw',
+    author='Geraint Palmer, Vincent Knight',
+    author_email='palmer.geraint@googlemail.com',
+    packages=['ciw'],
+    description='A discrete event simulation library for open queueing networks',
+    long_description=readme + '\n\n' + changes + '\n\n' + authors,
+    install_requires=["PyYAML", "networkx", "hypothesis", "tqdm"]
+)
