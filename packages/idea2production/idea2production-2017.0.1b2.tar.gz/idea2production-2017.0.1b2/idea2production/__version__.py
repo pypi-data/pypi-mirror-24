5310d095cce6cb1562da62d4e7aa4f100226d69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:30 +0000

    Add package idea2production/2017.0.1b2/idea2production-2017.0.1b2.tar.gz

---
+++ packages/idea2production/idea2production-2017.0.1b2.tar.gz/idea2production-2017.0.1b2/idea2production/__version__.py
@@ -0,0 +1,27 @@
+
+# M""MMMMM""MM                   dP
+# M  MMMMM  MM                   88
+# M         `M .d8888b. 88d888b. 88d888b. .d8888b. 88d888b.
+# M  MMMMM  MM 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88
+# M  MMMMM  MM 88.  .88 88       88.  .88 88.  .88 88
+# M  MMMMM  MM `88888P8 dP       88Y8888' `88888P' dP
+# MMMMMMMMMMMM
+#
+# MM"""""""`MM                                                       dP
+# MM  mmmm,  M                                                       88
+# M'        .M .d8888b. .d8888b. .d8888b. .d8888b. 88d888b. .d8888b. 88d888b.
+# MM  MMMb. "M 88ooood8 Y8ooooo. 88ooood8 88'  `88 88'  `88 88'  `"" 88'  `88
+# MM  MMMMM  M 88.  ...       88 88.  ... 88.  .88 88       88.  ... 88    88
+# MM  MMMMM  M `88888P' `88888P' `88888P' `88888P8 dP       `88888P' dP    dP
+# MMMMMMMMMMMM
+#
+# M""MMMMMMMM          dP                                    dP
+# M  MMMMMMMM          88                                    88
+# M  MMMMMMMM .d8888b. 88d888b. .d8888b. 88d888b. .d8888b. d8888P .d8888b. 88d888b. dP    dP
+# M  MMMMMMMM 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88   88   88'  `88 88'  `88 88    88
+# M  MMMMMMMM 88.  .88 88.  .88 88.  .88 88       88.  .88   88   88.  .88 88       88.  .88
+# M         M `88888P8 88Y8888' `88888P' dP       `88888P8   dP   `88888P' dP       `8888P88
+# MMMMMMMMMMM                                                                            .88
+#                                                                                    d8888P
+
+__version__ = '2017.0.1.b002'
