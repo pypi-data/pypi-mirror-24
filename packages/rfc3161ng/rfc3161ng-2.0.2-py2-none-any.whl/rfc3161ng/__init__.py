fc56fe68cc2980353cc1b69176a4dbc18a9a81ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:10 +0000

    Add package rfc3161ng/2.0.2/rfc3161ng-2.0.2-py2-none-any.whl

---
+++ packages/rfc3161ng/rfc3161ng-2.0.2-py2-none-any.whl/rfc3161ng/__init__.py
@@ -0,0 +1,29 @@
+from .types import (
+    TimeStampReq, MessageImprint, PKIFreeText, PKIStatus, PKIFailureInfo,
+    PKIStatusInfo, TimeStampResp, Accuracy, AnotherName, GeneralName,
+    TimeStampToken, TSTInfo,
+)
+from .constants import (
+    id_kp_timeStamping, id_sha1, id_sha256, id_sha384,
+    id_sha512, id_ct_TSTInfo, oid_to_hash,
+)
+from .api import (
+    RemoteTimestamper, check_timestamp, get_hash_oid,
+    TimestampingError, get_timestamp
+)
+
+__all__ = (
+    'VERSION',
+
+    'TimeStampReq', 'MessageImprint', 'PKIFreeText', 'PKIStatus', 'PKIFailureInfo',
+    'PKIStatusInfo', 'TimeStampResp', 'Accuracy', 'AnotherName', 'GeneralName',
+    'TimeStampToken', 'TSTInfo',
+
+    'id_kp_timeStamping', 'id_sha1', 'id_sha256', 'id_sha384',
+    'id_sha512', 'id_ct_TSTInfo', 'oid_to_hash',
+
+    'RemoteTimestamper', 'check_timestamp', 'get_hash_oid',
+    'TimestampingError', 'get_timestamp',
+)
+
+VERSION = '2.0.2'
