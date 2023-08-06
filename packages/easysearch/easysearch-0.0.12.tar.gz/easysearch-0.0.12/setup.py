da221319cbc970d46e8efed1ea853c955e0c144f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:09 +0000

    Add package easysearch/0.0.12/easysearch-0.0.12.tar.gz

---
+++ packages/easysearch/easysearch-0.0.12.tar.gz/easysearch-0.0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='easysearch',
+    description='Machine learning library for Easysearch',
+    packages=[
+        'easysearch',
+        'easysearch.doc2vec',
+        'easysearch.doc2vec.train',
+        'easysearch.lda',
+        'easysearch.lda.train',
+        'easysearch.solr',
+        'easysearch.utils'
+    ],
+    install_requires=[
+        'gensim>=2.3.0',
+        'numpy>=1.13.1',
+        'SolrClient>=0.2.1'
+    ],
+    version='0.0.12',
+    author='Pedro Castro',
+    author_email='pedro.castro@dataeasy.com.br',
+    url='http://github.com/DataEasy/easysearch'
+)
