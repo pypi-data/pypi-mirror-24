0b7753ce555c9b2c5bb5fc43d2ab036fe4042197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:38 +0000

    Add package flask_alcohol/0.2.2/flask_alcohol-0.2.2.tar.gz

---
+++ packages/flask_alcohol/flask_alcohol-0.2.2.tar.gz/flask_alcohol-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='flask_alcohol',
+      version='0.2.2',
+      description='Automatically generate API routes from Flask-SQLAlchemy models',
+      url='https://github.com/natfoster82/flask-alcohol',
+      author='Nat Foster',
+      author_email='nat.foster@gmail.com',
+      license='BSD',
+      packages=['flask_alcohol'],
+      install_requires=[
+          'flask_sqlalchemy',
+      ],
+      zip_safe=False)
