1cbd4fd87d775dc5a6f0406e52e542b9c8876837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:39 +0000

    Add package ftio/0.4/ftio-0.4.tar.gz

---
+++ packages/ftio/ftio-0.4.tar.gz/ftio-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'ftio',
+  packages = ['ftio'], # this must be the same as the name above
+  version = '0.4',
+  description = 'Python tools for working with fastText, an open-source library from the Facebook AI Research lab',
+  author = 'Signal N',
+  author_email = 'ftio@signaln.com',
+  url = 'https://github.com/signaln/ftio',
+  download_url = 'https://github.com/signaln/ftio/archive/0.4.tar.gz',
+  keywords = ['fastText', 'word2vec', 'word embeddings', 'natural language processing', 'text'],
+  classifiers = [],
+)
