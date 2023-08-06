863aff51cc2299a62454b9fb86e5772489bd0143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:45 +0000

    Add package grammpy/1.1.1/grammpy-1.1.1-py3.5.egg

---
+++ packages/grammpy/grammpy-1.1.1-py3.5.egg/grammpy/Rules/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 16.08.2017 19:28
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .Rule import Rule
+from ..Grammars.MultipleRulesGrammar import SplitRule
