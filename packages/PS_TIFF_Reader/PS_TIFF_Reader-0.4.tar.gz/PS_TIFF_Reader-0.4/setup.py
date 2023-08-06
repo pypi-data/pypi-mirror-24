99f3e19ad19bf04e8d6897d7c81810f0aad58d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:00 +0000

    Add package PS_TIFF_Reader/0.4/PS_TIFF_Reader-0.4.tar.gz

---
+++ packages/PS_TIFF_Reader/PS_TIFF_Reader-0.4.tar.gz/PS_TIFF_Reader-0.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 18:07:44 2017
+
+@author: alex
+"""
+
+from setuptools import setup, find_packages
+ 
+ 
+ 
+setup(name='PS_TIFF_Reader',
+ 
+      version='0.4',
+  
+      author='alex',
+ 
+      author_email='alex@parkafm.co.kr',
+ 
+      description='TIFF FILE READER',
+ 
+      packages=find_packages(exclude=['tests']),
+ 
+
+ 
+      zip_safe=False,
+ 
+      )
