91001ad13a3c3317679ff1290f2e700c42b673a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:42 +0000

    Add package aioautomatic/0.5.0/aioautomatic-0.5.0.tar.gz

---
+++ packages/aioautomatic/aioautomatic-0.5.0.tar.gz/aioautomatic-0.5.0/tests/test_validation.py
@@ -0,0 +1,54 @@
+"""Tests for automatic validation."""
+from datetime import datetime, timezone
+from aioautomatic import validation
+
+import pytest
+
+
+def test_valid_string_case_insensitive():
+    """Test that strings are matched insesnsitive."""
+    assert validation.string_case_insensitive("mock")("Mock") == "mock"
+    assert validation.string_case_insensitive("mock")("Mock") == "mock"
+
+
+def test_invalid_string_case_insensitive():
+    """Test that nonmatching strings fail validation."""
+    with pytest.raises(validation.vol.Invalid):
+        assert validation.string_case_insensitive("Mock")("mock2")
+
+
+def test_valid_timestamp():
+    """Test that a valid datetime returns the timestamp."""
+    dt = datetime(2017, 2, 21, 4, 35, 21, 123000, tzinfo=timezone.utc)
+    ts = validation.timestamp(dt)
+    assert ts == 1487651721.123
+
+
+def test_invalid_timestamp():
+    """Test that a valid datetime returns the timestamp."""
+    with pytest.raises(validation.vol.Invalid):
+        validation.timestamp("test")
+
+
+def test_coerce_existing_datetime():
+    """Test that a valid datetime is returned."""
+    dt = datetime(2017, 2, 21, 4, 35, 21, 123000, tzinfo=timezone.utc)
+    assert validation.coerce_datetime(dt) is dt
+
+
+def test_parse_datetime():
+    """Test that a valid string is parsed."""
+    dt = datetime(2014, 3, 20, 1, 43, 36, 738000, tzinfo=timezone.utc)
+    assert validation.coerce_datetime("2014-03-20T01:43:36.738000Z") == dt
+
+
+def test_parse_datetime_no_ms():
+    """Test that a valid string without milliseconds is parsed."""
+    dt = datetime(2014, 3, 20, 1, 43, 36, tzinfo=timezone.utc)
+    assert validation.coerce_datetime("2014-03-20T01:43:36Z") == dt
+
+
+def test_invalid_datetime():
+    """Test tahat an invalid string is not parsed."""
+    with pytest.raises(validation.vol.DatetimeInvalid):
+        validation.coerce_datetime("test")
