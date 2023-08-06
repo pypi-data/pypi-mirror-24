2b6e673534d7489de45380ed7c299459e6ed08f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:49 +0000

    Add package dlib/19.5.0/dlib-19.5.0.tar.gz

---
+++ packages/dlib/dlib-19.5.0.tar.gz/dlib-19.5.0/docs/docs/cache/dlib/revision.h
@@ -0,0 +1,8 @@
+#ifndef DLIB_REVISION_H
+// Version:  19.5
+// Date:     Sun Aug 27 19:34:26 EDT 2017
+// Mercurial Revision ID:  cbd9db3db5a2
+#define DLIB_MAJOR_VERSION  19
+#define DLIB_MINOR_VERSION  5
+#define DLIB_PATCH_VERSION  0
+#endif
