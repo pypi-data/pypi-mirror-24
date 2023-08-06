71bddc43ded2ada095e5c2daa76348dbe6107048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package htmlcompressor/1.1/htmlcompressor-1.1.tar.gz

---
+++ packages/htmlcompressor/htmlcompressor-1.1.tar.gz/htmlcompressor-1.1/htmlcompressor/cli.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os
+import click
+import htmlcompressor
+
+
+@click.command(help='compress html `source_or_path`')
+@click.argument('source_or_path')
+def compress(source_or_path):
+    if os.path.exists(source_or_path):
+        content = open(source_or_path, 'rb').read()
+        if hasattr(content, 'decode'):
+            content = content.decode()
+    else:
+        content = source_or_path
+    print(htmlcompressor.compress(content))
+
+
+if __name__ == '__main__':
+    compress()
