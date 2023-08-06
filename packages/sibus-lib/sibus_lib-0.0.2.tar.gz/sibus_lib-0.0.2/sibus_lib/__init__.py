3dac1329efe384938ddd5abe213bf0e43f1e5de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:33 +0000

    Add package sibus-lib/0.0.2/sibus_lib-0.0.2.tar.gz

---
+++ packages/sibus-lib/sibus_lib-0.0.2.tar.gz/sibus_lib-0.0.2/sibus_lib/__init__.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Ce module proclame la bonne parole de sieurs Sam et Max. Puissent-t-ils
+    retrouver une totale liberté de pensée cosmique vers un nouvel age
+    reminiscent.
+"""
+
+__version__ = "0.0.2"
+
+from DefaultLogger import mylogger
+from MessagingCore import BusElement, BusCore, MessageObject
+from SmartConfig import SmartConfigFile
+
