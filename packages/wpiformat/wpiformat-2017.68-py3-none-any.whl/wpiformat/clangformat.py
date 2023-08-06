2659bf7283c6f68fb8e4498b1f87fae3cb7ac4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:38 +0000

    Add package wpiformat/2017.68/wpiformat-2017.68-py3-none-any.whl

---
+++ packages/wpiformat/wpiformat-2017.68-py3-none-any.whl/wpiformat/clangformat.py
@@ -0,0 +1,30 @@
+"""This task runs clang-format on the file."""
+
+import subprocess
+import sys
+
+from . import task
+
+
+class ClangFormat(task.Task):
+
+    def should_process_file(self, name):
+        extensions = task.get_config("cExtensions") + \
+            task.get_config("cppHeaderExtensions") + \
+            task.get_config("cppSrcExtensions")
+
+        return any(name.endswith("." + ext) for ext in extensions)
+
+    def run_all(self, names):
+        args = ["-style=file", "-i"] + names
+        try:
+            returncode = subprocess.call(["clang-format-3.8"] + args)
+        except FileNotFoundError:
+            try:
+                returncode = subprocess.call(["clang-format"] + args)
+            except FileNotFoundError:
+                print(
+                    "Error: clang-format not found in PATH. Is it installed?",
+                    file=sys.stderr)
+                return False
+        return returncode == 0
