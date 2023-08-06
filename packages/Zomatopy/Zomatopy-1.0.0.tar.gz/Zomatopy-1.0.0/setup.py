b3374f7e8bf2dfd5608eed0b5c4021627f1b8691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:09 +0000

    Add package Zomatopy/1.0.0/Zomatopy-1.0.0.tar.gz

---
+++ packages/Zomatopy/Zomatopy-1.0.0.tar.gz/Zomatopy-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Zomatopy',
+    packages=['Zomatopy'],
+    version='1.0.0',
+    url='https://github.com/sharadbhat/Zomatopy',
+    description='A simple python wrapper for the Zomato API',
+    author='Sharad Bhat',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='Zomato',
+    install_requires=[
+        'requests==2.11.1'
+    ]
+)
