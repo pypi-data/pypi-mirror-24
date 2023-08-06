24c3bd6dd6a15e57b40d894f096536068c19173a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:13 +0000

    Add package it-movies/1.0.1/it_movies-1.0.1.tar.gz

---
+++ packages/it-movies/it_movies-1.0.1.tar.gz/it_movies-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='it_movies',
+    version='1.0.1',
+    py_modules=['it_movies'],
+    author='Iulian Constantinescu',
+    author_email='iulian@jxeelab.ro',
+    description='This module provides one function called print_lol() which prints lists that may or may not include nested lists.',
+    license='Public domain',
+    keywords='hello, world',
+)
