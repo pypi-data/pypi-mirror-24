40633e49d08db233844a757795ec588f91a690c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:24 +0000

    Add package fn-arg-validator/0.11/fn-arg-validator-0.11.tar.gz

---
+++ packages/fn-arg-validator/fn-arg-validator-0.11.tar.gz/fn-arg-validator-0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='fn-arg-validator',
+    version='0.11',
+    description="Provides a decorates which validates the arguments passed to a function",
+    author="aash",
+    author_email="aash.discover@gmail.com",
+    py_modules=['validator'],
+)
