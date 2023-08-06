489cfca8918d706e3f267eaaae6767b5092dfcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:40 +0000

    Add package h3mlcore/0.5.dev0/h3mlcore-0.5.dev0.tar.gz

---
+++ packages/h3mlcore/h3mlcore-0.5.dev0.tar.gz/h3mlcore-0.5.dev0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+
+setup(
+    name='h3mlcore',
+    version='0.5dev',
+    url='https://github.com/CognutSecurity/h3mlcore',
+    packages=find_packages(exclude=['test']),
+    author='Huang Xiao',
+    author_email='xh0217@gmail.com',
+    license='Creative Commons license',
+    long_description=open('README.md').read(),
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish (should match "license" above)
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='machine learning data mining',
+
+)
