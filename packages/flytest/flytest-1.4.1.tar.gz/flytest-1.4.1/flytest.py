4c99fb932bab00f13517392557a3d5cda405bcb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:00 +0000

    Add package flytest/1.4.1/flytest-1.4.1.tar.gz

---
+++ packages/flytest/flytest-1.4.1.tar.gz/flytest-1.4.1/flytest.py
@@ -0,0 +1,14 @@
+import sys
+
+"""这是一个“flytest.py”模块"""
+def print_lol(the_list,indent=False,level=0,fn=sys.stdout):
+    """函数说明"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,fn)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='',flie=fn)
+            print(each_item,file=fn)
+            
