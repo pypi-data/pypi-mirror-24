c703a12cdcaaf15ef92f3fac8d6f300ef36a2642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:23 +0000

    Add package djangorestframework-reactive/0.13.0/djangorestframework-reactive-0.13.0.tar.gz

---
+++ packages/djangorestframework-reactive/djangorestframework-reactive-0.13.0.tar.gz/djangorestframework-reactive-0.13.0/rest_framework_reactive/tests/models.py
@@ -0,0 +1,26 @@
+"""
+The models defined here are only used during testing.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.db import models
+
+
+class ExampleItem(models.Model):
+    enabled = models.BooleanField()
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        permissions = (
+            ("view_exampleitem", "Can view example item"),
+        )
+
+
+class ExampleSubItem(models.Model):
+    parent = models.ForeignKey(ExampleItem)
+    enabled = models.BooleanField()
+
+    class Meta:
+        permissions = (
+            ("view_examplesubitem", "Can view example sub item"),
+        )
---
+++ packages/djangorestframework-reactive/djangorestframework-reactive-0.13.0.tar.gz/djangorestframework-reactive-0.13.0/rest_framework_reactive/tests/views.py
@@ -0,0 +1,36 @@
+"""
+The views defined here are only used during testing.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from rest_framework import mixins, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from . import models, serializers
+
+
+class ExampleItemViewSet(mixins.RetrieveModelMixin,
+                         mixins.ListModelMixin,
+                         viewsets.GenericViewSet):
+
+    queryset = models.ExampleItem.objects.all()
+    serializer_class = serializers.ExampleItemSerializer
+    filter_fields = ('name', 'enabled')
+
+
+class ExampleSubItemViewSet(mixins.RetrieveModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+
+    queryset = models.ExampleSubItem.objects.all()
+    serializer_class = serializers.ExampleSubItemSerializer
+    filter_fields = ('parent__enabled', 'enabled')
+
+
+class PaginatedViewSet(mixins.RetrieveModelMixin,
+                       mixins.ListModelMixin,
+                       viewsets.GenericViewSet):
+
+    queryset = models.ExampleItem.objects.all()
+    serializer_class = serializers.ExampleItemSerializer
+    pagination_class = LimitOffsetPagination
