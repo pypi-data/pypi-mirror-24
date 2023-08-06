99b2cc78b13d519a1892a5c0e0ddf90a33828858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package amaasutils/1.2.4/amaasutils-1.2.4.tar.gz

---
+++ packages/amaasutils/amaasutils-1.2.4.tar.gz/amaasutils-1.2.4/amaasutils/hash.py
@@ -0,0 +1,16 @@
+import pickle
+from hashlib import sha256
+
+def compute_hash(attributes, ignored_attributes=None):
+    """
+    
+    """
+    if ignored_attributes is None:
+        ignored_attributes = {}
+
+    [attributes.pop(attr) for attr in ignored_attributes]
+    sorted_attributes = [(key, attributes[key]) for key in sorted(attributes.keys())]
+
+    hasher = sha256()
+    hasher.update(pickle.dumps(sorted_attributes))
+    return hasher.hexdigest()
