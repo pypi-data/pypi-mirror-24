5c5e1f6758145702e3ecbbc56cea478b6e5c1360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:01 +0000

    Add package drilsdown/0.15/drilsdown-0.15.tar.gz

---
+++ packages/drilsdown/drilsdown-0.15.tar.gz/drilsdown-0.15/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+try:
+    from jupyterpip import cmdclass
+except:
+    import pip, importlib
+    pip.main(['install', 'jupyter-pip']); cmdclass = importlib.import_module('jupyterpip').cmdclass
+
+setup(
+    name='drilsdown',
+    version='0.15',
+    url="https://github.com/Unidata/ipython-IDV",
+    author='Unidata',
+    author_email='drillsdown@ucar.edu',
+    license="MIT",
+    install_requires=['jupyter-pip'],
+    packages=['drilsdown'],
+    cmdclass=cmdclass('drilsdown', 'drilsdown/init'),
+    description="Jupyter extension for working with the IDV in a notebook ",
+    classifiers=[
+    'Development Status :: 4 - Beta',
+
+    'Topic :: Software Development :: Build Tools',
+     'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+]
+)
