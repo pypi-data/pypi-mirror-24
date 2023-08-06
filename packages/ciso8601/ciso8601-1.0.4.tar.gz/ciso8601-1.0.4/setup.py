a4df148de3484f5ec4535d902ecc363a112f638f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:23 +0000

    Add package ciso8601/1.0.4/ciso8601-1.0.4.tar.gz

---
+++ packages/ciso8601/ciso8601-1.0.4.tar.gz/ciso8601-1.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension
+
+setup(
+    name="ciso8601",
+    version="1.0.4",
+    description='Fast ISO8601 date time parser for Python written in C',
+    license="MIT",
+    ext_modules=[Extension("ciso8601", ["module.c"])],
+    test_suite='tests',
+    tests_require=['pytz'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
