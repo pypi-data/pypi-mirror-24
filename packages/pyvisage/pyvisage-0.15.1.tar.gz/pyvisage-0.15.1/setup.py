b2a18888a9bfc9881832dd3635f0fcfbc98b4ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:45 +0000

    Add package pyvisage/0.15.1/pyvisage-0.15.1.tar.gz

---
+++ packages/pyvisage/pyvisage-0.15.1.tar.gz/pyvisage-0.15.1/setup.py
@@ -0,0 +1,25 @@
+"""setup.py for visage."""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pyvisage',
+    packages=['visage'],
+    version='0.15.1',
+    description='Add virtual makeup to an image of a face.',
+    author='Hriddhi Dey',
+    author_email='hriddhidey@gmail.com',
+    url='https://github.com/hriddhidey/visage.git',
+    download_url='https://github.com/hriddhidey/visage/archive/0.15.1.tar.gz',
+    install_requires=[
+        'opencv-python',
+        'scikit-image',
+        'dlib',
+        'progressbar'
+    ],
+    include_package_data=True,
+    keywords='image processing virtual makeup face detection opencv',
+    classifiers=[],
+)
