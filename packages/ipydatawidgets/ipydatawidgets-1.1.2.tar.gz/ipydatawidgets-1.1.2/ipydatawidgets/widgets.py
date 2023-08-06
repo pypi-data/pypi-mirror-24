e8c53d53bf19b25a8d1345dde500473bd837c789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package ipydatawidgets/1.1.2/ipydatawidgets-1.1.2.tar.gz

---
+++ packages/ipydatawidgets/ipydatawidgets-1.1.2.tar.gz/ipydatawidgets-1.1.2/ipydatawidgets/widgets.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Common base widgets for ipydatawidgets.
+"""
+
+from ipywidgets import Widget
+from traitlets import Unicode
+
+module_name = "jupyter-datawidgets"
+module_version = "2.0.0"
+
+
+class DataWidget(Widget):
+    """An abstract widget class representing data.
+    """
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
---
+++ packages/ipydatawidgets/ipydatawidgets-1.1.2.tar.gz/ipydatawidgets-1.1.2/packages/jupyter-datawidgets/src/index.ts
@@ -0,0 +1,18 @@
+
+export {
+  NDArrayModel, JSONToArray, arrayToJSON, array_serialization,
+  IReceivedSerializedArray, ISendSerializedArray
+} from './ndarray';
+
+export {
+  JSONToUnion, JSONToUnionArray, unionToJSON, data_union_serialization,
+  data_union_array_serialization, getArrayFromUnion, listenToUnion
+} from './union';
+
+export {
+  JUPYTER_DATAWIDGETS_VERSION
+} from './version';
+
+export
+const version = (require('../package.json') as any).version;
+
