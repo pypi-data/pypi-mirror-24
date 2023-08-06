855fc2bfe72a13cc6ee3e09782ec6b560b96ae74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:47 +0000

    Add package ua2.forms/0.5.4/ua2.forms-0.5.4.tar.gz

---
+++ packages/ua2.forms/ua2.forms-0.5.4.tar.gz/ua2.forms-0.5.4/CHANGELOG.rst
@@ -0,0 +1,53 @@
+Changelog
+=========
+
+version 0.5.4
+
+* added SaveModelForm
+
+version 0.5.3
+
+* added Django 1.10 compatibility
+
+
+version 0.5.2
+
+* added method for base form class for override field ordering
+
+
+version 0.5.1
+
+* add compatibility with ManyRelatedManager in Django 1.8
+
+
+version 0.5.0
+
+* add DateTimeNaiveField field
+
+version 0.4.4
+
+* fix bug in LabelWidget format working with unicode
+
+version 0.4.3
+
+* fix issue with integer field valud 0 and set initial value for form
+
+version 0.4.2
+
+* fix issue in set_model_fields method
+
+
+version 0.4.0
+
+* add LabelWidget
+
+* add two functions for Form: set_field_error and set_non_field_error
+
+* improve Form.populate function
+
+
+version 0.3.0
+
+* add template tag ua2_render_field. This tag allow rendering fields
+with apped atributes. Suitable for bootstrap v3 templates
+
---
+++ packages/ua2.forms/ua2.forms-0.5.4.tar.gz/ua2.forms-0.5.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import os
+
+setup(
+    name='ua2.forms',
+    namespace_packages=['ua2'],
+    packages=find_packages('src'),
+    package_data={'': ['*.*']},
+    package_dir={'': 'src'},
+    entry_points={},
+    eager_resources=['ua2'],
+    version='0.5.4',
+    install_requires=['Django>=1.5', ],
+    license='BSD License',
+    include_package_data=True,
+    zip_safe=False,
+    author='Viacheslav Vic Bukhantsov',
+    author_email='vic@sdh.com.ua',
+    platforms=['OS Independent'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries'],
+    description='Alternative package for rendering tables',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://bitbucket.org/ua2web/ua2.forms',
+)
