0d72effc4b20faa78fdbe954daf48e5933dfc33f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package SteamAuthPy/2.0/SteamAuthPy-2.0.tar.gz

---
+++ packages/SteamAuthPy/SteamAuthPy-2.0.tar.gz/SteamAuthPy-2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name='SteamAuthPy',
+  packages=['SteamAuthPy'],
+  version='2.0',
+  description='Authenticate on Steam easily with Python 2.x',
+  author= 'ShellRox',
+  author_email='Shellrox@gmail.com',
+  url='https://github.com/ShellRox/SteamLoginPy',
+  download_url='https://github.com/ShellRox/SteamLoginPy/archive/master.zip',
+  keywords=['authentication', 'steam', 'simple'],
+  classifiers=[],
+)
