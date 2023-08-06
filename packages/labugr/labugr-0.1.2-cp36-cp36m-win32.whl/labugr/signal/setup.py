cc26b3463313134ae7c01b36933fa3ba58806121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:53 +0000

    Add package labugr/0.1.2/labugr-0.1.2-cp36-cp36m-win32.whl

---
+++ packages/labugr/labugr-0.1.2-cp36-cp36m-win32.whl/labugr/signal/setup.py
@@ -0,0 +1,17 @@
+from __future__ import division, print_function, absolute_import
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('signal', parent_package, top_path)
+
+    config.add_data_dir('tests') # Como tests no es considerado como un
+    							 # package, hay que anadirlo como 
+    							 # directorio de datos
+    config.add_subpackage('tools')
+    return config
+
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
