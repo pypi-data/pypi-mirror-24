5c787872304748570336ff9c46c01ae1d5b25ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:17 +0000

    Add package slack-sansio/0.0.2/slack_sansio-0.0.2-py3-none-any.whl

---
+++ packages/slack-sansio/slack_sansio-0.0.2-py3-none-any.whl/slack/io/sync.py
@@ -0,0 +1,20 @@
+import requests
+
+from . import abc
+
+
+class SlackAPI(abc.SlackAPI):
+
+    def _request(self, method, url, headers, body):
+
+        response = requests.request(method, url, headers=headers, data=body)
+        return response.status_code, response.headers, response.content
+
+    def post(self, *args, **kwargs):
+        return super().post(*args, **kwargs)
+
+    def postiter(self, *args, **kwargs):
+        yield from super().postiter(*args, **kwargs)
+
+    def rtm(self):
+        yield from super().rtm()
