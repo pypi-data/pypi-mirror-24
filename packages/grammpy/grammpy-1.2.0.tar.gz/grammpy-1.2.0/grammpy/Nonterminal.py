9c499a6d2fd579d550101244e4edf093f092e1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:09 +0000

    Add package grammpy/1.2.0/grammpy-1.2.0.tar.gz

---
+++ packages/grammpy/grammpy-1.2.0.tar.gz/grammpy-1.2.0/grammpy/Nonterminal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 28.07.2017 09:14
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .Rules import RuleChainable
+
+
+class Nonterminal(RuleChainable):
+    pass
