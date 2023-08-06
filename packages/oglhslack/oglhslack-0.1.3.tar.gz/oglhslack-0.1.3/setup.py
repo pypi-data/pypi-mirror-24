f62c2471735e0bd34d37e74558b3526ca5fc2124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:26 +0000

    Add package oglhslack/0.1.3/oglhslack-0.1.3.tar.gz

---
+++ packages/oglhslack/oglhslack-0.1.3.tar.gz/oglhslack-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'oglhslack',
+  packages = ['oglhslack'],
+  version = '0.1.3',
+  description = 'A Slack bot client for Lighthouse API',
+  author = 'Lighthouse Team',
+  author_email = 'engineering@opengear.com',
+  url = 'https://github.com/thiagolcmelo/oglhslack',
+  download_url = 'https://github.com/thiagolcmelo/oglhslack/archive/0.1.3.tar.gz',
+  keywords = ['api', 'opengear', 'lighthouse', 'slackbot'],
+  classifiers = [],
+  install_requires = ['pyyaml', 'slackclient', 'oglhclient'],
+  include_package_data=True,
+)
