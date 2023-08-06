681a8081ee99c3ad45d50f5ed0b6d734a841b2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:58 +0000

    Add package localnetwork/0.1/localnetwork-0.1.tar.gz

---
+++ packages/localnetwork/localnetwork-0.1.tar.gz/localnetwork-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='localnetwork',
+      version='0.1',
+      description='LocalNetwork runtime environment.',
+      url='https://github.com/freefall48/LocalNetwork',
+      author='Matthew Johnson',
+      author_email='mjohnson@localnetwork.nz',
+      license='proprietary',
+      packages=['LocalNetwork'],
+      zip_safe=False)
