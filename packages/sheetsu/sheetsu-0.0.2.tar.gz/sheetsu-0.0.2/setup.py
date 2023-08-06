e16f38cf69a850509d25a9b82da496550a6f69f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:03 +0000

    Add package sheetsu/0.0.2/sheetsu-0.0.2.tar.gz

---
+++ packages/sheetsu/sheetsu-0.0.2.tar.gz/sheetsu-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sheetsu',
+    version='0.0.2',
+    description='Sheetsu Python client',
+    url='http://github.com/andreffs18/sheetsu-python',
+    author='Andre Silva',
+    author_email='andreffs18@gmail.com',
+    license='MIT',
+    keywords='sheetsu api client sdk spreadsheet',
+    packages=['sheetsu'],
+    zip_safe=False
+)
