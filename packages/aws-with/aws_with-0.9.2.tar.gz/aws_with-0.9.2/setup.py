8b80b7863b4e034eb40ef23e565aeac755bb884e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:36 +0000

    Add package aws-with/0.9.2/aws_with-0.9.2.tar.gz

---
+++ packages/aws-with/aws_with-0.9.2.tar.gz/aws_with-0.9.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import aws_with
+
+setup(
+   name='aws_with',
+   version=aws_with.VERSION,
+   description='Command line utility to help manage AWS accounts, regions and organizations',
+   license="APACHE2",
+   long_description=open("README.rst").read(),
+   author='Mike Evans',
+   author_email='miev@amazon.com',
+   packages=['aws_with'],
+   install_requires=["boto3", "PyYAML"],
+   platforms='any',
+   data_files = [("", ["LICENSE.txt"])],
+   entry_points={'console_scripts': ['aws_with = aws_with.main:main']},
+   classifiers=['Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Intended Audience :: Developers',
+                'Intended Audience :: End Users/Desktop',
+                'Intended Audience :: Information Technology',
+                'Intended Audience :: System Administrators',
+                'License :: OSI Approved :: Apache Software License',
+                'Operating System :: POSIX',
+                'Operating System :: Microsoft :: Windows',
+                'Operating System :: MacOS :: MacOS X',
+                'Topic :: System :: Systems Administration',
+                'Topic :: Utilities',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7'],
+)
+
