d4d1476fb993176fdb10b1238aedd03ba35a3911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:16 +0000

    Add package pythonegardia/1.0.18/pythonegardia-1.0.18-py2-none-any.whl

---
+++ packages/pythonegardia/pythonegardia-1.0.18-py2-none-any.whl/pythonegardia/test.py
@@ -0,0 +1,7 @@
+import egardiadevice
+e = egardiadevice.EgardiaDevice("YOURIP",80,"YOURUSERNAME","YOURPASSWORD","")
+print(e.getstate())
+
+#import egardiaserver
+#es = egardiaserver.EgardiaServer(85)
+
