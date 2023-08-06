519950840e8bf1d7b582e505570e04364de8518a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package cartoview/1.2.69/cartoview-1.2.69.tar.gz

---
+++ packages/cartoview/cartoview-1.2.69.tar.gz/cartoview-1.2.69/cartoview/proxy/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from views import proxy_view
+
+urlpatterns = patterns(
+    '',
+    url(r'^(?P<url_name>[^/]+)/(?P<sub_url>.*)$',
+        proxy_view,
+        name='cartoview_proxy'),)
---
+++ packages/cartoview/cartoview-1.2.69.tar.gz/cartoview-1.2.69/cartoview/user_engage/viewer_widgets.py
@@ -0,0 +1,29 @@
+from django.templatetags.static import static
+widgets = [{
+    'title': 'User Engage',
+    'name': 'userEngage',
+    'config': {
+        'directive': '',
+        'dependencies': [],
+        'js': [],
+        "css": []
+    },
+    'view': {
+        'directive':
+            'collector',
+        'js': [
+            static("vendor/angular-resource/angular-resource.min.js"),
+            static("vendor/ng-image-appear/dist/ng-image-appear.min.js"),
+            static(
+                "vendor/lf-ng-md-file-input/dist/lf-ng-md-file-input.min.js"),
+            static("user_engage/js/app.js"),
+            static("user_engage/js/services.js"),
+            static("user_engage/js/directives.js"),
+        ],
+        "css": [
+            static("user_engage/css/main.css"),
+            static(
+                "vendor/lf-ng-md-file-input/dist/lf-ng-md-file-input.min.css"),
+        ]
+    },
+}]
