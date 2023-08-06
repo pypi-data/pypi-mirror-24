cf2486035eb0b22b8482efc9a0aed201ba43c0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:59 +0000

    Add package pact-test/0.1.31/pact-test-0.1.31.tar.gz

---
+++ packages/pact-test/pact-test-0.1.31.tar.gz/pact-test-0.1.31/pact_test/runners/pact_tests_runner.py
@@ -0,0 +1,30 @@
+from pact_test.either import *
+from pact_test.utils.logger import *
+from pact_test.config.config_builder import Config
+from pact_test.runners.service_consumers.test_suite import ServiceConsumerTestSuiteRunner
+from pact_test.runners.service_providers.provider_tests_runner import ProviderTestsRunner
+
+
+def verify(verify_consumers=False, verify_providers=False):
+    config = Config()
+
+    if verify_consumers:
+        run_consumer_tests(config)
+    if verify_providers:
+        run_provider_tests(config)
+
+
+def run_consumer_tests(config):
+    test_results = ServiceConsumerTestSuiteRunner(config).verify()
+    if type(test_results) is Left:
+        error(test_results.value)
+    else:
+        debug(test_results.value)
+        if type(test_results.value) is Left:
+            error(test_results.value.value)
+        else:
+            info(test_results.value.value)
+
+
+def run_provider_tests(config):
+    ProviderTestsRunner(config).verify()
