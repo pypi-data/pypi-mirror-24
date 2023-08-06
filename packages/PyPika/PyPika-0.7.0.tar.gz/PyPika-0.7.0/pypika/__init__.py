5e93dc4ed2d1358a891270b7dd0b40cef42b7d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:06 +0000

    Add package PyPika/0.7.0/PyPika-0.7.0.tar.gz

---
+++ packages/PyPika/PyPika-0.7.0.tar.gz/PyPika-0.7.0/pypika/__init__.py
@@ -0,0 +1,59 @@
+# coding: utf8
+"""
+PyPika is divided into a couple of modules, primarily the ``queries`` and ``terms`` modules.
+
+pypika.queries
+--------------
+
+This is where the ``Query`` class can be found which is the core class in PyPika.  Also, other top level classes such
+as ``Table`` can be found here.  ``Query`` is a container that holds all of the ``Term`` types together and also
+serializes the builder to a string.
+
+pypika.terms
+------------
+
+This module contains the classes which represent individual parts of queries that extend the ``Term`` base class.
+
+pypika.functions
+----------------
+
+Wrappers for common SQL functions are stored in this package.
+
+pypika.enums
+------------
+
+Enumerated values are kept in this package which are used as options for Queries and Terms.
+
+
+pypika.utils
+------------
+
+This contains all of the utility classes such as exceptions and decorators.
+
+"""
+from .enums import (Order,
+                    JoinType,
+                    DatePart)
+from .queries import (Query,
+                      Table,
+                      make_tables as Tables,
+                      Tuple)
+from .terms import (Field,
+                    Case,
+                    Not,
+                    Interval,
+                    Rollup)
+from .utils import (JoinException,
+                    GroupingException,
+                    CaseException,
+                    UnionException,
+                    RollupException)
+from .dialects import (MSSQLQuery,
+                       MySQLQuery,
+                       OracleQuery,
+                       PostgreSQLQuery,
+                       VerticaQuery)
+
+__author__ = "Timothy Heys"
+__email__ = "theys@kayak.com"
+__version__ = "0.7.0"
