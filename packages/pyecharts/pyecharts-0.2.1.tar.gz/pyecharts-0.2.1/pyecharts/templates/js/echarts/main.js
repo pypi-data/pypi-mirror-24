31ba502e69d9bc9ae43c3370f6054daf31f6f971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package pyecharts/0.2.1/pyecharts-0.2.1.tar.gz

---
+++ packages/pyecharts/pyecharts-0.2.1.tar.gz/pyecharts-0.2.1/pyecharts/templates/js/echarts/main.js
@@ -0,0 +1,8 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function load_ipython_extension() {
+        console.log("jupyter-echarts has been loaded");
+    }
+    exports.load_ipython_extension = load_ipython_extension;
+});
