b8f64b93d6813cf6a32d349e4c3a4d89862b7727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:25 +0000

    Add package django-codenerix-pos/1.0.4/django-codenerix-pos-1.0.4.tar.gz

---
+++ packages/django-codenerix-pos/django-codenerix-pos-1.0.4.tar.gz/django-codenerix-pos-1.0.4/codenerix_pos/static/codenerix_pos/js/local_uuid.js
@@ -0,0 +1,38 @@
+var uuid_websocket = undefined;
+var key_websocket = undefined;
+$(function() {
+    // When we're using HTTPS, use WSS too.
+    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+    var ws_path = ws_scheme + '://127.0.0.1:8080/codenerix_pos_client/';
+    // console.log("Connecting to " + ws_path)
+    var socket = new WebSocket(ws_path);
+    if (socket) {
+        socket.onmessage = function(message) {
+            socket.close();
+            try {
+                var data = JSON.parse(message.data);
+            } catch (e) {
+                var data = null;
+            }
+            if (typeof(data.uuid) != 'undefined') {
+                uuid_websocket = data.uuid;
+                key_websocket = data.key;
+                
+                data['csrfmiddlewaretoken'] = $("input[name='csrfmiddlewaretoken']").val();
+                $.post('/codenerix_pos/pos_session', data, function(data){
+                    if (data['msg'] != 'OK'){
+                        console.log(data['txt']);
+                    }
+                }).done(function(data){
+
+                }).fail(function(data){
+                    alert(data);
+                }).always(function(data){
+
+                });
+            } else {
+                console.error("I didn't get an UUID, I got: "+data);
+            }
+        };
+    }
+});
