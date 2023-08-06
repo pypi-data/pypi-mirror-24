7e9c340c3860a3b1b36e59cbb53850422a02b4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:59 +0000

    Add package robotica/0.1.23/robotica-0.1.23-py2.py3-none-any.whl

---
+++ packages/robotica/robotica-0.1.23-py2.py3-none-any.whl/robotica/cli.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for Robotica."""
+import asyncio
+import logging
+
+import click
+import click_log
+from hbmqtt.client import MQTTClient
+
+from robotica.executor import Executor
+from robotica.inputs.http import HttpInput
+from robotica.inputs.mqtt import MqttInput
+from robotica.outputs.audio import AudioOutput
+from robotica.outputs.lifx import LifxOutput
+from robotica.outputs.mqtt import MqttOutput
+from robotica.schedule import Schedule
+
+logger = logging.getLogger('robotica')
+click_log.basic_config(logger)
+
+
+@click.command()
+@click.option('--audio', default="config/audio.yaml", help='Path to audio config.')
+@click.option('--lifx', default="config/lifx.yaml", help='Path to LIFX config.')
+@click.option('--executor', default="config/executor.yaml", help='Path to executor config.')
+@click.option('--schedule', default="config/schedule.yaml", help='Path to schedule config.')
+@click.option('--http', default="config/http.yaml", help='Path to HTTP config.')
+@click.option('--mqtt', default="config/mqtt.yaml", help='Path to MQTT config.')
+@click_log.simple_verbosity_option(logger)
+def main(
+        audio: str, lifx: str, executor: str,
+        schedule: str, http: str, mqtt: str) -> None:
+    """Console script for robotica."""
+    loop = asyncio.get_event_loop()
+    client = MQTTClient()
+
+    lifx_output = LifxOutput(loop, lifx)
+    lifx_output.start()
+
+    audio_output = AudioOutput(loop, audio)
+
+    mqtt_output = MqttOutput(loop, mqtt, client)
+
+    executor_obj = Executor(loop, executor)
+    executor_obj.add_output(audio_output)
+    executor_obj.add_output(lifx_output)
+    executor_obj.add_output(mqtt_output)
+
+    schedule_obj = Schedule(schedule, executor_obj)
+    schedule_obj.start()
+
+    http_input = HttpInput(loop, http, executor_obj, schedule_obj)
+    http_input.start()
+
+    mqtt_input = MqttInput(loop, mqtt, executor_obj, schedule_obj, client)
+    mqtt_input.start()
+
+    try:
+        loop.run_forever()
+    finally:
+        mqtt_input.stop()
+        http_input.stop()
+        schedule_obj.stop()
+        lifx_output.stop()
+        pending = asyncio.Task.all_tasks()
+        for p in pending:
+            p.cancel()
+            try:
+                loop.run_until_complete(p)
+            except asyncio.CancelledError:
+                pass
+        loop.close()
---
+++ packages/robotica/robotica-0.1.23-py2.py3-none-any.whl/robotica/inputs/mqtt.py
@@ -0,0 +1,111 @@
+import asyncio
+import json
+import logging
+import platform
+
+import yaml
+
+from typing import Any, Optional
+
+from hbmqtt.client import MQTTClient, ClientException, QOS_0
+
+from robotica.executor import Executor
+from robotica.inputs import Input
+from robotica.schedule import Schedule
+
+logger = logging.getLogger(__name__)
+
+JsonType = Any
+
+
+class MqttInput(Input):
+    def __init__(
+            self, loop: asyncio.AbstractEventLoop,
+            config: str,
+            executor: Executor,
+            schedule: Schedule,
+            client: MQTTClient) -> None:
+        self._loop = loop
+        with open(config, "r") as file:
+            self._config = yaml.safe_load(file)
+        self._disabled = self._config['disabled']
+        self._broker_url = self._config['broker_url']
+        self._executor = executor
+        self._schedule = schedule
+        self._task = None  # type: Optional[asyncio.Task]
+        self._client = client
+
+    def start(self) -> None:
+        if not self._disabled:
+            self._task = self._loop.create_task(self._mqtt())
+
+    def stop(self) -> None:
+        if not self._disabled and self._task is not None:
+            self._client.unsubscribe('/execute/')
+            self._task.cancel()
+            try:
+                self._loop.run_until_complete(self._task)
+            except asyncio.CancelledError:
+                pass
+
+    async def _execute(self, data: JsonType) -> None:
+
+        reply_topic = data.get('reply_topic', None)
+        server = platform.node()
+
+        async def reply(data: JsonType) -> None:
+            client = self._client
+            raw_data = json.dumps(data).encode('UTF8')
+            if reply_topic is not None:
+                await client.publish(reply_topic, raw_data, qos=QOS_0)
+
+        try:
+            locations = data['locations']
+            actions = data['actions']
+        except KeyError:
+            logger.error("Required value missing.")
+            await reply({'status': 'error', 'server': server, })
+            return
+
+        try:
+            await reply({'status': 'processing', 'server': server, })
+            await self._executor.do_actions(locations, actions)
+            await reply({'status': 'success', 'server': server, })
+        except Exception as e:
+            logger.exception("Error in _execute")
+            await reply({'status': 'error', 'server': server, })
+
+        return
+
+    async def _process(self, topic: str, data: JsonType) -> None:
+        if topic == "/execute":
+            await self._execute(data)
+
+    async def _mqtt(self) -> None:
+        client = self._client
+        await client.connect(self._broker_url)
+        await client.subscribe([
+            ('/execute', QOS_0),
+        ])
+
+        while True:
+            try:
+                message = await client.deliver_message()
+                packet = message.publish_packet
+                topic = packet.variable_header.topic_name
+                raw_data = bytes(packet.payload.data).decode('UTF8')
+
+                try:
+                    data = json.loads(raw_data)
+                    await self._process(topic, data)
+                except json.JSONDecodeError as e:
+                    logger.error("JSON Error %s" % e)
+
+            except asyncio.CancelledError:
+                await client.unsubscribe(['/execute'])
+                await client.disconnect()
+                raise
+            except ClientException as e:
+                logger.error("Client exception: %s" % e)
+            except Exception as e:
+                logger.exception("Unknown exception.")
