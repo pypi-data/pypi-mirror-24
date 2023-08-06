1c9c0f29806bcbf9d61a30bcff7cf75ffe3c558b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:31 +0000

    Add package cmdo-App/0.1/cmdo-App-0.1.tar.gz

---
+++ packages/cmdo-App/cmdo-App-0.1.tar.gz/cmdo-App-0.1/cmdo/term.py
@@ -0,0 +1,37 @@
+import os
+
+# TOOD - Are these valid on unix
+# http://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+colors = {
+    'DANGER': '\033[91m',
+    'WARNING': '\033[33m',
+    'MILD': '\033[94m',
+    'OK': '\033[92m',
+    'RESET': '\033[0m',
+    'BLUE': '\033[94m',
+    'CYAN': '\033[96m',
+}
+
+def color(text, color):
+    if color in colors:
+        return ''.join([colors[color], text, colors['RESET']])
+    return text
+
+
+def reset_terminal():
+    # TODO - Check if this works on unix
+    os.system('color')
+
+# HEADER = '\033[95m'
+# OKBLUE = '\033[94m'
+# OKGREEN = '\033[92m'
+# WARNING = '\033[93m'
+# FAIL = '\033[91m'
+# ENDC = '\033[0m'
+# BOLD = '\033[1m'
+# UNDERLINE = '\033[4m'
+# NORMAL = '\033[0m'
+
+# This could be the reset command in windows, currently using os.system('color')
+
