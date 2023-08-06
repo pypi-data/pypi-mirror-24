b8bb4075e032d4ce280347642b25056cf85f658c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:21 +0000

    Add package amaasinfra/0.2.7.9/amaasinfra-0.2.7.9.tar.gz

---
+++ packages/amaasinfra/amaasinfra-0.2.7.9.tar.gz/amaasinfra-0.2.7.9/amaasinfra/infra/cache/asset_cache_item.py
@@ -0,0 +1,19 @@
+import os
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
+
+class AssetCacheItem(Model):
+    """
+    Cached record for asset
+    """
+    class Meta:
+        environment = os.environ.get('AWS_ENV') or 'dev'
+        table_name = "asset-cache-{}".format(environment)
+    asset_id = UnicodeAttribute(hash_key=True)
+    asset_manager_id = UnicodeAttribute(range_key=True)
+    asset_class = UnicodeAttribute(null=False)
+    asset_type = UnicodeAttribute(null=False)
+    asset_status = UnicodeAttribute(null=False)
+    fungible = BooleanAttribute()
+    updated_time = UnicodeAttribute(null=False)
+    hash_value = UnicodeAttribute(null=False)
