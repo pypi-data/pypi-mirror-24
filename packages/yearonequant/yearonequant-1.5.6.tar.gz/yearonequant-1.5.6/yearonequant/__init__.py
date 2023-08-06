af12480b5d1743e1b662e7fc016bcd3a1600d2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:47 +0000

    Add package yearonequant/1.5.6/yearonequant-1.5.6.tar.gz

---
+++ packages/yearonequant/yearonequant-1.5.6.tar.gz/yearonequant-1.5.6/yearonequant/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['util_quant', 'event_object', 'event', 'factor']
+from yearonequant import util_quant
+from yearonequant import event_object
+from yearonequant import event
+from yearonequant import factor
