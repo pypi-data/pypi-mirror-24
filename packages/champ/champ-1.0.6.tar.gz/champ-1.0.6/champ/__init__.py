874499fb4e13170edda55e9a88cfb8ea0b77b301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:17 +0000

    Add package champ/1.0.6/champ-1.0.6.tar.gz

---
+++ packages/champ/champ-1.0.6.tar.gz/champ-1.0.6/champ/__init__.py
@@ -0,0 +1,24 @@
+'''
+CHAMP (Convex Hull of Admissible Modularity Partitions) implementation
+'''
+from __future__ import absolute_import
+
+from .plot_domains import plot_2d_domains
+from .plot_domains import plot_single_layer_modularity_domains
+from .plot_domains import plot_line_halfspaces
+from .plot_domains import plot_line_coefficients
+from .plot_domains import plot_similarity_heatmap_single_layer
+
+from .champ_functions import create_coefarray_from_partitions
+from .champ_functions import create_halfspaces_from_array
+from .champ_functions import get_intersection
+from .champ_functions import get_random_halfspaces
+
+from .louvain_ext import PartitionEnsemble
+from .louvain_ext import parallel_louvain
+from .louvain_ext import run_louvain
+
+
+
+
+
---
+++ packages/champ/champ-1.0.6.tar.gz/champ-1.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+options=dict( name='champ',
+    version='1.0.6',
+    packages=['champ'],
+    url='http://github.com/wweir827/champ',
+    license='GPLv3+',
+    author='William H. Weir',
+    provides=['champ'],
+    author_email='wweir@med.unc.edu',
+    description='Modularity based networks partition selection tool',
+    zip_safe=False,
+    classifiers=["Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.3",
+                 "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                 "Topic :: Scientific/Engineering :: Information Analysis",
+                 ],
+    install_requires=['ipython<5.9','pyhull','matplotlib','future','numpy>1.13','h5py',
+                      'scipy','sklearn','louvain','python-igraph']
+)
+#    install_requires=['pyhull','igraph','louvain','matplotlib','numpy',]
+
+setup(**options)
