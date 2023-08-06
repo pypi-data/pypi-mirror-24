b68f9b4fae70abd1811d332bcd6e538d0f6cf7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:38 +0000

    Add package http_client/0.1.17/http_client-0.1.17.tar.gz

---
+++ packages/http_client/http_client-0.1.17.tar.gz/http_client-0.1.17/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="http_client",
+    version="0.1.17",
+    # description="Fast and robust HTTP client based on PyCurl",
+    # url="https://github.com/huynhminhson/http_client",
+    # author="Son Huynh",
+    # author_email="minhsonftu@gmail.com",
+    # license="MIT",
+    packages=["http_client"],
+    install_requires=["pycurl", "six"],
+)
