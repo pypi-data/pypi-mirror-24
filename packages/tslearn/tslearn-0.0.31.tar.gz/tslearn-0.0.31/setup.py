21575cd6d2888249a68173b69aeeefb7ad3283fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package tslearn/0.0.31/tslearn-0.0.31.tar.gz

---
+++ packages/tslearn/tslearn-0.0.31.tar.gz/tslearn-0.0.31/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension
+import numpy
+import tslearn
+
+have_cython = False
+try:
+    from Cython.Distutils import build_ext as _build_ext
+    have_cython = True
+except ImportError:
+    from distutils.command.build_ext import build_ext as _build_ext
+
+list_pyx = ['cydtw', 'cygak', 'cysax', 'cycc', 'soft_dtw_fast']
+if have_cython:
+    ext = [Extension('tslearn.%s' % s, ['tslearn/%s.pyx' % s]) for s in list_pyx]
+else:
+    ext = [Extension('tslearn.%s' % s, ['tslearn/%s.c' % s]) for s in list_pyx]
+
+setup(
+    name="tslearn",
+    description="A machine learning toolkit dedicated to time-series data",
+    include_dirs=[numpy.get_include()],
+    packages=['tslearn'],
+    package_data={"tslearn": [".cached_datasets/Trace.npz"]},
+    install_requires=['numpy', 'scipy', 'scikit-learn'],
+    ext_modules=ext,
+    cmdclass={'build_ext': _build_ext},
+    version=tslearn.__version__,
+    url="http://tslearn.readthedocs.io/",
+    author="Romain Tavenard",
+    author_email="romain.tavenard@univ-rennes2.fr"
+)  # TODO: test package_data option on PyPI deployment
