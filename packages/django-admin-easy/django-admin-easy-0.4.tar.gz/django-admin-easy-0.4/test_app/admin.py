721d7ea5cfac7ca01cb2fcb3c5fa7bd1162e5eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:10 +0000

    Add package django-admin-easy/0.4/django-admin-easy-0.4.tar.gz

---
+++ packages/django-admin-easy/django-admin-easy-0.4.tar.gz/django-admin-easy-0.4/test_app/admin.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+from django.contrib import admin
+from django.http.response import HttpResponse
+import easy
+from test_app.models import Choice, Question, Poll
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'poll_link', 'bool_sample', 'question_text', 'pub_date',)
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+    fieldsets = (
+        (None, {
+            'fields': ('poll', 'question_text',)
+        }),(
+            'Date information',
+            {'classes': ('collapse',),
+            'fields': ('pub_date',),
+            }
+        ),
+    )
+    inlines = (ChoiceInline,)
+
+    poll_link = easy.ForeignKeyAdminField('poll')
+    bool_sample = easy.BooleanAdminField(lambda x: x.id == 1, 'First')
+
+
+class PollAdmin(easy.MixinEasyViews, admin.ModelAdmin):
+    list_display = ('name', 'count_question')
+
+    count_question = easy.LinkChangeListAdminField('test_app', 'question', 'question_set.count', {'poll': 'id'}, 'Count')
+
+    def easy_view_test(self, request, *args):
+
+        return HttpResponse('test is ok with %s' % (args or 'list'))
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Question, QuestionAdmin)
---
+++ packages/django-admin-easy/django-admin-easy-0.4.tar.gz/django-admin-easy-0.4/test_app/models.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+from django.db import models
+from django.conf import settings
+
+class Poll(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    image = models.ImageField(upload_to=settings.STATIC_PATH + '/media', blank=True, null=True)
+    poll = models.ForeignKey(Poll)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
