5174f4d87cae7df373c7de88d003d6b934512cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:06 +0000

    Add package check-ezproxy/0.1.0/check_ezproxy-0.1.0-py2.py3-none-any.whl

---
+++ packages/check-ezproxy/check_ezproxy-0.1.0-py2.py3-none-any.whl/check_ezproxy/config.py
@@ -0,0 +1,7 @@
+cfg = {
+    'ezproxy_prefix': 'https://scroll.lib.westfield.ma.edu/login?url=',
+    'libguides_api_url': 'https://lgapi-us.libapps.com/1.1/assets?site_id=1761&key=82719699635fb57e2d192757924a7318&asset_types=10', # including api key
+    'ezproxy_error_text': 'If you are off campus, please contact <a href="mailto:askus@westfield.ma.edu">a librarian</a> for help.',
+    'kb_wskey': 'XfgrkU3HyAkKWC14s5rnhQ1vUBZJPQbgKMUBspKqbI2zBjPBVXqQTEPZtL6yykGCkaI5mU7tPJjEtKDa',
+    'kb_collections': ('customer.1410.5',)  # tuple or list
+}
