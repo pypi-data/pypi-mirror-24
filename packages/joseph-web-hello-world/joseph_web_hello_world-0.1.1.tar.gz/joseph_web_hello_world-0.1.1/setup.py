41eb10e4178d126053995a42de8255067564b4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:38 +0000

    Add package joseph-web-hello-world/0.1.1/joseph_web_hello_world-0.1.1.tar.gz

---
+++ packages/joseph-web-hello-world/joseph_web_hello_world-0.1.1.tar.gz/joseph_web_hello_world-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+__VERSION__ = "0.1.1"
+
+
+setup(
+    name="joseph_web_hello_world",
+    description="A basic HTTP handler for Joseph.",
+    author="Niek Keijzer",
+    author_email="info@niekkeijzer.com",
+    url="https://github.com/NiekKeijzer/HTTPTest",
+    download_url="https://github.com/NiekKeijzer/HTTPTest/archive/{}.tar.gaz".format(__VERSION__),
+    keywords=[
+        "http",
+        "web",
+        "hello_world"
+        "joseph"
+    ],
+    packages=[
+        "web"
+    ],
+    version=__VERSION__,
+    entry_points={
+        'joseph.http': [
+            '[GET]/test = web.handlers:index'
+        ]
+    }
+)
