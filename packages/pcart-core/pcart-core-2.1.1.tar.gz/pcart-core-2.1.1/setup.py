8c9df56eae4506610aca73885a7ac971360a7949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:01 +0000

    Add package pcart-core/2.1.1/pcart-core-2.1.1.tar.gz

---
+++ packages/pcart-core/pcart-core-2.1.1.tar.gz/pcart-core-2.1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+INSTALL_REQUIREMENTS = [
+    'Django>=1.10.5',
+    'Markdown==2.6.8',
+    'unicode-slugify>=0.1.3',
+    'django-cms>=3.4.2',
+]
+
+setup(
+    name='pcart-core',
+    version='2.1.1',
+    author='Oleh Korkh',
+    author_email='oleh.korkh@the7bits.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    license='BSD License',
+    description='A powerful e-commerce solution for Django CMS',
+    long_description=open('README.txt').read(),
+    platforms=['OS Independent'],
+    url='http://the7bits.com/',
+    install_requires=INSTALL_REQUIREMENTS,
+)
