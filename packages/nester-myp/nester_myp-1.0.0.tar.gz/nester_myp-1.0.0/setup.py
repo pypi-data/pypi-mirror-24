a617377d89e23260379c4227415ff71b0242463d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:12 +0000

    Add package nester-myp/1.0.0/nester_myp-1.0.0.tar.gz

---
+++ packages/nester-myp/nester_myp-1.0.0.tar.gz/nester_myp-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester_myp',
+        version         = '1.0.0',
+        py_modules       = ['nester_myp'],
+        author          = 'parkminyoung',
+        author_email    = 'alsdud3894@gmail.com',
+        url             = 'http://www,headfirstlabs.com',
+        description     = 'A simple printer of nestes list'
+)
