eb873800ee5f34b30003286ddc554176761b4d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:31 +0000

    Add package pylint-quotes/0.1.6/pylint-quotes-0.1.6.tar.gz

---
+++ packages/pylint-quotes/pylint-quotes-0.1.6.tar.gz/pylint-quotes-0.1.6/setup.py
@@ -0,0 +1,30 @@
+"""Setup for the pylint-quotes package.
+"""
+
+from setuptools import setup, find_packages
+
+from pylint_quotes import __version__
+
+
+setup(
+    name='pylint-quotes',
+    description='Quote consistency checker for Pylint',
+    license='MIT',
+    version=__version__,
+    author='Erick Daniszewski',
+    author_email='edaniszewski@gmail.com',
+    url='https://github.com/edaniszewski/pylint-quotes',
+    install_requires=[
+        'pylint',
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Utilities'
+    ],
+    keywords='pylint linting string quotes'
+)
