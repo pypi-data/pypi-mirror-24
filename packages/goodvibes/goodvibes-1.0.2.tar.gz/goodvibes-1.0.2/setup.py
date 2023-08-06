a3b3b70d84e008f02ad497428caeed209445ee8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:46 +0000

    Add package goodvibes/1.0.2/goodvibes-1.0.2.tar.gz

---
+++ packages/goodvibes/goodvibes-1.0.2.tar.gz/goodvibes-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'goodvibes',
+  packages = ['goodvibes'],
+  version = '1.0.2',
+  description = 'Calculates quasi-harmonic free energies from Gaussian output files with temperature and haptic corrections',
+  author = 'Robert Paton',
+  author_email = 'robert.paton@chem.ox.ac.uk',
+  url = 'https://github.com/bobbypaton/goodvibes',
+  download_url = 'https://zenodo.org/record/841362/files/bobbypaton/GoodVibes-1.0.2.zip',
+  keywords = ['compchem', 'thermochemistry', 'gaussian', 'vibrational-entropies', 'temperature'],
+  classifiers = [],
+)
