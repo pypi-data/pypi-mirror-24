90a4a8aa07c5598670abc82fa946a10f984da3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:36 +0000

    Add package pyexcel-odsr/0.4.2/pyexcel-odsr-0.4.2.tar.gz

---
+++ packages/pyexcel-odsr/pyexcel-odsr-0.4.2.tar.gz/pyexcel-odsr-0.4.2/setup.py
@@ -0,0 +1,113 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+NAME = 'pyexcel-odsr'
+AUTHOR = 'C.W.'
+VERSION = '0.4.2'
+EMAIL = 'wangc_2011@hotmail.com'
+LICENSE = 'New BSD'
+DESCRIPTION = (
+    'a plugin to pyexcel and provides the capbility to read data in ods for' +
+    'mats using tailored messytables.' +
+    ''
+)
+URL = 'https://github.com/pyexcel/pyexcel-odsr'
+DOWNLOAD_URL = '%s/archive/0.4.2.tar.gz' % URL
+FILES = ['README.rst', 'CONTRIBUTORS.rst', 'CHANGELOG.rst']
+KEYWORDS = [
+    'excel',
+    'ods',
+    'python'
+]
+
+CLASSIFIERS = [
+    'Topic :: Office/Business',
+    'Topic :: Utilities',
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: BSD License',
+]
+
+INSTALL_REQUIRES = [
+    'pyexcel-io>=0.4.0',
+    'lxml>=3.2',
+]
+
+
+PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
+EXTRAS_REQUIRE = {
+}
+
+
+def read_files(*files):
+    """Read files into setup"""
+    text = ""
+    for single_file in files:
+        content = read(single_file)
+        text = text + content + "\n"
+    return text
+
+
+def read(afile):
+    """Read a file into setup"""
+    with open(afile, 'r') as opened_file:
+        content = filter_out_test_code(opened_file)
+        content = "".join(list(content))
+        return content
+
+
+def filter_out_test_code(file_handle):
+    found_test_code = False
+    for line in file_handle.readlines():
+        if line.startswith('.. testcode:'):
+            found_test_code = True
+            continue
+        if found_test_code is True:
+            if line.startswith('  '):
+                continue
+            else:
+                empty_line = line.strip()
+                if len(empty_line) == 0:
+                    continue
+                else:
+                    found_test_code = False
+                    yield line
+        else:
+            for keyword in ['|version|', '|today|']:
+                if keyword in line:
+                    break
+            else:
+                yield line
+
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        author=AUTHOR,
+        version=VERSION,
+        author_email=EMAIL,
+        description=DESCRIPTION,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        long_description=read_files(*FILES),
+        license=LICENSE,
+        keywords=KEYWORDS,
+        extras_require=EXTRAS_REQUIRE,
+        tests_require=['nose'],
+        install_requires=INSTALL_REQUIRES,
+        packages=PACKAGES,
+        include_package_data=True,
+        zip_safe=False,
+        classifiers=CLASSIFIERS
+    )
