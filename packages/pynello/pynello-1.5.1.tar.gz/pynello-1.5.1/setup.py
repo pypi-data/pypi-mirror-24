88834ff00a2c223db7365ebaa14865b6c63c416b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:44 +0000

    Add package pynello/1.5.1/pynello-1.5.1.tar.gz

---
+++ packages/pynello/pynello-1.5.1.tar.gz/pynello-1.5.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pynello',
+    version='1.5.1',
+    license='GPL3',
+    description='Python library for nello.io intercoms',
+    long_description=read('README.rst'),
+    author='Philipp Schmitt',
+    author_email='philipp@schmitt.co',
+    url='https://github.com/pschmitt/pynello',
+    packages=find_packages(),
+    install_requires=['python-dateutil', 'requests'],
+    entry_points={'console_scripts': ['nello=pynello.__main__:main']}
+)
