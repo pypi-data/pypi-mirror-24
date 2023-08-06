bfea3f9996cb7f7b0b6c616561a6f65812af29e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:36 +0000

    Add package pandas-redshift/1.0.5/pandas_redshift-1.0.5.tar.gz

---
+++ packages/pandas-redshift/pandas_redshift-1.0.5.tar.gz/pandas_redshift-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'pandas_redshift',
+    packages = ['pandas_redshift'],
+    version = '1.0.5',
+    description = 'Load data from redshift into a pandas DataFrame and vice versa.',
+    author = 'Aidan Gawronski',
+    author_email = 'aidangawronski@gmail.com',
+    # url = 'https://github.com/agawronski/pandas_redshift',
+    python_requires = '>=3',
+    install_requires = ['psycopg2',
+                        'pandas',
+                        'boto3']
+)
