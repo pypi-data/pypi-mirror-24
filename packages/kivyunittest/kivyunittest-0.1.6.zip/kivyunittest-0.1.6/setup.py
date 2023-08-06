b19c20c836ea3ffe06d8c39281811c920da23bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:14 +0000

    Add package kivyunittest/0.1.6/kivyunittest-0.1.6.zip

---
+++ packages/kivyunittest/kivyunittest-0.1.6.zip/kivyunittest-0.1.6/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from os.path import dirname, abspath, join
+
+import kivyunittest as kut
+
+# it should be the "kivyunittest" folder near setup.py
+expected = join(
+    dirname(abspath(__file__)), 'kivyunittest'
+)
+if dirname(kut.__file__) == expected:
+    from kivyunittest.__main__ import __version__ as version
+else:
+    exit("Failed to import KivyUnitTest version for setup.py")
+
+setup(
+    name='kivyunittest',
+    version=version,
+    description='Unittesting for Kivy framework',
+    author='Peter Badida',
+    author_email='keyweeusr@gmail.com',
+    url='https://github.com/KeyWeeUsr/KivyUnitTest',
+    download_url=(
+        'https://github.com/KeyWeeUsr/KivyUnitTest/tarball/'
+        '{}'.format(version)
+    ),
+    packages=['kivyunittest'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Bug Tracking',
+        'Topic :: Software Development :: Testing',
+    ],
+    keywords=['unittest', 'testing', 'debug', 'kivy'],
+    license="License :: OSI Approved :: MIT License",
+)
