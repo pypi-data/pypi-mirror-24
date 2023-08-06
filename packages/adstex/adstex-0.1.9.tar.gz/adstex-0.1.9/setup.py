c8fc2d5dab1b3fc7caac7b0941953dfb49ca27e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:13 +0000

    Add package adstex/0.1.9/adstex-0.1.9.tar.gz

---
+++ packages/adstex/adstex-0.1.9.tar.gz/adstex-0.1.9/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+Find all citation keys in your LaTeX documents and search NASA ADS
+to generate corresponding bibtex entries.
+
+Project website: https://github.com/yymao/adstex
+
+The MIT License (MIT)
+Copyright (c) 2015-2017 Yao-Yuan Mao (yymao)
+http://opensource.org/licenses/MIT
+"""
+
+from setuptools import setup
+
+setup(
+    name='adstex',
+    version='0.1.9',
+    description='Find all citation keys in your LaTeX documents and search NASA ADS to generate corresponding bibtex entries.',
+    url='https://github.com/yymao/adstex',
+    author='Yao-Yuan Mao',
+    author_email='yymao.astro@gmail.com',
+    maintainer='Yao-Yuan Mao',
+    maintainer_email='yymao.astro@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
+    use_2to3=True,
+    keywords='bibtex ads',
+    packages=['adstex'],
+    install_requires=['ads>=0.12.3','bibtexparser>=0.6.2'],
+    entry_points={
+        'console_scripts': [
+            'adstex=adstex:main',
+        ],
+    },
+)
