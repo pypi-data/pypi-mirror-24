0625e83577569f7b8fd6e2978d8d306ca4123c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:10 +0000

    Add package Data-viz/0.8.3.dev0/Data-viz-0.8.3.dev0.tar.gz

---
+++ packages/Data-viz/Data-viz-0.8.3.dev0.tar.gz/Data-viz-0.8.3.dev0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+setup(
+        name='Data-viz',
+        version='0.8.3dev',
+        packages=['data_viz'],
+        description='Represents your high-dimensional datas in a 2D space and play wih it',
+        install_requires = open(os.path.join('data_viz', 'requirements/requirements.txt')).read(),
+        license = 'GPL V3',
+        author='Sofian Medbouhi',
+        author_email='sof.m.sk@free.fr',
+        )
+            
