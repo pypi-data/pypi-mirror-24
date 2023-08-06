73bdc7c96826aa253f2f55a07ba9e56fbd3b47fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:15 +0000

    Add package hart-amsaf/0.0.2/hart-amsaf-0.0.2.tar.gz

---
+++ packages/hart-amsaf/hart-amsaf-0.0.2.tar.gz/hart-amsaf-0.0.2/HartAmsaf/BSplineParameterMapGenerator.py
@@ -0,0 +1,34 @@
+from ParameterMapGenerator import ParameterMapGenerator
+
+
+class BSplineParameterMapGenerator(ParameterMapGenerator):
+    def __init__(self):
+        super(BSplineParameterMapGenerator, self).__init__()
+        self.transformType = 'bspline'
+        self.paramDict = {
+            'AutomaticParameterEstimation': ['true'],
+            'CheckNumberOfSamples': ['true'],
+            'DefaultPixelValue': ['0.000000'],
+            'FinalBSplineInterpolationOrder': ['3.000000'],
+            # 'FinalGridSpacingInPhysicalUnits': ['4.000000', '2.0', '1.0', '24.0'],
+            'FixedImagePyramid': ['FixedSmoothingImagePyramid'],
+            'ImageSampler': ['RandomCoordinate'],
+            'Interpolator': ['LinearInterpolator'],
+            'MaximumNumberOfIterations': ['1024.000000'],
+            'MaximumNumberOfSamplingAttempts': ['8.000000'],
+            'Metric0Weight': ['0', '0.5', '1.000000', '2.0'],
+            'Metric1Weight': ['1.000000'],
+            'MovingImagePyramid': ["MovingSmoothingImagePyramid"],
+            'NewSamplesEveryIteration': ['true'],
+            'NumberOfHistogramBins': ['32.000000'],
+            'NumberOfResolutions': ['4.000000'],
+            'NumberOfSamplesForExactGradient': ['4096.000000'],
+            'NumberOfSpatialSamples': ['2048.000000'],
+            'Optimizer': ['AdaptiveStochasticGradientDescent'],
+            'Registration': ['MultiMetricMultiResolutionRegistration'],
+            'ResampleInterpolator': ['FinalBSplineInterpolator'],
+            'Resampler': ['DefaultResampler'],
+            'Transform': ['BSplineTransform'],
+            'WriteIterationInfo': ['false'],
+            'WriteResultImage': ['true']
+        }
---
+++ packages/hart-amsaf/hart-amsaf-0.0.2.tar.gz/hart-amsaf-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='hart-amsaf',
+      version='0.0.2',
+      description='The HART Lab Automated MRI Segmentation and Analysis Framework',
+      url='https://github.com/iancmcdonald/AMSAF',
+      author='Ian McDonald and Daniel Ho',
+      author_email='ian.c.mcdonald@berkeley.edu',
+      license='MIT',
+      keywords='hart, hart-amsaf, HartAmsaf, amsaf, segmentation, mri, medical',
+      packages=['HartAmsaf'],
+      install_requires=['scipy', 'numpy', 'scikit-learn'],
+      zip_safe=False)
