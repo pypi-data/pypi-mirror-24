a3fdde9c7f86502fa737739784c4b015cf13a013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:19 +0000

    Add package clipoverlap/1.0.5/clipoverlap-1.0.5.tar.gz

---
+++ packages/clipoverlap/clipoverlap-1.0.5.tar.gz/clipoverlap-1.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme:
+    setup(
+        name='clipoverlap',
+        version='1.0.5',
+        packages=find_packages(),
+        long_description=readme.read(),
+        install_requires=['pysam'],
+        scripts=['bin/clip'],
+        url='https://github.com/innovate-invent/clip',
+        license='MIT',
+        author='Nolan',
+        author_email='innovate.invent@gmail.com',
+        description='Clips overlapping regions in read mates of SAM/BAM files.',
+        include_package_data=True
+    )
