fb21783c74b33cea428e1866d16b9ff62d26ea1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package Printer1111/1.0.0/Printer1111-1.0.0.tar.gz

---
+++ packages/Printer1111/Printer1111-1.0.0.tar.gz/Printer1111-1.0.0/mymodule.py
@@ -0,0 +1,17 @@
+'''This is the "mymodule.py" module and it provides
+    one function called "print_lol" which prints lists
+    may or may not include nested lists.
+'''
+def print_lol(the_list, leaves):
+    '''This function takes two positional arguments called "the_list"
+       and "leaves", respectively. "the_list" is any python list (probably
+       nested list) and each item in the list is (recursively) printed to
+       the screen on its own line indented according to "leaves"
+    '''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, leaves + 1)
+        else:
+            for space_stop in range(leaves):
+                print(' ', end = '')
+            print(each_item)
---
+++ packages/Printer1111/Printer1111-1.0.0.tar.gz/Printer1111-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'Printer1111',
+        version      = '1.0.0',
+        py_modules   = ['mymodule'],
+        author       = 'huym',
+        author_email = 'huym260@126.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+    )
