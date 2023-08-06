a134899e6ae8785b5e556229954f2c56b06e4222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:30 +0000

    Add package htmlcompressor/1.0.0/htmlcompressor-1.0.0.tar.gz

---
+++ packages/htmlcompressor/htmlcompressor-1.0.0.tar.gz/htmlcompressor-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='htmlcompressor',
+    version='1.0.0',
+    url='https://github.com/sxslex/htmlcompressor',
+    download_url='https://github.com/sxslex/htmlcompressor/archive/v1.0.tar.gz',
+    author='SleX',
+    author_email='sx.slex@gmail.com',
+    description='Compress HTML CSS and Java Script of a page.',
+    keywords=['html', 'css', 'javascript', 'compressor', 'minify'],
+    packages=['htmlcompressor', 'htmlcompressor.htmlmin'],
+    install_requires=['htmlmin', 'jsmin', 'csscompressor'],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'console_scripts': [
+            'htmlcompressor = htmlcompressor.cli:compress',
+        ],
+    },
+)
