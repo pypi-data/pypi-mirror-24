a99d22a6e99998c0f09ef09a5bf310b6d569081a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:58 +0000

    Add package lp_accounts/0.1/lp_accounts-0.1.tar.gz

---
+++ packages/lp_accounts/lp_accounts-0.1.tar.gz/lp_accounts-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name='lp_accounts',
+  packages=['lp_accounts'],
+  version='0.1',
+  description='REST Framework for User Accounts',
+  author='Jim Simon',
+  author_email='hello@launchpeer.com',
+  url='https://github.com/Launchpeer/django-rest-account',
+  download_url='https://github.com/Launchpeer/django-rest-account/archive/master.tar.gz',
+  keywords=[],
+  classifiers=[],
+)
