80866f26e2b789b03cb49b20faad0348d696f85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:38 +0000

    Add package mlpods/0.1.2/mlpods-0.1.2.tar.gz

---
+++ packages/mlpods/mlpods-0.1.2.tar.gz/mlpods-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'mlpods',
+  packages = ['mlpods'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'MLpods',
+  author = 'Janine Cheng, Ketan Patel',
+  author_email = 'janine.cheng@mlpods.com',
+  url = 'https://github.com/jcc-ne/mlpods', # use the URL to the github repo
+  download_url = 'https://github.com/jcc-ne/mlpods/archive/0.1.2.tar.gz',
+  keywords = ['MLpods', 'deployment', 'machine learning',
+              'ML', 'serverless'], # arbitrary keywords
+  classifiers = [],
+)
