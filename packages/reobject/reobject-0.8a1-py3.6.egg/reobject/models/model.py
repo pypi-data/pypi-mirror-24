cb8b3ae02be0887092879511aea069a10fd65857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:20 +0000

    Add package reobject/0.8a1/reobject-0.8a1-py3.6.egg

---
+++ packages/reobject/reobject-0.8a1-py3.6.egg/reobject/models/model.py
@@ -0,0 +1,57 @@
+import attr
+
+
+from reobject.models.manager import ManagerDescriptor
+
+
+class ModelBase(type):
+    """
+    Metaclass for all models, used to attach the objects class attribute
+    to the model instance at runtime.
+    """
+
+    def __new__(cls, name, bases, attrs):
+        attrs['objects'] = ManagerDescriptor()
+
+        mod = attr.s(
+            super(ModelBase, cls).__new__(cls, name, bases, attrs)
+        )
+
+        for field in attr.fields(mod):
+            if field.metadata.get('related'):
+                target = field.metadata['related']['target']
+                setattr(target, field + '_set', [])
+
+        return mod
+
+
+class Model(object, metaclass=ModelBase):
+    def __attrs_post_init__(self):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        instance = super(Model, cls).__new__(cls)
+        return cls.objects._add(instance)
+
+    @property
+    def id(self) -> int:
+        """
+        Returns a unique integer identifier for the object.
+        """
+        return id(self)
+
+    @property
+    def pk(self) -> int:
+        """
+        Returns a unique integer identifier for the object.
+
+        Alias of the id property.
+        """
+        return self.id
+
+    def delete(self) -> None:
+        type(self).objects._delete(self)
+
+    @property
+    def _attrs(self):
+        return set(self.__dict__.keys()) | {'id'}
