9b1a1df1829e449de8cf5a6e69f48960e431b5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:01 +0000

    Add package grammpy/1.1.8/grammpy-1.1.8.tar.gz

---
+++ packages/grammpy/grammpy-1.1.8.tar.gz/grammpy-1.1.8/grammpy/Grammars/StringGrammar.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 01.08.2017 07:33
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .RawGrammar import RawGrammar
+
+class StringGrammar(RawGrammar):
+    @staticmethod
+    def __to_string_arr(t):
+        if isinstance(t, str):
+            return [t]
+        return t
+
+    def remove_term(self, term=None):
+        return super().remove_term(StringGrammar.__to_string_arr(term))
+
+    def add_term(self, term):
+        return super().add_term(StringGrammar.__to_string_arr(term))
+
+    def term(self, term=None):
+        return self.get_term(term)
+
+    def get_term(self, term=None):
+        res = super().get_term(StringGrammar.__to_string_arr(term))
+        if isinstance(term, str):
+            return res[0]
+        return res
+
+    def have_term(self, term):
+        return super().have_term(StringGrammar.__to_string_arr(term))
