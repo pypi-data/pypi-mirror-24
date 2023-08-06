29622373df10e77c619dd91babfd1f8cb93fdcee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:30 +0000

    Add package starmie/1.0/starmie-1.0.tar.gz

---
+++ packages/starmie/starmie-1.0.tar.gz/starmie-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='starmie',
+    version='1.0',
+    description='A* algorithm implementation',
+    author='hoto17296',
+    license='MIT',
+    url='https://github.com/hoto17296/starmie',
+    py_modules=['starmie'],
+)
