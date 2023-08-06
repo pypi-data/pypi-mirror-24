9ed1a84c109734aa41f4e4c984ba61132ba61376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:04 +0000

    Add package MultiColProcessor/1.0.15/MultiColProcessor-1.0.15.tar.gz

---
+++ packages/MultiColProcessor/MultiColProcessor-1.0.15.tar.gz/MultiColProcessor-1.0.15/MultiColProcessor/__init__.py
@@ -0,0 +1,2 @@
+from MultiColProcessor import MultiColProcessor
+
---
+++ packages/MultiColProcessor/MultiColProcessor-1.0.15.tar.gz/MultiColProcessor-1.0.15/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'MultiColProcessor',
+  packages = ['MultiColProcessor'],
+  version = '1.0.15',
+  description='a simple package to process pandas categorical colomns via preprocessing in scikit-learn',
+  url='https://github.com/mekhod/Pandas-Multi-Colomn-Processor',
+  author='Mehdi Khodayari',
+  author_email='me.khodayari@gmail.com',
+  license='MIT',
+  classifiers = [],
+  keywords = ['label encoder', 'multi colomns'],
+)
