5fe35b00940cbc558b66d02a3d96c0fc576afa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:39 +0000

    Add package freshbook/0.1.3/freshbook-0.1.3-py2.py3-none-any.whl

---
+++ packages/freshbook/freshbook-0.1.3-py2.py3-none-any.whl/freshbook/freshbook.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from refreshbooks import api
+
+
+class Freshbook(object):
+
+    def __init__(self, account, token):
+        self.client = api.TokenClient(account, token)
+
+    def get_projects(self):
+        response = self.client.project.list()
+        return [p for p in response.projects.project]
+
+    def get_tasks(self, project_id):
+        response = self.client.task.list(project_id=project_id)
+        return [t for t in response.tasks.task]
+
+    def commit(self, project_id, task_id, **kwargs):
+        response = self.client.time_entry.create(
+            time_entry=dict(
+                project_id=project_id,
+                task_id=task_id,
+                **kwargs
+            )
+        )
+        return response.get('status')
+
+    def list(self, project_id, task_id, date_from=None, date_to=None):
+        payload = dict(
+            project_id=project_id,
+            task_id=task_id,
+            date_from=date_from,
+            date_to=date_to
+        )
+
+        response = self.client.time_entry.list(**payload)
+        total = response.time_entries.get('total')
+
+        if int(total) == 0:
+            return
+
+        while True:
+            for entry in response.time_entries.time_entry:
+                yield entry
+
+            page = response.time_entries.get('page')
+            pages = response.time_entries.get('pages')
+
+            if int(page) == int(pages):
+                break
+
+            payload['page'] = int(page) + 1
+            response = self.client.time_entry.list(**payload)
