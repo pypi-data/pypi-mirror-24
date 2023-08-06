e034ce395b4309381e52540ea8f511621ed732d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:59 +0000

    Add package polls/0.1/polls-0.1.tar.gz

---
+++ packages/polls/polls-0.1.tar.gz/polls-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='polls',
+      version='0.1',
+      description='poll application',
+      url='https://bitbucket.org/ripplemotion/django-tuto-solo-guillaume',
+      author='Guillaume GUILLET',
+      author_email='guillaume.guillet@ripplemotion.fr',
+      license='MIT',
+      packages=['polls'],
+      zip_safe=False)
