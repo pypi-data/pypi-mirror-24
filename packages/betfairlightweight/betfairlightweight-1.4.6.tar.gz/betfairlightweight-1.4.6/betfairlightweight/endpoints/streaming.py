307c3159714f95ba423033dacdda2d371b731eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package betfairlightweight/1.4.6/betfairlightweight-1.4.6.tar.gz

---
+++ packages/betfairlightweight/betfairlightweight-1.4.6.tar.gz/betfairlightweight-1.4.6/betfairlightweight/endpoints/streaming.py
@@ -0,0 +1,42 @@
+from ..streaming import (
+    BaseListener,
+    BetfairStream,
+)
+
+
+class Streaming(object):
+    """
+    Streaming operations.
+    """
+
+    def __init__(self, parent):
+        """
+        :param parent: API client.
+        """
+        self.client = parent
+
+    def create_stream(self, unique_id=0, listener=None, timeout=11, buffer_size=1024, description='BetfairSocket',
+                      host=None):
+        """
+        Creates BetfairStream.
+
+        :param dict unique_id: Id used to start unique id's of the stream (+1 before every request)
+        :param resources.Listener listener:  Listener class to use
+        :param float timeout: Socket timeout
+        :param int buffer_size: Socket buffer size
+        :param str description: Betfair stream description
+        :param str host: Host endpoint (prod (default) or integration)
+
+        :rtype: resources.BetfairStream
+        """
+        listener = listener if listener else BaseListener()
+        return BetfairStream(
+            unique_id,
+            listener,
+            app_key=self.client.app_key,
+            session_token=self.client.session_token,
+            timeout=timeout,
+            buffer_size=buffer_size,
+            description=description,
+            host=host,
+        )
