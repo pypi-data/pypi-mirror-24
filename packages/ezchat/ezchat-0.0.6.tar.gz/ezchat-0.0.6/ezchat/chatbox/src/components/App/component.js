478808682f53bbd284602ab49b411fbcd8965373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:38 +0000

    Add package ezchat/0.0.6/ezchat-0.0.6.tar.gz

---
+++ packages/ezchat/ezchat-0.0.6.tar.gz/ezchat-0.0.6/ezchat/chatbox/src/components/App/component.js
@@ -0,0 +1,18 @@
+
+import Chatbox from '../Chatbox/component.vue';
+
+
+export default {
+	name: 'app-main',
+	components: {
+		'app-chatbox': Chatbox,
+	},
+	data() {
+		return {
+		};
+	},
+	computed: {
+	},
+	methods: {
+	}
+};
