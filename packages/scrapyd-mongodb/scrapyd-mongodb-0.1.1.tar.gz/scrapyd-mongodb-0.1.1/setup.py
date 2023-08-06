fa2319e23706180506cb0ddd63d7fa1dbf9c6fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:03 +0000

    Add package scrapyd-mongodb/0.1.1/scrapyd-mongodb-0.1.1.tar.gz

---
+++ packages/scrapyd-mongodb/scrapyd-mongodb-0.1.1.tar.gz/scrapyd-mongodb-0.1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapyd-mongodb',
+    version='0.1.1',
+    description='Scrapyd Queue Management with MongoDB',
+    author='Tiago Lira',
+    author_email='tiagolira.dev@gmail.com',
+    license='MIT',
+    url='https://github.com/Tiago-Lira/scrapyd-mongodb',
+    download_url='https://github.com/Tiago-Lira/scrapyd-mongodb/tarball/0.1.1',
+    keywords=['scrapy', 'scrapyd', 'mongodb', 'queue',
+              'scrapyd-queue', 'scrapyd-backend', 'backend'],
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'scrapy',
+        'pymongo',
+    ],
+    classifiers=[],
+)
