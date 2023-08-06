7230826418c6448555eae5bc7ecee7b11912dc85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:05 +0000

    Add package high-templar/0.2.0/high_templar-0.2.0.tar.gz

---
+++ packages/high-templar/high_templar-0.2.0.tar.gz/high_templar-0.2.0/tests/test_auth.py
@@ -0,0 +1,33 @@
+import requests
+from .testapp.app import app, django_app_url, django_app_port
+from high_templar.test import TestCase, Client, MockResponse, MockWebSocket
+
+
+class TestAuth(TestCase):
+    def setUp(self):
+        self.client = Client(app)
+
+    def test_incoming_websocket_calls_bootstrap(self):
+        ws = MockWebSocket()
+        self.client.open_connection(ws)
+        self.assertEqual(1, requests.get.call_count)
+
+        external_url = 'http://{}:{}/api/bootstrap/'.format(django_app_url, django_app_port)
+        self.assertEqual(external_url, requests.get.call_args_list[0][0][0])
+
+    def test_unauth_closed(self):
+        def not_authenticated(request, **kwargs):
+            return MockResponse({}, 403)
+
+        self.client.set_mock_api(not_authenticated)
+
+        ws = MockWebSocket()
+        self.client.open_connection(ws)
+
+        self.assertEqual(True, ws.closed)
+
+    def test_lists_allowed_rooms(self):
+        ws = MockWebSocket()
+        self.client.open_connection(ws)
+
+        self.assertEqual(['{"allowed_rooms": ["{\\"target\\": \\"ride\\"}", "{\\"target\\": \\"car\\"}"]}'], ws.outgoing_messages)
