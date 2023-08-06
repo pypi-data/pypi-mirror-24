86de88563cdc9b4929927963b3a8b6d6df4519b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package python-smartqq-client/0.2.2/python-smartqq-client-0.2.2.tar.gz

---
+++ packages/python-smartqq-client/python-smartqq-client-0.2.2.tar.gz/python-smartqq-client-0.2.2/pyclient/SmartqqLoginPipeline.py
@@ -0,0 +1,33 @@
+import io
+from .GetBarcodeStepHandler import GetBarcodeStepHandler
+from .GetPsessionidHandler import GetPsessionidHandler
+from .GetPtwebqqHandler import GetPtwebqqHandler
+from .GetVfwebqqHandler import GetVfwebqqHandler
+from .LoginPipeline import LoginPipeline
+from .WaitForAuthHandler import WaitForAuthHandler
+from .LoginFinalizeHandler import LoginFinalizeHandler
+from .LoginSetupHandler import LoginSetupHandler
+
+
+class SmartqqLoginPipeline(LoginPipeline):
+    def __init__(self, session, barcode_handler=None):
+        super().__init__(session)
+        if barcode_handler is None:
+
+            def barcode_store(barcode: io.BytesIO):
+                with open("barcode.png", "wb") as barcode_file:
+                    import shutil
+                    shutil.copyfileobj(barcode, barcode_file)
+                    print("Please scan the barcode png to login")
+                    barcode.seek(0)
+
+            self.barcode_handler = barcode_store
+        else:
+            self.barcode_handler = barcode_handler
+        self.add_step(LoginSetupHandler(session))
+        self.add_step(GetBarcodeStepHandler(session))
+        self.add_step(WaitForAuthHandler(session, barcode_handler=self.barcode_handler))
+        self.add_step(GetPtwebqqHandler(session))
+        self.add_step(GetVfwebqqHandler(session))
+        self.add_step(GetPsessionidHandler(session))
+        self.add_step(LoginFinalizeHandler(session))
