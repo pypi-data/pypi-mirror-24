ad0c526c39bec2c227ea40df81e2f2bfec4b3fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:37 +0000

    Add package fantasy_data/1.0.2/fantasy_data-1.0.2.tar.gz

---
+++ packages/fantasy_data/fantasy_data-1.0.2.tar.gz/fantasy_data-1.0.2/setup.py
@@ -0,0 +1,39 @@
+#coding:utf-8
+import sys
+from distutils.core import setup
+
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['tests/']
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='fantasy_data',
+    version='1.0.2',
+    description='FantasyData Python',
+    author='Fantasy Football Calculator',
+    author_email='support@fantasyfootballcalculator.com',
+    url='https://github.com/ffcalculator/fantasydata-python',
+    packages=['fantasy_data'],
+    keywords=['fantasy', 'sports', 'football'],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
+    download_url='https://github.com/ffcalculator/fantasydata-python/archive/v1.0.2.tar.gz'
+)
