f748d45c2a5cc1330a97c00c3c47258a44eb14e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package pyflann3/1.8.4.1/pyflann3-1.8.4.1.tar.gz

---
+++ packages/pyflann3/pyflann3-1.8.4.1.tar.gz/pyflann3-1.8.4.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pyflann3',
+    version='1.8.4.1',
+    description='pyflann is the python bindings for FLANN - Fast Library for Approximate Nearest Neighbors.',
+    author='Marius Muja & Gefu Tang',
+    author_email='mariusm@cs.ubc.ca, tanggefu@gmail.com',
+    license='BSD',
+    keywords="flann",
+    url='https://github.com/primetang/pyflann',
+    packages=['pyflann', 'pyflann.io', 'pyflann.bindings',
+              'pyflann.util', 'pyflann.lib'],
+    package_dir={'pyflann.lib': 'pyflann/lib'},
+    package_data={'pyflann.lib': ['darwin/*.dylib', 'linux/*.so']},
+)
