bf1f17c6164a0343f5586074518032cd99cca599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:40 +0000

    Add package SiviCNCDriver/0.1.5/SiviCNCDriver-0.1.5.tar.gz

---
+++ packages/SiviCNCDriver/SiviCNCDriver-0.1.5.tar.gz/SiviCNCDriver-0.1.5/sivicncdriver/__init__.py
@@ -0,0 +1,18 @@
+"""
+The SiviCNCDriver Package
+=========================
+
+This is the SiviCNCDriver package. To run the application directly you should
+use the ``sivicnc`` command in a shell. Alternately you can use the ``main``
+function of the package which doesn't take any parameter.
+
+
+
+:Example:
+
+>>> from sivicncdriver.app import main
+>>> main()
+
+"""
+
+__version__ = "0.1.5"
