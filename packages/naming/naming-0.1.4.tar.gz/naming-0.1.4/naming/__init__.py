31098fa48867a5f08df18ce1b0d3a829e3b04505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:24 +0000

    Add package naming/0.1.4/naming-0.1.4.tar.gz

---
+++ packages/naming/naming-0.1.4.tar.gz/naming-0.1.4/naming/__init__.py
@@ -0,0 +1,318 @@
+# standard
+from pathlib import Path
+# package
+from .base import _BaseName
+
+__all__ = ['Name', 'File', 'Pipe', 'PipeFile']
+
+
+class Name(_BaseName):
+    """Inherited by: :class:`naming.File` :class:`naming.Pipe`
+
+    Base class for name objects.
+
+    Each subclass may have its own `config` attribute that should be a dictionary in the form of {field: pattern}
+    where pattern is a valid regular expression.
+
+    Classes may as well have a `drops` iterable attribute representing the fileds they want to ignore from their bases
+    and a `compounds` dictionary attribute for nesting existing fields into multiple new ones.
+
+    All fields should be unique. No duplicates are allowed.
+
+    ======  ==========
+    **Config:**
+    ------------------
+    *base*  Accepts any amount of word characters
+    ======  ==========
+
+    Basic use::
+
+        >>> from naming import Name
+        >>> n = Name()
+        >>> n.get_name()  # no name has been set on the object, convention is solved with [missing] fields
+        '[base]'
+        >>> n.values
+        {}
+        >>> n.set_name('hello_world')
+        >>> n.values
+        {'base': 'hello_world'}
+        >>> n.base = 'fields_as_properties'
+        >>> n.values
+        {'base': 'fields_as_properties'}
+    """
+    config = dict(base=r'\w+')
+    drops = tuple()
+    compounds = dict()
+
+    def __init__(self, name: str='', separator: str='_'):
+        """Sets the patterns defined by the `config` attribute. If any extra work is to be done by the class init
+        it should be implemented on the `_init_name_core` method.
+
+        :param name: Name to initialize the object with. Defaults to an empty string and it can later be set
+                     by calling the :func:`~naming.Name.set_name` method.
+        :param separator: Separator for the name fields. Defaults to an underscore.
+        """
+        self.__keys = self.config.keys()
+        self.__items = self.config.items()
+        super().__init__(name, separator)
+
+    def _set_values(self):
+        super()._set_values()
+        for k, v in self.__items:
+            setattr(self, rf'_{k}', v)
+        for ck, cv in self.compounds.items():
+            setattr(self, rf'_{ck}', ''.join(getattr(self, rf'_{v}') for v in cv))
+
+    def _set_patterns(self):
+        super()._set_patterns()
+        self._set_pattern(*self.__keys)
+
+    def get_pattern_list(self):
+        result = super().get_pattern_list()
+        result.extend(rf'_{k}' for k in self.__keys if not (k in self.drops or k in self._compounds_fields))
+        return result
+
+
+class File(Name):
+    """Inherited by: :class:`naming.PipeFile`
+
+    File Name objects.
+
+    ===========  ===========
+    **Unique Fields:**
+    ------------------------
+    *extension*  Any amount of word characters
+    ===========  ===========
+
+    Basic use::
+
+        >>> from naming import File
+        >>> f = File()
+        >>> f.get_name()
+        '[basse].[extension]'
+        >>> f.get_name(extension='png')
+        '[base].png'
+        >>> f.set_name('hello.world')
+        >>> f.values
+        {'base': 'hello', 'extension': 'world'}
+        >>> f.extension
+        'world'
+        >>> f.extension = 'abc'
+        >>> f.name
+        'hello.abc'
+        >>> f.values
+        {'base': 'hello', 'extension': 'abc'}
+    """
+    def __init__(self, *args, **kwargs):
+        self._cwd = kwargs.pop('cwd', None)
+        super().__init__(*args, **kwargs)
+
+    def _set_values(self):
+        super()._set_values()
+        self._extension = '[.](?P<extension>\w+)'
+        self._add_field_property('extension')
+
+    def _get_joined_pattern(self) -> str:
+        return rf'{super()._get_joined_pattern()}{self._extension}'
+
+    def get_name(self, **values) -> str:
+        if not values and self.name:
+            return super().get_name(**values)
+        extension = values.get('extension') or self.extension or "[extension]"
+        return rf'{super().get_name(**values)}.{extension}'
+
+    def get_path_pattern_list(self):
+        return []
+
+    @property
+    def cwd(self):
+        return self._cwd
+
+    @cwd.setter
+    def cwd(self, value):
+        self._cwd = value
+
+    @property
+    def path(self) -> Path:
+        """The Path representing this object on the filesystem."""
+        args = list(self._iter_translated_pattern_list('get_path_pattern_list'))
+        args.append(self.get_name())
+        return Path(*args)
+
+    @property
+    def full_path(self) -> Path:
+        """The resolved full Path representing this object on the filesystem."""
+        cwd = Path.home() if not self.cwd else Path(self.cwd)
+        return Path.joinpath(cwd, self.path)
+
+
+class Pipe(Name):
+    """Inherited by: :class:`naming.PipeFile`
+
+    Pipe Name objects.
+
+    =========  =========
+    **Unique Fields:**
+    --------------------
+    *output**  Any amount of word characters
+    *version*  Any amount of digits
+    *frame***  Any amount of digits
+    \* optional field. ** exists only when *output* is there as well.
+    ====================
+
+    ======  ============
+    **Composed Fields:**
+    --------------------
+    *pipe*  Combination of unique fields in the form of: (.{output})\*.{version}.{frame}**
+    \* optional field. ** exists only when *output* is there as well.
+    ====================
+
+    Basic use::
+
+        >>> from naming import Pipe
+        >>> p = Pipe()
+        >>> p.get_name()
+        '[base].[pipe]'
+        >>> p.get_name(version=10)
+        '[base].10'
+        >>> p.get_name(output='data')
+        '[base].data.[version]'
+        >>> p.get_name(output='cache', version=7, frame=24)
+        '[base].cache.7.24'
+        >>> p = Pipe('my_wip_data.1')
+        >>> p.version
+        '1'
+        >>> p.values
+        {'base': 'my_wip_data', 'version': '1'}
+        >>> p.get_name(output='exchange')  # returns a new string
+        'my_wip_data.exchange.1'
+        >>> p.name
+        'my_wip_data.1'
+        >>> p.output = 'exchange'  # mutates the object
+        >>> p.name
+        'my_wip_data.exchange.1'
+        >>> p.frame = 101
+        >>> p.version = 7
+        >>> p.name
+        'my_wip_data.exchange.7.101'
+        >>> p.values
+        {'base': 'my_wip_data', 'output': 'exchange', 'version': '7', 'frame': '101'}
+    """
+    _pipe_fields = ('output', 'version', 'frame')
+
+    def _set_values(self):
+        super()._set_values()
+        self._version = '\d+'
+        self._output = '\w+'
+        self._frame = '\d+'
+        sep = self._pipe_separator_pattern
+        self._pipe = rf'(({sep}{self._output})?{sep}{self._version}({sep}{self._frame})?)'
+
+    def _set_patterns(self):
+        super()._set_patterns()
+        self._set_pattern('pipe', *self._pipe_fields)
+
+    def _get_joined_pattern(self):
+        return rf'{super()._get_joined_pattern()}{self._pipe}'
+
+    @property
+    def pipe_separator(self) -> str:
+        return '.'
+
+    @property
+    def _pipe_separator_pattern(self):
+        return rf'\{self.pipe_separator}'
+
+    @property
+    def pipe_name(self) -> str:
+        """The pipe name string of this object."""
+        pipe_suffix = self.pipe or rf"{self.pipe_separator}[pipe]"
+        return rf'{self.nice_name}{pipe_suffix}'
+
+    def _filter_k(self, k):
+        return k == 'pipe'
+
+    def _format_pipe_field(self, k, v):
+        if k == 'frame' and v is None:
+            return ''
+        return rf'{self.pipe_separator}{v if v is not None else rf"[{k}]"}'
+
+    def _get_pipe_field(self, output=None, version=None, frame=None) -> str:
+        fields = dict(output=output or None, version=version, frame=frame)
+        # comparisons to None due to 0 being a valid value
+        fields = {k: v if v is not None else getattr(self, k) for k, v in fields.items()}
+
+        if all(v is None for v in fields.values()):
+            suffix = rf'{self.pipe_separator}[pipe]'
+            return self.pipe or suffix if self.name else suffix
+
+        if not fields['output'] and fields['frame'] is None:  # optional fields
+            return rf'.{fields["version"] or 0}'
+
+        return ''.join(self._format_pipe_field(k, v) for k, v in fields.items())
+
+    def get_name(self, **values) -> str:
+        if not values and self.name:
+            return super().get_name(**values)
+        try:
+            # allow for getting name without pipe field in subclasses e.g. .File
+            pipe = values['pipe'] or ''
+        except KeyError:
+            kwargs = {k: values.get(k) for k in self._pipe_fields}
+            pipe = self._get_pipe_field(**kwargs)
+        return rf'{super().get_name(**values)}{pipe}'
+
+
+class PipeFile(File, Pipe):
+    """
+    Basic use::
+
+        >>> from naming import PipeFile
+        >>> p = PipeFile('wipfile.7.ext')
+        >>> p.values
+        {'base': 'wipfile', 'version': '7', 'extension': 'ext'}
+        >>> [p.get_name(frame=x, output='render') for x in range(10)]
+        ['wipfile.render.7.0.ext',
+        'wipfile.render.7.1.ext',
+        'wipfile.render.7.2.ext',
+        'wipfile.render.7.3.ext',
+        'wipfile.render.7.4.ext',
+        'wipfile.render.7.5.ext',
+        'wipfile.render.7.6.ext',
+        'wipfile.render.7.7.ext',
+        'wipfile.render.7.8.ext',
+        'wipfile.render.7.9.ext']
+        >>> class ProjectFile(PipeFile):
+        ...     config = dict(year='[0-9]{4}',
+        ...                   user='[a-z]+',
+        ...                   another='(constant)',
+        ...                   last='[a-zA-Z0-9]+')
+        ...
+        >>> pf = ProjectFile('project_data_name_2017_christianl_constant_iamlast.data.17.abc')
+        >>> pf.values
+        {'base': 'project_data_name',
+        'year': '2017',
+        'user': 'christianl',
+        'another': 'constant',
+        'last': 'iamlast',
+        'output': 'data',
+        'version': '17',
+        'extension': 'abc'}
+        >>> pf.nice_name  # no pipe & extension fields
+        'project_data_name_2017_christianl_constant_iamlast'
+        >>> pf.year
+        '2017'
+        >>> pf.year = 'nondigits'  # mutating with invalid fields raises a NameError
+        Traceback (most recent call last):
+        ...
+        NameError: Can't set invalid name 'project_data_name_nondigits_christianl_constant_iamlast.data.17.abc' on ProjectFile instance. Valid convention is: [base]_[year]_[user]_[another]_[last].[pipe].[extension]
+        >>> pf.year = 1907
+        >>> pf.name
+        'project_data_name_1907_christianl_constant_iamlast.data.17.abc'
+        >>> pf.extension
+        'abc'
+        >>> pf.separator = '  '  # you can set the separator to a different set of characters
+        >>> pf.name
+        'project_data_name  1907  christianl  constant  iamlast.data.17.abc'
+    """
+    pass
---
+++ packages/naming/naming-0.1.4.tar.gz/naming-0.1.4/naming/base.py
@@ -0,0 +1,218 @@
+import re
+import abc
+import typing
+
+
+def _regex_property(field_name: str) -> property:
+    def getter(self):
+        pattern = getattr(self, rf'__{field_name}')
+        return rf'(?P<{field_name}>{pattern})'
+
+    def setter(self, value):
+        setattr(self, rf'__{field_name}', value)
+    return property(getter, setter)
+
+
+def _field_property(field_name: str) -> property:
+    def getter(self):
+        return self._values.get(field_name)
+
+    def setter(self, value):
+        if value and str(value) == self._values.get(field_name):
+            return
+        new_name = self.get_name(**{field_name: value})
+        self.set_name(new_name)
+    return property(getter, setter)
+
+
+class _ABCName(abc.ABCMeta):
+    def __new__(mcs, name, bases, args):
+        new_cls = super().__new__(mcs, name, bases, args)
+        mcs._merge_bases_dict('config', new_cls, bases)
+        mcs._merge_bases_dict('compounds', new_cls, bases)
+        new_cls._compounds_fields = set().union(*[v for v in new_cls.compounds.values()])
+        mcs._merge_bases_drops(new_cls, bases)
+        return new_cls
+
+    @staticmethod
+    def _merge_bases_dict(name, new_cls, bases):
+        base_maps = [getattr(b, name, {}) for b in bases]
+        base_maps.append(getattr(new_cls, name, {}))
+        new_map = {}
+        for dic in base_maps:
+            new_map.update(dic)
+        setattr(new_cls, name, new_map)
+
+    @staticmethod
+    def _merge_bases_drops(new_cls, bases):
+        new_drops = set(getattr(new_cls, 'drops', tuple()))
+        for b in bases:
+            for drop in getattr(b, 'drops', tuple()):
+                new_drops.add(drop)
+                new_cls.config.pop(drop, None)
+        new_cls.drops = new_drops
+
+
+class _BaseName(metaclass=_ABCName):
+    """This is the base abstract class for Name objects. You should not need to subclass directly from this object.
+    All subclasses are encouraged to inherit from Name instead of this one."""
+
+    _regex_property_name = re.compile(r'[a-zA-Z]\w+')
+
+    def __init__(self, name: str='', separator: str='_'):
+        super().__init__()
+        self.__values = {}
+        self.__items = self.__values.items()
+        self._set_separator(separator)
+        self._set_patterns()
+        self._init_name_core(name)
+
+    def _init_name_core(self, name: str):
+        """Runs whenever a Name object is initialized or its name is set."""
+        self.__set_name(name)
+        self._set_values()
+        self.__set_regex()
+        self.__validate()
+
+    def _set_separator(self, separator: str):
+        self._separator = rf'{separator}'
+        self._separator_pattern = rf'\{separator}'
+
+    @property
+    def separator(self) -> str:
+        """The string that acts as a separator of all the fields in the name."""
+        return self._separator
+
+    @separator.setter
+    def separator(self, value: str):
+        self._set_separator(value)
+        name = self.get_name(**self.values) if self.name else None
+        self._init_name_core(name)
+
+    @abc.abstractmethod
+    def _set_patterns(self):
+        pass
+
+    def _set_pattern(self, *patterns):
+        for p in patterns:
+            setattr(self.__class__, rf'_{p}', _regex_property(p))
+            self._add_field_property(p)
+
+    def _add_field_property(self, field_name):
+        setattr(self.__class__, field_name, _field_property(field_name))
+
+    def __set_name(self, name: str):
+        self.__name = rf'{name}' if name else None
+
+    @property
+    def name(self) -> str:
+        """The name string set on the object."""
+        return self.__name
+
+    @abc.abstractmethod
+    def _set_values(self):
+        pass
+
+    def __set_regex(self):
+        self.__regex = re.compile(r'^{}$'.format(self._get_joined_pattern()))
+
+    def __validate(self):
+        if not self.name:
+            return
+        self.set_name(self.name)
+
+    def set_name(self, name: str):
+        """Set this object's name to the provided string.
+
+        :param name: The name to be set on this object.
+        :raises NameError: If an invalid string is provided.
+        :returns: Reference to self.
+        """
+        match = self.__regex.match(name)
+        if not match:
+            msg = ' '.join([rf"Can't set invalid name '{name}' on {self.__class__.__name__} instance.",
+                            rf"Valid convention is: {self.__class__().get_name()}"])
+            raise NameError(msg)
+        self.__set_name(name)
+        self.__values.update(match.groupdict())
+        return self
+
+    @property
+    def _values(self) -> typing.Dict[str, str]:
+        return self.__values
+
+    @abc.abstractmethod
+    def get_pattern_list(self) -> typing.List[str]:
+        return []
+
+    def _get_values_pattern(self) -> typing.List[str]:
+        return [getattr(self, p) for p in self.get_pattern_list()]
+
+    def _get_joined_pattern(self) -> str:
+        return self._separator_pattern.join(self._get_values_pattern())
+
+    @property
+    def values(self) -> dict:
+        """The field values of this object's name as a dictionary in the form of {field: value}."""
+        return {k: v for k, v in self.__items if not self._filter_kv(k, v)}
+
+    def _filter_kv(self, k: str, v) -> bool:
+        if self._filter_k(k) or self._filter_v(v):
+            return True
+
+    def _filter_k(self, k: str):
+        pass
+
+    def _filter_v(self, v):
+        return v is None
+
+    @property
+    def nice_name(self) -> str:
+        """This object's pure name attribute."""
+        return self._get_nice_name()
+
+    def _get_nice_name(self, **values) -> str:
+        return self._separator.join(self._iter_translated_pattern_list('get_pattern_list', **values))
+
+    def get_name(self, **values) -> str:
+        """Get a new name string from this object's name values.
+
+        :param values: Variable keyword arguments where the **key** should refer to a field on this object that will
+                       use the provided value to build the new name.
+        """
+        if not values and self.name:
+            return self.name
+        if values:
+            for ck, cv in getattr(self, 'compounds', {}).items():
+                if ck not in values:
+                    compounds = []
+                    for c in cv:
+                        value = values.get(c) or getattr(self, c)
+                        if value is None:  # 0 is a valid value
+                            break
+                        compounds.append(rf'{value}')
+                    else:
+                        values[ck] = ''.join(compounds)
+        return self._get_nice_name(**values)
+
+    def _iter_translated_pattern_list(self, pattern: str, **values) -> typing.Generator:
+        self_values = self._values
+        for p in getattr(self, pattern)():
+            nice_name = self._regex_property_name.search(p).group(0)
+            if nice_name in values:
+                value = str(values[nice_name])
+            elif self_values:
+                try:
+                    value = str(self_values[nice_name])
+                except KeyError:
+                    value = getattr(self, p)  # must be a valid property
+            else:
+                value = rf'[{nice_name}]'
+            yield value
+
+    def __str__(self):
+        return self.get_name()
+
+    def __repr__(self):
+        name = self.name or ''
+        return rf'{self.__class__.__name__}("{name}")'
