06ed2c0527744dde009b20e5302a55e5102b246b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package mklaren/1.0/mklaren-1.0.tar.gz

---
+++ packages/mklaren/mklaren-1.0.tar.gz/mklaren-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mklaren',
+  packages = ['mklaren'],
+  version = '1.0',
+  description = 'The Multiple Kernel Learning Python Library.',
+  author = 'Martin Strazar',
+  author_email = 'martin.strazar@gmail.com',
+  url = 'https://github.com/mstrazar/mklaren',
+  download_url = 'https://github.com/mstrazar/mklaren/archive/1.1.tar.gz',
+  keywords = ['machine learning', 'kernel methods', 'low-rank approximation', 'regression'],
+  classifiers = [],
+)
