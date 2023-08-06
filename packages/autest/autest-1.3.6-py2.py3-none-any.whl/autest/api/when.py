f115bf69f8975b144c34e1e289f591f8381876d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:34 +0000

    Add package autest/1.3.6/autest-1.3.6-py2.py3-none-any.whl

---
+++ packages/autest/autest-1.3.6-py2.py3-none-any.whl/autest/api/when.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function
+import autest.glb as glb
+import hosts.output as host
+
+
+def AddWhenFunction(func, name=None):
+    if name is None:
+        name = func.__name__
+
+    def wrapper(self, *lst, **kw):
+        if lst != () or kw != {}:
+            # we have arguments, bind them to call with lambda
+            return lambda: func(*lst, **kw)
+        else:
+            # just pass back function
+            return func
+
+    wrapper.when_wrapper = True
+    method = wrapper
+    setattr(glb.When, name, method)
+    host.WriteVerbose("api", 'Added extension function "{0}"'.format(name))
