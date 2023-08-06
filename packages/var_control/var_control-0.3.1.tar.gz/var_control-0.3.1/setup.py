dc631252c6274ffd7ed3ac32ec384449a7e8ad53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package var_control/0.3.1/var_control-0.3.1.tar.gz

---
+++ packages/var_control/var_control-0.3.1.tar.gz/var_control-0.3.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='var_control',
+      version='0.3.1',
+      description='Decorrelates variable from a pandas dataframe.',
+      url='https://github.com/eddbrown/var_control',
+      author='Edward Brown',
+      author_email='edward.j.e.brown@gmail.com',
+      packages=['var_control'],
+      zip_safe=False)
