098a63b67b86362cbcaba28b22a36d14aedd9237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:45 +0000

    Add package code-contracts/0.1.0/code-contracts-0.1.0.tar.gz

---
+++ packages/code-contracts/code-contracts-0.1.0.tar.gz/code-contracts-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# Copyright 2017 Benoit Bernard All Rights Reserved.
+
+import contracts
+from setuptools import setup
+
+with open("README.rst", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(name=contracts.__title__,
+      version=contracts.__version__,
+      description='A library that provides code contracts and assertions.',
+      long_description=readme,
+      url='https://github.com/mbenbernard/contracts',
+      author=contracts.__author__,
+      author_email=contracts.__author_email__,
+      license=contracts.__license__,
+      packages=['contracts'],
+      zip_safe=False,
+      python_requires="~=3.3",
+      install_requires=[],
+      classifiers=(
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy'
+      ))
