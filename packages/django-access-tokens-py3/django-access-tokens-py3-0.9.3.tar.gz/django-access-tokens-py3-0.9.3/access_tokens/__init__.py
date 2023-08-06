d177e71cb336860dc1d7eebc05166ac66cc964d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:52 +0000

    Add package django-access-tokens-py3/0.9.3/django-access-tokens-py3-0.9.3.tar.gz

---
+++ packages/django-access-tokens-py3/django-access-tokens-py3-0.9.3.tar.gz/django-access-tokens-py3-0.9.3/access_tokens/__init__.py
@@ -0,0 +1,15 @@
+"""
+A Django app for for generating secure scoped access tokens.
+
+Developed by Mohawk.
+
+<http://www.mohawkhq.com/>
+
+
+Contributors
+------------
+
+- Dave Hall <http://blog.etianen.com/>
+"""
+
+__version__ = (0, 9, 3)
