22d628cb2b4bb147474fbb484edc03758c553d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package cat2017-8-25/2.0.0/cat2017-8-25-2.0.0.tar.gz

---
+++ packages/cat2017-8-25/cat2017-8-25-2.0.0.tar.gz/cat2017-8-25-2.0.0/cat2017-8-25.py
@@ -0,0 +1,11 @@
+"""这是一个例子"""
+def print_lol(the_list,indent=False,level=0,fh=sys.stdout):
+        """success finally"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,indent,level+1,fh=sys.stdout)
+                else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t",end='',file=fh)
+                        print(each_item,file=fh)
