2cf15ba2eb4dbd9998303a0116b09b3f94c7db36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:24 +0000

    Add package andromeda/0.1.0/andromeda-0.1.0.tar.gz

---
+++ packages/andromeda/andromeda-0.1.0.tar.gz/andromeda-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='andromeda',
+      version='0.1.0',
+      description='declarative, class-driven framework written around flask',
+      author='Roy Sommer',
+      url='https://www.github.com/illberoy/andromeda',
+      packages=find_packages(),
+      include_package_data=True,
+      py_modules=['andromeda'],
+      install_requires=['flask==0.12.2'])
