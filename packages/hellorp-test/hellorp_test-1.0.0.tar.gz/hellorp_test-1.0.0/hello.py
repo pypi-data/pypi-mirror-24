54a713a2f64467f446a088e5d131ab625c018160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package hellorp-test/1.0.0/hellorp_test-1.0.0.tar.gz

---
+++ packages/hellorp-test/hellorp_test-1.0.0.tar.gz/hellorp_test-1.0.0/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+for python package test
+just for fun
+'''
+
+def hello():
+    '''print hello to terminal'''
+    print("hello")
+
+if __name__ == "__main__":
+    hello()
