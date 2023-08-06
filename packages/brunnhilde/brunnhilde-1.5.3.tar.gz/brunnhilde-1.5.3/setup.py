ed8571c4e63fe667b9ddec7f74cd619bf6b9d52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:19 +0000

    Add package brunnhilde/1.5.3/brunnhilde-1.5.3.tar.gz

---
+++ packages/brunnhilde/brunnhilde-1.5.3.tar.gz/brunnhilde-1.5.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name = 'brunnhilde',
+    version = '1.5.3',
+    url = 'https://github.com/timothyryanwalsh/brunnhilde',
+    author = 'Tim Walsh',
+    author_email = 'timothyryanwalsh@gmail.com',
+    py_modules = ['brunnhilde'],
+    scripts = ['brunnhilde.py'],
+    description = 'A Siegfried-based digital archives reporting tool for directories and disk images',
+    keywords = 'archives reporting characterization identification diskimages',
+    platforms = ['POSIX'],
+    test_suite='test',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Natural Language :: English', 
+        'Operating System :: MacOS',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Communications :: File Sharing',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities'
+    ],
+)
