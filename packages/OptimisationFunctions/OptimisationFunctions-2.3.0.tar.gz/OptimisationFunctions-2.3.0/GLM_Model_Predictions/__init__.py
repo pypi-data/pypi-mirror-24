0fb2c0ee5fae4ae55cc389f427053dbb4322f92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:29 +0000

    Add package OptimisationFunctions/2.3.0/OptimisationFunctions-2.3.0.tar.gz

---
+++ packages/OptimisationFunctions/OptimisationFunctions-2.3.0.tar.gz/OptimisationFunctions-2.3.0/GLM_Model_Predictions/__init__.py
@@ -0,0 +1,5 @@
+from .churns import churn_percentage_matrix
+from .churns import customers_on_base
+from .data_functions import base_data_creator
+from .arpus import ARPU_numbers_matrix
+from .arpus import ARPU_numbers
