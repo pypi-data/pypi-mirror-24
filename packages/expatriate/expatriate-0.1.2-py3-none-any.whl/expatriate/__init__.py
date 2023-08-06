958ff7b50a2fec7aa1b57119dc763291914fb84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:36 +0000

    Add package expatriate/0.1.2/expatriate-0.1.2-py3-none-any.whl

---
+++ packages/expatriate/expatriate-0.1.2-py3-none-any.whl/expatriate/__init__.py
@@ -0,0 +1,36 @@
+# Copyright 2016 Casey Jaymes
+
+# This file is part of Expatriate.
+#
+# Expatriate is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Expatriate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Expatriate.  If not, see <http://www.gnu.org/licenses/>.
+
+from .CharacterData import CharacterData
+from .Comment import Comment
+from .Document import Document
+from .Element import Element
+from .Element import DuplicateNamespaceException
+from .Element import UnknownNamespaceException
+from .ProcessingInstruction import ProcessingInstruction
+from .xpath import SyntaxException
+
+__all__ = [
+    'CharacterData',
+    'Comment',
+    'Document',
+    'Element',
+    'ProcessingInstruction',
+    'DuplicateNamespaceException',
+    'UnknownNamespaceException',
+    'SyntaxException',
+]
