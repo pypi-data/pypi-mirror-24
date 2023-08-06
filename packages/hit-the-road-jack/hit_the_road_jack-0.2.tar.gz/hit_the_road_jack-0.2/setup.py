755f509894f1e3c96c1f54a5e24df94a438736c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:58 +0000

    Add package hit-the-road-jack/0.2/hit_the_road_jack-0.2.tar.gz

---
+++ packages/hit-the-road-jack/hit_the_road_jack-0.2.tar.gz/hit_the_road_jack-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hit_the_road_jack',
+      version='0.2',
+      description='''And don't you come back no more''',
+      url='https://github.com/octopyth/hit_the_road_jack',
+      author='Sam Rock',
+      author_email='sam.rock@octopyth.com',
+      license='GNU',
+      packages=['hit_the_road_jack'],
+      zip_safe=False)
