762678a9a75bb41bb2fc532f3bbb04bec5f2dc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:43 +0000

    Add package django-arcgis-marketplace/0.0.9/django-arcgis-marketplace-0.0.9.tar.gz

---
+++ packages/django-arcgis-marketplace/django-arcgis-marketplace-0.0.9.tar.gz/django-arcgis-marketplace-0.0.9/tests/test_managers.py
@@ -0,0 +1,33 @@
+import responses
+
+from django.test import TestCase
+
+from arcgis_marketplace import factories
+from arcgis_marketplace import models
+
+from . import factories as test_factories
+from .api.shortcuts import add_response
+
+
+class ManagersTests(TestCase):
+
+    @responses.activate
+    def test_create_item(self):
+        add_response(
+            'POST',
+            'content/users/test/addItem',
+            json={'success': True}
+        )
+
+        account = factories.AccountFactory()
+        item = test_factories.WebMapingAppZipFactory()
+
+        item_in_account = models.ItemInAccount.objects.create_item(
+            account,
+            item,
+            arcgis_group={'test': True})
+
+        self.assertEqual(item_in_account.account, account)
+        self.assertEqual(item_in_account.item, item)
+
+        self.assertTrue(item_in_account.arcgis_group['test'])
