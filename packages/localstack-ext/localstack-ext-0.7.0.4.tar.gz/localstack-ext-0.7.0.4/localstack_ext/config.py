8625c322f5000d6420837678b27137144615a36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package localstack-ext/0.7.0.4/localstack-ext-0.7.0.4.tar.gz

---
+++ packages/localstack-ext/localstack-ext-0.7.0.4.tar.gz/localstack-ext-0.7.0.4/localstack_ext/config.py
@@ -0,0 +1,39 @@
+import os
+from localstack import config as localstack_config
+from localstack import constants as localstack_constants
+from localstack.config import HOSTNAME, USE_SSL
+
+# version of LocalStack-extended
+VERSION = '0.7.0.4'
+
+# api server config
+API_PATH = '/v1'
+API_URL = localstack_constants.API_ENDPOINT
+
+# api endpoints
+API_PATH_SUBSCRIBE = '%s/subscribe' % API_PATH
+API_PATH_ACTIVATE = '%s/activate' % API_PATH
+API_PATH_EVENTS = '%s/events' % API_PATH
+API_PATH_GITHUB = '%s/github' % API_PATH
+API_PATH_USER = '%s/user' % API_PATH
+API_PATH_CONFIG = '%s/config' % API_PATH
+API_PATH_CI = '%s/ci' % API_PATH
+
+ROOT_FOLDER = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+PROTECTED_FOLDERS = ('services', 'utils')
+
+# database connection settings
+DB_NAME = 'localstack'
+DB_USER = os.environ.get('DB_USER') or 'localstack'
+DB_PASS = os.environ.get('DB_PASS')
+
+# add default service ports
+localstack_constants.DEFAULT_SERVICE_PORTS['cognito-idp'] = 4590
+localstack_constants.DEFAULT_SERVICE_PORTS['cognito-identity'] = 4591
+localstack_constants.DEFAULT_SERVICE_PORTS['sts'] = 4592
+localstack_constants.DEFAULT_SERVICE_PORTS['iam'] = 4593
+localstack_constants.DEFAULT_SERVICE_PORTS['edge'] = 443
+
+# re-initialize configs in localstack
+localstack_config.populate_configs()
