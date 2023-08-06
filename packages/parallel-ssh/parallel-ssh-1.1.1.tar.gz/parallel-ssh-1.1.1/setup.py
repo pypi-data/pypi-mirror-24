ead7e1845cf1e67f331ab974063abdbcd415608d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package parallel-ssh/1.1.1/parallel-ssh-1.1.1.tar.gz

---
+++ packages/parallel-ssh/parallel-ssh-1.1.1.tar.gz/parallel-ssh-1.1.1/setup.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2014-2017 Panos Kittenis
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, version 2.1.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+from setuptools import setup, find_packages
+from platform import python_version
+
+import versioneer
+
+gevent_req = 'gevent<=1.1' if python_version() < '2.7' else 'gevent>=1.1'
+
+setup(name='parallel-ssh',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Asynchronous parallel SSH library',
+      long_description=open('README.rst').read(),
+      author='Panos Kittenis',
+      author_email='22e889d8@opayq.com',
+      url="https://github.com/ParallelSSH/parallel-ssh",
+      packages=find_packages(
+          '.', exclude=('embedded_server', 'embedded_server.*',
+                        'tests', 'tests.*',
+                        '*.tests', '*.tests.*')
+      ),
+      install_requires=['paramiko<2.2', gevent_req],
+      classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: Microsoft :: Windows',
+        ],
+      )
