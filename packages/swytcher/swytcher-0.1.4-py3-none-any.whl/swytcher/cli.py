32dbb202adf61fc94dca59a812ad1c3d7387a9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:55 +0000

    Add package swytcher/0.1.4/swytcher-0.1.4-py3-none-any.whl

---
+++ packages/swytcher/swytcher-0.1.4-py3-none-any.whl/swytcher/cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""CLI frontend for Swytcher"""
+import logging
+
+import click
+
+import swytcher.settings as settings
+import swytcher.swytcher as swytcher
+
+log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+logging.basicConfig(level=logging.INFO)
+
+
+@click.command()
+def main(args=None):
+    """Console script for swytcher"""
+    # click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo(__name__)
+    settings.load_configs()  # pylint: disable=protected-access
+
+    swytcher.main(args)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
