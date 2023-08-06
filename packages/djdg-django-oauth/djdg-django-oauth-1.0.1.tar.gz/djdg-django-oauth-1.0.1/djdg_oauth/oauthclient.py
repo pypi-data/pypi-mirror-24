d88e38930149be657458513963bd04e63a0a0b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:51 +0000

    Add package djdg-django-oauth/1.0.1/djdg-django-oauth-1.0.1.tar.gz

---
+++ packages/djdg-django-oauth/djdg-django-oauth-1.0.1.tar.gz/djdg-django-oauth-1.0.1/djdg_oauth/oauthclient.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+import requests
+from django.conf import settings as django_settings
+import re
+try:
+    from urlparse import urlparse, urlunparse
+except ImportError:
+    from urllib.parse import urlparse, urlunparse
+from .oauthcore import get_verify_key, verifySign, set_parameters, getSign, get_sign_key
+import logging
+from django.http.response import HttpResponse
+log_settings = "oauthlib"
+if hasattr(django_settings, 'DJDG_AUTH'):
+    log_settings = django_settings.DJDG_AUTH.get('log', log_settings)
+log = logging.getLogger(log_settings)
+from .oauthcore import to_unicode
+
+
+class Http401Response(HttpResponse):
+
+    status_code = 401
+
+
+class OAuthClient(object):
+
+    keys = dict()
+    apps = dict()
+
+    """
+    TODO: add docs
+    """
+
+    def __init__(self):
+        """
+        :params server: An instance of oauthlib.oauth2.Server class
+        """
+        pass
+
+    def _get_escaped_full_path(self, request):
+        """
+        Django considers "safe" some characters that aren't so for oauthlib.
+        We have to search for them and properly escape.
+        """
+        parsed = list(urlparse(request.get_full_path()))
+        return urlunparse(parsed)
+
+    def _get_key_obj(self, appid):
+        if self.keys.get(appid):
+            keys_obj = self.keys.get(appid)
+        else:
+            keys_obj = get_verify_key(appid)
+            self.keys[appid] = keys_obj
+        return keys_obj
+
+    def _get_app_obj(self, app):
+        if self.apps.get(app):
+            return self.apps[app]
+        else:
+            key_app = get_sign_key(app)
+            self.apps[app] = key_app
+            return key_app
+
+
+    def _extract_params(self, request):
+        """
+        """
+        uri = self._get_escaped_full_path(request)
+        http_method = request.method
+        headers = self.extract_headers(request)
+        body = self.extract_body(request)
+        return uri, http_method, body, headers
+
+    def extract_headers(self, request):
+        """
+        Extracts headers from the Django request object
+        :param request: The current django.http.HttpRequest object
+        :return: a dictionary with OAuthLib needed headers
+        """
+        headers = request.META.copy()
+        if 'HTTP_AUTHORIZATION' in headers:
+            headers['Authorization'] = headers['HTTP_AUTHORIZATION']
+        return headers
+
+    def extract_body(self, request):
+        """
+        Extracts the POST body from the Django request object
+        :param request: The current django.http.HttpRequest object
+        :return: provided POST parameters
+        """
+        CONTENT_TYPE = request.environ.get("CONTENT_TYPE")
+        if CONTENT_TYPE and CONTENT_TYPE.upper().find("APPLICATION/JSON") >= 0:
+            return request.body
+        elif request.method == "GET":
+            return request.GET.dict()
+        else:
+            return request.POST.dict()
+
+    def verify_request(self, request):
+        """
+        :param request: The current django.http.HttpRequest object
+        """
+        if hasattr(django_settings, "DJDG_AUTH"):
+            full_escape_url = django_settings.DJDG_AUTH.get(
+                'FULL_ESCAPE_URL', [])
+            regex_escape_url = django_settings.DJDG_AUTH.get(
+                'REGEX_ESCAPE_URL', [])
+            regex_check_url = django_settings.DJDG_AUTH.get(
+                'REGEX_CHECK_URL', [])
+            path = request.path.strip('/')
+            if regex_check_url:
+                for url in regex_check_url:
+                    if re.match(url, path):
+                        break
+                else:
+                    return
+            else:
+                if path in full_escape_url:
+                    return
+                for url in regex_escape_url:
+                    if re.match(url, path):
+                        return
+        uri, http_method, body, headers = self._extract_params(request)
+        dict_body = body
+        if body and not isinstance(body, dict):
+            dict_body = json.loads(body)
+        if not dict_body.get("appid"):
+            raise Exception(u"can not find appid in request body")
+        appid = dict_body["appid"]
+        keys_obj = self._get_key_obj(appid)
+        if not keys_obj:
+            raise Exception(
+                "can not fetch any settings, please make sure appid incorrect")
+        signature = headers.get("Authorization")
+        request.client_type = keys_obj.app
+        return verifySign(body, keys_obj.secret, signature)
+
+    @staticmethod
+    def oauth_request(url, method, app, parameters=None, headers=None, files=None):
+        appkeys = OAuthClient()._get_app_obj(app)
+        secret, parameters = set_parameters(parameters, appkeys)
+        if method == "get":
+            params = parameters
+        else:
+            params = json.dumps(to_unicode(parameters))
+        try:
+            signature = getSign(params, secret)
+        except Exception as e:
+            log.info(e.message)
+            return {"statusCode": 500, "msg": e.message}
+        headers_dict = {
+            "Accept": "application/json",
+            "Authorization": signature
+        }
+        if files:
+            re = requests.Request(
+                method=method, url=url,
+                params=params, headers=headers_dict,
+                files=files
+                )
+        elif method == "get":
+            re = requests.Request(
+                method=method, url=url,
+                params=params, headers=headers_dict)
+        else:
+            headers_dict["Content-Type"] = "application/json; charset=utf-8"
+            re = requests.Request(
+                method=method, url=url,
+                data=params, headers=headers_dict)
+        pre_re = re.prepare()
+        res_session = requests.Session()
+        r = res_session.send(pre_re)
+        if r.status_code != 200:
+            log.error(r.content)
+            return {"statusCode": 500, "msg": r.content}
+        else:
+                try:
+                    log.info(r.json())
+                    return {"statusCode": 0, "content": r.json()}
+                except:
+                    log.info(r.content)
+                    return {"statusCode": 0, "content": r.content}
---
+++ packages/djdg-django-oauth/djdg-django-oauth-1.0.1.tar.gz/djdg-django-oauth-1.0.1/djdg_oauth/oauthcore.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+import sys
+from django.conf import settings as django_settings
+import random
+import hashlib
+from .models import OauthApps
+import urlparse
+import logging
+PY3 = sys.version_info[0] == 3
+
+
+if PY3:
+    unicode_type = str
+    bytes_type = bytes
+else:
+    unicode_type = unicode
+    bytes_type = str
+log_settings = "oauthlib"
+if hasattr(django_settings, 'DJDG_AUTH'):
+    log_settings = django_settings.DJDG_AUTH.get('log', log_settings)
+log = logging.getLogger(log_settings)
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def createNoncestr(length=32):
+    """产生随机字符串，不长于32位"""
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    strs = []
+    for x in range(length):
+        strs.append(chars[random.randrange(0, len(chars))])
+    return "".join(strs)
+
+
+def formatBizQueryParaMap(paraMap, urlencode):
+    """格式化参数，签名过程需要使用"""
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    if isinstance(paraMap, (str, unicode)):
+        return paraMap
+    paraMap = to_unicode(paraMap)
+    slist = sorted(paraMap)
+    buff = []
+    for k in slist:
+        v = paraMap[k]
+        if v is None or v == "":
+            # 为空直接跳过
+            continue
+        buff.append("{0}={1}".format(k, str(v)))
+    return "&".join(buff)
+
+
+def getSign(obj, secret):
+    """生成签名"""
+    # 签名步骤一：按字典序排序参数,formatBizQueryParaMap已做
+    String = formatBizQueryParaMap(obj, True)
+    # 签名步骤二：在string后加入KEY
+    String = "{0}&secret={1}".format(String, secret)
+    # 签名步骤三：MD5加密
+    String = hashlib.md5(String).hexdigest()
+    # 签名步骤四：所有字符转为大写
+    result_ = String.upper()
+    return result_
+
+
+def verifySign(obj, secret, signature):
+    sign = getSign(obj, secret)
+    log.info("request Sign: {}".format(signature))
+    log.info("verify Sign: {}".format(sign))
+    return signature == sign
+
+
+def get_sign_key(app):
+    if hasattr(django_settings, "DJDG_AUTH"):
+        auth_list = [
+            x for x in django_settings.DJDG_AUTH.get(
+                "APPS", []) if x["app"] == app]
+        return auth_list[0] if auth_list else None
+    else:
+        return None
+
+
+def get_verify_key(appid):
+    auth_list = OauthApps.objects.filter(appid=appid)
+    return auth_list[0] if auth_list else None
+
+
+def set_parameters(parameters, appkeys):
+    if not appkeys:
+        raise Exception("app secret not find, please check you settings file")
+    parameters["appid"] = appkeys["appid"]
+    parameters["nonce_str"] = createNoncestr(12)
+    return appkeys["secret"], parameters
+
+
+def add_querystr_to_params(url, params):
+    """pick url querystring to params"""
+    sch, net, path, par, query, fra = urlparse.urlparse(url)
+    query_params = urlparse.parse_qsl(query, keep_blank_values=True)
+    params.update(query_params)
+    uri = urlparse.urlunparse((sch, net, path, par, '', fra))
+    return uri, params
+
+
+def to_unicode(data, encoding='UTF-8'):
+    """Convert a number of different types of objects to unicode."""
+    import datetime
+    import decimal
+    type_to_str = (datetime.datetime, decimal.Decimal)
+    if hasattr(data, '__iter__'):
+        if isinstance(data, list):
+            # Assume it's a one list data structure
+            data = [to_unicode(i, encoding) for i in data]
+        else:
+            # We support 2.6 which lacks dict comprehensions
+            if hasattr(data, 'items'):
+                data = data.items()
+            data = dict([(to_unicode(k, encoding), to_unicode(v, encoding)) for k, v in data])
+    if isinstance(data, type_to_str):
+        data = str(data)
+    if isinstance(data, bytes_type):
+        data = unicode(data, encoding='utf-8')
+    return data
