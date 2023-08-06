b0c70e72bafb29dd6ab378ed3bf2ebc5ede7b925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:10 +0000

    Add package pylatus/2017.8.25/pylatus-2017.8.25.tar.gz

---
+++ packages/pylatus/pylatus-2017.8.25.tar.gz/pylatus-2017.8.25/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+    import compile
+    compile.main()
+except ImportError:
+    pass
+
+
+setup(
+    name='pylatus',
+    version='2017.8.25',
+    packages=[
+        'Pylatus',
+        'Pylatus.gui',
+        'Pylatus.gui.ui',
+        'Pylatus.scripo',
+        'Pylatus.devices',
+        'Pylatus.controller',
+    ],
+    url='https://hg.3lp.cx/pylatus',
+    license='GPL',
+    author='Vadim Dyadkin',
+    author_email='diadkin@esrf.fr',
+    description='Driver for diffractometers based on SPEC and Pilatus',
+    entry_points={
+        'gui_scripts': [
+            'pylatus=Pylatus:main',
+        ],
+    },
+    install_requires=[
+        'pyqtgraph',
+        'aspic',
+        'cryio',
+        'integracio',
+        'scipy',
+        'numpy',
+        'auxygen',
+        'kombu',
+        'pylatus_brokers',
+    ]
+)
