38853dc331730d9cd519e0c3bdff912296271dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:32 +0000

    Add package purse/1.0.0a1/purse-1.0.0a1.tar.gz

---
+++ packages/purse/purse-1.0.0a1.tar.gz/purse-1.0.0a1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+setup(
+    name='purse',
+    version='1.0.0a1',
+    description='Python Cryptocurrency Wallet Library',
+    author='Cochise Ruhulessin',
+    author_email='cochiseruhulessin@gmail.com',
+    url='https://www.wizardsofindustry.net',
+    project_name='Purse',
+    install_requires=[],
+    packages=[
+        'purse',
+    ]
+)
