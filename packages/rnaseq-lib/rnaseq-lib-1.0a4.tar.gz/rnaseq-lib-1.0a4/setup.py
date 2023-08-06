c3789706540a1e2a46b6efe6600acd416677b5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:00 +0000

    Add package rnaseq-lib/1.0a4/rnaseq-lib-1.0a4.tar.gz

---
+++ packages/rnaseq-lib/rnaseq-lib-1.0a4.tar.gz/rnaseq-lib-1.0a4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='rnaseq-lib',
+      version='1.0a4',
+      description='',
+      url='http://github.com/jvivian/rnaseq-lib',
+      author='John Vivian',
+      author_email='jtvivian@gmail.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      install_requires=['pandas',
+                        'numpy',
+                        'seaborn'])
