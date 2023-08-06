c76303d8339c7b01dc3624a427b2f482cd6ba213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:02 +0000

    Add package example2/0.1/example2-0.1.tar.gz

---
+++ packages/example2/example2-0.1.tar.gz/example2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='example2',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/marineF/georgette',
+      author='Georgette',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['example2'],
+      zip_safe=False)
