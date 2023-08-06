f870911942eabb6effbab7b4d8970f4bfabeda89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:29 +0000

    Add package Staticfy/1.5/Staticfy-1.5.tar.gz

---
+++ packages/Staticfy/Staticfy-1.5.tar.gz/Staticfy-1.5/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Staticfy',
+      version='1.5',
+      description='Convert static assets links to dynamic web framework links',
+      url='https://github.com/danidee10/Staticfy',
+      author='Osaetin Daniel',
+      author_email='osaetindaniel@gmail.com',
+      license='GPL',
+      entry_points={
+        'console_scripts': ['staticfy = staticfy.staticfy:main']
+      },
+      packages=['staticfy'],
+      install_requires=[
+          'beautifulsoup4',
+      ],
+      zip_safe=False)
