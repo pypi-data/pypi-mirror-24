5b947828b5838e41eb3c0cbe4883b53be9225d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:02 +0000

    Add package PCIT/1.1.1/PCIT-1.1.1.tar.gz

---
+++ packages/PCIT/PCIT-1.1.1.tar.gz/PCIT-1.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PCIT',
+    version='1.1.1',
+    packages=['', 'PCIT'],
+    url='https://github.com/SamBurkart/PCIT',
+    license='MIT License',
+    author='Sam',
+    author_email='samuel.burkart@aol.com',
+    description='',
+    install_requires=['sklearn', 'scipy', 'mlxtend'],
+)
