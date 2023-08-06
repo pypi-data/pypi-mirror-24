16d9a33dc3f1581d6992e9a498b624be30802fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:23 +0000

    Add package comiccrawler/2017.8.16/comiccrawler-2017.8.16.tar.gz

---
+++ packages/comiccrawler/comiccrawler-2017.8.16.tar.gz/comiccrawler-2017.8.16/comiccrawler/mods/eight.py
@@ -0,0 +1,87 @@
+#! python3
+
+"""this is 8comic module for comiccrawler.
+	
+http://www.comicbus.com/html/197.html
+
+"""
+
+import re
+from urllib.parse import urljoin
+
+from node_vm2 import VM
+
+from ..core import Episode, grabhtml, clean_tags
+
+domain = ["www.8comic.com", "www.comicvip.com", "www.comicbus.com"]
+name = "無限"
+
+def get_title(html, url):
+	return re.search("<title>([^<]+?)漫畫,",html).group(1)
+
+def get_episodes(html, url):
+	html = html.replace("\n", "")
+	
+	js = """
+		var output;
+		function getCookie() {}
+		var window = {
+			open: function(result){
+				output = result;
+			}
+		};
+		var document = {
+			location: {
+				href: ""
+			}
+		};
+	""" + grabhtml(urljoin(url, "/js/comicview.js"))
+	
+	s = []
+	matches = re.finditer(
+		r'<a [^>]*?onclick="(cview[^"]+?);[^>]*>(.+?)</a>',
+		html, re.M
+	)
+	with VM(js) as vm:
+		for match in matches:
+			cview, title = match.groups()
+			
+			vm.run(cview)
+			ep_url = vm.run("output")
+			title = clean_tags(title)
+
+			e = Episode(title, urljoin(url, ep_url))
+			s.append(e)
+	return s
+	
+def get_images(html, url):
+	m = re.search("ch=(\d+)", url)
+	if m is None:
+		ch = "1"
+	else:
+		ch = m.group(1)
+
+	def ss(str): # pylint: disable=invalid-name
+		return re.sub("[a-z]+", "", str)
+
+	cs = re.search("cs='([^']+)", html).group(1)
+	ti = re.search("ti=(\d+);", html).group(1)
+
+	i = 0
+	while i < len(cs):
+		if ch == ss(cs[i:i+4]):
+			code = cs[i:i+50]
+			break
+		i += 50
+	else:
+		code = cs[-50:]
+
+	pages = int(ss(code[7:10]))
+
+	s = []
+	for p in range(1, pages + 1):
+		hash = (((p - 1) // 10) % 10) + (((p - 1) % 10) * 3)
+		src = "http://img{}.8comic.com/{}/{}/{}/{:03}_{}.jpg".format(
+				ss(code[4:6]), code[6:7], ti, ch, p, code[hash + 10:hash + 13])
+		s.append(src)
+	return s
---
+++ packages/comiccrawler/comiccrawler-2017.8.16.tar.gz/comiccrawler-2017.8.16/comiccrawler/mods/facebook.py
@@ -0,0 +1,56 @@
+#! python3
+
+"""this is facebook module for comiccrawler
+
+Ex:
+	https://www.facebook.com/gtn.moe/photos/pcb.942148229165656/942147902499022/?type=3&theater
+	https://www.facebook.com/Nisinsheep/photos/pcb.1667254973516031/1667254333516095/?type=3&theater
+	https://www.facebook.com/photo.php?fbid=10202533447211476&set=a.10202533447131474.1073741835.1654599523&type=3&theater
+
+"""
+
+import re, urllib.parse, json
+# from urllib.parse import urljoin
+from html import unescape
+from ..core import Episode, grabhtml
+
+domain = ["www.facebook.com"]
+name = "FB"
+circular = True
+noepfolder = True
+
+def get_title(html, url):
+	try:
+		id = re.search(r"photos/([^/]+)", url).group(1)
+	except AttributeError:
+		id = re.search("set=([^&]+)", url).group(1)
+	title = re.search("<title[^>]*>([^<]+)", html).group(1)
+	return unescape("{} ({})".format(title, id))
+
+def get_episodes(html, url):
+	return [Episode("image", url)]
+
+def get_images(html, url):
+	try:
+		id = re.search(r"photos/[^/]+/(\d+)", url).group(1)
+	except AttributeError:
+		id = re.search("fbid=([^&]+)", url).group(1)
+	return urllib.parse.urljoin(url, "/photo/download/?fbid=" + id)
+
+def get_next_page(html, url):
+	match = re.search('photoPageNextNav"[^>]*?href="([^"]+)', html)
+	if match:
+		return urllib.parse.urljoin(url, match.group(1))
+		
+	fbset, fbid = re.search('photos/([^/]+)/([^/]+)', url).groups()
+	query = urllib.parse.urlencode({
+		"data": json.dumps({"fbid": fbid, "set": fbset}),
+		"__a": 1
+	})
+	pagelet = grabhtml(
+		"https://www.facebook.com/ajax/pagelet/generic.php/"
+		"PhotoViewerInitPagelet?" + query)
+	
+	next_id = re.search(r'"addPhotoFbids".*?(\d+)', pagelet).group(1)
+	return urllib.parse.urljoin(url, "../" + next_id + "/")
+	
