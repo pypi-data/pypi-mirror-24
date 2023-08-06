febe5c057824a09900a019f6b7e9e85b9bac3c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:11 +0000

    Add package pysitra/0.3.4/pysitra-0.3.4.tar.gz

---
+++ packages/pysitra/pysitra-0.3.4.tar.gz/pysitra-0.3.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pysitra',
+      version='0.3.4',
+      description='Python implementation of the most popular slovenian transformation methods (SiTra!)',
+      long_description=readme(),
+      url='https://github.com/marjanmo/pysitra',
+      author='Marjan Moderc',
+      author_email='marjan.moderc@gmail.com',
+      license='MIT',
+      packages=['pysitra'],
+      install_requires=['pandas','geopandas',"numpy","scipy","shapely","click"],
+      include_package_data=True,
+      entry_points = {'console_scripts': ['sitra=pysitra.sitra_cli:cli']},
+      zip_safe=False)
