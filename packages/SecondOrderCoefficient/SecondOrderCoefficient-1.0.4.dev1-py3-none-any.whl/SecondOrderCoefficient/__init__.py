c32c0324409067b977eb424a9320c11472732d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:25 +0000

    Add package SecondOrderCoefficient/1.0.4.dev1/SecondOrderCoefficient-1.0.4.dev1-py3-none-any.whl

---
+++ packages/SecondOrderCoefficient/SecondOrderCoefficient-1.0.4.dev1-py3-none-any.whl/SecondOrderCoefficient/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Haoruo Liu'
+__title__ = 'SecondOrderCoefficient'
+
+
+
+
+import SecondOrderCoefficient.second_order_max_degree
+from SecondOrderCoefficient.second_order_max_degree import *
+
+import SecondOrderCoefficient.second_order_average_degree
+from SecondOrderCoefficient.second_order_average_degree import *
