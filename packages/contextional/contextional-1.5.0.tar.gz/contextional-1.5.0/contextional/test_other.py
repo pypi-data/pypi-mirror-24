d81889360533a51a996efe036791905d7b81f45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:51 +0000

    Add package contextional/1.5.0/contextional-1.5.0.tar.gz

---
+++ packages/contextional/contextional-1.5.0.tar.gz/contextional-1.5.0/contextional/test_other.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def test_it():
+    pass
+
+
+def test_i2t():
+    pass
+
+
+class TestThings(unittest.TestCase):
+
+    def test_a(self):
+        pass
+
+    def test_b(self):
+        pass
