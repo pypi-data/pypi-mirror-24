8d8d2949da946c58a15b30511e59b95df47e2756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:39 +0000

    Add package django-connectwise/0.0.69a0/django-connectwise-0.0.69a0.tar.gz

---
+++ packages/django-connectwise/django-connectwise-0.0.69a0.tar.gz/django-connectwise-0.0.69a0/djconnectwise/tests/fixture_utils.py
@@ -0,0 +1,79 @@
+from . import fixtures
+from . import mocks
+from djconnectwise import models
+from djconnectwise import sync
+
+
+def init_boards():
+    """
+    Initialize local board instances from fixture data.
+    """
+    models.ConnectWiseBoard.objects.all().delete()
+    mocks.service_api_get_boards_call(fixtures.API_BOARD_LIST)
+    synchronizer = sync.BoardSynchronizer()
+    return synchronizer.sync()
+
+
+def init_board_statuses():
+    init_boards()
+    mocks.service_api_get_statuses_call(fixtures.API_BOARD_STATUS_LIST)
+    synchronizer = sync.BoardStatusSynchronizer()
+    return synchronizer.sync()
+
+
+def init_teams():
+    models.Team.objects.all().delete()
+    mocks.service_api_get_teams_call(fixtures.API_SERVICE_TEAM_LIST)
+    synchronizer = sync.TeamSynchronizer()
+    return synchronizer.sync()
+
+
+def init_members():
+    models.Member.objects.all().delete()
+    mocks.system_api_get_members_call(fixtures.API_MEMBER_LIST)
+    synchronizer = sync.MemberSynchronizer()
+    return synchronizer.sync()
+
+
+def init_tickets():
+    mocks.service_api_tickets_call()
+    synchronizer = sync.TicketSynchronizer()
+    return synchronizer.sync()
+
+
+def init_priorities():
+    mocks.service_api_get_priorities_call([fixtures.API_SERVICE_PRIORITY])
+    synchronizer = sync.PrioritySynchronizer()
+    return synchronizer.sync()
+
+
+def init_projects():
+    mocks.project_api_get_projects_call(fixtures.API_PROJECT_LIST)
+    synchronizer = sync.ProjectSynchronizer()
+    return synchronizer.sync()
+
+
+def init_companies():
+    mocks.company_api_get_call(fixtures.API_COMPANY_LIST)
+    synchronizer = sync.CompanySynchronizer()
+    return synchronizer.sync()
+
+
+def init_locations():
+    mocks.service_api_get_locations_call(fixtures.API_SERVICE_LOCATION_LIST)
+    synchronizer = sync.LocationSynchronizer()
+    return synchronizer.sync()
+
+
+def init_opportunity_statuses():
+    mocks.sales_api_get_opportunity_statuses_call(
+        fixtures.API_SALES_OPPORTUNITY_STATUSES)
+    synchronizer = sync.OpportunityStatusSynchronizer()
+    return synchronizer.sync()
+
+
+def init_opportunity_types():
+    mocks.sales_api_get_opportunity_types_call(
+        fixtures.API_SALES_OPPORTUNITY_TYPES)
+    synchronizer = sync.OpportunityTypeSynchronizer()
+    return synchronizer.sync()
