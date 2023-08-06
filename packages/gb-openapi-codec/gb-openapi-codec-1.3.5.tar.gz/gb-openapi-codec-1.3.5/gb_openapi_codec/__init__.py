fa05e669d3a06d87de7565f2c6410cda1064e591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:40 +0000

    Add package gb-openapi-codec/1.3.5/gb-openapi-codec-1.3.5.tar.gz

---
+++ packages/gb-openapi-codec/gb-openapi-codec-1.3.5.tar.gz/gb-openapi-codec-1.3.5/gb_openapi_codec/__init__.py
@@ -0,0 +1,11 @@
+import importlib
+openapi_codec = importlib.import_module('gb_openapi_codec.python-openapi-codec.openapi_codec')
+# https://stackoverflow.com/a/21221452/784648
+module_dict = openapi_codec.__dict__
+try:
+    to_import = openapi_codec.__all__
+except AttributeError:
+    to_import = [name for name in module_dict if not name.startswith('_')]
+locals().update({name: module_dict[name] for name in to_import})
+__all__ = list(locals().keys())
+__version__ = '1.3.5'
