6caa5419707046f1cfe45d4c0e91c60cf50d7218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:51 +0000

    Add package jackspaeeow/1.0.0/jackspaeeow-1.0.0.tar.gz

---
+++ packages/jackspaeeow/jackspaeeow-1.0.0.tar.gz/jackspaeeow-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'jackspaeeow', 
+    version = '1.0.0',  
+    py_modules = ['jacksparrow'], 
+    author = 'davystokess',  
+    author_email = 'davystokess@outlook.com', 
+    url = 'http://www.nanohust.com', 
+    description = 'A simple example'
+      ) 
