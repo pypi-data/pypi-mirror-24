d4e48d45765b15280248310cd4d90b154e49f697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:11 +0000

    Add package onegov.org/0.14.6/onegov.org-0.14.6-py3-none-any.whl

---
+++ packages/onegov.org/onegov.org-0.14.6-py3-none-any.whl/onegov/org/views/settings.py
@@ -0,0 +1,47 @@
+""" The settings view, defining things like the logo or color of the org. """
+
+from onegov.core.security import Secret
+from onegov.org.elements import Link
+from onegov.org.layout import DefaultLayout
+from onegov.org import _
+from onegov.org.app import OrgApp
+from onegov.org.forms import SettingsForm
+from onegov.org.models import Organisation
+
+
+@OrgApp.form(
+    model=Organisation, name='einstellungen', template='form.pt',
+    permission=Secret, form=SettingsForm)
+def handle_settings(self, request, form):
+    """ Handles the GET and POST login requests. """
+
+    layout = DefaultLayout(self, request)
+    layout.include_editor()
+    layout.include_code_editor()
+
+    request.include('check_contrast')
+
+    if form.submitted(request):
+        form.populate_obj(self, exclude={'primary_color', 'footer-height'})
+        self.theme_options = form.theme_options
+
+        request.success(_("Your changes were saved"))
+
+        if 'return-to' in request.params:
+            return request.redirect(request.link(self, name='einstellungen'))
+
+    elif request.method == 'GET':
+        form.process(obj=self)
+        form.theme_options = self.theme_options
+
+    layout.breadcrumbs = [
+        Link(_("Homepage"), layout.homepage_url),
+        Link(_("Settings"), request.link(self))
+    ]
+
+    return {
+        'layout': layout,
+        'title': _('Settings'),
+        'form': form,
+        'form_width': 'huge'
+    }
