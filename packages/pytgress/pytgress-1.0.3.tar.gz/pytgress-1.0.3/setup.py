708e9609e3452459d865523b5a163af0fb634cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:59 +0000

    Add package pytgress/1.0.3/pytgress-1.0.3.tar.gz

---
+++ packages/pytgress/pytgress-1.0.3.tar.gz/pytgress-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pytgress',
+    packages=['pytgress', 'pytgress.loaders'],
+    version='1.0.3',
+    description='Beautiful progress bars for the CLI',
+    author='Sayan Goswami',
+    author_email='goswami.sayan47+pygress@gmail.com',
+    url='https://github.com/sayan98/pytgress',
+    download_url='https://github.com/sayan98/pytgress/archive/v1.0.3.tar.gz',
+    keywords=['progress', 'bar', 'spinner', "loader", "loading"],
+    classifiers=[],
+)
