e54952a22ab9394fafabf1983b4b2cc7dd5c9736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package ss-tools/0.0.4/ss-tools-0.0.4.tar.gz

---
+++ packages/ss-tools/ss-tools-0.0.4.tar.gz/ss-tools-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ss-tools',
+    version='0.0.4',
+    py_modules=['sstools'],
+    author='sspan',
+    author_email='sspantiezheng@gmail.com',
+    url='http://sspan.org',
+    description='a tools set of ss',
+)
---
+++ packages/ss-tools/ss-tools-0.0.4.tar.gz/ss-tools-0.0.4/sstools.py
@@ -0,0 +1,28 @@
+import math
+import random
+import sys
+
+
+# generate a guassrandom
+def guassrandom():
+    u = random.random()
+    v = random.random()
+    w = math.sin(2 * math.pi * v) * pow(((-2) * math.log10(u)), 1 / 2)
+    return w
+
+
+def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level + 1, fn)
+        else:
+            if indent:
+                print('|', end='')
+                for tab_stop in range(level):
+                    print('-' * 4, end='', file=fn)
+            print(each_item, file=fn)
+
+
+# test
+a = [1, 2, [3, [4, [5]], [[6], 7, [8, 9]]]]
+print_lol(a, True)
