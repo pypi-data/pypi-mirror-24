e614f502d20bd5d2d78416ef823f7beb76579fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:28 +0000

    Add package ecopy/0.1.2.1/ecopy-0.1.2.1.tar.gz

---
+++ packages/ecopy/ecopy-0.1.2.1.tar.gz/ecopy-0.1.2.1/ecopy/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1.2.1'
+
+
+from .regression import *
+from .ordination import *
+from .diversity import *
+from .matrix_comp import *
+from .base_funcs import *
+from .utils import *
