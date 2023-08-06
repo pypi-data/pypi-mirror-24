c0118995a41dc20d23ad0cb8095e78645909ab4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package howto-pypi/0.0.4/howto-pypi-0.0.4.tar.gz

---
+++ packages/howto-pypi/howto-pypi-0.0.4.tar.gz/howto-pypi-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='howto-pypi',
+    version='0.0.4',
+    description='A template project about how to package and distribute \
+    projects',
+    url='https://github.com/yang-zhang/howto-pypi',
+    packages=find_packages(exclude=['contrib', 'tests'])
+
+)
