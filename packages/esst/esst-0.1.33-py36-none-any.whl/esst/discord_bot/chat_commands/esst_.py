ef01d3b36d04ad15a97575cddc071b2d68ffc10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:26 +0000

    Add package esst/0.1.33/esst-0.1.33-py36-none-any.whl

---
+++ packages/esst/esst-0.1.33-py36-none-any.whl/esst/discord_bot/chat_commands/esst_.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+"""
+Manages commands related to ESST itself
+"""
+
+from esst.core import CFG, __version__
+from esst.core.logger import log_file_path
+from esst.discord_bot.commands import DISCORD
+
+from .arg import arg
+
+
+def log():
+    """
+    Show ESST log file
+    """
+    DISCORD.send(log_file_path(CFG.saved_games_dir))
+
+
+def version():
+    """
+    Show ESST version
+    """
+    DISCORD.say(f'ESST v{__version__}')
+
+
+@arg(protected=True)
+def restart():
+    """
+    Restart ESST (protected)
+
+    """
+    DISCORD.say('This command is not yet implemented')
+
+
+NAMESPACE = '!esst'
+TITLE = 'Manage ESST application'
