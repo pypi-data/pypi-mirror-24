97a5f4ab931419c8cac3d36a84552ad8a207b3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:01 +0000

    Add package pyx12/2.3.3/pyx12-2.3.3.tar.gz

---
+++ packages/pyx12/pyx12-2.3.3.tar.gz/pyx12-2.3.3/pyx12/__init__.py
@@ -0,0 +1,9 @@
+"""
+Validate and transform HIPAA X12 documents
+"""
+
+__author__ = "John Holland <john.holland@swmbh.org>"
+__status__ = "production"
+from .version import __version__
+__date__ = "2016-05-21"
+__revision__ = ""
