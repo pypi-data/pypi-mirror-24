306e4519bad0c5dfed4e027e1bc04c400327c3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:22 +0000

    Add package jinbotron/0.1/jinbotron-0.1.tar.gz

---
+++ packages/jinbotron/jinbotron-0.1.tar.gz/jinbotron-0.1/jinbotron/__init__.py
@@ -0,0 +1,6 @@
+def do_job():
+    print 'Hello, I am Jinbotron. I am doing my job now.'
+
+def send_email():
+    print 'I am now sending emails.'
+
---
+++ packages/jinbotron/jinbotron-0.1.tar.gz/jinbotron-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jinbotron',
+    version='0.1',
+    description='The most interactive bot you got.',
+    url='http://www.facebook.com/YujinYuz',
+    author='yujinyuz',
+    author_email='jinyuzprodigy@gmail.com',
+    license='MIT',
+    packages=['jinbotron'],
+    zip_safe=False
+)
