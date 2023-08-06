dfd5f91f39e292ac7cfebd8082dc8e3c7c2fe180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:38 +0000

    Add package zof/0.1.1/zof-0.1.1.tar.gz

---
+++ packages/zof/zof-0.1.1.tar.gz/zof-0.1.1/CHANGELOG.rst
@@ -0,0 +1,13 @@
+CHANGELOG
+=========
+
+Version 0.1.1
+-------------
+
+- Fix bug in datapath service.
+
+
+Version 0.1.0
+-------------
+
+- Initial release.
---
+++ packages/zof/zof-0.1.1.tar.gz/zof-0.1.1/zof/api_run.py
@@ -0,0 +1,37 @@
+"""
+Environment Variables:
+
+    zof_DEBUG            Debug mode.
+"""
+
+import sys
+from .api_args import common_args
+from .controller import Controller
+from .logging import init_logging
+
+
+def run(*, args=None):
+    """Run event loop for zof.
+
+    Args:
+        args (Optional[argparse.Namespace]): Arguments derived from
+            ArgumentParser. If None, use `common_args` parser.
+    """
+    if args is None:
+        args = common_args(include_x_modules=True).parse_args()
+
+    if args.loglevel:
+        init_logging(args.loglevel, args.logfile)
+
+    if args.x_uvloop:
+        import uvloop  # pylint: disable=import-error
+        import asyncio
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+    controller = Controller.singleton()
+    exit_status = controller.run_loop(args=args)
+
+    if not args.x_under_test:
+        sys.exit(exit_status)
+
+    return exit_status
