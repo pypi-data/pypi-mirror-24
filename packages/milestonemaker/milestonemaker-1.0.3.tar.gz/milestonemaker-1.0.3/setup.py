3809ee371192c4922ce78708250426eadeb6ea3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:44 +0000

    Add package milestonemaker/1.0.3/milestonemaker-1.0.3.tar.gz

---
+++ packages/milestonemaker/milestonemaker-1.0.3.tar.gz/milestonemaker-1.0.3/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2015-2017 Lionheart Software LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import runpy
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+package_name = "milestonemaker"
+
+metadata_filename = "{}/metadata.py".format(package_name)
+metadata = runpy.run_path(metadata_filename)
+
+setup(
+    name=package_name,
+    version=metadata['__version__'],
+    license=metadata['__license__'],
+    description="Create milestones in GitHub automatically",
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
+    url="http://lionheartsw.com/",
+    packages=[package_name],
+    scripts=['bin/milestonemaker'],
+)
+
