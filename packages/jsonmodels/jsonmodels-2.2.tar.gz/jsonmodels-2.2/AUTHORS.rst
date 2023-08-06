0c5be5ab5a917b834eb3d0702cbab97949ccd00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:27 +0000

    Add package jsonmodels/2.2/jsonmodels-2.2.tar.gz

---
+++ packages/jsonmodels/jsonmodels-2.2.tar.gz/jsonmodels-2.2/AUTHORS.rst
@@ -0,0 +1,19 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Szczepan Cieślik <szczepan.cieslik@gmail.com>
+
+Contributors
+------------
+
+(In alphabetical order)
+
+* Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
+* Johannes Garimort <johannes.garimort@gmx.net>
+* Omer Anson <omer.anson@toganetworks.com>
+* Pavel Lipchak <kazemat92@gmail.com>
+* Vorren
---
+++ packages/jsonmodels/jsonmodels-2.2.tar.gz/jsonmodels-2.2/jsonmodels/models.py
@@ -0,0 +1,115 @@
+from . import parsers, errors
+from .fields import BaseField
+from .errors import ValidationError
+
+
+class _CacheKey(object):
+    pass
+
+
+class Base(object):
+
+    """Base class for all models."""
+
+    def __init__(self, **kwargs):
+        self._cache_key = _CacheKey()
+        self.populate(**kwargs)
+
+    def populate(self, **kw):
+        """Populate values to fields. Skip non-existing."""
+        for name, field in self:
+            if name in kw:
+                field.__set__(self, kw[name])
+
+    def get_field(self, field_name):
+        """Get field associated with given attribute."""
+        for attr_name, field in self:
+            if field_name == attr_name:
+                return field
+
+        raise errors.FieldNotFound('Field not found', field_name)
+
+    def __iter__(self):
+        """Iterate through fields and values."""
+        for name, field in self.iterate_over_fields():
+            yield name, field
+
+    def validate(self):
+        """Explicitly validate all the fields."""
+        for name, field in self:
+            try:
+                field.validate_for_object(self)
+            except ValidationError as error:
+                raise ValidationError(
+                    "Error for field '{name}'.".format(name=name),
+                    error,
+                )
+
+    @classmethod
+    def iterate_over_fields(cls):
+        """Iterate through fields and values."""
+        for attr in dir(cls):
+            clsattr = getattr(cls, attr)
+            if isinstance(clsattr, BaseField):
+                yield attr, clsattr
+
+    def to_struct(self):
+        """Cast model to Python structure."""
+        return parsers.to_struct(self)
+
+    @classmethod
+    def to_json_schema(cls):
+        """Generate JSON schema for model."""
+        return parsers.to_json_schema(cls)
+
+    def __repr__(self):
+        attrs = {}
+        for name, _ in self:
+            try:
+                attr = getattr(self, name)
+                if attr is not None:
+                    attrs[name] = repr(attr)
+            except ValidationError:
+                pass
+
+        return '{class_name}({fields})'.format(
+            class_name=self.__class__.__name__,
+            fields=', '.join(
+                '{0[0]}={0[1]}'.format(x) for x in sorted(attrs.items())
+            ),
+        )
+
+    def __str__(self):
+        return '{name} object'.format(name=self.__class__.__name__)
+
+    def __setattr__(self, name, value):
+        try:
+            return super(Base, self).__setattr__(name, value)
+        except ValidationError as error:
+            raise ValidationError(
+                "Error for field '{name}'.".format(name=name),
+                error
+            )
+
+    def __eq__(self, other):
+        if type(other) is not type(self):
+            return False
+
+        for name, _ in self.iterate_over_fields():
+            try:
+                our = getattr(self, name)
+            except errors.ValidationError:
+                our = None
+
+            try:
+                their = getattr(other, name)
+            except errors.ValidationError:
+                their = None
+
+            if our != their:
+                return False
+
+        return True
+
+    def __ne__(self, other):
+        return not (self == other)
