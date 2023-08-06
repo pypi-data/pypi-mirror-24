259b086c4ab1e6f60dc365f588800c17a0e47c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:31 +0000

    Add package eventregistry/6.5.1/eventregistry-6.5.1.tar.gz

---
+++ packages/eventregistry/eventregistry-6.5.1.tar.gz/eventregistry-6.5.1/setup.py
@@ -0,0 +1,27 @@
+﻿from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='eventregistry',
+      version='6.5.1',
+      description = "A package that can be used to query information in Event Registry (http://eventregistry.org/)",
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: General',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Intended Audience :: Information Technology'
+      ],
+      url='https://github.com/EventRegistry/event-registry-python',
+      author='Gregor Leban',
+      author_email='gregor@eventregistry.org',
+      license='MIT',
+      packages=['eventregistry'],
+      install_requires=[
+          'requests', 'six', 'pytz'
+      ],
+      zip_safe=False)
