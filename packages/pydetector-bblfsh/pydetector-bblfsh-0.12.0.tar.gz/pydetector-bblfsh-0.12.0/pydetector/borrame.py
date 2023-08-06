ef4ea37c8c33741fe4a05f3571aa47b3c1796f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:55 +0000

    Add package pydetector-bblfsh/0.12.0/pydetector-bblfsh-0.12.0.tar.gz

---
+++ packages/pydetector-bblfsh/pydetector-bblfsh-0.12.0.tar.gz/pydetector-bblfsh-0.12.0/pydetector/borrame.py
@@ -0,0 +1,4 @@
+class Cls:  # ClassDef, 1,3
+    @classmethod
+    def mymethod(cls):
+        cls.BBLFSH_SERVER_EXISTED = ensure_bblfsh_is_running()
---
+++ packages/pydetector-bblfsh/pydetector-bblfsh-0.12.0.tar.gz/pydetector-bblfsh-0.12.0/pydetector/borrame2.py
@@ -0,0 +1,10 @@
+class Repo2nBOW(Repo2Base):
+    @property
+    def id2vec(self):
+        return self._id2vec
+
+def empty():
+    pass
+
+def empty2(a, b, c):
+    pass
