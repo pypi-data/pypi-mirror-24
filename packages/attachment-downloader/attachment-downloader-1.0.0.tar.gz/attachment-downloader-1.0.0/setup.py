de26c25028616269f82fb8c364647b0f844ae04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:15 +0000

    Add package attachment-downloader/1.0.0/attachment-downloader-1.0.0.tar.gz

---
+++ packages/attachment-downloader/attachment-downloader-1.0.0.tar.gz/attachment-downloader-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='attachment-downloader',
+    version='1.0.0',
+    description='Downloads mail attachment',
+    author='James Ridgway',
+    url='https://github.com/jamesridgway/attachment-downloader',
+    license='MIT',
+    packages=['attachment_downloader'],
+    scripts=['bin/attachment-downloader']
+)
