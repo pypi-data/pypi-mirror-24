abfa8bf5237ed47fdd4935191b8de29d0fed9cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:20 +0000

    Add package onegov.notice/0.0.2/onegov.notice-0.0.2.tar.gz

---
+++ packages/onegov.notice/onegov.notice-0.0.2.tar.gz/onegov.notice-0.0.2/HISTORY.rst
@@ -0,0 +1,15 @@
+Changelog
+---------
+0.0.2 (2017-08-09)
+~~~~~~~~~~~~~~~~~~~
+
+- Allows to filter notices by a search term.
+  [msom]
+
+- Allows to filter notices by user IDs.
+  [msom]
+
+0.0.1 (2017-07-14)
+~~~~~~~~~~~~~~~~~~
+
+- Initial Release
---
+++ packages/onegov.notice/onegov.notice-0.0.2.tar.gz/onegov.notice-0.0.2/onegov/notice/upgrade.py
@@ -0,0 +1,19 @@
+from onegov.core.upgrade import upgrade_task
+from sqlalchemy import Column
+from sqlalchemy import Text
+from onegov.notice.models import OfficialNotice
+
+
+@upgrade_task('Adds a text column to the official notices')
+def add_text_column(context):
+    if not context.has_column('official_notices', 'text'):
+        # Create the new row
+        context.operations.add_column(
+            'official_notices',
+            Column('text', Text, nullable=True)
+        )
+
+        # Copy the data
+        for notice in context.session.query(OfficialNotice):
+            notice.text = notice.content.get('text', '')
+            notice.content = {}
