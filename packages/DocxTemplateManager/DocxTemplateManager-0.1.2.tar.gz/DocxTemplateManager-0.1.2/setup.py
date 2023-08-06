8d8dfdfbfb0d2d19d217293b29b89b4a692be97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:54 +0000

    Add package DocxTemplateManager/0.1.2/DocxTemplateManager-0.1.2.tar.gz

---
+++ packages/DocxTemplateManager/DocxTemplateManager-0.1.2.tar.gz/DocxTemplateManager-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'DocxTemplateManager',
+  packages = ['DocxTemplateManager'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'A library for managing templated Microsoft Word Documents with imports from Excel.',
+  author = 'Brian Carlson',
+  author_email = 'briancarlson6174@gmail.com',
+  url = 'https://github.com/TheRobotCarlson/DocxTemplateManager', # use the URL to the github repo
+  download_url = 'https://github.com/TheRobotCarlson/DocxTemplateManager/archive/0.1.tar.gz', # 
+  install_requires=['DocxMerge', 'docxtpl', 'openpyxl'],
+  keywords = ['docx', 'merge', 'word', 'excel'], # arbitrary keywords
+)
