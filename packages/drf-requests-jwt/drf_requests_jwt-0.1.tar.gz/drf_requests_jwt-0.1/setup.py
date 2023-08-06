a97f43bdac3358030eeb94ad9bb1fc79a455747b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:02 +0000

    Add package drf-requests-jwt/0.1/drf_requests_jwt-0.1.tar.gz

---
+++ packages/drf-requests-jwt/drf_requests_jwt-0.1.tar.gz/drf_requests_jwt-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='drf_requests_jwt',
+      version='0.1',
+      description='Django Rest Framework Requests with JWT support',
+      url='https://github.com/sensidev/drf-requests-jwt',
+      author='Sensidev',
+      author_email='lucian.corduneanu@sensidev.com',
+      license='MIT',
+      packages=['drf_requests_jwt'],
+      zip_safe=False)
