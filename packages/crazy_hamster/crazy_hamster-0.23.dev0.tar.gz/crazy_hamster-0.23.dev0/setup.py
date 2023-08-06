5fea0e534d7e30b3563b90b0d57d90b0f64074cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:22 +0000

    Add package crazy_hamster/0.23.dev0/crazy_hamster-0.23.dev0.tar.gz

---
+++ packages/crazy_hamster/crazy_hamster-0.23.dev0.tar.gz/crazy_hamster-0.23.dev0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(name='crazy_hamster',
+                 version='0.23.dev0',
+                 description='The craziest hamster in the world',
+                 url='https://github.com/ehsangolshani/crazy-hamster',
+                 author='Ehsan Golshani',
+                 author_email='ehsanroman74@gmail.com',
+                 license='GNU',
+                 install_requires=[
+                     'aiohttp',
+                     'asyncio',
+                 ],
+                 packages=['crazy_hamster'],
+                 zip_safe=False)
