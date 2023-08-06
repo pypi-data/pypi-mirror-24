9ba55a48685926998bdc1eee18d2e60fbb4efeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:19 +0000

    Add package nyaapy/0.1/nyaapy-0.1.tar.gz

---
+++ packages/nyaapy/nyaapy-0.1.tar.gz/nyaapy-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='nyaapy',
+      version='0.1',
+      url='https://github.com/juanjosalvador/nyaapy',
+      download_url = 'https://github.com/juanjosalvador/nyaapy/archive/0.1.tar.gz',
+      license='MIT',
+      author='Juanjo Salvador',
+      author_email='juanjosalvador@netc.eu',
+      description='Allows you to make requests on Nyaa.si and nyaa.pantsu.cat',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
