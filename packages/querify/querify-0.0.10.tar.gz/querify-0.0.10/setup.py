8f01be5dbebe7cc7725e7ebc7f4d9f85e2ab4ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:18 +0000

    Add package querify/0.0.10/querify-0.0.10.tar.gz

---
+++ packages/querify/querify-0.0.10.tar.gz/querify-0.0.10/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+
+VERSION = '0.0.10'
+
+
+setup(
+    name='querify',
+    packages=['querify'],
+    version=VERSION,
+    description='InfluxQL / MySQL / Mongo query generator from json-represented filters (mongodb-like query objects)',
+    author='Raychee Zhang',
+    author_email='raychee.zhang@gmail.com',
+    url='https://github.com/Raychee/querify',
+    download_url='https://github.com/Raychee/querify/tarball/' + VERSION,
+    keywords=['json', 'mongo', 'influx', 'mysql'],
+    install_requires=[
+        'qutils>=0.3.0'
+    ]
+)
