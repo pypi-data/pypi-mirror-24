a3299d0eb9830cdea90d862fb98c4c2b6a997c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package abelian/0.0.1/abelian-0.0.1.tar.gz

---
+++ packages/abelian/abelian-0.0.1.tar.gz/abelian-0.0.1/abelian/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pkg_resources
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except:
+    __version__ = '0.0.1'
+
+
+from abelian.linalg.solvers import solve
+from abelian.linalg.factorizations import smith_normal_form, hermite_normal_form
+from abelian.groups import LCA
+from abelian.morphisms import HomFGA, HomLCA, Homomorphism
+
+
+__all__ = []
