2a8d9c06c7ec63774a0c69fb791ce15a046891df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:15 +0000

    Add package scrapy-amazon-robot-middleware-jondot/0.2.4/scrapy-amazon-robot-middleware-jondot-0.2.4.tar.gz

---
+++ packages/scrapy-amazon-robot-middleware-jondot/scrapy-amazon-robot-middleware-jondot-0.2.4.tar.gz/scrapy-amazon-robot-middleware-jondot-0.2.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version = '0.2.4'
+
+REQUIREMENTS = ['beautifulsoup4', 'scrapy', 'pillow==4.0.0', 'requests']
+
+setup(
+    name='scrapy-amazon-robot-middleware-jondot',
+    version=version,
+    packages=find_packages(),
+    package_data={'captchabuster': ['iconset/**/*.gif']},
+    url='https://github.com/leon-wu/scrapy-amazon-robot-middleware',
+    license='LICENSE.txt',
+    author='Mark Sanders',
+    author_email='xmwlzhi@gmail.com',
+    install_requires=REQUIREMENTS,
+    description='Scrapy middleware module which uses image parsing to submit a captcha response to amazon.',
+    include_package_data=True)
