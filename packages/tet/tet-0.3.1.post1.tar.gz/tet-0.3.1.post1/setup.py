6b5311caa4322fc5e787d3b4a84d5acb8ca2d39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:42 +0000

    Add package tet/0.3.1.post1/tet-0.3.1.post1.tar.gz

---
+++ packages/tet/tet-0.3.1.post1.tar.gz/tet-0.3.1.post1/setup.py
@@ -0,0 +1,52 @@
+import os, sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = """
+    pyramid>=1.9
+    passlib
+    six
+    sqlalchemy
+    pyramid_services
+""".split()
+
+if sys.version_info < (3, 6):
+    requires.append('backports.typing>=1.1,<1.2')
+
+
+setup(name='tet',
+      version='0.3.1post1',
+      description='Unearthly intelligent batteries-included application framework built on Pyramid',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Framework :: Pyramid",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Python Software Foundation License",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3 :: Only",
+          "Topic :: Internet :: WWW/HTTP :: WSGI",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+          "Topic :: Software Development :: Libraries :: Application Frameworks"
+      ],
+      author='Antti Haapala',
+      author_email='antti.haapala@anttipatterns.com',
+      url='http://www.anttipatterns.com',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='tet',
+      entry_points = """\
+        [pyramid.scaffold]
+        basictet=tet.scaffolds:BasicTetTemplate
+      """,
+      install_requires=requires,
+)
