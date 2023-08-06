47785cc8d23bfc90cd226202d171e52f00587f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:27 +0000

    Add package soxs/2.0.0/soxs-2.0.0-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/soxs/soxs-2.0.0-cp27-cp27m-manylinux1_x86_64.whl/soxs/__init__.py
@@ -0,0 +1,58 @@
+__version__ = "2.0.0"
+
+from soxs.utils import soxs_cfg
+
+from soxs.simput import \
+    write_photon_list, \
+    read_simput_catalog, \
+    PhotonList, SimputCatalog
+
+from soxs.spectra import \
+    Spectrum, \
+    ApecGenerator, \
+    ConvolvedSpectrum
+
+from soxs.instrument import \
+    instrument_simulator, \
+    AuxiliaryResponseFile, \
+    RedistributionMatrixFile, \
+    make_background_file, \
+    FlatResponse, simulate_spectrum
+
+from soxs.instrument_registry import \
+    add_instrument_to_registry, \
+    show_instrument_registry, \
+    write_instrument_json, \
+    get_instrument_from_registry, \
+    instrument_registry
+
+from soxs.spatial import \
+    PointSourceModel, \
+    RadialFunctionModel, \
+    RadialArrayModel, \
+    RadialFileModel, \
+    AnnulusModel, \
+    BetaModel, \
+    FillFOVModel, \
+    RectangleModel, \
+    SpatialModel
+
+from soxs.events import \
+    write_spectrum, \
+    write_image, \
+    write_radial_profile, \
+    plot_spectrum, \
+    make_exposure_map
+
+from soxs.background import \
+    add_instrumental_background, \
+    BackgroundSpectrum, \
+    ConvolvedBackgroundSpectrum, \
+    InstrumentalBackgroundSpectrum
+
+from soxs.cosmology import \
+    make_cosmological_sources_file
+
+from soxs.background import \
+    make_point_sources_file, \
+    make_point_source_list
