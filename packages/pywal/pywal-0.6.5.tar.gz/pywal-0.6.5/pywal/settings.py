8f21e28f34adbd7046a3e8c1fc9a2405bec63f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:14 +0000

    Add package pywal/0.6.5/pywal-0.6.5.tar.gz

---
+++ packages/pywal/pywal-0.6.5.tar.gz/pywal-0.6.5/pywal/settings.py
@@ -0,0 +1,23 @@
+"""
+                                      '||
+... ...  .... ... ... ... ...  ....    ||
+ ||'  ||  '|.  |   ||  ||  |  '' .||   ||
+ ||    |   '|.|     ||| |||   .|' ||   ||
+ ||...'     '|       |   |    '|..'|' .||.
+ ||      .. |
+''''      ''
+Created by Dylan Araps.
+"""
+
+import os
+import platform
+
+
+__version__ = "0.6.5"
+
+
+HOME = os.environ["HOME"]
+CACHE_DIR = os.path.join(HOME, ".cache/wal/")
+MODULE_DIR = os.path.dirname(__file__)
+COLOR_COUNT = 16
+OS = platform.uname()[0]
