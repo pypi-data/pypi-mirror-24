39fccbc64fc76375dc008ec4a65d1c0cc1de1b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:15 +0000

    Add package portfoliomanager/0.1.1/portfoliomanager-0.1.1.tar.gz

---
+++ packages/portfoliomanager/portfoliomanager-0.1.1.tar.gz/portfoliomanager-0.1.1/README.rst
@@ -0,0 +1,51 @@
+Portfolio Manager
+===================
+
+Tracks N number of investments equally weighted against IVV etf.
+
+Installation
+------------
+
+To install Portfolio Manager from PyPI:
+
+.. code-block:: bash
+
+  $ pip install portfoliomanager 
+
+From git repo:
+
+.. code-block:: bash
+
+  $ git clone https://github.com/JamesWhiteleyIV/Portfolio-Manager.git
+  $ cd Portfolio-Manager 
+  $ python setup.py install
+
+
+Documentation
+-------------
+
+.. code:: python
+  
+  import datetime as dt
+  import portfoliomanager as pm
+
+  #initialize start and end dates of portfolio 1/1/2017 - current date
+  start = dt.date(2017, 1, 1)
+  end = dt.date.today()
+
+  #create list of investments and name of portfolio
+  tickers = ['OSUR', 'COHR', 'ENTG', 'HSKA', 'FIVE', 'MDSO', 'LOPE', 'MBUU', 'OLLI', 'TRU']
+  name = "Stock Portfolio A"
+
+  #initialize Portfolio object
+  port = pm.Portfolio(tickers, start, end, name)
+
+  #plot portfolio vs. IVV (S&P 500 etf) as a PDF in current directory
+  port.pltPort()
+
+
+Authors
+-------
+
+**James Whiteley IV** 
+
---
+++ packages/portfoliomanager/portfoliomanager-0.1.1.tar.gz/portfoliomanager-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='portfoliomanager',
+        version='0.1.1',
+        description='Tracks N number of investments equally weighted against IVV etf.',
+        url='https://github.com/JamesWhiteleyIV/Portfolio-Manager',
+        author='James Whiteley IV',
+        author_email='jameswhiteleyiv@gmail.com',
+        license='MIT',
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            ],
+        keywords='investment utility pandas portfolio management',
+        packages=find_packages(exclude=['docs', 'tests*']),
+        install_requires=['investmentutility'] #these get installed before this package
+        )
