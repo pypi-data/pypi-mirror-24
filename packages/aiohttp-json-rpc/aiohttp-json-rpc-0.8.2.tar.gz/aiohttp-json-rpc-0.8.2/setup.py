8d6d44268f705aae2bcc30c4a70a555e538a55d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:54 +0000

    Add package aiohttp-json-rpc/0.8.2/aiohttp-json-rpc-0.8.2.tar.gz

---
+++ packages/aiohttp-json-rpc/aiohttp-json-rpc-0.8.2.tar.gz/aiohttp-json-rpc-0.8.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='aiohttp-json-rpc',
+      version='0.8.2',
+      author='Florian Scherf',
+      url='https://github.com/pengutronix/aiohttp-json-rpc/',
+      author_email='f.scherf@pengutronix.de',
+      license='Apache 2.0',
+      install_requires=['aiohttp>=2,<2.1'],
+      packages=find_packages(),
+      zip_safe=False,
+      entry_points={
+          'pytest11': [
+              'aiohttp-json-rpc = aiohttp_json_rpc.pytest',
+          ]
+      },
+      classifiers=[
+          'Framework :: Pytest',
+      ])
