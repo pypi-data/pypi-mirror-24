ee05b4a03b172c1fe3c89a82a0bd661959433969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:40 +0000

    Add package track-grabber/0.2/track_grabber-0.2.tar.gz

---
+++ packages/track-grabber/track_grabber-0.2.tar.gz/track_grabber-0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="track_grabber",
+      version="0.2",
+      description="Track grabber finds the most popular songs of a given artists and generates an html page with youtube links to those songs.",
+      author="Joseph Jones",
+      py_modules=["track_grabber"]
+     )
