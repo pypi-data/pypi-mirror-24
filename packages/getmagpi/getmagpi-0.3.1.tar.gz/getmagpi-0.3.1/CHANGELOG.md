a1ca9b0d908b0f815699b1f145cbbade57b4626b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:21 +0000

    Add package getmagpi/0.3.1/getmagpi-0.3.1.tar.gz

---
+++ packages/getmagpi/getmagpi-0.3.1.tar.gz/getmagpi-0.3.1/CHANGELOG.md
@@ -0,0 +1,44 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+- .magignore file
+- Logging
+
+## [0.3.1] - 2017-09-04
+### Fixed
+- setup.py and packaging 
+
+## [0.3.0] - 2017-09-03
+### Changed
+- Switched to BeautifulSoup for HTML parsing.  (Original experiment was to be Standard Library-only.  Troublesome across Python versions.)
+
+## [0.2.2] - 2017-08-10
+### Fixed
+- Fix packaging bug
+
+## [0.2.1] - 2017-08-06
+### Added
+- restructured text README for PyPi
+
+## [0.2.0] - 2017-08-06
+### Changed
+- Moved Standard Library URL handling to 'requests'
+- Refactors to support common Python 2/3
+
+### Added
+- setup.py for local install and pypi
+
+## [0.1.0] - 2017-08-05
+### Added
+- HTML parser for remote file listing.
+- Filesystem handler for local PDF library.
+- Handler for deltas between Source and Target librarys ('--compare').
+- Switch for non-English files (uses file description terms).
+- Standard-Library file downloader.
+- Create README.
+- Add LICENSE.
+- Create this CHANGELOG.
---
+++ packages/getmagpi/getmagpi-0.3.1.tar.gz/getmagpi-0.3.1/setup.py
@@ -0,0 +1,108 @@
+#! /usr/bin/python
+# coding: utf-8
+
+import io
+import sys
+from setuptools import setup, find_packages, Command
+from codecs import open
+from shutil import rmtree
+import os
+
+
+# General Attributes and Requirements
+NAME = 'getmagpi'
+DESCRIPTION = 'A simple utility to synchronize free MagPi PDF content.'
+URL = 'https://jnario.github.io/getmagpi/'
+EMAIL = 'jose@nario.com',
+AUTHOR = 'Jose Nario',
+REQUIRED = ['requests', 'beautifulsoup4']
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Long Description -- remember to add readme to MANIFEST.IN
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Version
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+
+    # Packages (one or the other)
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    py_modules=['getmagpi'],
+
+    # entry_points={
+    #     'console_scripts': [
+    #         'getmagpi = getmagpi:main'
+    #     ],
+    # },
+    install_requires=REQUIRED,
+    include_package_date=True,
+    license='MIT',
+    keywords='magpi raspberrypi library',
+    classifiers=[
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: System :: Archiving :: Mirroring',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand,
+    },
+)
+
+
