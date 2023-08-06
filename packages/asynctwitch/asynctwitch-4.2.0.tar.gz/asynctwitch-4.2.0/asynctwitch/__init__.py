124833354d8cfbc461a6ad7c0a6ffc40af2b8370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:52 +0000

    Add package asynctwitch/4.2.0/asynctwitch-4.2.0.tar.gz

---
+++ packages/asynctwitch/asynctwitch-4.2.0.tar.gz/asynctwitch-4.2.0/asynctwitch/__init__.py
@@ -0,0 +1,6 @@
+from .dataclasses import Badge, Emote, Color,\
+    Command, SubCommand, Message, User, Song, Object
+from .bots import Bot, CommandBot, CurrencyBot,\
+    ViewTimeBot, RankedBot
+
+Colour = Color
---
+++ packages/asynctwitch/asynctwitch-4.2.0.tar.gz/asynctwitch-4.2.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='asynctwitch',
+
+    version='4.2.0',
+
+    description='Asynchonous wrapper for twitch IRC3',
+    long_description=long_description,
+
+    url='https://github.com/martmists/asynctwitch',
+
+    author='martmists',
+    author_email='martmists@gmail.com',
+
+    license='BSD-3-Clause',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Chat',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=[
+        'aiohttp',
+        'isodate'
+    ],
+    packages=find_packages(),
+    keywords='asyncio twitch irc',
+)
