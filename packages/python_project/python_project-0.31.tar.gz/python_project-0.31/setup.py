1d3d5703200f9f8ed9977faca28beb7b875a0ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:36 +0000

    Add package python_project/0.31/python_project-0.31.tar.gz

---
+++ packages/python_project/python_project-0.31.tar.gz/python_project-0.31/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+        #long_description='A great pip packaging example',
+
+setup(name='python_project',
+      version='0.31',
+      description='Python Project Template',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='http://github.com/rehmanz/python_project',
+      author='Zile Rehman',
+      author_email='rehmanz@yahoo.com',
+      license='MIT',
+      packages=['python_project'],
+      install_requires=[
+          'markdown',
+      ],
+      include_package_data=True,
+      zip_safe=False)
