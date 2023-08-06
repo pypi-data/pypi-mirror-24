c3a66986ce0afa36ed20c29afbeda58ee368134c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:23 +0000

    Add package grokcore.catalog/2.3/grokcore.catalog-2.3.tar.gz

---
+++ packages/grokcore.catalog/grokcore.catalog-2.3.tar.gz/grokcore.catalog-2.3/src/grokcore/catalog/ftests/test_grok_functional.py
@@ -0,0 +1,42 @@
+import doctest
+import unittest
+from pkg_resources import resource_listdir
+
+from zope.app.appsetup.testlayer import ZODBLayer
+import grokcore.catalog
+
+
+FunctionalLayer = ZODBLayer(grokcore.catalog)
+
+
+def suiteFromPackage(name):
+    files = resource_listdir(__name__, name)
+    suite = unittest.TestSuite()
+    for filename in files:
+        if not filename.endswith('.py'):
+            continue
+        if filename == '__init__.py':
+            continue
+
+        dottedname = 'grokcore.catalog.ftests.%s.%s' % (name, filename[:-3])
+        test = doctest.DocTestSuite(
+            dottedname,
+            extraglobs=dict(getRootFolder=FunctionalLayer.getRootFolder),
+            optionflags=(doctest.ELLIPSIS +
+                         doctest.NORMALIZE_WHITESPACE +
+                         doctest.REPORT_NDIFF))
+        test.layer = FunctionalLayer
+
+        suite.addTest(test)
+    return suite
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    for name in ["catalog"]:
+        suite.addTest(suiteFromPackage(name))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
