e464a8237d156d424eea37dfc2f48370260174d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:22 +0000

    Add package inoon-lora-packet/0.4.2/inoon_lora_packet-0.4.2.tar.gz

---
+++ packages/inoon-lora-packet/inoon_lora_packet-0.4.2.tar.gz/inoon_lora_packet-0.4.2/inoon_lora_packet/ack.py
@@ -0,0 +1,32 @@
+from enum import IntEnum
+
+from inoon_lora_packet.packet import Packet, HexConverter
+
+
+class AckType(IntEnum):
+    ext_mgmt = 0x00,
+    dev_reset = 0x80,
+    per_change = 0x81,
+    immediate = 0x82
+
+
+class AckV2Packet(Packet):
+    def _field_spec(self):
+        return [
+            {'name': 'ack_type',
+             'bytes': 1,
+             'convert': HexConverter.hex_to_uint,
+             'restrict': [AckType[k] for k in AckType._member_map_]},
+
+            {'name': 'rssi',
+             'bytes': 1,
+             'convert': HexConverter.hex_to_int8,
+             'restrict': None},
+        ]
+
+    @classmethod
+    def encode(cls, ack_type, rssi):
+        enc = ''
+        enc += format(ack_type, '02x')
+        enc += HexConverter.int_to_hex(rssi, 1, True)
+        return enc
