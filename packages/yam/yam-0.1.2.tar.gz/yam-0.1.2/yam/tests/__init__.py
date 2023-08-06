d29a2e9172731e65d3c491a27b509a93c35f3913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package yam/0.1.2/yam-0.1.2.tar.gz

---
+++ packages/yam/yam-0.1.2.tar.gz/yam-0.1.2/yam/tests/__init__.py
@@ -0,0 +1,26 @@
+"""
+Tests for the rf package.
+
+yam-runtests [-h] [-v] [-p] [-d]
+
+-h    short help
+-v    be verbose
+-p    use permanent tempdir
+-d    empty permanent tempdir at start
+"""
+
+from pkg_resources import resource_filename
+import sys
+import unittest
+
+
+def run():
+    if '-h' in sys.argv[1:]:
+        print(__doc__)
+        return
+    loader = unittest.TestLoader()
+    test_dir = resource_filename('yam', 'tests')
+    suite = loader.discover(test_dir)
+    runner = unittest.runner.TextTestRunner()
+    ret = not runner.run(suite).wasSuccessful()
+    sys.exit(ret)
