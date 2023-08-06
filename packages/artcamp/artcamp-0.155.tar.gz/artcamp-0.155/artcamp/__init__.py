b604034512644e84349dc6c9e3eacd65dceca444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:11 +0000

    Add package artcamp/0.155/artcamp-0.155.tar.gz

---
+++ packages/artcamp/artcamp-0.155.tar.gz/artcamp-0.155/artcamp/__init__.py
@@ -0,0 +1,10 @@
+from .code.lsi import GensimLsi
+from .cleaner import TextProcessor
+from .vectorizers import GensimTfidf
+from .model import OrgSim
+
+
+__all__ = ['GensimLsi',
+           'TextProcessor',
+           'GensimTfidf',
+           'OrgSim']
