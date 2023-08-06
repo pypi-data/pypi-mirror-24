276fd0cd672dc999d4f1679e4d82b4a5358b3904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:36 +0000

    Add package aek/0.0.1/aek-0.0.1.tar.gz

---
+++ packages/aek/aek-0.0.1.tar.gz/aek-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='aek',
+    version='0.0.1',
+    url='https://github.com/RitterHou/aek',
+    author='derobukal',
+    author_email='derobukal@gmail.com',
+    license='GPL3',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
