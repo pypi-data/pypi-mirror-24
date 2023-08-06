4857280e47ad0b411220f0a3c25ebea933cb3bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:57 +0000

    Add package gecosistema_lite/0.0.47/gecosistema_lite-0.0.47.zip

---
+++ packages/gecosistema_lite/gecosistema_lite-0.0.47.zip/gecosistema_lite-0.0.47/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='gecosistema_lite',
+      version='0.0.47',
+      description='A simple python package',
+      author='Valerio Luzzi',
+      author_email='valerio.luzzi@gecosistema.it',
+      url='https://github.com/valluzzi/libcore/',
+      license='MIT',
+      packages=['gecosistema_lite'],
+      install_requires=['xlrd', 'xlwt', 'xlutils'],
+      python_requires='>=2.7'
+      )
