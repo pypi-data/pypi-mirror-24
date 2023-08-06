4f884facfddfd7123de7daabdac4bb186157c5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package NeXpy/0.10.2/NeXpy-0.10.2.tar.gz

---
+++ packages/NeXpy/NeXpy-0.10.2.tar.gz/NeXpy-0.10.2/src/nexpy/requires.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+'''package requirements are checked at runtime and installation time'''
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2016, NeXpy Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING, distributed with this software.
+#-----------------------------------------------------------------------------
+
+pkg_requirements = [
+    'nexusformat>=0.4.9',
+    'numpy>=1.6.0',
+    'scipy',
+    'h5py',
+    'jupyter',
+    'ipython>=4.0.0',
+    'matplotlib>=1.4.0',
+]
+extra_requirements = {
+    'spec': ['spec2nexus>=2017.901.4',],
+}
