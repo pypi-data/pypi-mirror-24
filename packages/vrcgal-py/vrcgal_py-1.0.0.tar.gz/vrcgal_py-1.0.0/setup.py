f7f734600b5e5a3b15a53491c08fbf5af9b57ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:18 +0000

    Add package vrcgal-py/1.0.0/vrcgal_py-1.0.0.tar.gz

---
+++ packages/vrcgal-py/vrcgal_py-1.0.0.tar.gz/vrcgal_py-1.0.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Virtual Reality and Clinical Gait Analysis Laboratory
+
+from distutils.core import setup
+
+
+setup(
+    name='vrcgal_py',
+    version='1.0.0',
+    packages=['vrcgal_py'],
+    license='MIT',
+    url='https://github.com/vrcgal/vrcgal_py',
+    author='Ben Long',
+    author_email="14benj@gmail.com",
+    long_description=open('README').read()
+)
