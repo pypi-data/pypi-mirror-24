e48bec947ed2481420b6ea3ce8577615d4b24d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:06 +0000

    Add package netjsonconfig/0.6.2/netjsonconfig-0.6.2.tar.gz

---
+++ packages/netjsonconfig/netjsonconfig-0.6.2.tar.gz/netjsonconfig-0.6.2/tests/openwrt/test_backend.py
@@ -0,0 +1,410 @@
+import json
+import os
+import tarfile
+import unittest
+from hashlib import md5
+from time import sleep
+
+from netjsonconfig import OpenWrt
+from netjsonconfig.exceptions import ValidationError
+from netjsonconfig.utils import _TabsMixin
+
+
+class TestBackend(unittest.TestCase, _TabsMixin):
+    maxDiff = None
+
+    def test_config_copy(self):
+        config = {'interfaces': []}
+        o = OpenWrt(config)
+        o.validate()
+        self.assertDictEqual(config, {'interfaces': []})
+
+    def test_json_method(self):
+        config = {
+            "type": "DeviceConfiguration",
+            "interfaces": [
+                {
+                    "name": "lo",
+                    "type": "loopback",
+                    "addresses": [
+                        {
+                            "address": "127.0.0.1",
+                            "mask": 8,
+                            "proto": "static",
+                            "family": "ipv4"
+                        }
+                    ]
+                }
+            ]
+        }
+        o = OpenWrt(config)
+        self.assertEqual(json.loads(o.json()), config)
+
+    def test_string_argument(self):
+        OpenWrt('{}')
+
+    def test_validate(self):
+        o = OpenWrt({'interfaces': 'WRONG'})
+        with self.assertRaises(ValidationError):
+            o.validate()
+
+        o = OpenWrt({'interfaces': []})
+        o.validate()
+
+        o.config['interfaces'] = 'CHANGED'
+        try:
+            o.validate()
+        except ValidationError as e:
+            self.assertEqual(e.details.instance, 'CHANGED')
+            self.assertIn("ValidationError 'CHANGED' is not of type 'array'", str(e))
+        else:
+            self.fail('ValidationError not raised')
+
+    def test_find_bridge_skip_error(self):
+        o = OpenWrt({'interfaces': ['WRONG']})
+        with self.assertRaises(ValidationError):
+            o.validate()
+
+    def test_type_error(self):
+        with self.assertRaises(TypeError):
+            OpenWrt([])
+        with self.assertRaises(TypeError):
+            OpenWrt('NOTJSON[]\{\}')
+
+    def test_system_invalid_timezone(self):
+        o = OpenWrt({
+            "general": {
+                "hostname": "test_system",
+                "timezone": "WRONG",
+            }
+        })
+        with self.assertRaises(ValidationError):
+            o.validate()
+
+    def test_schema_radio_wrong_driver(self):
+        o = OpenWrt({
+            "radios": [
+                {
+                    "name": "radio0",
+                    "phy": "phy0",
+                    "driver": "iamwrong",
+                    "protocol": "802.11ac",
+                    "channel": 132,
+                    "channel_width": 80,
+                    "tx_power": 8
+                }
+            ]
+        })
+        with self.assertRaises(ValidationError):
+            o.validate()
+
+    def test_schema_radio_wrong_protocol(self):
+        o = OpenWrt({
+            "radios": [
+                {
+                    "name": "radio0",
+                    "phy": "phy0",
+                    "driver": "mac80211",
+                    "protocol": "802.11ad",  # ad is not supported by OpenWRT yet
+                    "channel": 132,
+                    "channel_width": 80,
+                    "tx_power": 8
+                }
+            ]
+        })
+        with self.assertRaises(ValidationError):
+            o.validate()
+
+    _config1 = {
+        "interfaces": [
+            {
+                "name": "wlan0",
+                "type": "wireless",
+                "addresses": [
+                    {
+                        "address": "192.168.1.1",
+                        "mask": 24,
+                        "proto": "static",
+                        "family": "ipv4"
+                    }
+                ],
+                "wireless": {
+                    "radio": "radio0",
+                    "mode": "access_point",
+                    "ssid": "MyWifiAP",
+                    "hidden": True
+                }
+            }
+        ],
+        "radios": [
+            {
+                "name": "radio0",
+                "phy": "phy0",
+                "driver": "mac80211",
+                "protocol": "802.11n",
+                "channel": 3,
+                "channel_width": 20,
+                "tx_power": 3
+            }
+        ]
+    }
+
+    def test_generate(self):
+        o = OpenWrt(self._config1)
+        tar = tarfile.open(fileobj=o.generate(), mode='r')
+        self.assertEqual(len(tar.getmembers()), 2)
+        # network
+        network = tar.getmember('etc/config/network')
+        contents = tar.extractfile(network).read().decode()
+        expected = self._tabs("""config interface 'wlan0'
+    option ifname 'wlan0'
+    option ipaddr '192.168.1.1'
+    option netmask '255.255.255.0'
+    option proto 'static'
+
+""")
+        self.assertEqual(contents, expected)
+        # wireless
+        wireless = tar.getmember('etc/config/wireless')
+        contents = tar.extractfile(wireless).read().decode()
+        expected = self._tabs("""config wifi-device 'radio0'
+    option channel '3'
+    option htmode 'HT20'
+    option hwmode '11g'
+    option phy 'phy0'
+    option txpower '3'
+    option type 'mac80211'
+
+config wifi-iface 'wifi_wlan0'
+    option device 'radio0'
+    option hidden '1'
+    option ifname 'wlan0'
+    option mode 'ap'
+    option network 'wlan0'
+    option ssid 'MyWifiAP'
+""")
+        self.assertEqual(contents, expected)
+        tar.close()
+
+    def test_double_rendering(self):
+        o = OpenWrt(self._config1)
+        self.assertEqual(o.render(), o.render())
+
+    def test_write(self):
+        o = OpenWrt({
+            "general": {
+                "hostname": "test"
+            }
+        })
+        o.write(name='test', path='/tmp')
+        tar = tarfile.open('/tmp/test.tar.gz', mode='r')
+        self.assertEqual(len(tar.getmembers()), 1)
+        tar.close()
+        os.remove('/tmp/test.tar.gz')
+
+    def test_templates_type_error(self):
+        config = {
+            "general": {
+                "hostname": "test_templates",
+            }
+        }
+        with self.assertRaises(TypeError):
+            OpenWrt(config, templates={'a': 'a'})
+
+    def test_templates_config_error(self):
+        config = {
+            "general": {
+                "hostname": "test_templates",
+            }
+        }
+        with self.assertRaises(TypeError):
+            OpenWrt(config, templates=['O{]O'])
+
+    def test_templates(self):
+        loopback_template = {
+            "interfaces": [
+                {
+                    "name": "lo",
+                    "type": "loopback",
+                    "addresses": [
+                        {
+                            "address": "127.0.0.1",
+                            "mask": 8,
+                            "proto": "static",
+                            "family": "ipv4"
+                        }
+                    ]
+                }
+            ]
+        }
+        radio_template = {
+            "interfaces": [
+                {
+                    "name": "wlan0",
+                    "type": "wireless",
+                    "addresses": [
+                        {
+                            "address": "192.168.1.1",
+                            "mask": 24,
+                            "proto": "static",
+                            "family": "ipv4"
+                        }
+                    ],
+                    "wireless": {
+                        "radio": "radio0",
+                        "mode": "access_point",
+                        "ssid": "MyWifiAP",
+                        "hidden": True
+                    }
+                }
+            ],
+            "radios": [
+                {
+                    "name": "radio0",
+                    "phy": "phy0",
+                    "driver": "mac80211",
+                    "protocol": "802.11n",
+                    "channel": 3,
+                    "channel_width": 20,
+                    "tx_power": 3
+                }
+            ]
+        }
+        config = {
+            "general": {
+                "hostname": "test_templates",
+            }
+        }
+        o = OpenWrt(config, templates=[loopback_template, radio_template])
+        self.assertEqual(o.config['general']['hostname'], 'test_templates')
+        self.assertIn('radios', o.config)
+        self.assertEqual(len(o.config['radios']), 1)
+        self.assertEqual(o.config['radios'][0]['name'], 'radio0')
+        self.assertIn('interfaces', o.config)
+        self.assertEqual(len(o.config['interfaces']), 2)
+        self.assertEqual(o.config['interfaces'][0]['name'], 'lo')
+        self.assertEqual(o.config['interfaces'][1]['name'], 'wlan0')
+
+    def test_file_inclusion(self):
+        o = OpenWrt({
+            "files": [
+                {
+                    "path": "/etc/crontabs/root",
+                    "mode": "0644",
+                    "contents": '* * * * * echo "test" > /etc/testfile\n'
+                                '* * * * * echo "test2" > /etc/testfile2'
+                },
+                {
+                    "path": "/etc/dummy.conf",
+                    "mode": "0644",
+                    "contents": "testing!"
+                }
+            ]
+        })
+        output = o.render()
+        self.assertNotIn('package files', output)
+        self.assertIn('* * * * * echo', output)
+        # ensure the additional files are there present in the tar.gz archive
+        tar = tarfile.open(fileobj=o.generate(), mode='r')
+        self.assertEqual(len(tar.getmembers()), 2)
+        # first file
+        crontab = tar.getmember('etc/crontabs/root')
+        contents = tar.extractfile(crontab).read().decode()
+        self.assertEqual(contents, o.config['files'][0]['contents'])
+        self.assertEqual(crontab.mtime, 0)
+        self.assertEqual(crontab.mode, 420)
+        # second file
+        dummy = tar.getmember('etc/dummy.conf')
+        contents = tar.extractfile(dummy).read().decode()
+        self.assertEqual(contents, o.config['files'][1]['contents'])
+        self.assertEqual(dummy.mode, 420)
+        tar.close()
+
+    def test_file_permissions(self):
+        o = OpenWrt({
+            "files": [
+                {
+                    "path": "/tmp/hello.sh",
+                    "mode": "0755",
+                    "contents": "echo 'hello world'",
+                }
+            ]
+        })
+        tar = tarfile.open(fileobj=o.generate(), mode='r')
+        script = tar.getmember('tmp/hello.sh')
+        # check permissions
+        self.assertEqual(script.mode, 493)
+        tar.close()
+
+    def test_file_schema(self):
+        c = {
+            "files": [
+                {
+                    "path": "/tmp/hello.sh",
+                    "mode": "0644",
+                    "contents": "echo 'hello world'"
+                }
+            ]
+        }
+        # valid
+        c['files'][0]['mode'] = '3555'
+        o = OpenWrt(c)
+        o.validate()
+        # valid
+        c['files'][0]['mode'] = '755'
+        o = OpenWrt(c)
+        o.validate()
+        # too long
+        c['files'][0]['mode'] = '00777'
+        o = OpenWrt(c)
+        with self.assertRaises(ValidationError):
+            o.validate()
+        # too short
+        c['files'][0]['mode'] = '75'
+        o = OpenWrt(c)
+        with self.assertRaises(ValidationError):
+            o.validate()
+        # invalid
+        c['files'][0]['mode'] = '0855'
+        o = OpenWrt(c)
+        with self.assertRaises(ValidationError):
+            o.validate()
+
+    def test_checksum(self):
+        """ ensures checksum of same config doesn't change """
+        o = OpenWrt({"general": {"hostname": "test"}})
+        # md5 is good enough and won't slow down test execution too much
+        checksum1 = md5(o.generate().getvalue()).hexdigest()
+        sleep(1)
+        checksum2 = md5(o.generate().getvalue()).hexdigest()
+        self.assertEqual(checksum1, checksum2)
+
+    def test_override(self):
+        config = {
+            "interfaces": [
+                {
+                    "name": "eth0",
+                    "type": "ethernet",
+                    "disabled": False
+                }
+            ]
+        }
+        template = {
+            "interfaces": [
+                {
+                    "name": "eth0",
+                    "type": "ethernet",
+                    "disabled": True
+                }
+            ]
+        }
+        o = OpenWrt(config, templates=[template])
+        self.assertFalse(o.config['interfaces'][0]['disabled'])
+
+    def test_value_error(self):
+        with self.assertRaises(ValueError):
+            OpenWrt()
+        with self.assertRaises(ValueError):
+            OpenWrt(templates=[])
+        with self.assertRaises(ValueError):
+            OpenWrt(context=[])
---
+++ packages/netjsonconfig/netjsonconfig-0.6.2.tar.gz/netjsonconfig-0.6.2/tests/openwrt/test_network.py
@@ -0,0 +1,294 @@
+import unittest
+
+from netjsonconfig import OpenWrt
+from netjsonconfig.utils import _TabsMixin
+
+
+class TestNetwork(unittest.TestCase, _TabsMixin):
+    maxDiff = None
+    _ula_netjson = {"general": {"ula_prefix": "fd8e:f40a:6701::/48"}}
+    _ula_uci = """package network
+
+config globals 'globals'
+    option ula_prefix 'fd8e:f40a:6701::/48'
+"""
+    _ula_netjson_id = {
+        "general": {
+            "ula_prefix": "fd8e:f40a:6701::/48",
+            "globals_id": "arbitrary_id"
+        }
+    }
+    _ula_uci_id = """package network
+
+config globals 'arbitrary_id'
+    option ula_prefix 'fd8e:f40a:6701::/48'
+"""
+
+    def test_render_ula_prefix(self):
+        o = OpenWrt(self._ula_netjson)
+        expected = self._tabs(self._ula_uci)
+        self.assertEqual(o.render(), expected)
+
+    def test_parse_ula_prefix(self):
+        o = OpenWrt(native=self._ula_uci)
+        self.assertEqual(o.config, self._ula_netjson)
+
+    def test_render_ula_prefix_id(self):
+        o = OpenWrt(self._ula_netjson_id)
+        expected = self._tabs(self._ula_uci_id)
+        self.assertEqual(o.render(), expected)
+
+    def test_parse_ula_prefix_id(self):
+        o = OpenWrt(native=self._ula_uci_id)
+        self.assertEqual(o.config, self._ula_netjson_id)
+
+    _routes_netjson = {
+        "routes": [
+            {
+                "device": "eth1",
+                "destination": "192.168.3.1/24",
+                "next": "192.168.2.1",
+                "cost": 0
+            },
+            {
+                "device": "eth1",
+                "destination": "192.168.4.1/24",
+                "next": "192.168.2.2",
+                "cost": 2,
+                "source": "192.168.1.10",
+                "table": "2",
+                "onlink": True,
+                "mtu": "1450",
+                "type": "unicast"
+            },
+            {
+                "name": "arbitrary_name",
+                "device": "eth1",
+                "destination": "fd89::1/128",
+                "next": "fd88::1",
+                "cost": 0
+            },
+            {
+                "device": "eth1",
+                "destination": "fd90::1/128",
+                "next": "fd88::2",
+                "cost": 3,
+                "source": "fd87::10"
+            }
+        ]
+    }
+    _routes_uci = """package network
+
+config route 'route1'
+    option gateway '192.168.2.1'
+    option interface 'eth1'
+    option metric '0'
+    option netmask '255.255.255.0'
+    option target '192.168.3.1'
+
+config route 'route2'
+    option gateway '192.168.2.2'
+    option interface 'eth1'
+    option metric '2'
+    option mtu '1450'
+    option netmask '255.255.255.0'
+    option onlink '1'
+    option source '192.168.1.10'
+    option table '2'
+    option target '192.168.4.1'
+    option type 'unicast'
+
+config route6 'arbitrary_name'
+    option gateway 'fd88::1'
+    option interface 'eth1'
+    option metric '0'
+    option target 'fd89::1/128'
+
+config route6 'route4'
+    option gateway 'fd88::2'
+    option interface 'eth1'
+    option metric '3'
+    option source 'fd87::10'
+    option target 'fd90::1/128'
+"""
+
+    def test_render_routes(self):
+        o = OpenWrt(self._routes_netjson)
+        expected = self._tabs(self._routes_uci)
+        self.assertEqual(o.render(), expected)
+
+    def test_parse_routes(self):
+        o = OpenWrt(native=self._routes_uci)
+        self.assertEqual(o.config, self._routes_netjson)
+
+    _rules_netjson = {
+        "ip_rules": [
+            {
+                "in": "eth0",
+                "out": "eth1",
+                "src": "192.168.1.0/24",
+                "dest": "192.168.2.0/24",
+                "tos": 2,
+                "mark": "0x0/0x1",
+                "invert": True,
+                "lookup": "0",
+                "action": "blackhole"
+            },
+            {
+                "src": "192.168.1.0/24",
+                "dest": "192.168.3.0/24",
+                "goto": 0
+            },
+            {
+                "name": "arbitrary_name",
+                "in": "vpn",
+                "dest": "fdca:1234::/64",
+                "action": "prohibit"
+            },
+            {
+                "in": "vpn",
+                "src": "fdca:1235::/64",
+                "action": "prohibit"
+            }
+        ]
+    }
+    _rules_uci = """package network
+
+config rule 'rule1'
+    option action 'blackhole'
+    option dest '192.168.2.0/24'
+    option in 'eth0'
+    option invert '1'
+    option lookup '0'
+    option mark '0x0/0x1'
+    option out 'eth1'
+    option src '192.168.1.0/24'
+    option tos '2'
+
+config rule 'rule2'
+    option dest '192.168.3.0/24'
+    option goto '0'
+    option src '192.168.1.0/24'
+
+config rule6 'arbitrary_name'
+    option action 'prohibit'
+    option dest 'fdca:1234::/64'
+    option in 'vpn'
+
+config rule6 'rule4'
+    option action 'prohibit'
+    option in 'vpn'
+    option src 'fdca:1235::/64'
+"""
+
+    def test_render_rules(self):
+        o = OpenWrt(self._rules_netjson)
+        expected = self._tabs(self._rules_uci)
+        self.assertEqual(o.render(), expected)
+
+    def test_parse_rules(self):
+        o = OpenWrt(native=self._rules_uci)
+        self.assertEqual(o.config, self._rules_netjson)
+
+    def test_rules_no_src_dest(self):
+        o = OpenWrt({
+            "ip_rules": [
+                {
+                    "in": "eth0",
+                    "out": "eth1",
+                    "tos": 2,
+                    "mark": "0x0/0x1",
+                    "invert": True,
+                    "lookup": "0",
+                    "action": "blackhole"
+                }
+            ]
+        })
+        expected = self._tabs("""package network
+
+config rule 'rule1'
+    option action 'blackhole'
+    option in 'eth0'
+    option invert '1'
+    option lookup '0'
+    option mark '0x0/0x1'
+    option out 'eth1'
+    option tos '2'
+""")
+        self.assertEqual(o.render(), expected)
+
+    _switch_netjson = {
+        "switch": [
+            {
+                "name": "switch0",
+                "reset": True,
+                "enable_vlan": True,
+                "vlan": [
+                    {
+                        "device": "switch0",
+                        "vlan": 1,
+                        "ports": "0t 2 3 4 5"
+                    },
+                    {
+                        "device": "switch0",
+                        "vlan": 2,
+                        "vid": None,  # ``None`` or empty string disable ``vid``
+                        "ports": "0t 1"
+                    }
+                ]
+            },
+            {
+                "id": "s1",
+                "name": "switch1",
+                "reset": True,
+                "enable_vlan": True,
+                "vlan": [
+                    {
+                        "id": "v1",
+                        "device": "switch1",
+                        "vlan": 3,
+                        "vid": 130,
+                        "ports": "0t 6 7"
+                    }
+                ]
+            }
+        ]
+    }
+    _switch_uci = """package network
+
+config switch 'switch0'
+    option enable_vlan '1'
+    option name 'switch0'
+    option reset '1'
+
+config switch_vlan 'switch0_vlan1'
+    option device 'switch0'
+    option ports '0t 2 3 4 5'
+    option vid '1'
+    option vlan '1'
+
+config switch_vlan 'switch0_vlan2'
+    option device 'switch0'
+    option ports '0t 1'
+    option vlan '2'
+
+config switch 's1'
+    option enable_vlan '1'
+    option name 'switch1'
+    option reset '1'
+
+config switch_vlan 'v1'
+    option device 'switch1'
+    option ports '0t 6 7'
+    option vid '130'
+    option vlan '3'
+"""
+
+    def test_render_switch(self):
+        o = OpenWrt(self._switch_netjson)
+        expected = self._tabs(self._switch_uci)
+        self.assertEqual(o.render(), expected)
+
+    def test_parse_switch(self):
+        o = OpenWrt(native=self._switch_uci)
+        self.assertEqual(o.config, self._switch_netjson)
