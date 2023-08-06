f61c51a707473892406cef2d1c1570c16cdd577f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:17 +0000

    Add package easysearch/0.0.7/easysearch-0.0.7.tar.gz

---
+++ packages/easysearch/easysearch-0.0.7.tar.gz/easysearch-0.0.7/easysearch/doc2vec/train/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+
+from easysearch.doc2vec.doc_similarity import DocumentSimilarity
+from easysearch.utils import text
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--collection')
+parser.add_argument('--job_id')
+parser.add_argument('--text_fields')
+parser.add_argument('--host')
+parser.add_argument('--port')
+args = parser.parse_args()
+
+collection = args.collection
+print('collection', collection)
+job_id = args.job_id
+print('job_id', job_id)
+text_fields = text.parse_text_args(args.text_fields)
+print('text_fields', text_fields)
+host = args.host
+print('host', host)
+port = args.port
+print('port', port)
+
+text_fields.append('api_Conteúdo_tg')
+
+doc_similarity = DocumentSimilarity(collection=collection, job_id=job_id, text_fields=text_fields, easysearch_address=host, easysearch_port='8080')
+doc_similarity.train()
