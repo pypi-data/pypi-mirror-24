de704d9442ba000b03ec211ffaedd257a75f8e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:51 +0000

    Add package mosfit/0.7.7/mosfit-0.7.7.tar.gz

---
+++ packages/mosfit/mosfit-0.7.7.tar.gz/mosfit-0.7.7/test.py
@@ -0,0 +1,13 @@
+"""Run a test of instantiating `Fitter`, running `fit_events`."""
+import mosfit
+
+my_fitter = mosfit.fitter.Fitter()
+
+print('Running `fit_events` test.')
+
+entries, ps, lnprobs = my_fitter.fit_events(
+    events=['SN2009do', 'SN2007bg'], models=['magni', 'slsn'], iterations=1,
+    quiet=False, test=True, offline=True, user_fixed_parameters=['covariance'])
+
+print('Model WAICs: ',
+      [[y['models'][0]['score']['value'] for y in x] for x in entries])
