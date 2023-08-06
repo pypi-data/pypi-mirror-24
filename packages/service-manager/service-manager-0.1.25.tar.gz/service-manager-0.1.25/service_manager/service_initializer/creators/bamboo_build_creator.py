01e08f022325539cd5b0bbc0723a569a2b6b61a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:53 +0000

    Add package service-manager/0.1.25/service-manager-0.1.25.tar.gz

---
+++ packages/service-manager/service-manager-0.1.25.tar.gz/service-manager-0.1.25/service_manager/service_initializer/creators/bamboo_build_creator.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+from service_manager.util.services import invoke_process
+
+
+class BambooBuildCreator(object):
+
+    def __init__(self, template_dir,dry_run):
+        self.dry_run = dry_run
+        self.template_dir = template_dir
+        bamboo_config = os.path.join(os.path.abspath(template_dir),"build-config.json")
+        with open(bamboo_config) as builtin:
+            settings = json.load(builtin)
+            self.url = settings['bamboo-url']
+
+    def create_project(self, definition):
+        args = [
+            'java',
+            '-jar',
+            'bamboo-plan-1.0-SNAPSHOT.jar',
+            '--service-type', definition.get_service_type(),
+            '--bamboo-url', self.url,
+            '--application', definition.get_app(),
+            '--role', definition.get_role()
+        ]
+        invoke_process(args, exec_dir=self.template_dir, dry_run=self.dry_run)
