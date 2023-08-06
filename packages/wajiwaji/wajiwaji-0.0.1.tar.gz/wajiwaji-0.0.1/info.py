21bbefff24d1fb68a84b3c91465b054164c07770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:11 +0000

    Add package wajiwaji/0.0.1/wajiwaji-0.0.1.tar.gz

---
+++ packages/wajiwaji/wajiwaji-0.0.1.tar.gz/wajiwaji-0.0.1/info.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+# package information.
+INFO = dict(
+    name        = "wajiwaji",
+    description = "Hello I'm wajiwaji :-)",
+    author      = "Hisato Kawaji",
+    author_email = "hisato.kawaji@gmail.com",
+    license     = "MIT License",
+    url         = "https://github.com/hisato-kawaji/pypi-test",
+    classifiers = [
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
---
+++ packages/wajiwaji/wajiwaji-0.0.1.tar.gz/wajiwaji-0.0.1/lib/wajiwaji/wajiwaji/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+__info__ = dict(
+    name        = "wajiwaji",
+    version     = "0.0.1",
+    description = "Hello, I'm wajiwaji :-)",
+    author      = "Hisato Kawaji",
+    author_email = "hisato.kawaji@gmail.com",
+    license     = "MIT License",
+    url         = "https://github.com/wajiwaji/pypi-test",
+    classifiers = [
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
+
+from .hello import Hello
