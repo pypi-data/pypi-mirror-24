3196b047c33e95ead6729294c42092a880626c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:26 +0000

    Add package cat2017-8-25/1.1.1/cat2017-8-25-1.1.1.tar.gz

---
+++ packages/cat2017-8-25/cat2017-8-25-1.1.1.tar.gz/cat2017-8-25-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'cat2017-8-25', 
+    version = '1.1.1',  
+    py_modules = ['cat2017-8-25'], 
+    author = 'davystokess',  
+    author_email = '1092227565@qq.com', 
+    url = 'http://www.nanohust.com', 
+    description = 'A simple printer of nester lists'
+      ) 
