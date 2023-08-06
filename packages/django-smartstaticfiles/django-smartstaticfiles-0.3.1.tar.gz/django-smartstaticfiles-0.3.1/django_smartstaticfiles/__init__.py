f268d115b4eef34b34047ac0eff19448416cf69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:50 +0000

    Add package django-smartstaticfiles/0.3.1/django-smartstaticfiles-0.3.1.tar.gz

---
+++ packages/django-smartstaticfiles/django-smartstaticfiles-0.3.1.tar.gz/django-smartstaticfiles-0.3.1/django_smartstaticfiles/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django import VERSION as DJANGO_VERSION
+
+PROPER_DJANGO_VERSION = (1, 11)
+
+assert DJANGO_VERSION[0] == PROPER_DJANGO_VERSION[0] and \
+    DJANGO_VERSION[1] == PROPER_DJANGO_VERSION[1], \
+    '%s is only compatible with Django %s.%s' % (
+        (__name__,) + PROPER_DJANGO_VERSION
+    )
+
+VERSION = (0, 3, 1)
+__version__ = '.'.join(map(str, VERSION))
