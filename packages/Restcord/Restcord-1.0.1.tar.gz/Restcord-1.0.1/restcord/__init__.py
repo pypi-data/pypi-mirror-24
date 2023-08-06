ae7cd2d5b30d5e2118c2d018eaaea0bf8e3476f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:18 +0000

    Add package Restcord/1.0.1/Restcord-1.0.1.tar.gz

---
+++ packages/Restcord/Restcord-1.0.1.tar.gz/Restcord-1.0.1/restcord/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Discord API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Discord API.
+
+:copyright: (c) 2015-2017 JustMaffie
+:license: MIT, see LICENSE for more details.
+
+"""
+from .core import *
+from .models import *
+from .http import *
+from .vars import *
