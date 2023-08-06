b801e2e51a9b9ee7b7d929908e673620d3205239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:38 +0000

    Add package TryPackNest/1.0.0/TryPackNest-1.0.0.tar.gz

---
+++ packages/TryPackNest/TryPackNest-1.0.0.tar.gz/TryPackNest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+         name         = 'TryPackNest',
+		 pymodules     = ['TryPackNest'],
+         version      = '1.0.0',
+		 description  = 'A simple printer of nested lists',
+		# url		  = '',
+         author       = 'gloomy mind',
+		 author_email = 'gloomymind123@gmail.com',
+		 
+	 )
