7e03d2276d7d41e87ecac2638f47b5b988c8f912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package pylatconverter/0.1.6/pylatconverter-0.1.6.tar.gz

---
+++ packages/pylatconverter/pylatconverter-0.1.6.tar.gz/pylatconverter-0.1.6/README.rst
@@ -0,0 +1,40 @@
+==============
+pyLatConverter
+==============
+
+
+.. image:: https://img.shields.io/pypi/v/pylatconverter.svg
+        :target: https://pypi.python.org/pypi/pylatconverter
+
+.. image:: https://img.shields.io/travis/FSalema/pylatconverter.svg
+        :target: https://travis-ci.org/FSalema/pylatconverter
+
+.. image:: https://readthedocs.org/projects/pylatconverter/badge/?version=latest
+        :target: https://pylatconverter.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/FSalema/pylatconverter/shield.svg
+     :target: https://pyup.io/repos/github/FSalema/pylatconverter/
+     :alt: Updates
+
+
+Convert your variables into latex code
+
+
+* Free software: MIT license
+* Documentation: https://pylatconverter.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
---
+++ packages/pylatconverter/pylatconverter-0.1.6.tar.gz/pylatconverter-0.1.6/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# setup.py sdist
+# setup.py bdist_wheel
+# twine upload dist/*
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    # TODO(FSalema): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='pylatconverter',
+    version='0.1.6',
+    description="Convert your variables into latex code",
+    long_description=readme + '\n\n' + history,
+    author="Francisco Salema",
+    author_email='x_salema@hotmail.com',
+    url='https://github.com/FSalema/pylatconverter',
+    packages=find_packages(include=['pylatconverter']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='pylatconverter',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
