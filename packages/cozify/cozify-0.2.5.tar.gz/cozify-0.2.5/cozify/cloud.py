0a25bf13ab558cc0ac59fb805f4d00178e5b10c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:20 +0000

    Add package cozify/0.2.5/cozify-0.2.5.tar.gz

---
+++ packages/cozify/cozify-0.2.5.tar.gz/cozify-0.2.5/cozify/cloud.py
@@ -0,0 +1,264 @@
+"""Module for handling Cozify Cloud API operations
+
+Attributes:
+    cloudBase(string): API endpoint including version
+
+"""
+
+import json, requests, logging
+
+from . import config as c
+from . import hub
+
+from .Error import APIError, AuthenticationError
+
+cloudBase='https://cloud2.cozify.fi/ui/0.2/'
+
+def authenticate(trustCloud=True, trustHub=True):
+    """Authenticate with the Cozify Cloud and Hub.
+
+    Interactive only when absolutely needed, mostly on the first run.
+    By default authentication is run selectively only for the portions needed.
+
+    Authentication is a multistep process:
+        - trigger sending OTP to email address
+        - perform email login with OTP to acquire cloud token
+        - acquire hub information and authenticate with hub with cloud token
+        - store hub token for further use
+
+    Args:
+        trustCloud(bool): Trust current stored state of cloud auth. Default True.
+        trustHub(bool): Trust current stored state of hub auth. Default True.
+    """
+
+    if 'email' not in  c.state['Cloud'] or not  c.state['Cloud']['email']:
+         c.state['Cloud']['email'] = _getEmail()
+         c.stateWrite()
+    email = c.state['Cloud']['email']
+
+    if _needRemoteToken(trustCloud):
+        try:
+            _requestlogin(email)
+        except APIError:
+            resetState() # a bogus email will shaft all future attempts, better to reset
+            raise
+
+        # get OTP from user, not stored anywhere since they have a very short lifetime
+        otp = _getotp()
+        if not otp:
+            message = "OTP unavailable, authentication cannot succeed. This may happen if running non-interactively (closed stdin)."
+            logging.critical(message)
+            raise AuthenticationError(message)
+
+        try:
+            remoteToken = _emaillogin(email, otp)
+        except APIError:
+            logging.error('OTP authentication has failed.')
+            resetState()
+            raise
+
+        # save the successful remoteToken
+        c.state['Cloud']['remoteToken'] = remoteToken
+        c.stateWrite()
+    else:
+        # remoteToken already fine, let's just use it
+        remoteToken = c.state['Cloud']['remoteToken']
+
+    if _needHubToken(trustHub):
+        hubIps = _lan_ip()
+        hubkeys = _hubkeys(remoteToken)
+        if not hubIps:
+            raise Exception('No LAN ip returned, is your hub registered?')
+
+        for hubIp in hubIps: # hubIps is returned as a list of all hubs
+            hubInfo = hub._hub(hubIp)
+            hubId = hubInfo['hubId']
+            hubName = hubInfo['name']
+            if hubId in hubkeys:
+                hubToken = hubkeys[hubId]
+            else:
+                logging.error('The hub "%s" is not linked to the given account: "%s"' % (hubName, c.state['Cloud']['email']))
+                resetState()
+                return False
+
+            # if hub name not already known, create named section
+            hubSection = 'Hubs.' + hubName
+            if hubSection not in c.state:
+                c.state[hubSection] = {}
+            # if default hub not set, set this hub as the first as the default
+            if 'default' not in c.state['Hubs']:
+                c.state['Hubs']['default'] = hubName
+
+            # store Hub data under it's named section
+            c.state[hubSection]['hubToken'] = hubToken
+            c.state[hubSection]['host'] = hubIp
+            c.state[hubSection]['hubId'] = hubId # not really used for anything but doesn't hurt
+            c.stateWrite()
+    return True
+
+def resetState():
+    """Reset stored cloud state.
+    
+    Any further authentication flow will start from a clean slate.
+    Hub state is left intact.
+    """
+
+    c.state['Cloud'] = {}
+    c.stateWrite()
+
+def ping():
+    """Test cloud token validity.
+
+    Returns:
+        bool: validity of stored token.
+
+    """
+
+    try:
+        _hubkeys(c.state['Cloud']['remoteToken']) # TODO(artanicus): see if there's a cheaper API call
+    except APIError as e:
+        if e.status_code == 401:
+            return False
+        else:
+            raise
+    else:
+        return True
+
+def refresh():
+    """Renew current cloud token and store new token in state.
+
+    This call will only succeed if the current cloud token is still valid.
+    A new refreshed token is requested from the API and stored in state.
+
+    Returns:
+        bool: Success of refresh attempt.
+    """
+    try:
+        newRemoteToken = _refreshsession(c.state['Cloud']['remoteToken'])
+    except APIError as e:
+        if e.status_code == 401:
+            # too late, our token is already dead
+            return False
+        else:
+            raise
+    else:
+        c.state['Cloud']['remoteToken'] = newRemoteToken
+        c.stateWrite()
+        return True
+
+
+
+def _needRemoteToken(trust):
+    """Validate current remote token and decide if we'll request it during authentication.
+
+    Args:
+        trust(bool): Set to False to always decide to renew.
+
+    Returns:
+        bool: True to indicate a need to request token.
+    """
+
+    # check if we've got a remoteToken before doing expensive checks
+    if trust and 'remoteToken' in c.state['Cloud']:
+        if c.state['Cloud']['remoteToken'] is None:
+            return True
+        else: # perform more expensive check
+            return not ping()
+    return True
+
+def _needHubToken(trust):
+    """Validate current hub token and decide if we'll request it during authentication.
+
+    Args:
+        trust(bool): Set to False to always decide to renew.
+
+    Returns:
+        bool: True to indicate a need to request token.
+    """
+
+    # First do quick checks, i.e. do we even have a token already
+    if trust and ('default' not in c.state['Hubs'] or 'hubtoken' not in c.state['Hubs.' + c.state['Hubs']['default']]):
+        return True
+    else: # if we have a token, we need to test if the API is callable
+        return not hub.ping()
+
+def _getotp():
+    try:
+        return input('OTP from your email: ')
+    except EOFError: # if running non-interactive or ^d
+        return None
+
+def _getEmail():
+    return input('Enter your Cozify account email address: ')
+
+def _requestlogin(email):
+    """Raw Cloud API call, request OTP to be sent to account email address.
+
+    Args:
+        email(str): Email address connected to Cozify account.
+    """
+
+    payload = { 'email': email }
+    response = requests.post(cloudBase + 'user/requestlogin', params=payload)
+    if response.status_code is not 200:
+        raise APIError(response.status_code, response.text)
+
+def _emaillogin(email, otp):
+    """Raw Cloud API call, request cloud token with email address & OTP.
+
+    Args:
+        email(str): Email address connected to Cozify account.
+        otp(int): One time passcode.
+
+    Returns:
+        str: cloud token
+    """
+
+    payload = {
+            'email': email,
+            'password': otp
+    }
+
+    response = requests.post(cloudBase + 'user/emaillogin', params=payload)
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise APIError(response.status_code, response.text)
+
+# 1:1 implementation of hub/lan_ip
+# returns list of hub ip's
+# Oddly enough remoteToken isn't needed here and on the flipside doesn't help.
+# By testing it seems hub/lan_ip will use the source ip of the request to determine the validity of the request.
+# Thus, only if you're making the request from the same public ip (or ip block?) will this call succeed with useful results
+def _lan_ip():
+    response = requests.get(cloudBase + 'hub/lan_ip')
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        raise APIError(response.status_code, response.text)
+
+# 1:1 implementation of user/hubkeys
+# remoteToken: cozify Cloud remoteToken
+# returns map of hubs: { hubId: hubToken }
+def _hubkeys(remoteToken):
+    headers = {
+            'Authorization': remoteToken
+    }
+    response = requests.get(cloudBase + 'user/hubkeys', headers=headers)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        raise APIError(response.status_code, response.text)
+
+# 1:1 implementation of 'refreshsession'
+# remoteToken: cozify Cloud remoteToken
+# returns new remoteToken, not automatically stored into state
+def _refreshsession(remoteToken):
+    headers = {
+            'Authorization': remoteToken
+    }
+    response = requests.get(cloudBase + 'user/refreshsession', headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise APIError(response.status_code, response.text)
---
+++ packages/cozify/cozify-0.2.5.tar.gz/cozify-0.2.5/cozify/hub.py
@@ -0,0 +1,101 @@
+"""Module for handling Cozify Hub API operations
+
+Attributes:
+    apiPath(string): API endpoint path including version
+
+"""
+
+import requests, json, logging
+from . import config as c
+from . import cloud
+
+from .Error import APIError
+
+apiPath = '/cc/1.6/'
+
+def getDevices(hubName=None):
+    """Get up to date full devices data set as a dict
+
+    Args:
+        hubName(str): optional name of hub to query. Defaults to result of ``getDefaultHub()``
+
+    Returns:
+        dict: full live device state as returned by the API
+
+    """
+    if hubName is None:
+        hubName = getDefaultHub()
+
+    configName = 'Hubs.' + hubName
+    if configName not in c.state or 'hubtoken' not in c.state[configName]:
+        logging.warning('No valid authentication token, requesting authentication')
+        cloud.authenticate()
+
+    # at this stage we have a valid name and an auth token. We don't know if the token actually works!
+    headers = {
+        'Content-type': "application/json",
+        'Accept': "application/json",
+        'Authorization': c.state[configName]['hubtoken'],
+        'Cache-control': "no-cache",
+    }
+
+    host = c.state[configName]['host']
+    response = requests.get(_getBase(host=host) + 'devices', headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise APIError(response.status_code, '%s - %s - %s' % (response.reason, response.url, response.text))
+
+# return name of default Hub
+# if default is unknown, run auth to make default known
+def getDefaultHub():
+    if 'default' not in c.state['Hubs']:
+        print('no hub name given and no default known, running authentication')
+        cloud.authenticate()
+    return c.state['Hubs']['default']
+
+def _getBase(host, port=8893, api=apiPath):
+    # TODO(artanicus): this may still need some auth hook
+    return 'http://%s:%s%s' % (host, port, api)
+
+# perform a cheap API call to trigger any potential APIError and return boolean for success/failure
+def ping(hub_name=None):
+    global pings
+    if hub_name is None:
+        hub_name =  getDefaultHub()
+    try:
+        config_name = 'Hubs.' + hub_name
+        hub_token = c.state[config_name]['hubtoken']
+        hub_host = c.state[config_name]['host']
+        tz = _tz(hub_host, hub_token)
+    except APIError as e:
+        if e.status_code == 401:
+            return False
+        else:
+            raise
+    else:
+        return True
+
+
+# 1:1 implementation of /hub API call
+# hubHost: valid ip/host to hub, defaults to state data
+# returns map of hub state
+# interestingly enough remoteToken isn't needed here and the hub will answer regardless of auth
+def _hub(host):
+    response = requests.get(_getBase(host=host, api='/') + 'hub')
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        raise APIError(response.status_code, response.text)
+
+# 1:1 implementation of /hub/tz API call
+# hubHost: valid ip/host to hub, defaults to state data
+# hub_token: authentication token
+# returns timezone of hub, e.g. Europe/Helsinki
+def _tz(host, hub_token):
+    headers = { 'Authorization': hub_token }
+    response = requests.get(_getBase(host=host) + 'hub/tz', headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise APIError(response.status_code, response.text)
