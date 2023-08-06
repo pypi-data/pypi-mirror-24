1e29ff800a0a0ff272c606ed341659c8fa3c1e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:40 +0000

    Add package dotcli/0.1.2/dotcli-0.1.2-py2.py3-none-any.whl

---
+++ packages/dotcli/dotcli-0.1.2-py2.py3-none-any.whl/dot/controllers/pull_controller.py
@@ -0,0 +1,13 @@
+from cement.core.controller import CementBaseController, expose
+from services import git_service
+
+class PullController(CementBaseController):
+    class Meta:
+        label = 'pull'
+        description = 'Pull dotfiles'
+        stacked_on = 'base'
+        stacked_type = 'nested'
+
+    @expose(hide=True)
+    def default(self):
+        git_service.pull()
