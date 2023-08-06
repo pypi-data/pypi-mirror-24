91c08b143be9ea0663950868139bb406ae188df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:23 +0000

    Add package DocxMerge/0.1.1/DocxMerge-0.1.1.tar.gz

---
+++ packages/DocxMerge/DocxMerge-0.1.1.tar.gz/DocxMerge-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+  name = 'DocxMerge',
+  packages = ['DocxMerge'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'A simple library for merging Microsoft Word Documents.',
+  author = 'Brian Carlson',
+  author_email = 'briancarlson6174@gmail.com',
+  url = 'https://github.com/TheRobotCarlson/DocxMerge', # use the URL to the github repo
+  download_url = 'https://github.com/TheRobotCarlson/DocxMerge/archive/0.1.tar.gz', # 
+  keywords = ['docx', 'merge', 'word'], # arbitrary keywords
+)
