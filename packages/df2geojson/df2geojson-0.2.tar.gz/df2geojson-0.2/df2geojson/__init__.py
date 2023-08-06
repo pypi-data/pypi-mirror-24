391779907e870797daf3bb2a877b5521a21c8e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:08 +0000

    Add package df2geojson/0.2/df2geojson-0.2.tar.gz

---
+++ packages/df2geojson/df2geojson-0.2.tar.gz/df2geojson-0.2/df2geojson/__init__.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 22 22:12:05 2017
+
+@author: Tom
+"""
+import sys
+import json
+
+def convert(df, LonColName, LatColName, progress=True):
+    #Define outside structure of geojson
+    geojson = {"features":[], "type": "FeatureCollection"}
+    #Get the columns of the dataframe 
+    cols = df.columns.tolist()
+    #Remove longitude and latitude from list
+    cols.remove(LonColName)
+    cols.remove(LatColName)
+    #Iterate through dataframe
+    for i in range(0, len(df.index)):
+        tempDict = {"type": "Feature","properties": {},"geometry":{"coordinates": [],"type": "Point"}}
+        #Add properties
+        for prop in cols:
+            try:
+                tempDict["properties"][prop] = df.iloc[i][prop].tolist()
+            except:
+                tempDict["properties"][prop] = df.iloc[i][prop]
+        #Add coordinates
+        tempDict["geometry"]["coordinates"].append(df.iloc[i][LonColName].tolist())
+        tempDict["geometry"]["coordinates"].append(df.iloc[i][LatColName].tolist())
+        #Add to outside structure
+        geojson["features"].append(tempDict)
+        if progress == True:
+            percent = str(round((i/len(df.index))*100,1))
+            if(i == (len(df.index) - 1)):
+                sys.stdout.write("\rConversion Complete            ")
+                sys.stdout.flush()
+            else:
+                sys.stdout.write("\rProgress: %s percent complete" %percent)
+                sys.stdout.flush()
+    return geojson
+
+def dump(path, geojson):
+    with open(path, 'w') as fp:
+        json.dump(geojson, fp)
+        
+def convert_and_dump(df, LonColName, LatColName, path, progress=True):
+    #Define outside structure of geojson
+    geojson = {"features":[], "type": "FeatureCollection"}
+    #Get the columns of the dataframe 
+    cols = df.columns.tolist()
+    #Remove longitude and latitude from list
+    cols.remove(LonColName)
+    cols.remove(LatColName)
+    #Iterate through dataframe
+    for i in range(0, len(df.index)):
+        tempDict = {"type": "Feature","properties": {},"geometry":{"coordinates": [],"type": "Point"}}
+        #Add properties
+        for prop in cols:
+            try:
+                tempDict["properties"][prop] = df.iloc[i][prop].tolist()
+            except:
+                tempDict["properties"][prop] = df.iloc[i][prop]
+        #Add coordinates
+        tempDict["geometry"]["coordinates"].append(df.iloc[i][LonColName].tolist())
+        tempDict["geometry"]["coordinates"].append(df.iloc[i][LatColName].tolist())
+        #Add to outside structure
+        geojson["features"].append(tempDict)
+        if progress == True:
+            percent = str(round((i/len(df.index))*100,1))
+            if(i == (len(df.index) - 1)):
+                sys.stdout.write("\rConversion Complete            ")
+                sys.stdout.flush()
+            else:
+                sys.stdout.write("\rProgress: %s percent complete" %percent)
+                sys.stdout.flush()
+    dump(path, geojson)
+    
---
+++ packages/df2geojson/df2geojson-0.2.tar.gz/df2geojson-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'df2geojson',
+    packages = ['df2geojson'],
+    version = '0.2',
+    description = 'A small Python module for converting Pandas Dataframes to geojson format',
+    author = 'Thomas Preece',
+    author_email = 'thomaspreece@hotmail.co.uk',
+    url = 'https://github.com/TPreece101/df2geojson',
+    download_url = 'https://github.com/TPreece101/df2geojson/archive/0.2.tar.gz',
+    keywords = ['dataframe', 'geojson', 'convert'],
+    classifiers = [],
+)
