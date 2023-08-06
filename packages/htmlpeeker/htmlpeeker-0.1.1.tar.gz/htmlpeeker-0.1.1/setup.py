ff12077793faa19d02f920f6ccff479f556006e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:49 +0000

    Add package htmlpeeker/0.1.1/htmlpeeker-0.1.1.tar.gz

---
+++ packages/htmlpeeker/htmlpeeker-0.1.1.tar.gz/htmlpeeker-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='htmlpeeker',
+    version='0.1.1',
+    description='Peek html:)',
+    url='https://github.com/harukaeru/HTMLPeeker',
+    author='harukaeru',
+    author_email='nekoneko.myaomyao@gmail.com',
+    license='MIT',
+    py_modules=['peeker']
+)
