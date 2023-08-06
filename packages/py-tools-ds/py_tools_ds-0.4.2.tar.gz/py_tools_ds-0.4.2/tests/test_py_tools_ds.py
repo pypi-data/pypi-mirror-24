d2cd8b1d78c132c6bc4dc8d90999c16b53cb22bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:43 +0000

    Add package py-tools-ds/0.4.2/py_tools_ds-0.4.2.tar.gz

---
+++ packages/py-tools-ds/py_tools_ds-0.4.2.tar.gz/py_tools_ds-0.4.2/tests/test_py_tools_ds.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_py_tools_ds
+----------------------------------
+
+Tests for `py_tools_ds` module.
+"""
+
+
+import sys
+import unittest
+
+from py_tools_ds import *
+
+
+
+class TestPy_tools_ds(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
