3f5affe9cd2108d46a421467b6fcec00fa66673d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:42 +0000

    Add package tripal/2.0/tripal-2.0.tar.gz

---
+++ packages/tripal/tripal-2.0.tar.gz/tripal-2.0/setup.py
@@ -0,0 +1,28 @@
+# http://bugs.python.org/issue15881#msg170215
+from setuptools import setup, find_packages
+
+setup(
+    name="tripal",
+    version='2.0',
+    description="Tripal library",
+    author="Anthony Bretaudeau",
+    author_email="anthony.bretaudeau@inra.fr",
+    url="https://github.com/galaxy-genome-annotation/python-tripal",
+    install_requires=['requests>=2.4.3'],
+    packages=find_packages(),
+    license='MIT',
+    platforms="Posix; MacOS X; Windows",
+    entry_points='''
+        [console_scripts]
+        tripaille=tripaille.cli:tripaille
+    ''',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7"
+    ])
