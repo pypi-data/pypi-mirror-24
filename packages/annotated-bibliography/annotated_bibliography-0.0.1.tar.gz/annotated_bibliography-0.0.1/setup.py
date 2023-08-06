3e60e676632e01d8a88f51e45e3a41c413486ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:25 +0000

    Add package annotated-bibliography/0.0.1/annotated_bibliography-0.0.1.tar.gz

---
+++ packages/annotated-bibliography/annotated_bibliography-0.0.1.tar.gz/annotated_bibliography-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='annotated_bibliography',
+      version='0.0.1',
+      description='Simple annotated bibliography generator',
+      url='https://github.com/manoelhortaribeiro/annotated_bibliography',
+      download_url='https://github.com/manoelhortaribeiro/annotated_bibliography/dist/annotated_bibliography-0.1.tar.gz',
+      author='Manoel Horta Ribeiro',
+      author_email='manoelhortaribeiro@gmail.com',
+      license='MIT',
+      packages=['annotated_bibliography'],
+      keywords='bibliography research annotations',
+      install_requires=['matplotlib', 'mistune', 'pybtex', 'jinja2', 'numpy'],
+      entry_points={
+            'console_scripts': ['make_ann_bib=annotated_bibliography.make_ann_bib:make_html']
+      },
+      zip_safe=False)
