6bfc75c6b6d3fd02dd3f402b73bcd7c7ccb25603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:41 +0000

    Add package dotcli/0.1.4/dotcli-0.1.4.tar.gz

---
+++ packages/dotcli/dotcli-0.1.4.tar.gz/dotcli-0.1.4/dot/controllers/push_controller.py
@@ -0,0 +1,13 @@
+from cement.core.controller import CementBaseController, expose
+from services import git_service
+
+class PushController(CementBaseController):
+    class Meta:
+        label = 'push'
+        description = 'Push dotfiles'
+        stacked_on = 'base'
+        stacked_type = 'nested'
+
+    @expose(hide=True)
+    def default(self):
+        git_service.push()
