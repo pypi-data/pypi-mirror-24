7e5825dfc10daf5db23b82e18bd58a8a5d9a66f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:14 +0000

    Add package spyder-reports/0.1.1/spyder-reports-0.1.1.tar.gz

---
+++ packages/spyder-reports/spyder-reports-0.1.1.tar.gz/spyder-reports-0.1.1/spyder_reports/_version.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Version File."""
+VERSION_INFO = (0, 1, 1)
+__version__ = '.'.join(map(str, VERSION_INFO))
---
+++ packages/spyder-reports/spyder-reports-0.1.1.tar.gz/spyder-reports-0.1.1/spyder_reports/utils/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Spyder Reports Utils."""
+import os.path as osp
+
+WELCOME_PATH = osp.join(osp.dirname(__file__), 'templates', 'welcome.md')
