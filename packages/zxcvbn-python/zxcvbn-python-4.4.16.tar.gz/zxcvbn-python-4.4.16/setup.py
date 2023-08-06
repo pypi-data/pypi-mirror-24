95c9f6746917e239d8a6c0198fe53759c95fbebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:41 +0000

    Add package zxcvbn-python/4.4.16/zxcvbn-python-4.4.16.tar.gz

---
+++ packages/zxcvbn-python/zxcvbn-python-4.4.16.tar.gz/zxcvbn-python-4.4.16/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='zxcvbn-python',
+    version='4.4.16',
+    packages=['zxcvbn'],
+    url='https://github.com/dwolfhub/zxcvbn-python',
+    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/v4.4.16',
+    license='MIT',
+    author='Daniel Wolf',
+    author_email='danielrwolf5@gmail.com',
+    description='Python implementation of Dropbox\'s realistic password '
+                'strength estimator, zxcvbn',
+    keywords=['zxcvbn', 'password', 'security'],
+)
