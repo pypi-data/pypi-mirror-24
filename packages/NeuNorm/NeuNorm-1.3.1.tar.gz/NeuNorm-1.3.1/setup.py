33dd0fda25c3c052e2601ef95d1d7637c74d1bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:45 +0000

    Add package NeuNorm/1.3.1/NeuNorm-1.3.1.tar.gz

---
+++ packages/NeuNorm/NeuNorm-1.3.1.tar.gz/NeuNorm-1.3.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = "NeuNorm",
+    version = "1.3.1",
+    author = "Jean Bilheux",
+    author_email = "bilheuxjm@ornl.gov",
+    packages = find_packages(exclude=['tests', 'notebooks']),
+    include_package_data = True,
+    test_suite = 'tests',
+    install_requires = [
+        'numpy',
+        'pyfits',
+        'pillow',
+        'pathlib',
+        'astropy',
+        'scipy',
+    ],
+    dependency_links = [
+    ],
+    description = "neutron normalization data",
+    license = 'BSD',
+    keywords = "neutron normalization imaging",
+    url = "https://github.com/ornlneutronimaging/NeuNorm",
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5'],
+)
+
+
+# End of file
