0c95cf3a05ced81bdb48e3b26d7829f5b627ca27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:17 +0000

    Add package PyUniProt/0.0.3/PyUniProt-0.0.3.tar.gz

---
+++ packages/PyUniProt/PyUniProt-0.0.3.tar.gz/PyUniProt-0.0.3/src/pyuniprot/manager/__init__.py
@@ -0,0 +1,9 @@
+"""
+The pyctd.manager module serves as an interface between the CTD graph data structure and underlying relational
+databases.
+"""
+from . import defaults
+from . import models
+from . import database
+from . import query
+from . import make_json_serializable
