a417bd537f3db53f6df12fa43744791330a4850a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:37 +0000

    Add package NHANES-semantic-data-dictionary-annotation/1.0.3/NHANES-semantic-data-dictionary-annotation-1.0.3.tar.gz

---
+++ packages/NHANES-semantic-data-dictionary-annotation/NHANES-semantic-data-dictionary-annotation-1.0.3.tar.gz/NHANES-semantic-data-dictionary-annotation-1.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name = 'NHANES-semantic-data-dictionary-annotation',
+    version = '1.0.3',
+    description = 'Scripts for extracting semantic data dictionaries from NHANES data',
+    url = 'https://github.com/rashidsabbir/extractor',
+    author = 'Ryan Sherman, Sabbir Rashid',
+    author_email='rjsfox8@gmail.com',
+    license='MIT',
+    classifiers=[
+	'Development Status :: 3 - Alpha',
+	'Intended Audience :: Developers'],
+    scripts = ['annotation_engine.py', 'NHANES_extractor_exp.py', 'utils.py'],
+    install_requires=[
+          'labkey',
+          'PyDictionary',
+          'sklearn',
+          'multiprocessing',
+          'inflect',
+          'urllib2',
+          'requests',
+          'urllib',
+          'bs4',
+          'lxml',
+          'numpy',
+          'scipy'
+      ],
+    zip_safe = False)
