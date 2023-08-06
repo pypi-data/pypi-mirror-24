07e00e20ae3e905de716567d4b085677c2e66066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:03 +0000

    Add package gnore/0.1.1/gnore-0.1.1.tar.gz

---
+++ packages/gnore/gnore-0.1.1.tar.gz/gnore-0.1.1/gnore/config.py
@@ -0,0 +1,16 @@
+NAME = 'gnore'
+
+AUTHOR = 'Jam Risser'
+
+VERSION = '0.1.1'
+
+COPYRIGHT = '2017'
+
+BANNER = '''
+g'nore v''' + VERSION + '''
+
+"I'm just gonna g'nore you"
+  - My Little Sister
+
+Copyright (c) ''' + COPYRIGHT + ' ' + AUTHOR + '''
+'''
