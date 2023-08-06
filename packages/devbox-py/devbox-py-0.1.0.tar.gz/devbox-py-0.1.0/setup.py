7fa004538d8a131044994bedc4cd4b94aa9035e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:45 +0000

    Add package devbox-py/0.1.0/devbox-py-0.1.0.tar.gz

---
+++ packages/devbox-py/devbox-py-0.1.0.tar.gz/devbox-py-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='devbox-py',
+      description='DevBox for Python - Managing environments and boilerplates.',
+      version='0.1.0',
+      author='D. Misic',
+      author_email='dion@gmail.com',
+      packages=['devbox'],
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
