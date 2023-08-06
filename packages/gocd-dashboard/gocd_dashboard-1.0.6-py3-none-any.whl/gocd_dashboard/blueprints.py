199e367053dc41a9f8f4d8372f20f4548a48dc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:42 +0000

    Add package gocd-dashboard/1.0.6/gocd_dashboard-1.0.6-py3-none-any.whl

---
+++ packages/gocd-dashboard/gocd_dashboard-1.0.6-py3-none-any.whl/gocd_dashboard/blueprints.py
@@ -0,0 +1,16 @@
+import flask
+
+import gocd_dashboard.config
+
+ui = flask.Blueprint('ui', __name__)
+
+
+@ui.before_app_first_request
+def config():
+    flask.current_app.configuration = gocd_dashboard.config.Config.load()
+
+
+@ui.route('/')
+def dashboard():
+    return flask.render_template(
+        'home.html', groups=flask.current_app.configuration.groups())
