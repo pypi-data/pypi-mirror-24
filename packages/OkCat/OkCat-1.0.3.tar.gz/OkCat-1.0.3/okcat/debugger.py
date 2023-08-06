feb3240b7491ecc549b499d3aea6f090ce128bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:15 +0000

    Add package OkCat/1.0.3/OkCat-1.0.3.tar.gz

---
+++ packages/OkCat/OkCat-1.0.3.tar.gz/OkCat-1.0.3/okcat/debugger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python -u
+
+"""
+Copyright (C) 2017 Jacksgong(blog.dreamtobe.cn)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__author__ = 'JacksGong'
+
+DEBUG_LEVEL = 'E'
+LEVEL_E = 'E'
+LEVEL_W = 'W'
+LEVEL_I = 'I'
+LEVEL_V = 'V'
+
+# def debug_e(tag, message):
+# def debug(level, tag, message):
