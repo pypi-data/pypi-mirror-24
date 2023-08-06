ed68ae302806c922f1a420bd84c3f2a8073e767c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:24 +0000

    Add package lebot-cerebro/0.1.0.dev2/lebot-cerebro-0.1.0.dev2.tar.gz

---
+++ packages/lebot-cerebro/lebot-cerebro-0.1.0.dev2.tar.gz/lebot-cerebro-0.1.0.dev2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='lebot-cerebro',
+      version='0.1.0dev2',
+      description='Core engine for LeBot',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      keywords=['Chat Bot, AI'],
+      url='https://github.com/Le-Bot/cerebro',
+      author='LeBot',
+      author_email='sanket.upadhyay@infoud.co.in',
+      license='MIT',
+      packages=['cerebro'],
+      install_requires=[
+          'scikit-learn',
+          'scipy',
+          'numpy',
+          'pandas',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
