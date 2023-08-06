84487ecafa21ad20afb28a74a34ba3a45100f49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:55 +0000

    Add package amaasinfra/0.2.5.7/amaasinfra-0.2.5.7.tar.gz

---
+++ packages/amaasinfra/amaasinfra-0.2.5.7.tar.gz/amaasinfra-0.2.5.7/amaasinfra/logging/api_call.py
@@ -0,0 +1,37 @@
+""" Logs calls to the AMaaS APIs """
+
+from datetime import datetime
+
+import boto3
+
+class APICallLogger(object):
+    """ The current implementation of the API Call Logger uses Cloudwatch metrics """
+
+    def __init__(self):
+        self.client = boto3.client('cloudwatch')
+
+    def record_api_call(self, asset_manager_id, api_call_type, api_call_subtype, client_id, token_asset_manager_id, username, environment='dev'):
+        """ Record the API call """
+        asset_manager_id = str(asset_manager_id)  # In case it gets sent in as an integer
+        token_asset_manager_id = str(token_asset_manager_id)
+        username = str(username)
+        self.client.put_metric_data(
+            Namespace='amaas',
+            MetricData=[
+                {
+                    'MetricName': 'AMaaSAPICalls',
+                    'Dimensions': [
+                        {'Name': 'api_call_type', 'Value': api_call_type},
+                        {'Name': 'api_call_subtype', 'Value': api_call_subtype},
+                        {'Name': 'asset_manager_id', 'Value': asset_manager_id},
+                        {'Name': 'environment', 'Value': environment},
+                        {'Name': 'client_id', 'Value': client_id},
+                        {'Name': 'token_asset_manager_id', 'Value': token_asset_manager_id},
+                        {'Name': 'username', 'Value': username}
+                    ],
+                    'Timestamp': datetime.utcnow(),
+                    'Value': 1,
+                    'Unit': 'Count'
+                },
+            ]
+        )
