0e364677146c52f30f5916888f275126ef687c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:27 +0000

    Add package django-slicepaginator/0.0.1/django-slicepaginator-0.0.1.tar.gz

---
+++ packages/django-slicepaginator/django-slicepaginator-0.0.1.tar.gz/django-slicepaginator-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='django-slicepaginator',
+	version='0.0.1',
+	description='Slice page_range of default django paginator.page_range',
+	author = 'HS Moon',
+	author_email = 'mhs9089@gmail.com',
+	py_modules = ['slicer'],
+	url='https://github.com/zwolf21/django-slicepaginator'
+)
