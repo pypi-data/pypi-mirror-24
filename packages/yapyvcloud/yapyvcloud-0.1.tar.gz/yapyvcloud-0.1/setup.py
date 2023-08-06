571a40eeb28dd4b8320ccdb2a410ae1242dc458d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:22 +0000

    Add package yapyvcloud/0.1/yapyvcloud-0.1.tar.gz

---
+++ packages/yapyvcloud/yapyvcloud-0.1.tar.gz/yapyvcloud-0.1/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(
+    name = 'yapyvcloud',
+    packages = ['yapyvcloud'],
+    version = '0.1',
+    description = 'yet another pyvcloud',
+    author = 'lean.serpent',
+    author_email = 'lean.serpent@gmail.com',
+    url = 'https://github.com/leanserpent/yapyvcloud',
+    download_url = 'https://github.com/leanserpent/yapyvcloud/archive/v0.1.tar.gz',
+    keywords = ['python', 'vcloud'],
+    install_requires = ['nose'],
+    scripts = []
+)
+
