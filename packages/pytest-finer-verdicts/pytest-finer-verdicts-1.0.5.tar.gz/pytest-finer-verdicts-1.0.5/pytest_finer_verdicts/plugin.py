c9047707276e477b90f9d671b029fb7d126b1064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:14 +0000

    Add package pytest-finer-verdicts/1.0.5/pytest-finer-verdicts-1.0.5.tar.gz

---
+++ packages/pytest-finer-verdicts/pytest-finer-verdicts-1.0.5.tar.gz/pytest-finer-verdicts-1.0.5/pytest_finer_verdicts/plugin.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """Treat non-assertion and non-pytest.fail failures as test errors."""
+    outcome = yield
+    test_report = outcome.get_result()
+
+    if call.excinfo and test_report.outcome == 'failed':
+        when = call.when
+        excinfo = call.excinfo
+        if when == "call" and \
+            excinfo.typename != "AssertionError" and \
+                excinfo.typename != "Failed":
+            when = 'setup'
+        test_report.longrepr = item.repr_failure(excinfo)
+        test_report.when = when
