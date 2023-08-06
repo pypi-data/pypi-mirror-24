bbfdb89dce5c42f93539ec5ebbd9c9090befbb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:30 +0000

    Add package tsputil/1.1.1a1/tsputil-1.1.1a1.tar.gz

---
+++ packages/tsputil/tsputil-1.1.1a1.tar.gz/tsputil-1.1.1a1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='tsputil',
+      version='1.1.1a1',
+      description=' Helper functions to work with .tsp and .sol files in the context of the Traveling Salesperson Problem',
+      url='https://bitbucket.org/TimRach/tsp-python',
+      author='Tim Rach',
+      author_email='tim.rach91@gmail.com',
+      license='CC BY-SA 3.0',
+      packages=['tsputil'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Utilities',
+          'Programming Language :: Python :: 3.5',
+      ],
+      keywords='TSP utilities',
+      python_requires='>=3',
+      zip_safe=False)
