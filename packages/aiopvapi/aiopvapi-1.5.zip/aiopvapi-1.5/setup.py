df6e2a7c478042b388fe8b32a04578b98197787f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:15 +0000

    Add package aiopvapi/1.5/aiopvapi-1.5.zip

---
+++ packages/aiopvapi/aiopvapi-1.5.zip/aiopvapi-1.5/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+
+VERSION = "1.5"
+
+setup(
+    name='aiopvapi',
+    version=VERSION,
+    packages=['aiopvapi'],
+    download_url='https://github.com/sander76/aio-powerview-api/archive/' + VERSION + '.tar.gz',
+    url="https://github.com/sander76/aio-powerview-api",
+    license='BSD License',
+    author='Sander Teunissen',
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    author_email='',
+    description='Powerview blinds api',
+    install_requires=[
+        'async_timeout',
+        'aiohttp',
+        'yarl'
+    ]
+)
