3e0542e894a75cefe02b1198671cc5e3e27ccbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:49 +0000

    Add package aiogram/0.4/aiogram-0.4-py3-none-any.whl

---
+++ packages/aiogram/aiogram-0.4-py3-none-any.whl/aiogram/utils/exceptions.py
@@ -0,0 +1,41 @@
+_PREFIXES = ['Error: ', '[Error]: ', 'Bad Request: ']
+
+
+def _clean_message(text):
+    for prefix in _PREFIXES:
+        if text.startswith(prefix):
+            text = text[len(prefix):]
+    return text
+
+
+class TelegramAPIError(Exception):
+    def __init__(self, message):
+        super(TelegramAPIError, self).__init__(_clean_message(message))
+
+
+class ValidationError(TelegramAPIError):
+    pass
+
+
+class BadRequest(TelegramAPIError):
+    pass
+
+
+class Unauthorized(TelegramAPIError):
+    pass
+
+
+class NetworkError(TelegramAPIError):
+    pass
+
+
+class RetryAfter(TelegramAPIError):
+    def __init__(self, retry_after):
+        super(RetryAfter, self).__init__(f"Flood control exceeded. Retry in {retry_after} seconds.")
+        self.timeout = retry_after
+
+
+class MigrateToChat(TelegramAPIError):
+    def __init__(self, chat_id):
+        super(MigrateToChat, self).__init__(f"The group has been migrated to a supergroup. New id: {chat_id}.")
+        self.migrate_to_chat_id = chat_id
