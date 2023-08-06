bed62b9d9fb9f4ad1d52cefe2d103666ffa963d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:55 +0000

    Add package esgprep/2.7.11/esgprep-2.7.11.tar.gz

---
+++ packages/esgprep/esgprep-2.7.11.tar.gz/esgprep-2.7.11/esgprep/mapfile/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    :platform: Unix
+    :synopsis: Constants used in this module.
+
+"""
+
+# Mapfile extension during processing
+WORKING_EXTENSION = '.part'
+
+# Progress bar message length
+LEN_MSG = 21
+
+# Out of version scope (OOVS) tag
+OOVS = 'oovs'
