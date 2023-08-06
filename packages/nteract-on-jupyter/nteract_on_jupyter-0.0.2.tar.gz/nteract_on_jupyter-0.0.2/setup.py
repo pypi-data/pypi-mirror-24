d886bff982d4a87fd0ee829a1b12c7c7efb87b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:42 +0000

    Add package nteract-on-jupyter/0.0.2/nteract_on_jupyter-0.0.2.tar.gz

---
+++ packages/nteract-on-jupyter/nteract_on_jupyter-0.0.2.tar.gz/nteract_on_jupyter-0.0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+from os.path import join
+
+version = '0.0.2'
+
+setuptools.setup(
+  name="nteract_on_jupyter",
+  version=version,
+  url="https://github.com/nteract/nteract",
+  author="nteract contributors",
+  author_email="jupyter@googlegroups.com",
+  description="Extension for the jupyter notebook server and nteract",
+  packages=setuptools.find_packages(),
+  include_package_data=True,
+  zip_safe=False,
+  install_requires=[]
+)
+
