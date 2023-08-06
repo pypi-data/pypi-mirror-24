b605a187afd1c0bb606b50538afe8bae7a36305c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:26 +0000

    Add package devbox-py/0.1.4/devbox-py-0.1.4.tar.gz

---
+++ packages/devbox-py/devbox-py-0.1.4.tar.gz/devbox-py-0.1.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import sys
+import os
+import subprocess
+import devbox
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        develop.run(self)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        os.system('python3 devbox/__init__.py')
+        install.run(self)
+
+setup(name='devbox-py',
+      description='DevBox for Python - Managing environments and boilerplates.',
+      version='0.1.4',
+      author='D. Misic',
+      author_email='dion@gmail.com',
+      packages=['devbox'],
+      license='MIT',
+      cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+        },
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
