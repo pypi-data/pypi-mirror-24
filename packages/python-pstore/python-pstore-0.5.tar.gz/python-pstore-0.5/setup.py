e70986b61147c9d5bbfd27a0ca783b9014e5f0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:56 +0000

    Add package python-pstore/0.5/python-pstore-0.5.tar.gz

---
+++ packages/python-pstore/python-pstore-0.5.tar.gz/python-pstore-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-pstore',
+    version='0.5',
+    packages=['pythonpstore',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=['boto3']
+)
