55c972c427c61d18df27125ff29b4e884a236f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:34 +0000

    Add package loady/1.6.3/loady-1.6.3.tar.gz

---
+++ packages/loady/loady-1.6.3.tar.gz/loady-1.6.3/loady/data.py
@@ -0,0 +1,38 @@
+import json, os, requests
+from . import raw
+
+
+def load(location, use_json=None):
+    """Return data at either a file location or at the raw version of a
+    URL, or raise an exception.
+
+    locations containing a : are URLs, otherwise they are file
+    locations.
+
+    If the URL ends in .json and json=True, convert the data from JSON."""
+    if ':' in location:
+        r = requests.get(raw.raw(location))
+        if not r.ok:
+            raise ValueError('Couldn\'t read %s with code %s:\n%s' %
+                             location, r.status_code, r.text)
+        data = r.text
+    else:
+        try:
+            f = os.path.realpath(os.path.abspath(os.path.expanduser(location)))
+            data = open(f).read()
+        except Exception as e:
+            e.args = (
+                'There was an error reading the file', location, f) + e.args
+            raise
+
+    if use_json is None:
+        use_json = location.endswith('.json')
+
+    if not use_json:
+        return data
+
+    try:
+        return json.loads(data)
+    except Exception as e:
+        e.args = ('There was a JSON error in the file', location) + e.args
+        raise
