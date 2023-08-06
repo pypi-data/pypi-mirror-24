4260896eb707056d3677e3f2e1eeecb10eb8f704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:32 +0000

    Add package sh-copy/0.0.1/sh_copy-0.0.1.tar.gz

---
+++ packages/sh-copy/sh_copy-0.0.1.tar.gz/sh_copy-0.0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='sh_copy',
+    version='0.0.1',
+    description=(
+        '<文件复制>'
+    ),
+    long_description=open('README.rst').read(),
+    author='hnf',
+    author_email='369685930@qq.com',
+    maintainer=' hnf',
+    maintainer_email=' 369685930@qq.com',
+    license='BSD License',
+    packages=find_packages(),  #  申明你的包里面要包含的目录，比如  ['mypackage', 'mypackage_test']  可以是这种使用我的示例，让setuptools自动决定要包含哪些包
+    platforms=["all"],
+    # url='<项目的网址，我一般都是github的url>',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
