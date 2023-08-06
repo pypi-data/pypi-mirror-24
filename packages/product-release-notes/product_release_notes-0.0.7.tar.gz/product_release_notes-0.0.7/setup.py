40c44fa2829a40207a0a112e7a68848b8db0ddbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:55 +0000

    Add package product-release-notes/0.0.7/product_release_notes-0.0.7.tar.gz

---
+++ packages/product-release-notes/product_release_notes-0.0.7.tar.gz/product_release_notes-0.0.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+DESCRIPTION = "Release notes page for your product."
+
+
+setup(
+    name="product_release_notes",
+    version="0.0.7",
+    url='https://github.com/nickromano/product-release-notes',
+    license='BSD',
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    author='Nick Romano',
+    author_email='nick.r.romano@gmail.com',
+    packages=find_packages(),
+    package_data={'product_release_notes': ['templates/release_notes/*.html']},
+    install_requires=[
+        'Django>=1.8',
+        'requests',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    tests_require=[
+        'mock'
+    ],
+    test_suite='testrunner.runtests'
+)
