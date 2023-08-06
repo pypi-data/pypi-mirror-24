3c1c135d0a0aeafda3462b69c0c47e0f6b7ca10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:07 +0000

    Add package libsourcemap/0.8.0/libsourcemap-0.8.0-py2.py3-none-manylinux1_i686.whl

---
+++ packages/libsourcemap/libsourcemap-0.8.0-py2.py3-none-manylinux1_i686.whl/libsourcemap/_compat.py
@@ -0,0 +1,30 @@
+import sys
+
+
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    text_type = unicode
+    xrange = xrange
+    NULL_BYTE = b'\x00'
+
+    def implements_to_string(cls):
+        cls.__unicode__ = cls.__str__
+        cls.__str__ = lambda x: unicode(x).encode('utf-8')
+        return cls
+else:
+    text_type = str
+    xrange = range
+
+    def implements_to_string(self):
+        return self
+
+    NULL_BYTE = 0
+
+
+def to_bytes(x):
+    if isinstance(x, text_type):
+        return x.encode('utf-8')
+    if not isinstance(x, bytes):
+        raise TypeError('Bytes or string expected')
+    return x
