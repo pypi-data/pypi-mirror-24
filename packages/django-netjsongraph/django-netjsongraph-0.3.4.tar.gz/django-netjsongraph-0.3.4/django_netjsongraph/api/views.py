996c30866cf6e778ef29421c7323f31815f4e1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:29 +0000

    Add package django-netjsongraph/0.3.4/django-netjsongraph-0.3.4.tar.gz

---
+++ packages/django-netjsongraph/django-netjsongraph-0.3.4.tar.gz/django-netjsongraph-0.3.4/django_netjsongraph/api/views.py
@@ -0,0 +1,20 @@
+from ..models import Topology
+from .generics import (BaseNetworkCollectionView, BaseNetworkGraphView,
+                       BaseReceiveTopologyView)
+
+
+class NetworkCollectionView(BaseNetworkCollectionView):
+    queryset = Topology.objects.filter(published=True)
+
+
+class NetworkGraphView(BaseNetworkGraphView):
+    queryset = Topology.objects.filter(published=True)
+
+
+class ReceiveTopologyView(BaseReceiveTopologyView):
+    model = Topology
+
+
+network_collection = NetworkCollectionView.as_view()
+network_graph = NetworkGraphView.as_view()
+receive_topology = ReceiveTopologyView.as_view()
