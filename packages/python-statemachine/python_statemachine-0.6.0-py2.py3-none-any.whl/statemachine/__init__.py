66cf6fa5ace9e6f9c7afc17c79ff41b0e744a2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:40 +0000

    Add package python-statemachine/0.6.0/python_statemachine-0.6.0-py2.py3-none-any.whl

---
+++ packages/python-statemachine/python_statemachine-0.6.0-py2.py3-none-any.whl/statemachine/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+from .statemachine import StateMachine, State, Transition
+
+__author__ = """Fernando Macedo"""
+__email__ = 'fgmacedo@gmail.com'
+__version__ = '0.6.0'
