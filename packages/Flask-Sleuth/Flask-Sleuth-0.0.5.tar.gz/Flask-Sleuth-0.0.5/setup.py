9570c956c4beb68ed084aadc0b0edee7cc4782c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:45 +0000

    Add package Flask-Sleuth/0.0.5/Flask-Sleuth-0.0.5.tar.gz

---
+++ packages/Flask-Sleuth/Flask-Sleuth-0.0.5.tar.gz/Flask-Sleuth-0.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import unittest
+
+
+def test_suite():
+    loader = unittest.TestLoader()
+    suite = loader.discover('tests', pattern='test_*.py')
+    return suite
+
+
+setup(name='Flask-Sleuth',
+      version='0.0.5',
+      description='Spring Cloud Sleuth logging implementation for Python 2/3.',
+      author='David Carboni',
+      author_email='david@carboni.io',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Topic :: System :: Logging',
+          'Topic :: Internet :: Log Analysis',
+          'Intended Audience :: Developers',
+          'Framework :: Flask',
+          'License :: OSI Approved :: MIT License',
+      ],
+      keywords=['logging', 'b3', 'distributed', 'tracing', 'spring', 'boot', 'cloud', 'sleuth'],
+      url='https://github.com/davidcarboni/flask_sleuth',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'flask',
+      ],
+      test_suite='setup.test_suite',
+      include_package_data=True,
+      zip_safe=True,
+      )
