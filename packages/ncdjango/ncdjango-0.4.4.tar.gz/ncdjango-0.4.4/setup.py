3d6838f388f8b8786fbb446da47a74f4dc8004b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:29 +0000

    Add package ncdjango/0.4.4/ncdjango-0.4.4.tar.gz

---
+++ packages/ncdjango/ncdjango-0.4.4.tar.gz/ncdjango-0.4.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='ncdjango',
+    description='A map server for NetCDF data',
+    keywords='netcdf,django,map server',
+    version='0.4.4',
+    packages=[
+        'ncdjango', 'ncdjango.geoprocessing', 'ncdjango.geoprocessing.tasks', 'ncdjango.migrations',
+        'ncdjango.interfaces', 'ncdjango.interfaces.arcgis', 'ncdjango.interfaces.arcgis_extended',
+        'ncdjango.interfaces.data'
+    ],
+    install_requires=[
+        'six', 'requests', 'Django>=1.7.0,<1.9.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'django-tastypie>=0.11.1',
+        'netCDF4>=1.1.6', 'numpy>=1.8.1', 'pyproj>=1.9.4', 'fiona', 'rasterio>=0.28.0', 'clover', 'ply>=3.8',
+        'celery>=3.1.19', 'djangorestframework'
+    ],
+    dependency_links=[
+        'git+https://github.com/consbio/clover.git'
+    ],
+    url='https://github.com/consbio/ncdjango',
+    license='BSD',
+)
