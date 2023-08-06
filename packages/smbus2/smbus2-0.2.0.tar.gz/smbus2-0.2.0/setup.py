015c83d20bb80a6ba7fa0817357ba224e0a94998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:39 +0000

    Add package smbus2/0.2.0/smbus2-0.2.0.tar.gz

---
+++ packages/smbus2/smbus2-0.2.0.tar.gz/smbus2-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os import path
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), 'r') as f:
+    readme = f.read()
+
+setup(
+    name="smbus2",
+    version="0.2.0",
+    author="Karl-Petter Lindegaard",
+    author_email="kp.lindegaard@gmail.com",
+    description="smbus2 is a drop-in replacement for smbus-cffi/smbus-python in pure Python",
+    license="MIT",
+    keywords=['smbus', 'smbus2', 'python', 'i2c', 'raspberrypi', 'linux'],
+    url="https://github.com/kplindegaard/smbus2",
+    packages=['smbus2'],
+    long_description=readme,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+)
