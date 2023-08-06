ff87da4ee32d8a6f201575c797432d65c061d1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:42 +0000

    Add package spyder-reports/0.1.0/spyder-reports-0.1.0.tar.gz

---
+++ packages/spyder-reports/spyder-reports-0.1.0.tar.gz/spyder-reports-0.1.0/setup.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Setup script for spyder_reports."""
+
+# Standard library imports
+import ast
+import os
+
+# Third party imports
+from setuptools import find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(module='spyder_reports'):
+    """Get version."""
+    with open(os.path.join(HERE, module, '_version.py'), 'r') as f:
+        data = f.read()
+    lines = data.split('\n')
+    for line in lines:
+        if line.startswith('VERSION_INFO'):
+            version_tuple = ast.literal_eval(line.split('=')[-1].strip())
+            version = '.'.join(map(str, version_tuple))
+            break
+    return version
+
+
+def get_description():
+    """Get long description."""
+    with open(os.path.join(HERE, 'README.rst'), 'r') as f:
+        data = f.read()
+    return data
+
+
+REQUIREMENTS = ['spyder>=3.2.0', 'pweave']
+
+
+setup(
+    name='spyder-reports',
+    version=get_version(),
+    keywords=['Spyder', 'Plugin'],
+    url='https://github.com/spyder-ide/spyder-ide',
+    license='MIT',
+    author='Spyder Project Contributors',
+    author_email='admin@spyder-ide.org',
+    description='Spyder-IDE plugin for Markdown reports using Pweave.',
+    long_description=get_description(),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    package_data={'spyder_reports.utils': ['*.md']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ])
