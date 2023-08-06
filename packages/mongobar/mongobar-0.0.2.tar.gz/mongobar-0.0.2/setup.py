4f3a85205151ac532f6575ebcfc103e9a9f70b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:08 +0000

    Add package mongobar/0.0.2/mongobar-0.0.2.tar.gz

---
+++ packages/mongobar/mongobar-0.0.2.tar.gz/mongobar-0.0.2/setup.py
@@ -0,0 +1,85 @@
+import io
+import os
+import sys
+import shutil
+import subprocess
+import setuptools
+import mongobar
+
+
+class PublishCommand(setuptools.Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            shutil.rmtree(os.path.join(here, 'dist'))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setuptools.setup(
+    name="mongobar",
+    version="0.0.2",
+    description="MongoDB Backup and Restore Manager",
+    keywords="mongodb backup restore mongodump mongorestore",
+    url="https://github.com/chrisantonellis/mongobar",
+
+    author="Christopher Antonellis",
+    author_email="christopher.antonellis@gmail.com",
+    license="MIT",
+    packages=[
+        "mongobar"
+    ],
+    # https://stackoverflow.com/a/5899643/1671562
+    package_data={
+        "mongobar": [
+            "data/nouns.txt",
+            "data/verbs.txt"
+        ]
+    },
+    scripts=[
+        "mongobar/scripts/mongobar"
+    ],
+    install_requires=[
+        "pymongo",
+        "colorama",
+        "argcomplete",
+        "terminaltables",
+        "python-dateutil"
+    ],
+    extras_require={
+        "tests": [
+            "green",
+            "coverage"
+        ],
+    },
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand
+    }
+)
