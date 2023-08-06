4088ec191d7f9c46c32f8a95c2879d8687d385a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:10 +0000

    Add package flake8-graphql/0.2.3/flake8-graphql-0.2.3.tar.gz

---
+++ packages/flake8-graphql/flake8-graphql-0.2.3.tar.gz/flake8-graphql-0.2.3/flake8_graphql.py
@@ -0,0 +1,46 @@
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import ast
+
+from graphql import Source, parse
+from graphql.error import GraphQLError
+
+
+class GraphQLChecker(object):
+    """
+    flake8 plugin that lints your graphql strings
+    """
+    name = 'flake8-graphql'
+    version = '0.2.3'
+
+    def __init__(self, tree, *args, **kwargs):
+        self.tree = tree
+
+    @classmethod
+    def add_options(cls, parser):
+        parser.add_option(
+            '--gql-identifier',
+            type=str,
+            default='gql',
+            help='Name of function or class to identify GraphQL strings with',
+            parse_from_config=True,
+        )
+
+    @classmethod
+    def parse_options(cls, options):
+        cls.gql_identifier = options.gql_identifier
+
+    def run(self):
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.Call) and getattr(node.func, 'id', None) == self.gql_identifier:
+                first_arg = node.args[0]
+
+                if isinstance(first_arg, ast.Str):
+                    query = first_arg.s
+                    try:
+                        Source(query)
+                        parse(query)
+                    except GraphQLError as e:
+                        lines = e.message.split('\n')
+                        yield (node.lineno, node.col_offset, 'GQL100 ' + lines[0], type(self))
