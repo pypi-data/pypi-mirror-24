1f30bdef96416a5e3feadaae72ecf20dd953d394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:36 +0000

    Add package ieml/0.2.37/ieml-0.2.37.tar.gz

---
+++ packages/ieml/ieml-0.2.37.tar.gz/ieml-0.2.37/ieml/syntax/__init__.py
@@ -0,0 +1,5 @@
+from .words import Morpheme, Word
+from .sentences import Clause, Sentence, SuperClause, SuperSentence
+from .texts import Text
+from .hypertexts import Hyperlink, Hypertext, PropositionPath
+from .parser import IEMLParser
