e210b6b42d77b6076a138bb6ecb9e30449abf33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:59 +0000

    Add package wyqpython/0.1.5/wyqpython-0.1.5-py2.py3-none-any.whl

---
+++ packages/wyqpython/wyqpython-0.1.5-py2.py3-none-any.whl/wyqpython/base/_path.py
@@ -0,0 +1,30 @@
+'''
+Created on 2017年7月12日
+
+@author: WYQ
+'''
+
+
+def get_desktop():
+    '''
+    Get Desktop path
+    '''
+    import winreg
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
+    return winreg.QueryValueEx(key, "Desktop")[0]
+
+
+def get_home():
+    '''
+    Get home path
+    '''
+    import os
+    return os.getenv('USERPROFILE', '')
+
+
+def get_home_desktop():
+    '''
+    Get Desktop path
+    '''
+    import os
+    return os.getenv('USERPROFILE', '') + '\Desktop'
---
+++ packages/wyqpython/wyqpython-0.1.5-py2.py3-none-any.whl/wyqpython/io/_directory.py
@@ -0,0 +1,5 @@
+'''
+Created on 2017年7月12日
+
+@author: WYQ
+'''
