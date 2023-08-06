0b987e19debe6ae0abd11a0744d07f3e587166b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:30 +0000

    Add package python-smartqq-client/0.2.4/python-smartqq-client-0.2.4.tar.gz

---
+++ packages/python-smartqq-client/python-smartqq-client-0.2.4.tar.gz/python-smartqq-client-0.2.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+version = "0.2.4"
+setup(
+    name="python-smartqq-client",
+    version=version,
+    description="A simple smartqq client implemented with python to be used as component in other projects",
+    author="Extremezhazha",
+    author_email="extremezhazha@gmail.com",
+    license="MIT",
+    keywords="python smartqq",
+    install_requires=["requests", "pymongo"],
+    packages=["pyclient"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
