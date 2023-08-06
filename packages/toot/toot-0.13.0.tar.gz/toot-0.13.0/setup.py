dbb7b697e0611a491f7d0930157adbef3062f793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:14 +0000

    Add package toot/0.13.0/toot-0.13.0.tar.gz

---
+++ packages/toot/toot-0.13.0.tar.gz/toot-0.13.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.rst") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt") as f:
+    install_requires = f.read().split()
+
+setup(
+    name='toot',
+    version='0.13.0',
+    description='Interact with Mastodon social networks from the command line.',
+    long_description=long_description,
+    author='Ivan Habunek',
+    author_email='ivan@habunek.com',
+    url='https://github.com/ihabunek/toot/',
+    keywords='mastodon toot',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['toot'],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'toot=toot.console:main',
+        ],
+    }
+)
