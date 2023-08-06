9bbccfdc01eccf9ec92ced60c018d8ce5e0133d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:45 +0000

    Add package grammpy/1.1.11/grammpy-1.1.11.tar.gz

---
+++ packages/grammpy/grammpy-1.1.11.tar.gz/grammpy-1.1.11/grammpy/Grammars/PrettyApiGrammar.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 01.08.2017 07:33
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .MultipleRulesGrammar import MultipleRulesGrammar
+
+
+class PrettyApiGrammar(MultipleRulesGrammar):
+    def __init__(self,
+                 terminals=None,
+                 nonterminals=None,
+                 rules=None,
+                 start_symbol=None):
+        if isinstance(terminals, str):
+            temp = []
+            for ch in terminals:
+                temp.append(ch)
+            terminals = temp
+        super().__init__(terminals=terminals,
+                         nonterminals=nonterminals,
+                         rules=rules,
+                         start_symbol=start_symbol)
---
+++ packages/grammpy/grammpy-1.1.11.tar.gz/grammpy-1.1.11/grammpy/Nonterminal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 28.07.2017 09:14
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+
+class Nonterminal:
+    pass
