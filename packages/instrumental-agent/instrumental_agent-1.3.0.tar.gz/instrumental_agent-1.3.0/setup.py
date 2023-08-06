86fa138aa718c56404d54ad8b2efc38e87466337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:07 +0000

    Add package instrumental-agent/1.3.0/instrumental_agent-1.3.0.tar.gz

---
+++ packages/instrumental-agent/instrumental_agent-1.3.0.tar.gz/instrumental_agent-1.3.0/setup.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "instrumental_agent"
+VERSION = "1.3.0"
+
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = []
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="A wrapper for the Instrumental service.",
+    author="Expected Behavior",
+    author_email="support@instrumentalapp.com",
+    maintainer="Instrumental Support",
+    maintainer_email="support@instrumentalapp.com",
+    url="https://github.com/instrumental/instrumental_agent-python",
+    keywords=["Instrumental", "API", "Metrics", "APM"],
+    classifiers=[ # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+
+        "Environment :: Web Environment",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+    ],
+    license="",
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    A native client library for the Instrumental service.
+    """
+)
