d41703e60fcf188ee27d4404a943135112c9f776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:56 +0000

    Add package yamaha-av/0.9.0/yamaha_av-0.9.0.tar.gz

---
+++ packages/yamaha-av/yamaha_av-0.9.0.tar.gz/yamaha_av-0.9.0/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+setup(
+    name="yamaha_av",
+    version="0.9.0",
+    author="Anthony Casagrande",
+    author_email="birdapi@gmail.com",
+    description=("Control Yamaha Receivers over the network"),
+    license="MIT",
+    keywords="yamaha av rxv network receivers birdapi",
+    url="https://pypi.python.org/pypi/yamaha_av",
+    packages=['yamaha_av'],
+    install_requires=[
+        "six>=1.10.0,<1.11.0"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Home Automation",
+        "Topic :: System :: Networking",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
