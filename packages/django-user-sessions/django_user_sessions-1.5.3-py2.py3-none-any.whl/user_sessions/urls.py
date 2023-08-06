bca43209edbd61611efc04ef81ff842c23395e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:27 +0000

    Add package django-user-sessions/1.5.3/django_user_sessions-1.5.3-py2.py3-none-any.whl

---
+++ packages/django-user-sessions/django_user_sessions-1.5.3-py2.py3-none-any.whl/user_sessions/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import url
+from user_sessions.views import SessionDeleteOtherView
+
+from .views import SessionListView, SessionDeleteView
+
+
+app_name = 'user_sessions'
+urlpatterns = [
+    url(
+        regex=r'^account/sessions/$',
+        view=SessionListView.as_view(),
+        name='session_list',
+    ),
+    url(
+        regex=r'^account/sessions/other/delete/$',
+        view=SessionDeleteOtherView.as_view(),
+        name='session_delete_other',
+    ),
+    url(
+        regex=r'^account/sessions/(?P<pk>\w+)/delete/$',
+        view=SessionDeleteView.as_view(),
+        name='session_delete',
+    ),
+]
