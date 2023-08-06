ee535bffcc763b116f9c8ce7cb438f9dba6da289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:08 +0000

    Add package fmapi/0.1/fmapi-0.1.tar.gz

---
+++ packages/fmapi/fmapi-0.1.tar.gz/fmapi-0.1/setup.py
@@ -0,0 +1,12 @@
+"Package Setup"
+from setuptools import setup
+
+setup(name='fmapi',
+      version='0.1',
+      description='Foundation Mercury Python API Client',
+      url='https://github.com/FoundationMercury/fmpyapi',
+      author='Foundation Mercury',
+      author_email='info@foundationmercury.com',
+      license='MIT',
+      packages=['fmapi'],
+      zip_safe=False)
