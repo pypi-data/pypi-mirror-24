13c375f64a3c9927b87a8c2b3ff00e34d09a23b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:31 +0000

    Add package modoboa-stats/1.2.0/modoboa-stats-1.2.0.tar.gz

---
+++ packages/modoboa-stats/modoboa-stats-1.2.0.tar.gz/modoboa-stats-1.2.0/modoboa_stats/forms.py
@@ -0,0 +1,37 @@
+"""Modoboa stats forms."""
+
+from django.utils.translation import ugettext_lazy
+from django import forms
+
+from modoboa.lib import form_utils
+from modoboa.parameters import forms as param_forms
+
+
+class ParametersForm(param_forms.AdminParametersForm):
+    """Stats global parameters."""
+
+    app = "modoboa_stats"
+
+    general_sep = form_utils.SeparatorField(label=ugettext_lazy("General"))
+
+    logfile = forms.CharField(
+        label=ugettext_lazy("Path to the log file"),
+        initial="/var/log/mail.log",
+        help_text=ugettext_lazy("Path to log file used to collect statistics"),
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+
+    rrd_rootdir = forms.CharField(
+        label=ugettext_lazy("Directory to store RRD files"),
+        initial="/tmp/modoboa",
+        help_text=ugettext_lazy(
+            "Path to directory where RRD files are stored"),
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+
+    greylist = form_utils.YesNoField(
+        label=ugettext_lazy("Show greylisted messages"),
+        initial=False,
+        help_text=ugettext_lazy(
+            "Differentiate between hard and soft rejects (greylisting)")
+    )
