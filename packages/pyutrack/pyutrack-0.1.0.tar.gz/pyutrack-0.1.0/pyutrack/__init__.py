def5f5d2d964c717f38f1cf45e7e7310ba07b379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:59 +0000

    Add package pyutrack/0.1.0/pyutrack-0.1.0.tar.gz

---
+++ packages/pyutrack/pyutrack-0.1.0.tar.gz/pyutrack-0.1.0/pyutrack/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for YouTrack CLI."""
+
+__author__ = """Ali-Akber Saifee"""
+__email__ = 'ali@indydevs.org'
+__version__ = '0.1.0'
+
+from pyutrack.connection import Connection, Credentials
+from pyutrack.resources import *
