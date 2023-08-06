4c58544e707e402a311369c8016938c0a7e02ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:32 +0000

    Add package Telethon/0.13/Telethon-0.13.tar.gz

---
+++ packages/Telethon/Telethon-0.13.tar.gz/Telethon-0.13/telethon/tl/types/help/__init__.py
@@ -0,0 +1,6 @@
+from .config_simple import ConfigSimple
+from .app_update import AppUpdate
+from .no_app_update import NoAppUpdate
+from .invite_text import InviteText
+from .support import Support
+from .terms_of_service import TermsOfService
