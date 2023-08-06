e5b4daa7dacc8c59737b1b4644efc936e24c7919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:12 +0000

    Add package skylinesms/1.0/skylinesms-1.0.tar.gz

---
+++ packages/skylinesms/skylinesms-1.0.tar.gz/skylinesms-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='skylinesms',
+    version='1.0',
+    description='A module to send sms using the Skyline SMS REST apis, www.skylinesms.com',
+    author='Abdu Ssekalala',
+    author_email='assekalala@gmail.com',
+    url='https://github.com/assekalala/python-skyline-sms.git',
+    keywords=['sms', 'skyline', 'skylinesms'],
+    license='MIT',
+    classifiers = [],
+)
