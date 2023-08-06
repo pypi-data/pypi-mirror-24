e05227590f5baa94a112838376ceff94dc592602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:48 +0000

    Add package wsgi-lineprof/0.4.0/wsgi_lineprof-0.4.0-cp27-cp27m-manylinux1_i686.whl

---
+++ packages/wsgi-lineprof/wsgi_lineprof-0.4.0-cp27-cp27m-manylinux1_i686.whl/wsgi_lineprof/middleware.py
@@ -0,0 +1,34 @@
+from io import TextIOWrapper  # noqa: F401
+import sys
+from typing import Callable, Iterable, TextIO, Union  # noqa: F401
+
+from wsgi_lineprof.profiler import LineProfiler
+from wsgi_lineprof.stats import FilterType  # noqa: F401
+
+
+class LineProfilerMiddleware(object):
+    def __init__(self,
+                 app,  # type: Callable
+                 stream=None,  # type: Union[TextIO, TextIOWrapper]
+                 filters=tuple()  # type: Iterable[FilterType]
+                 ):
+        # type: (...) -> None
+        self.app = app
+        self.stream = sys.stdout if stream is None else stream
+        self.filters = filters
+
+    def __call__(self, env, start_response):
+        profiler = LineProfiler()
+
+        profiler.enable()
+        result = self.app(env, start_response)
+        profiler.disable()
+
+        stats = profiler.get_stats()
+
+        for f in self.filters:
+            stats = stats.filter(f)
+
+        stats.write_text(self.stream)
+
+        return result
