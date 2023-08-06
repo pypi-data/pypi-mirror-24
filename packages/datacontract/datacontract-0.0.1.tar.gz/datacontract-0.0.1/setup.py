1e47b936b47fafac436119000ac5018d8efd89db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:24 +0000

    Add package datacontract/0.0.1/datacontract-0.0.1.tar.gz

---
+++ packages/datacontract/datacontract-0.0.1.tar.gz/datacontract-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='datacontract',
+    version='0.0.1',
+    packages=['datacontract'],
+    url='',
+    license='MIT',
+    description='DataContract',
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
