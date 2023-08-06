fbea28770f0c2d19937e4ce0d6bc4fd2a90edb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:18 +0000

    Add package rnaseq-lib/1.0a10/rnaseq-lib-1.0a10.tar.gz

---
+++ packages/rnaseq-lib/rnaseq-lib-1.0a10.tar.gz/rnaseq-lib-1.0a10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='rnaseq-lib',
+      version='1.0a10',
+      description='',
+      url='http://github.com/jvivian/rnaseq-lib',
+      author='John Vivian',
+      author_email='jtvivian@gmail.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      package_data={'rnaseq_lib.utils.data': ['samples.pickle',
+                                              'UCSF-genes.csv',
+                                              'cancer-MAB-gene-targets.txt',
+                                              'gene_map.pickle']},
+      install_requires=['pandas',
+                        'numpy',
+                        'seaborn',
+                        'requests'])
