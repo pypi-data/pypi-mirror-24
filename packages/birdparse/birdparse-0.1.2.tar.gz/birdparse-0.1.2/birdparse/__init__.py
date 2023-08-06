f547003019968e876103ce305bc17301572169a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:37 +0000

    Add package birdparse/0.1.2/birdparse-0.1.2.tar.gz

---
+++ packages/birdparse/birdparse-0.1.2.tar.gz/birdparse-0.1.2/birdparse/__init__.py
@@ -0,0 +1,32 @@
+import birdjson
+import birdyaml
+
+
+class ParseException(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+
+def load_file(filename):
+    if filename.endswith('.yml') or filename.endswith('.yaml'):
+        return birdyaml.load_file(filename)
+    elif filename.endswith('.json'):
+        return birdjson.load_file(filename)
+    else:
+        raise ParseException('unknown file format')
+
+
+def load_to_json(filename):
+    return to_json(load_file(filename))
+
+
+def load_to_yaml(filename):
+    return to_yaml(load_file(filename))
+
+
+def to_yaml(json_object):
+    return birdyaml.YamlObject(**json_object.__items__())
+
+
+def to_json(yaml_object):
+    return birdjson.JsonObject(**yaml_object.__items__())
