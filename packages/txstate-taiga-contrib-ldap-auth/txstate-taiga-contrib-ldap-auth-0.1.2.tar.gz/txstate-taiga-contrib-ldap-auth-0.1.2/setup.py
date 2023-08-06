46a2f6a21d207d6f0b55ecd53a1e669db4cc6091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:53 +0000

    Add package txstate-taiga-contrib-ldap-auth/0.1.2/txstate-taiga-contrib-ldap-auth-0.1.2.tar.gz

---
+++ packages/txstate-taiga-contrib-ldap-auth/txstate-taiga-contrib-ldap-auth-0.1.2.tar.gz/txstate-taiga-contrib-ldap-auth-0.1.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = 'txstate-taiga-contrib-ldap-auth',
+    version = "0.1.2",
+    description = "TxState adaptation of the Taiga plugin for ldap authentication",
+    long_description = "",
+    keywords = 'txstate taiga',
+    author = 'Andrew Thyng',
+    author_email = 'ajt79@txstate.edu',
+    url = 'https://github.com/txstate-etc/txstate-taiga-contrib-ldap-auth/tree/master/txstate_taiga_contrib_ldap_auth',
+    license = 'AGPL',
+    include_package_data = True,
+    packages = ['txstate_taiga_contrib_ldap_auth'],
+    install_requires=[
+        'django >= 1.7',
+		'ldap3 >= 0.9.8.4'
+    ],
+    setup_requires = [
+        'versiontools >= 1.8',
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
