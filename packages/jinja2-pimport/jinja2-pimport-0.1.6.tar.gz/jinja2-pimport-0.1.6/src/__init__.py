7d438b3bfd060293bef41a452cfdd84feb93fc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:53 +0000

    Add package jinja2-pimport/0.1.6/jinja2-pimport-0.1.6.tar.gz

---
+++ packages/jinja2-pimport/jinja2-pimport-0.1.6.tar.gz/jinja2-pimport-0.1.6/src/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .pimportextension import PImportExtension
+
+__author__ = 'Hong-She Liang'
+__email__ = 'starofrainnight@gmail.com'
+__version__ = '0.1.6'
+
+__all__ = ['PImportExtension']
