c9a6c8975e9be1802c08eb45554d991d65561939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:07 +0000

    Add package brokerpackager/0.0.4/brokerpackager-0.0.4.tar.gz

---
+++ packages/brokerpackager/brokerpackager-0.0.4.tar.gz/brokerpackager-0.0.4/brokerpackager/installer.py
@@ -0,0 +1,13 @@
+from brokerpackager.python.manager import PyManager
+from brokerpackager.r.manager import RManager
+
+class Installer(object):
+    def __init__(self, config={}):
+        python_log_file = config.get('python', {}).get('log_file', '')
+        r_log_file = config.get('r', {}).get('log_file', '')
+        self.managers = {'python': PyManager(python_log_file), 'r': RManager(r_log_file)}
+
+    def install(self, config):
+        for manager_name in self.managers:
+            manager = self.managers[manager_name]
+            manager.install_list(config.get(manager_name, {}).get('paths', []), config.get(manager_name, {}))
