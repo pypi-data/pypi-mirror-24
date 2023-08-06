053e66e06e1d6ceddf2e5ee5f40e3538ffda3dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:00 +0000

    Add package markdocs/0.0.0/markdocs-0.0.0-py3-none-any.whl

---
+++ packages/markdocs/markdocs-0.0.0-py3-none-any.whl/markdocs/__init__.py
@@ -0,0 +1,15 @@
+"""Markdocs - Python documentation using markdown"""
+__version__ = '0.0.0'
+
+"""!
+# Why Markdocs
+
+- Markdown is easier to learn
+- Github README file is mostly writen in md format
+- Majority of projects has simple documentation needs (the complex should use .rst)
+
+## Lets make it happen
+
+> markdocs :)
+"""
+from .cli import main  # noqa
