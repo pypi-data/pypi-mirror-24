a147730e82f2a20b8045f36c5faaa5929deaef47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:10 +0000

    Add package pyconrad/0.0.6.0/pyconrad-0.0.6.0.tar.gz

---
+++ packages/pyconrad/pyconrad-0.0.6.0.tar.gz/pyconrad-0.0.6.0/pyconrad/_imageutils.py
@@ -0,0 +1,36 @@
+
+# Copyright (C) 2010-2017 - Andreas Maier
+# CONRAD is developed as an Open Source project under the GNU General Public License (GPL-3.0)
+
+from ._pyconrad import PyConrad
+from ._pygrid import PyGrid
+
+class ImageUtil:
+
+    #################
+    ## Save Images ##
+    #################
+
+    @staticmethod
+    def save_grid_as_tiff(grid, path):
+        PyConrad.get_instance().classes.stanford.rsl.conrad.utils.ImageUtil.saveAs(grid, path)
+
+    @staticmethod
+    def save_numpy_as_tiff(array, path):
+        grid = ImageUtil.grid_from_numpy(array)
+        PyConrad.get_instance().classes.stanford.rsl.conrad.utils.ImageUtil.saveAs(grid, path)
+
+    #################
+    ## Load Images ##
+    #################
+
+    @staticmethod
+    def grid_from_tiff(path):
+        grid = PyConrad.get_instance().classes.stanford.rsl.conrad.utils.ImageUtil.wrapImagePlus(PyConrad.get_instance().ij.openImage(path))
+        return grid
+
+    @staticmethod
+    def array_from_tiff(path):
+        grid = PyConrad.get_instance().classes.stanford.rsl.conrad.utils.ImageUtil.wrapImagePlus(PyConrad.get_instance().ij.openImage(path))
+        return ImageUtil.numpy_from_grid(grid)
+
