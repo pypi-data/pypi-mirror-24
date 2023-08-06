79f28a2b6ec188f194a8ba7b7921a51b5612520e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:39 +0000

    Add package snnm/0.2.3/snnm-0.2.3.tar.gz

---
+++ packages/snnm/snnm-0.2.3.tar.gz/snnm-0.2.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+requirements = convert_deps_to_pip(Project().parsed_pipfile['packages'], r=False)
+
+setup(name='snnm',
+      version='0.2.3',
+      author='Gabriel Lima',
+      author_email='gvclima@gmail.com',
+      description='Naming helper',
+      license='MIT',
+      keywords='synonym',
+      url='https://github.com/ewilazarus/snnm',
+      download_url='https://github.com/ewilazarus/snnm/tarball/0.2.3',
+      setup_requires='pipenv',
+      install_requires=requirements,
+      entry_points={
+          'console_scripts': ['snnm=snnm:main']
+      })
