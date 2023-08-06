1da1ffe5867b0bbe6e6c4799746e99361ece0728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:53 +0000

    Add package django-mongoengine-forms/0.4.4/django_mongoengine_forms-0.4.4-py2-none-any.whl

---
+++ packages/django-mongoengine-forms/django_mongoengine_forms-0.4.4-py2-none-any.whl/tests/test_document.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from mongoengine import Document, fields
+from mongodbforms import DocumentForm
+import unittest
+
+
+class MyDocument(Document):
+    mystring = fields.StringField()
+    myverbosestring = fields.StringField(verbose_name="Foobar")
+    myrequiredstring = fields.StringField(required=True)
+
+
+class MyForm(DocumentForm):
+    class Meta:
+        document = MyDocument
+
+
+class SimpleDocumentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        settings.configure()
+
+    def test_form(self):
+        form = MyForm()
+        self.assertEquals(len(form.fields), 3)
+        self.assertFalse(form.fields['mystring'].required)
+        self.assertEquals(form.fields['myverbosestring'].label, "Foobar")
+        self.assertTrue(form.fields['myrequiredstring'].required)
