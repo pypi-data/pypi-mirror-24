0cb0bcc398739fbf8d677db9e9d50a73e7a4b3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:27 +0000

    Add package quantum-grove/1.1.0/quantum-grove-1.1.0.tar.gz

---
+++ packages/quantum-grove/quantum-grove-1.1.0.tar.gz/quantum-grove-1.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+##############################################################################
+# Copyright 2016-2017 Rigetti Computing
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################
+
+from setuptools import setup
+
+setup(
+    name="quantum-grove",
+    version="1.1.0",
+    author="Rigetti Computing",
+    author_email="softapps@rigetti.com",
+    description="A collection of quantum algorithms built using pyQuil and Forest",
+    url="https://github.com/rigetticomputing/grove.git",
+    packages=["grove", "grove.pyqaoa", "grove.pyvqe", "grove.qft",
+              "grove.phaseestimation", "grove.teleport",
+              "grove.deutsch_jozsa", "grove.arbitrary_state",
+              "grove.bernstein_vazirani", "grove.simon",
+              "grove.amplification"],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pyquil >= 1.0.0',
+        'mock',
+        'networkx',
+        'matplotlib'
+    ],
+    license='LICENSE',
+    keywords='quantum quil programming hybrid'
+)
