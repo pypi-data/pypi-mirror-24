0c10f673dce960ab2d16c4ccf3aa2a888b926130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:05 +0000

    Add package shellstorm/0.0.0.dev0/shellstorm-0.0.0.dev0.tar.gz

---
+++ packages/shellstorm/shellstorm-0.0.0.dev0.tar.gz/shellstorm-0.0.0.dev0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='shellstorm',
+    version='0.0.0.dev0',
+    description='Terminals over websockets with tornado.',
+    author='ASI Data Science',
+    author_email='engineering@asidatascience.com',
+    url='https://www.asidatascience.com/',
+    packages=find_packages()
+)
