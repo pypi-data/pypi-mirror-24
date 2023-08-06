c5d5fdf532584521155610abbc7123298f1bc6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:01 +0000

    Add package pibake/0.2.6/pibake-0.2.6-py3-none-any.whl

---
+++ packages/pibake/pibake-0.2.6-py3-none-any.whl/pibake/settings.py
@@ -0,0 +1,16 @@
+import os
+from xdg.BaseDirectory import xdg_cache_home
+
+NAME = 'pibake'
+
+__version__ = '0.2.6'
+
+IMAGE_SOURCE_LOOKUP = {
+    'NOOBS': os.getenv('PIBAKE_NOOBS_LATEST_URL', 'https://downloads.raspberrypi.org/NOOBS_latest'),
+    'NOOBS_LITE': os.getenv('PIBAKE_NOOBS_LITE_LATEST_URL', 'https://downloads.raspberrypi.org/NOOBS_lite_latest'),
+    'RASPIAN': 'https://downloads.raspberrypi.org/raspbian_latest',
+    'RASPIAN_LITE': 'https://downloads.raspberrypi.org/raspbian_latest',
+}
+
+# where to store the downloaded images
+CACHE = os.getenv('PIBAKE_CACHE', os.path.join(xdg_cache_home, NAME))
