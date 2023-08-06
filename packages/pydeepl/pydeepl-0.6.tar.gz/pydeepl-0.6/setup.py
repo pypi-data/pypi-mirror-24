4443c21feb572ac8c9c540b1f9fcf900183f56f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:24 +0000

    Add package pydeepl/0.6/pydeepl-0.6.tar.gz

---
+++ packages/pydeepl/pydeepl-0.6.tar.gz/pydeepl-0.6/setup.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert(source='README.md', format='markdown_github', to='rst', outputfile='README.rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='pydeepl',
+    packages=['pydeepl'],
+    version='0.6',
+    description='A python API wrapper for deepl.com translating service.',
+    long_description=long_description,
+    author='Emilio Carrión Peñalba',
+    author_email='emiliok1997@gmail.com',
+    url='https://github.com/EmilioK97/pydeepl',
+    keywords=['deepl', 'API', 'translate', 'translation'],
+    classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+      'requests>=2.18.4',
+    ],
+)
