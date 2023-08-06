26e661a89c555cea121a0350e6431dba481a2ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:44 +0000

    Add package django-templateselector/0.2.4/django_templateselector-0.2.4-py2.py3-none-any.whl

---
+++ packages/django-templateselector/django_templateselector-0.2.4-py2.py3-none-any.whl/templateselector/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+__version_info__ = '0.2.4'
+__version__ = '0.2.4'
+version = '0.2.4'
+VERSION = '0.2.4'
+
+def get_version():
+    return version  # pragma: no cover
