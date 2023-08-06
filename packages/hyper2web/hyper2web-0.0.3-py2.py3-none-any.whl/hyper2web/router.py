130c07087a73e32f81c8224520506ccf30e6c448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package hyper2web/0.0.3/hyper2web-0.0.3-py2.py3-none-any.whl

---
+++ packages/hyper2web/hyper2web-0.0.3-py2.py3-none-any.whl/hyper2web/router.py
@@ -0,0 +1,88 @@
+from .abstract import AbstractRouter
+from .exceptions import RouteNotRegisteredException
+from .http import HTTP, Stream, Request, Response
+
+
+class Router(AbstractRouter):
+	"""User should never construct Router"""
+
+	# todo: I may want to change the constructor
+	def __init__(self, default_get):
+		"""
+		:param default_get: should be a handler function
+		"""
+		self._routes = {
+			'GET': {},
+			'POST': {}
+		}
+		self.default_get = default_get
+
+	def register(self, method: str, route: str, handler):
+		assert method in ['GET', 'POST']  # 这只是目前为了测试而加的限制
+		self._routes[method][route] = handler
+
+	def find_match(self, path: str):
+		"""
+		'user/{userId}' should match 'user/abc'
+		userId = abc
+		return a tuple (matched, parameters)
+		matched is the route which matches the incoming path
+		parameters is a dict of parameters and their values
+		"""
+		# todo: now the problem is how to implement it
+		# todo: pattern matching should be independent from :method,
+		# todo: but the current implementation doesn't support it. Should improve it later.
+
+		for routes_of_this_method in self._routes.values():
+			for route in routes_of_this_method:
+				matched, parameters = self._match(route, path)
+				# this function returns the first match, not the best match
+				if matched:
+					return route, parameters
+		# did not find any route which matches the path
+		return None, None
+
+	@classmethod
+	def _match(cls, route, path):
+		# '/something/xxx/' to 'something/xxx'. Get rid of '/' at the left and the right end of a string
+		route = route.lstrip('/').rstrip('/').split('/')
+		path = path.lstrip('/').rstrip('/').split('/')
+
+		if len(route) != len(path):
+			return False, None
+		else:
+			# todo: optimize the logic
+			parameters = {}
+			for r, p in zip(route, path):
+				if r == p == '':
+					return True, None
+				if r == '' and r != p:
+					return False, None
+				if r[0] == '{' and r[-1] == '}':
+					parameters[r[1:-1]] = p
+				elif r != p:
+					return False, None
+			return True, parameters
+
+	# async
+	async def handle_route(self, http: HTTP, stream: Stream):
+		path = stream.headers[':path']
+		method = stream.headers[':method']
+
+		route, parameters = self.find_match(path)
+
+		# 如果没有任何匹配，就默认为静态文件读取
+		if route is None:
+			if method == 'GET':
+				handler = self.default_get
+			else:
+				handler = None
+		else:
+			handler = self._routes[method].get(route, None)
+
+		if handler is not None:
+			req = Request(stream, parameters)
+			res = Response(stream.stream_id, http)
+			await handler(req, res)
+		else:
+			raise RouteNotRegisteredException(path + ' is not a registered route')
---
+++ packages/hyper2web/hyper2web-0.0.3-py2.py3-none-any.whl/hyper2web/server.py
@@ -0,0 +1,66 @@
+"""
+A fully-functional HTTP/2 server written for curio.
+
+Requires Python 3.5+.
+"""
+
+from curio import spawn
+
+import h2.config
+import h2.connection
+import h2.events
+
+# hyper2web imports
+from . import sslsocket, abstract, http
+create_listening_ssl_socket = sslsocket.create_listening_ssl_socket
+HTTP = http.HTTP
+
+
+# todo: move this to app.App
+async def h2_server(address, certfile, keyfile, app: abstract.AbstractApp):
+    """
+    Create an HTTP/2 server at the given address.
+    """
+    sock = create_listening_ssl_socket(address, certfile, keyfile)
+    print("Now listening on %s:%d" % address)
+
+    async with sock:
+        while True:
+            client, _ = await sock.accept()
+            server = H2Server(client, app)
+            await spawn(server.run())
+
+
+class H2Server:
+    """
+    This class just connects to socket and that's about it.
+    Most heavy lifting is done by http.HTTP
+    """
+    def __init__(self, sock, app: abstract.AbstractApp):
+        config = h2.config.H2Configuration(client_side=False, header_encoding='utf-8')
+        self.sock = sock
+        self.conn = h2.connection.H2Connection(config=config)
+
+        # the Application that needs this server
+        # this server runs this app
+        self.http = HTTP(app=app, sock=self.sock, connection=self.conn)
+
+    async def run(self):
+        """
+        Loop over the connection, managing it appropriately.
+        """
+        self.conn.initiate_connection()
+        await self.sock.sendall(self.conn.data_to_send())
+
+        while True:
+            # 65535 is basically arbitrary here: this amounts to "give me
+            # whatever data you have".
+            data = await self.sock.recv(65535)
+            if not data:
+                break
+
+            events = self.conn.receive_data(data)
+            for event in events:
+                await self.http.handle_event(event)
+
+            await self.sock.sendall(self.conn.data_to_send())
