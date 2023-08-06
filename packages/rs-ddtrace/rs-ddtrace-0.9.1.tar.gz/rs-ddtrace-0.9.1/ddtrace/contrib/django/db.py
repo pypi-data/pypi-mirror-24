332dde7ffe04c30431927196ef518db4292a7561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:04 +0000

    Add package rs-ddtrace/0.9.1/rs-ddtrace-0.9.1.tar.gz

---
+++ packages/rs-ddtrace/rs-ddtrace-0.9.1.tar.gz/rs-ddtrace-0.9.1/ddtrace/contrib/django/db.py
@@ -0,0 +1,106 @@
+
+import logging
+
+from django.db import connections
+
+# project
+from ...ext import sql as sqlx
+from ...ext import AppTypes
+
+from .conf import settings
+
+
+log = logging.getLogger(__name__)
+
+
+def patch_db(tracer):
+    for c in connections.all():
+        patch_conn(tracer, c)
+
+
+def patch_conn(tracer, conn):
+    attr = '_datadog_original_cursor'
+    if hasattr(conn, attr):
+        log.debug("already patched")
+        return
+
+    conn._datadog_original_cursor = conn.cursor
+
+    def cursor():
+        return TracedCursor(tracer, conn, conn._datadog_original_cursor())
+
+    conn.cursor = cursor
+
+
+class TracedCursor(object):
+
+    def __init__(self, tracer, conn, cursor):
+        self.tracer = tracer
+        self.conn = conn
+        self.cursor = cursor
+
+        self._vendor = getattr(conn, 'vendor', 'db')     # e.g sqlite, postgres
+        self._alias = getattr(conn, 'alias', 'default')  # e.g. default, users
+
+        prefix = sqlx.normalize_vendor(self._vendor)
+        self._name = "%s.%s" % (prefix, "query")  # e.g sqlite.query
+
+        database_prefix = (
+            '{}-'.format(settings.DEFAULT_DATABASE_PREFIX)
+            if settings.DEFAULT_DATABASE_PREFIX else ''
+        )
+
+        self._service = "%s%s%s" % (
+            database_prefix,
+            self._alias,
+            "db"
+        )  # e.g. service-defaultdb or service-postgresdb
+
+        self.tracer.set_service_info(
+            service=self._service,
+            app=prefix,
+            app_type=AppTypes.db,
+        )
+
+    def _trace(self, func, sql, params):
+        span = self.tracer.trace(
+            self._name,
+            resource=sql,
+            service=self._service,
+            span_type=sqlx.TYPE
+        )
+
+        with span:
+            span.set_tag(sqlx.QUERY, sql)
+            span.set_tag("django.db.vendor", self._vendor)
+            span.set_tag("django.db.alias", self._alias)
+            try:
+                return func(sql, params)
+            finally:
+                rows = self.cursor.cursor.rowcount
+                if rows and 0 <= rows:
+                    span.set_tag(sqlx.ROWS, self.cursor.cursor.rowcount)
+
+    def callproc(self, procname, params=None):
+        return self._trace(self.cursor.callproc, procname, params)
+
+    def execute(self, sql, params=None):
+        return self._trace(self.cursor.execute, sql, params)
+
+    def executemany(self, sql, param_list):
+        return self._trace(self.cursor.executemany, sql, param_list)
+
+    def close(self):
+        return self.cursor.close()
+
+    def __getattr__(self, attr):
+        return getattr(self.cursor, attr)
+
+    def __iter__(self):
+        return iter(self.cursor)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
---
+++ packages/rs-ddtrace/rs-ddtrace-0.9.1.tar.gz/rs-ddtrace-0.9.1/ddtrace/contrib/django/middleware.py
@@ -0,0 +1,119 @@
+import logging
+
+# project
+from .conf import settings
+
+from ...ext import http
+from ...contrib import func_name
+
+# 3p
+from django.core.exceptions import MiddlewareNotUsed
+from django.conf import settings as django_settings
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+    MiddlewareClass = MiddlewareMixin
+except ImportError:
+    MiddlewareClass = object
+
+log = logging.getLogger(__name__)
+
+def insert_exception_middleware():
+    exception_middleware = 'ddtrace.contrib.django.TraceExceptionMiddleware'
+    middleware_attributes = ['MIDDLEWARE', 'MIDDLEWARE_CLASSES']
+    for middleware_attribute in middleware_attributes:
+        middleware = getattr(django_settings, middleware_attribute, None)
+        if middleware and exception_middleware not in set(middleware):
+            setattr(django_settings, middleware_attribute, middleware + type(middleware)((exception_middleware,)))
+
+
+class InstrumentationMixin(MiddlewareClass):
+    """
+    Useful mixin base class for tracing middlewares
+    """
+    def __init__(self, get_response=None):
+        # disable the middleware if the tracer is not enabled
+        # or if the auto instrumentation is disabled
+        self.get_response = get_response
+        if not settings.AUTO_INSTRUMENT:
+            raise MiddlewareNotUsed
+
+
+class TraceExceptionMiddleware(InstrumentationMixin):
+    """
+    Middleware that traces exceptions raised
+    """
+    def process_exception(self, request, exception):
+        try:
+            span = _get_req_span(request)
+            if span:
+                span.set_tag(http.STATUS_CODE, '500')
+                span.set_traceback() # will set the exception info
+        except Exception:
+            log.debug("error processing exception", exc_info=True)
+
+
+class TraceMiddleware(InstrumentationMixin):
+    """
+    Middleware that traces Django requests
+    """
+    def process_request(self, request):
+        tracer = settings.TRACER
+        try:
+            span = tracer.trace(
+                'django.request',
+                service=settings.DEFAULT_SERVICE,
+                resource='unknown',  # will be filled by process view
+                span_type=http.TYPE,
+            )
+
+            span.set_tag(http.METHOD, request.method)
+            span.set_tag(http.URL, request.path)
+            _set_req_span(request, span)
+        except Exception:
+            log.debug('error tracing request', exc_info=True)
+
+    def process_view(self, request, view_func, *args, **kwargs):
+        span = _get_req_span(request)
+        if span:
+            span.resource = func_name(view_func)
+
+    def process_response(self, request, response):
+        try:
+            span = _get_req_span(request)
+            if span:
+                span.set_tag(http.STATUS_CODE, response.status_code)
+                span = _set_auth_tags(span, request)
+                span.finish()
+        except Exception:
+            log.debug("error tracing request", exc_info=True)
+        finally:
+            return response
+
+
+def _get_req_span(request):
+    """ Return the datadog span from the given request. """
+    return getattr(request, '_datadog_request_span', None)
+
+def _set_req_span(request, span):
+    """ Set the datadog span on the given request. """
+    return setattr(request, '_datadog_request_span', span)
+
+def _set_auth_tags(span, request):
+    """ Patch any available auth tags from the request onto the span. """
+    user = getattr(request, 'user', None)
+    if not user:
+        return span
+
+    if hasattr(user, 'is_authenticated'):
+        span.set_tag('django.user.is_authenticated', user.is_authenticated())
+
+    uid = getattr(user, 'pk', None)
+    if uid:
+        span.set_tag('django.user.id', uid)
+
+    uname = getattr(user, 'username', None)
+    if uname:
+        span.set_tag('django.user.name', uname)
+
+    return span
