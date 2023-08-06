012f9f2beb1106b0ead392d327daceab042c665e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:04 +0000

    Add package povme/3.0.35/POVME-3.0.35-py2-none-any.whl

---
+++ packages/povme/POVME-3.0.35-py2-none-any.whl/POVME/examples/analysis_workflow_example/2-POVME_analysis/runAll.sh
@@ -0,0 +1,5 @@
+POVME3.py povme_1UYF_every50ns_input.ini  
+POVME3.py povme_3D0B_every50ns_input.ini  
+POVME3.py povme_4R3M_every50ns_input.ini  
+POVME3.py povme_1UYI_every50ns_input.ini  
+POVME3.py povme_1UYL_every50ns_input.ini  
