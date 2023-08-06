73c4573ed1b43f6abf99521ae4a6ca905a52aeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:47 +0000

    Add package pyhtmlex/1.1.0/pyhtmlex-1.1.0.tar.gz

---
+++ packages/pyhtmlex/pyhtmlex-1.1.0.tar.gz/pyhtmlex-1.1.0/pyhtmlex/utils/console.py
@@ -0,0 +1,24 @@
+from pyhtmlex.base.head import *
+from pyhtmlex.base.body import *
+
+
+class Console(Div):
+    style = Style()
+    style.add_class("console", margin=0, color='#0a0', background='#000', display='block', overflow='auto')
+
+    def __init__(self, lines):
+        super(Console, self).__init__('console')
+
+        pre = Pre()
+        ul = Ul()
+        pre.push_back(ul)
+        ul.set_attributes(style="list-style-type:none")
+        for line in lines:
+            if line == '':
+                line = ' '
+            script = Script(line)
+            script.set_attributes(type='text/html', style="display:block")
+            li = Li()
+            li.push_back(script)
+            ul.push_back(li)
+        self.children.append(pre)
