ea43814fdd0b7b4b7276ccc5dc58002d1a57fed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:25 +0000

    Add package Data-viz/0.12/Data-viz-0.12.tar.gz

---
+++ packages/Data-viz/Data-viz-0.12.tar.gz/Data-viz-0.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+setup(
+        name='Data-viz',
+        version='0.12',
+        packages=['data_viz/'],
+        entry_points = {
+            'console_scripts':['vizuka=data_viz.launch_viz:main'],
+            },
+        description='Represents your high-dimensional datas in a 2D space and play wih it',
+        long_description = open(os.path.join(os.path.dirname(__file__),'README.md')).read(),
+        install_requires = open('data_viz/requirements/requirements.txt').read(),
+        license = 'GPL V3',
+        author='Sofian Medbouhi',
+        author_email='sof.m.sk@free.fr',
+        )
+            
