d326d48c6f4adf342214f3db44fba688a2323548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:51 +0000

    Add package perseuspy/0.1.1/perseuspy-0.1.1.tar.gz

---
+++ packages/perseuspy/perseuspy-0.1.1.tar.gz/perseuspy-0.1.1/perseuspy/__init__.py
@@ -0,0 +1,16 @@
+"""
+perseuspy module for Python-Perseus interop.
+
+Currently there is no support for the following Perseus matrix features:
+ - numerical annotation rows
+ - multi-numeric rows
+"""
+import perseuspy.dependent_peptides
+import perseuspy.io
+from perseuspy.io.perseus import to_perseus, read_perseus
+# Monkey-patching pandas
+import pandas as pd
+pd.DataFrame.to_perseus = to_perseus
+pd.read_perseus = read_perseus
+
+__version__ = '0.1.1'
