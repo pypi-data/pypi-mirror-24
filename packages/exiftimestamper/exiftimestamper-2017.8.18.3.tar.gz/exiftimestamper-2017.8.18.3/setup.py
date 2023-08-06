1e231b08298a6bace5c8b122ee004737ed4b489e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:07 +0000

    Add package exiftimestamper/2017.8.18.3/exiftimestamper-2017.8.18.3.tar.gz

---
+++ packages/exiftimestamper/exiftimestamper-2017.8.18.3.tar.gz/exiftimestamper-2017.8.18.3/setup.py
@@ -0,0 +1,20 @@
+import sys
+from setuptools import setup
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+setup(
+    name='exiftimestamper',
+    version="2017.8.18.3",
+    author = "Ernesto Alfonso",
+    author_email = "erjoalgo@gmail.com",
+    # url='https://github.com/jrfonseca/gprof2dot',
+    description="""A command-line tool to update jpeg file timestamps
+    based on their 'EXIF DateTimeOriginal' metadata tag """,
+    long_description="""A command-line tool to update jpeg file timestamps
+    based on their 'EXIF DateTimeOriginal' metadata tag """,
+    license="LGPL",
+    py_modules=['exiftimestamper'],
+    entry_points=dict(console_scripts=['exiftimestamper=exiftimestamper:main']),
+    install_requires=['exifread']
+)
