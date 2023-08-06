4fc9d43b5509ea14dd2587738697a723b92d9bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:23 +0000

    Add package mlarchive2maildir/0.0.4/mlarchive2maildir-0.0.4.tar.gz

---
+++ packages/mlarchive2maildir/mlarchive2maildir-0.0.4.tar.gz/mlarchive2maildir-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='mlarchive2maildir',
+    use_scm_version={'write_to': 'mlarchive2maildir/version.py'},
+    setup_requires=['setuptools_scm'],
+    description='Imports mail from (pipermail) archives into a maildir',
+    install_requires=['bs4', 'requests', 'cleo', 'six'],
+    tests_require=[],
+    packages=['mlarchive2maildir'],
+    entry_points={
+        'console_scripts': {
+            'mlarchive2maildir = mlarchive2maildir.commands:main'
+        }
+    },
+    author='Florian Klink',
+    author_email='flokli@flokli.de',
+    license='MIT'
+)
