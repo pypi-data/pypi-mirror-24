3e1315ccd319c66ecffbb7d2a87af72000f4f92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:11 +0000

    Add package dimedbpy/0.0.1.01a/dimedbpy-0.0.1.01a.tar.gz

---
+++ packages/dimedbpy/dimedbpy-0.0.1.01a.tar.gz/dimedbpy-0.0.1.01a/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from pip.req import parse_requirements
+
+setup(
+    name='dimedbpy',
+    version='0.0.1.01a',
+    packages=['dimedbpy'],
+    url='https://www.github.com/KeironO/dimedbpy',
+    license='MIT',
+    author='Keiron OShea',
+    author_email = 'keo7@aber.ac.uk',
+    description = 'Python wrapper for DIMEdb\'s REST API',
+    install_requires=[str(ir.req) for ir in parse_requirements('base_requirements.txt', session=False)],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers'
+    ]
+)
