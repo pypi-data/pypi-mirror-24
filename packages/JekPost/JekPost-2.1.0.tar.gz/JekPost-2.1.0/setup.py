e22cd7646d698cd1703e188a6995112f0f66c02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:31 +0000

    Add package JekPost/2.1.0/JekPost-2.1.0.tar.gz

---
+++ packages/JekPost/JekPost-2.1.0.tar.gz/JekPost-2.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='JekPost',
+    version='2.1.0',
+    author='Arjun Krishna Babu',
+    author_email='arjunkrishnababu96@gmail.com',
+    packages=['jekpost'],
+    url='https://github.com/arjunkrishnababu96/jekpost',
+    license='MIT',
+    description='Package to ease the process of creating a new Jekyll post',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ],
+    entry_points={
+        'console_scripts':  [
+            'jekpost=jekpost.jekpost:main',
+        ],
+    }
+)
