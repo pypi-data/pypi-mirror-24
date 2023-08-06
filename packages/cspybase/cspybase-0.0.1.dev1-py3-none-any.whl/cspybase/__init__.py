8776eb7602633189a1c8470a366cae176b33fc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package cspybase/0.0.1.dev1/cspybase-0.0.1.dev1-py3-none-any.whl

---
+++ packages/cspybase/cspybase-0.0.1.dev1-py3-none-any.whl/cspybase/__init__.py
@@ -0,0 +1,13 @@
+""" Definição dos módulos da biblioteca."""
+
+## @package cspybase Definição dos módulos da biblioteca.
+## @author André Luiz Clinio - Tecgraf/PUC-Rio
+
+__version__ = '0.0.1.dev1'
+
+from cspybase.core.access import CsPyAccess
+from cspybase.core.connection import CsPyConnection
+from cspybase.core.exception import CsPyException
+from cspybase.core.project import CsPyProject
+from cspybase.core.file import CsPyFile
+from cspybase.core.user import CsPyUser
