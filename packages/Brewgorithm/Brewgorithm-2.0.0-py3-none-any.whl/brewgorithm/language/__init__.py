8477ec0c4f4c9bfe3006d9785974bffdf63546a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:01 +0000

    Add package Brewgorithm/2.0.0/Brewgorithm-2.0.0-py3-none-any.whl

---
+++ packages/Brewgorithm/Brewgorithm-2.0.0-py3-none-any.whl/brewgorithm/language/__init__.py
@@ -0,0 +1,6 @@
+"""Contains functionality for tokenizing, parsing, embedding language."""
+
+from . import parsing
+from . import sense_emb
+from . import cleaning
+from .config import NATURAL_EMB_DIM
