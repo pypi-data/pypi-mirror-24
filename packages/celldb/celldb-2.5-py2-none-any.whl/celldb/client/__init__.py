f00bfb5b5c7246929e5dd5401b4ed64b5379d2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:02 +0000

    Add package celldb/2.5/celldb-2.5-py2-none-any.whl

---
+++ packages/celldb/celldb-2.5-py2-none-any.whl/celldb/client/__init__.py
@@ -0,0 +1,16 @@
+from client import (
+    connect,
+    upsert_sample,
+    list_features,
+    list_samples,
+    matrix,
+    upsert_samples,
+    sparse_matrix)
+
+assert connect
+assert upsert_sample
+assert list_features
+assert list_samples
+assert matrix
+assert upsert_samples
+assert sparse_matrix
