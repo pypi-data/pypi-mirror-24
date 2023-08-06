a69a519c55b493275b287cc3f75f99a8d9f9e6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:04 +0000

    Add package cryptocompare/0.3/cryptocompare-0.3.tar.gz

---
+++ packages/cryptocompare/cryptocompare-0.3.tar.gz/cryptocompare-0.3/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import cryptocompare
+import datetime
+import time
+
+coins = ['BTC', 'ETH', 'XMR', 'NEO']
+currencies = ['EUR', 'USD', 'GBP']
+
+print('================== COIN LIST =====================')
+print(cryptocompare.get_coin_list())
+print(cryptocompare.get_coin_list(True))
+
+print('===================== PRICE ======================')
+print(cryptocompare.get_price(coins[0]))
+print(cryptocompare.get_price(coins[1], curr='USD'))
+print(cryptocompare.get_price(coins[2], curr=['EUR','USD','GBP']))
+print(cryptocompare.get_price(coins[2], full=True))
+print(cryptocompare.get_price(coins[0], curr='USD', full=True))
+print(cryptocompare.get_price(coins[1], curr=['EUR','USD','GBP'], full=True))
+
+print('==================================================')
+print(cryptocompare.get_price(coins))
+print(cryptocompare.get_price(coins, curr='USD'))
+print(cryptocompare.get_price(coins, curr=['EUR','USD','GBP']))
+
+print('==================== HIST PRICE ==================')
+print(cryptocompare.get_historical_price(coins[0]))
+print(cryptocompare.get_historical_price(coins[0], curr='USD'))
+print(cryptocompare.get_historical_price(coins[1], curr=['EUR','USD','GBP']))
+print(cryptocompare.get_historical_price(coins[1], 'USD',datetime.datetime.now()))
+print(cryptocompare.get_historical_price(coins[2], ['EUR','USD','GBP'],time.time()))
+
+print('======================== AVG =====================')
+print(cryptocompare.get_avg(coins[2]))
+print(cryptocompare.get_avg(coins[2], curr='USD'))
+
+
