ecec5fa796489305073492a2442a0f7fcf72c03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:18 +0000

    Add package jupyter-linenumbers/0.2.0/jupyter-linenumbers-0.2.0.tar.gz

---
+++ packages/jupyter-linenumbers/jupyter-linenumbers-0.2.0.tar.gz/jupyter-linenumbers-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='jupyter-linenumbers',
+    version='0.2.0',
+    author='Wouter de Vries',
+    author_email='wdevries@leukeleu.nl',
+    url='https://github.com/leukeleu/jupyter-linenumbers/',
+    packages=['linenumbers'],
+    package_data={'linenumbers': ['static/*.js']},
+    description='Continuous line numbers for Jupyter Notebooks',
+    long_description=open('README.md').read(),
+)
