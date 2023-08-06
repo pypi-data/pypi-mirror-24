4d0c64ebf96b1fcc1d1c83fead62eb7d75192762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:39 +0000

    Add package qpack/0.0.17/qpack-0.0.17.tar.gz

---
+++ packages/qpack/qpack-0.0.17.tar.gz/qpack-0.0.17/setup.py
@@ -0,0 +1,48 @@
+"""setup.py
+
+Upload to PyPI, Thx to: http://peterdowns.com/posts/first-time-with-pypi.html
+
+python3 setup.py register -r pypitest
+python3 setup.py sdist upload -r pypitest
+
+python3 setup.py register -r pypi
+python3 setup.py sdist upload -r pypi
+"""
+import setuptools
+from distutils.core import setup, Extension
+from qpack import __version__
+
+module = Extension(
+    'qpack._qpack',
+    define_macros=[],
+    include_dirs=['./qpack'],
+    libraries=[],
+    sources=['./qpack/_qpack.c'],
+    extra_compile_args=["--std=c99", "-pedantic"]
+)
+
+VERSION = __version__
+
+setup(
+    name='qpack',
+    packages=['qpack'],
+    version=VERSION,
+    description='QPack (de)serializer',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/transceptor-technology/qpack',
+    ext_modules=[module],
+    download_url='https://github.com/transceptor-technology/'
+        'qpack/tarball/{}'.format(VERSION),
+    keywords=['serializer', 'deserializer'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development'
+    ],
+)
