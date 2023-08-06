2d5d2430a7808e0e02d6b9a386e8152d1232101e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:31 +0000

    Add package image_converter/0.0.6/image_converter-0.0.6.tar.gz

---
+++ packages/image_converter/image_converter-0.0.6.tar.gz/image_converter-0.0.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python   
+try:                    
+    from setuptools import setup, find_packages
+except:                 
+    from distutils.core import setup, find_packages
+                        
+setup(                  
+    version="0.0.6",
+    description="Let you convert any image size.",
+    author="sparrow",    
+    author_email="sprrow.jang@gmail.com",
+    url="https://github.com/SparrowJang/image_converter",
+    name='image_converter',
+    keywords="image convert resize",
+    packages=find_packages(),
+    install_requires=["Pillow"] 
+)      
