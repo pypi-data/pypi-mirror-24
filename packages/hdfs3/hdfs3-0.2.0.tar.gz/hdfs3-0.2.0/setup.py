ced7909d36ce2e46de640c56f753b61b1536b754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:43 +0000

    Add package hdfs3/0.2.0/hdfs3-0.2.0.tar.gz

---
+++ packages/hdfs3/hdfs3-0.2.0.tar.gz/hdfs3-0.2.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(name='hdfs3',
+      version='0.2.0',
+      description='Python wrappers for libhdfs3, a native HDFS client',
+      url='http://github.com/dask/hdfs3/',
+      maintainer='Martin Durant',
+      maintainer_email='mdurant@continuum.io',
+      license='BSD',
+      keywords='hdfs',
+      packages=['hdfs3'],
+      install_requires=[],
+      long_description=(open('README.rst').read() if os.path.exists('README.rst')
+                        else ''),
+      zip_safe=False)
