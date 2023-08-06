e20658c2ba0d53bc8b87cf453e28892de2d0a98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:29 +0000

    Add package CarDataProcess/0.1/CarDataProcess-0.1-py2-none-any.whl

---
+++ packages/CarDataProcess/CarDataProcess-0.1-py2-none-any.whl/CarDataProcess/CarDataProcess.py
@@ -0,0 +1,7 @@
+class Cardataprocess:
+    def cardataprocess(name):
+        print "hello,world,%s"%name
+        
+if __name__=="__main__":
+    c=Cardataprocess()
+    c.cardataprocess("baomingguang")
