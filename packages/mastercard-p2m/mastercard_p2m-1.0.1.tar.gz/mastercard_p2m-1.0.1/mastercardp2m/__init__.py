30803928866799e5a406dd048f4e4be0718a174b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package mastercard-p2m/1.0.1/mastercard_p2m-1.0.1.tar.gz

---
+++ packages/mastercard-p2m/mastercard_p2m-1.0.1.tar.gz/mastercard_p2m-1.0.1/mastercardp2m/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardp2m.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardp2m.merchantpaymenttransfer import *
+from mastercardp2m.merchanttransfer import *
