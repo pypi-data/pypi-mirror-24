960bff3325e24ca297f20f86de984d93f356ac4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package gtlang_detection/0.1/gtlang_detection-0.1.tar.gz

---
+++ packages/gtlang_detection/gtlang_detection-0.1.tar.gz/gtlang_detection-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="gtlang_detection",
+    packages=["gtlang_detection"],
+    version="0.1",
+    description="A simple wrapper to detect languages using Google Cloud Platform.",
+    author="DizAzTor",
+    author_email="dizaztor@protonmail.com",
+    url="https://github.com/DizAzTor/GT-Lang-Detection-py",
+    download_url="https://github.com/DizAzTor/GT-Lang-Detection-py/archive/v1.0.tar.gz",
+    keywords="google cloud platform language detection simple wrapper",
+    classifiers=[],
+)
