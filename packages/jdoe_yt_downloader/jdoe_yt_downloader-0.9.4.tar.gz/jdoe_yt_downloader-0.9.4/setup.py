ddb0ac2d005a6e2124aaca1bbdd4fe976a14515d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:27 +0000

    Add package jdoe_yt_downloader/0.9.4/jdoe_yt_downloader-0.9.4.tar.gz

---
+++ packages/jdoe_yt_downloader/jdoe_yt_downloader-0.9.4.tar.gz/jdoe_yt_downloader-0.9.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='jdoe_yt_downloader',
+	version='0.9.4',
+	description='Download YT video in mp3 format',
+	url='https://github.com/JohnnyDeeee/YT_MP3_Downloader.git',
+	author='JohnDoe',
+	author_email='anotherfakeone@live.nl',
+	license='MIT',
+	packages=['jdoe_yt_downloader'],
+	zip_safe=False,
+	entry_points={
+		'console_scripts': [
+			'jdoe_yt_downloader = jdoe_yt_downloader.__main__:main'
+		]
+	},
+	install_requires=[
+		'progressbar2',
+		'google-api-python-client',
+		'argparse'
+	])
