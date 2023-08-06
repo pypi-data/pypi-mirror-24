12d2ed98f17f28d031187411ed7a4b3920c2b477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package dnutils/0.1.1/dnutils-0.1.1.tar.gz

---
+++ packages/dnutils/dnutils-0.1.1.tar.gz/dnutils-0.1.1/dnutils/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION'), 'r') as f:
+    __version__ = f.read()
+__author__ = 'Daniel Nyga'
+
+from .debug import out, stop, trace, stoptrace
+from .tools import ifnone, ifnot, allnone, allnot, edict, idxif, first, last
+from .signals import add_handler, rm_handler, enable_ctrlc
+from .threads import Lock, RLock, Condition, Event, Semaphore, BoundedSemaphore, Barrier, Relay, Thread, \
+    SuspendableThread
+from .logs import loggers, newlogger, getlogger, DEBUG, INFO, WARNING, ERROR, CRITICAL, expose, inspect, active_exposures
+from .console import ProgressBar, StatusMsg, bf
+
+enable_ctrlc()
---
+++ packages/dnutils/dnutils-0.1.1.tar.gz/dnutils-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r') as f:
+    __version = f.read()
+
+setup(
+    name = 'dnutils',
+    packages = ['dnutils'],
+    version = __version,
+    description = 'A collection of convenience tools for everyday Python programming',
+    author = 'Daniel Nyga',
+    author_email = 'daniel.nyga@t-online.de',
+    url = 'https://spritinio.de/dnutils',
+    download_url = 'https://github.com/danielnyga/dnutils/archive/%s.tar.gz' % __version,
+    keywords = ['testing', 'logging', 'threading', 'multithreading', 'debugging', 'tools'],
+    classifiers = [],
+    install_requires=[
+        'colored==1.3.5',
+        'tabulate',
+        'portalocker',
+    ]
+)
