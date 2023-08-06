64bc15ecbe8904df8f5f4f6ed50b1125fa6e9d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:12 +0000

    Add package Test12313132321/1.0.0/Test12313132321-1.0.0.tar.gz

---
+++ packages/Test12313132321/Test12313132321-1.0.0.tar.gz/Test12313132321-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'Test12313132321',
+        version = '1.0.0',
+        py_modules = ['Test'],
+        author = 'Simon',
+        author_email = 'qanpan.lo@gmail.com',
+        url = 'http://blog.csdn.net/a85582048',
+        description = 'A simple printer of nested lists',
+    )
