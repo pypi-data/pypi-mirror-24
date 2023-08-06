750b723c6206f7aad83ba5a16730baa412481fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:54 +0000

    Add package dongGe/1.0/dongGe-1.0.tar.gz

---
+++ packages/dongGe/dongGe-1.0.tar.gz/dongGe-1.0/Test/__init__.py
@@ -0,0 +1,2 @@
+from . import sendmsg
+from . import recvmsg
---
+++ packages/dongGe/dongGe-1.0.tar.gz/dongGe-1.0/setup.py
@@ -0,0 +1,7 @@
+#from distutils.core import setup
+#setup(name="ljb",version="1.0",description="this is firt backpage",author="ljb",py_modules=["Test.sendmsg","Test.recvmsg"])
+
+
+from distutils.core import setup
+
+setup(name="dongGe",author_email="848017622@qq.com", version="1.0", description="dongGe's module", author="dongGe", py_modules=['Test.sendmsg', 'Test.recvmsg'])
