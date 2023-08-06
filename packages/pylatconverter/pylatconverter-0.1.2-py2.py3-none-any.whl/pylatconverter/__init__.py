72a573e6fd981c4e8b990e4c619e2f397d5a6307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:01 +0000

    Add package pylatconverter/0.1.2/pylatconverter-0.1.2-py2.py3-none-any.whl

---
+++ packages/pylatconverter/pylatconverter-0.1.2-py2.py3-none-any.whl/pylatconverter/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import pylatconverter
+"""Top-level package for pyLatConverter."""
+
+__author__ = """Francisco Salema"""
+__email__ = 'x_salema@hotmail.com'
+__version__ = '0.1.2'
