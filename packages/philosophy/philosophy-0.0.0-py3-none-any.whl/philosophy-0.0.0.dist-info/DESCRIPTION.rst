e7c8eeca8c8720ec4e9ff0a4ff25c9b23ea73616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:53 +0000

    Add package philosophy/0.0.0/philosophy-0.0.0-py3-none-any.whl

---
+++ packages/philosophy/philosophy-0.0.0-py3-none-any.whl/philosophy-0.0.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+Home-page: https://github.com/philipov/philosophy
+Author: Philip Loguinov
+Author-email: philipov@gmail.com
+License: UNKNOWN
+Description: UNKNOWN
+Platform: UNKNOWN
---
+++ packages/philosophy/philosophy-0.0.0-py3-none-any.whl/philosophy/__init__.py
@@ -0,0 +1,21 @@
+#-- philosophy
+
+"""--- shameless appropriation of cultural capital
+"""
+
+__setup__ = dict(
+    name='philosophy',
+    packages=['philosophy'],
+    version='0.0.0',
+    description=__doc__,
+
+    url='https://github.com/philipov/philosophy',
+    author='Philip Loguinov',
+    author_email='philipov@gmail.com',
+
+)
+
+__version__ = __setup__['version']
+
+
+#----------------------------------------------------------------------#
