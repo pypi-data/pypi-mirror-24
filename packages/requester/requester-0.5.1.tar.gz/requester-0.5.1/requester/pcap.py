815f7414fa7fc15f4b959388bde3c7c13e382425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:55 +0000

    Add package requester/0.5.1/requester-0.5.1.tar.gz

---
+++ packages/requester/requester-0.5.1.tar.gz/requester-0.5.1/requester/pcap.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+from scapy.all import *
+
+from parser import ResponseParser
+
+SRC_IP = 0
+DST_IP = 1
+PAYLOAD = 2
+
+START_LINE = 2
+HEADERS = 3
+BODY = 4
+
+class PcapHandler(object):
+
+    __attrs__ = [
+        "file_name", "pcap_data", "payload", "request_data", "response_data_raw", "response_data"
+    ]
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.pcap_data = self.read_pcap_file()
+
+        self.payload = []
+        self.request_data = []
+        self.response_data_raw = []
+        self.response_data = []
+
+
+    def read_pcap_file(self):
+        try:
+            pcap_data = rdpcap(self.file_name)
+        except scapy.error.Scapy_Exception:
+            print ("Error : This is not a normal pcap file.")
+            return None
+        else:
+            return pcap_data
+
+
+    def extraction_request(self):
+        if self.pcap_data is None:
+            return False
+
+        self.pcap_handler()
+        self.write_request_data()
+        return True
+
+
+    def prepare_comparison_response(self):
+        if self.pcap_data is None:
+            return False
+
+        self.pcap_handler()
+        self.parse_response_data_in_pcap()
+        return True
+
+
+    def parse_response_data_in_pcap(self):
+        for data in self.response_data_raw:
+            parser = ResponseParser(data[PAYLOAD])
+            parser.run()
+
+            data_tuple = ( data[SRC_IP], data[DST_IP],          \
+                            getattr(parser, "start_line", ""),  \
+                            getattr(parser, "headers", {}),     \
+                            getattr(parser, "body", "") )
+            self.response_data.append(data_tuple)
+
+
+    def comparison_response_header(self, first_headers, second_headers):
+        first_headers_keys = set(first_headers.keys())
+        second_headers_keys = set(second_headers.keys())
+        intersection_keys = first_headers_keys.intersection(second_headers_keys)
+
+        if first_headers_keys != intersection_keys or second_headers_keys != intersection_keys:
+            return False
+
+        same_value_keys = set(o for o in intersection_keys if first_headers[o] == second_headers[o])
+        if intersection_keys != same_value_keys:
+            return True if (intersection_keys - same_value_keys) == set(['date']) else False
+
+        return True
+
+
+    def comparison_response(self, response_data):
+        if len(response_data) != len(self.response_data):
+            print ("Error : The number of response data does not match.")
+            return
+
+        for recived_data, pcap_data in zip(response_data, self.response_data):
+            # Response header verification.
+            if self.comparison_response_header(recived_data.headers, pcap_data[HEADERS]) == False:
+                print ("The value of the response header is different.")
+
+            if pcap_data[BODY].encode("hex") == recived_data.text.encode("utf-8").replace("\r\n","").encode("hex"):
+                print ("The value of the reponse body is same.")
+            else:
+                print ("The value of the reponse body is different.")
+
+
+    def seperate_response_and_request(self):
+        for data in self.payload:
+            src_ip = data[SRC_IP]
+            dst_ip = data[DST_IP]
+            payload = data[PAYLOAD].strip()
+
+            # Check request start-line
+            if payload[:3] != "GET" and payload[:3] != "POST":
+                self.response_data_raw.append((src_ip, dst_ip, payload))
+                continue
+
+            self.request_data.append((src_ip, dst_ip, payload))
+
+
+    def pcap_handler(self):
+        for packet in self.pcap_data:
+            ip_layer = packet.getlayer("IP")
+
+            tcp_layer = packet.getlayer("TCP")
+            if tcp_layer == None:
+                continue
+
+            if packet.getlayer("Raw"):
+                if len(self.payload) == 0:
+                    self.payload.append((ip_layer.src, ip_layer.dst, str(tcp_layer.payload)))
+                    continue
+
+                pre_src = self.payload[len(self.payload)-1][SRC_IP]
+                pre_dst = self.payload[len(self.payload)-1][DST_IP]
+
+                if pre_src == ip_layer.src and pre_dst == ip_layer.dst:
+                    pre_payload = self.payload[len(self.payload)-1][PAYLOAD]
+                    self.payload[len(self.payload)-1] = (pre_src, pre_dst, pre_payload + str(tcp_layer.payload))
+                else:
+                    self.payload.append((ip_layer.src, ip_layer.dst, str(tcp_layer.payload)))
+        self.seperate_response_and_request()
+
+
+    def write_request_data(self):
+        for idx, data in enumerate(self.request_data):
+            payload = data[PAYLOAD]
+
+            file_name = "%s_request_%s" % (self.file_name, idx)
+            with open(file_name, "a") as f:
+                f.write(payload)
+
+            print ("\"%s\" file has been created." % file_name)
---
+++ packages/requester/requester-0.5.1.tar.gz/requester-0.5.1/requester/session.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+class Session(object):
+
+    __attrs__ = [
+        "session", "request_headers", "request_body", "method", "allow_redirects", "timeout", "response"
+    ]
+
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.allow_redirects = False
+        self.timeout = 30
+
+        self.request_headers = {}
+        self.request_body = ""
+        self.response = None
+
+
+    def update_headers(self, headers):
+        self.request_headers = getattr(self.session, "headers") if headers is None else headers
+        self.session.headers.update(self.request_headers)
+
+
+    def update_body(self, body):
+        self.request_body = body
+
+
+    def update_connection_info(self, method, allow_redirects = False, timeout = 30):
+        self.method = method
+        self.allow_redirects = False
+        self.timeout = timeout
+
+
+    def send(self, url, verbose=True):
+        try:
+            if self.method == "GET":
+                self.response = self.session.get(url, allow_redirects = self.allow_redirects, timeout = self.timeout)
+            elif self.method == "POST":
+                self.response = self.session.post(url, allow_redirects = self.allow_redirects, timeout = self.timeout, data = self.request_body)
+            else:
+                print ("Error : The HTTP method is not valid")
+                return False
+        except requests.exceptions.ConnectionError:
+            print ("Error : The host(%s) is not reachable" % url)
+            return False
+
+        if verbose == True:
+            self.show_request()
+            print ("\n\n")
+            self.show_response()
+
+        return True
+
+    def show_request(self):
+        print ("** REQUEST **")
+        print ("======================================================")
+        print ("Method : %s" % self.method)
+        print ("======================================================")
+        print ("= Request header =")
+        for key, value in self.request_headers.items():
+            print ("%s : %s" % (key, value))
+        print ("======================================================")
+        print ("= Request body =")
+        print (self.request_body)
+        print ("======================================================")
+
+
+    def show_response(self):
+        print ("** RESPONSE **")
+        print ("======================================================")
+        print ("Status code : %s" % self.response.status_code)
+        print ("Status reasone : %s" % self.response.reason)
+        print ("======================================================")
+        print ("= Response header =")
+        for key, value in self.response.headers.items():
+            print ("%s : %s" % (key, value))
+        print ("======================================================")
+        print ("= Response body =")
+        print (self.response.text.encode("utf-8"))
+        print ("======================================================")
