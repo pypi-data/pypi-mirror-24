1ffe46f37365c443d5402818aff680807f3ed721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:56 +0000

    Add package oglhslack/0.1.4.2.2/oglhslack-0.1.4.2.2.tar.gz

---
+++ packages/oglhslack/oglhslack-0.1.4.2.2.tar.gz/oglhslack-0.1.4.2.2/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+setup(
+  name = 'oglhslack',
+  version = '0.1.4.2.2',
+  description = 'A Slack bot client for Lighthouse API',
+  author = 'Lighthouse Team',
+  author_email = 'engineering@opengear.com',
+  url = 'https://github.com/thiagolcmelo/oglhslack',
+  download_url = 'https://github.com/thiagolcmelo/oglhslack/archive/0.1.4.2.2.tar.gz',
+  keywords = ['api', 'opengear', 'lighthouse', 'slackbot'],
+  classifiers = ['Programming Language :: Python :: 2'],
+  install_requires = ['pyyaml', 'slackclient', 'oglhclient', 'requests', 'urllib', 'future'],
+  packages=find_packages(),
+)
