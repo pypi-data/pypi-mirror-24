87f11c3475b0b3aa3dfed52071ef66f9e122b040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:08 +0000

    Add package httprequests/0.3/httprequests-0.3.tar.gz

---
+++ packages/httprequests/httprequests-0.3.tar.gz/httprequests-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='httprequests',
+    version='0.3',
+    description='Abstracts HTTP requests to be easier to use',
+    url='https://github.com/mateoguaman/httprequests',
+    author='Mateo Guaman Castro',
+    author_email='mateo.guaman1998@gmail.com',
+    license='MIT',
+    packages=['httprequests'],
+    zip_safe=False)
