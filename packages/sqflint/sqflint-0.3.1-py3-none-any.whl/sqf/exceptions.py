b31619a13db83565273d189a8faca70c095da9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:26 +0000

    Add package sqflint/0.3.1/sqflint-0.3.1-py3-none-any.whl

---
+++ packages/sqflint/sqflint-0.3.1-py3-none-any.whl/sqf/exceptions.py
@@ -0,0 +1,39 @@
+class SQFError(Exception):
+    pass
+
+
+class SQFParserException(SQFError):
+    """
+    Raised by the parser and analyzer
+    """
+    def __init__(self, position, message):
+        assert(isinstance(position, tuple))
+        self.position = position
+        self.message = message.replace("\n", "\\n").replace("\t", "\\t").replace("\r", "\\r")
+
+
+class SQFParserError(SQFParserException):
+    """
+    Raised by the parser and analyzer
+    """
+    def __init__(self, position, message):
+        super().__init__(position, "error:%s" % message)
+
+
+class SQFParenthesisError(SQFParserError):
+    pass
+
+
+class SQFWarning(SQFParserException):
+    """
+    Something that the interpreter understands but that is a bad practice or potentially
+    semantically incorrect.
+    """
+    def __init__(self, position, message):
+        super().__init__(position, "warning:%s" % message)
+
+
+
+
+class ExecutionError(SQFError):
+    pass
