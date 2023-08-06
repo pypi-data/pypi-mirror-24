7233268942fa899eb390e982bfde71e0f5ae5f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:40 +0000

    Add package instagram_api/0.1.4/instagram_api-0.1.4.tar.gz

---
+++ packages/instagram_api/instagram_api-0.1.4.tar.gz/instagram_api-0.1.4/src/instagram/media.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Instagram API
+# Copyright (c) 2008-2017 Hive Solutions Lda.
+#
+# This file is part of Hive Instagram API.
+#
+# Hive Instagram API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Instagram API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Instagram API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2017 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+class MediaAPI(object):
+
+    def get_media(self, id):
+        url = self.base_url + "v1/media/%s" % id
+        contents = self.get(url)
+        return contents["data"]
