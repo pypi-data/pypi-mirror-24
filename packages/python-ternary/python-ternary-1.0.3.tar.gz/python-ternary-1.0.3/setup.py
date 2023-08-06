0a12fce1677d336ab301567ebf37f6d30c21f6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:07 +0000

    Add package python-ternary/1.0.3/python-ternary-1.0.3.tar.gz

---
+++ packages/python-ternary/python-ternary-1.0.3.tar.gz/python-ternary-1.0.3/setup.py
@@ -0,0 +1,36 @@
+
+from distutils.core import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Visualization"
+]
+
+setup(
+    name="python-ternary",
+    version="1.0.3",
+    packages=['ternary'],
+
+    install_requires=["matplotlib>=1.4"],
+
+    author="Marc Harper",
+    author_email="marc.harper@gmail.com",
+    classifiers=classifiers,
+    description="Make ternary plots in python with matplotlib",
+    long_description=long_description,
+    keywords="matplotlib ternary plotting",
+    license="MIT",
+
+    url="https://github.com/marcharper/python-ternary",
+    download_url="https://github.com/marcharper/python-ternary/tarball/1.0.3",
+)
