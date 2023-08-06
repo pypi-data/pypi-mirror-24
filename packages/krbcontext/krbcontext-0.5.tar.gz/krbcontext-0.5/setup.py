3f061bac82d9fe8ff2ed9f3840d281e1d59f39ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:09 +0000

    Add package krbcontext/0.5/krbcontext-0.5.tar.gz

---
+++ packages/krbcontext/krbcontext-0.5.tar.gz/krbcontext-0.5/setup.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import krbcontext
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    return '''%s
+
+%s''' % (open('README.rst', 'r').read(), open('CHANGELOG.rst', 'r').read())
+
+
+def get_install_requires():
+    with open('requirements.txt', 'r') as fin:
+        return [package.strip() for package in fin]
+
+
+def get_test_requires():
+    with open('test-requirements.txt', 'r') as fin:
+        return [package.strip() for package in fin
+                if not package.startswith('-r')]
+
+
+name = krbcontext.__name__
+version = krbcontext.__version__
+author = krbcontext.__author__
+
+
+setup(
+    name=name,
+    version=version,
+    description='A Kerberos context manager',
+    long_description=get_long_description(),
+    keywords='kerberos context',
+    license='GPLv3',
+    author=author,
+    author_email='qcxhome@gmail.com',
+    url='https://github.com/krbcontext/python-krbcontext',
+
+    packages=find_packages(),
+
+    install_requires=get_install_requires(),
+    tests_require=get_test_requires(),
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration',
+    ],
+)
