3d98db12229b7fb03f447ff2436a8efa395264dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:13 +0000

    Add package docker-etude/0.3.1/docker-etude-0.3.1.tar.gz

---
+++ packages/docker-etude/docker-etude-0.3.1.tar.gz/docker-etude-0.3.1/docker_etude/models/composition.py
@@ -0,0 +1,108 @@
+"""
+A docker composition.
+
+"""
+from collections import OrderedDict
+from yaml import dump, load
+from yaml.representer import SafeRepresenter
+try:
+    from yaml import CSafeDumper as SafeDumper, CSafeLoader as SafeLoader
+except ImportError:
+    from yaml import SafeDumper, SafeLoader
+finally:
+    # Register OrderedDict to dump like a regular dict
+    SafeRepresenter.add_representer(OrderedDict, SafeRepresenter.represent_dict)
+
+from docker_etude.models.base import Model
+from docker_etude.models.network import Network
+from docker_etude.models.service import Service
+from docker_etude.models.volume import Volume
+
+
+class Composition(Model):
+    """
+    The top-level model for a docker composition.
+
+    Maps to/from a docker-compose.yml file.
+
+    """
+    def __init__(self,
+                 networks=None,
+                 services=None,
+                 version="3",
+                 volumes=None):
+        self.version = version
+        self.networks = networks or OrderedDict()
+        self.services = services or OrderedDict()
+        self.volumes = volumes or OrderedDict()
+
+    def add_network(self, network):
+        self.networks[network.name] = network
+
+    def add_service(self, service):
+        self.services[service.name] = service
+
+    def add_volume(self, volume):
+        self.volumes[volume.name] = volume
+
+    def to_dict(self):
+        return OrderedDict(
+            version=self.version,
+            networks=self.dump_model_dict(self.networks),
+            services=self.dump_model_dict(self.services),
+            volumes=self.dump_model_dict(self.volumes),
+        )
+
+    def dump_model_dict(self, dct):
+        if not dct:
+            return None
+
+        return {
+            name: model.to_safe_dict() if model else None
+            for name, model in dct.items()
+        }
+
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(
+            version=dct["version"],
+            networks=cls.load_model_dict(dct.get("networks"), Network),
+            services=cls.load_model_dict(dct.get("services"), Service),
+            volumes=cls.load_model_dict(dct.get("volumes"), Volume),
+        )
+
+    @classmethod
+    def load_model_dict(cls, dct, model_cls):
+        if not dct:
+            return None
+
+        return {
+            name: model_cls.from_dict(dict(
+                name=name,
+                **model_dct
+            )) if model_dct else None
+            for name, model_dct in dct.items()
+        }
+
+    def to_yaml(self):
+        """
+        Pretty print dump as YAML.
+
+        """
+        return dump(
+            self.to_safe_dict(),
+            # show every document in its own block
+            default_flow_style=False,
+            # start a new document (via "---") before every resource
+            explicit_start=True,
+            # follow (modern) PEP8 max line length and indent
+            width=99,
+            indent=4,
+            Dumper=SafeDumper,
+        )
+
+    @classmethod
+    def from_yaml(cls, data):
+        return cls.from_dict(
+            load(data, Loader=SafeLoader),
+        )
---
+++ packages/docker-etude/docker-etude-0.3.1.tar.gz/docker-etude-0.3.1/docker_etude/models/service.py
@@ -0,0 +1,82 @@
+"""
+A docker compose service.
+
+"""
+from collections import OrderedDict
+
+from docker_etude.models.base import Model
+
+
+class Service(Model):
+    """
+    A single docker compose service.
+
+    """
+    def __init__(self,
+                 image,
+                 command=None,
+                 container_name=None,
+                 depends_on=None,
+                 deploy=None,
+                 environment=None,
+                 links=None,
+                 name=None,
+                 networks=None,
+                 ports=None,
+                 ulimits=None,
+                 volumes=None):
+        """
+        Build a service definition.
+
+        The service minimally needs an image name. The service should also known its name
+        (from the left-hand-side of the composition's "services" mapping) because most/many
+        composition transformations will need to know this name.
+
+        :param image: the image name (required)
+        :param name: the service's name in the composition definition (strongly encouraged)
+
+        """
+        self.name = name
+        self.image = image
+        self.command = command
+        self.container_name = container_name
+        self.depends_on = depends_on
+        self.deploy = deploy
+        self.environment = environment or {}
+        self.links = links or []
+        self.networks = networks or {}
+        self.ports = ports or []
+        self.ulimits = ulimits or {}
+        self.volumes = volumes or []
+
+    def to_dict(self):
+        return OrderedDict(
+            command=self.command,
+            container_name=self.container_name,
+            depends_on=self.depends_on,
+            deploy=self.deploy,
+            environment=self.environment,
+            image=self.image,
+            links=self.links,
+            networks=self.networks if self.networks else None,
+            ports=self.ports,
+            ulimits=self.ulimits,
+            volumes=self.volumes,
+        )
+
+    @classmethod
+    def from_dict(cls, dct):
+        return cls(
+            command=dct.get("command"),
+            container_name=dct.get("container_name"),
+            depends_on=dct.get("depends_on"),
+            deploy=dct.get("deploy"),
+            environment=dct.get("environment"),
+            image=dct["image"],
+            links=dct.get("links"),
+            name=dct.get("name"),
+            networks=dct.get("networks"),
+            ports=dct.get("ports"),
+            ulimits=dct.get("ulimits"),
+            volumes=dct.get("volumes"),
+        )
