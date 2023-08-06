e324c51a8a901c7924db85f9f712cdfca74fc500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:32 +0000

    Add package yifi/0.2.3/yifi-0.2.3.tar.gz

---
+++ packages/yifi/yifi-0.2.3.tar.gz/yifi-0.2.3/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__),fname)).read()
+
+setup(
+    name = "yifi",
+    version = "0.2.3",
+    author = "vitaminC",
+    author_email = "dmakhil@gmail.com",
+    url="https://github.com/AkhilMaskery/yifi",
+    description = ("browse yifi on your command line"),
+    entry_points={'console_scripts':['yifi=yifi.command_line:main']},
+    license = "GPLv3",
+    keywords = "yifi torrent download",
+    packages = ['yifi'],
+    install_requires=[
+              'requests',
+          ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+)
+
