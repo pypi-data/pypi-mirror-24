18a24bccb653e52ffeedc1fbcfba9974bee4c57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:59 +0000

    Add package mycroftapi/0.1.0/mycroftapi-0.1.0.tar.gz

---
+++ packages/mycroftapi/mycroftapi-0.1.0.tar.gz/mycroftapi-0.1.0/mycroftapi/__init__.py
@@ -0,0 +1,17 @@
+from websocket import create_connection, WebSocket
+
+
+class MycroftAPI(object):
+    def __init__(self, mycroft_ip):
+        self.mycroft_ip = mycroft_ip
+        self.url = "ws://" + self.mycroft_ip + ":8181/core"
+        self._ws = create_connection(self.url)
+
+    def speak_text(self, text):
+        mycroft_speak = ('"{}"'.format(text))
+        mycroft_type = '"speak"'
+        mycroft_data = '{"expect_response": false, "utterance": %s}, "context": null' % mycroft_speak
+        message = '{"type": ' + mycroft_type + ', "data": ' + mycroft_data + '}'
+        self._ws.send(message)
+        response = "Message Sent to Mycroft Instance: {}".format(self.mycroft_ip)
+        return response
