2e136b5e7b7f5c04c55e10dd7417454d00d3f997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package ispyb/0.4/ispyb-0.4.tar.gz

---
+++ packages/ispyb/ispyb-0.4.tar.gz/ispyb-0.4/ispyb/api/reprocessing.py
@@ -0,0 +1,24 @@
+from __future__ import division, absolute_import
+
+class API(object):
+  '''Functions in this object are added to the main ISPyB API object.'''
+
+  def get_reprocessing_id(self, reprocessing_id):
+    '''Here is some explanation of this method.
+       It takes one parameter.
+       It returns a string that is stored in the database.'''
+    self._notimplemented()
+
+  def get_reprocessing_sweeps(self, reprocessing_id):
+    '''returns a list of image sweeps relevant for a particular reprocessing
+       job.'''
+    self._notimplemented()
+
+  def get_reprocessing_parameters(self, reprocessing_id):
+    '''Returns a dictionary containing all key/value pairs defined in ISPyB
+       for a particular reprocessing job.'''
+    self._notimplemented()
+
+  def undocumented_function(self, thingA, thingB, thingC):
+    '''This is a function.'''
+    self._notimplemented()
---
+++ packages/ispyb/ispyb-0.4.tar.gz/ispyb-0.4/ispyb/driver/dummy.py
@@ -0,0 +1,5 @@
+from __future__ import division, absolute_import
+import ispyb.api.main
+
+class ISPyBDummyDriver(ispyb.api.main.API):
+  pass
