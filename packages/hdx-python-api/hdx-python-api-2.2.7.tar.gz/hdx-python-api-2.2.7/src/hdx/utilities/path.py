c9cfe91dc67b6dc515f0cbf78593cfe7d20a0db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:12 +0000

    Add package hdx-python-api/2.2.7/hdx-python-api-2.2.7.tar.gz

---
+++ packages/hdx-python-api/hdx-python-api-2.2.7.tar.gz/hdx-python-api-2.2.7/src/hdx/utilities/path.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Directory Path Utilities"""
+
+import inspect
+import sys
+from os.path import abspath, realpath, dirname, join
+
+from typing import Optional
+
+
+def script_dir(pyobject, follow_symlinks=True):
+    # type: (Any, Optional[bool]) -> str
+    """Get current script's directory
+
+    Args:
+        pyobject (Any): Any Python object in the script
+        follow_symlinks (Optional[bool]): Follow symlinks or not. Defaults to True.
+
+    Returns:
+        str: Current script's directory
+    """
+    if getattr(sys, 'frozen', False):  # py2exe, PyInstaller, cx_Freeze
+        path = abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(pyobject)
+    if follow_symlinks:
+        path = realpath(path)
+    return dirname(path)
+
+
+def script_dir_plus_file(filename, pyobject, follow_symlinks=True):
+    # type: (str, Any, Optional[bool]) -> str
+    """Get current script's directory and then append a filename
+
+    Args:
+        filename (str): Filename to append to directory path
+        pyobject (Any): Any Python object in the script
+        follow_symlinks (Optional[bool]): Follow symlinks or not. Defaults to True.
+
+    Returns:
+        str: Current script's directory and with filename appended
+    """
+    return join(script_dir(pyobject, follow_symlinks), filename)
