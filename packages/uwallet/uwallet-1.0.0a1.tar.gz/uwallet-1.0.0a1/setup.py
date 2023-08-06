34cc2c5a67488eaf5b4dc0ffba481f007671dac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:30 +0000

    Add package uwallet/1.0.0a1/uwallet-1.0.0a1.tar.gz

---
+++ packages/uwallet/uwallet-1.0.0a1.tar.gz/uwallet-1.0.0a1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+setup(
+    name='uwallet',
+    version='1.0.0a1',
+    description='Python Blockchain Wallet Library',
+    author='Cochise Ruhulessin',
+    author_email='cochiseruhulessin@gmail.com',
+    url='https://www.wizardsofindustry.net',
+    project_name='Purse',
+    install_requires=[],
+    packages=[
+        'uwallet',
+    ]
+)
