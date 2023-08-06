3fb415f22a010af8d6549360adc80eaddfd1bc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:55 +0000

    Add package ipydatawidgets/1.0.1/ipydatawidgets-1.0.1.tar.gz

---
+++ packages/ipydatawidgets/ipydatawidgets-1.0.1.tar.gz/ipydatawidgets-1.0.1/ipydatawidgets/nbextension/static/lib/index.d.ts
@@ -0,0 +1,4 @@
+export { NDArrayModel, JSONToArray, arrayToJSON, array_serialization, IReceivedSerializedArray, ISendSerializedArray } from './ndarray';
+export { JSONToUnion, unionToJSON, data_union_serialization } from './union';
+export { JUPYTER_DATAWIDGETS_VERSION } from './version';
+export declare const version: any;
