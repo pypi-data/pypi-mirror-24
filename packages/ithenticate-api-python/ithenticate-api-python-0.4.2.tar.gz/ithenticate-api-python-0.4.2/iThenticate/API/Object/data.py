1210ab173ce91906e2a078d6f1fa4bd1882283c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:52 +0000

    Add package ithenticate-api-python/0.4.2/ithenticate-api-python-0.4.2.tar.gz

---
+++ packages/ithenticate-api-python/ithenticate-api-python-0.4.2.tar.gz/ithenticate-api-python-0.4.2/iThenticate/API/Object/data.py
@@ -0,0 +1,50 @@
+class Data(dict):
+    def __init__(self, xml, object_name, status=None, messages=None):
+        """
+        Process the freakin' xml instance to a friendly dictionary!
+        """
+        xml_regex = ".//member[name='{object_name}']/value/array/data/value/struct"
+        struct_nodes = xml.findall(xml_regex.format(object_name=object_name))
+
+        content = {
+            'data': [],
+            'status': status or 200,
+            'messages': messages or []
+        }
+        for struct_node in struct_nodes:
+            _dict = self.break_down_struct_node(struct_node)
+            content['data'].append(_dict)
+
+        dict.__init__(self, content)
+
+    def break_down_array_node(self, node):
+        """
+        Dummy: To be implemented to be able to return nested array structs.
+               Now, the node will return an empty string.
+        """
+        return node
+
+    def break_down_struct_node(self, struct):
+        # All items returned
+        node_items = struct.findall('member')
+        _dict = {}
+
+        for item in node_items:
+            # All properties in a single item
+            key = item.find('name').text
+            value_node = item.find('.//value')[0]
+
+            # Value of node
+            if value_node == 'int':
+                value = int(value_node.text.strip())
+            elif value_node == 'array':
+                value = self.break_down_array_node(value_node)
+            elif value_node == 'struct':
+                value = self.break_down_struct_node(value_node)
+            elif value_node == 'string':
+                value = value_node.text.strip()
+            else:
+                # dateTime.iso8601 or something exotic
+                value = value_node.text
+            _dict[key] = value
+        return _dict
