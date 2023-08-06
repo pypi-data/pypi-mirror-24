c31bfc9ce86302a14ae4723e0708e71953580b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:54 +0000

    Add package python-pstore/0.2/python-pstore-0.2.tar.gz

---
+++ packages/python-pstore/python-pstore-0.2.tar.gz/python-pstore-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='python-pstore',
+    version='0.2',
+    packages=['pythonpstore',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
