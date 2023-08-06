6284731393948332c246e2b6116a59ab52aab1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:32 +0000

    Add package grammpy/1.1.6/grammpy-1.1.6.tar.gz

---
+++ packages/grammpy/grammpy-1.1.6.tar.gz/grammpy-1.1.6/grammpy/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:45
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .Rules import Rule
+from .Constants import EPSILON, EPS
+from .Grammars import Grammar
+from .Nonterminal import Nonterminal
