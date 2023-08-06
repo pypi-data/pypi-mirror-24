5ac0fcdb8c31379632a94ba19892339fd269b23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:24 +0000

    Add package pylcurves/0.0.2.1/pylcurves-0.0.2.1-py3-none-any.whl

---
+++ packages/pylcurves/pylcurves-0.0.2.1-py3-none-any.whl/pylcurves/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+'''
+    Le définition de ce package est à initialiser selon ce que l'auteur souhaite en faire.
+'''
+ 
+__version__ = '0.0.2.1'
+ 
+from pylcurves.introduction import introduction
+from pylcurves.learning_curves import plot_learning_curves
