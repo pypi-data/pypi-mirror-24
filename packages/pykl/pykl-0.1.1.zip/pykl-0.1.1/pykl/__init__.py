df7b872306dad377351358a6ed51a56ca2ec9cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package pykl/0.1.1/pykl-0.1.1.zip

---
+++ packages/pykl/pykl-0.1.1.zip/pykl-0.1.1/pykl/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+    pykl
+    ~~~~~
+
+    kltool for python, toolset for web, http, cache, dht, xml, json and so on.
+
+    :copyright: (c) 2017 by Kong Lin.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.1'
+
+import gdom
+
+import pyhtml
+
+import pyjson
+
+import pylext
+
+__all__ = ['gdom', 'pyhtml', 'pyjson', 'pylext']
