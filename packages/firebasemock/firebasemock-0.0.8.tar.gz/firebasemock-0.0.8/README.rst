369eb83dc7705922edcf9569f0d6f4177f2a2cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:39 +0000

    Add package firebasemock/0.0.8/firebasemock-0.0.8.tar.gz

---
+++ packages/firebasemock/firebasemock-0.0.8.tar.gz/firebasemock-0.0.8/README.rst
@@ -0,0 +1,18 @@
+firebase-mock
+=============
+
+**NOTE:** This is alpha quality and is subject to drastic change.
+
+Mock HTTP API's of the Google Firebase and Google Instance ID (IID) services.
+
+Python 3.6+ only.
+
+
+|Version| |Docker|
+
+
+.. |Version| image:: https://img.shields.io/pypi/v/firebasemock.svg?
+   :target: https://pypi.python.org/pypi/firebasemock
+
+.. |Docker| image:: https://img.shields.io/docker/build/nvllsvm/firebase-mock.svg?
+   :target: https://hub.docker.com/r/nvllsvm/firebase-mock/
---
+++ packages/firebasemock/firebasemock-0.0.8.tar.gz/firebasemock-0.0.8/firebasemock/app.py
@@ -0,0 +1,49 @@
+from tornado import ioloop
+from tornado import web
+
+from firebasemock import helpers
+from firebasemock.handlers import admin, firebase, iid
+
+
+STATE = helpers.new_shared_state()
+
+
+def make_app(urls):
+    app = web.Application(urls)
+    app.shared = STATE
+    return app
+
+
+def make_mock_admin_app():
+    return make_app([
+        ('/generate/(.*)', admin.GenerateHandler),
+        ('/messages', admin.MessagesHandler),
+        ('/reset', admin.ResetHandler)
+    ])
+
+
+def make_mock_iid_app():
+    return make_app([
+        ('/iid/v1:batchImport', iid.BatchImportHandler),
+        ('/iid/v1%3AbatchImport', iid.BatchImportHandler)
+    ])
+
+
+def make_mock_firebase_app():
+    return make_app([
+        ('/fcm/send', firebase.SendMessageHandler)
+    ])
+
+
+def run():  # pragma: no cover
+    admin_app = make_mock_admin_app()
+    admin_app.listen(8000)
+    iid_app = make_mock_iid_app()
+    iid_app.listen(8001)
+    firebase_app = make_mock_firebase_app()
+    firebase_app.listen(8002)
+    ioloop.IOLoop.current().start()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
