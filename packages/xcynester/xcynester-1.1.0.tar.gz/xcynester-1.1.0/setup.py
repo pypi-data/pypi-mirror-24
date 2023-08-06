708df052e9e6a803551ecdfc4a858c94176d6706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:47 +0000

    Add package xcynester/1.1.0/xcynester-1.1.0.tar.gz

---
+++ packages/xcynester/xcynester-1.1.0.tar.gz/xcynester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name          ='xcynester',
+        version       ='1.1.0',
+        py_modules    =['xcynester'],
+        author        ='philis',
+        author_email  ='1173942805@qq.com',
+        url           ='https://pypi.python.org/pypi',
+        description   ='A simple printer of xcynested lists',
+    )
