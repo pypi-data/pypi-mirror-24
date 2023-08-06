696f01dcfec9ff60416b99e3047c470da6050296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:52 +0000

    Add package tzwhere/3.0.3/tzwhere-3.0.3.tar.gz

---
+++ packages/tzwhere/tzwhere-3.0.3.tar.gz/tzwhere-3.0.3/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tzwhere',
+    version='3.0.3',
+    packages=['tzwhere'],
+    package_data={
+        'tzwhere': [
+            'tz_world.json.gz',
+            'tz_world_shortcuts.json'
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'shapely'
+    ],
+    license='MIT License',
+    description='Python library to look up timezone from lat / long offline',
+    long_description=README,
+    url='https://github.com/pegler/pytzwhere',
+    author='Matt Pegler',
+    author_email='matt@pegler.co',
+    maintainer='Christoph Stich',
+    maintainer_email='christoph@stich.xyz',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Localization',
+    ],
+)
