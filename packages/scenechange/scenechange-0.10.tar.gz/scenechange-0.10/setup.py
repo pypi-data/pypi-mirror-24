45994d5f21c3c8325846bd38e60c3e3ab3ae51e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:47 +0000

    Add package scenechange/0.10/scenechange-0.10.tar.gz

---
+++ packages/scenechange/scenechange-0.10.tar.gz/scenechange-0.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    author='Kieran O\'Leary',
+    author_email='kieran.o.leary@gmail.com',
+    description='Insert chapter markers for scene changes using Matroska using ffmpeg and mkvpropedit',
+    scripts=['scenechange/scenechange.py'],
+    entry_points={
+        'console_scripts': [
+            'scenechange=scenechange:main',
+        ],
+    },
+    license='MIT',
+    name='scenechange',
+    version='0.10'
+)
