18b3479fcf122057ddae01d043fd86981f4afa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:01 +0000

    Add package regcore/4.1.0/regcore-4.1.0.tar.gz

---
+++ packages/regcore/regcore-4.1.0.tar.gz/regcore-4.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="regcore",
+    version="4.1.0",
+    license="public domain",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'cached_property',
+        'django>=1.8,<1.12',
+        'django-mptt',
+        'jsonschema',
+        'six',
+        'webargs',
+    ],
+    extras_require={
+        'backend-elastic': ['pyelasticsearch'],
+        'backend-haystack': ['django-haystack'],
+        'backend-pgsql': ['django>=1.10', 'psycopg2'],
+    },
+)
