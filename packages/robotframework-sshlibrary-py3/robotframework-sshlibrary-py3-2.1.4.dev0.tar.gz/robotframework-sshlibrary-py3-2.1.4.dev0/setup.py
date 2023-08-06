3556863514a6f0f910a4211709e88082a0e62412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:42 +0000

    Add package robotframework-sshlibrary-py3/2.1.4.dev0/robotframework-sshlibrary-py3-2.1.4.dev0.tar.gz

---
+++ packages/robotframework-sshlibrary-py3/robotframework-sshlibrary-py3-2.1.4.dev0.tar.gz/robotframework-sshlibrary-py3-2.1.4.dev0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from sys import platform
+
+requires = {}
+
+try:
+    from setuptools import setup
+
+    if not platform.startswith('java'):
+        requires = {
+            'install_requires': ['robotframework', 'paramiko >= 1.8.0'],
+        }
+except ImportError:
+    from distutils.core import setup
+
+
+from os.path import abspath, dirname, join
+
+CURDIR = dirname(abspath(__file__))
+exec(compile(open(join(CURDIR, 'src', 'SSHLibrary', 'version.py')).read(), 'version.py', 'exec'))
+with open(join(CURDIR, 'README.rst')) as readme:
+    README = readme.read()
+CLASSIFIERS = """
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
+
+setup(
+    name='robotframework-sshlibrary-py3',
+    version=VERSION, # resolved with execfile
+    description='Robot Framework test library for SSH and SFTP',
+    long_description=README,
+    author='Robot Framework Developers',
+    author_email='robotframework@gmail.com',
+    url='https://github.com/Brian-Williams/SSHLibrary',
+    license='Apache License 2.0',
+    keywords='robotframework testing testautomation ssh sftp',
+    platforms='any',
+    classifiers=CLASSIFIERS.splitlines(),
+    package_dir={'': 'src'},
+    packages=['SSHLibrary'],
+    **requires
+)
