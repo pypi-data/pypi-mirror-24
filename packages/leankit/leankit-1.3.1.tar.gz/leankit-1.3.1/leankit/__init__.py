8d917f48a13cf47551d522983549f1dd697cfe17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:38 +0000

    Add package leankit/1.3.1/leankit-1.3.1.tar.gz

---
+++ packages/leankit/leankit-1.3.1.tar.gz/leankit-1.3.1/leankit/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from logging import getLogger
+
+log = getLogger(__name__)
+
+from .connector import api
+from .kanban import Board
+
+
+__author__ = "Guillermo Guirao Aguilar"
+__email__ = "contact@guillermoguiraoaguilar.com"
+__license__ = "MIT"
+__version__ = "1.3.1"
+
+
+def get_boards():
+    log.debug('Getting boards')
+    return api.get('/Boards')
+
+
+def get_newer_if_exists(board_id, version, timezone='UTC'):
+    """ Downloads a board if a newer version number exists """
+    url = '/Board/{}/BoardVersion/{}/GetNewerIfExists'
+    board = api.get(url.format(board_id, version))
+    if board:
+        return Board(board, timezone)
+    else:
+        return None
