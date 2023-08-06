8b249c1b92859ca227f05e84fed9b9b73492c807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:02 +0000

    Add package pycube/0.1.3/pycube-0.1.3.tar.gz

---
+++ packages/pycube/pycube-0.1.3.tar.gz/pycube-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pycube',
+  packages = ['pycube'],
+  version = '0.1.3',
+  description = 'Python implementation of the Cube cipher',
+  author = 'Karl Zander',
+  author_email = 'pvial00@gmail.com',
+  url = 'https://github.com/pvial00/pycube',
+  download_url = 'https://github.com/pvial00/pycube/archive/0.1.3.tar.gz',
+  keywords = ['cryptography', 'encryption', 'security'],
+  classifiers = [],
+)
