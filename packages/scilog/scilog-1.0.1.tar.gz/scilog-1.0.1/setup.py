c26d9a856adb37cf3867a49db5db00775b96f157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:20 +0000

    Add package scilog/1.0.1/scilog-1.0.1.tar.gz

---
+++ packages/scilog/scilog-1.0.1.tar.gz/scilog-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='scilog',
+      version='1.0.1',
+      long_description=open('README.rst').read(),
+      description='Keep track of numerical experiments',
+      author='Soeren Wolfers',
+      author_email='soeren.wolfers@gmail.com',
+      packages=find_packages(exclude=['*tests']),#,'examples*']),
+      install_requires=['swutil','IPython'],
+      entry_points={
+          'console_scripts': [
+              'scilog = scilog:main',
+          ],
+      }
+)
