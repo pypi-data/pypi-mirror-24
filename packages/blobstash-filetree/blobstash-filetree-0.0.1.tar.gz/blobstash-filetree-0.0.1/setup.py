1af321ea7c9b64bf9014509b98696763f0e088ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:08 +0000

    Add package blobstash-filetree/0.0.1/blobstash-filetree-0.0.1.tar.gz

---
+++ packages/blobstash-filetree/blobstash-filetree-0.0.1.tar.gz/blobstash-filetree-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='blobstash-filetree',
+    version='0.0.1',
+    description='BlobStash FileTree client',
+    long_description=long_description,
+    author='Thomas Sileo',
+    author_email='t@a4.io',
+    url='https://github.com/tsileo/blobstash-python-filetree',
+    packages=['blobstash.filetree'],
+    license='MIT',
+    install_requires=[
+        'blobstash-base==0.0.2',
+        'requests',
+    ],
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='blobstash FileTree client',
+)
