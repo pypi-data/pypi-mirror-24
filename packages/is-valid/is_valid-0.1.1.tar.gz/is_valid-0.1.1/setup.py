3b2ef8c66bf1775eff492f2091ac792739fe71c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:19 +0000

    Add package is-valid/0.1.1/is_valid-0.1.1.tar.gz

---
+++ packages/is-valid/is_valid-0.1.1.tar.gz/is_valid-0.1.1/setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+
+setup(
+    name='is_valid',
+    packages=['is_valid'],
+    package_dir={'is_valid': 'is_valid'},
+    license='MIT',
+    version='0.1.1',
+    description='A small validation library.',
+    author='Daan van der Kallen',
+    author_email='mail@daanvdk.com',
+    url='https://github.com/daanvdk/is_valid',
+    keywords=['validation', 'nested'],
+    classifiers=[],
+    test_suite='tests'
+)
