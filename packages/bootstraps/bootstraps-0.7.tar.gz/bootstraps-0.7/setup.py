83039c1268c83c973c63ec461360d2cab4f137e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:09 +0000

    Add package bootstraps/0.7/bootstraps-0.7.tar.gz

---
+++ packages/bootstraps/bootstraps-0.7.tar.gz/bootstraps-0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'bootstraps',
+  packages = ['bootstraps', 'bootstraps/macros'], # this must be the same as the name above
+  version = '0.7',
+  description = 'A library to handle user related tasks in Flask',
+  author = 'Patrick Allen',
+  author_email = 'patrickgallen@gmail.com',
+  url = 'https://github.com/pgallen90/bootstraps',
+  download_url = 'https://github.com/pgallen90/bootstraps/archive/0.7.tar.gz',
+  keywords = ['bootstrap', 'flask', 'forms'], 
+  classifiers = [],
+  include_package_data=True,
+)
