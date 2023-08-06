d5c1ec482361660843c1b9becfa468e8672741d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:58 +0000

    Add package stereo7/1.1.77/stereo7-1.1.77.tar.gz

---
+++ packages/stereo7/stereo7-1.1.77.tar.gz/stereo7-1.1.77/stereo7/game.py
@@ -0,0 +1,27 @@
+import fileutils
+
+
+class _Cache:
+    _units = []
+
+
+def get_units_list():
+    if _Cache._units:
+        return _Cache._units
+    units = []
+    all_files = []
+    all_files.extend(fileutils.getFilesList(fileutils.root_dir + '/Resources/ini/units/creep/', False))
+    all_files.extend(fileutils.getFilesList(fileutils.root_dir + '/Resources/ini/units/tower/', False))
+    all_files.extend(fileutils.getFilesList(fileutils.root_dir + '/Resources/ini/units/hero/', False))
+    all_files.extend(fileutils.getFilesList(fileutils.root_dir + '/Resources/ini/units/', False))
+    for file in all_files:
+        if file.startswith('_'):
+            continue
+        if not file.endswith('.xml'):
+            continue
+        name = file[0: -4]
+        if name in ['death', 'death2']:
+            continue
+        units.append(name)
+    _Cache._units = units
+    return units
