befafbd6553f62f0aa06dd06ea6616257bf82b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:01 +0000

    Add package pact-test/0.1.48/pact-test-0.1.48.tar.gz

---
+++ packages/pact-test/pact-test-0.1.48.tar.gz/pact-test-0.1.48/pact_test/runners/pact_tests_runner.py
@@ -0,0 +1,44 @@
+from pact_test.either import *
+from pact_test.utils.logger import *
+from pact_test.config.config_builder import Config
+from pact_test.runners.service_consumers.test_suite import ServiceConsumerTestSuiteRunner
+from pact_test.runners.service_providers.provider_tests_runner import ProviderTestsRunner
+
+
+def verify(verify_consumers=False, verify_providers=False):
+    config = Config()
+
+    if verify_consumers:
+        run_consumer_tests(config)
+    if verify_providers:
+        run_provider_tests(config)
+
+
+def run_consumer_tests(config):
+    test_results = ServiceConsumerTestSuiteRunner(config).verify()
+    if type(test_results) is Left:
+        error(test_results.value)
+    else:
+        if type(test_results.value) is Left:
+            error(test_results.value.value)
+        else:
+            for test_result in test_results.value:
+                print()
+                info('Test: ' + test_result.value['test'])
+                for result in test_result.value['results']:
+                    info('  GIVEN ' + result.value['state'] + ' UPON RECEIVING ' + result.value['description'])
+                    info('    status: ' + result.value['status'])
+                    # info('  Description: ' + result.value['description'])
+                    # info('  State:       ' + result.value['state'])
+                    # info('  Status:      ' + result.value['status'])
+                    for test_error in result.value['errors']:
+                        error('      expected: ' + str(test_error['expected']))
+                        error('      actual:   ' + str(test_error['actual']))
+                        error('      message:  ' + str(test_error['message']))
+    info('')
+    info('Goodbye!')
+    print()
+
+
+def run_provider_tests(config):
+    ProviderTestsRunner(config).verify()
