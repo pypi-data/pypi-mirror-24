6c6864975d42b1b39624d817f7d36eb0d46e2281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:18 +0000

    Add package tsstats/1.5.1/tsstats-1.5.1.tar.gz

---
+++ packages/tsstats/tsstats-1.5.1.tar.gz/tsstats-1.5.1/tsstats/client.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import logging
+from collections import MutableMapping
+
+from tsstats.exceptions import InvalidLog
+
+logger = logging.getLogger('tsstats')
+
+
+class Clients(MutableMapping):
+    '''
+    A high-level-interface to multiple Client-objects
+    '''
+    def __init__(self, ident_map=None, *args, **kwargs):
+        '''
+        Initialize a new Client-collection
+
+        :param ident_map: Identity-map (see :doc:`identmap`)
+        :type ident_map: dict
+        '''
+        self.ident_map = ident_map or {}
+
+        self.store = dict()
+        self.update(dict(*args, **kwargs))
+
+    def __add__(self, client):
+        '''
+        Add a Client to the collection
+
+        :param client: Client to add to the collection
+        :type id_or_uid: Client
+        '''
+        identifier = client.identifier
+        self.store[self.ident_map.get(identifier, identifier)] = client
+        return self
+
+    def __iter__(self):
+        '''
+        Yield all Client-objects from the collection
+        '''
+        return iter(self.store.values())
+
+    def __getitem__(self, key):
+        return self.store[self.ident_map.get(key, key)]
+
+    def __delitem__(self, key):
+        del self.store[key]
+
+    def __len__(self):
+        return len(self.store)
+
+    def __setitem__(self, key, value):
+        self.store[self.ident_map.get(key, key)] = value
+
+
+class Client(object):
+    '''
+    Client provides high-level-access to a Teamspeak-Client
+    '''
+
+    def __init__(self, identifier, nick=None):
+        '''
+        Initialize a new Client
+
+        :param identifier: Identifier of the client
+        :type identifier: int or str
+        '''
+        # public
+        self.identifier = identifier
+        self.nick = nick
+        self.nick_history = set()
+        self.connected = 0
+        self.onlinetime = datetime.timedelta()
+        self.kicks = 0
+        self.pkicks = 0
+        self.bans = 0
+        self.pbans = 0
+        self.last_seen = None
+        # private
+        self._last_connect = 0
+
+    def connect(self, timestamp):
+        '''
+        Connect client at `timestamp`
+
+        :param timestamp: time of connect
+        :type timestamp: int
+        '''
+        logger.debug('[%s] CONNECT %s', timestamp, self)
+        self.connected += 1
+        self._last_connect = timestamp
+
+    def disconnect(self, timestamp):
+        '''
+        Disconnect client at `timestamp`
+
+        :param timestamp: time of disconnect
+        :type timestamp: int
+        '''
+        logger.debug('[%s] DISCONNECT %s', timestamp, self)
+        if not self.connected:
+            logger.debug('^ disconnect before connect')
+            raise InvalidLog('disconnect before connect!')
+        self.connected -= 1
+        session_time = timestamp - self._last_connect
+        logger.debug('Session lasted %s', session_time)
+        self.onlinetime += session_time
+        self.last_seen = timestamp
+
+    def kick(self, target):
+        '''
+        Let client kick `target`
+
+        :param target: client to kick
+        :type target: Client
+        '''
+        logger.debug('KICK %s -> %s', self, target)
+        target.pkicks += 1
+        self.kicks += 1
+
+    def ban(self, target):
+        '''
+        Let client ban `target`
+
+        :param target: client to ban
+        :type target: Client
+        '''
+        logger.debug('BAN %s -> %s', self, target)
+        target.pbans += 1
+        self.bans += 1
+
+    def __str__(self):
+        return u'<{}, {}>'.format(self.identifier, self.nick)
---
+++ packages/tsstats/tsstats-1.5.1.tar.gz/tsstats-1.5.1/tsstats/exceptions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+class InvalidConfiguration(Exception):
+    '''
+    The configuration is invalid (either config-file or cli-args)
+    '''
+
+
+class InvalidLog(Exception):
+    '''
+    Something impossible appeared at the logs,
+    for example a disconnect before a connect
+    '''
+    pass
