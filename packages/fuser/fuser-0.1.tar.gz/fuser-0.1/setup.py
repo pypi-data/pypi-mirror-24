cc7c2af274c968c1682acc2f8ecd021793679301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:15 +0000

    Add package fuser/0.1/fuser-0.1.tar.gz

---
+++ packages/fuser/fuser-0.1.tar.gz/fuser-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fuser',
+  packages = ['fuser'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library to handle user related tasks in Flask',
+  author = 'Patrick Allen',
+  author_email = 'patrickgallen@gmail.com',
+  url = 'https://github.com/pgallen90/fuser',
+  download_url = 'https://github.com/pgallen90/fuser/archive/0.1.tar.gz',
+  keywords = ['flask', 'auth', 'password', 'login', 'dashboard'], 
+  classifiers = [],
+)
