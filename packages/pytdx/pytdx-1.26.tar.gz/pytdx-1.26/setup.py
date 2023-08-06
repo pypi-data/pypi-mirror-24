757448c578a3f81aa4587e8d425af23aa6fb3496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:15 +0000

    Add package pytdx/1.26/pytdx-1.26.tar.gz

---
+++ packages/pytdx/pytdx-1.26.tar.gz/pytdx-1.26/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+
+setup(
+    name='pytdx',
+    version='1.26',
+    description='A Python Interface to TDX protocol',
+    long_description=long_description,
+    author='RainX<Jing Xu>',
+    author_email='i@rainx.cc',
+    url='https://github.com/rainx/pytdx',
+    packages=find_packages(),
+    install_requires=[
+          'click',
+          'pandas',
+          'six'
+    ],
+    entry_points={
+          'console_scripts': [
+              'hqget=pytdx.bin.hqget:main',
+              'hqreader=pytdx.bin.hqreader:main'
+
+          ]
+      }
+    )
+
