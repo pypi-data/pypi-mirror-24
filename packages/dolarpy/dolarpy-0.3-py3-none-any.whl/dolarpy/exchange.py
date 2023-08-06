2b71f8063abd4f9ef702e9a12d797df9a7d66108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:17 +0000

    Add package dolarpy/0.3/dolarpy-0.3-py3-none-any.whl

---
+++ packages/dolarpy/dolarpy-0.3-py3-none-any.whl/dolarpy/exchange.py
@@ -0,0 +1,17 @@
+class Exchange(object):
+
+    def __init__(self, nombre=None, compra=None, venta=None, updated=None):
+        self.nombre = nombre
+        self.compra = compra
+        self.venta = venta
+        self.updated = updated
+
+    def __repr__(self):
+        return str(self.nombre)
+
+
+class BCP(Exchange):
+
+    def __init__(self, nombre=None, compra=None, venta=None, referencial=None, updated=None):
+        self.referencial = referencial
+        super(BCP, self).__init__(nombre, compra, venta, updated)
