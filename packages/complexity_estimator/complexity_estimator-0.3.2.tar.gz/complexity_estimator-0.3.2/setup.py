c45c9b0af42dc7a78a27b5bdff5c0cbc18e9efe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:03 +0000

    Add package complexity_estimator/0.3.2/complexity_estimator-0.3.2.tar.gz

---
+++ packages/complexity_estimator/complexity_estimator-0.3.2.tar.gz/complexity_estimator-0.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='complexity_estimator',
+      version='0.3.2',
+      description='Estimating complexity of given function in a given module',
+      url='https://github.com/AGHPythonCourse2017/zad2-detori',
+      author='Wojciech Kubaty',
+      include_package_data=True,
+      packages=['complexity_estimator'],
+      zip_safe=False)
