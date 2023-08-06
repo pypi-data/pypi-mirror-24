637e0678d9ffdc87f99d804fa5db3e9692790177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:00 +0000

    Add package Pygroove/1.0.2/Pygroove-1.0.2.tar.gz

---
+++ packages/Pygroove/Pygroove-1.0.2.tar.gz/Pygroove-1.0.2/pygroove/__init__.py
@@ -0,0 +1,1 @@
+from .pygroove import Groove
---
+++ packages/Pygroove/Pygroove-1.0.2.tar.gz/Pygroove-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name = 'Pygroove',
+  packages = ['pygroove'], 
+  version = '1.0.2',
+  description = 'A python wrapper for Microsoft Groove Music API',
+  author = 'P G Nithin Reddy',
+  author_email = 'reddy.nithinpg@live.com',
+  license='MIT',
+  url = 'https://github.com/rednithin/Pygroove', 
+  keywords = ['Groove Music', 'Groove', 'API'], 
+  classifiers = [
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3.6'
+  ],
+  install_requires=[
+      'requests==2.18.3'
+  ]
+)
