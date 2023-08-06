270e07ec2c13e19c8b1501f5c4ac6739154b6bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:54 +0000

    Add package gpgmailencrypt/3.3.0/gpgmailencrypt-3.3.0.tar.gz

---
+++ packages/gpgmailencrypt/gpgmailencrypt-3.3.0.tar.gz/gpgmailencrypt-3.3.0/gmeutils/child.py
@@ -0,0 +1,53 @@
+#License GPL v3
+#Author Horst Knorr <gpgmailencrypt@gmx.de>
+
+##########
+#_gmechild
+##########
+
+class _gmechild:
+	"base class of all classes that will be used from class gme"
+
+	def __init__(self,parent,filename):
+		self.parent=parent
+		self._level=1
+		self.filename=filename
+
+	def log(self,
+			msg,
+			infotype="m",
+			ln=-1,
+			filename=""):
+
+		if filename=="":
+			filename=self.filename
+
+		try:
+			self.parent._logger._level+=self._level
+			self.parent.log(msg=msg,infotype=infotype,ln=ln,filename=filename)
+			self.parent._logger._level-=self._level
+		except:
+			pass
+
+	def log_traceback(self):
+
+		try:
+			self.parent.log_traceback()
+		except:
+			pass
+
+	def debug(  self,
+				msg,
+				lineno=0,
+				filename=""):
+
+		if filename=="":
+			filename=self.filename
+
+		try:
+			self.parent._logger._level+=self._level
+			self.parent.debug(msg=msg,lineno=lineno,filename=filename)
+			self.parent._logger._level-=self._level
+		except:
+			pass
+
---
+++ packages/gpgmailencrypt/gpgmailencrypt-3.3.0.tar.gz/gpgmailencrypt-3.3.0/gmeutils/version.py
@@ -0,0 +1,19 @@
+#License GPL v3
+#Author Horst Knorr <gpgmailencrypt@gmx.de>
+VERSION="3.3.0"
+DATE="18.08.2017"
+COPYRIGHTYEAR="2017"
+unicodeerror="replace"
+#330 mail decryption added
+#320 use additional keys for encryption
+#passwords user better hashing algorithms via passlib
+#sercurelevels may|redirect|bounce
+#database improvements
+#create tables
+#add pdf password storage in database
+#working mysql, sqlite3 and postgres.
+#MSSQL remains untested
+#Admin console supports createtable command
+#Admin console supports resetmessages command
+#gpgclass can extract public keys
+
