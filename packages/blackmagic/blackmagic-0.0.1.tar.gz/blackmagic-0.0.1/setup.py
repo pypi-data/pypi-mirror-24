dabc71acef8c865e37d85161af297fe943ef78cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package blackmagic/0.0.1/blackmagic-0.0.1.tar.gz

---
+++ packages/blackmagic/blackmagic-0.0.1.tar.gz/blackmagic-0.0.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='blackmagic',
+    packages=['blackmagic'],
+    version='0.0.1',
+    description='A Python black magic library.',
+    long_description=readme(),
+    url='https://github.com/devunt/blackmagic',
+    download_url='',
+    author='Bae Junehyeon',
+    author_email='devunt' '@' 'gmail.com',
+    license='MIT License (MIT)',
+    py_modules=['blackmagic'],
+    keywords=['magic', 'hack'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Debuggers',
+    ],
+)
