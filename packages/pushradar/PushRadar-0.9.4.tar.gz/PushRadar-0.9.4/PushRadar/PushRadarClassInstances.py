d11b3216a8b366c486b99ae9f92e210e8a542a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:09 +0000

    Add package pushradar/0.9.4/PushRadar-0.9.4.tar.gz

---
+++ packages/pushradar/PushRadar-0.9.4.tar.gz/PushRadar-0.9.4/PushRadar/PushRadarClassInstances.py
@@ -0,0 +1,5 @@
+class PushRadarClassInstances:
+    """ Stores instances of classes used throughout the library to avoid re-initialisation. """
+
+    # The instance of the PushRadar API client
+    api_client = None
