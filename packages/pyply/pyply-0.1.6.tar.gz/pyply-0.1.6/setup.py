9a29d28e3be50f35c547f406b0704f5ee4b9a47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:06 +0000

    Add package pyply/0.1.6/pyply-0.1.6.tar.gz

---
+++ packages/pyply/pyply-0.1.6.tar.gz/pyply-0.1.6/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='pyply',
+      version='0.1.6',
+      description="""Based largely off functions found in R, pyply provides syntax in a functional programming style to make data manipulation easier.
+                     This is meant especially for those transitioning from R to Python.""",
+      url='http://theautomatic.net/pyply-documentation/',
+      author='Andrew Treadway',
+      author_email='opensourcecoder11@gmail.com',
+      license='',
+      packages=['pyply'],
+      zip_safe=False)
