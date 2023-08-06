f29244703c32b96d7753c7a2038c39819d14f63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:00 +0000

    Add package random-script/0.4/random_script-0.4.tar.gz

---
+++ packages/random-script/random_script-0.4.tar.gz/random_script-0.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
+setup(
+    name='random_script',
+
+    version='0.4',
+
+    description='A sample Python project',
+    long_description= "empty",
+
+    url='https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=random-script',
+
+    author='Ahmed Maher',
+    author_email='ahmed_maher1992@hotmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6'
+    ],
+	
+    keywords='sample setuptools development',
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    py_modules=["random_script"],
+    # install_requires=['peppercorn'],
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+    # data_files=[('my_data', ['data/data_file'])],
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+)
