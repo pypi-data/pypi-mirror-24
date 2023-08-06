4ff5580f67651aca7b58f1eae1d1dd4b4096cd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:58 +0000

    Add package llamicron-i/0.1/llamicron-i-0.1.tar.gz

---
+++ packages/llamicron-i/llamicron-i-0.1.tar.gz/llamicron-i-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='llamicron-i',
+    packages=['i'],
+    version='0.1',
+    description='Connect to servers',
+    author='Luke Sweeney',
+    author_email='luke@thesweeneys.org',
+    url='https://github.com/llamicron/i',
+    download_url='https://github.com/llamicron/i/archive/0.1.tar.gz',
+    keywords=['ssh', 'server', 'connect'],
+    classifiers=[],
+)
