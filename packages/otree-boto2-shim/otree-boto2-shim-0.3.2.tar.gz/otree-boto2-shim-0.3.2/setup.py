b8f808886713d76d8231db7e01f5b453cf6b0fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:01 +0000

    Add package otree-boto2-shim/0.3.2/otree-boto2-shim-0.3.2.tar.gz

---
+++ packages/otree-boto2-shim/otree-boto2-shim-0.3.2.tar.gz/otree-boto2-shim-0.3.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="otree-boto2-shim",
+	version='0.3.2',
+	description="Shim package so that boto2 imports don't fail when using oTree.",
+    url='http://www.otree.org',
+	author='Chris',
+	author_email='chris@otree.org',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+)
