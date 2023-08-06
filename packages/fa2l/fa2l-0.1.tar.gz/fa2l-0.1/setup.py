5ec4d0ba9f50694666f6d5d67328a6b59785afe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:38 +0000

    Add package fa2l/0.1/fa2l-0.1.tar.gz

---
+++ packages/fa2l/fa2l-0.1.tar.gz/fa2l-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="fa2l",
+    version="0.1",
+    author="Eugene Bosiakov",
+    author_email="eugenebosyakov@gmail.com",
+    url='https://github.com/bosiakov/fa2l',
+    description="Force Atlas 2 graph layout",
+    packages=['fa2l'],
+    install_requires=[
+        "networkx<2.0.0",
+        "numpy"
+    ],
+)
