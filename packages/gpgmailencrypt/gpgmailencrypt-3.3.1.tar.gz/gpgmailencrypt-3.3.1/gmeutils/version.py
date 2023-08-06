3968fc1fa600401c8ba421f79b5f4ce8a7f90280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:36 +0000

    Add package gpgmailencrypt/3.3.1/gpgmailencrypt-3.3.1.tar.gz

---
+++ packages/gpgmailencrypt/gpgmailencrypt-3.3.1.tar.gz/gpgmailencrypt-3.3.1/gmeutils/version.py
@@ -0,0 +1,20 @@
+#License GPL v3
+#Author Horst Knorr <gpgmailencrypt@gmx.de>
+VERSION="3.3.1"
+DATE="31.08.2017"
+COPYRIGHTYEAR="2017"
+unicodeerror="replace"
+#331 securitylevel=script and pdfpasswordmode=script, starting windows support 
+#330 mail decryption added
+#320 use additional keys for encryption
+#passwords user better hashing algorithms via passlib
+#sercurelevels may|redirect|bounce
+#database improvements
+#create tables
+#add pdf password storage in database
+#working mysql, sqlite3 and postgres.
+#MSSQL remains untested
+#Admin console supports createtable command
+#Admin console supports resetmessages command
+#gpgclass can extract public keys
+
