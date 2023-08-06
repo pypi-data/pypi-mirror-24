f329bfd811a9ae152f7890725e1b191d2ef120ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package neurokit/0.2.0/neurokit-0.2.0.tar.gz

---
+++ packages/neurokit/neurokit-0.2.0.tar.gz/neurokit-0.2.0/README.rst
@@ -0,0 +1,7 @@
+NeuroKit.py 
+###############
+A Python Toolbox for Statistics and Neurophysiological Signal Processing (EEG, EDA, ECG, EMG...).
+
+
+Click `here for the main webpage <https://github.com/neuropsychology/NeuroKit.py>`_.
+
---
+++ packages/neurokit/neurokit-0.2.0.tar.gz/neurokit-0.2.0/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+import re
+
+
+# ------------------
+def find_version():
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format("__version__"), open('neurokit/__init__.py').read())
+    return result.group(1)
+# ------------------
+
+
+
+# ------------------
+setup(
+name = "neurokit",
+description = ("A Python Toolbox for Statistics and Signal Processing (EEG, EDA, ECG, EMG...)."),
+version = find_version(),
+license = "MIT",
+author = "Dominique Makowski",
+author_email = "dom.makowski@gmail.com",
+maintainer = "Dominique Makowski",
+maintainer_email = "dom.makowski@gmail.com",
+packages = find_packages(),
+package_data = {
+        "neurokit.materials":["*.model"]},
+install_requires = [
+        'numpy',
+        'pandas',
+        'scipy',
+        'sklearn',
+        'matplotlib',
+        'mne',
+        'bioread',
+        'nolds',
+        'biosppy',
+        'Pillow',
+        'cvxopt'],
+dependency_links=[],
+long_description = open('README.md').read(),
+keywords = "python signal processing EEG EDA ECG hrv rpeaks biosignals complexity",
+url = "https://github.com/neuropsychology/NeuroKit.py",
+download_url = 'https://github.com/neuropsychology/NeuroKit.py/tarball/master',
+test_suite="nose.collector",
+tests_require=[
+        'pytest',
+        'nose',
+        'coverage'],
+classifiers = [
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6']
+)
