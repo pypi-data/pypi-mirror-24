a3495ef78009f9f9567b16ca00433f30551e02e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:32 +0000

    Add package amaasinfra/0.2.7.28/amaasinfra-0.2.7.28.tar.gz

---
+++ packages/amaasinfra/amaasinfra-0.2.7.28.tar.gz/amaasinfra-0.2.7.28/amaasinfra/infra/cache/asset_manager_cache_item.py
@@ -0,0 +1,16 @@
+import os
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
+
+class AssetManagerRelationshipCacheItem(Model):
+    """
+    Cached record for asset manager
+    """
+    class Meta:
+        environment = os.environ.get('AWS_ENV') or 'dev'
+        region = os.environ.get('AWS_DEFAULT_REGION', 'ap-southeast-1')
+        table_name = "asset-manager-relationship-cache-{}".format(environment.lower())
+    asset_manager_id = UnicodeAttribute(hash_key=True)
+    related_id = UnicodeAttribute(range_key=True)
+    relationship_type = UnicodeAttribute(null = False)
+    relationship_status = UnicodeAttribute(null = False)    
