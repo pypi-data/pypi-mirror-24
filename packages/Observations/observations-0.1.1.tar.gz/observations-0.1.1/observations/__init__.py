c1cbd4d211f68527db4c9690a7fbb5a3a09619f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:36 +0000

    Add package Observations/0.1.1/observations-0.1.1.tar.gz

---
+++ packages/Observations/observations-0.1.1.tar.gz/observations-0.1.1/observations/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from observations.abalone import abalone
+from observations.boston_housing import boston_housing
+from observations.caltech101_silhouettes import caltech101_silhouettes
+from observations.celeba import celeba
+from observations.cifar10 import cifar10
+from observations.cifar100 import cifar100
+from observations.enwik8 import enwik8
+from observations.enwik8 import enwik8
+from observations.iris import iris
+from observations.lsun import lsun
+from observations.mnist import mnist
+from observations.nips import nips
+from observations.ptb import ptb
+from observations.sick import sick
+from observations.small32_imagenet import small32_imagenet
+from observations.small64_imagenet import small64_imagenet
+from observations.snli import snli
+from observations.stanford_sentiment_treebank import stanford_sentiment_treebank
+from observations.svhn import svhn
+from observations.text8 import text8
+from observations.util import maybe_download_and_extract
+from observations.wikitext2 import wikitext2
+from observations.wine import wine
+from observations.yelp17 import yelp17
+
+__version__ = '0.1.1'
+VERSION = __version__
---
+++ packages/Observations/observations-0.1.1.tar.gz/observations-0.1.1/observations/boston_housing.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from observations.util import maybe_download_and_extract
+
+
+def boston_housing(path):
+  """Load the Boston Housing data set (Harrison and Rubinfeld, 1978).
+  It contains 506 examples of housing values in suburbs of Boston,
+  each with 13 continuous attributes and 1 binary attribute.
+
+  Args:
+    path: str.
+      Path to directory which either stores file or otherwise file will
+      be downloaded and extracted there. Filename is `housing.data`.
+
+  Returns:
+    Tuple of np.darray `x_train` and dictionary `metadata` of column
+    headers (feature names).
+  """
+  import pandas as pd
+  path = os.path.expanduser(path)
+  filename = 'housing.data'
+  if not os.path.exists(os.path.join(path, filename)):
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data'
+    maybe_download_and_extract(path, url)
+
+  x_train = pd.read_csv(os.path.join(path, filename),
+                        header=None, delimiter=r"\s+").as_matrix()
+  columns = ['CRIM',
+             'ZN',
+             'INDUS',
+             'CHAS',
+             'NOX',
+             'RM',
+             'AGE',
+             'DIS',
+             'RAD',
+             'TAX',
+             'PTRATIO',
+             'B',
+             'LSTAT',
+             'MEDV']
+  metadata = {'columns': columns}
+  return x_train, metadata
