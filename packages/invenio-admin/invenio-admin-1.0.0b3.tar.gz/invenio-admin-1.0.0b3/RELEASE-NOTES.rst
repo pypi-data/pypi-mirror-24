e098784203054785443a7d0f4098410ce5f097b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:43 +0000

    Add package invenio-admin/1.0.0b3/invenio-admin-1.0.0b3.tar.gz

---
+++ packages/invenio-admin/invenio-admin-1.0.0b3.tar.gz/invenio-admin-1.0.0b3/RELEASE-NOTES.rst
@@ -0,0 +1,34 @@
+========================
+ Invenio-Admin v1.0.0b3
+========================
+
+Invenio-Admin v1.0.0b3 was released on August 10, 2017.
+
+About
+-----
+
+Administration interface for Invenio.
+
+What's new
+----------
+
+- Initial public release.
+
+Installation
+------------
+
+   $ pip install invenio-admin==1.0.0b3
+
+Documentation
+-------------
+
+   https://invenio-admin.readthedocs.io/
+
+Happy hacking and thanks for flying Invenio-Admin.
+
+| Invenio Development Team
+|   Email: info@inveniosoftware.org
+|   IRC: #invenio on irc.freenode.net
+|   Twitter: https://twitter.com/inveniosoftware
+|   GitHub: https://github.com/inveniosoftware/invenio-admin
+|   URL: http://inveniosoftware.org
---
+++ packages/invenio-admin/invenio-admin-1.0.0b3.tar.gz/invenio-admin-1.0.0b3/tests/demo/three.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Mocks of DB Models and Admin's ModelViews for entrypoint testing."""
+
+from flask_admin.contrib.sqla import ModelView
+from invenio_db import db
+
+
+class ModelThree(db.Model):
+    """Test model with just one column."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    """Id of the model."""
+
+
+class ModelThreeModelView(ModelView):
+    """AdminModelView of the ModelOne."""
+
+    pass
+
+three = dict(modelview=ModelThreeModelView, model=ModelThree)
