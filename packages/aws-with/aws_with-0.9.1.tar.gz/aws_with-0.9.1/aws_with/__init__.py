ef5d28fb551aefa8742b0ad979e6a40ed1135900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:36 +0000

    Add package aws-with/0.9.1/aws_with-0.9.1.tar.gz

---
+++ packages/aws-with/aws_with-0.9.1.tar.gz/aws_with-0.9.1/aws_with/__init__.py
@@ -0,0 +1,14 @@
+""" aws_with module definition """
+
+from . import utils
+from . import regions
+from . import organizations
+from . import commands
+from . import workplan
+from . import output
+from . import cli
+from . import main
+
+VERSION = "0.9.1"
+BUILD_TIME = "2017-08-27 11:14:00"
+BUILD_COMMIT_HASH = "0012300400g0hf020304f34f"
