0c7aeac45bd00f360d57b16f4c7320f4c36c5f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:47 +0000

    Add package sci2pdf/0.0.2/sci2pdf-0.0.2.tar.gz

---
+++ packages/sci2pdf/sci2pdf-0.0.2.tar.gz/sci2pdf-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+readme = open('README','r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name="sci2pdf",
+    version="0.0.2",
+    packages = find_packages(exclude=["build",]),
+    scripts=["sci2pdf/bin/sci2pdf"],
+    long_description = README_TEXT,
+    install_requires = ["bibtexparser",
+                        "titletobib",
+                        "future",
+                        "requests",
+                        "lxml"],
+    include_package_data=True,
+    license="GPLv3",
+    description="Downloads pdfs via a DOI number, article title or a bibtex file, sci-hub",
+    author="Bruno Messias",
+    author_email="messias.physics@gmail.com",
+    download_url="https://github.com/bibcure/sci2pdf/archive/0.0.2.tar.gz",
+    keywords=["bibtex", "sci-hub", "libgen", "doi",  "science","scientific-journals"],
+
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Topic :: Text Processing :: Markup :: LaTeX",
+    ],
+    url="https://github.com/bibcure/sci2pdf"
+)
