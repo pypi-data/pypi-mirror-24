f80b4f3910d673c379baed21a861a97e03d6a31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:53 +0000

    Add package ProsperDatareader/1.0.0/ProsperDatareader-1.0.0.tar.gz

---
+++ packages/ProsperDatareader/ProsperDatareader-1.0.0.tar.gz/ProsperDatareader-1.0.0/prosper/datareader/config.py
@@ -0,0 +1,3 @@
+import prosper.common.prosper_logging as p_logging
+
+LOGGER = p_logging.DEFAULT_LOGGER
---
+++ packages/ProsperDatareader/ProsperDatareader-1.0.0.tar.gz/ProsperDatareader-1.0.0/prosper/datareader/exceptions.py
@@ -0,0 +1,34 @@
+"""exceptions.py: collection of exceptions for Datareader libraries"""
+
+class DatareaderException(Exception):
+    """base class for Datareader exceptions"""
+    pass
+class DatareaderWarning(UserWarning):
+    """base class for Datareader warnings"""
+    pass
+class PaginationWarning(DatareaderWarning):
+    """hard limit reached for recursive page diving"""
+    pass
+
+###########
+## Utils ##
+###########
+class UtilsNLTKDownloadFailed(DatareaderException):
+    """Unable to download lexicon from NLTK corpus"""
+    pass
+class VaderClassificationException(DatareaderException):
+    """Invalid options for mapping vader->pandas"""
+    pass
+
+############
+## Stocks ##
+############
+class StocksException(DatareaderException):
+    """base class for Datareader.stocks"""
+    pass
+class StocksPricesException(StocksException):
+    """base class for Datareader.stocks.prices"""
+    pass
+class StocksNewsException(StocksException):
+    """base class for Datareader.stocks.prices"""
+    pass
