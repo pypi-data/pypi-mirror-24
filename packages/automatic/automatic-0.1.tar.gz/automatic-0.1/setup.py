777399606a8ece7ef77b1452eb5dfe6e4777ea29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:41 +0000

    Add package automatic/0.1/automatic-0.1.tar.gz

---
+++ packages/automatic/automatic-0.1.tar.gz/automatic-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'automatic',
+  packages = ['automatic'],
+  version = '0.1',
+  description = 'Client library for the Automatic API',
+  author = 'Kyle Rector',
+  author_email = 'kylearector@outlook.com',
+  url = 'https://github.com/KyleARector/automatic',
+  keywords = ['car', 'automatic'],
+  classifiers = [],
+)
