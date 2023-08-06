61208ceeefe18ca2133bd9667f4d90f19f3ec659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:15 +0000

    Add package scsequtil/0.0.1/scsequtil-0.0.1.tar.gz

---
+++ packages/scsequtil/scsequtil-0.0.1.tar.gz/scsequtil-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='scsequtil',
+    version='0.0.1',
+    description='Utilities for large scale single cell data processing',
+    author='Ambrose J. Carr',
+    author_email='mail@ambrosejcarr.com',
+    package_dir={'': 'src'},
+    packages=['scsequtil', 'scsequtil/plot', 'scsequtil/test'],
+    install_requires=[
+        'numpy',
+        'pysam',
+        'matplotlib>=2.0.0',
+        'nose2',
+        'jinja2',
+        'weasyprint'
+    ],
+    include_package_data=True
+)
