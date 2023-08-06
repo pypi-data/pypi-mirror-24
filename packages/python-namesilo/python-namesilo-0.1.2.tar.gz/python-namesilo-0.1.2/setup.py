9a89c2e7c3c5719badf72a16b90f49a4b961d928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package python-namesilo/0.1.2/python-namesilo-0.1.2.tar.gz

---
+++ packages/python-namesilo/python-namesilo-0.1.2.tar.gz/python-namesilo-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='python-namesilo',
+    version='0.1.2',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=['requests', 'xmltodict'],
+    python_requires='>=3.6',
+    py_modules=['namesilo'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    url='https://github.com/goranvrbaski/python-namesilo',
+    license='MIT',
+    author='Goran Vrbaski',
+    author_email='vrbaski.goran@gmail.com',
+    description='API wrapper for Namesilo service'
+)
