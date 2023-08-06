5cfe8dbef3d7a9aad5c471f8b3594015433d3ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:15 +0000

    Add package us-polygon-mapper/1.0.1/us_polygon_mapper-1.0.1.tar.gz

---
+++ packages/us-polygon-mapper/us_polygon_mapper-1.0.1.tar.gz/us_polygon_mapper-1.0.1/us_polygon_mapper/color_rgb_dict.py
@@ -0,0 +1,22 @@
+color_rgb_dict = {
+    'red': (255, 0, 0),
+    'light_red': (240, 128, 128),
+
+    'orange': (255, 102, 0),
+    'light_orange': (255, 190, 97),
+
+    'yellow': (255, 204, 0),
+    'light_yellow': (255, 255, 125),
+
+    'green': (0, 204, 0),
+    'light_green': (102, 255, 102),
+
+    'blue': (0, 0, 255),
+    'light_blue': (100, 149, 237),
+
+    'purple': (128, 0, 128),
+    'light_purple': (193, 114, 213),
+
+    'brown': (139, 69, 19),
+    'light_brown': (215, 175, 123)
+}
