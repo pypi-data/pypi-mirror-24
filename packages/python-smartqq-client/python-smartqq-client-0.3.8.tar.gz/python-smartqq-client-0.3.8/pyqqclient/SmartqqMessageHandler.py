fbcf68f883550726861a7ccdf9b513cd147a2c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:34 +0000

    Add package python-smartqq-client/0.3.8/python-smartqq-client-0.3.8.tar.gz

---
+++ packages/python-smartqq-client/python-smartqq-client-0.3.8.tar.gz/python-smartqq-client-0.3.8/pyqqclient/SmartqqMessageHandler.py
@@ -0,0 +1,49 @@
+from .MessageHandler import MessageHandler
+from .MessageErrcodeException import MessageErrcodeException
+
+
+class SmartqqMessageHandler(MessageHandler):
+    @staticmethod
+    def friend_message_filter(message):
+        return message["poll_type"] == "message"
+
+    @staticmethod
+    def group_message_filter(message):
+        return message["poll_type"] == "group_message"
+
+    @staticmethod
+    def friend_message_print_handler(message):
+        print("this is a friend message")
+        return False
+
+    @staticmethod
+    def group_message_print_handler(message):
+        print("this is a group message")
+        return False
+
+    @staticmethod
+    def any_message_handler(message):
+        print("this message has been ignored")
+        return False
+
+    @staticmethod
+    def print_all_handler(friend_message_handler=None, group_message_handler=None):
+
+        def message_preprocess(data):
+            message = data.json()
+            if "errcode" in message:
+                print(message)
+                raise MessageErrcodeException
+            if "result" not in message:
+                print(message)
+                raise MessageErrcodeException
+            return message["result"][0]
+        result = SmartqqMessageHandler(message_preprocess)
+        result.add_handler(SmartqqMessageHandler.friend_message_filter,
+                           friend_message_handler if friend_message_handler is not None
+                           else SmartqqMessageHandler.friend_message_print_handler)
+        result.add_handler(SmartqqMessageHandler.group_message_filter,
+                           group_message_handler if group_message_handler is not None
+                           else SmartqqMessageHandler.group_message_print_handler)
+        result.add_handler(SmartqqMessageHandler.any, SmartqqMessageHandler.any_message_handler)
+        return result
