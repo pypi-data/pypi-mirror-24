64b9e0cf309e06a635ea74a2bd5c38d6f9da58e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:03 +0000

    Add package scotchwsgi/0.1.1/scotchwsgi-0.1.1.tar.gz

---
+++ packages/scotchwsgi/scotchwsgi-0.1.1.tar.gz/scotchwsgi-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='scotchwsgi',
+    version='0.1.1',
+    author='Christopher Thorne',
+    author_email='libcthorne@gmail.com',
+    url='https://cthorne.me',
+    packages=['scotchwsgi'],
+    scripts=[
+        'bin/scotchwsgi',
+    ],
+)
