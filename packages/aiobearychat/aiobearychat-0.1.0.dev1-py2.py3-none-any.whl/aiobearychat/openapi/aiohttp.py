cb8f02e67a836c3e5cd5da3c130df0d32733e1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:46 +0000

    Add package aiobearychat/0.1.0.dev1/aiobearychat-0.1.0.dev1-py2.py3-none-any.whl

---
+++ packages/aiobearychat/aiobearychat-0.1.0.dev1-py2.py3-none-any.whl/aiobearychat/openapi/aiohttp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import asyncio
+from typing import Dict, Optional, Any
+
+import aiohttp
+import attr
+
+from aiobearychat.requesters.aiohttp import Requester
+from aiobearychat import openapi
+
+
+class OpenAPI(openapi.OpenAPI):
+    def __init__(self, session: aiohttp.ClientSession,
+                 token: str = attr.NOTHING, *,
+                 base_url: str = ''):
+        requester = Requester(session=session)
+        super().__init__(requester=requester, token=token, base_url=base_url)
