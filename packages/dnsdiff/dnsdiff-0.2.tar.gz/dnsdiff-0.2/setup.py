28a664cb388b9554a6da0756a2176d9fe02367fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:13 +0000

    Add package dnsdiff/0.2/dnsdiff-0.2.tar.gz

---
+++ packages/dnsdiff/dnsdiff-0.2.tar.gz/dnsdiff-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'dnsdiff',
+  packages = ['dnsdiff'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Utility to quickly suss out discrepancies between nameservers',
+  author = 'M Anzuoni',
+  author_email = 'me.anzuoni@gmail.com',
+  url = 'https://github.com/gawkermedia/dnsdiff', # use the URL to the github repo
+  download_url = 'https://github.com/gawkermedia/dnsdiff/archive/0.2.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'dns',], # arbitrary keywords
+  classifiers = [],
+  install_requires=['dnspython']
+
+)
