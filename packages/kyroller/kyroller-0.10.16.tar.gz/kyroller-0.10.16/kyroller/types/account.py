9f109932a1ddba9b185531b93d9160cecaf9d442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:13 +0000

    Add package kyroller/0.10.16/kyroller-0.10.16.tar.gz

---
+++ packages/kyroller/kyroller-0.10.16.tar.gz/kyroller-0.10.16/kyroller/types/account.py
@@ -0,0 +1,278 @@
+from .position_book import Position, PositionBook
+from .types import *
+from .order import Order
+import abc
+from .tick import Tick
+from .bar import Bar
+from .exe_rpt import ExecutionRpt
+
+
+class AccountListener:
+    @abc.abstractclassmethod
+    def handle_order_status_cahnge(self, order, src_status):
+        pass
+
+    @abc.abstractclassmethod
+    def handle_exerpt(self, rpt):
+        pass
+
+
+class TradeError(Exception):
+    pass
+
+
+class Account:
+    def __str__(self):
+        _str = "余额: %.2f, 冻结: %.2f 持仓: " % (
+            self._balance, self._frozen_balance)
+        if len(self.positions) == 0:
+            _str += 'none'
+        else:
+            arr = map(lambda p: '%s(volume:%d,cost:%.2f,market:%.2f)' %
+                      (p.symbol, p.volume, p.cost, p.market_value), self.positions)
+            _str += ', '.join(arr)
+        _str += ' 全部股票市值：%.2f' % self.market_value
+        _str += ' 股票+现金：%.2f' % self.total_value
+        return _str
+
+    def __eq__(self, other):
+        # 余额等全部相等
+        if other is None or self.balance != other.balance or self.frozen_balance != other.frozen_balance:
+            return False
+        else:
+            pos1 = self._positionBookDic
+            pos2 = other._positionBookDic
+            if len(pos1) != len(pos2):
+                return False
+            for symbol in pos1:
+                if symbol not in pos2:
+                    return False
+                elif pos1[symbol].volume != pos2[symbol].volume:
+                    return False
+            return True
+        return True
+
+    def __init__(self, balance, frozen_balance=0, positions=None, bid_fee_radio=0, ask_fee_radio=0):
+        self._balance = float(balance)
+        self._positionBookDic = {}
+        self._frozen_balance = float(frozen_balance)
+        if positions is not None:
+            for p in positions:
+                if p.symbol not in self._positionBookDic:
+                    book = PositionBook(p.symbol)
+                    self._positionBookDic[p.symbol] = book
+                self._positionBookDic[p.symbol].add_position(
+                    p.volume, p.price, update_market_price=True)
+        self._orders = []
+        self._bid_fee_radio = bid_fee_radio
+        self._ask_fee_radio = ask_fee_radio
+        self._order_listenres = []
+
+    @property
+    def frozen_balance(self):
+        '''
+        冻结金额，买入下单后会冻结相关金额
+        '''
+        return self._frozen_balance
+
+    def freeze_balance(self, money):
+        '''
+        冻结金额，内部api
+        '''
+        if money > self._balance:
+            return False
+        self._balance -= money
+        self._frozen_balance += money
+        return True
+
+    def release_frozen_balance(self, money):
+        '''
+        释放冻结金额，内部api
+        '''
+        if money > self._frozen_balance:
+            return False
+        self._balance += money
+        self._frozen_balance -= money
+        return True
+
+    def freeze_volume(self, symbol, volume):
+        '''
+        冻结股票份额，内部api
+        '''
+        if symbol not in self._positionBookDic:
+            return False
+        return self._positionBookDic[symbol].freeze_volume(volume)
+
+    def cost_volume(self, symbol, volume, price):
+        '''
+        真实扣减股票份额，内部api
+        '''
+        if symbol not in self._positionBookDic:
+            return False
+        return self._positionBookDic[symbol].cost_volume(volume, price)
+
+    def release_volume(self, symbol, volume):
+        '''
+        释放冻结的股份，内部api
+        '''
+        if symbol not in self._positionBookDic:
+            return False
+        return self._positionBookDic[symbol].release_volume(volume)
+
+    def add_balance(self, money):
+        '''
+        增加余额，内部api
+        '''
+        self._balance += money
+
+    def cost_frozen_balance(self, money):
+        '''
+        扣减冻结的金额，订单成交时会扣除冻结金额，内部api
+        '''
+        if money > self._frozen_balance:
+            return False
+        self._frozen_balance -= money
+        return True
+
+    def add_position(self, symbol, volume, price):
+        '''
+        增加份额，买入订单成交时调用，内部api
+        '''
+        if symbol not in self._positionBookDic:
+            self._positionBookDic[symbol] = PositionBook(symbol)
+        return self._positionBookDic[symbol].add_position(volume, price)
+
+    @property
+    def balance(self):
+        '''
+        可用余额
+        '''
+        return self._balance
+
+    @property
+    def positions(self):
+        '''
+        持仓列表
+        '''
+        return self._positionBookDic.values()
+
+    def get_positions(self):
+        '''
+        返回持仓列表
+        '''
+        return self.positions
+
+    def get_position(self, symbol):
+        '''
+        返回制定代码的持仓，如果为空，返回 None
+        '''
+        return self._positionBookDic[symbol] if symbol in self._positionBookDic else None
+
+    def update_market_price(self, symbol, price):
+        '''
+        更新股票价格，更新市值,内部api
+        '''
+        if symbol not in self._positionBookDic:
+            return
+        self._positionBookDic[symbol].price = price
+
+    @property
+    def market_value(self):
+        '''
+        股票总市值，不包含现金
+        '''
+        return sum(map(lambda p: p.market_value, self.positions))
+
+    @property
+    def cash(self):
+        '''
+        现金，包含余额+冻结部分
+        '''
+        return self._balance + self._frozen_balance
+
+    @property
+    def total_value(self):
+        '''
+        总市值，包含现金
+        '''
+        return self.market_value + self._balance + self._frozen_balance
+
+    @property
+    def total_cost(self):
+        '''
+        股票总成本
+        '''
+        return sum(map(lambda p: p.cost, self.positions))
+
+    def make_snapshot(self):
+        '''
+        创建资产快照，最好不要频繁调用
+        '''
+        return State({
+            "balance": self._balance,
+            "frozen_balance": self._frozen_balance,
+            "cash": self.cash,
+            "total_cost": self.total_cost,
+            "market_value": self.market_value,
+            "total_value": self.total_value
+        })
+
+    def get_volume_of_symbol(self, symbol):
+        '''
+        返回指定股票持有份额，包含所有状态
+        '''
+        if symbol not in self._positionBookDic:
+            return 0
+        return self._positionBookDic[symbol].volume
+
+    def get_frozen_volume_of_symbol(self, symbol):
+        '''
+        返回指定股票被冻结的份额，卖出中
+        '''
+        if symbol not in self._positionBookDic:
+            return 0
+        return self._positionBookDic[symbol].frozen_volume
+
+    def get_free_volume_of_symbol(self, symbol):
+        '''
+        返回指定股票可交易部分的份额
+        '''
+        if symbol not in self._positionBookDic:
+            return 0
+        return self._positionBookDic[symbol].free_volume
+
+    def get_locked_volume_of_symbol(self, symbol):
+        '''
+        返回指定股票锁定的份额
+        '''
+        if symbol not in self._positionBookDic:
+            return 0
+        return self._positionBookDic[symbol].locked_volume
+
+    def get_orders(self)->list:
+        '''
+        获取订单
+        '''
+        return self._orders
+
+    @abc.abstractclassmethod
+    def mk_order(self, order: Order):
+        pass
+
+    @abc.abstractclassmethod
+    def cancel_order(self, order_cid):
+        pass
+
+    def add_order_listener(self, callback):
+        self._order_listenres.append(callback)
+
+    def _order_change_callback(self, order, src_status):
+        for listener in self._order_listenres:
+            listener.handle_order_status_cahnge(order, src_status)
+
+    def _order_rpt(self, rpt):
+        for listener in self._order_listenres:
+            listener.handle_exerpt(rpt)
+
+    def handle_events(self, _type, msg):
+        pass
---
+++ packages/kyroller/kyroller-0.10.16.tar.gz/kyroller-0.10.16/kyroller/types/position_book.py
@@ -0,0 +1,122 @@
+from .position import Position
+
+
+class PositionBook:
+    '''
+        持仓记录单
+    '''
+
+    def __str__(self):
+        return "份额：%d (冻结:%d,锁定:%d),现价:%.2f, 均价:%.2f,市值:%.2f,成本:%.2f" % (self._volume, self._frozen_volume,
+                                                                         self._locked_volume, self.price,
+                                                                         self.avg_cost_price,
+                                                                         self.market_value, self._cost)
+
+    def __init__(self, symbol):
+        self._exchange = ''
+        self._symbol = symbol
+        self._volume = 0
+        self._price = 0
+        self._frozen_volume = 0
+        self._locked_volume = 0
+        self._cost = 0
+        self._avg_cost_price = 0
+        self._positions = list()
+
+    def add_position(self, volume, price, locked=False, update_market_price=False):
+        p = Position(self._symbol, volume, price)
+        self._positions.append(p)
+        # 记录总量
+        self._volume += volume
+        # 总成本
+        self._cost += volume * price
+        # 平均成本
+        self._avg_cost_price = self._cost / self._volume
+        # 是否锁定
+        if locked:
+            self._locked_volume += volume
+        if update_market_price:
+            self._price = price
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @property
+    def frozen_volume(self):
+        '''
+         冻结股份
+        '''
+        return self._frozen_volume
+
+    def freeze_volume(self, volume):
+        '''
+         冻结指定数量的股份，卖出操作
+        '''
+        if self.free_volume < volume:
+            return False
+        self._frozen_volume += volume
+        return True
+
+    def cost_volume(self, volume, price):
+        '''
+        从冻结扣减股份，卖出成交操作
+        '''
+
+        if self._frozen_volume < volume:
+            return False
+        self.add_position(0 - volume, price, update_market_price=True)
+        self._frozen_volume -= volume
+        return True
+
+    def release_volume(self, volume):
+        '''
+        释放冻结的股份
+        '''
+        if self._frozen_volume < volume:
+            return False
+        self._frozen_volume -= volume
+        return True
+
+    @property
+    def cost(self):
+        '''
+        持有成本
+        '''
+        return self._cost
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+        for p in self._positions:
+            p.price = value
+        # 更新市值
+
+    @property
+    def avg_cost_price(self):
+        '''
+        平均成本
+        '''
+        return self._avg_cost_price
+
+    @property
+    def market_value(self):
+        '''
+         市值
+        '''
+        return self._price * self._volume
+
+    @property
+    def free_volume(self):
+        '''
+            可操作的股份
+        '''
+        return self._volume - self._frozen_volume - self._locked_volume
