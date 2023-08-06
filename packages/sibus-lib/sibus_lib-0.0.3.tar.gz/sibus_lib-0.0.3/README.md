3172f2755a23dff40aac89f1bbba4435169f24d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:20 +0000

    Add package sibus-lib/0.0.3/sibus_lib-0.0.3.tar.gz

---
+++ packages/sibus-lib/sibus_lib-0.0.3.tar.gz/sibus_lib-0.0.3/README.md
@@ -0,0 +1,13 @@
+SiBus Lib - Library for creating SiBus clients and servers
+========================================================
+
+Vous pouvez l'installer avec pip:
+
+    pip install sibus_lib
+
+Exemple d'usage:
+
+    >>> from sibus_lib import proclamer
+    >>> proclamer()
+
+Ce code est sous licence WTFPL.
---
+++ packages/sibus-lib/sibus_lib-0.0.3.tar.gz/sibus_lib-0.0.3/sibus_lib/__init__.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Ce module proclame la bonne parole de sieurs Sam et Max. Puissent-t-ils
+    retrouver une totale liberté de pensée cosmique vers un nouvel age
+    reminiscent.
+"""
+
+__version__ = "0.0.3"
+
+from sibus_lib.DefaultLogger import mylogger
+from sibus_lib.MessagingCore import BusElement, BusCore, MessageObject
+from sibus_lib.SmartConfig import SmartConfigFile
+from sibus_lib.CoreDatabase import CoreDatabase, CoreDBBase
+
