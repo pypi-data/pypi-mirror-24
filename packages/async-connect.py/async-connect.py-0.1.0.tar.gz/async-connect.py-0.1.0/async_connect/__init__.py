a58bcfb7d4ea6c591572ffdc8cde696e0627cf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:04 +0000

    Add package async-connect.py/0.1.0/async-connect.py-0.1.0.tar.gz

---
+++ packages/async-connect.py/async-connect.py-0.1.0.tar.gz/async-connect.py-0.1.0/async_connect/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Asynchronous Connect API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the Monstercat Connect API.
+
+:copyright: (c) 2017 GiovanniMCMXCIX
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'async_connect'
+__author__ = 'GiovanniMCMXCIX'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 GiovanniMCMXCIX'
+__version__ = '0.1.0'
+
+from .errors import *
+from .client import Client
+from .release import Release
+from .track import Track, BrowseEntry
+from .artist import Artist
+from .playlist import Playlist
+from . import utils
+from collections import namedtuple
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='final', serial=0)
