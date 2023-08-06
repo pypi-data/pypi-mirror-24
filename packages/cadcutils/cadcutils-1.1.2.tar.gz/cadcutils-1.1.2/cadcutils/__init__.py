b02f61e3287dac1e03ee89caab3734f41995e9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:58 +0000

    Add package cadcutils/1.1.2/cadcutils-1.1.2.tar.gz

---
+++ packages/cadcutils/cadcutils-1.1.2.tar.gz/cadcutils-1.1.2/cadcutils/__init__.py
@@ -0,0 +1,20 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+The cadcutils package contains libraries commonly use in CADC applications.
+
+The main modules and packages in cadcutils are:
+
+    -utils: collection of utilities for time formats, application logging
+            and command line argument parsing
+    -net: network utilities: authentication, Web service base client, etc
+    -exceptions: exceptions thrown by the cadcutils libraries
+
+"""
+
+try:
+    _PACKAGE_SETUP_
+except NameError:
+    # do any package imports here
+    pass
+
