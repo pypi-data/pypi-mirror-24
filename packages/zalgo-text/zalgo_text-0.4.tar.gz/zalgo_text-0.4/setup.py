6984edca7149729ac6eb537f2f6ad61a2e7d538f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:54 +0000

    Add package zalgo-text/0.4/zalgo_text-0.4.tar.gz

---
+++ packages/zalgo-text/zalgo_text-0.4.tar.gz/zalgo_text-0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'zalgo_text',
+  packages = ['zalgo_text'], # this must be the same as the name above
+  version = '0.4',
+  description = 'A small library to create zalgo text',
+  author = 'Greg Pritchard',
+  author_email = 'gregcraig23@gmail.com',
+  url = 'https://github.com/gregoryneal/zalgo', # use the URL to the github repo
+  download_url = 'https://github.com/gregoryneal/zalgo/dist/0.4.tar.gz',
+  keywords = ['diacritic', 'filter', 'zalgo'], # arbitrary keywords
+  classifiers = [],
+)
