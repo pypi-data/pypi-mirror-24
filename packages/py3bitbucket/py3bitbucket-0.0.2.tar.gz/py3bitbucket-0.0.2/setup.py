c56c8832d24ad2e3eae630c387ae35c06a10b87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:48 +0000

    Add package py3bitbucket/0.0.2/py3bitbucket-0.0.2.tar.gz

---
+++ packages/py3bitbucket/py3bitbucket-0.0.2.tar.gz/py3bitbucket-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+requirements = [
+    'requests',
+    'oauthlib',
+    'requests_oauthlib',
+    'click',
+    'PyYAML',
+    'pprint',
+    'tabulate'
+]
+test_requirements = [ ]
+setup(name='py3bitbucket',
+      version='0.0.2',
+      description='bitbucket client',
+      author='Yo-An lin',
+      author_email='yoanlin93@gmail.com',
+      packages=['bitbucket'],
+      license='MIT',
+      url='https://github.com/c9s/py-bitbucket',
+      install_requires=requirements,
+      tests_require=test_requirements
+      )
