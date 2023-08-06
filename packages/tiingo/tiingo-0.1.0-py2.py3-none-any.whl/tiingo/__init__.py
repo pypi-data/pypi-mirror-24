81a40670392678f88545f9244b437e1e19689199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:01 +0000

    Add package tiingo/0.1.0/tiingo-0.1.0-py2.py3-none-any.whl

---
+++ packages/tiingo/tiingo-0.1.0-py2.py3-none-any.whl/tiingo/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Tiingo Python."""
+
+__author__ = """Cameron Yick"""
+__email__ = 'cameron.yick@enigma.com'
+__version__ = '0.1.0'
+
+from tiingo.api import TiingoClient
