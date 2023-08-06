27105a80009bf12a9f7619a053b459365041f343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:13 +0000

    Add package scrapy-cdr/0.5/scrapy-cdr-0.5.tar.gz

---
+++ packages/scrapy-cdr/scrapy-cdr-0.5.tar.gz/scrapy-cdr-0.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+setup(
+    name='scrapy-cdr',
+    version='0.5',
+    packages=['scrapy_cdr'],
+    install_requires=[
+        'botocore',
+        'cachetools',
+        'elasticsearch',
+        'elasticsearch-dsl',
+        'futures',
+        'json_lines',
+        'requests',
+        'scrapy',
+        'six',
+        'tqdm',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cdr-v2-to-v3=scrapy_cdr.v2_to_v3:main',
+            'cdr-es-upload=scrapy_cdr.es_upload:main',
+            'cdr-es-download=scrapy_cdr.es_download:main',
+            ],
+    },
+    license='MIT license',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
