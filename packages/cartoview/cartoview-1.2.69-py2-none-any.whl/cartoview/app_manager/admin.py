a20876d54660bc823e79898c918a31f2d333aedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:38 +0000

    Add package cartoview/1.2.69/cartoview-1.2.69-py2-none-any.whl

---
+++ packages/cartoview/cartoview-1.2.69-py2-none-any.whl/cartoview/app_manager/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from models import App, AppTag, AppInstance, AppStore, Logo
+
+
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    ordering = ('order',)
+
+
+# admin.site.register(App)
+admin.site.register(AppTag)
+admin.site.register(AppInstance)
+admin.site.register(AppStore)
+admin.site.register(Logo)
