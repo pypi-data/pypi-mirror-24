ea40c06908d86f79df89c124ba0ad6d8063e3081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:39 +0000

    Add package suncalcPy/0.1/suncalcPy-0.1.tar.gz

---
+++ packages/suncalcPy/suncalcPy-0.1.tar.gz/suncalcPy-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'suncalcPy',
+  packages = ['suncalcPy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python library for calculating sun position, sunlight phases, moon position and lunar phases for a given location and time.',
+  author = 'Abe Miessler',
+  author_email = 'abemiesler@gmail.com',
+  url = 'https://github.com/broham/suncalcpy', # use the URL to the github repo
+  download_url = 'https://github.com/broham/suncalcpy/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['sun', 'moon', 'phases', 'position', 'time', 'location'], # arbitrary keywords
+  classifiers = [],
+)
