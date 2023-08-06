03a07ca61acf3443e574079cc18315e937fd8df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package dankdungeon/0.1.2/dankdungeon-0.1.2.tar.gz

---
+++ packages/dankdungeon/dankdungeon-0.1.2.tar.gz/dankdungeon-0.1.2/dankdungeon/__init__.py
@@ -0,0 +1,18 @@
+'''
+dankdungeon
+
+generate dungeons and their monster occupants
+'''
+
+__title__ = 'dankdungeon'
+__version__ = '0.1.2'
+__all__ = ()
+__author__ = 'Johan Nestaas <johannestaas@gmail.com>'
+__license__ = 'GPLv3+'
+__copyright__ = 'Copyright 2017 Johan Nestaas'
+
+from .monster import Monster, main
+
+
+if __name__ == '__main__':
+    main()
