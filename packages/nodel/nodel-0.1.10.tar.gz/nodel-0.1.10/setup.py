cacad6ea8dfb8bdf8e424fbe33aebd39e9098b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:32 +0000

    Add package nodel/0.1.10/nodel-0.1.10.tar.gz

---
+++ packages/nodel/nodel-0.1.10.tar.gz/nodel-0.1.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from os import path
+
+setup(
+	name='nodel',
+	version='0.1.10',
+	packages=['nodel'],
+	url='https://github.com/ary4n/nodel',
+	license='MIT',
+	author='aryan',
+	author_email='alikhaniaryan@live.com',
+	description='django project manager',
+	keywords='minimal django project manager',
+	download_url="https://github.com/ary4n/nodel/archive/0.1.10.tar.gz",
+	install_requires=[
+		'python-dotenv',
+	],
+)
