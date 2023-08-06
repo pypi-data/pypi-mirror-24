57467318ad00adc3c0a1779bc2bfd4086815d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:34 +0000

    Add package uvhttp/1.14/uvhttp-1.14.tar.gz

---
+++ packages/uvhttp/uvhttp-1.14.tar.gz/uvhttp-1.14/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+
+requirements = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+setup(
+    name='uvhttp',
+    version='1.14',
+    description='High performance Python HTTP client',
+    url='https://github.com/justinbarrick/uvhttp',
+    package_data={'uvhttp': ['example.pem']},
+    include_package_data=True,
+    packages=['uvhttp'],
+    install_requires=[ r.rstrip() for r in open(requirements).readlines() ]
+)
