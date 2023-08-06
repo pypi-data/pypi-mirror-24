25d7ee14994606d7007925f9b215095dd48741bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:05 +0000

    Add package showtime-cli/0.1/showtime-cli-0.1.tar.gz

---
+++ packages/showtime-cli/showtime-cli-0.1.tar.gz/showtime-cli-0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='showtime-cli',
+    version='0.1',
+    packages=[
+        'showtime',
+    ],
+    author = 'Evgeniy Vasilev',
+    author_email = 'aquilax@gmail.com',
+    description = 'Command line show tracker using the TVMaze public API',
+    url = 'https://github.com/aquilax/showtime',
+    keywords = ['tv', 'command line', 'application', 'show', 'tvmaze'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+    install_requires = [
+        'cmd2==0.7.5',
+        'python-tvmaze==1.0.1',
+        'ratelimit==1.4.0',
+        'tinydb==3.3.1',
+        'terminaltables==3.1.0',
+    ],
+    entry_points={
+        'console_scripts':
+        [
+            'showtime = showtime.command:main'
+        ]
+    }
+)
---
+++ packages/showtime-cli/showtime-cli-0.1.tar.gz/showtime-cli-0.1/showtime/output.py
@@ -0,0 +1,86 @@
+from terminaltables import SingleTable as Table
+
+class Output():
+
+    def __init__(self, print_function, error_function, feedback_function):
+        self.print_function = print_function
+        self.error_function = error_function
+        self.feedback_function = feedback_function
+
+    def poutput(self, str):
+        self.print_function(str)
+
+    def perror(self, str):
+        self.error_function(str)
+
+    def pfeedback(self, str):
+        self.feedback_function(str)
+
+    def format_search_results(self, search_result):
+        data = []
+        data.append([
+            'ID',
+            'Name',
+            'Premiered',
+            'Status'
+            'URL'
+        ])
+        for show in search_result:
+            data.append([
+                show.id,
+                show.name,
+                show.premiered,
+                show.status,
+                show.url
+            ])
+        return Table(data, title='Search Results').table
+
+    def format_episodes(self, show, episodes):
+        title = '({id}) {name} - {premiered}'.format(
+                id=show['id'], name=show['name'], premiered=show['premiered'])
+        data =self.get_episodes_data(episodes)
+        return Table(data, title=title).table
+
+    def get_episodes_data(self, episodes):
+        data = []
+        data.append([
+            'ID',
+            'S',
+            'E',
+            'Name',
+            'Aired',
+            'Watched'
+        ])
+        for episode in episodes:
+            data.append([
+                episode['id'],
+                'S{season:0>2}'.format(season=episode['season']),
+                'E{episode:0>2}'.format(episode=episode['number']),
+                episode['name'],
+                episode['airdate'],
+                episode['watched']
+            ])
+        return data
+
+    def format_unwatched(self, episodes):
+        title = 'Episodes to watch'
+        data =self.get_episodes_data(episodes)
+        return Table(data, title=title).table
+
+    def shows_table(self, shows):
+        data = []
+        data.append([
+            'ID',
+            'Name',
+            'Premiered',
+            'Status'
+        ])
+        for show in shows:
+            data.append([
+                show['id'],
+                show['name'],
+                show['premiered'],
+                show['status'],
+            ])
+        title = 'Followed shows'
+        return Table(data, title=title).table
