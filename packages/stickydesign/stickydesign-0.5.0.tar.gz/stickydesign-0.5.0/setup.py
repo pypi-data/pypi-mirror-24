225f3702fefceca404c44bfef556a3c3ca0a0702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:52 +0000

    Add package stickydesign/0.5.0/stickydesign-0.5.0.tar.gz

---
+++ packages/stickydesign/stickydesign-0.5.0.tar.gz/stickydesign-0.5.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+import numpy
+
+# define the extension module
+stickyext = Extension('stickydesign._stickyext', sources=['stickydesign/_stickyext.c'],
+                          include_dirs=[numpy.get_include()])
+
+# run the setup
+setup(
+    name='stickydesign',
+    version='0.5.0',
+
+    install_requires = ['numpy'],
+
+    packages=['stickydesign'],
+    ext_modules=[stickyext],
+    package_data={'stickydesign': ['params/dnastackingbig.csv']},
+
+    author = "Constantine Glen Evans",
+    author_email = "cgevans@evans.foundation",
+    description = "StickyDesign DNA Tile Sticky End Package",
+    url = 'http://dna.caltech.edu/StickyDesign',
+    zip_safe = True,
+
+    test_suite = 'nose.collector'
+)
