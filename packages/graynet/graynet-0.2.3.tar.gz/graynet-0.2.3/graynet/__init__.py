f713b2d34e7fcfa29066dbfd48bc861d83513575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:03 +0000

    Add package graynet/0.2.3/graynet-0.2.3.tar.gz

---
+++ packages/graynet/graynet-0.2.3.tar.gz/graynet-0.2.3/graynet/__init__.py
@@ -0,0 +1,6 @@
+
+from graynet import parcellate, freesurfer
+from graynet.graynet import extract
+from graynet.parcellate import read_atlas
+
+__all__ = ['graynet', 'extract', 'parcellate', 'freesurfer', 'read_atlas']
