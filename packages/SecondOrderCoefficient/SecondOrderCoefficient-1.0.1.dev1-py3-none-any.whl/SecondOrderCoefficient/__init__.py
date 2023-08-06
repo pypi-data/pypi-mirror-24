503bebf4efe33c125e35afc592b8dd234b4db283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:14 +0000

    Add package SecondOrderCoefficient/1.0.1.dev1/SecondOrderCoefficient-1.0.1.dev1-py3-none-any.whl

---
+++ packages/SecondOrderCoefficient/SecondOrderCoefficient-1.0.1.dev1-py3-none-any.whl/SecondOrderCoefficient/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Haoruo Liu'
+__title__ = 'SecondOrderCoefficient'
+
+
+
+
+import SecondOrderCoefficient.secord_order_largest_degree
+from SecondOrderCoefficient.second_order_largest_degree import *
+
+import SecondOrderCoefficient.secord_order_average_degree
+from SecondOrderCoefficient.second_order_average_degree import *
---
+++ packages/SecondOrderCoefficient/SecondOrderCoefficient-1.0.1.dev1-py3-none-any.whl/SecondOrderCoefficient/second_order_average_degree.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import pandas as pd
+import scipy.stats as stats
+
+
+def node_neighbor_average_degree_xy(G, x='out', y='in', weight=None, nodes=None):
+    if nodes is None:
+        nodes = set(G)
+    else:
+        nodes = set(nodes)
+    xdeg = G.degree
+    ydeg = G.degree
+    for start_node_ID in xdeg(nodes):
+        end_node = (nbr for _,nbr in G.edges(start_node_ID) if nbr in nodes)
+        for end_node_ID in end_node:
+            nbrdeg_start = G.degree(G[start_node_ID])
+            nbrdeg_end = G.degree(G[end_node_ID])
+            
+            nbrdeg_start.pop(end_node_ID)
+            nbrdeg_end.pop(start_node_ID)
+            a = []
+            b = []
+            for n in nbrdeg_start:                
+                a.append(nbrdeg_start[n])
+            if a:
+                degu = (sum(a) / float(len(a))) - 1
+            else:
+                degu = 1
+            for m in nbrdeg_end:
+                b.append(nbrdeg_end[m])
+            if b:
+                degv = (sum(b) / float(len(b))) - 1
+            else:
+                degv = 1
+            
+            yield degu,degv         
+
+def secord_order_average_degree(G):
+    xy=node_neighbor_average_degree_xy(G)
+    x,y=zip(*xy)
+    return stats.pearsonr(x,y)[0]
