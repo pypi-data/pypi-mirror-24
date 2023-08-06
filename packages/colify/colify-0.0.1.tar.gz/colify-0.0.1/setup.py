cabe7941ed16a38ec7be2ce6e02e33e851b54be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:18 +0000

    Add package colify/0.0.1/colify-0.0.1.tar.gz

---
+++ packages/colify/colify-0.0.1.tar.gz/colify-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='colify',
+    version='0.0.1',
+    description='Print arrays as columns',
+    url='https://github.com/looneym/colify',
+    author='Micheal Looney',
+    license='MIT',
+    keywords='print array column',
+)
