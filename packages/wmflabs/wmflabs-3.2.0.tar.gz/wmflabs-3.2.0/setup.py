79cf0d9ae691fe9b084b973409a4428e7fbcae38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:09 +0000

    Add package wmflabs/3.2.0/wmflabs-3.2.0.tar.gz

---
+++ packages/wmflabs/wmflabs-3.2.0.tar.gz/wmflabs-3.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='wmflabs',
+    version='3.2.0',
+    packages=['wmflabs'],
+    url='https://wikitech.wikimedia.org/wiki/User:Legoktm/wmflib',
+    license='GPL-3.0+',
+    author='Kunal Mehta',
+    author_email='legoktm@member.fsf.org',
+    description='Small library for common tasks on Wikimedia Labs',
+    install_requires=['requests', 'pymysql'],
+    test_suite="tests",
+)
