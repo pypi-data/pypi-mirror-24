bc97b367b432758080daf2f989b557705286345b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:14 +0000

    Add package clearblade/2.0/clearblade-2.0-py2.py3-none-any.whl

---
+++ packages/clearblade/clearblade-2.0-py2.py3-none-any.whl/clearblade/Code.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from . import cbLogs
+from . import restcall
+
+
+class Service():
+    def __init__(self, system, name):
+        self.name = name
+        self.url = system.url + "/api/v/1/code/" + system.systemKey + "/" + name
+
+    def execute(self, authenticatedUser, params={}):
+        cbLogs.info("Executing code service", self.name)
+        resp = restcall.post(self.url, headers=authenticatedUser.headers, data=params)
+        return resp
+
+
+class Library():
+    def __init__(self):
+        pass
