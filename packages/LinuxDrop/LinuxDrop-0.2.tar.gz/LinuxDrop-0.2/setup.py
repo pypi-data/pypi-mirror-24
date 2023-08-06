e47a5458f3a98d042a2fd908fffc0aa79c9cf28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package LinuxDrop/0.2/LinuxDrop-0.2.tar.gz

---
+++ packages/LinuxDrop/LinuxDrop-0.2.tar.gz/LinuxDrop-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='LinuxDrop',
+    version='0.2',
+    packages=['sender', 'receiver'],
+    url='https://github.com/Livin21/LinuxDrop',
+    license='MIT',
+    author='livin',
+    author_email='livinmathew99@gmail.com',
+    description='Airdrop alternative for Linux <3'
+)
