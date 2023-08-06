b781fdb0027770f369e240b88621e1eb8ed15299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:47 +0000

    Add package raspir/1.0.2/raspir-1.0.2.tar.gz

---
+++ packages/raspir/raspir-1.0.2.tar.gz/raspir-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'raspir',
+  packages = ['raspir'],
+  version = '1.0.2',
+  description = 'A library to detect IR signals from receivers on Raspberry Pi',
+  author = 'Pavlo Kochylo',
+  author_email = 'kochylo.paolo@gmail.com',
+  url = 'https://github.com/SyncroIT/Raspir',
+  download_url = 'https://github.com/SyncroIT/Raspir/archive/1.0.2.tar.gz',
+  keywords = ['ir', 'raspberry', 'infrared', 'decoder'],
+  classifiers = [],
+)
