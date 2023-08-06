8248b1743c5219f18cb3a3866b68414097fc7327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:00 +0000

    Add package epitran/0.35/epitran-0.35-py2.py3-none-any.whl

---
+++ packages/epitran/epitran-0.35-py2.py3-none-any.whl/epitran/test_farsi.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import unittest
+
+import _epitran
+
+
+class TestSorani(unittest.TestCase):
+    def setUp(self):
+        self.epi = _epitran.Epitran(u'fas-Arab')
+
+    def test_faarsi(self):
+        tr = self.epi.transliterate('فارسی')
+        self.assertEqual(tr, 'fɒrsj')
+
+    def test_rowshan(self):
+        tr = self.epi.transliterate('روشن')
+        self.assertEqual(tr, 'rvʃn')
+
+    def test_hamaye(self):
+        tr = self.epi.transliterate('همهٔ')
+        self.assertEqual(tr, 'hmhʔ')
+
+    def test_aafraad(self):
+        tr = self.epi.transliterate('افراد')
+        self.assertEqual(tr, 'ɒfrɒd')
+
+    def test_bashar(self):
+        tr = self.epi.transliterate('بشر')
+        self.assertEqual(tr, 'bʃr')
+
+    def test_aazaad(self):
+        tr = self.epi.transliterate('آزاد')
+        self.assertEqual(tr, 'ɒzɒd')
+
+    def test_donjaa(self):
+        tr = self.epi.transliterate('دنیا')
+        self.assertEqual(tr, 'dnjɒ')
+
+    def test_miaayand(self):
+        tr = self.epi.transliterate('می‌آیند')
+        self.assertEqual(tr, 'mj‌ɒjnd')
+
+    def test_heysiyaat(self):
+        tr = self.epi.transliterate('حیثیت')
+        self.assertEqual(tr, 'hjsjt')
