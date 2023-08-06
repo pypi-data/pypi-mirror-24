3ad2e789889c071a6b26709c1cc18a1bad4e106b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package sqlitehouse/1.1/sqlitehouse-1.1.tar.gz

---
+++ packages/sqlitehouse/sqlitehouse-1.1.tar.gz/sqlitehouse-1.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name="sqlitehouse",
+      version="1.1",
+      description="Simple library for reading/writing records in a SQLite database.",
+      author="Tumthe3",
+      author_email="tumthe3@gmail.com",
+      url="https://github.com/Tumthe3/SQLitehouse",
+      py_modules=["sqlitehouse"],
+      license="MIT"
+     )
