bb22df3619d2561e0b47b879537a610f9ea7c6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:24 +0000

    Add package geoweather/0.22/geoweather-0.22.tar.gz

---
+++ packages/geoweather/geoweather-0.22.tar.gz/geoweather-0.22/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='geoweather',
+      version='0.22',
+      description='Useful weather functions',
+      url='http://github.com/nanodan/geoweather',
+      author='Daniel J. Lewis',
+      license='MIT',
+      packages=['geoweather'],
+      zip_safe=False,
+      requires=['numpy','matplotlib','pandas']
+)
