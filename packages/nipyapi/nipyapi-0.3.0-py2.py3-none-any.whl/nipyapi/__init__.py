9d3049423f2bb24413688ea9dfcec398a4d797da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package nipyapi/0.3.0/nipyapi-0.3.0-py2.py3-none-any.whl

---
+++ packages/nipyapi/nipyapi-0.3.0-py2.py3-none-any.whl/nipyapi/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Nipyapi."""
+
+from __future__ import absolute_import
+
+__author__ = """Daniel Chaffelson"""
+__email__ = 'chaffelson@gmail.com'
+__version__ = '0.3.0'
+
+from .canvas import Canvas
+from .system import System
