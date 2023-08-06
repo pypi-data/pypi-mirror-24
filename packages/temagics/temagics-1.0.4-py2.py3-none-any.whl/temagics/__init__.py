e15ae84c3eaf2c6c655a7fb60d6a18405a670a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:50 +0000

    Add package temagics/1.0.4/temagics-1.0.4-py2.py3-none-any.whl

---
+++ packages/temagics/temagics-1.0.4-py2.py3-none-any.whl/temagics/__init__.py
@@ -0,0 +1,10 @@
+from .magics import teMagics
+from .comm_handlers import convert_file_comm, save_file_comm
+
+ip = get_ipython()
+ip.register_magics(teMagics)
+ip.kernel.comm_manager.register_target('convert_file_comm', convert_file_comm)
+ip.kernel.comm_manager.register_target('save_file_comm', save_file_comm)
+
+# with open('/tmp/nterout', 'a') as myfile:
+#     myfile.write('finished registering magics and comms\n')
