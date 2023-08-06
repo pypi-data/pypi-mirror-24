ceb701d30f9a743c1c7e5ba85dc84440e38c5e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:11 +0000

    Add package osuex/0.0.5/osuex-0.0.5.tar.gz

---
+++ packages/osuex/osuex-0.0.5.tar.gz/osuex-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import osuex
+setup(
+    name = "osuex",
+    version = "0.0.5",
+    author = "Jacob Zipper",
+    author_email = "zipper@jacobzipper.com",
+    description = ("An osu extraction tool for linux users"),
+    url = "http://packages.python.org/osuex",
+    packages=['osuex'],
+    entry_points={
+        "console_scripts": [
+            "osuex = osuex.osuex:do",
+        ]
+    },
+    install_requires=[
+        'click'
+]
+)
