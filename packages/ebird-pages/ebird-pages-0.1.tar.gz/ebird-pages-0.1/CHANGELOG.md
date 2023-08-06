3356953af2f9c14ceb0d4e1c634f57040e111fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package ebird-pages/0.1/ebird-pages-0.1.tar.gz

---
+++ packages/ebird-pages/ebird-pages-0.1.tar.gz/ebird-pages-0.1/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/).
+This project adheres to [PEP440](https://www.python.org/dev/peps/pep-0440/)
+and by implication, [Semantic Versioning](http://semver.org/).
+
+## [0.1] - 2017-08-21
+### Added
+- Added get_checklist for scraping the data from the view checklist page.
+- Added script so get_checklist can be called from the command line.
---
+++ packages/ebird-pages/ebird-pages-0.1.tar.gz/ebird-pages-0.1/setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+
+from setuptools import setup
+
+from ebird.pages.version import __version__
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as fp:
+        return fp.read()
+
+
+def test_suite():
+    # Force test discovery to only look at the tests directory
+    # otherwise all the tests get executed twice.
+    test_loader = unittest.TestLoader()
+    return test_loader.discover('tests', pattern='test_*.py')
+
+
+setup(
+    name='ebird-pages',
+    version=__version__,
+    description='Scrapers for pages on the eBird web site',
+    long_description=read("README.md"),
+    author='ProjectBabbler',
+    author_email='projectbabbler@gmail.com',
+    url='http://pypi.python.org/pypi/ebird-pages/',
+    license='GPL',
+    keywords='eBird web scraper',
+    packages=['ebird.pages'],
+    test_suite='setup.test_suite',
+    entry_points="""
+        [console_scripts]
+        get-checklist=ebird.pages.scripts.get_checklist:cli
+    """,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Natural Language :: English',
+        'Topic :: Utilities',
+        'Topic :: Internet',
+    ],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'lxml',
+        'Click'
+    ],
+)
