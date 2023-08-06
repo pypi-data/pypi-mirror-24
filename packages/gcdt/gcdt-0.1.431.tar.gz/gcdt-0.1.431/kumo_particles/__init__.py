68bb4758470bbb742f3d23d2908310de881126ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:49 +0000

    Add package gcdt/0.1.431/gcdt-0.1.431.tar.gz

---
+++ packages/gcdt/gcdt-0.1.431.tar.gz/gcdt-0.1.431/kumo_particles/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+
+# note: we have some sample particles here but you need to start your own
+# particle packages on company or team level
+
+from instance_particle import create_instance
