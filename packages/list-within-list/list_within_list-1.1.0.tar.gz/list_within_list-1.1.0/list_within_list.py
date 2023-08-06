425f3913db6fc80d5b9f5b149a87070f79e83466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:29 +0000

    Add package list-within-list/1.1.0/list_within_list-1.1.0.tar.gz

---
+++ packages/list-within-list/list_within_list-1.1.0.tar.gz/list_within_list-1.1.0/list_within_list.py
@@ -0,0 +1,23 @@
+"""This list_within_list.py module holds a function checkiteminlist
+which prints all items in a list which may be or may not be nested
+list"""
+
+def checkiteminlist(takesalist, level):
+	""" This function takes two inputs 
+	"takesalist" which should be a list
+	and "level" which should be numeric 
+	for indentation for nestes list.The 
+	list is iterated through and checked if 
+	an item in the list is list, if so the 
+	checkiteminlist is called recursively.
+	If an item is not a list, the item is 
+	printed. The iterated loop continues 
+	until all items in the list is printed"""
+    
+	for each_item in takesalist:
+		if isinstance(each_item,list):
+			checkiteminlist(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print ("\t",end="")
+			print (each_item)
