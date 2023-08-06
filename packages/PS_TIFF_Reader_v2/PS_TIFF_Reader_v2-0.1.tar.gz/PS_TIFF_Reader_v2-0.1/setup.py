da3d1ca1ceddcb02d3e0f9bc708c01a5a2861f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:06 +0000

    Add package PS_TIFF_Reader_v2/0.1/PS_TIFF_Reader_v2-0.1.tar.gz

---
+++ packages/PS_TIFF_Reader_v2/PS_TIFF_Reader_v2-0.1.tar.gz/PS_TIFF_Reader_v2-0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 18:07:44 2017
+
+@author: alex
+"""
+
+from setuptools import setup, find_packages
+ 
+ 
+ 
+setup(name='PS_TIFF_Reader_v2',
+ 
+      version='0.1',
+  
+      author='alex',
+ 
+      author_email='alex@parkafm.co.kr',
+ 
+      description='TIFF FILE READER',
+ 
+      packages=find_packages(exclude=['tests']),
+ 
+
+ 
+      zip_safe=False,
+ 
+      )
