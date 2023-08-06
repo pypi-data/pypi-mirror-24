9f57aa925e981d9909342d1aac18a9df33e0d3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:53 +0000

    Add package find-job-titles/0.5.0/find_job_titles-0.5.0.tar.gz

---
+++ packages/find-job-titles/find_job_titles-0.5.0.tar.gz/find_job_titles-0.5.0/tests/test_finder.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+from find_job_titles import Finder
+
+# FIXME: should be in setup code
+finder = Finder()
+
+
+def test_some_matches():
+    fds = [x[0] for x in finder.finditer('I am the Senior Vice President')]
+    assert fds == ['Senior Vice President']
+
+
+def test_no_matches():
+    assert not finder.findall('defo no matches in here')
+
+
+def test_position():
+    fds = [x[1] for x in finder.findall('asdf sdksa President')]
+    assert fds == [11]
