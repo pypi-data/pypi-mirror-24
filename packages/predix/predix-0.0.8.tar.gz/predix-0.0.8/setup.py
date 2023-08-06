c967fefa41dfdba0407f93fa5d2a080aaa2369e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:02 +0000

    Add package predix/0.0.8/predix-0.0.8.tar.gz

---
+++ packages/predix/predix-0.0.8.tar.gz/predix-0.0.8/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import setup, find_packages
+
+install_requires = [
+        "python-dateutil",
+        "PyYAML",
+        "requests",
+        "boto3",
+        "websocket",
+        "websocket-client"
+    ]
+
+setup_requires = [
+    ]
+
+setup(
+        name="predix",
+        version="0.0.8",
+        author="Jayson DeLancey",
+        author_email="jayson.delancey@ge.com",
+        description="Python Client SDK for Predix Services",
+        setup_requires=setup_requires,
+        install_requires=install_requires,
+        package_data={},
+        packages=find_packages(exclude=['test', 'test.*']),
+        test_suite="test",
+        entry_points={
+            'console_scripts': [
+                ]
+        },
+        tests_require=['tox'],
+        keywords=['predix', 'ge', 'time', 'asset', 'analytics'],
+        url="https://github.com/predixpy/predixpy",
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 2.7',
+            'License :: Other/Proprietary License',
+            'Development Status :: 3 - Alpha']
+    )
