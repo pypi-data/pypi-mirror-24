adab2d32359ce64278c93e069face430ea43c02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:55 +0000

    Add package loop_list/1.0.0/loop_list-1.0.0.tar.gz

---
+++ packages/loop_list/loop_list-1.0.0.tar.gz/loop_list-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+name='loop_list',
+version = '1.0.0',
+author = 'roddy',
+author_email = '923401910@qq.com',
+url = 'http://www.roddpy.com',
+description = 'test loop list'
+
+)
+
+
