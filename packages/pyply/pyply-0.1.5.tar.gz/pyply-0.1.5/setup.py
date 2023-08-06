b4e70284917300bc6fb30e8b2f7c4692233b7d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:00 +0000

    Add package pyply/0.1.5/pyply-0.1.5.tar.gz

---
+++ packages/pyply/pyply-0.1.5.tar.gz/pyply-0.1.5/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  8 20:46:50 2017
+
+@author: atrea
+"""
+
+from setuptools import setup
+
+setup(name='pyply',
+      version='0.1.5',
+      description="""Based largely off functions found in R, pyply provides syntax in a functional programming style to make data manipulation easier.
+                     This is meant especially for those transitioning from R to Python.""",
+      url='https://github.com/atreadw1492/pyply',
+      author='Andrew Treadway',
+      author_email='opensourcecoder11@gmail.com',
+      license='',
+      packages=['pyply'],
+      zip_safe=False)
