5de374c0a1e4da1a44e528c4ec74b3bb96c9eb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package satella/2.0.10/satella-2.0.10-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.0.10-py2.py3-none-any.whl/satella/coding/__init__.py
@@ -0,0 +1,12 @@
+# coding=UTF-8
+"""
+Just useful objects to make your coding nicer every day
+"""
+from __future__ import print_function, absolute_import, division
+
+from .typecheck import  typed, List, Tuple, Dict, NewType, Callable, Sequence, \
+    TypeVar, Generic, Mapping, Iterable, Union, Any, Optional, CallSignature
+
+from .structures import TimeBasedHeap, CallableGroup
+from .monitor import Monitor, RMonitor
+from .algos import merge_dicts
