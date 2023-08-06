1910ddfc52704519b08359ff8d12be3ba5c3426c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:58 +0000

    Add package acqpack/0.8.1/acqpack-0.8.1.tar.gz

---
+++ packages/acqpack/acqpack-0.8.1.tar.gz/acqpack-0.8.1/acqpack/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for acqpack."""
+
+__author__ = """Scott Longwell"""
+__email__ = 'longwell@stanford.edu'
+__version__ = '0.8.1'
+
+import gui
+import utils
+from asicontroller import AsiController
+from autosampler import Autosampler
+from fractioncollector import FractionCollector
+from manifold import Manifold
+from motor import Motor
+
+# TODO: packaging https://python-packaging.readthedocs.io/en/latest/non-code-files.html
