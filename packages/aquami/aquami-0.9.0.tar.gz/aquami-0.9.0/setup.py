2ce626a6b94821ddd0ba433e83e7fd3ca68a50b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:32 +0000

    Add package aquami/0.9.0/aquami-0.9.0.tar.gz

---
+++ packages/aquami/aquami-0.9.0.tar.gz/aquami-0.9.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.rst", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='aquami',
+   version='0.9.0',
+   description=''.join(('A module to extract quantitative microstructure ',
+                        'information from micrographs of morphologically ',
+                        'complex microstructures.')),
+   license="MIT",
+   long_description=long_description,
+   author='Joshua Stuckner',
+   author_email='stuckner@vt.edu',
+   url="https://github.com/JStuckner/aquami",
+   packages=['aquami'],
+   install_requires=['matplotlib>=1.5.3',
+                     'numpy>=1.12.0',
+                     'openpyxl>=2.3.2',
+                     'pandas>=0.18.1',
+                     'pytesseract>=0.1.6',
+                     'scipy>=0.18.1',
+                     'scikit_image>=0.12.3',
+                     'Pillow>=4.0.0',
+                     'setuptools>=34.3.2'],
+   python_requires='>=3'
+)
+
