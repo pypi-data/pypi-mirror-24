4875fb83eda54e77fb458c06aa80e6decc7952e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:55 +0000

    Add package geoarray/0.5.11/geoarray-0.5.11.tar.gz

---
+++ packages/geoarray/geoarray-0.5.11.tar.gz/geoarray-0.5.11/geoarray/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+if 'MPLBACKEND' not in os.environ:
+    os.environ['MPLBACKEND'] = 'Agg'
+
+
+from .baseclasses import GeoArray
+from .masks import BadDataMask
+from .masks import NoDataMask
+from .masks import CloudMask
+
+
+__author__ = """Daniel Scheffler"""
+__email__ = 'danschef@gfz-potsdam.de'
+__version__ = '0.5.11'
+__versionalias__ = 'v20170901.01'
+__all__ = ['GeoArray',
+           'BadDataMask',
+           'NoDataMask',
+           'CloudMask'
+           ]
