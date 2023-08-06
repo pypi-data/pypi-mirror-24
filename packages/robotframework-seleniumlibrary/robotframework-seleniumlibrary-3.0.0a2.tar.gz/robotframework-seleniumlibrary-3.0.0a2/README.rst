a02b175221666c8f706568ed0aa7bc48271a7a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:33 +0000

    Add package robotframework-seleniumlibrary/3.0.0a2/robotframework-seleniumlibrary-3.0.0a2.tar.gz

---
+++ packages/robotframework-seleniumlibrary/robotframework-seleniumlibrary-3.0.0a2.tar.gz/robotframework-seleniumlibrary-3.0.0a2/README.rst
@@ -0,0 +1,80 @@
+SeleniumLibrary
+===============
+
+.. image:: https://api.travis-ci.org/robotframework/SeleniumLibrary.png
+    :target: http://travis-ci.org/robotframework/SeleniumLibrary
+
+.. image:: https://img.shields.io/pypi/v/robotframework-seleniumlibrary.svg?label=version
+    :target: https://pypi.python.org/pypi/robotframework-seleniumlibrary
+
+.. image:: https://img.shields.io/pypi/l/robotframework-seleniumlibrary.svg
+    :target: http://www.apache.org/licenses/LICENSE-2.0
+
+Introduction
+------------
+
+SeleniumLibrary is a web testing library for `Robot Framework`_ that utilizes
+the popular Selenium_ tool internally. See the `keyword documentation`_
+for available keywords and `wiki`_ for some more general information
+about the library.
+
+SeleniumLibrary is based on the `old SeleniumLibrary`_ that was forked to
+Selenium2Library and then later renamed back to SeleniumLibrary.
+
+The new SeleniumLibrary is currently in pre-release phase. The final
+SeleniumLibrary 3.0 release is planned for early September.
+
+Installation
+------------
+
+The recommended installation method is using pip_::
+
+    pip install --pre robotframework-seleniumlibrary
+
+Notice that ``--pre`` is needed to get the current SeleniumLibrary,
+not the `old SeleniumLibrary`_, until the final SeleniumLibrary 3.0
+is released.
+
+To install the old Selenium2Library, use this command instead::
+
+    pip install robotframework-selenium2library
+
+See `INSTALL.rst`_ for more details about installation.
+
+Usage
+-----
+
+To write tests with Robot Framework and SeleniumLibrary,
+SeleniumLibrary must be imported into your Robot test suite.
+See `Robot Framework User Guide`_ for more information.
+
+A demo project illustrating how to use this library can be found from:
+https://bitbucket.org/robotframework/webdemo
+
+Support
+-------
+
+Best places to ask for support:
+
+- `robotframework-users`_ mailing list
+- ``#seleniumlibrary`` channel on `Robot Framework Slack`_
+
+When asking for help or reporting problems, please include the following
+information:
+
+- Full description of what you are trying to do and expected outcome
+- Version number of SeleniumLibrary, Selenium, and Robot Framework
+- Version number of the browser and the browser driver
+- Traceback or other debug output containing error information
+
+.. _Robot Framework: http://robotframework.org
+.. _Selenium: http://seleniumhq.org
+.. _Old SeleniumLibrary: https://github.com/robotframework/OldSeleniumLibrary/
+.. _pip: http://pip-installer.org
+.. _Wiki: https://github.com/robotframework/SeleniumLibrary/wiki
+.. _Keyword Documentation: http://robotframework.org/SeleniumLibrary/SeleniumLibrary.html
+.. _INSTALL.rst: https://github.com/robotframework/SeleniumLibrary/blob/master/INSTALL.rst
+.. _BUILD.rst: https://github.com/robotframework/SeleniumLibrary/blob/master/BUILD.rst
+.. _Robot Framework User Guide: http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html
+.. _robotframework-users: http://groups.google.com/group/robotframework-users
+.. _Robot Framework Slack: https://robotframework-slack-invite.herokuapp.com/
---
+++ packages/robotframework-seleniumlibrary/robotframework-seleniumlibrary-3.0.0a2.tar.gz/robotframework-seleniumlibrary-3.0.0a2/src/SeleniumLibrary/keywords/runonfailure.py
@@ -0,0 +1,77 @@
+# Copyright 2008-2011 Nokia Networks
+# Copyright 2011-2016 Ryan Tomac, Ed Manlove and contributors
+# Copyright 2016-     Robot Framework Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from robot.libraries.BuiltIn import BuiltIn
+
+from SeleniumLibrary.base import LibraryComponent, keyword
+
+
+class RunOnFailureKeywords(LibraryComponent):
+
+    @keyword
+    def register_keyword_to_run_on_failure(self, keyword):
+        """Sets the keyword to execute when a SeleniumLibrary keyword fails.
+
+        `keyword_name` is the name of a keyword (from any available
+        libraries) that  will be executed if a SeleniumLibrary keyword fails.
+        It is not possible to use a keyword that requires arguments.
+        Using the value "Nothing" will disable this feature altogether.
+
+        The initial keyword to use is set in `importing`, and the
+        keyword that is used by default is `Capture Page Screenshot`.
+        Taking a screenshot when something failed is a very useful
+        feature, but notice that it can slow down the execution.
+
+        This keyword returns the name of the previously registered
+        failure keyword. It can be used to restore the original
+        value later.
+
+        Example:
+        | Register Keyword To Run On Failure  | Log Source | # Run `Log Source` on failure. |
+        | ${previous kw}= | Register Keyword To Run On Failure  | Nothing    | # Disables run-on-failure functionality and stores the previous kw name in a variable. |
+        | Register Keyword To Run On Failure  | ${previous kw} | # Restore to the previous keyword. |
+        """
+        old_keyword = self.ctx._run_on_failure_keyword
+        old_keyword_text = old_keyword if old_keyword else "No keyword"
+
+        new_keyword = keyword if keyword.strip().lower() != "nothing" else None
+        new_keyword_text = new_keyword if new_keyword else "No keyword"
+
+        self.ctx._run_on_failure_keyword = new_keyword
+        self.info('%s will be run on failure.' % new_keyword_text)
+
+        return old_keyword_text
+
+    def run_on_failure(self):
+        if not self.ctx._run_on_failure_keyword:
+            return
+        if self.ctx._running_on_failure_routine:
+            return
+        self.ctx._running_on_failure_routine = True
+        try:
+            BuiltIn().run_keyword(self.ctx._run_on_failure_keyword)
+        except Exception as err:
+            self.run_on_failure_error(err)
+        finally:
+            self.ctx._running_on_failure_routine = False
+
+    def run_on_failure_error(self, err):
+        err = ("Keyword '%s' could not be run on failure: %s"
+               % (self.ctx._run_on_failure_keyword, err))
+        if hasattr(self, 'warn'):
+            self.warn(err)
+            return
+        raise Exception(err)
