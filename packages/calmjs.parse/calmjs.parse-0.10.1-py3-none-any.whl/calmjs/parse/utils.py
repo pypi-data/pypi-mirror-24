c027166b2c17ce39db532edbd9b7c1c0339973fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:59 +0000

    Add package calmjs.parse/0.10.1/calmjs.parse-0.10.1-py3-none-any.whl

---
+++ packages/calmjs.parse/calmjs.parse-0.10.1-py3-none-any.whl/calmjs/parse/utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Various utilities and helpers
+"""
+
+import sys
+
+try:
+    from pkg_resources import working_set
+    from pkg_resources import Requirement
+    ply_dist = working_set.find(Requirement.parse('ply'))
+except ImportError:  # pragma: no cover
+    ply_dist = None
+
+py_major = sys.version_info.major
+
+
+def generate_tab_names(name):
+    """
+    Return the names to lextab and yacctab modules for the given module
+    name.  Typical usage should be like so::
+
+    >>> lextab, yacctab = generate_tab_names(__name__)
+    """
+
+    package_name, module_name = name.rsplit('.', 1)
+
+    version = ply_dist.version.replace(
+        '.', '_') if ply_dist is not None else 'unknown'
+    data = (package_name, module_name, py_major, version)
+    lextab = '%s.lextab_%s_py%d_ply%s' % data
+    yacctab = '%s.yacctab_%s_py%d_ply%s' % data
+    return lextab, yacctab
+
+
+def format_lex_token(token):
+    return '%r at %s:%s' % (
+        token.value, token.lineno, getattr(token, 'colno', '?'))
