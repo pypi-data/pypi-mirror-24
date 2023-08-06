32b74d5103152fb028027ce60ad618fbbee2f1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:33 +0000

    Add package list-within-list/1.3.0/list_within_list-1.3.0.tar.gz

---
+++ packages/list-within-list/list_within_list-1.3.0.tar.gz/list_within_list-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+ name = 'list_within_list',
+ version = '1.3.0',
+ py_modules = ['list_within_list'],
+ author = 'panneer',
+ author_email = 'panneer.selvamqa@outlook.com',
+ url = 'http://www.google.com',
+ description = 'A simple printer of nested lists',
+ )
