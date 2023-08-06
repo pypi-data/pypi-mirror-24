3627964d0f43ca4d976f255aaf3fc860e46ebd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:27 +0000

    Add package meraki/0.13/meraki-0.13.tar.gz

---
+++ packages/meraki/meraki-0.13.tar.gz/meraki-0.13/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'meraki',
+  packages = ['meraki'],
+  version = '0.13',
+  description = 'Cisco Meraki Dashboard API Python 3.6 Module',
+  author = 'Shiyue Cheng',
+  author_email = 'shiyue@gmail.com',
+  url = 'https://github.com/shiyuechengineer/meraki-dashboard',
+  download_url = 'https://github.com/shiyuechengineer/meraki-dashboard/archive/0.11.tar.gz',
+  keywords = ['meraki', 'dashboard', 'cisco'],
+  classifiers = [],
+)
