ae2ba477cc3acd97f6b5e9fe6b93e7e72b61c28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:28 +0000

    Add package infrasim-compute/3.5.1.0rc1/infrasim_compute-3.5.1.0rc1-py2-none-any.whl

---
+++ packages/infrasim-compute/infrasim_compute-3.5.1.0rc1-py2-none-any.whl/infrasim/ipmi.py
@@ -0,0 +1,74 @@
+'''
+*********************************************************
+Copyright @ 2015 EMC Corporation All Rights Reserved
+*********************************************************
+'''
+# -*- coding: utf-8 -*-
+
+import os
+import yaml
+import config
+from . import run_command, logger, ArgsNotCorrect, CommandNotFound, CommandRunFailed, InfraSimError
+from model import CBMC, CNode
+
+
+def get_ipmi():
+    try:
+        code, ipmi_cmd = run_command("which ipmi_sim")
+        return ipmi_cmd.strip(os.linesep)
+    except CommandRunFailed:
+        raise CommandNotFound("ipmi_sim")
+
+
+def status_ipmi():
+    try:
+        run_command("pidof ipmi_sim")
+        print "InfraSim IPMI service is running"
+    except CommandRunFailed:
+        print "Infrasim IPMI service is stopped"
+
+
+def start_ipmi(conf_file=config.infrasim_default_config):
+    try:
+        with open(conf_file, 'r') as f_yml:
+            conf = yaml.load(f_yml)
+
+        node = CNode(node_info=conf)
+        if "name" in conf:
+            node.set_node_name(conf["name"])
+
+        node.init()
+
+        bmc = CBMC(conf.get('bmc', {}))
+        node_name = conf["name"] if "name" in conf else "node-0"
+        bmc.set_task_name("{}-bmc".format(node_name))
+        bmc.set_log_path("/var/log/infrasim/{}/openipmi.log".
+                         format(node_name))
+        bmc.set_type(conf["type"])
+        bmc.enable_sol(False)
+        bmc.set_workspace(node.workspace.get_workspace())
+        bmc.init()
+        bmc.precheck()
+        cmd = bmc.get_commandline()
+        logger.debug(cmd)
+        run_command(cmd+" &", True, None, None)
+
+        logger.info("bmc start")
+    except CommandRunFailed as e:
+        logger.error(e.value)
+        raise e
+    except ArgsNotCorrect as e:
+        logger.error(e.value)
+        raise e
+    except InfraSimError as e:
+        logger.error(e.value)
+        raise e
+
+
+def stop_ipmi(conf_file=config.infrasim_default_config):
+    ipmi_stop_cmd = "pkill ipmi_sim"
+    try:
+        run_command(ipmi_stop_cmd, True, None, None)
+        logger.info("ipmi stopped")
+    except CommandRunFailed:
+        logger.error("ipmi stop failed")
