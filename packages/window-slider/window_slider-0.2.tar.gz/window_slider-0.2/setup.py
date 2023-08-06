3993b5602a1c30e4cd080052f74a5490235440c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:26 +0000

    Add package window-slider/0.2/window_slider-0.2.tar.gz

---
+++ packages/window-slider/window_slider-0.2.tar.gz/window_slider-0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='window_slider',
+    packages=['window_slider'],
+    version='0.2',
+    description='A lib to implement sliding window with overlapping',
+    author='Ravi Sharma',
+    author_email='ravi.sharma.cs11@gmail.com',
+    url='https://github.com/ravi2020/sliding_window',
+    keywords=['window', 'sliding', 'overlap'],
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
+)
