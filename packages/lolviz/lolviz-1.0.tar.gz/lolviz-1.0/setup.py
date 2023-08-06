ee5529acf0b4dcd916de97321e657a517e5d6316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:21 +0000

    Add package lolviz/1.0/lolviz-1.0.tar.gz

---
+++ packages/lolviz/lolviz-1.0.tar.gz/lolviz-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='lolviz',
+    version='1.0',
+    url='https://github.com/parrt/lolviz',
+    license='BSD',
+    author='Terence Parr',
+    author_email='parrt@antlr.org',
+    install_requires=['graphviz'],
+    description='A simple Python data-structure visualization tool for lists of lists, lists, dictionaries',
+    keywords='visualization data structures',
+    python_requires='<3',
+    classifiers=['Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Developers']
+)
